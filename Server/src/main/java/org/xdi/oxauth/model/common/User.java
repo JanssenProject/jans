318fbbf9f3cc7a1a1f0271e06ddeb760317afb21
Oleksiy Tataryn <otataryn@gmail.com>
2014-09-30 19:34:34 +0300

    removed extra objectclasses from user entry

--- Server/src/main/java/org/xdi/oxauth/model/common/User.java
+++ Server/src/main/java/org/xdi/oxauth/model/common/User.java
@@ -16,7 +16,7 @@ import org.gluu.site.ldap.persistence.annotation.LdapObjectClass;
  * @author Yuriy Movchan Date: 06/11/2013
  */
 @LdapEntry
-@LdapObjectClass(values = { "top", "person", "organizationalPerson", "inetOrgPerson", "gluuPerson","eduPerson", "oxEntry", "oxCustomAttributes" })
+@LdapObjectClass(values = { "top", "gluuPerson" })
 public class User extends SimpleUser {
 
     private static final long serialVersionUID = 6634191420188575733L;
