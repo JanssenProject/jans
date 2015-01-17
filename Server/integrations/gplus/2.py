3cc05e2d3d155ec985da3d5cfc1dfa5685c356d6
yurem <Yuriy.Movchan@gmail.com>
2015-01-17 06:07:26 +0200

    Don't use old Id gen API

---
+++ Server/integrations/gplus/2.py
@@ -0,0 +1,77 @@
+from org.jboss.seam.security import Identity
+from org.xdi.model.custom.script.type.auth import CustomAuthenticatorType
+from org.xdi.oxauth.service import UserService
+from org.xdi.util import StringHelper
+
+import java
+
+class CustomAuthenticator(CustomAuthenticatorType):
+    def __init__(self, currentTimeMillis):
+        self.currentTimeMillis = currentTimeMillis
+
+    def init(self, configurationAttributes):
+        print "Basic. Initialization"
+        print "Basic. Initialized successfully"
+        return True   
+
+    def destroy(self, configurationAttributes):
+        print "Basic. Destroy"
+        print "Basic. Destroyed successfully"
+        return True
+
+    def getApiVersion(self):
+        return 1
+
+    def isValidAuthenticationMethod(self, usageType, configurationAttributes):
+        return True
+
+    def getAlternativeAuthenticationMethod(self, usageType, configurationAttributes):
+        return None
+
+    def authenticate(self, configurationAttributes, requestParameters, step):
+        if (step == 1):
+            print "Basic. Authenticate for step 1"
+
+            credentials = Identity.instance().getCredentials()
+            user_name = credentials.getUsername()
+            user_password = credentials.getPassword()
+
+            logged_in = False
+            if (StringHelper.isNotEmptyString(user_name) and StringHelper.isNotEmptyString(user_password)):
+                userService = UserService.instance()
+                logged_in = userService.authenticate(user_name, user_password)
+
+            if (not logged_in):
+                return False
+# Commented out becuase we do the same in AuthenticationService.authenticate method
+#
+#            user = userService.getUser(user_name)
+#            if (user == None):
+#                print "Basic. Authenticate for step 1. Failed to find user in local LDAP"
+#                return False
+#
+#            # Store user to allow use this module for web services 
+#            credentials.setUser(user);
+
+            return True
+        else:
+            return False
+
+    def prepareForStep(self, configurationAttributes, requestParameters, step):
+        if (step == 1):
+            print "Basic. Prepare for Step 1"
+            return True
+        else:
+            return False
+
+    def getExtraParametersForStep(self, configurationAttributes, step):
+        return None
+
+    def getCountAuthenticationSteps(self, configurationAttributes):
+        return 1
+
+    def getPageForStep(self, configurationAttributes, step):
+        return ""
+
+    def logout(self, configurationAttributes, requestParameters):
+        return True
