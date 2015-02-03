af8cad7f00e9a8371ee869afda63920277a8d81f
yurem <Yuriy.Movchan@gmail.com>
2015-02-03 21:55:25 +0200

    Fix base dirt detection

--- Server/src/main/java/org/xdi/oxauth/model/config/ConfigurationFactory.java
+++ Server/src/main/java/org/xdi/oxauth/model/config/ConfigurationFactory.java
@@ -40,10 +40,19 @@ public class ConfigurationFactory {
 
     private static final Log LOG = Logging.getLog(ConfigurationFactory.class);
 
-    private static final String BASE_DIR =
-            System.getProperty("catalina.base") != null ? System.getProperty("catalina.base") :
-            System.getProperty("catalina.home") != null ? System.getProperty("catalina.home") :
-            System.getProperty("jbosss.home.dir");
+    static {
+    	 if (System.getProperty("catalina.base") != null) {
+    		 BASE_DIR = System.getProperty("catalina.base");
+    	 } else if (System.getProperty("catalina.home") != null) {
+    		 BASE_DIR = System.getProperty("catalina.home");
+    	 } else if (System.getProperty("jboss.home.dir") != null) {
+    		 BASE_DIR = System.getProperty("jboss.home.dir");
+    	 } else {
+    		 BASE_DIR = null;
+    	 } 
+    }
+    
+    private static final String BASE_DIR;
     private static final String DIR = BASE_DIR + File.separator + "conf" + File.separator;
 
     private static final String CONFIG_FILE_PATH = DIR + "oxauth-config.xml";
