900ebea7316cf95e3bc7f6cbdca08a8508409faa
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2015-02-09 17:49:03 -0400

    Move claims-supported and its mapping from XML config to LDAP #7
    Added oxAuthClaimName to the gluuAttribute Object Class

--- oxService/src/main/java/org/xdi/model/GluuAttribute.java
+++ oxService/src/main/java/org/xdi/model/GluuAttribute.java
@@ -2,154 +2,161 @@
  * oxCore is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
  *
  * Copyright (c) 2014, Gluu
- */package org.xdi.model;
-
-import java.io.Serializable;
-
-import javax.persistence.Transient;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+ */
+package org.xdi.model;
 
 import lombok.Data;
-
 import org.gluu.site.ldap.persistence.annotation.LdapAttribute;
 import org.gluu.site.ldap.persistence.annotation.LdapEntry;
 import org.gluu.site.ldap.persistence.annotation.LdapObjectClass;
 import org.xdi.ldap.model.Entry;
 import org.xdi.ldap.model.GluuStatus;
 
+import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
 /**
  * Attribute Metadata
- * 
- * @author Yuriy Movchan Date: 10.07.2010
+ *
+ * @author Yuriy Movchan
+ * @author Javier Rojas Blum
+ * @version February 9, 2015
  */
-@LdapEntry(sortBy = { "displayName" })
-@LdapObjectClass(values = { "top", "gluuAttribute" })
+@LdapEntry(sortBy = {"displayName"})
+@LdapObjectClass(values = {"top", "gluuAttribute"})
 public @Data class GluuAttribute extends Entry implements Serializable {
 
-	private static final long serialVersionUID = 4817004894646725606L;
+    private static final long serialVersionUID = 4817004894646725606L;
 
-	private transient boolean selected;
+    private transient boolean selected;
 
-	@LdapAttribute(ignoreDuringUpdate = true)
-	private String inum;
-	
-	@LdapAttribute(name = "oxAttributeType")
-	private String type;
-	
-	@LdapAttribute
-	private String lifetime;
-	
-	@LdapAttribute(name = "oxSourceAttribute")
-	private String sourceAttribute;
-	
-	@LdapAttribute
-	private String salt;
-	
-	@LdapAttribute(name = "oxNameIdType")
-	private String nameIdType;	
-	
-	@NotNull
-	@Pattern(regexp = "^[a-zA-Z_]+$", message = "Name should contain only letters and underscores")
-	@Size(min = 1, max = 30, message = "Length of the Name should be between 1 and 30")
-	@LdapAttribute(name = "gluuAttributeName")
-	private String name;
+    @LdapAttribute(ignoreDuringUpdate = true)
+    private String inum;
 
-	@NotNull
-	@Size(min = 0, max = 60, message = "Length of the Display Name should not exceed 60")
-	@LdapAttribute
-	private String displayName;
+    @LdapAttribute(name = "oxAttributeType")
+    private String type;
 
-	@NotNull
-	@Size(min = 0, max = 4000, message = "Length of the Description should not exceed 4000")
-	@LdapAttribute
-	private String description;
+    @LdapAttribute
+    private String lifetime;
 
-	@LdapAttribute(name = "gluuAttributeOrigin")
-	private String origin;
+    @LdapAttribute(name = "oxSourceAttribute")
+    private String sourceAttribute;
 
-	@NotNull
-	@LdapAttribute(name = "gluuAttributeType")
-	private GluuAttributeDataType dataType;
+    @LdapAttribute
+    private String salt;
 
-	@NotNull
-	@LdapAttribute(name = "gluuAttributeEditType")
-	private GluuUserRole[] editType;
+    @LdapAttribute(name = "oxNameIdType")
+    private String nameIdType;
 
-	@NotNull
-	@LdapAttribute(name = "gluuAttributeViewType")
-	private GluuUserRole[] viewType;
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z_]+$", message = "Name should contain only letters and underscores")
+    @Size(min = 1, max = 30, message = "Length of the Name should be between 1 and 30")
+    @LdapAttribute(name = "gluuAttributeName")
+    private String name;
 
-	@LdapAttribute(name = "gluuAttributeUsageType")
-	private GluuAttributeUsageType[] usageType;
+    @NotNull
+    @Size(min = 0, max = 60, message = "Length of the Display Name should not exceed 60")
+    @LdapAttribute
+    private String displayName;
 
-	@LdapAttribute(name = "seeAlso")
-	private String seeAlso;
+    @NotNull
+    @Size(min = 0, max = 4000, message = "Length of the Description should not exceed 4000")
+    @LdapAttribute
+    private String description;
 
-	@LdapAttribute(name = "gluuStatus")
-	private GluuStatus status;
+    @LdapAttribute(name = "gluuAttributeOrigin")
+    private String origin;
 
-	@LdapAttribute(name = "gluuSAML1URI")
-	private String saml1Uri;
+    @NotNull
+    @LdapAttribute(name = "gluuAttributeType")
+    private GluuAttributeDataType dataType;
 
-	@LdapAttribute(name = "gluuSAML2URI")
-	private String saml2Uri;
+    @NotNull
+    @LdapAttribute(name = "gluuAttributeEditType")
+    private GluuUserRole[] editType;
 
-	@LdapAttribute(ignoreDuringUpdate = true)
-	private String urn;
+    @NotNull
+    @LdapAttribute(name = "gluuAttributeViewType")
+    private GluuUserRole[] viewType;
 
-	@LdapAttribute(name = "oxSCIMCustomAttribute")
-	private ScimCustomAtribute oxSCIMCustomAttribute;
+    @LdapAttribute(name = "gluuAttributeUsageType")
+    private GluuAttributeUsageType[] usageType;
 
-	@LdapAttribute(name = "oxMultivaluedAttribute")
-	private OxMultivalued oxMultivaluedAttribute;
+    @LdapAttribute(name = "gluuLdapAttributeName")
+    private String gluuLdapAttributeName;
 
-	@Transient
-	private boolean custom;
+    @LdapAttribute(name = "oxAuthClaimName")
+    private String oxAuthClaimName;
 
-	@Transient
-	private boolean requred;
-	
-	@LdapAttribute(name = "gluuRegExp")
-	private String regExp;
-	
-	@LdapAttribute(name = "gluuTooltip")
-	private String gluuTooltip;
+    @LdapAttribute(name = "seeAlso")
+    private String seeAlso;
 
-	public boolean allowEditBy(GluuUserRole role) {
-		return GluuUserRole.containsRole(editType, role);
-	}
+    @LdapAttribute(name = "gluuStatus")
+    private GluuStatus status;
 
-	public boolean allowViewBy(GluuUserRole role) {
-		return GluuUserRole.containsRole(viewType, role);
-	}
+    @LdapAttribute(name = "gluuSAML1URI")
+    private String saml1Uri;
 
-	public boolean isAdminCanAccess() {
-		return isAdminCanView() | isAdminCanEdit();
-	}
+    @LdapAttribute(name = "gluuSAML2URI")
+    private String saml2Uri;
 
-	public boolean isAdminCanView() {
-		return allowViewBy(GluuUserRole.ADMIN);
-	}
+    @LdapAttribute(ignoreDuringUpdate = true)
+    private String urn;
 
-	public boolean isAdminCanEdit() {
-		return allowEditBy(GluuUserRole.ADMIN);
-	}
+    @LdapAttribute(name = "oxSCIMCustomAttribute")
+    private ScimCustomAtribute oxSCIMCustomAttribute;
 
-	public boolean isUserCanAccess() {
-		return isUserCanView() | isUserCanEdit();
-	}
+    @LdapAttribute(name = "oxMultivaluedAttribute")
+    private OxMultivalued oxMultivaluedAttribute;
 
-	public boolean isUserCanView() {
-		return allowViewBy(GluuUserRole.USER);
-	}
+    @Transient
+    private boolean custom;
 
-	public boolean isWhitePagesCanView() {
-		return allowViewBy(GluuUserRole.WHITEPAGES);
-	}
+    @Transient
+    private boolean requred;
 
-	public boolean isUserCanEdit() {
-		return allowEditBy(GluuUserRole.USER);
-	}
+    @LdapAttribute(name = "gluuRegExp")
+    private String regExp;
+
+    @LdapAttribute(name = "gluuTooltip")
+    private String gluuTooltip;
+
+    public boolean allowEditBy(GluuUserRole role) {
+        return GluuUserRole.containsRole(editType, role);
+    }
+
+    public boolean allowViewBy(GluuUserRole role) {
+        return GluuUserRole.containsRole(viewType, role);
+    }
+
+    public boolean isAdminCanAccess() {
+        return isAdminCanView() | isAdminCanEdit();
+    }
+
+    public boolean isAdminCanView() {
+        return allowViewBy(GluuUserRole.ADMIN);
+    }
+
+    public boolean isAdminCanEdit() {
+        return allowEditBy(GluuUserRole.ADMIN);
+    }
+
+    public boolean isUserCanAccess() {
+        return isUserCanView() | isUserCanEdit();
+    }
+
+    public boolean isUserCanView() {
+        return allowViewBy(GluuUserRole.USER);
+    }
+
+    public boolean isWhitePagesCanView() {
+        return allowViewBy(GluuUserRole.WHITEPAGES);
+    }
+
+    public boolean isUserCanEdit() {
+        return allowEditBy(GluuUserRole.USER);
+    }
 }
