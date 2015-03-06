bad4c5d7a171878d81470df4321e8312a24e0efc
yurem <Yuriy.Movchan@gmail.com>
2015-03-06 15:36:26 +0200

    Update OneId to support latest API

--- Server/integrations/oneid/OneIdExternalAuthenticator.py
+++ Server/integrations/oneid/OneIdExternalAuthenticator.py
@@ -8,7 +8,6 @@ from org.xdi.model.custom.script.type.auth import PersonAuthenticationType
 from org.xdi.oxauth.service import UserService
 from org.xdi.oxauth.service import AuthenticationService
 from org.xdi.oxauth.service.net import HttpService
-from org.xdi.util.security import StringEncrypter 
 from org.xdi.util import StringHelper
 from org.xdi.util import ArrayHelper
 from java.util import Arrays
@@ -27,7 +26,17 @@ class PersonAuthentication(PersonAuthenticationType):
         self.currentTimeMillis = currentTimeMillis
 
     def init(self, configurationAttributes):
+        print "OneId. Initialization"
+        print "OneId. Initialized successfully"
         return True   
+
+    def destroy(self, configurationAttributes):
+        print "OneId. Destroy"
+        print "OneId. Destroyed successfully"
+        return True
+
+    def getApiVersion(self):
+        return 1
 
     def isValidAuthenticationMethod(self, usageType, configurationAttributes):
         return True
@@ -41,8 +50,6 @@ class PersonAuthentication(PersonAuthenticationType):
         userService = UserService.instance()
         httpService = HttpService.instance();
 
-        stringEncrypter = StringEncrypter.defaultInstance()
-
         server_flag = configurationAttributes.get("oneid_server_flag").getValue2()
         callback_attrs = configurationAttributes.get("oneid_callback_attrs").getValue2()
         creds_file = configurationAttributes.get("oneid_creds_file").getValue2()
@@ -54,16 +61,16 @@ class PersonAuthentication(PersonAuthenticationType):
         authn.creds_file = creds_file;
 
         if (step == 1):
-            print "OneID authenticate for step 1"
+            print "OneId. Authenticate for step 1"
 
             # Find OneID request
             json_data_array = requestParameters.get("json_data")
             if ArrayHelper.isEmpty(json_data_array):
-                print "OneID authenticate for step 1. json_data is empty"
+                print "OneId. Authenticate for step 1. json_data is empty"
                 return False
 
             request = json_data_array[0]
-            print "OneID authenticate for step 1. request: " + request
+            print "OneId. Authenticate for step 1. request: " + request
 
             if (StringHelper.isEmptyString(request)):
                 return False
@@ -75,13 +82,13 @@ class PersonAuthentication(PersonAuthenticationType):
             auth_data = httpService.encodeBase64(authn.api_id + ":" + authn.api_key)
             http_response = httpService.executePost(http_client, authn.helper_server + "/validate", auth_data, request, ContentType.APPLICATION_JSON)
             validation_content = httpService.convertEntityToString(httpService.getResponseContent(http_response))
-            print "OneID authenticate for step 1. validation_content: " + validation_content
+            print "OneId. Authenticate for step 1. validation_content: " + validation_content
             
             if (StringHelper.isEmptyString(validation_content)):
                 return False
 
             validation_resp = json.loads(validation_content)
-            print "OneID authenticate for step 1. validation_resp: " + str(validation_resp)
+            print "OneId. Authenticate for step 1. validation_resp: " + str(validation_resp)
 
             if (not authn.success(validation_resp)):
                 return False
@@ -91,38 +98,41 @@ class PersonAuthentication(PersonAuthenticationType):
                 response[x] = validation_resp[x]
 
             oneid_user_uid = response['uid']
-            print "OneID authenticate for step 1. oneid_user_uid: " + oneid_user_uid
+            print "OneId. Authenticate for step 1. oneid_user_uid: " + oneid_user_uid
 
             # Check if the is user with specified oneid_user_uid
             find_user_by_uid = userService.getUserByAttribute("oxExternalUid", "oneid:" + oneid_user_uid)
 
             if (find_user_by_uid == None):
-                print "OneID authenticate for step 1. Failed to find user"
-                print "OneID authenticate for step 1. Setting count steps to 2"
+                print "OneId. Authenticate for step 1. Failed to find user"
+                print "OneId. Authenticate for step 1. Setting count steps to 2"
                 context.set("oneid_count_login_steps", 2)
-                context.set("oneid_user_uid", stringEncrypter.encrypt(oneid_user_uid))
+                context.set("oneid_user_uid", oneid_user_uid)
                 return True
 
             found_user_name = find_user_by_uid.getUserId()
-            print "OneID authenticate for step 1. found_user_name: " + found_user_name
+            print "OneId. Authenticate for step 1. found_user_name: " + found_user_name
 
             credentials = Identity.instance().getCredentials()
             credentials.setUsername(found_user_name)
             credentials.setUser(find_user_by_uid)
             
-            print "OneID authenticate for step 1. Setting count steps to 1"
+            print "OneId. Authenticate for step 1. Setting count steps to 1"
             context.set("oneid_count_login_steps", 1)
 
             return True
         elif (step == 2):
-            print "OneID authenticate for step 2"
-            
-            oneid_user_uid_array = requestParameters.get("oneid_user_uid")
-            if ArrayHelper.isEmpty(oneid_user_uid_array):
-                print "OneID authenticate for step 2. oneid_user_uid is empty"
+            print "OneId. Authenticate for step 2"
+
+            sessionAttributes = context.get("sessionAttributes")
+            if (sessionAttributes == None) or not sessionAttributes.isSet("oneid_user_uid"):
+                print "OneId. Authenticate for step 2. oneid_user_uid is empty"
                 return False
 
-            oneid_user_uid = stringEncrypter.decrypt(oneid_user_uid_array[0])
+            oneid_user_uid = sessionAttributes.get("oneid_user_uid")
+            passed_step1 = StringHelper.isNotEmptyString(oneid_user_uid)
+            if (not passed_step1):
+                return False
 #
             credentials = Identity.instance().getCredentials()
 
@@ -151,13 +161,13 @@ class PersonAuthentication(PersonAuthenticationType):
                 # Add oneid_user_uid to user one id UIDs
                 find_user_by_uid = userService.addUserAttribute(user_name, "oxExternalUid", "oneid:" + oneid_user_uid)
                 if (find_user_by_uid == None):
-                    print "OneID authenticate for step 2. Failed to update current user"
+                    print "OneId. Authenticate for step 2. Failed to update current user"
                     return False
 
                 return True
             else:
                 found_user_name = find_user_by_uid.getUserId()
-                print "OneID authenticate for step 2. found_user_name: " + found_user_name
+                print "OneId. Authenticate for step 2. found_user_name: " + found_user_name
     
                 if StringHelper.equals(user_name, found_user_name):
                     return True
@@ -181,19 +191,19 @@ class PersonAuthentication(PersonAuthenticationType):
         authn.creds_file = creds_file; 
 
         if (step == 1):
-            print "OneID prepare for step 1"
+            print "OneId. Prepare for step 1"
 
             auth_mode_array = requestParameters.get("auth_mode")
             if ArrayHelper.isEmpty(auth_mode_array):
-                print "OneID prepare for step 1. auth_mode is empty"
+                print "OneId. Prepare for step 1. auth_mode is empty"
                 return False
 
             request = FacesContext.getCurrentInstance().getExternalContext().getRequest()
             validation_page = request.getContextPath() + "/postlogin?" + "request_uri=&" + authenticationService.parametersAsString()
-            print "OneID prepare for step 1. validation_page: " + validation_page
+            print "OneId. Prepare for step 1. validation_page: " + validation_page
 
             oneid_login_button = authn.draw_signin_button(validation_page, callback_attrs, True)
-            print "OneID prepare for step 1. oneid_login_button: " + oneid_login_button
+            print "OneId. Prepare for step 1. oneid_login_button: " + oneid_login_button
             
             context.set("oneid_login_button", oneid_login_button)
             context.set("oneid_script_header", authn.script_header)
@@ -201,7 +211,7 @@ class PersonAuthentication(PersonAuthenticationType):
 
             return True
         elif (step == 2):
-            print "OneID prepare for step 2"
+            print "OneId. Prepare for step 2"
 
             return True
         else:
@@ -227,6 +237,3 @@ class PersonAuthentication(PersonAuthenticationType):
 
     def logout(self, configurationAttributes, requestParameters):
         return True
-
-    def getApiVersion(self):
-        return 3
