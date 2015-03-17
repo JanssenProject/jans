1960e11da33997f42dae90358d7c8f6c20e86f47
yurem <Yuriy.Movchan@gmail.com>
2015-03-17 12:33:44 +0200

    Don't use loombok in GluuAttribute. Remove old service

--- oxService/src/main/java/org/xdi/model/GluuAttribute.java
+++ oxService/src/main/java/org/xdi/model/GluuAttribute.java
@@ -5,18 +5,18 @@
  */
 package org.xdi.model;
 
-import lombok.Data;
-import org.gluu.site.ldap.persistence.annotation.LdapAttribute;
-import org.gluu.site.ldap.persistence.annotation.LdapEntry;
-import org.gluu.site.ldap.persistence.annotation.LdapObjectClass;
-import org.xdi.ldap.model.Entry;
-import org.xdi.ldap.model.GluuStatus;
+import java.io.Serializable;
 
 import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-import java.io.Serializable;
+
+import org.gluu.site.ldap.persistence.annotation.LdapAttribute;
+import org.gluu.site.ldap.persistence.annotation.LdapEntry;
+import org.gluu.site.ldap.persistence.annotation.LdapObjectClass;
+import org.xdi.ldap.model.Entry;
+import org.xdi.ldap.model.GluuStatus;
 
 /**
  * Attribute Metadata
@@ -27,7 +27,7 @@ import java.io.Serializable;
  */
 @LdapEntry(sortBy = {"displayName"})
 @LdapObjectClass(values = {"top", "gluuAttribute"})
-public @Data class GluuAttribute extends Entry implements Serializable {
+public class GluuAttribute extends Entry implements Serializable {
 
     private static final long serialVersionUID = 4817004894646725606L;
 
@@ -124,7 +124,231 @@ public @Data class GluuAttribute extends Entry implements Serializable {
     @LdapAttribute(name = "gluuTooltip")
     private String gluuTooltip;
 
-    public boolean allowEditBy(GluuUserRole role) {
+    public boolean isSelected() {
+		return selected;
+	}
+
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+
+	public String getInum() {
+		return inum;
+	}
+
+	public void setInum(String inum) {
+		this.inum = inum;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getLifetime() {
+		return lifetime;
+	}
+
+	public void setLifetime(String lifetime) {
+		this.lifetime = lifetime;
+	}
+
+	public String getSourceAttribute() {
+		return sourceAttribute;
+	}
+
+	public void setSourceAttribute(String sourceAttribute) {
+		this.sourceAttribute = sourceAttribute;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	public String getNameIdType() {
+		return nameIdType;
+	}
+
+	public void setNameIdType(String nameIdType) {
+		this.nameIdType = nameIdType;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+	public GluuAttributeDataType getDataType() {
+		return dataType;
+	}
+
+	public void setDataType(GluuAttributeDataType dataType) {
+		this.dataType = dataType;
+	}
+
+	public GluuUserRole[] getEditType() {
+		return editType;
+	}
+
+	public void setEditType(GluuUserRole[] editType) {
+		this.editType = editType;
+	}
+
+	public GluuUserRole[] getViewType() {
+		return viewType;
+	}
+
+	public void setViewType(GluuUserRole[] viewType) {
+		this.viewType = viewType;
+	}
+
+	public GluuAttributeUsageType[] getUsageType() {
+		return usageType;
+	}
+
+	public void setUsageType(GluuAttributeUsageType[] usageType) {
+		this.usageType = usageType;
+	}
+
+	public String getGluuLdapAttributeName() {
+		return gluuLdapAttributeName;
+	}
+
+	public void setGluuLdapAttributeName(String gluuLdapAttributeName) {
+		this.gluuLdapAttributeName = gluuLdapAttributeName;
+	}
+
+	public String getOxAuthClaimName() {
+		return oxAuthClaimName;
+	}
+
+	public void setOxAuthClaimName(String oxAuthClaimName) {
+		this.oxAuthClaimName = oxAuthClaimName;
+	}
+
+	public String getSeeAlso() {
+		return seeAlso;
+	}
+
+	public void setSeeAlso(String seeAlso) {
+		this.seeAlso = seeAlso;
+	}
+
+	public GluuStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(GluuStatus status) {
+		this.status = status;
+	}
+
+	public String getSaml1Uri() {
+		return saml1Uri;
+	}
+
+	public void setSaml1Uri(String saml1Uri) {
+		this.saml1Uri = saml1Uri;
+	}
+
+	public String getSaml2Uri() {
+		return saml2Uri;
+	}
+
+	public void setSaml2Uri(String saml2Uri) {
+		this.saml2Uri = saml2Uri;
+	}
+
+	public String getUrn() {
+		return urn;
+	}
+
+	public void setUrn(String urn) {
+		this.urn = urn;
+	}
+
+	public ScimCustomAtribute getOxSCIMCustomAttribute() {
+		return oxSCIMCustomAttribute;
+	}
+
+	public void setOxSCIMCustomAttribute(ScimCustomAtribute oxSCIMCustomAttribute) {
+		this.oxSCIMCustomAttribute = oxSCIMCustomAttribute;
+	}
+
+	public OxMultivalued getOxMultivaluedAttribute() {
+		return oxMultivaluedAttribute;
+	}
+
+	public void setOxMultivaluedAttribute(OxMultivalued oxMultivaluedAttribute) {
+		this.oxMultivaluedAttribute = oxMultivaluedAttribute;
+	}
+
+	public boolean isCustom() {
+		return custom;
+	}
+
+	public void setCustom(boolean custom) {
+		this.custom = custom;
+	}
+
+	public boolean isRequred() {
+		return requred;
+	}
+
+	public void setRequred(boolean requred) {
+		this.requred = requred;
+	}
+
+	public String getRegExp() {
+		return regExp;
+	}
+
+	public void setRegExp(String regExp) {
+		this.regExp = regExp;
+	}
+
+	public String getGluuTooltip() {
+		return gluuTooltip;
+	}
+
+	public void setGluuTooltip(String gluuTooltip) {
+		this.gluuTooltip = gluuTooltip;
+	}
+
+	public boolean allowEditBy(GluuUserRole role) {
         return GluuUserRole.containsRole(editType, role);
     }
 
