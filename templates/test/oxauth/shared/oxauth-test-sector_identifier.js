c1980978a481d364ea67b71feaa0f8f06b4c260a
yurem <Yuriy.Movchan@gmail.com>
2015-04-01 19:13:11 +0300

    Add federation test data. Allow to render test templates in subfolders

--- templates/test/oxauth/shared/oxauth-test-sector_identifier.js
+++ templates/test/oxauth/shared/oxauth-test-sector_identifier.js
@@ -1,6 +1,6 @@
 [ "https://idevforce.com",
-    "https://localhost:8443/oxauth-rp/home.seam",
-    "https://%(hostname)s/oxauth-rp/home.seam",
+    "https://localhost:8443/oxauth-rp/home",
+    "https://%(hostname)s/oxauth-rp/home",
     "https://client.example.org/callback",
     "https://client.example.org/callback2",
     "https://client.other_company.example.net/callback",
