6fe51281df46b7abc80371009f3556011ff2c428
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2015-04-06 11:21:21 +0300

    uma : fixed equals for ClaimToken

--- Model/src/main/java/org/xdi/oxauth/model/uma/ClaimToken.java
+++ Model/src/main/java/org/xdi/oxauth/model/uma/ClaimToken.java
@@ -49,8 +49,23 @@ public class ClaimToken implements Serializable {
     }
 
 
-//        public Map<String, List<String>> getFormat() {
-//            return format;
-//        }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        ClaimToken that = (ClaimToken) o;
+
+        if (format != null ? !format.equals(that.format) : that.format != null) return false;
+        if (token != null ? !token.equals(that.token) : that.token != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = format != null ? format.hashCode() : 0;
+        result = 31 * result + (token != null ? token.hashCode() : 0);
+        return result;
+    }
 }
