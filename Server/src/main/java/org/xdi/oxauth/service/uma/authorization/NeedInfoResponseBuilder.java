fe206bfde6a91e1603652094efd795831513c4f0
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2015-04-14 15:27:38 +0300

    uma : added Trust Elevation support

---
+++ Server/src/main/java/org/xdi/oxauth/service/uma/authorization/NeedInfoResponseBuilder.java
@@ -0,0 +1,44 @@
+package org.xdi.oxauth.service.uma.authorization;
+
+import org.codehaus.jettison.json.JSONObject;
+import org.jboss.seam.log.Log;
+import org.jboss.seam.log.Logging;
+import org.xdi.oxauth.util.ServerUtil;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ * @version 0.9, 14/04/2015
+ */
+
+public class NeedInfoResponseBuilder {
+
+    private final static Log LOG = Logging.getLog(NeedInfoResponseBuilder.class);
+
+    private NeedInfoResponseBuilder() {
+    }
+
+    public static String entityForResponse(NeedInfoAuthenticationContext authenticationContext,
+                                           NeedInfoRequestingPartyClaims requestingPartyClaims) {
+
+        JSONObject result = new JSONObject();
+        JSONObject errorDetails = new JSONObject();
+
+        try {
+            result.put("error", "need_info");
+            result.put("error_details", errorDetails);
+
+            if (authenticationContext != null) {
+                errorDetails.put("authentication_context", new JSONObject(ServerUtil.asJson(authenticationContext)));
+            }
+            if (requestingPartyClaims != null) {
+                errorDetails.put("requesting_party_claims", new JSONObject(ServerUtil.asJson(requestingPartyClaims)));
+            }
+        } catch (Exception ex) {
+            LOG.error("Failed to generate 'need_info' json response", ex);
+            return null;
+        }
+
+        return result.toString();
+    }
+
+}
