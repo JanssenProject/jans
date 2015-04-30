e4131b02d83b05542f42726cfe8ca65e9ec46140
adrian <adrian@gluu.org>
2015-04-30 11:32:33 -0400

    fixed debian dir

--- debian/oxd-server/usr/share/oxd-server/oxd-common/src/main/java/org/xdi/oxd/common/response/IOpResponse.java
+++
@@ -1,9 +0,0 @@
-package org.xdi.oxd.common.response;
-
-/**
- * @author Yuriy Zabrovarnyy
- * @version 0.9, 13/08/2013
- */
-
-public interface IOpResponse /*extends Serializable*/ {
-}
--- debian/oxd-server/usr/share/oxd-server/oxd-license-client/target/classes/org/xdi/oxd/license/client/js/JsonFileConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.xdi.oxd.license.client.js;
-
-/**
- * @author Yuriy Zabrovarnyy
- * @version 0.9, 23/09/2014
- */
-
-public class JsonFileConfiguration extends Configuration {
-}
