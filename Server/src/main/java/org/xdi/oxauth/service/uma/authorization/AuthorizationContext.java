5859df15f69f053232c7855ce6403cf15510c259
yurem <Yuriy.Movchan@gmail.com>
2015-07-01 21:49:09 +0300

    Add Interception Script to enable Dynamic User Claim Generation #49

--- Server/src/main/java/org/xdi/oxauth/service/uma/authorization/AuthorizationContext.java
+++ Server/src/main/java/org/xdi/oxauth/service/uma/authorization/AuthorizationContext.java
@@ -6,56 +6,46 @@
 
 package org.xdi.oxauth.service.uma.authorization;
 
-import com.unboundid.ldap.sdk.Attribute;
-import com.unboundid.ldap.sdk.LDAPConnectionPool;
-import com.unboundid.ldap.sdk.SearchResultEntry;
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.net.util.SubnetUtils;
-import org.gluu.site.ldap.persistence.LdapEntryManager;
-import org.jboss.seam.log.Log;
-import org.jboss.seam.log.Logging;
-import org.xdi.model.GluuAttribute;
-import org.xdi.oxauth.model.common.IAuthorizationGrant;
-import org.xdi.oxauth.model.common.uma.UmaRPT;
-import org.xdi.oxauth.model.uma.ClaimToken;
-import org.xdi.oxauth.model.uma.persistence.ResourceSetPermission;
-import org.xdi.oxauth.model.util.Util;
-import org.xdi.oxauth.service.AttributeService;
-import org.xdi.oxauth.util.ServerUtil;
-
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.xdi.model.GluuAttribute;
+import org.xdi.oxauth.model.common.IAuthorizationGrant;
+import org.xdi.oxauth.model.common.uma.UmaRPT;
+import org.xdi.oxauth.model.uma.ClaimToken;
+import org.xdi.oxauth.model.uma.persistence.ResourceSetPermission;
+import org.xdi.oxauth.service.AttributeService;
+import org.xdi.oxauth.service.external.context.ExternalScriptContext;
+
 /**
  * @author Yuriy Zabrovarnyy
  * @author Javier Rojas Blum
+ * @author Yuriy Movchan
  * @version 0.9 February 12, 2015
  */
 
-public class AuthorizationContext {
-
-    private static final Log LOG = Logging.getLog(AuthorizationContext.class);
-
-    private final LdapEntryManager ldapEntryManager = ServerUtil.getLdapManager();
+public class AuthorizationContext extends ExternalScriptContext {
 
     private final UmaRPT m_rpt;
     private final ResourceSetPermission m_permission;
     private final IAuthorizationGrant m_grant;
-    private final HttpServletRequest m_httpRequest;
     private final Map<String, List<String>> m_claims;
     private NeedInfoAuthenticationContext needInfoAuthenticationContext;
     private NeedInfoRequestingPartyClaims needInfoRequestingPartyClaims;
 
     public AuthorizationContext(UmaRPT p_rpt, ResourceSetPermission p_permission, IAuthorizationGrant p_grant,
                                 HttpServletRequest p_httpRequest, List<ClaimToken> claims) {
-        m_rpt = p_rpt;
+    	super(p_httpRequest);
+
+    	m_rpt = p_rpt;
         m_permission = p_permission;
         m_grant = p_grant;
-        m_httpRequest = p_httpRequest;
         m_claims = new HashMap<String, List<String>>();
         if (claims != null) {
             for (ClaimToken claim : claims) {
@@ -69,14 +59,6 @@ public class AuthorizationContext {
         }
     }
 
-    public Log getLog() {
-        return LOG;
-    }
-
-    public HttpServletRequest getHttpRequest() {
-        return m_httpRequest;
-    }
-
     public List<String> getRequestClaim(String p_claimName) {
         if (StringUtils.isNotBlank(p_claimName) && m_claims != null) {
             final List<String> value = m_claims.get(p_claimName);
@@ -85,19 +67,6 @@ public class AuthorizationContext {
             }
         }
         return Collections.emptyList();
-    }
-
-    public String getIpAddress() {
-        return m_httpRequest != null ? m_httpRequest.getRemoteAddr() : "";
-    }
-
-    public boolean isInNetwork(String p_cidrNotation) {
-        final String ip = getIpAddress();
-        if (Util.allNotBlank(ip, p_cidrNotation)) {
-            final SubnetUtils utils = new SubnetUtils(p_cidrNotation);
-            return utils.getInfo().isInRange(ip);
-        }
-        return false;
     }
 
     public IAuthorizationGrant getGrant() {
@@ -133,27 +102,6 @@ public class AuthorizationContext {
 
     public ResourceSetPermission getPermission() {
         return m_permission;
-    }
-
-    private SearchResultEntry getEntryByDn(String p_dn) {
-        final LDAPConnectionPool pool = ldapEntryManager.getLdapOperationService().getConnectionPool();
-        try {
-            return pool.getEntry(p_dn);
-        } catch (Exception e) {
-            LOG.error(e.getMessage(), e);
-        }
-        return null;
-    }
-
-    private String getEntryAttributeValue(String p_dn, String p_attributeName) {
-        final SearchResultEntry entry = getEntryByDn(p_dn);
-        if (entry != null) {
-            final Attribute attribute = entry.getAttribute(p_attributeName);
-            if (attribute != null) {
-                return attribute.getValue();
-            }
-        }
-        return "";
     }
 
     public NeedInfoAuthenticationContext getNeedInfoAuthenticationContext() {
