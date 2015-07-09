285a05cf089581621e571e7a7e5adbff9c593eb7
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2015-07-09 15:36:29 +0300

    oxauth #22: reload configuration on oxauth.config.reload

--- Server/src/main/java/org/xdi/oxauth/model/config/Configuration.java
+++ Server/src/main/java/org/xdi/oxauth/model/config/Configuration.java
@@ -120,8 +120,6 @@ public class Configuration {
     private String cssLocation;
     private String jsLocation;
     private String imgLocation;
-    
-    private String configurationReloadTrigger;
 
     @XmlElement(name = "uma-keep-client-during-resource-set-registration")
     public Boolean getUmaKeepClientDuringResourceSetRegistration() {
@@ -1069,14 +1067,4 @@ public class Configuration {
     public void setImgLocation(String imgLocation) {
         this.imgLocation = imgLocation;
     }
-
-    @XmlElement(name = "configuration-reload-trigger")
-    public String getConfigurationReloadTrigger() {
-        return configurationReloadTrigger;
-    }
-
-    public void setConfigurationReloadTrigger(String configurationReloadTrigger) {
-        this.configurationReloadTrigger = configurationReloadTrigger;
-    }
-
 }
