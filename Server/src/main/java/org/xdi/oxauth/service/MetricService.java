bb3791e074970d6d08e0f5b53eff2d226197ec0f
yurem <Yuriy.Movchan@gmail.com>
2015-08-03 21:02:17 +0300

    Add service to record metrics

---
+++ Server/src/main/java/org/xdi/oxauth/service/MetricService.java
@@ -0,0 +1,66 @@
+/*
+ * oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2014, Gluu
+ */
+
+package org.xdi.oxauth.service;
+
+import org.jboss.seam.Component;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.Startup;
+import org.jboss.seam.log.Log;
+import org.xdi.oxauth.model.config.Configuration;
+import org.xdi.oxauth.model.config.ConfigurationFactory;
+
+/**
+ * Store and retrieve metric
+ *
+ * @author Yuriy Movchan Date: 07/30/2015
+ */
+@Scope(ScopeType.APPLICATION)
+@Name(MetricService.METRIC_SERVICE_COMPONENT_NAME)
+@Startup
+public class MetricService extends org.xdi.service.metric.MetricService {
+	
+	public static final String METRIC_SERVICE_COMPONENT_NAME = "metricService";
+
+	private static final long serialVersionUID = 7875838160379126796L;
+
+	@Logger
+    private Log log;
+
+	@In
+    private ApplianceService applianceService;
+    
+    @Create
+    public void create() {
+    	init(60);
+    }
+
+	@Override
+	public String baseDn() {
+        String metricBaseDn = ConfigurationFactory.instance().getBaseDn().getMetrics();
+		return metricBaseDn;
+	}
+
+	@Override
+	public String applianceInum() {
+		return applianceService.getApplianceInum();
+	}
+
+	@Override
+	public String getComponentName() {
+		return METRIC_SERVICE_COMPONENT_NAME;
+	}
+
+    public static MetricService instance() {
+        return (MetricService) Component.getInstance(MetricService.class);
+    }
+
+}
