59b9174ee82c4a533e035e2647e6b8435ac8fa4e
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2015-09-22 12:45:15 +0300

    oxd : base classes for get_tokens_by_code

---
+++ oxd-common/src/main/java/org/xdi/oxd/common/params/GetTokensByCodeParams.java
@@ -0,0 +1,76 @@
+package org.xdi.oxd.common.params;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ * @version 0.9, 22/09/2015
+ */
+
+public class GetTokensByCodeParams implements IParams {
+
+    @JsonProperty(value = "oxd_id")
+    private String oxdId;
+    @JsonProperty(value = "code")
+    private String code;
+    @JsonProperty(value = "state")
+    private String state;
+
+    public GetTokensByCodeParams() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getOxdId() {
+        return oxdId;
+    }
+
+    public void setOxdId(String oxdId) {
+        this.oxdId = oxdId;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GetTokensByCodeParams that = (GetTokensByCodeParams) o;
+
+        if (code != null ? !code.equals(that.code) : that.code != null) return false;
+        if (oxdId != null ? !oxdId.equals(that.oxdId) : that.oxdId != null) return false;
+        return !(state != null ? !state.equals(that.state) : that.state != null);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = oxdId != null ? oxdId.hashCode() : 0;
+        result = 31 * result + (code != null ? code.hashCode() : 0);
+        result = 31 * result + (state != null ? state.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("GetTokensByCodeParams");
+        sb.append("{code='").append(code).append('\'');
+        sb.append(", oxdId='").append(oxdId).append('\'');
+        sb.append(", state='").append(state).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
