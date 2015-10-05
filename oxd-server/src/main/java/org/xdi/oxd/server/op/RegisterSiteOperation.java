1ee9d71bf050532867756650908dc61684dc3340
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2015-10-05 12:09:59 +0300

    in case of dynamic client registration keep all client details in site configuration file

--- oxd-server/src/main/java/org/xdi/oxd/server/op/RegisterSiteOperation.java
+++ oxd-server/src/main/java/org/xdi/oxd/server/op/RegisterSiteOperation.java
@@ -1,8 +1,13 @@
 package org.xdi.oxd.server.op;
 
+import com.google.common.base.Strings;
 import com.google.inject.Injector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xdi.oxauth.client.RegisterClient;
+import org.xdi.oxauth.client.RegisterRequest;
+import org.xdi.oxauth.client.RegisterResponse;
+import org.xdi.oxauth.model.register.ApplicationType;
 import org.xdi.oxd.common.Command;
 import org.xdi.oxd.common.CommandResponse;
 import org.xdi.oxd.common.params.RegisterSiteParams;
@@ -22,6 +27,8 @@ public class RegisterSiteOperation extends BaseOperation {
 
     private static final Logger LOG = LoggerFactory.getLogger(RegisterSiteOperation.class);
 
+    private SiteConfigurationService siteService;
+
     /**
      * Base constructor
      *
@@ -29,6 +36,7 @@ public class RegisterSiteOperation extends BaseOperation {
      */
     protected RegisterSiteOperation(Command p_command, final Injector injector) {
         super(p_command, injector);
+        siteService = getInjector().getInstance(SiteConfigurationService.class);
     }
 
     @Override
@@ -49,12 +57,73 @@ public class RegisterSiteOperation extends BaseOperation {
 
     private void persistSiteConfiguration(String siteId) {
         final RegisterSiteParams params = asParams(RegisterSiteParams.class);
-        final SiteConfigurationService siteService = getInjector().getInstance(SiteConfigurationService.class);
+
         try {
-            siteService.persist(createSiteConfiguration(siteId, params));
+            final SiteConfiguration siteConfiguration = createSiteConfiguration(siteId, params);
+
+            if (!hasClient()) {
+                final RegisterResponse registerResponse = registerClient();
+                siteConfiguration.setClientId(registerResponse.getClientId());
+                siteConfiguration.setClientSecret(registerResponse.getClientSecret());
+                siteConfiguration.setClientRegistrationAccessToken(registerResponse.getRegistrationAccessToken());
+                siteConfiguration.setClientRegistrationClientUri(registerResponse.getRegistrationClientUri());
+                siteConfiguration.setClientIdIssuedAt(registerResponse.getClientIdIssuedAt());
+                siteConfiguration.setClientSecretExpiresAt(registerResponse.getClientSecretExpiresAt());
+            }
+
+            siteService.persist(siteConfiguration);
         } catch (IOException e) {
             throw new RuntimeException("Failed to persist site configuration, params: " + params, e);
         }
+    }
+
+    private boolean hasClient() {
+        final RegisterSiteParams params = asParams(RegisterSiteParams.class);
+        return !Strings.isNullOrEmpty(params.getClientId()) && !Strings.isNullOrEmpty(params.getClientSecret());
+    }
+
+    private RegisterResponse registerClient() {
+        final RegisterClient registerClient = new RegisterClient(getDiscoveryService().getConnectDiscoveryResponse().getRegistrationEndpoint());
+        registerClient.setRequest(createRegisterClientRequest());
+        registerClient.setExecutor(getHttpService().getClientExecutor());
+        final RegisterResponse response = registerClient.exec();
+        if (response != null && !Strings.isNullOrEmpty(response.getClientId()) && !Strings.isNullOrEmpty(response.getClientSecret())) {
+            return response;
+        }
+        throw new RuntimeException("Failed to register client for site.");
+    }
+
+    private RegisterRequest createRegisterClientRequest() {
+        final RegisterSiteParams params = asParams(RegisterSiteParams.class);
+
+        final ApplicationType applicationType = ApplicationType.fromString(params.getApplicationType());
+
+//        final List<ResponseType> responseTypes = ResponseType.fromString(responseTypeString, " ");
+//        final RegisterRequest request = new RegisterRequest(applicationType, params.getClientName(), params.getRedirectUrl());
+//        request.setResponseTypes(responseTypes);
+//        request.setJwksUri(params.getJwksUri());
+//        request.setPostLogoutRedirectUris(org.xdi.oxauth.model.util.StringUtils.spaceSeparatedToList(params.getLogoutRedirectUrl()));
+//
+//        if (StringUtils.isNotBlank(params.getContacts())) {
+//            request.setContacts(org.xdi.oxauth.model.util.StringUtils.spaceSeparatedToList(params.getContacts()));
+//        }
+//
+//        if (StringUtils.isNotBlank(params.getGrantTypes())) {
+//            request.setGrantTypes(grantTypes(params.getGrantTypes()));
+//        }
+//
+//        if (StringUtils.isNotBlank(params.getTokenEndpointAuthMethod())) {
+//            final AuthenticationMethod authenticationMethod = AuthenticationMethod.fromString(params.getTokenEndpointAuthMethod());
+//            if (authenticationMethod != null) {
+//                request.setTokenEndpointAuthMethod(authenticationMethod);
+//            }
+//        }
+//
+//        if (params.getRequestUris() != null && !params.getRequestUris().isEmpty()) {
+//            request.setRequestUris(params.getRequestUris());
+//        }
+//        return request;
+        return null; // todo
     }
 
     private SiteConfiguration createSiteConfiguration(String siteId, RegisterSiteParams params) {
