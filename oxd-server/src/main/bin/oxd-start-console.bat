13c8805ba98993c33191578e4c4c91476148db3c
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2015-11-17 14:57:53 +0200

    corrected console script

--- oxd-server/src/main/bin/oxd-start-console.bat
+++ oxd-server/src/main/bin/oxd-start-console.bat
@@ -1,5 +1,5 @@
 "cd /d %~dp0"
 set LIB=../lib
-set CONF=../conf/configuration.json
+set CONF=../conf/oxd-conf.json
 echo CONF=%CONF%
 java -Doxd.server.config=%CONF% -cp %LIB%/bcprov-jdk16-1.46.jar;%LIB%/resteasy-jaxrs-2.3.4.Final.jar;%LIB%/oxd-server-jar-with-dependencies.jar org.xdi.oxd.server.ServerLauncher
