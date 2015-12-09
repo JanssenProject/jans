a580af9969832a314b1160c9da9c2ae9a997cfae
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2015-12-09 16:03:40 +0200

    uma configuration endpoint (1.0.1) : introduced RptProfiles and avoid direct string inlining.

---
+++ Model/src/main/java/org/xdi/oxauth/model/uma/RptProfiles.java
@@ -0,0 +1,21 @@
+package org.xdi.oxauth.model.uma;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ * @version 0.9, 09/12/2015
+ */
+
+public enum RptProfiles {
+
+    BEARER("https://docs.kantarainitiative.org/uma/profiles/uma-token-bearer-1.0");
+
+    private String identifyingUri;
+
+    private RptProfiles(String identifyingUri) {
+        this.identifyingUri = identifyingUri;
+    }
+
+    public String getIdentifyingUri() {
+        return identifyingUri;
+    }
+}
