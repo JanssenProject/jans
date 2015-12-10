263d59ea567dee453dad6e3cb789bd7b59a86794
Mike Schwartz <mike@gluu.org>
2015-12-09 20:06:14 -0600

    Fixes to setup, export, and import

--- setup.py
+++ setup.py
@@ -440,7 +440,9 @@ class Setup(object):
                               ['set-attribute-syntax-prop', '--syntax-name', '"Directory String"',   '--set', 'allow-zero-length-values:true'],
                               ['set-password-policy-prop', '--policy-name', '"Default Password Policy"', '--set', 'allow-pre-encoded-passwords:true'],
                               ['set-log-publisher-prop', '--publisher-name', '"File-Based Audit Logger"', '--set', 'enabled:true'],
-                              ['create-backend', '--backend-name', 'site', '--set', 'base-dn:o=site', '--type local-db', '--set', 'enabled:true']]
+                              ['create-backend', '--backend-name', 'site', '--set', 'base-dn:o=site', '--type local-db', '--set', 'enabled:true'],
+                              ['set-global-configuration-prop', '--set', 'reject-unauthenticated-requests:true']
+                              ]
             for changes in config_changes:
                 dsconfigCmd = " ".join(['cd %s/bin ; ' % self.ldapBaseFolder,
                                         self.ldapDsconfigCommand,
