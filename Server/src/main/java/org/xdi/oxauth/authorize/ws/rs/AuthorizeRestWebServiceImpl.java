cdb7745a04594b9d4357df89c6121e13e21aab91
yurem <Yuriy.Movchan@gmail.com>
2015-12-21 15:20:35 +0200

    Clean up after filter fix

--- Server/src/main/java/org/xdi/oxauth/authorize/ws/rs/AuthorizeRestWebServiceImpl.java
+++ Server/src/main/java/org/xdi/oxauth/authorize/ws/rs/AuthorizeRestWebServiceImpl.java
@@ -158,11 +158,7 @@ public class AuthorizeRestWebServiceImpl implements AuthorizeRestWebService {
                 userService.getUserByDn(sessionUser.getUserDn()) : null;
 
         try {
-            SessionState persistedSessionState = sessionStateService.assertAuthenticatedSessionCorrespondsToNewRequest(sessionUser, redirectUri, acrValuesStr);
-        	if ((persistedSessionState == null) || (SessionIdState.UNAUTHENTICATED == persistedSessionState.getState())) {
-                log.debug("Session is not authentication. Session user should be null");
-        		user = null;
-        	}
+            sessionStateService.assertAuthenticatedSessionCorrespondsToNewRequest(sessionUser, redirectUri, acrValuesStr);
 
             if (!AuthorizeParamsValidator.validateParams(responseType, clientId, prompts, nonce, request, requestUri)) {
                 if (clientId != null && redirectUri != null && redirectionUriService.validateRedirectionUri(clientId, redirectUri) != null) {
