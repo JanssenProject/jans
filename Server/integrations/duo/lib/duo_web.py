0e43a64fa909c01fcb3981bb8b853d76ca011052
yurem <Yuriy.Movchan@gmail.com>
2015-12-22 09:18:23 +0200

    Update oxAuth DUO integration #114

--- Server/integrations/duo/lib/duo_web.py
+++ Server/integrations/duo/lib/duo_web.py
@@ -1,4 +1,4 @@
-# 
+#
 # duo_web.py
 #
 # Copyright (c) 2011 Duo Security
@@ -13,6 +13,8 @@ import time
 DUO_PREFIX  = 'TX'
 APP_PREFIX  = 'APP'
 AUTH_PREFIX = 'AUTH'
+ENROLL_PREFIX = 'ENROLL'
+ENROLL_REQUEST_PREFIX = 'ENROLL_REQUEST'
 
 DUO_EXPIRE = 300
 APP_EXPIRE = 3600
@@ -35,43 +37,51 @@ def _sign_vals(key, vals, prefix, expire):
     exp = str(int(time.time()) + expire)
 
     val = '|'.join(vals + [ exp ])
-    b64 = base64.b64encode(val)
+    b64 = base64.b64encode(val.encode('utf-8')).decode('utf-8')
     cookie = '%s|%s' % (prefix, b64)
 
-    sig = _hmac_sha1(key, cookie)
+    sig = _hmac_sha1(key.encode('utf-8'), cookie.encode('utf-8'))
     return '%s|%s' % (cookie, sig)
 
-def _parse_vals(key, val, prefix):
+def _parse_vals(key, val, prefix, ikey):
     ts = int(time.time())
     u_prefix, u_b64, u_sig = val.split('|')
+    cookie = '%s|%s' % (u_prefix, u_b64)
+    e_key = key.encode('utf-8')
+    e_cookie = cookie.encode('utf-8')
 
-    sig = _hmac_sha1(key, '%s|%s' % (u_prefix, u_b64))
-    if _hmac_sha1(key, sig) != _hmac_sha1(key, u_sig):
+    sig = _hmac_sha1(e_key, e_cookie)
+    if _hmac_sha1(e_key, sig.encode('utf-8')) != _hmac_sha1(e_key, u_sig.encode('utf-8')):
         return None
 
     if u_prefix != prefix:
         return None
 
-    user, ikey, exp = base64.b64decode(u_b64).split('|')
+    decoded = base64.b64decode(u_b64).decode('utf-8')
+    user, u_ikey, exp = decoded.split('|')
+
+    if u_ikey != ikey:
+        return None
 
     if ts >= int(exp):
         return None
 
     return user
 
-def sign_request(ikey, skey, akey, username):
+def _sign_request(ikey, skey, akey, username, prefix):
     """Generate a signed request for Duo authentication.
-    The returned value should be passed into the Duo.init() call 
+    The returned value should be passed into the Duo.init() call
     in the rendered web page used for Duo authentication.
-    
     Arguments:
-    
     ikey      -- Duo integration key
     skey      -- Duo secret key
     akey      -- Application secret key
     username  -- Primary-authenticated username
+    prefix    -- DUO_PREFIX or ENROLL_REQUEST_PREFIX
     """
     if not username:
+        return ERR_USER
+    if '|' in username:
         return ERR_USER
     if not ikey or len(ikey) != IKEY_LEN:
         return ERR_IKEY
@@ -83,32 +93,83 @@ def sign_request(ikey, skey, akey, username):
     vals = [ username, ikey ]
 
     try:
-        duo_sig = _sign_vals(skey, vals, DUO_PREFIX, DUO_EXPIRE)
+        duo_sig = _sign_vals(skey, vals, prefix, DUO_EXPIRE)
         app_sig = _sign_vals(akey, vals, APP_PREFIX, APP_EXPIRE)
-    except:
+    except Exception:
         return ERR_UNKNOWN
 
     return '%s:%s' % (duo_sig, app_sig)
 
-def verify_response(ikey, skey, akey, sig_response):
+
+def sign_request(ikey, skey, akey, username):
+    """Generate a signed request for Duo authentication.
+    The returned value should be passed into the Duo.init() call
+    in the rendered web page used for Duo authentication.
+    Arguments:
+    ikey      -- Duo integration key
+    skey      -- Duo secret key
+    akey      -- Application secret key
+    username  -- Primary-authenticated username
+    """
+    return _sign_request(ikey, skey, akey, username, DUO_PREFIX)
+
+
+def sign_enroll_request(ikey, skey, akey, username):
+    """Generate a signed request for Duo authentication.
+    The returned value should be passed into the Duo.init() call
+    in the rendered web page used for Duo authentication.
+    Arguments:
+    ikey      -- Duo integration key
+    skey      -- Duo secret key
+    akey      -- Application secret key
+    username  -- Primary-authenticated username
+    """
+    return _sign_request(ikey, skey, akey, username, ENROLL_REQUEST_PREFIX)
+
+
+def _verify_response(ikey, skey, akey, prefix, sig_response):
     """Validate the signed response returned from Duo.
     Returns the username of the authenticated user, or None.
-    
     Arguments:
-    
     ikey          -- Duo integration key
     skey          -- Duo secret key
     akey          -- Application secret key
+    prefix        -- AUTH_PREFIX or ENROLL_PREFIX that sig_response
+                     must match
     sig_response  -- The signed response POST'ed to the server
     """
     try:
         auth_sig, app_sig = sig_response.split(':')
-        auth_user = _parse_vals(skey, auth_sig, AUTH_PREFIX)
-        app_user = _parse_vals(akey, app_sig, APP_PREFIX)
-    except:
+        auth_user = _parse_vals(skey, auth_sig, AUTH_PREFIX, ikey)
+        app_user = _parse_vals(akey, app_sig, APP_PREFIX, ikey)
+    except Exception:
         return None
 
     if auth_user != app_user:
         return None
 
     return auth_user
+
+
+def verify_response(ikey, skey, akey, sig_response):
+    """Validate the signed response returned from Duo.
+    Returns the username of the authenticated user, or None.
+    Arguments:
+    ikey          -- Duo integration key
+    skey          -- Duo secret key
+    akey          -- Application secret key
+    sig_response  -- The signed response POST'ed to the server
+    """
+    return _verify_response(ikey, skey, akey, AUTH_PREFIX, sig_response)
+
+
+def verify_enroll_response(ikey, skey, akey, sig_response):
+    """Validate the signed response returned from Duo.
+    Returns the username of the enrolled user, or None.
+    Arguments:
+    ikey          -- Duo integration key
+    skey          -- Duo secret key
+    akey          -- Application secret key
+    sig_response  -- The signed response POST'ed to the server
+    """
+    return _verify_response(ikey, skey, akey, ENROLL_PREFIX, sig_response)
