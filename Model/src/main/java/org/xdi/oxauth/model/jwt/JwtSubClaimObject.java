977613fc3ba94810f23cdb5efd699f08bba47f13
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-03-02 16:49:30 +0200

    added utility methods to create jwt sub claim object from boolean and string maps

--- Model/src/main/java/org/xdi/oxauth/model/jwt/JwtSubClaimObject.java
+++ Model/src/main/java/org/xdi/oxauth/model/jwt/JwtSubClaimObject.java
@@ -1,5 +1,7 @@
 package org.xdi.oxauth.model.jwt;
 
+import java.util.Map;
+
 /**
  * @author Javier Rojas Blum
  * @version Jun 10, 2015
@@ -14,5 +16,21 @@ public class JwtSubClaimObject extends JwtClaimSet {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static JwtSubClaimObject fromMap(Map<String, String> map) {
+        JwtSubClaimObject result = new JwtSubClaimObject();
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            result.setClaim(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
+
+    public static JwtSubClaimObject fromBooleanMap(Map<String, Boolean> map) {
+        JwtSubClaimObject result = new JwtSubClaimObject();
+        for (Map.Entry<String, Boolean> entry : map.entrySet()) {
+            result.setClaim(entry.getKey(), entry.getValue());
+        }
+        return result;
     }
 }
