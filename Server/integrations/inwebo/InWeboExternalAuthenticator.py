97a621eba98c6989943eeb69eed5f312868b5474
yurem <Yuriy.Movchan@gmail.com>
2016-03-04 18:01:37 +0200

    Add headers to authentication scripts

--- Server/integrations/inwebo/InWeboExternalAuthenticator.py
+++ Server/integrations/inwebo/InWeboExternalAuthenticator.py
@@ -1,3 +1,9 @@
+# oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+# Copyright (c) 2016, Gluu
+#
+# Author: Yuriy Movchan
+#
+
 from org.jboss.seam.contexts import Context, Contexts
 from org.jboss.seam.security import Identity
 from org.jboss.seam import Component
