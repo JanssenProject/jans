ef0ed0abbb7ee01892b3df42c7395edf1d507111
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-03-14 18:54:45 +0200

    #20 : fixed tests

--- oxd-common/src/main/java/org/xdi/oxd/common/response/UpdateSiteResponse.java
+++ oxd-common/src/main/java/org/xdi/oxd/common/response/UpdateSiteResponse.java
@@ -1,5 +1,7 @@
 package org.xdi.oxd.common.response;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+
 /**
  * @author Yuriy Zabrovarnyy
  * @version 0.9, 14/03/2016
@@ -7,6 +9,17 @@ package org.xdi.oxd.common.response;
 
 public class UpdateSiteResponse implements IOpResponse {
 
+    @JsonProperty(value = "oxd_id")
+    private String oxdId;
+
     public UpdateSiteResponse() {
+    }
+
+    public String getOxdId() {
+        return oxdId;
+    }
+
+    public void setOxdId(String oxdId) {
+        this.oxdId = oxdId;
     }
 }
