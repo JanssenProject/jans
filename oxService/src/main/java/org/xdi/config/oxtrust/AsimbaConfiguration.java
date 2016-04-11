917e1e23571b4612d1b8b5466ba32502590b3689
Dmitry Ognyannikov <dmogn@mail.ru>
2016-04-11 11:04:09 -0300

    add Asimba configuration LDAP Entry

---
+++ oxService/src/main/java/org/xdi/config/oxtrust/AsimbaConfiguration.java
@@ -0,0 +1,154 @@
+/*
+ * oxCore is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2016, Gluu
+ */
+package org.xdi.config.oxtrust;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+/**
+ * Asimba settings configuration entry. 
+ * 
+ * @author Dmitry Ognyannikov
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@XmlRootElement(name = "baseDN")
+public class AsimbaConfiguration implements Serializable {
+    
+    private static final long serialVersionUID = -1207383590239619576L;
+    
+    private String orgInum;
+    
+    private String applianceInum;
+    
+    private String appliance;
+    
+    private String oxasimba;
+    
+    private String idps;
+    
+    private String selectors;
+    
+    private String requestors;
+    
+    private String requestorpools;
+
+    /**
+     * @return the orgInum
+     */
+    public String getOrgInum() {
+        return orgInum;
+    }
+
+    /**
+     * @param orgInum the orgInum to set
+     */
+    public void setOrgInum(String orgInum) {
+        this.orgInum = orgInum;
+    }
+
+    /**
+     * @return the applianceInum
+     */
+    public String getApplianceInum() {
+        return applianceInum;
+    }
+
+    /**
+     * @param applianceInum the applianceInum to set
+     */
+    public void setApplianceInum(String applianceInum) {
+        this.applianceInum = applianceInum;
+    }
+
+    /**
+     * @return the appliance
+     */
+    public String getAppliance() {
+        return appliance;
+    }
+
+    /**
+     * @param appliance the appliance to set
+     */
+    public void setAppliance(String appliance) {
+        this.appliance = appliance;
+    }
+
+    /**
+     * @return the oxasimba
+     */
+    public String getOxasimba() {
+        return oxasimba;
+    }
+
+    /**
+     * @param oxasimba the oxasimba to set
+     */
+    public void setOxasimba(String oxasimba) {
+        this.oxasimba = oxasimba;
+    }
+
+    /**
+     * @return the idps
+     */
+    public String getIdps() {
+        return idps;
+    }
+
+    /**
+     * @param idps the idps to set
+     */
+    public void setIdps(String idps) {
+        this.idps = idps;
+    }
+
+    /**
+     * @return the selectors
+     */
+    public String getSelectors() {
+        return selectors;
+    }
+
+    /**
+     * @param selectors the selectors to set
+     */
+    public void setSelectors(String selectors) {
+        this.selectors = selectors;
+    }
+
+    /**
+     * @return the requestors
+     */
+    public String getRequestors() {
+        return requestors;
+    }
+
+    /**
+     * @param requestors the requestors to set
+     */
+    public void setRequestors(String requestors) {
+        this.requestors = requestors;
+    }
+
+    /**
+     * @return the requestorpools
+     */
+    public String getRequestorpools() {
+        return requestorpools;
+    }
+
+    /**
+     * @param requestorpools the requestorpools to set
+     */
+    public void setRequestorpools(String requestorpools) {
+        this.requestorpools = requestorpools;
+    }
+    
+}
--- oxService/src/main/java/org/xdi/config/oxtrust/LdapOxAsimbaConfiguration.java
+++ oxService/src/main/java/org/xdi/config/oxtrust/LdapOxAsimbaConfiguration.java
@@ -6,8 +6,8 @@
 package org.xdi.config.oxtrust;
 
 import org.gluu.site.ldap.persistence.annotation.LdapAttribute;
-import org.gluu.site.ldap.persistence.annotation.LdapDN;
 import org.gluu.site.ldap.persistence.annotation.LdapEntry;
+import org.gluu.site.ldap.persistence.annotation.LdapJsonObject;
 import org.gluu.site.ldap.persistence.annotation.LdapObjectClass;
 import org.xdi.ldap.model.Entry;
 
@@ -21,13 +21,42 @@ import org.xdi.ldap.model.Entry;
 public class LdapOxAsimbaConfiguration extends Entry {
 
     private static final long serialVersionUID = -12489347651302948L;
-
-    @LdapDN
-    private String dn;
+    
+    @LdapJsonObject
+    @LdapAttribute(name = "oxConfApplication")
+    private AsimbaConfiguration applicationConfiguration;
 
     @LdapAttribute(name = "oxRevision")
     private long revision;
     
     public LdapOxAsimbaConfiguration() {}
+
+    /**
+     * @return the applicationConfiguration
+     */
+    public AsimbaConfiguration getApplicationConfiguration() {
+        return applicationConfiguration;
+    }
+
+    /**
+     * @param applicationConfiguration the applicationConfiguration to set
+     */
+    public void setApplicationConfiguration(AsimbaConfiguration applicationConfiguration) {
+        this.applicationConfiguration = applicationConfiguration;
+    }
+
+    /**
+     * @return the revision
+     */
+    public long getRevision() {
+        return revision;
+    }
+
+    /**
+     * @param revision the revision to set
+     */
+    public void setRevision(long revision) {
+        this.revision = revision;
+    }
     
 }
