e419fc126b54cf516d3e959e043fd900d14ae680
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2016-05-11 10:42:57 +0300

    Fix cacerts path in Ubuntu 16.04

--- setup.py
+++ setup.py
@@ -1363,7 +1363,7 @@ class Setup(object):
         return None
 
     def configureOsPatches(self):
-        if self.os_type in ['debian']:
+        if self.os_type in ['debian', 'ubuntu']:
             self.defaultTrustStoreFN = '/etc/ssl/certs/java/cacerts'
 
     def makeFolders(self):
