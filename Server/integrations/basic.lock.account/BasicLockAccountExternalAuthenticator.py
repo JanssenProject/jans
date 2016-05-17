0e974015fb4a98bb65744e96f7760a4e9a84d84a
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2016-05-17 20:55:48 +0300

    Pre-release version of lock user script

---
+++ Server/integrations/basic.lock.account/BasicLockAccountExternalAuthenticator.py
@@ -0,0 +1,146 @@
+# oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+# Copyright (c) 2016, Gluu
+#
+# Author: Yuriy Movchan
+#
+
+from org.jboss.seam.security import Identity
+from org.xdi.model.custom.script.type.auth import PersonAuthenticationType
+from org.xdi.oxauth.service import UserService
+from org.xdi.util import StringHelper
+from org.gluu.site.ldap.persistence.exception import AuthenticationException
+
+import java
+
+class PersonAuthentication(PersonAuthenticationType):
+    def __init__(self, currentTimeMillis):
+        self.currentTimeMillis = currentTimeMillis
+
+    def init(self, configurationAttributes):
+        print "Basic (lock account). Initialization"
+        print "Basic (lock account). Initialized successfully"
+        return True   
+
+    def destroy(self, configurationAttributes):
+        print "Basic (lock account). Destroy"
+        print "Basic (lock account). Destroyed successfully"
+        return True
+
+    def getApiVersion(self):
+        return 1
+
+    def isValidAuthenticationMethod(self, usageType, configurationAttributes):
+        return True
+
+    def getAlternativeAuthenticationMethod(self, usageType, configurationAttributes):
+        return None
+
+    def authenticate(self, configurationAttributes, requestParameters, step):
+        if step == 1:
+            print "Basic (lock account). Authenticate for step 1"
+
+            credentials = Identity.instance().getCredentials()
+            user_name = credentials.getUsername()
+            user_password = credentials.getPassword()
+
+            logged_in = False
+            if (StringHelper.isNotEmptyString(user_name) and StringHelper.isNotEmptyString(user_password)):
+                userService = UserService.instance()
+                try:
+                    logged_in = userService.authenticate(user_name, user_password)
+                except AuthenticationException:
+                    print "Basic (lock account). Authenticate. Failed to authenticate user '%s'" % user_name
+
+            if (not logged_in):
+                countInvalidLoginArributeValue = self.getUserAttributeValue(user_name, "oxCountInvalidLogin")
+                countInvalidLogin = StringHelper.toInteger(countInvalidLoginArributeValue, 0)
+                if countInvalidLogin < 3:
+                    self.setUserAttributeValue(user_name, "oxCountInvalidLogin", StringHelper.toString(countInvalidLogin + 1))
+                else:
+                    self.lockUser(user_name)
+                    
+                return False
+
+            self.setUserAttributeValue(user_name, "oxCountInvalidLogin", StringHelper.toString(0))
+
+            return True
+        else:
+            return False
+
+    def prepareForStep(self, configurationAttributes, requestParameters, step):
+        if step == 1:
+            print "Basic (lock account). Prepare for Step 1"
+            return True
+        else:
+            return False
+
+    def getExtraParametersForStep(self, configurationAttributes, step):
+        return None
+
+    def getCountAuthenticationSteps(self, configurationAttributes):
+        return 1
+
+    def getPageForStep(self, configurationAttributes, step):
+        return ""
+
+    def logout(self, configurationAttributes, requestParameters):
+        return True
+
+    def getUserAttributeValue(self, user_name, attribute_name):
+        if StringHelper.isEmpty(user_name):
+            return None
+
+        userService = UserService.instance()
+
+        find_user_by_uid = userService.getUser(user_name, attribute_name)
+        if find_user_by_uid == None:
+            return None
+
+        custom_attribute_value = userService.getCustomAttribute(find_user_by_uid, attribute_name)
+        if custom_attribute_value == None:
+            return None
+        
+        attribute_value = custom_attribute_value.getValue()
+
+        print "Basic (lock account). Get user attribute. User's '%s' attribute '%s' value is '%s'" % (user_name, attribute_name, attribute_value)
+
+        return attribute_value
+
+    def setUserAttributeValue(self, user_name, attribute_name, attribute_value):
+        if StringHelper.isEmpty(user_name):
+            return None
+
+        userService = UserService.instance()
+
+        find_user_by_uid = userService.getUser(user_name)
+        if find_user_by_uid == None:
+            return None
+        
+        userService.setCustomAttribute(find_user_by_uid, attribute_name, attribute_value)
+        updated_user = userService.updateUser(find_user_by_uid)
+
+        print "Basic (lock account). Set user attribute. User's '%s' attribute '%s' value is '%s'" % (user_name, attribute_name, attribute_value)
+
+        return updated_user
+
+    def lockUser(self, user_name):
+        if StringHelper.isEmpty(user_name):
+            return None
+
+        userService = UserService.instance()
+
+        find_user_by_uid = userService.getUser(user_name, "gluuStatus")
+        if (find_user_by_uid == None):
+            return None
+
+        status_attribute_value = userService.getCustomAttribute(find_user_by_uid, "gluuStatus")
+        if status_attribute_value != None:
+            user_status = status_attribute_value.getValue()
+            if StringHelper.equals(user_status, "inactive"):
+                print "Basic (lock account). Lock user. User '%s' locked already" % user_name
+                return
+        
+        userService.setCustomAttribute(find_user_by_uid, "gluuStatus", "inactive")
+        updated_user = userService.updateUser(find_user_by_uid)
+
+        print "Basic (lock account). Lock user. User '%s' locked" % user_name
