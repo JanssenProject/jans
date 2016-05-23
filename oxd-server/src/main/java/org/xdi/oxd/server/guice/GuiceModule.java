539374d6147ae799c85a9e9eae8e9fa8aaf1c5cc
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-05-23 16:30:30 +0300

    oxd : added validation service

--- oxd-server/src/main/java/org/xdi/oxd/server/guice/GuiceModule.java
+++ oxd-server/src/main/java/org/xdi/oxd/server/guice/GuiceModule.java
@@ -4,15 +4,15 @@
 package org.xdi.oxd.server.guice;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import org.xdi.oxd.server.Configuration;
-import org.xdi.oxd.server.service.DiscoveryService;
-import org.xdi.oxd.server.service.HttpService;
 import org.xdi.oxd.server.license.LicenseService;
 import org.xdi.oxd.server.service.ConfigurationService;
+import org.xdi.oxd.server.service.DiscoveryService;
+import org.xdi.oxd.server.service.HttpService;
 import org.xdi.oxd.server.service.SiteConfigurationService;
 import org.xdi.oxd.server.service.SocketService;
+import org.xdi.oxd.server.service.ValidationService;
 
 /**
  * @author Yuriy Zabrovarnyy
@@ -29,6 +29,7 @@ public class GuiceModule extends AbstractModule {
         bind(SiteConfigurationService.class).in(Singleton.class);
         bind(HttpService.class).in(Singleton.class);
         bind(DiscoveryService.class).in(Singleton.class);
+        bind(ValidationService.class).in(Singleton.class);
 
         bind(Configuration.class).toProvider(ConfigurationService.class);
     }
