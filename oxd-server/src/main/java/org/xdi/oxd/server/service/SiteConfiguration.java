145319d5bc067f21603fbf3cd9963ff8f5ab7979
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-05-23 15:45:16 +0300

    oxd : passing op_host during register_site operation

--- oxd-server/src/main/java/org/xdi/oxd/server/service/SiteConfiguration.java
+++ oxd-server/src/main/java/org/xdi/oxd/server/service/SiteConfiguration.java
@@ -18,6 +18,9 @@ public class SiteConfiguration implements Serializable {
     @JsonProperty(value = "oxd_id")
     private String oxdId;
 
+    @JsonProperty(value = "op_host")
+    private String opHost;
+
     @JsonProperty(value = "id_token")
     private String idToken;
     @JsonProperty(value = "access_token")
@@ -70,6 +73,8 @@ public class SiteConfiguration implements Serializable {
 
     public SiteConfiguration(SiteConfiguration conf) {
         this.oxdId = conf.oxdId;
+
+        this.opHost = conf.opHost;
 
         this.idToken = conf.idToken;
         this.accessToken = conf.accessToken;
@@ -242,6 +247,14 @@ public class SiteConfiguration implements Serializable {
         this.oxdId = oxdId;
     }
 
+    public String getOpHost() {
+        return opHost;
+    }
+
+    public void setOpHost(String opHost) {
+        this.opHost = opHost;
+    }
+
     public List<String> getRedirectUris() {
         return redirectUris;
     }
@@ -274,13 +287,13 @@ public class SiteConfiguration implements Serializable {
         this.uiLocales = uiLocales;
     }
 
-
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder();
         sb.append("SiteConfiguration");
         sb.append("{acrValues=").append(acrValues);
         sb.append(", oxdId='").append(oxdId).append('\'');
+        sb.append(", opHost='").append(opHost).append('\'');
         sb.append(", authorizationRedirectUri='").append(authorizationRedirectUri).append('\'');
         sb.append(", applicationType='").append(applicationType).append('\'');
         sb.append(", sectorIdentifierUri='").append(sectorIdentifierUri).append('\'');
