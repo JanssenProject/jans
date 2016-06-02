c26b67e336833afd4e4a3070fc52a29ecdf5ce53
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-06-02 18:22:40 +0300

    taken advantage of HasOxdId interface for all parameters validation

--- oxd-common/src/main/java/org/xdi/oxd/common/params/RpAuthorizeRptParams.java
+++ oxd-common/src/main/java/org/xdi/oxd/common/params/RpAuthorizeRptParams.java
@@ -1,9 +1,23 @@
 package org.xdi.oxd.common.params;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+
 /**
  * @author Yuriy Zabrovarnyy
  * @version 0.9, 31/05/2016
  */
 
-public class RpAuthorizeRptParams {
+public class RpAuthorizeRptParams implements HasOxdIdParams {
+
+    @JsonProperty(value = "oxd_id")
+    private String oxdId;
+
+    @Override
+    public String getOxdId() {
+        return oxdId;
+    }
+
+    public void setOxdId(String oxdId) {
+        this.oxdId = oxdId;
+    }
 }
