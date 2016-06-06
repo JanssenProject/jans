c9ec668c1bfafc6a095407c221369571cd73c0cf
yurem <Yuriy.Movchan@gmail.com>
2016-06-06 20:50:05 +0300

    Allow user to do U2F enrollment using enrollment_code

--- Server/src/main/java/org/xdi/oxauth/service/fido/u2f/ValidationService.java
+++ Server/src/main/java/org/xdi/oxauth/service/fido/u2f/ValidationService.java
@@ -14,8 +14,11 @@ import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.log.Log;
 import org.xdi.oxauth.model.common.SessionState;
+import org.xdi.oxauth.model.common.User;
 import org.xdi.oxauth.model.config.Constants;
+import org.xdi.oxauth.model.fido.u2f.U2fConstants;
 import org.xdi.oxauth.service.SessionStateService;
+import org.xdi.oxauth.service.UserService;
 import org.xdi.util.StringHelper;
 
 /**
@@ -34,6 +37,9 @@ public class ValidationService {
 	@In
 	private SessionStateService sessionStateService;
 
+	@In
+	private UserService userService;
+
 	public boolean isValidSessionState(String userName, String sessionState) {
 		if (sessionState == null) {
 			log.error("In two step authentication workflow session_state is mandatory");
@@ -49,6 +55,32 @@ public class ValidationService {
 		String sessionStateUser = ldapSessionState.getSessionAttributes().get(Constants.AUTHENTICATED_USER);
 		if (!StringHelper.equalsIgnoreCase(userName, sessionStateUser)) {
 			log.error("Username '{0}' and session_state '{1}' don't match", userName, sessionState);
+			return false;
+		}
+
+		return true;
+	}
+
+	public boolean isValidEnrollmentCode(String userName, String enrollmentCode) {
+		if (enrollmentCode == null) {
+			log.error("In two step authentication workflow enrollment_code is mandatory");
+			return false;
+		}
+		
+		User user = userService.getUser(userName, U2fConstants.U2F_ENROLLMENT_CODE_ATTRIBUTE);
+		if (user == null) {
+			log.error("Specified user_name '{0}' is invalid", userName);
+			return false;
+		}
+		
+		String userEnrollmentCode = user.getAttribute(U2fConstants.U2F_ENROLLMENT_CODE_ATTRIBUTE);
+		if (userEnrollmentCode == null) {
+			log.error("Specified enrollment_code '{0}' is invalid", enrollmentCode);
+			return false;
+		}
+
+		if (!StringHelper.equalsIgnoreCase(userEnrollmentCode, enrollmentCode)) {
+			log.error("Username '{0}' and enrollment_code '{1}' don't match", userName, enrollmentCode);
 			return false;
 		}
 
