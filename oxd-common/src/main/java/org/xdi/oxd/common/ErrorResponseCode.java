22aadce62ee98f8ae5874eceb940116a5b10e6cb
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-06-14 16:44:32 +0300

    uma_rp_authorize_rpt : full implementation of operation

--- oxd-common/src/main/java/org/xdi/oxd/common/ErrorResponseCode.java
+++ oxd-common/src/main/java/org/xdi/oxd/common/ErrorResponseCode.java
@@ -25,8 +25,10 @@ public enum ErrorResponseCode {
     NO_CONNECT_DISCOVERY_RESPONSE("no_connect_discovery_response", "Unable to fetch Connect discovery response /.well-known/openid-configuration"),
     NO_UMA_DISCOVERY_RESPONSE("no_uma_discovery_response", "Unable to fetch UMA discovery response /.well-known/uma-configuration"),
     NO_UMA_RESOURCES_TO_PROTECT("invalid_uma_request", "Resources list to protect is empty or blank. Please check it according to protocol definition at https://www.gluu.org/docs-oxd"),
-    NO_UMA_HTTP_METHOD("invalid_uma_http_method", "http_method is not specified or otherwise not GET or POST or PUT or DELETE. Please check it according to protocol definition at https://www.gluu.org/docs-oxd"),
-    NO_UMA_PATH_PARAMETER("invalid_uma_path_parameter", "path parameter is not specified or otherwise not valid"),
+    NO_UMA_HTTP_METHOD("invalid_http_method", "http_method is not specified or otherwise not GET or POST or PUT or DELETE. Please check it according to protocol definition at https://www.gluu.org/docs-oxd"),
+    NO_UMA_PATH_PARAMETER("invalid_path_parameter", "path parameter is not specified or otherwise not valid"),
+    NO_UMA_TICKET_PARAMETER("invalid_ticket_parameter", "ticket parameter is not specified or otherwise is not valid"),
+    NO_UMA_RPT_PARAMETER("invalid_rpt_parameter", "rpt parameter is not specified or otherwise is not valid"),
     FAILED_TO_GET_RPT("internal_error", "Failed to get RPT.");
 
     private final String code;
