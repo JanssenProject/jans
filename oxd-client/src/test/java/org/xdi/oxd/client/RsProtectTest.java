04cd85aa61272fe432e6a401648946c1fae9e870
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-06-15 16:59:49 +0300

    Merge with master

--- oxd-client/src/test/java/org/xdi/oxd/client/RsProtectTest.java
+++ oxd-client/src/test/java/org/xdi/oxd/client/RsProtectTest.java
@@ -1,17 +1,12 @@
 package org.xdi.oxd.client;
 
-import org.apache.commons.lang.StringUtils;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.xdi.oxd.common.Command;
 import org.xdi.oxd.common.CommandType;
-import org.xdi.oxd.common.params.RsCheckAccessParams;
 import org.xdi.oxd.common.params.RsProtectParams;
 import org.xdi.oxd.common.response.RegisterSiteResponse;
-import org.xdi.oxd.common.response.RsCheckAccessResponse;
 import org.xdi.oxd.common.response.RsProtectResponse;
-import org.xdi.oxd.rs.protect.Jackson;
-import org.xdi.oxd.rs.protect.RsResourceList;
 
 import java.io.IOException;
 
@@ -35,31 +30,15 @@ public class RsProtectTest {
 
             final RsProtectParams commandParams = new RsProtectParams();
             commandParams.setOxdId(site.getOxdId());
-            commandParams.setResources(resourceList(rsProtect));
+            commandParams.setResources(UmaFullTest.resourceList(rsProtect));
 
             final Command command = new Command(CommandType.RS_PROTECT)
                     .setParamsObject(commandParams);
 
             final RsProtectResponse resp = client.send(command).dataAsResponse(RsProtectResponse.class);
             assertNotNull(resp);
-
-            //checkAccessPositive(client);
         } finally {
             CommandClient.closeQuietly(client);
         }
-    }
-
-    private void checkAccessPositive(CommandClient client) {
-        RsCheckAccessParams params = new RsCheckAccessParams();
-        final Command command = new Command(CommandType.RS_CHECK_ACCESS)
-                .setParamsObject(params);
-
-        final RsCheckAccessResponse resp = client.send(command).dataAsResponse(RsCheckAccessResponse.class);
-        assertNotNull(resp);
-    }
-
-    private RsResourceList resourceList(String rsProtect) throws IOException {
-        rsProtect = StringUtils.replace(rsProtect, "'", "\"");
-        return Jackson.createJsonMapper().readValue(rsProtect, RsResourceList.class);
     }
 }
