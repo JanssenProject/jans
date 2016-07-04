fe771fed13e5fd39939cee32e0d16e2db762368c
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-07-04 13:09:17 +0300

    fixed bouncycsatle lib version

--- oxd-server/src/main/bin/oxd-start.bat
+++ oxd-server/src/main/bin/oxd-start.bat
@@ -2,4 +2,4 @@
 set LIB=../lib
 set CONF=../conf/oxd-conf.json
 echo CONF=%CONF%
-start /b javaw -Doxd.server.config=%CONF% -cp %LIB%/bcprov-jdk16-1.46.jar;%LIB%/resteasy-jaxrs-2.3.4.Final.jar;%LIB%/oxd-server-jar-with-dependencies.jar org.xdi.oxd.server.ServerLauncher
+start /b javaw -Doxd.server.config=%CONF% -cp %LIB%/bcprov-jdk15on-1.54.jar;%LIB%/resteasy-jaxrs-2.3.7.Final.jar;%LIB%/oxd-server-jar-with-dependencies.jar org.xdi.oxd.server.ServerLauncher
--- oxd-server/src/main/bin/oxd-start.sh
+++ oxd-server/src/main/bin/oxd-start.sh
@@ -9,4 +9,4 @@ echo CONF=$CONF
 LIB=$BASEDIR/../lib
 javaExe=java
 #javaExe=/usr/java/jdk1.6.0_30/bin/java
-$javaExe -Doxd.server.config=$CONF -cp $LIB/bcprov-jdk16-1.46.jar:$LIB/resteasy-jaxrs-2.3.4.Final.jar:$LIB/oxd-server-jar-with-dependencies.jar org.xdi.oxd.server.ServerLauncher
+$javaExe -Doxd.server.config=$CONF -cp $LIB/bcprov-jdk15on-1.54.jar:$LIB/resteasy-jaxrs-2.3.7.Final.jar:$LIB/oxd-server-jar-with-dependencies.jar org.xdi.oxd.server.ServerLauncher
