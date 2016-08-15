3faa824d89fc53dcd2e4287f2e9486fbb4319c2e
worm333 <flexblademaster@gmail.com>
2016-08-15 11:38:13 +0300

    - removed: 'http_logout_supported', 'logout_session_supported', 'federation_metadata_endpoint' and 'federation_endpoint' from /oxauth/gluu-configuration

--- Client/src/main/java/org/xdi/oxauth/client/GluuConfigurationResponse.java
+++ Client/src/main/java/org/xdi/oxauth/client/GluuConfigurationResponse.java
@@ -8,10 +8,6 @@ import java.util.Set;
  */
 public class GluuConfigurationResponse extends BaseResponse {
 
-    private String federationMetadataEndpoint;
-
-    private String federationEndpoint;
-
     private String idGenerationEndpoint;
 
     private String introspectionEndpoint;
@@ -20,25 +16,6 @@ public class GluuConfigurationResponse extends BaseResponse {
 
     private Map<String, Set<String>> scopeToClaimsMapping;
 
-    private String httpLogoutSupported;
-
-    private String logoutSessionSupported;
-
-    public String getFederationMetadataEndpoint() {
-        return federationMetadataEndpoint;
-    }
-
-    public void setFederationMetadataEndpoint(String federationMetadataEndpoint) {
-        this.federationMetadataEndpoint = federationMetadataEndpoint;
-    }
-
-    public String getFederationEndpoint() {
-        return federationEndpoint;
-    }
-
-    public void setFederationEndpoint(String federationEndpoint) {
-        this.federationEndpoint = federationEndpoint;
-    }
 
     public String getIdGenerationEndpoint() {
         return idGenerationEndpoint;
@@ -72,33 +49,13 @@ public class GluuConfigurationResponse extends BaseResponse {
         this.scopeToClaimsMapping = scopeToClaimsMapping;
     }
 
-    public String getHttpLogoutSupported() {
-        return httpLogoutSupported;
-    }
-
-    public void setHttpLogoutSupported(String httpLogoutSupported) {
-        this.httpLogoutSupported = httpLogoutSupported;
-    }
-
-    public String getLogoutSessionSupported() {
-        return logoutSessionSupported;
-    }
-
-    public void setLogoutSessionSupported(String logoutSessionSupported) {
-        this.logoutSessionSupported = logoutSessionSupported;
-    }
-
     @Override
     public String toString() {
         return "GluuConfigurationResponse{" +
-                "federationMetadataEndpoint='" + federationMetadataEndpoint + '\'' +
-                ", federationEndpoint='" + federationEndpoint + '\'' +
-                ", idGenerationEndpoint='" + idGenerationEndpoint + '\'' +
+                "idGenerationEndpoint='" + idGenerationEndpoint + '\'' +
                 ", introspectionEndpoint='" + introspectionEndpoint + '\'' +
                 ", authLevelMapping=" + authLevelMapping +
                 ", scopeToClaimsMapping=" + scopeToClaimsMapping +
-                ", httpLogoutSupported='" + httpLogoutSupported + '\'' +
-                ", logoutSessionSupported='" + logoutSessionSupported + '\'' +
                 '}';
     }
 }
