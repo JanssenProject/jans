35bf7950f120829300b699cbb469db45156d84b1
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2016-08-24 17:28:28 -0400

    Cover JwtUtil methods by tests #178 - oxAuth-RP updated.

--- RP/src/main/java/org/xdi/oxauth/validator/JSonValidator.java
+++ RP/src/main/java/org/xdi/oxauth/validator/JSonValidator.java
@@ -6,17 +6,22 @@
 
 package org.xdi.oxauth.validator;
 
+import com.google.common.base.Strings;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.intercept.BypassInterceptors;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
-import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.intercept.BypassInterceptors;
-
+/**
+ * @author Javier Rojas Blum
+ * @version August 24, 2016
+ */
 @Name("jsonValidator")
 @BypassInterceptors
 @org.jboss.seam.annotations.faces.Validator
@@ -25,7 +30,9 @@ public class JSonValidator implements Validator {
     @Override
     public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
         try {
-            new JSONObject((String) value);
+            if (!Strings.isNullOrEmpty((String) value)) {
+                new JSONObject((String) value);
+            }
         } catch (JSONException e) {
             FacesMessage msg = new FacesMessage("Invalid JSON format.");
             msg.setSeverity(FacesMessage.SEVERITY_ERROR);
