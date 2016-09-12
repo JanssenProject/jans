6934c69a312a90feee53d3359e2cbf798108b15e
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-09-12 13:06:22 +0300

    #44 : return op_host on registration (useful in case op_host is not provided as parameter but is used from fallback config)

--- oxd-common/src/main/java/org/xdi/oxd/common/response/RegisterSiteResponse.java
+++ oxd-common/src/main/java/org/xdi/oxd/common/response/RegisterSiteResponse.java
@@ -11,6 +11,8 @@ public class RegisterSiteResponse implements IOpResponse {
 
     @JsonProperty(value = "oxd_id")
     private String oxdId;
+    @JsonProperty(value = "op_host")
+    private String opHost;
 
     public RegisterSiteResponse() {
     }
@@ -23,11 +25,20 @@ public class RegisterSiteResponse implements IOpResponse {
         this.oxdId = oxdId;
     }
 
+    public String getOpHost() {
+        return opHost;
+    }
+
+    public void setOpHost(String opHost) {
+        this.opHost = opHost;
+    }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder();
         sb.append("RegisterSiteResponse");
         sb.append("{oxdId='").append(oxdId).append('\'');
+        sb.append("{opHost='").append(opHost).append('\'');
         sb.append('}');
         return sb.toString();
     }
