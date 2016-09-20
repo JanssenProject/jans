4f46794b0a0693d2ecceee21e501cbb3c7c59fef
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2016-09-20 19:54:55 +0300

    Updae filters to use lastModifiedTime | modifyTimestamp

--- Server/src/main/java/org/xdi/oxauth/service/SessionStateService.java
+++ Server/src/main/java/org/xdi/oxauth/service/SessionStateService.java
@@ -521,7 +521,8 @@ public class SessionStateService {
     public List<SessionState> getUnauthenticatedIdsOlderThan(int p_intervalInSeconds) {
         try {
             final long dateInPast = new Date().getTime() - TimeUnit.SECONDS.toMillis(p_intervalInSeconds);
-            final Filter filter = Filter.create(String.format("&(lastModifiedTime<=%s)(oxState=unauthenticated)", StaticUtils.encodeGeneralizedTime(new Date(dateInPast))));
+            String dateInPastString = StaticUtils.encodeGeneralizedTime(new Date(dateInPast));
+            final Filter filter = Filter.create(String.format("&(|(lastModifiedTime<=%s)(modifyTimestamp<=%s))(oxState=unauthenticated)", dateInPastString, dateInPastString));
             return ldapEntryManager.findEntries(getBaseDn(), SessionState.class, filter);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
@@ -533,7 +534,8 @@ public class SessionStateService {
     public List<SessionState> getIdsOlderThan(int p_intervalInSeconds) {
         try {
             final long dateInPast = new Date().getTime() - TimeUnit.SECONDS.toMillis(p_intervalInSeconds);
-            final Filter filter = Filter.create(String.format("(lastModifiedTime<=%s)", StaticUtils.encodeGeneralizedTime(new Date(dateInPast))));
+            String dateInPastString = StaticUtils.encodeGeneralizedTime(new Date(dateInPast));
+            final Filter filter = Filter.create(String.format("|(lastModifiedTime<=%s)(modifyTimestamp<=%s))", dateInPastString, dateInPastString));
             return ldapEntryManager.findEntries(getBaseDn(), SessionState.class, filter);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
