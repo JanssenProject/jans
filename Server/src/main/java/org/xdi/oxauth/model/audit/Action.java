f63227d3028dcebfb0f29e5cdd209e4c7f504ac8
worm333 <flexblademaster@gmail.com>
2016-10-24 15:44:45 +0300

    - added OAuth2 audit logging

---
+++ Server/src/main/java/org/xdi/oxauth/model/audit/Action.java
@@ -0,0 +1,23 @@
+package org.xdi.oxauth.model.audit;
+
+public enum Action {
+    CLIENT_REGISTRATION("CLIENT_REGISTRATION"),
+    CLIENT_UPDATE("CLIENT_UPDATE"),
+    CLIENT_READ("CLIENT_READ"),
+    CLIENT_DELETE("CLIENT_DELETE"),
+    USER_AUTHORIZATION("USER_AUTHORIZATION"),
+    USER_INFO("USER_INFO"),
+    TOKEN_REQUEST("TOKEN_REQUEST"),
+    TOKEN_VALIDATE("TOKEN_VALIDATE");
+
+    private String value;
+
+    Action(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
