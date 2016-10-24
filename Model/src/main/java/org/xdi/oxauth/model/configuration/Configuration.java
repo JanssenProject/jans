22ee0e3e608833c66294381a104e66533f9acf15
worm333 <flexblademaster@gmail.com>
2016-10-24 15:44:45 +0300

    - added OAuth2 audit logging

--- Model/src/main/java/org/xdi/oxauth/model/configuration/Configuration.java
+++ Model/src/main/java/org/xdi/oxauth/model/configuration/Configuration.java
@@ -136,6 +136,7 @@ public class Configuration {
     private String oxElevenDeleteKeyEndpoint;
 
     private Boolean endSessionWithAccessToken;
+    private Boolean enabledOAuthAuditLogging;
 
     private List<String> clientWhiteList;
     private List<String> clientBlackList;
@@ -1179,6 +1180,14 @@ public class Configuration {
 
     public void setEndSessionWithAccessToken(Boolean endSessionWithAccessToken) {
         this.endSessionWithAccessToken = endSessionWithAccessToken;
+    }
+
+    public Boolean getEnabledOAuthAuditLogging() {
+        return enabledOAuthAuditLogging;
+    }
+
+    public void setEnabledOAuthAuditLogging(Boolean enabledOAuthAuditLogging) {
+        this.enabledOAuthAuditLogging = enabledOAuthAuditLogging;
     }
 
     public List<String> getClientWhiteList() {
