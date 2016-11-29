e36e7b5c6bb90d23dda2ec90b4ed62cc6488406d
Mike Schwartz <mike@gluu.org>
2016-11-29 14:04:07 -0600

    Some handy scripts

---
+++ static/scripts/ldap_encrypt.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import os, hashlib, sys
+
+def ldap_encode(password):
+        salt = os.urandom(4)
+        sha = hashlib.sha1(password)
+        sha.update(salt)
+        b64encoded = '{0}{1}'.format(sha.digest(), salt).encode('base64').strip()
+        encrypted_password = '{{SSHA}}{0}'.format(b64encoded)
+        return encrypted_password
+
+print ldap_encode(sys.argv[1])
+
