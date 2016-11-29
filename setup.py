aa027a578f9d9bde5778d68926e1a33d180c94a9
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2016-11-29 12:37:30 +0200

    Unable to connect to OpenLDAP using openldap.crt #214

--- setup.py
+++ setup.py
@@ -1129,7 +1129,7 @@ class Setup(object):
             self.gen_cert('idp-encryption', self.shibJksPass, 'jetty')
             self.gen_cert('idp-signing', self.shibJksPass, 'jetty')
             self.gen_cert('asimba', self.asimbaJksPass, 'jetty')
-            self.gen_cert('openldap', self.openldapKeyPass, 'ldap', certCn)
+            self.gen_cert('openldap', self.openldapKeyPass, 'ldap', "localhost")
             # Shibboleth IDP and Asimba will be added soon...
             self.gen_keystore('shibIDP',
                               self.shibJksFn,
