4b26b2b7673625a844f16e944a0c1f6f767ca0ad
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2016-12-09 13:45:04 +0200

    Dump raw headers

--- Client/src/main/java/org/xdi/oxauth/client/ClientUtils.java
+++ Client/src/main/java/org/xdi/oxauth/client/ClientUtils.java
@@ -28,6 +28,10 @@ public class ClientUtils {
         System.out.println("RESPONSE:");
         System.out.println("-------------------------------------------------------");
         System.out.println(client.getResponseAsString());
+		if (client.getResponse() != null) {
+			System.out.println("RAW HEADERS:");
+			System.out.println(client.getResponse().getHeaders());
+		}
         System.out.println("");
     }
 
