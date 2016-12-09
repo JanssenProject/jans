ff331527c0a142c33e8effce38d4b12c31f356bd
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2016-12-09 13:42:01 +0200

    Revert "Update resteasy, jaxrs to latest 3.0.X from 2.3.7.Final"
    This reverts commit 47f94a22ff7849c8ffd11a194de56dbd75bfbcf1.

--- Client/src/main/java/org/xdi/oxauth/client/OpenIdConfigurationClient.java
+++ Client/src/main/java/org/xdi/oxauth/client/OpenIdConfigurationClient.java
@@ -78,7 +78,7 @@ public class OpenIdConfigurationClient extends BaseClient<OpenIdConfigurationReq
 
             String entity = clientResponse.getEntity(String.class);
             getResponse().setEntity(entity);
-            getResponse().setHeaders(clientResponse.getStringHeaders());
+            getResponse().setHeaders(clientResponse.getHeaders());
             if (StringUtils.isNotBlank(entity)) {
                 JSONObject jsonObj = new JSONObject(entity);
 
--- Client/src/main/java/org/xdi/oxauth/client/ValidateTokenClient.java
+++ Client/src/main/java/org/xdi/oxauth/client/ValidateTokenClient.java
@@ -85,7 +85,7 @@ public class ValidateTokenClient extends BaseClient<ValidateTokenRequest, Valida
             int status = clientResponse.getStatus();
 
             setResponse(new ValidateTokenResponse(status));
-            getResponse().setHeaders(clientResponse.getStringHeaders());
+            getResponse().setHeaders(clientResponse.getHeaders());
 
             String entity = clientResponse.getEntity(String.class);
             getResponse().setEntity(entity);
