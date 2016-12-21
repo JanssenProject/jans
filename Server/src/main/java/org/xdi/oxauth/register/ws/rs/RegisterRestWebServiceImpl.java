6c110dd4c79d3cae4a0a83b12bda79ecc10c03a6
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2016-12-21 19:55:59 +0200

    More fixes

--- Server/src/main/java/org/xdi/oxauth/register/ws/rs/RegisterRestWebServiceImpl.java
+++ Server/src/main/java/org/xdi/oxauth/register/ws/rs/RegisterRestWebServiceImpl.java
@@ -26,6 +26,7 @@ import org.xdi.oxauth.model.common.ResponseType;
 import org.xdi.oxauth.model.common.Scope;
 import org.xdi.oxauth.model.common.SubjectType;
 import org.xdi.oxauth.model.config.ConfigurationFactory;
+import org.xdi.oxauth.model.config.StaticConf;
 import org.xdi.oxauth.model.configuration.Configuration;
 import org.xdi.oxauth.model.crypto.signature.SignatureAlgorithm;
 import org.xdi.oxauth.model.error.ErrorResponseFactory;
@@ -88,8 +89,12 @@ public class RegisterRestWebServiceImpl implements RegisterRestWebService {
 
     @In
     private ExternalDynamicClientRegistrationService externalDynamicClientRegistrationService;
+
     @In
     private Configuration configuration;
+
+    @In
+    private StaticConf staticConfiguration;
 
     @Override
     public Response requestRegister(String requestParams, String authorization, HttpServletRequest httpRequest, SecurityContext securityContext) {
@@ -136,7 +141,7 @@ public class RegisterRestWebServiceImpl implements RegisterRestWebService {
                         } else {
                             RegisterParamsValidator.validateLogoutUri(r.getLogoutUris(), r.getRedirectUris(), errorResponseFactory);
 
-                            String clientsBaseDN = ConfigurationFactory.instance().getBaseDn().getClients();
+                            String clientsBaseDN = staticConfiguration.getBaseDn().getClients();
 
                             String inum = inumService.generateClientInum();
                             String generatedClientSecret = UUID.randomUUID().toString();
