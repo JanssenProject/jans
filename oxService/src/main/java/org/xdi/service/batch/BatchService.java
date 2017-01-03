35f00378db54815cd4b19dad770dad86273e35b6
worm333 <flexblademaster@gmail.com>
2017-01-03 22:20:10 +0200

    - refactored BatchService

--- oxService/src/main/java/org/xdi/service/batch/BatchService.java
+++ oxService/src/main/java/org/xdi/service/batch/BatchService.java
@@ -9,22 +9,18 @@ import java.util.List;
  */
 public abstract class BatchService<T> {
 
-    private int chunkSize;
-
-    public BatchService(int chunkSize) {
-        this.chunkSize = chunkSize;
-    }
-
     protected abstract List<T> getChunkOrNull(int offset, int chunkSize);
 
     protected abstract void performAction(List<T> objects);
 
-    public void execute() {
+    public void iterateAllByChunks(int chunkSize) {
         List<T> objects;
         int offset = 0;
         while (!CollectionUtils.isEmpty(objects = getChunkOrNull(offset, chunkSize))) {
             offset += objects.size();
             performAction(objects);
+            if(objects.size() < chunkSize)
+                break;
         }
     }
 }
