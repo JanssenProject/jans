947d32e70ec931f66ea53a6520216317d0b10da2
Pooya Parsa <pooya@pi0.ir>
2017-01-06 14:04:03 +0330

    Don't raise exceptions if openLdap dist not exists

--- setup.py
+++ setup.py
@@ -2281,7 +2281,7 @@ class Setup(object):
                 packageName = "%s/%s" % ( openLdapDistFolder, file )
 
         if packageName == None:
-            raise Exception('Failed to find OpenLDAP package in folder %s !' % directory)
+            self.logIt('Failed to find OpenLDAP package in folder %s !' % directory, True)
 
         self.logIt("Found package '%s' for install" % packageName)
         if packageRpm:
