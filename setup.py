9ff592ee35af52bfcb0403a5d457f757fd9ba180
Dmitry Ognyannikov <dmogn@mail.ru>
2017-01-13 23:50:05 -0300

    conf dir for asimba metadata store

--- setup.py
+++ setup.py
@@ -1592,6 +1592,7 @@ class Setup(object):
         self.copyFile(self.asimba_configuration, self.asimba_configuration_xml)
         self.copyFile(self.asimba_selector_configuration, self.asimba_selector_configuration_xml)
         self.run([self.cmd_chmod, 'uga+x', self.asimba_conf_folder])
+        self.run([self.cmd_chmod, 'u+w', self.asimba_conf_folder])
         self.run([self.cmd_chmod, 'uga+r', self.asimba_configuration_xml, self.asimba_selector_configuration_xml])
 
         self.logIt("Copying asimba.war into jetty webapps folder...")
