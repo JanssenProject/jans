df44df05a8cdda935429e394a80ba840da723c85
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2017-01-20 15:58:24 +0200

    Add method for unsafe XML loading when we now XML source

--- oxService/src/main/java/org/xdi/service/XmlService.java
+++ oxService/src/main/java/org/xdi/service/XmlService.java
@@ -167,9 +167,13 @@ public class XmlService {
     }
 
     public Document getXmlDocument(byte[] xmlDocumentBytes) throws SAXException, IOException, ParserConfigurationException {
+    	return getXmlDocument(xmlDocumentBytes, false);
+    }
+
+    public Document getXmlDocument(byte[] xmlDocumentBytes, boolean skipValidation) throws SAXException, IOException, ParserConfigurationException {
     	ByteArrayInputStream bis = new ByteArrayInputStream(xmlDocumentBytes);
     	try {
-    		DocumentBuilderFactory fty = creaeDocumentBuilderFactory();
+    		DocumentBuilderFactory fty = createDocumentBuilderFactory(skipValidation);
 
     		return fty.newDocumentBuilder().parse(bis);
     	} finally {
@@ -178,34 +182,43 @@ public class XmlService {
     }
 
     public Document getXmlDocument(InputStream is) throws SAXException, IOException, ParserConfigurationException {
-		DocumentBuilderFactory fty = creaeDocumentBuilderFactory();
+		DocumentBuilderFactory fty = createDocumentBuilderFactory();
 
 		return fty.newDocumentBuilder().parse(is);
     }
 
     public Document getXmlDocument(InputSource is) throws SAXException, IOException, ParserConfigurationException {
-		DocumentBuilderFactory fty = creaeDocumentBuilderFactory();
+		DocumentBuilderFactory fty = createDocumentBuilderFactory();
 
 		return fty.newDocumentBuilder().parse(is);
     }
 
     public Document getXmlDocument(String uri) throws SAXException, IOException, ParserConfigurationException {
-		DocumentBuilderFactory fty = creaeDocumentBuilderFactory();
+		DocumentBuilderFactory fty = createDocumentBuilderFactory();
 
 		return fty.newDocumentBuilder().parse(uri);
     }
 
-	private DocumentBuilderFactory creaeDocumentBuilderFactory() throws ParserConfigurationException {
+    private DocumentBuilderFactory createDocumentBuilderFactory() throws ParserConfigurationException {
+    	return createDocumentBuilderFactory(false);
+    }
+
+	private DocumentBuilderFactory createDocumentBuilderFactory(boolean skipValidation) throws ParserConfigurationException {
 		DocumentBuilderFactory fty = DocumentBuilderFactory.newInstance();
 
+		if (skipValidation) {
+			return fty;
+		}
+
 		fty.setNamespaceAware(true);
-		
+			
 		// Fix XXE vulnerability
 		fty.setXIncludeAware(false);
 		fty.setExpandEntityReferences(false);
 		fty.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
 		fty.setFeature("http://xml.org/sax/features/external-general-entities", false);
 		fty.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+
 		return fty;
 	}
 
