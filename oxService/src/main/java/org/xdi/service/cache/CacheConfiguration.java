a8fccbbb925532e33b42c66eed1509c0b08ab699
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2017-02-22 02:32:54 +0200

    cache : set in-memory as fallback cache provider

--- oxService/src/main/java/org/xdi/service/cache/CacheConfiguration.java
+++ oxService/src/main/java/org/xdi/service/cache/CacheConfiguration.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
 public class CacheConfiguration implements Serializable {
 
     @XmlElement(name = "cacheProviderType", required = true)
-    private CacheProviderType cacheProviderType;
+    private CacheProviderType cacheProviderType = CacheProviderType.IN_MEMORY;
 
     @XmlElement(name = "memcachedConfiguration")
     private MemcachedConfiguration memcachedConfiguration;
