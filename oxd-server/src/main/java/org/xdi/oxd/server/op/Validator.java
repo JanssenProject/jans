53385a46e02cb8162916c91ec85aea36e95985b7
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2017-03-14 13:56:54 +0200

    create new validator abstraction to unify validation logic within oxd. Added more details error messages for all stages of validation.

--- oxd-server/src/main/java/org/xdi/oxd/server/op/Validator.java
+++ oxd-server/src/main/java/org/xdi/oxd/server/op/Validator.java
@@ -1,5 +1,7 @@
 package org.xdi.oxd.server.op;
 
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import org.jboss.resteasy.client.core.executors.ApacheHttpClient4Executor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +16,9 @@ import org.xdi.oxauth.model.jwt.Jwt;
 import org.xdi.oxauth.model.jwt.JwtClaimName;
 import org.xdi.oxauth.model.jwt.JwtHeaderName;
 import org.xdi.oxd.common.CoreUtils;
+import org.xdi.oxd.common.ErrorResponseCode;
+import org.xdi.oxd.common.ErrorResponseException;
+import org.xdi.oxd.server.service.StateService;
 
 import java.util.Date;
 
@@ -26,7 +31,33 @@ public class Validator {
 
     private static final Logger LOG = LoggerFactory.getLogger(Validator.class);
 
-    private Validator() {
+    private final Jwt idToken;
+    private final RSASigner rsaSigner;
+    private final OpenIdConfigurationResponse discoveryResponse;
+
+    public Validator(Jwt idToken, OpenIdConfigurationResponse discoveryResponse) {
+        Preconditions.checkNotNull(idToken);
+        Preconditions.checkNotNull(discoveryResponse);
+
+        this.idToken = idToken;
+        this.discoveryResponse = discoveryResponse;
+        this.rsaSigner = createRSASigner(idToken, discoveryResponse);
+    }
+
+    public void validateAccessToken(String accessToken) {
+        if (!Strings.isNullOrEmpty(accessToken)) {
+            if (!rsaSigner.validateAccessToken(accessToken, idToken)) {
+                throw new ErrorResponseException(ErrorResponseCode.INVALID_ACCESS_TOKEN_BAD_HASH);
+            }
+        }
+    }
+
+    public void validateAuthorizationCode(String code) {
+        if (!Strings.isNullOrEmpty(code)) {
+            if (!rsaSigner.validateAuthorizationCode(code, idToken)) {
+                throw new ErrorResponseException(ErrorResponseCode.INVALID_AUTHORIZATION_CODE_BAD_HASH);
+            }
+        }
     }
 
     public static RSASigner createRSASigner(Jwt jwt, OpenIdConfigurationResponse discoveryResponse) {
@@ -60,48 +91,68 @@ public class Validator {
         }
     }
 
-    public static boolean isIdTokenValid(Jwt idToken, OpenIdConfigurationResponse discoveryResponse, String nonce, String clientId) {
-        return isIdTokenValid(idToken, discoveryResponse, nonce, clientId, createRSASigner(idToken, discoveryResponse));
+    public void validateNonce(StateService stateService) {
+        final String nonceFromToken = idToken.getClaims().getClaimAsString(JwtClaimName.NONCE);
+        if (!stateService.isNonceValid(nonceFromToken)) {
+            throw new ErrorResponseException(ErrorResponseCode.INVALID_NONCE);
+        }
     }
 
-    public static boolean isIdTokenValid(Jwt idToken, OpenIdConfigurationResponse discoveryResponse, String nonce, String clientId, RSASigner rsaSigner) {
+    public boolean isIdTokenValid(String clientId) {
+        try {
+            validateIdToken(clientId);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void validateIdToken(String clientId) {
+        validateIdToken(null, clientId);
+    }
+
+    public void validateIdToken(String nonce, String clientId) {
         try {
             final String issuer = idToken.getClaims().getClaimAsString(JwtClaimName.ISSUER);
             final String nonceFromToken = idToken.getClaims().getClaimAsString(JwtClaimName.NONCE);
             final String audienceFromToken = idToken.getClaims().getClaimAsString(JwtClaimName.AUDIENCE);
 
-            if (!nonceFromToken.endsWith(nonce)) {
+            if (!Strings.isNullOrEmpty(nonce) && !nonceFromToken.endsWith(nonce)) {
                 LOG.error("ID Token has invalid nonce. Expected nonce: " + nonce + ", nonce from token is: " + nonceFromToken);
-                return false;
+                throw new ErrorResponseException(ErrorResponseCode.INVALID_ID_TOKEN_BAD_NONCE);
             }
 
             if (!clientId.equalsIgnoreCase(audienceFromToken)) {
                 LOG.error("ID Token has invalid audience. Expected audience: " + clientId + ", audience from token is: " + audienceFromToken);
-                return false;
+                throw new ErrorResponseException(ErrorResponseCode.INVALID_ID_TOKEN_BAD_AUDIENCE);
             }
 
             final Date expiresAt = idToken.getClaims().getClaimAsDate(JwtClaimName.EXPIRATION_TIME);
             final Date now = new Date();
             if (now.after(expiresAt)) {
                 LOG.error("ID Token is expired. (It is after " + now + ").");
-                return false;
+                throw new ErrorResponseException(ErrorResponseCode.INVALID_ID_TOKEN_EXPIRED);
             }
 
             // 1. validate issuer
             if (!issuer.equals(discoveryResponse.getIssuer())) {
                 LOG.error("ID Token issuer is invalid. Token issuer: " + issuer + ", discovery issuer: " + discoveryResponse.getIssuer());
-                return false;
+                throw new ErrorResponseException(ErrorResponseCode.INVALID_ID_TOKEN_BAD_ISSUER);
             }
 
             // 2. validate signature
             final boolean signature = rsaSigner.validate(idToken);
             if (!signature) {
                 LOG.error("ID Token signature is invalid.");
+                throw new ErrorResponseException(ErrorResponseCode.INVALID_ID_TOKEN_BAD_SIGNATURE);
             }
-            return signature;
         } catch (Exception e) {
             LOG.error(e.getMessage(), e);
-            return false;
+            throw new ErrorResponseException(ErrorResponseCode.INVALID_ID_TOKEN_UNKNOWN);
         }
+    }
+
+    public Jwt getIdToken() {
+        return idToken;
     }
 }
