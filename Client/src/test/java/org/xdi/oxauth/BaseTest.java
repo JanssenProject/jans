8026836621416a3f49741a784535b0baa12bb152
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2017-03-22 19:12:59 +0200

    Check if session client/user is not empty to avoid NPE

--- Client/src/test/java/org/xdi/oxauth/BaseTest.java
+++ Client/src/test/java/org/xdi/oxauth/BaseTest.java
@@ -283,7 +283,6 @@ public abstract class BaseTest {
 
         if (userSecret != null) {
             if (userId != null) {
-            	System.out.println("authenticateResourceOwnerAndGrantAccess: pageSource:" + driver.getPageSource());
                 WebElement usernameElement = driver.findElement(By.name(loginFormUsername));
                 usernameElement.sendKeys(userId);
             }
