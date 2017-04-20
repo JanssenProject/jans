c5170d727e03a540c6bb32351e20d6f38cfae80c
yurem <Yuriy.Movchan@gmail.com>
2017-04-20 12:28:55 +0300

    Replace s:fragmenet with ui:fragement

--- Server/src/main/java/org/xdi/oxauth/util/ServerUtil.java
+++ Server/src/main/java/org/xdi/oxauth/util/ServerUtil.java
@@ -144,37 +144,22 @@ public class ServerUtil {
 		}
 	}
 
-    /*
-     * Try to avoid using this method. It's better to use injected Instance into bean
-     */
     public static <T> Instance<T> instance(Class<T> p_clazz) {
 		return CDI.current().select(p_clazz);
     }    	
 
-    /*
-     * Try to avoid using this method. It's better to use injected Instance into bean
-     */
     public static <T> Instance<T> instance(Class<T> p_clazz, String name) {
 		return CDI.current().select(p_clazz, NamedLiteral.of(name));
     }    	
 
-    /*
-     * Try to avoid using this method. It's better to use injected Instance into bean
-     */
     public static <T> T bean(Class<T> p_clazz) {
 		return instance(p_clazz).get();
     }    	
 
-    /*
-     * Try to avoid using this method. It's better to use injected Instance into bean
-     */
     public static <T> T bean(Class<T> p_clazz, String name) {
 		return instance(p_clazz, name).get();
     }    	
 
-    /*
-     * Try to avoid using this method. It's better to use injected Instance into bean
-     */
     public static <T> void destroy(Class<T> p_clazz) {
 		Instance<T> instance = instance(p_clazz);
 		if (instance.isResolvable()) {
@@ -182,9 +167,6 @@ public class ServerUtil {
 		}
     }    	
 
-    /*
-     * Try to avoid using this method. It's better to use injected Instance into bean
-     */
     public static <T> T destroy(Class<T> p_clazz, String name) {
 		Instance<T> instance = instance(p_clazz, name);
 		if (instance.isResolvable()) {
@@ -197,9 +179,6 @@ public class ServerUtil {
 		return null;
     }    	
 
-    /*
-     * Try to avoid using this method. It's better to use injected Instance into bean
-     */
     public static LdapEntryManager getLdapManager() {
         return bean(LdapEntryManager.class, AppInitializer.LDAP_ENTRY_MANAGER_NAME);
     }
