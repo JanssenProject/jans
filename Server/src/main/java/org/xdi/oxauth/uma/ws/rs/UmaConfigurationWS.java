8c69401bfa228390c1a7cb1fea5d594a2069d9a3
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2017-05-17 15:10:42 +0300

    #534 UMA 2 : introduced new metadata

--- Server/src/main/java/org/xdi/oxauth/uma/ws/rs/UmaConfigurationWS.java
+++ Server/src/main/java/org/xdi/oxauth/uma/ws/rs/UmaConfigurationWS.java
@@ -6,6 +6,20 @@
 
 package org.xdi.oxauth.uma.ws.rs;
 
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
+import org.slf4j.Logger;
+import org.xdi.oxauth.model.common.GrantType;
+import org.xdi.oxauth.model.common.ResponseType;
+import org.xdi.oxauth.model.configuration.AppConfiguration;
+import org.xdi.oxauth.model.error.ErrorResponseFactory;
+import org.xdi.oxauth.model.uma.UmaConfiguration;
+import org.xdi.oxauth.model.uma.UmaConstants;
+import org.xdi.oxauth.model.uma.UmaErrorResponseType;
+import org.xdi.oxauth.util.ServerUtil;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -13,30 +27,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 
-import org.slf4j.Logger;
-import org.xdi.oxauth.model.common.GrantType;
-import org.xdi.oxauth.model.common.TokenType;
-import org.xdi.oxauth.model.configuration.AppConfiguration;
-import org.xdi.oxauth.model.error.ErrorResponseFactory;
-import org.xdi.oxauth.model.uma.RptProfiles;
-import org.xdi.oxauth.model.uma.UmaConfiguration;
-import org.xdi.oxauth.model.uma.UmaConstants;
-import org.xdi.oxauth.model.uma.UmaErrorResponseType;
-import org.xdi.oxauth.util.ServerUtil;
-
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiResponse;
-import com.wordnik.swagger.annotations.ApiResponses;
-
 /**
- * The endpoint at which the requester can obtain UMA metadata configuration.
- *
- * @author Yuriy Movchan Date: 10/25/2012
- * @author Yuriy Zabrovarnyy Date: 03/12/2015
+ * The endpoint at which the requester can obtain UMA2 metadata configuration.
  */
-@Path("/oxauth/uma-configuration")
-@Api(value = "/.well-known/uma-configuration", description = "The authorization server endpoint that provides configuration data in a JSON [RFC4627] document that resides in at /.well-known/uma-configuration directory at its hostmeta [hostmeta] location. The configuration data documents conformance options and endpoints supported by the authorization server. ")
+@Path("/oxauth/uma2-configuration")
+@Api(value = "/.well-known/uma2-configuration", description = "The authorization server endpoint that provides configuration data in a JSON [RFC4627] document that resides in at /.well-known/uma2-configuration directory at its hostmeta [hostmeta] location. The configuration data documents conformance options and endpoints supported by the authorization server. ")
 public class UmaConfigurationWS {
 
     public static final String UMA_SCOPES_SUFFIX = "/uma/scopes";
@@ -64,34 +59,23 @@ public class UmaConfigurationWS {
             final String baseEndpointUri = appConfiguration.getBaseEndpoint();
 
             final UmaConfiguration c = new UmaConfiguration();
-            c.setVersion("1.0");
             c.setIssuer(appConfiguration.getIssuer());
-            c.setPatProfilesSupported(new String[]{TokenType.BEARER.getName()});
-            c.setAatProfilesSupported(new String[]{TokenType.BEARER.getName()});
-            c.setRptProfilesSupported(new String[]{RptProfiles.BEARER.getIdentifyingUri()});
-            c.setPatGrantTypesSupported(new String[]{
+            c.setGrantTypesSupported(new String[]{
                     GrantType.AUTHORIZATION_CODE.getValue(),
                     GrantType.IMPLICIT.getValue(),
                     GrantType.CLIENT_CREDENTIALS.getValue()
             });
-            c.setAatGrantTypesSupported(new String[]{
-                    GrantType.AUTHORIZATION_CODE.getValue(),
-                    GrantType.IMPLICIT.getValue(),
-                    GrantType.CLIENT_CREDENTIALS.getValue()
+            c.setResponseTypesSupported(new String[] {
+                    ResponseType.CODE.getValue(), ResponseType.ID_TOKEN.getValue(), ResponseType.TOKEN.getValue()
             });
-            c.setClaimTokenProfilesSupported(new String[]{"openid"});
+
             c.setUmaProfilesSupported(new String[0]);
-            c.setDynamicClientEndpoint(baseEndpointUri + "/oxauth/register");
+            c.setRegistrationEndpoint(baseEndpointUri + "/oxauth/register");
             c.setTokenEndpoint(baseEndpointUri + "/oxauth/token");
             c.setAuthorizationEndpoint(baseEndpointUri + "/requester/perm");
-            c.setRequestingPartyClaimsEndpoint("");
             c.setIntrospectionEndpoint(baseEndpointUri + "/rpt/status");
-            c.setResourceSetRegistrationEndpoint(baseEndpointUri + "/host/rsrc/resource_set");
-            c.setPermissionRegistrationEndpoint(baseEndpointUri + "/host/rsrc_pr");
-            c.setRptEndpoint(baseEndpointUri + "/requester/rpt");
-            c.setGatEndpoint(baseEndpointUri + "/requester/gat");
-            c.setScopeEndpoint(baseEndpointUri + UMA_SCOPES_SUFFIX);
-            c.setRptAsJwt(appConfiguration.getUmaRptAsJwt());
+            c.setResourceRegistrationEndpoint(baseEndpointUri + "/host/rsrc/resource_set");
+            c.setPermissionEndpoint(baseEndpointUri + "/host/rsrc_pr");
 
             // convert manually to avoid possible conflicts between resteasy providers, e.g. jettison, jackson
             final String entity = ServerUtil.asPrettyJson(c);
