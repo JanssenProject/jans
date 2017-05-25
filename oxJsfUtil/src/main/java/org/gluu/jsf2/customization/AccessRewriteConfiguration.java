67fa030f2b3dd4248b5357054e16015d04094efa
yurem <Yuriy.Movchan@gmail.com>
2017-05-25 21:22:39 +0300

    Ignore xhtml in /WEB-INF. Fix navigation.xml load when oxTrust exploded

--- oxJsfUtil/src/main/java/org/gluu/jsf2/customization/AccessRewriteConfiguration.java
+++ oxJsfUtil/src/main/java/org/gluu/jsf2/customization/AccessRewriteConfiguration.java
@@ -1,6 +1,19 @@
 package org.gluu.jsf2.customization;
 
-import com.sun.faces.util.FacesLogger;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+
+import javax.servlet.ServletContext;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.apache.commons.io.filefilter.RegexFileFilter;
@@ -13,15 +26,7 @@ import org.ocpsoft.rewrite.servlet.config.rule.Join;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 
-import javax.servlet.ServletContext;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Level;
+import com.sun.faces.util.FacesLogger;
 
 /**
  * Created by eugeniuparvan on 4/27/17.
@@ -29,6 +34,8 @@ import java.util.logging.Level;
 public class AccessRewriteConfiguration extends HttpConfigurationProvider {
 
     private static String REWRITE = "rewrite";
+    private static final String DEFAULT_NAVIGATION_PATH = "META-INF/navigation";
+    private static final String WEB_INF_PATH = "/WEB-INF";
 
     @Override
     public Configuration getConfiguration(final ServletContext context) {
@@ -38,32 +45,43 @@ public class AccessRewriteConfiguration extends HttpConfigurationProvider {
         factory.setNamespaceAware(true);
         try {
             DocumentBuilder documentBuilder = factory.newDocumentBuilder();
-            addRulesForAllXHTML(documentBuilder, context.getRealPath(""), builder);
+            Enumeration<URL> urlEnumeration = getClass().getClassLoader().getResources(DEFAULT_NAVIGATION_PATH);
+            if (urlEnumeration.hasMoreElements()) {
+                URL url = urlEnumeration.nextElement();
+                addRulesForAllXHTML(documentBuilder, context.getRealPath(""), url.getPath(), builder);
+            }
 
             if (!Utils.isCustomPagesDirExists())
                 return builder;
-            addRulesForAllXHTML(documentBuilder, Utils.getCustomPagesPath(), builder);
-        } catch (ParserConfigurationException e) {
-            FacesLogger.CONFIG.getLogger().log(Level.SEVERE, "Can't parse rewrite rules", e);
-        }
+            addRulesForAllXHTML(documentBuilder, Utils.getCustomPagesPath(), Utils.getCustomPagesPath(), builder);
+        } catch (ParserConfigurationException ex) {
+            FacesLogger.CONFIG.getLogger().log(Level.SEVERE, "Can't parse rewrite rules", ex);
+        } catch (IOException ex) {
+            FacesLogger.CONFIG.getLogger().log(Level.SEVERE, "Can't load navigation rules", ex);
+		}
         return builder;
     }
 
-    private void addRulesForAllXHTML(DocumentBuilder documentBuilder, String path, ConfigurationBuilder builder) {
-        Collection<File> xhtmlFiles = FileUtils.listFiles(new File(path), new RegexFileFilter(".*\\.xhtml$"), DirectoryFileFilter.DIRECTORY);
+    private void addRulesForAllXHTML(DocumentBuilder documentBuilder, String contexPath, String path, ConfigurationBuilder builder) {
+        Collection<File> xhtmlFiles = FileUtils.listFiles(new File(contexPath), new RegexFileFilter(".*\\.xhtml$"), DirectoryFileFilter.DIRECTORY);
         Collection<File> navigationFiles = FileUtils.listFiles(new File(path), new RegexFileFilter(".*\\.navigation\\.xml"), DirectoryFileFilter.DIRECTORY);
         Map<String, String> rewriteMap = getRewriteMap(documentBuilder, navigationFiles);
         for (File file : xhtmlFiles) {
             String xhtmlPath = file.getAbsolutePath();
             xhtmlPath = xhtmlPath.replace("\\", "/");
-            String xhtmlUri = xhtmlPath.substring(path.length(), xhtmlPath.lastIndexOf(".xhtml"));
+            String xhtmlUri = xhtmlPath.substring(contexPath.length(), xhtmlPath.lastIndexOf(".xhtml"));
 
             try {
                 String rewriteKey = file.getName().split("\\.")[0];
-                if (rewriteMap.containsKey(rewriteKey))
+                if (rewriteMap.containsKey(rewriteKey)) {
                     builder.addRule(Join.path(rewriteMap.get(rewriteKey)).to(xhtmlUri + ".htm"));
-                else
-                    builder.addRule(Join.path(xhtmlUri).to(xhtmlUri + ".htm"));
+                    System.out.println(rewriteMap.get(rewriteKey) + "-->" + xhtmlUri + ".htm");
+                } else {
+                	if (!xhtmlUri.startsWith(WEB_INF_PATH)) {
+                		builder.addRule(Join.path(xhtmlUri).to(xhtmlUri + ".htm"));
+                    	System.out.println(xhtmlUri + "-->" + xhtmlUri + ".htm");
+                	}
+                }
             } catch (ParameterizedPatternSyntaxException ex) {
                 FacesLogger.CONFIG.getLogger().log(Level.SEVERE, "Failed to add rule for " + xhtmlPath, ex);
             }
