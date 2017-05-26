9f1b459e2f3a0c04fc7220c3da0f1f309ef4f31b
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2017-05-26 10:28:45 +0300

    #558 UMA2 : adapting uma introspection endpoint to latest spec

--- Model/src/main/java/org/xdi/oxauth/model/uma/PermissionTicket.java
+++ Model/src/main/java/org/xdi/oxauth/model/uma/PermissionTicket.java
@@ -15,10 +15,9 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * Resource set permission ticket
- * 
- * @author Yuriy Movchan
+ *
  * @author Yuriy Zabrovarnyy
- * Date: 10/18/2012
+ * @author Yuriy Movchan
  */
 @IgnoreMediaTypes("application/*+json") // try to ignore jettison as it's recommended here: http://docs.jboss.org/resteasy/docs/2.3.4.Final/userguide/html/json.html
 @JsonPropertyOrder({ "ticket" })
@@ -41,28 +40,19 @@ public class PermissionTicket {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((ticket == null) ? 0 : ticket.hashCode());
-		return result;
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		PermissionTicket that = (PermissionTicket) o;
+
+		return !(ticket != null ? !ticket.equals(that.ticket) : that.ticket != null);
+
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PermissionTicket other = (PermissionTicket) obj;
-		if (ticket == null) {
-			if (other.ticket != null)
-				return false;
-		} else if (!ticket.equals(other.ticket))
-			return false;
-		return true;
+	public int hashCode() {
+		return ticket != null ? ticket.hashCode() : 0;
 	}
 
 	@Override
