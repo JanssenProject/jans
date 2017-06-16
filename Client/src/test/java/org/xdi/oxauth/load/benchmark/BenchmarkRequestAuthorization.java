294c83e2bfb4224225d6ab744db91fddef621bf9
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2017-06-16 09:28:38 +0300

    Fix benchmarking test

--- Client/src/test/java/org/xdi/oxauth/load/benchmark/BenchmarkRequestAuthorization.java
+++ Client/src/test/java/org/xdi/oxauth/load/benchmark/BenchmarkRequestAuthorization.java
@@ -17,6 +17,7 @@ import org.xdi.oxauth.load.benchmark.suite.BenchmarkTestListener;
 import org.xdi.oxauth.load.benchmark.suite.BenchmarkTestSuiteListener;
 import org.xdi.oxauth.model.common.Prompt;
 import org.xdi.oxauth.model.common.ResponseType;
+import org.xdi.oxauth.model.common.SubjectType;
 import org.xdi.oxauth.model.register.ApplicationType;
 import org.xdi.oxauth.model.util.StringUtils;
 
@@ -37,23 +38,18 @@ import static org.testng.Assert.assertNotNull;
 public class BenchmarkRequestAuthorization extends BaseTest {
 
     private String clientId;
+	private String clientSecret;
     private String redirectUri;
 
-    @Parameters({"userId", "userSecret", "redirectUris", "sectorIdentifierUri"})
+    @Parameters({"userId", "userSecret", "redirectUris", "redirectUri", "sectorIdentifierUri"})
     @BeforeClass
-    public void registerClient(final String userId, final String userSecret, String redirectUris, String sectorIdentifierUri) throws Exception {
+    public void registerClient(final String userId, final String userSecret, String redirectUris, final String redirectUri, String sectorIdentifierUri) throws Exception {
         Reporter.log("Register client", true);
 
         List<ResponseType> responseTypes = Arrays.asList(ResponseType.CODE, ResponseType.ID_TOKEN);
-        List<String> redirectUrisList = StringUtils.spaceSeparatedToList(redirectUris);
+        List<String> scopes = Arrays.asList("openid", "profile", "address", "email", "user_name");
 
-        RegisterRequest registerRequest = new RegisterRequest(ApplicationType.WEB, "oxAuth benchmark test app", redirectUrisList);
-        registerRequest.setResponseTypes(responseTypes);
-        registerRequest.setSectorIdentifierUri(sectorIdentifierUri);
-
-        RegisterClient registerClient = new RegisterClient(registrationEndpoint);
-        registerClient.setRequest(registerRequest);
-        RegisterResponse registerResponse = registerClient.exec();
+        RegisterResponse registerResponse = registerClient(redirectUris, responseTypes, scopes, sectorIdentifierUri);
 
         assertEquals(registerResponse.getStatus(), 200, "Unexpected response code: " + registerResponse.getEntity());
         assertNotNull(registerResponse.getClientId());
@@ -63,45 +59,34 @@ public class BenchmarkRequestAuthorization extends BaseTest {
         assertNotNull(registerResponse.getClientSecretExpiresAt());
 
         this.clientId = registerResponse.getClientId();
-        this.redirectUri = redirectUrisList.get(0);
+        this.clientSecret = registerResponse.getClientSecret();
+        this.redirectUri = redirectUri;
     }
 
-    @Parameters({"userId", "userSecret"})
+    @Parameters({"userId", "userSecret", "redirectUri"})
     @Test(invocationCount = 1000, threadPoolSize = 10)
-    public void testAuthorization1(final String userId, final String userSecret) throws Exception {
-        testAuthorizationImpl(userId, userSecret, this.redirectUri, this.clientId);
+    public void testAuthorization1(final String userId, final String userSecret, final String redirectUri) throws Exception {
+        testAuthorizationImpl(userId, userSecret, this.clientId, this.redirectUri);
     }
 
-    @Parameters({"userId", "userSecret"})
+    @Parameters({"userId", "userSecret", "redirectUri"})
     @Test(invocationCount = 1000, threadPoolSize = 10, dependsOnMethods = {"testAuthorization1"})
-    public void testAuthorization2(final String userId, final String userSecret) throws Exception {
-        testAuthorizationImpl(userId, userSecret, this.redirectUri, this.clientId);
+    public void testAuthorization2(final String userId, final String userSecret, final String redirectUri) throws Exception {
+        testAuthorizationImpl(userId, userSecret, this.clientId, this.redirectUri);
     }
 
-    @Parameters({"userId", "userSecret"})
+    @Parameters({"userId", "userSecret", "redirectUri"})
     @Test(invocationCount = 500, threadPoolSize = 2, dependsOnMethods = {"testAuthorization2"})
-    public void testAuthorization3(final String userId, final String userSecret) throws Exception {
-        testAuthorizationImpl(userId, userSecret, this.redirectUri, this.clientId);
+    public void testAuthorization3(final String userId, final String userSecret, final String redirectUri) throws Exception {
+        testAuthorizationImpl(userId, userSecret, this.clientId, this.redirectUri);
     }
 
-    private void testAuthorizationImpl(final String userId, final String userSecret, String redirectUri, String clientId) {
+    private void testAuthorizationImpl(final String userId, final String userSecret, final String clientId, final String redirectUri) {
         List<ResponseType> responseTypes = Arrays.asList(ResponseType.CODE, ResponseType.ID_TOKEN);
         List<String> scopes = Arrays.asList("openid", "profile", "address", "email", "user_name");
-        String state = UUID.randomUUID().toString();
         String nonce = UUID.randomUUID().toString();
 
-        AuthorizationRequest authorizationRequest = new AuthorizationRequest(responseTypes, clientId, scopes, redirectUri, null);
-
-        authorizationRequest.setAuthUsername(userId);
-        authorizationRequest.setAuthPassword(userSecret);
-
-        authorizationRequest.setState(state);
-        authorizationRequest.setNonce(nonce);
-        authorizationRequest.getPrompts().add(Prompt.NONE);
-
-        AuthorizeClient authorizeClient = new AuthorizeClient(this.authorizationEndpoint);
-        authorizeClient.setRequest(authorizationRequest);
-        AuthorizationResponse response = authorizeClient.exec();
+        AuthorizationResponse response = requestAuthorization(userId, userSecret, redirectUri, responseTypes, scopes, clientId, nonce);
 
         assertEquals(response.getStatus(), 302, "Unexpected response code: " + response.getEntity());
         assertNotNull(response.getLocation(), "The location is null");
@@ -109,6 +94,43 @@ public class BenchmarkRequestAuthorization extends BaseTest {
         assertNotNull(response.getIdToken(), "The id_token is null");
         assertNotNull(response.getState(), "The state is null");
         assertNotNull(response.getScope(), "The scope is null");
+    }
+
+    private AuthorizationResponse requestAuthorization(final String userId, final String userSecret, final String redirectUri,
+                                                       List<ResponseType> responseTypes, List<String> scopes, String clientId, String nonce) {
+        String state = UUID.randomUUID().toString();
+
+        AuthorizationRequest authorizationRequest = new AuthorizationRequest(responseTypes, clientId, scopes, redirectUri, nonce);
+        authorizationRequest.setState(state);
+
+        AuthorizationResponse authorizationResponse = authenticateResourceOwnerAndGrantAccess(
+                authorizationEndpoint, authorizationRequest, userId, userSecret);
+
+        return authorizationResponse;
+    }
+
+    private RegisterResponse registerClient(
+            final String redirectUris, List<ResponseType> responseTypes, List<String> scopes, String sectorIdentifierUri) {
+        RegisterRequest registerRequest = new RegisterRequest(ApplicationType.WEB, "oxAuth benchmark test app",
+                StringUtils.spaceSeparatedToList(redirectUris));
+        registerRequest.setResponseTypes(responseTypes);
+        registerRequest.setScopes(scopes);
+        registerRequest.setSubjectType(SubjectType.PAIRWISE);
+        registerRequest.setSectorIdentifierUri(sectorIdentifierUri);
+
+        RegisterClient registerClient = new RegisterClient(registrationEndpoint);
+        registerClient.setRequest(registerRequest);
+        RegisterResponse registerResponse = registerClient.exec();
+
+        showClient(registerClient);
+        assertEquals(registerResponse.getStatus(), 200, "Unexpected response code: " + registerResponse.getEntity());
+        assertNotNull(registerResponse.getClientId());
+        assertNotNull(registerResponse.getClientSecret());
+        assertNotNull(registerResponse.getRegistrationAccessToken());
+        assertNotNull(registerResponse.getClientIdIssuedAt());
+        assertNotNull(registerResponse.getClientSecretExpiresAt());
+
+        return registerResponse;
     }
 
 }
