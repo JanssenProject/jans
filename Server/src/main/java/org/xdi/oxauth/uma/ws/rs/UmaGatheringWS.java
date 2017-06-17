1b7c8c577d993e62237f14761cf976de47b846de
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2017-06-17 15:18:42 +0300

    #533 - UMA 2 : added claims_redirect_uri validation

--- Server/src/main/java/org/xdi/oxauth/uma/ws/rs/UmaGatheringWS.java
+++ Server/src/main/java/org/xdi/oxauth/uma/ws/rs/UmaGatheringWS.java
@@ -1,9 +1,19 @@
 package org.xdi.oxauth.uma.ws.rs;
 
 import org.slf4j.Logger;
+import org.xdi.oxauth.model.error.ErrorResponseFactory;
+import org.xdi.oxauth.model.registration.Client;
+import org.xdi.oxauth.model.uma.UmaConstants;
+import org.xdi.oxauth.model.uma.UmaErrorResponseType;
+import org.xdi.oxauth.uma.authorization.UmaWebException;
+import org.xdi.oxauth.uma.service.UmaValidationService;
 
 import javax.inject.Inject;
-import javax.ws.rs.Path;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import java.net.URI;
 
 /**
  * @author yuriyz on 06/04/2017.
@@ -13,4 +23,46 @@ public class UmaGatheringWS {
 
     @Inject
     private Logger log;
+
+    @Inject
+    private ErrorResponseFactory errorResponseFactory;
+
+    @Inject
+    private UmaValidationService umaValidationService;
+
+    @POST
+    @Consumes({UmaConstants.JSON_MEDIA_TYPE})
+    @Produces({UmaConstants.JSON_MEDIA_TYPE})
+    public Response gatherClaims(
+            @FormParam("client_id")
+            String clientId,
+            @FormParam("ticket")
+            String ticket,
+            @FormParam("claims_redirect_uri")
+            String claimRedirectUri,
+            @FormParam("state")
+            String state,
+            @Context HttpServletRequest httpRequest) {
+        try {
+            log.trace("gatherClaims client_id: {}, ticket: {}, claims_redirect_uri: {}, state: {}, queryString: {}"
+                    , clientId, ticket, claimRedirectUri, state, httpRequest.getQueryString());
+
+            Client client = umaValidationService.validateClientWithRedirect(clientId, claimRedirectUri);
+
+            // todo
+
+            String redirectUri = claimRedirectUri;
+            return Response.status(Response.Status.FOUND)
+                    .location(URI.create(redirectUri))
+                    .build();
+        } catch (Exception ex) {
+            log.error("Exception happened", ex);
+            if (ex instanceof WebApplicationException) {
+                throw (WebApplicationException) ex;
+            }
+        }
+
+        log.error("Failed to handle to UMA Claims Gathering Endpoint.");
+        throw new UmaWebException(Response.Status.INTERNAL_SERVER_ERROR, errorResponseFactory, UmaErrorResponseType.SERVER_ERROR);
+    }
 }
