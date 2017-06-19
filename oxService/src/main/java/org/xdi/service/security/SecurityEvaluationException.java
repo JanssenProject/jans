b8fd1539c83893ba734fe961443a74e794cf303b
yurem <Yuriy.Movchan@gmail.com>
2017-06-19 21:54:49 +0300

    Add ApplicationException annotation

--- oxService/src/main/java/org/xdi/service/security/SecurityEvaluationException.java
+++ oxService/src/main/java/org/xdi/service/security/SecurityEvaluationException.java
@@ -1,8 +1,11 @@
 package org.xdi.service.security;
 
+import javax.ejb.ApplicationException;
+
 /**
  * @author Yuriy Movchan Date: 05/22/2017
  */
+@ApplicationException(rollback=true)
 public class SecurityEvaluationException extends RuntimeException {
 
 	private static final long serialVersionUID = 7115786700134354355L;
