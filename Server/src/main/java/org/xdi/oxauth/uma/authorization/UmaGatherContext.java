bf48763ad58a67dc1bc4e56e6bdf1a7c8ebeaaf8
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2017-06-21 09:55:24 +0300

    #300 - UMA 2 : WIP - added user redirect uri wrapper, introduced uma gatherer

--- Server/src/main/java/org/xdi/oxauth/uma/authorization/UmaGatherContext.java
+++ Server/src/main/java/org/xdi/oxauth/uma/authorization/UmaGatherContext.java
@@ -2,9 +2,12 @@ package org.xdi.oxauth.uma.authorization;
 
 import org.xdi.oxauth.model.common.SessionState;
 import org.xdi.oxauth.service.external.context.ExternalScriptContext;
+import org.xdi.oxauth.uma.service.RedirectParameters;
 import org.xdi.oxauth.uma.service.UmaSessionService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author yuriyz on 06/18/2017.
@@ -13,10 +16,37 @@ public class UmaGatherContext extends ExternalScriptContext {
 
     private final SessionState session;
     private final UmaSessionService sessionService;
+    private final RedirectParameters redirectUserParameters = new RedirectParameters();
 
     public UmaGatherContext(HttpServletRequest httpRequest, SessionState session, UmaSessionService sessionService) {
         super(httpRequest);
         this.session = session;
         this.sessionService = sessionService;
     }
+
+    public int getStep() {
+        return sessionService.getStep(session);
+    }
+
+    public void setStep(int step) {
+        sessionService.setStep(step, session);
+    }
+
+
+    public void addRedirectUserParam(String paramName, String paramValue) {
+        redirectUserParameters.add(paramName, paramValue);
+    }
+
+    public void removeRedirectUserParameter(String paramName) {
+        redirectUserParameters.remove(paramName);
+    }
+
+    public RedirectParameters getRedirectUserParameters() {
+        return redirectUserParameters;
+    }
+
+    public Map<String, Set<String>> getRedirectUserParametersMap() {
+        return redirectUserParameters.map();
+    }
+
 }
