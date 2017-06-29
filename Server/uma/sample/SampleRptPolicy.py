7b6448b5678bfe091f2b1753cf5e0b3f40dd8d69
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2017-06-30 01:44:41 +0300

    #300 - UMA 2 : provided sample scripts

---
+++ Server/uma/sample/SampleRptPolicy.py
@@ -0,0 +1,62 @@
+# oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+# Copyright (c) 2017, Gluu
+#
+# Author: Yuriy Zabrovarnyy
+#
+
+from org.xdi.model.custom.script.type.uma import UmaRptPolicyType
+from org.xdi.model.uma import ClaimDefinitionBuilder
+from java.lang import String
+
+class SampleRptPolicy(UmaRptPolicyType):
+    def __init__(self, currentTimeMillis):
+        self.currentTimeMillis = currentTimeMillis
+
+    def init(self, configurationAttributes):
+        print "RPT Policy. Initializing ..."
+        print "RPT Policy. Initialized successfully"
+
+        return True
+
+    def destroy(self, configurationAttributes):
+        print "RPT Policy. Destroying ..."
+        print "RPT Policy. Destroyed successfully"
+        return True
+
+    def getApiVersion(self):
+        return 1
+
+    # Returns required claims definitions.
+    # This method must provide definition of all claims that is used in 'authorize' method.
+    # Note : name in both places must match.
+    def getRequiredClaims(self, context): # context is reference of org.xdi.oxauth.uma.authorization.UmaAuthorizationContext
+        return ClaimDefinitionBuilder.build(String.format("""[
+        {
+            "issuer" : [ "%s" ],
+            "name" : "country",
+            "claim_token_format" : [ "http://openid.net/specs/openid-connect-core-1_0.html#IDToken" ],
+            "claim_type" : "string",
+            "friendly_name" : "country"
+        },
+        {
+            "issuer" : [ "%s" ],
+            "name" : "city",
+            "claim_token_format" : [ "http://openid.net/specs/openid-connect-core-1_0.html#IDToken" ],
+            "claim_type" : "string",
+            "friendly_name" : "city"
+        }
+        ]""", context.getIssuer()))
+
+    # Main authorization method. Must return True or False.
+    def authorize(self, context): # context is reference of org.xdi.oxauth.uma.authorization.UmaAuthorizationContext
+        print "RPT Policy. Authorizing ..."
+
+        if (context.getClaim("country") == 'US' and context.getClaim("city") == 'NY'):
+            print "Authorized successfully!"
+            return True
+
+        return False
+
+    # Returns name of the Claims-Gathering script which will be invoked if need_info error is returned.
+    def getClaimsGatheringScriptName(self, context): # context is reference of org.xdi.oxauth.uma.authorization.UmaAuthorizationContext
+        return "sampleClaimsGathering"
