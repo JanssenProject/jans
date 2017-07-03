cdd4f9ea1ea61ddecf0862c0edd2a65e3c2b8362
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2017-07-03 09:43:19 +0300

    UMA 2 - WIP - upgrading

--- oxd-client/src/test/java/org/xdi/oxd/client/kong/KongTest.java
+++ oxd-client/src/test/java/org/xdi/oxd/client/kong/KongTest.java
@@ -1,23 +1,12 @@
 package org.xdi.oxd.client.kong;
 
 import com.google.common.base.Strings;
-import com.google.common.collect.Lists;
-import junit.framework.Assert;
-import org.apache.commons.lang.StringUtils;
 import org.codehaus.jackson.JsonNode;
 import org.jboss.resteasy.client.ClientResponse;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import org.xdi.oxd.client.CommandClient;
-import org.xdi.oxd.client.RegisterSiteTest;
-import org.xdi.oxd.common.Command;
-import org.xdi.oxd.common.CommandResponse;
-import org.xdi.oxd.common.CommandType;
-import org.xdi.oxd.common.params.RpGetGatParams;
-import org.xdi.oxd.common.response.RegisterSiteResponse;
-import org.xdi.oxd.common.response.RpGetRptResponse;
 
 import javax.ws.rs.core.Response;
 import java.io.IOException;
@@ -75,41 +64,14 @@ public class KongTest {
         System.out.println("GET /status/200 status: " + mockResponse.getStatus() + ", entity: " + mockResponse.getEntity());
         assertTrue(mockResponse.getStatus() == Response.Status.UNAUTHORIZED.getStatusCode());
 
-        // 4. obtain GAT with correct scope (gatScope)
-        String gat = getGat(oxdHost, oxdPort, opHost, redirectUrl, gatScope);
-        System.out.println("GAT: " + gat);
+        // todo - Upgrade kong. Kong supports UMA 1.0.1 (it does not support UMA 2).
+        // 4. obtain GAT with correct scope (gatScope) -
+        String gat = "";//getGat(oxdHost, oxdPort, opHost, redirectUrl, gatScope);
+//        System.out.println("GAT: " + gat);
 
         // 5. call api (must be unauthorized)
         mockResponse = mockBinService.status200Hello(kongApiRequestHost, "Bearer " + gat);
         System.out.println("GET /status/200 status: " + mockResponse.getStatus() + ", entity: " + mockResponse.getEntity());
         assertTrue(mockResponse.getStatus() == Response.Status.OK.getStatusCode());
     }
-
-
-    public String getGat(String host, int port, String opHost, String redirectUrl, String gatScope) throws IOException {
-        CommandClient client = null;
-        try {
-            client = new CommandClient(host, port);
-
-            RegisterSiteResponse site = RegisterSiteTest.registerSite(client, opHost, redirectUrl);
-
-            final RpGetGatParams params = new RpGetGatParams();
-            params.setOxdId(site.getOxdId());
-            params.setScopes(Lists.newArrayList(gatScope));
-
-            final Command command = new Command(CommandType.RP_GET_GAT);
-            command.setParamsObject(params);
-            final CommandResponse response = client.send(command);
-            Assert.assertNotNull(response);
-            System.out.println(response);
-
-            final RpGetRptResponse rptResponse = response.dataAsResponse(RpGetRptResponse.class);
-            Assert.assertNotNull(rptResponse);
-            Assert.assertTrue(StringUtils.isNotBlank(rptResponse.getRpt()));
-            return rptResponse.getRpt();
-        } finally {
-            CommandClient.closeQuietly(client);
-        }
-    }
-
 }
--- oxd-server/src/main/java/org/xdi/oxd/server/op/RpGetGetClaimsGatheringUrlOperation.java
+++ oxd-server/src/main/java/org/xdi/oxd/server/op/RpGetGetClaimsGatheringUrlOperation.java
@@ -7,7 +7,7 @@ import org.xdi.oxd.common.Command;
 import org.xdi.oxd.common.CommandResponse;
 import org.xdi.oxd.common.ErrorResponseCode;
 import org.xdi.oxd.common.ErrorResponseException;
-import org.xdi.oxd.common.params.RpGetGatParams;
+import org.xdi.oxd.common.params.RpGetGetClaimsGatheringUrlParams;
 import org.xdi.oxd.common.response.RpGetRptResponse;
 
 /**
@@ -15,16 +15,16 @@ import org.xdi.oxd.common.response.RpGetRptResponse;
  * @version 0.9, 17/06/2016
  */
 
-public class RpGetGatOperation extends BaseOperation<RpGetGatParams> {
+public class RpGetGetClaimsGatheringUrlOperation extends BaseOperation<RpGetGetClaimsGatheringUrlParams> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(RpGetGatOperation.class);
+    private static final Logger LOG = LoggerFactory.getLogger(RpGetGetClaimsGatheringUrlOperation.class);
 
-    protected RpGetGatOperation(Command command, final Injector injector) {
-        super(command, injector, RpGetGatParams.class);
+    protected RpGetGetClaimsGatheringUrlOperation(Command command, final Injector injector) {
+        super(command, injector, RpGetGetClaimsGatheringUrlParams.class);
     }
 
     @Override
-    public CommandResponse execute(RpGetGatParams params) {
+    public CommandResponse execute(RpGetGetClaimsGatheringUrlParams params) {
         validate(params);
 
         final RpGetRptResponse r = new RpGetRptResponse();
@@ -32,7 +32,7 @@ public class RpGetGatOperation extends BaseOperation<RpGetGatParams> {
         return okResponse(r);
     }
 
-    private void validate(RpGetGatParams params) {
+    private void validate(RpGetGetClaimsGatheringUrlParams params) {
         if (params.getScopes() == null || params.getScopes().isEmpty()) {
             throw new ErrorResponseException(ErrorResponseCode.INVALID_REQUEST_SCOPES_REQUIRED);
         }
