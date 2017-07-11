a2c87206e8d99f87554513ab32ee8aad2beb6a50
yurem <Yuriy.Movchan@gmail.com>
2017-07-11 09:26:53 +0300

    Fix UAF and SuperGluu scripts

--- Server/integrations/super_gluu/SuperGluuExternalAuthenticator.py
+++ Server/integrations/super_gluu/SuperGluuExternalAuthenticator.py
@@ -122,7 +122,7 @@ class PersonAuthentication(PersonAuthenticationType):
             print "Super-Gluu. Authenticate. redirect_uri is not set"
             return False
 
-        self.setEventContextParameters(context)
+        self.setRequestScopedParameters(identity)
 
         # Validate form result code and initialize QR code regeneration if needed (retry_current_step = True)
         identity.setWorkingParameter("retry_current_step", False)
@@ -278,7 +278,7 @@ class PersonAuthentication(PersonAuthenticationType):
             print "Super-Gluu. Prepare for step. redirect_uri is not set"
             return False
 
-        self.setEventContextParameters(context)
+        self.setRequestScopedParameters(identity)
 
         if step == 1:
             print "Super-Gluu. Prepare for step 1"
@@ -631,7 +631,7 @@ class PersonAuthentication(PersonAuthenticationType):
 
         return session_attributes.get("redirect_uri")
 
-    def setEventContextParameters(self, context):
+    def setRequestScopedParameters(self, identity):
         if self.registrationUri != None:
             identity.setWorkingParameter("external_registration_uri", self.registrationUri)
 
