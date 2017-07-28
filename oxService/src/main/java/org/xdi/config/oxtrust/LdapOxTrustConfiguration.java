bf901b3b9d5e7697670fdbec55009488fa2e9b16
rajnikant <rk@gluu.org>
2017-07-28 18:06:42 +0530

    add oxTrustAttributeResolver property

--- oxService/src/main/java/org/xdi/config/oxtrust/LdapOxTrustConfiguration.java
+++ oxService/src/main/java/org/xdi/config/oxtrust/LdapOxTrustConfiguration.java
@@ -40,6 +40,9 @@ public class LdapOxTrustConfiguration extends Entry {
     @LdapJsonObject //issue 102 - begin  : changed by shekhar
     @LdapAttribute(name = "oxTrustConfImportPerson")
     private ImportPersonConfig importPersonConfig; //issue 102 - end  : changed by shekhar
+    
+    @LdapAttribute(name = "oxTrustAttributeResolver")
+    private String oxTrustAttributeResolver;
 
 	public LdapOxTrustConfiguration() {
 	}
@@ -76,6 +79,15 @@ public class LdapOxTrustConfiguration extends Entry {
 	public void setImportPersonConfig(ImportPersonConfig importPersonConfig) {
 		this.importPersonConfig = importPersonConfig;
 	}//issue 102 - end  : changed by shekhar
+	
+
+	public String getOxTrustAttributeResolver() {
+		return oxTrustAttributeResolver;
+	}
+
+	public void setOxTrustAttributeResolver(String oxTrustAttributeResolver) {
+		this.oxTrustAttributeResolver = oxTrustAttributeResolver;
+	}
 
 	@Override
 	public String toString() {
