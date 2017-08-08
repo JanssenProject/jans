39ee808781eb09912045289fd84f4a1353d50096
yurem <Yuriy.Movchan@gmail.com>
2017-08-08 22:09:34 +0300

    Add method to use client_assertion in UMA 2.0 for authorization

--- Client/src/main/java/org/xdi/oxauth/client/uma/UmaTokenService.java
+++ Client/src/main/java/org/xdi/oxauth/client/uma/UmaTokenService.java
@@ -22,4 +22,18 @@ public interface UmaTokenService {
             @FormParam("pct") String pctCode,
             @FormParam("rpt") String rptCode,
             @FormParam("scope") String scope);
+
+    @POST
+    @Consumes({UmaConstants.JSON_MEDIA_TYPE})
+    @Produces({UmaConstants.JSON_MEDIA_TYPE})
+    UmaTokenResponse requestJwtAuthorizationRpt(
+            @FormParam("client_assertion_type") String clientAssertionType,
+            @FormParam("client_assertion") String clientAssertion,
+            @FormParam("grant_type") String grantType,
+            @FormParam("ticket") String ticket,
+            @FormParam("claim_token") String claimToken,
+            @FormParam("claim_token_format") String claimTokenFormat,
+            @FormParam("pct") String pctCode,
+            @FormParam("rpt") String rptCode,
+            @FormParam("scope") String scope);
 }
