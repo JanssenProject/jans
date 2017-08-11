ba68d323f30e3d0e5f62a8378876dc51ca3c350d
rajnikant <rk@gluu.org>
2017-08-11 11:27:10 +0530

    Added field enableClientGrantUpdate with getter setter

--- Model/src/main/java/org/xdi/oxauth/model/configuration/AppConfiguration.java
+++ Model/src/main/java/org/xdi/oxauth/model/configuration/AppConfiguration.java
@@ -148,6 +148,7 @@ public class AppConfiguration implements Configuration {
     private Boolean updateClientAccessTime;
     private Boolean logClientIdOnClientAuthentication;
     private Boolean logClientNameOnClientAuthentication;
+	private Boolean enableClientGrantUpdate;
 
     /**
      * Used in ServletLoggingFilter to enable http request/response logging.
@@ -1226,4 +1227,13 @@ public class AppConfiguration implements Configuration {
     public void setExternalLoggerConfiguration(String externalLoggerConfiguration) {
         this.externalLoggerConfiguration = externalLoggerConfiguration;
     }
+
+	public Boolean getEnableClientGrantUpdate() {
+		return enableClientGrantUpdate;
+	}
+
+	public void setEnableClientGrantUpdate(Boolean enableClientGrantUpdate) {
+		this.enableClientGrantUpdate = enableClientGrantUpdate;
+	}
+    
 }
