5e628ca3c0631155ce6703ccf38066fd5a0e1ec8
yurem <Yuriy.Movchan@gmail.com>
2017-08-21 10:45:19 +0300

    Resolve conflicts with previous pull request for issue #527

--- Model/src/main/java/org/xdi/oxauth/model/configuration/AppConfiguration.java
+++ Model/src/main/java/org/xdi/oxauth/model/configuration/AppConfiguration.java
@@ -154,7 +154,6 @@ public class AppConfiguration implements Configuration {
     private Boolean updateClientAccessTime;
     private Boolean logClientIdOnClientAuthentication;
     private Boolean logClientNameOnClientAuthentication;
-	private Boolean enableClientGrantUpdate;
 
     /**
      * Used in ServletLoggingFilter to enable http request/response logging.
@@ -1232,13 +1231,5 @@ public class AppConfiguration implements Configuration {
     public void setExternalLoggerConfiguration(String externalLoggerConfiguration) {
         this.externalLoggerConfiguration = externalLoggerConfiguration;
     }
-
-	public Boolean getEnableClientGrantUpdate() {
-		return enableClientGrantUpdate;
-	}
-
-	public void setEnableClientGrantUpdate(Boolean enableClientGrantUpdate) {
-		this.enableClientGrantUpdate = enableClientGrantUpdate;
-	}
     
 }
