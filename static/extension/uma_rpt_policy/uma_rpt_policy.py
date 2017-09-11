0f7f911d1cdc69444050f333b900a7ce3035b22c
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2017-09-11 15:58:50 +0300

    UMA 2 : fixed default sample RPT Policy script

--- static/extension/uma_rpt_policy/uma_rpt_policy.py
+++ static/extension/uma_rpt_policy/uma_rpt_policy.py
@@ -3,6 +3,16 @@
 #
 # Author: Yuriy Zabrovarnyy
 #
+# Call sequence
+# 1. First is call constructor of the Script __init__
+# 2. Next init() method
+# 3. Next getRequiredClaims() - method returns required claims, so UMA engine checks whether
+#    in request RP provided all claims that are required. Pay attention that there can be
+#    multiple scripts bound to the scopes, means that UMA engine will build set of required claims
+#    from all scripts. If not all claims are provided need_info error is sent to RP.
+#    During need_info construction getClaimsGatheringScriptName() method is called
+# 4. authorize() method is called if all required claims are provided.
+# 5. destroy()
 
 from org.xdi.model.custom.script.type.uma import UmaRptPolicyType
 from org.xdi.model.uma import ClaimDefinitionBuilder
@@ -29,57 +39,32 @@ class UmaRptPolicy(UmaRptPolicyType):
     # Returns required claims definitions.
     # This method must provide definition of all claims that is used in 'authorize' method.
     # Note : name in both places must match.
+    # %1$s - placeholder for issuer. It uses standard Java Formatter, docs : https://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html
     def getRequiredClaims(self, context): # context is reference of org.xdi.oxauth.uma.authorization.UmaAuthorizationContext
-        print context.getIssuer()
         json = """[
         {
-            "issuer" : [ "%s" ],
+            "issuer" : [ "%1$s" ],
             "name" : "country",
             "claim_token_format" : [ "http://openid.net/specs/openid-connect-core-1_0.html#IDToken" ],
             "claim_type" : "string",
             "friendly_name" : "country"
         },
         {
-            "issuer" : [ "%s" ],
+            "issuer" : [ "%1$s" ],
             "name" : "city",
             "claim_token_format" : [ "http://openid.net/specs/openid-connect-core-1_0.html#IDToken" ],
             "claim_type" : "string",
             "friendly_name" : "city"
         }
         ]"""
-
-        json2 = """[
-        {
-            "issuer" : [ "{}" ],
-            "name" : "country",
-            "claim_token_format" : [ "http://openid.net/specs/openid-connect-core-1_0.html#IDToken" ],
-            "claim_type" : "string",
-            "friendly_name" : "country"
-        },
-        {
-            "issuer" : [ "{}" ],
-            "name" : "city",
-            "claim_token_format" : [ "http://openid.net/specs/openid-connect-core-1_0.html#IDToken" ],
-            "claim_type" : "string",
-            "friendly_name" : "city"
-        }
-        ]"""
-        print json
-        print json2
-        print json2.format(context.getIssuer(), context.getIssuer())
-
-        string_format = String.format(json, context.getIssuer())
-
-        print "string_format : " + string_format
-        context.getLog().debug("string_format: " + string_format)
-
-        return ClaimDefinitionBuilder.build(string_format)
+        context.addRedirectUserParam("customUserParam1", "value1") # pass some custom parameters to need_info uri. It can be removed if you don't need custom parameters.
+        return ClaimDefinitionBuilder.build(String.format(json, context.getIssuer()))
 
     # Main authorization method. Must return True or False.
     def authorize(self, context): # context is reference of org.xdi.oxauth.uma.authorization.UmaAuthorizationContext
         print "RPT Policy. Authorizing ..."
 
-        if (context.getClaim("country") == 'US' and context.getClaim("city") == 'NY'):
+        if context.getClaim("country") == 'US' and context.getClaim("city") == 'NY':
             print "Authorized successfully!"
             return True
 
@@ -87,4 +72,5 @@ class UmaRptPolicy(UmaRptPolicyType):
 
     # Returns name of the Claims-Gathering script which will be invoked if need_info error is returned.
     def getClaimsGatheringScriptName(self, context): # context is reference of org.xdi.oxauth.uma.authorization.UmaAuthorizationContext
+        context.addRedirectUserParam("customUserParam2", "value2") # pass some custom parameters to need_info uri. It can be removed if you don't need custom parameters.
         return "sampleClaimsGathering"
