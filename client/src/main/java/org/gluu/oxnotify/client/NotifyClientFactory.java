53f55518217a77fe0023f5c6a43d9ebd275e0328
yurem <Yuriy.Movchan@gmail.com>
2017-09-19 20:45:27 +0300

    Use .well-known url for oxNotify metadata configuration

--- client/src/main/java/org/gluu/oxnotify/client/NotifyClientFactory.java
+++ client/src/main/java/org/gluu/oxnotify/client/NotifyClientFactory.java
@@ -50,7 +50,7 @@ public class NotifyClientFactory {
 	}
 
 	public NotifyMetadataClientService createMetaDataConfigurationService(String issuer) {
-		ResteasyWebTarget target = client.target(UriBuilder.fromPath(issuer + "/oxnotify/restv1/notify-configuration"));
+		ResteasyWebTarget target = client.target(UriBuilder.fromPath(issuer + "/.well-known/notify-configuration"));
 		return target.proxy(NotifyMetadataClientService.class);
 	}
 
