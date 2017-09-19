6f489e7cf1fa3c87954c4e8985e05a2806bad27d
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2017-09-19 10:23:45 +0300

    #649 UMA 2 : added convenient methods to UmaAuthorizationContext for fetching user and client claims (cherry picked from commit 9253a50)

--- Server/src/main/java/org/xdi/oxauth/uma/authorization/UmaAuthorizationContextBuilder.java
+++ Server/src/main/java/org/xdi/oxauth/uma/authorization/UmaAuthorizationContextBuilder.java
@@ -2,11 +2,14 @@ package org.xdi.oxauth.uma.authorization;
 
 import org.xdi.model.custom.script.conf.CustomScriptConfiguration;
 import org.xdi.oxauth.model.configuration.AppConfiguration;
+import org.xdi.oxauth.model.registration.Client;
 import org.xdi.oxauth.model.uma.persistence.UmaPermission;
 import org.xdi.oxauth.model.uma.persistence.UmaResource;
 import org.xdi.oxauth.model.uma.persistence.UmaScopeDescription;
 import org.xdi.oxauth.service.AttributeService;
+import org.xdi.oxauth.service.UserService;
 import org.xdi.oxauth.uma.service.UmaResourceService;
+import org.xdi.oxauth.uma.service.UmaSessionService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashSet;
@@ -26,22 +29,30 @@ public class UmaAuthorizationContextBuilder {
     private final Claims claims;
     private final HttpServletRequest httpRequest;
     private final AppConfiguration configuration;
+    private final UmaSessionService sessionService;
+    private final UserService userService;
+    private final Client client;
 
     public UmaAuthorizationContextBuilder(AppConfiguration configuration, AttributeService attributeService, UmaResourceService resourceService,
                                           List<UmaPermission> permissions, Map<UmaScopeDescription, Boolean> scopes,
-                                          Claims claims, HttpServletRequest httpRequest) {
+                                          Claims claims, HttpServletRequest httpRequest,
+                                          UmaSessionService sessionService, UserService userService, Client client) {
         this.configuration = configuration;
         this.attributeService = attributeService;
         this.resourceService = resourceService;
         this.permissions = permissions;
+        this.client = client;
         this.scopes = scopes;
         this.claims = claims;
         this.httpRequest = httpRequest;
+        this.sessionService = sessionService;
+        this.userService = userService;
     }
 
     public UmaAuthorizationContext build(CustomScriptConfiguration script) {
         return new UmaAuthorizationContext(configuration, attributeService, scopes, getResources(), claims,
-                script.getCustomScript().getDn(), httpRequest, script.getConfigurationAttributes());
+                script.getCustomScript().getDn(), httpRequest, script.getConfigurationAttributes(),
+                sessionService, userService, client);
     }
 
     public Set<String> getResourceIds() {
