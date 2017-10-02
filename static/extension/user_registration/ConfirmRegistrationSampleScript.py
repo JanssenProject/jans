b881e5d55acc33ec86e444a6a535b9a09395b180
yurem <Yuriy.Movchan@gmail.com>
2017-10-02 12:47:47 +0300

    Fix register person scripts

--- static/extension/user_registration/ConfirmRegistrationSampleScript.py
+++ static/extension/user_registration/ConfirmRegistrationSampleScript.py
@@ -2,16 +2,15 @@
 # Copyright (c) 2016, Gluu
 #
 
-
 from org.xdi.service.cdi.util import CdiUtil
 from org.xdi.model.custom.script.type.user import UserRegistrationType
 from org.xdi.service import MailService
-from org.gluu.oxtrust.ldap.service import IPersonService
+from org.gluu.oxtrust.ldap.service import PersonService
 from org.xdi.ldap.model import GluuStatus
 from org.xdi.util import StringHelper, ArrayHelper
 from java.util import Arrays, ArrayList
 from org.xdi.config.oxtrust import AppConfiguration
-from org.gluu.oxtrust.util import ServiceUtil
+from javax.faces.context import ExternalContext
 
 import java
 
@@ -45,13 +44,12 @@ class UserRegistration(UserRegistrationType):
     #   configurationAttributes is java.util.Map<String, SimpleCustomProperty>
     def preRegistration(self, user, requestParameters, configurationAttributes):
         print "User registration. Pre method"
-
-	userStatus = GluuStatus.INACTIVE
-
+        
+        userStatus = GluuStatus.INACTIVE
 
         # Disable/Enable registered user
         user.setStatus(userStatus)
-        self.guid=StringHelper.getRandomString(16)
+        self.guid = StringHelper.getRandomString(16)
         user.setGuid(self.guid)
         return True
 
@@ -61,34 +59,49 @@ class UserRegistration(UserRegistrationType):
     #   configurationAttributes is java.util.Map<String, SimpleCustomProperty>
     def postRegistration(self, user, requestParameters, configurationAttributes):
         print "User registration. Post method"
-	appConfiguration= CdiUtil.bean(AppConfiguration)
-        servername = appConfiguration.getApplianceUrl()
+        appConfiguration = CdiUtil.bean(AppConfiguration)
+
+        hostName = appConfiguration.getApplianceUrl()
+        externalContext = CdiUtil.bean(ExternalContext)
+        contextPath = externalContext.getRequest().getContextPath() 
+
         mailService = CdiUtil.bean(MailService)
         subject = "Confirmation mail for user registration"
-        body = "User Registered for %s. Please Confirm User Registration by clicking url: %s/confirm/registration?code=%s" % (user.getMail(),servername,self.guid)
-        print body
+        body = "User Registered for %s. Please Confirm User Registration by clicking url: %s%s/confirm/registration?code=%s" % (user.getMail(), hostName, contextPath, self.guid)
+        print "User registration. Post method. Attempting to send e-mail to '%s' message '%s'" % (user.getMail(), body)
+
         mailService.sendMail(user.getMail(), subject, body)
         return True
 
     def confirmRegistration(self, user, requestParameters, configurationAttributes):
-	print "User registration. Confirm method"
-	confirmation_code = ServerUtil.getFirstValue(requestParameters, "code")
-        if not confirmation_code:
-	    	print "User registration. Confirm method. Confirmation code not existin tin request"
-	    	return False
+        print "User registration. Confirm method"
 
-        personService = CdiUtil.bean(IPersonService)
+        code_array = requestParameters.get("code")
+        if ArrayHelper.isEmpty(code_array):
+            print "User registration. Confirm method. code is empty"
+            return False
+
+        confirmation_code = code_array[0]
+        print "User registration. Confirm method. code: '%s'" % confirmation_code
+
+        if confirmation_code == None:
+            print "User registration. Confirm method. Confirmation code not exist in request"
+            return False
+
+        personService = CdiUtil.bean(PersonService)
         user = personService.getPersonByAttribute("oxGuid", confirmation_code)
         if user == None:
-		print "User registration. Confirm method. There is no user by confirmation code: '%s'" % confirmation_code
-		return False
+            print "User registration. Confirm method. There is no user by confirmation code: '%s'" % confirmation_code
+            return False
 
-	if confirmation_code == user.getGuid():
-		user.setStatus(GluuStatus.ACTIVE)
-                user.setGuid("")
-                personService.updatePerson(user)
-		return True
+        if confirmation_code == user.getGuid():
+            user.setStatus(GluuStatus.ACTIVE)
+            user.setGuid("")
+            personService.updatePerson(user)
+            print "User registration. Confirm method. User '%s' confirmed his registration" % user.getUid()
+            return True
 
+        print "User registration. Confirm method. Confirmation code for user '%s' is invalid" % user.getUid()
     	return False
 
     def getApiVersion(self):
