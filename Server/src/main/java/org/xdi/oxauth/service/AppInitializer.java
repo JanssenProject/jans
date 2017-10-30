7c0037f778452a3710b6e5629c8e332420704521
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2017-10-30 20:44:35 +0300

    External authorization services

--- Server/src/main/java/org/xdi/oxauth/service/AppInitializer.java
+++ Server/src/main/java/org/xdi/oxauth/service/AppInitializer.java
@@ -177,8 +177,8 @@ public class AppInitializer {
         pythonService.initPythonInterpreter(configurationFactory.getLdapConfiguration().getString("pythonModulesDir", null));
 
 		// Initialize script manager
-        List<CustomScriptType> supportedCustomScriptTypes = Arrays.asList(CustomScriptType.PERSON_AUTHENTICATION, CustomScriptType.CLIENT_REGISTRATION,
-				CustomScriptType.ID_GENERATOR, CustomScriptType.UMA_RPT_POLICY, CustomScriptType.UMA_CLAIMS_GATHERING,
+        List<CustomScriptType> supportedCustomScriptTypes = Arrays.asList(CustomScriptType.PERSON_AUTHENTICATION, CustomScriptType.OPENID_AUTHORIZATION,
+        		CustomScriptType.CLIENT_REGISTRATION, CustomScriptType.ID_GENERATOR, CustomScriptType.UMA_RPT_POLICY, CustomScriptType.UMA_CLAIMS_GATHERING,
 				CustomScriptType.APPLICATION_SESSION, CustomScriptType.DYNAMIC_SCOPE);
 
         // Start timer
