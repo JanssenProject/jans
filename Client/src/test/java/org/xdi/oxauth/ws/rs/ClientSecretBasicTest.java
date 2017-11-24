584bb3a790ea5fe5c3df469490be1e4175981b71
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2017-11-23 23:49:51 -0400

    oxAuth #647 Support Custom Params

--- Client/src/test/java/org/xdi/oxauth/ws/rs/ClientSecretBasicTest.java
+++ Client/src/test/java/org/xdi/oxauth/ws/rs/ClientSecretBasicTest.java
@@ -1,3 +1,9 @@
+/*
+ * oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2014, Gluu
+ */
+
 package org.xdi.oxauth.ws.rs;
 
 import org.testng.annotations.Test;
