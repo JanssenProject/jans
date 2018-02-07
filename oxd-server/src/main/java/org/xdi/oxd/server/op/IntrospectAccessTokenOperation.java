9c53c6e520e73e7d2099f114353a28849a463a01
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-02-07 15:32:26 +0200

    fixing introspection compatibility issue #145

--- oxd-server/src/main/java/org/xdi/oxd/server/op/IntrospectAccessTokenOperation.java
+++ oxd-server/src/main/java/org/xdi/oxd/server/op/IntrospectAccessTokenOperation.java
@@ -2,15 +2,13 @@ package org.xdi.oxd.server.op;
 
 import com.google.inject.Injector;
 import org.codehaus.jackson.node.POJONode;
-import org.jboss.resteasy.client.ClientResponseFailure;
-import org.jboss.resteasy.client.ProxyFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.xdi.oxauth.client.service.IntrospectionService;
 import org.xdi.oxauth.model.common.IntrospectionResponse;
 import org.xdi.oxd.common.Command;
 import org.xdi.oxd.common.CommandResponse;
 import org.xdi.oxd.common.params.IntrospectAccessTokenParams;
+import org.xdi.oxd.server.service.IntrospectionService;
 
 /**
  * @author yuriyz
@@ -33,22 +31,8 @@ public class IntrospectAccessTokenOperation extends BaseOperation<IntrospectAcce
     public CommandResponse execute(IntrospectAccessTokenParams params) throws Exception {
         getValidationService().validate(params);
 
-        final IntrospectionService introspectionService = ProxyFactory.create(IntrospectionService.class, getDiscoveryService().getConnectDiscoveryResponseByOxdId(params.getOxdId()).getIntrospectionEndpoint(), getHttpService().getClientExecutor());
-        IntrospectionResponse response = null;
-
-        try {
-            response = introspectionService.introspectToken("Bearer " + getUmaTokenService().getPat(params.getOxdId()).getToken(), params.getAccessToken());
-        } catch (ClientResponseFailure e) {
-            int status = e.getResponse().getStatus();
-            LOG.debug("Failed to introspect token. Entity: " + e.getResponse().getEntity(String.class) + ", status: " + status, e);
-            if (status == 400 || status == 401) {
-                LOG.debug("Try maybe PAT is lost on AS, force refresh PAT and re-try ...");
-                getUmaTokenService().obtainPat(params.getOxdId()); // force to refresh PAT
-                response = introspectionService.introspectToken("Bearer " + getUmaTokenService().getPat(params.getOxdId()).getToken(), params.getAccessToken());
-            } else {
-                throw e;
-            }
-        }
+        final IntrospectionService introspectionService = getInstance(IntrospectionService.class);
+        IntrospectionResponse response = introspectionService.introspectToken(params.getOxdId(), params.getAccessToken());
 
         return CommandResponse.ok().setData(new POJONode(response));
     }
