c733fb8fde5a2c2f18750163f4e698f51981a063
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2018-02-15 20:49:16 +0300

    Fix persistence styles

--- oxPersistModel/src/main/java/org/gluu/persist/model/base/CustomAttribute.java
+++ oxPersistModel/src/main/java/org/gluu/persist/model/base/CustomAttribute.java
@@ -21,7 +21,7 @@ public class CustomAttribute implements Serializable, Comparable<CustomAttribute
 
     private static final long serialVersionUID = 1468450094325306154L;
 
-    private static final Logger log = LoggerFactory.getLogger(CustomAttribute.class);
+    private static final Logger LOG = LoggerFactory.getLogger(CustomAttribute.class);
 
     private String name;
     private List<String> values;
@@ -95,12 +95,18 @@ public class CustomAttribute implements Serializable, Comparable<CustomAttribute
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         CustomAttribute that = (CustomAttribute) o;
 
-        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) {
+            return false;
+        }
 
         return true;
     }
