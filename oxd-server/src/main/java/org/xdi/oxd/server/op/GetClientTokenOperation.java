a93494ec31f27a93b1c8a6e0e234dbcd94f88db7
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-02-19 13:48:17 +0200

    get-client-token : added support for private_key_jwt authentication method #144

--- oxd-server/src/main/java/org/xdi/oxd/server/op/GetClientTokenOperation.java
+++ oxd-server/src/main/java/org/xdi/oxd/server/op/GetClientTokenOperation.java
@@ -5,10 +5,15 @@ import com.google.inject.Injector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xdi.oxauth.client.TokenClient;
+import org.xdi.oxauth.client.TokenRequest;
 import org.xdi.oxauth.client.TokenResponse;
+import org.xdi.oxauth.model.common.AuthenticationMethod;
+import org.xdi.oxauth.model.common.GrantType;
+import org.xdi.oxauth.model.crypto.signature.SignatureAlgorithm;
 import org.xdi.oxauth.model.util.Util;
 import org.xdi.oxd.common.Command;
 import org.xdi.oxd.common.CommandResponse;
+import org.xdi.oxd.common.ErrorResponseCode;
 import org.xdi.oxd.common.ErrorResponseException;
 import org.xdi.oxd.common.params.GetClientTokenParams;
 import org.xdi.oxd.common.response.GetClientTokenResponse;
@@ -38,9 +43,35 @@ public class GetClientTokenOperation extends BaseOperation<GetClientTokenParams>
     @Override
     public CommandResponse execute(GetClientTokenParams params) throws Exception {
         try {
-            final TokenClient tokenClient = new TokenClient(getDiscoveryService().getConnectDiscoveryResponse(params.getOpHost(), params.getOpDiscoveryPath()).getTokenEndpoint());
+            final AuthenticationMethod authenticationMethod = AuthenticationMethod.fromString(params.getAuthenticationMethod());
+            final String tokenEndpoint = getDiscoveryService().getConnectDiscoveryResponse(params.getOpHost(), params.getOpDiscoveryPath()).getTokenEndpoint();
+            final TokenClient tokenClient = new TokenClient(tokenEndpoint);
             tokenClient.setExecutor(getHttpService().getClientExecutor());
-            final TokenResponse tokenResponse = tokenClient.execClientCredentialsGrant(scopeAsString(params), params.getClientId(), params.getClientSecret());
+
+            final TokenResponse tokenResponse;
+            if (authenticationMethod == AuthenticationMethod.PRIVATE_KEY_JWT) {
+                LOG.trace("Getting client token with private_key_jwt client authentication ...");
+
+                SignatureAlgorithm algorithm = SignatureAlgorithm.fromString(params.getAlgorithm());
+                if (algorithm == null) {
+                    throw new ErrorResponseException(ErrorResponseCode.INVALID_ALGORITHM);
+                }
+
+                TokenRequest tokenRequest = new TokenRequest(GrantType.CLIENT_CREDENTIALS);
+                tokenRequest.setScope(scopeAsString(params));
+                tokenRequest.setAuthUsername(params.getClientId());
+                tokenRequest.setAuthenticationMethod(AuthenticationMethod.PRIVATE_KEY_JWT);
+                tokenRequest.setAlgorithm(algorithm);
+                tokenRequest.setCryptoProvider(getCryptoProvider());
+                tokenRequest.setKeyId(params.getKeyId());
+                tokenRequest.setAudience(tokenEndpoint);
+
+                tokenClient.setRequest(tokenRequest);
+                tokenResponse = tokenClient.exec();
+            } else {
+                tokenResponse = tokenClient.execClientCredentialsGrant(scopeAsString(params), params.getClientId(), params.getClientSecret());
+            }
+
             if (tokenResponse != null) {
                 if (Util.allNotBlank(tokenResponse.getAccessToken())) {
                     GetClientTokenResponse response = new GetClientTokenResponse();
@@ -56,6 +87,7 @@ public class GetClientTokenOperation extends BaseOperation<GetClientTokenParams>
                 }
             } else {
                 LOG.error("No response from TokenClient");
+                LOG.error("Please check AS logs for more details (oxauth.log for CE).");
             }
         } catch (ErrorResponseException e) {
             throw e;
