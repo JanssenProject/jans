0456cc7c8a243cd0c6bb2cf76d1d9a1f2c905bc7
Jose G <bonustrack310@gmail.com>
2018-03-16 14:34:18 -0500

    Fixes in master branch for https://github.com/GluuFederation/oxTrust/issues/797

--- Server/src/main/java/org/xdi/oxauth/service/SessionIdService.java
+++ Server/src/main/java/org/xdi/oxauth/service/SessionIdService.java
@@ -142,10 +142,10 @@ public class SessionIdService {
 	
 	                log.info("Acr is changed. Session acr: " + sessionAcr + "(level: " + sessionAcrLevel + "), " +
 	                        "current acr: " + acrValue + "(level: " + currentAcrLevel + ")");
-	                
-	                // Requested acr method which not enabled
+
+	                // Requested acr method not enabled
 	                if (currentAcrLevel == null) {
-	                    throw new AcrChangedException();
+	                    throw new AcrChangedException(false);
 	                }
 
 	                if (sessionAcrLevel < currentAcrLevel) {
