3ee5097dade32cab4f8045e4e221dc51597bc772
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2018-03-18 21:44:40 +0300

    Improve password reset functionality #903

--- oxService/src/main/java/org/xdi/service/MailService.java
+++ oxService/src/main/java/org/xdi/service/MailService.java
@@ -4,13 +4,16 @@
  * Copyright (c) 2014, Gluu
  */package org.xdi.service;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.Properties;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
@@ -26,132 +29,128 @@ import org.xdi.util.StringHelper;
 
 /**
  * Provides operations with sending E-mails
- *
+ * 
  * @author Yuriy Movchan Date: 20/04/2014
  */
 @RequestScoped
 @Named
 public class MailService {
 
-    @Inject
-    private Logger log;
+	@Inject
+	private Logger log;
 
-    @Inject
-    private SmtpConfiguration smtpConfiguration;
+	@Inject
+	private SmtpConfiguration smtpConfiguration;
 
-    private long connectionTimeout = 5000;
+	private long connectionTimeout = 5000;
 
-    public boolean sendMail(String to, String subject, String message) {
-        return sendMail(smtpConfiguration, null, null, to, null, subject, message, null);
-    }
+	public boolean sendMail(String to, String subject, String message) {
+		return sendMail(smtpConfiguration, null, null, to, null, subject, message, null);
+	}
 
-    public boolean sendMail(String to, String toDisplayName, String subject, String message, String htmlMessage) {
-        return sendMail(smtpConfiguration, null, null, to, null, subject, message, htmlMessage);
-    }
+	public boolean sendMail(String to, String toDisplayName, String subject, String message, String htmlMessage) {
+		return sendMail(smtpConfiguration, null, null, to, null, subject, message, htmlMessage);
+	}
 
-    public boolean sendMail(String from, String fromDisplayName, String to, String toDisplayName, String subject, String message,
-            String htmlMessage) {
-        return sendMail(smtpConfiguration, from, fromDisplayName, to, null, subject, message, htmlMessage);
-    }
+	public boolean sendMail(String from, String fromDisplayName, String to, String toDisplayName, String subject, String message, String htmlMessage) {
+		return sendMail(smtpConfiguration, from, fromDisplayName, to, null, subject, message, htmlMessage);
+	}
 
-    public boolean sendMail(SmtpConfiguration mailSmtpConfiguration, String from, String fromDisplayName, String to, String toDisplayName,
-            String subject, String message, String htmlMessage) {
-        if (mailSmtpConfiguration == null) {
-            log.error("Failed to send message from '{}' to '{}' because the SMTP configuration isn't valid!", from, to);
-            return false;
-        }
+	public boolean sendMail(SmtpConfiguration mailSmtpConfiguration, String from, String fromDisplayName, String to, String toDisplayName, String subject, String message, String htmlMessage) {
+		if (mailSmtpConfiguration == null) {
+			log.error("Failed to send message from '{}' to '{}' because the SMTP configuration isn't valid!", from, to);
+			return false;
+		}
 
-        log.debug("Host name: " + mailSmtpConfiguration.getHost() + ", port: " + mailSmtpConfiguration.getPort() + ", connection time out: "
-                + this.connectionTimeout);
+		log.debug("Host name: " + mailSmtpConfiguration.getHost() + ", port: " + mailSmtpConfiguration.getPort() + ", connection time out: "
+				+ this.connectionTimeout);
 
-        String mailFrom = from;
-        if (StringHelper.isEmpty(mailFrom)) {
-            mailFrom = mailSmtpConfiguration.getFromEmailAddress();
-        }
+		String mailFrom = from;
+		if (StringHelper.isEmpty(mailFrom)) {
+			mailFrom = mailSmtpConfiguration.getFromEmailAddress();
+		}
 
-        String mailFromName = fromDisplayName;
-        if (StringHelper.isEmpty(mailFromName)) {
-            mailFromName = mailSmtpConfiguration.getFromName();
-        }
+		String mailFromName = fromDisplayName;
+		if (StringHelper.isEmpty(mailFromName)) {
+			mailFromName = mailSmtpConfiguration.getFromName();
+		}
 
-        Properties props = new Properties();
-        props.put("mail.smtp.host", mailSmtpConfiguration.getHost());
-        props.put("mail.smtp.port", mailSmtpConfiguration.getPort());
-        props.put("mail.from", mailFrom);
-        props.put("mail.smtp.connectiontimeout", this.connectionTimeout);
-        props.put("mail.smtp.timeout", this.connectionTimeout);
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.ssl.trust", mailSmtpConfiguration.getHost());
-        props.put("mail.debug", true);
+		Properties props = new Properties();
+		props.put("mail.smtp.host", mailSmtpConfiguration.getHost());
+		props.put("mail.smtp.port", mailSmtpConfiguration.getPort());
+		props.put("mail.from", mailFrom);
+		props.put("mail.smtp.connectiontimeout", this.connectionTimeout);
+		props.put("mail.smtp.timeout", this.connectionTimeout);
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.ssl.trust", mailSmtpConfiguration.getHost());
 
-        if (mailSmtpConfiguration.isRequiresSsl()) {
-            props.put("mail.smtp.socketFactory.port", mailSmtpConfiguration.getPort());
-            props.put("mail.smtp.starttls.enable", true);
-            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-            props.put("mail.transport.protocol", "smtps");
-        }
+		if (mailSmtpConfiguration.isRequiresSsl()) {
+			props.put("mail.smtp.socketFactory.port", mailSmtpConfiguration.getPort());
+			props.put("mail.smtp.starttls.enable", true);
+			props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		}
 
-        Session session = null;
-        if (mailSmtpConfiguration.isRequiresAuthentication()) {
-            props.put("mail.smtp.auth", "true");
+		Session session = null;
+		if (mailSmtpConfiguration.isRequiresAuthentication()) {
+			props.put("mail.smtp.auth", "true");
 
-            final String userName = mailSmtpConfiguration.getUserName();
-            final String password = mailSmtpConfiguration.getPasswordDecrypted();
+			final String userName = mailSmtpConfiguration.getUserName();
+			final String password = mailSmtpConfiguration.getPasswordDecrypted();
 
-            session = Session.getInstance(props, new javax.mail.Authenticator() {
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(userName, password);
-                }
-            });
-        } else {
-            Session.getInstance(props, null);
-        }
+			session = Session.getInstance(props, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(userName, password);
+				}
+			});
+		} else {
+			Session.getInstance(props, null);
+		}
 
-        MimeMessage msg = new MimeMessage(session);
-        try {
-            msg.setFrom(new InternetAddress(mailFrom, mailFromName));
-            if (StringHelper.isEmpty(toDisplayName)) {
-                msg.setRecipients(Message.RecipientType.TO, to);
-            } else {
-                msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to, toDisplayName));
-            }
-            msg.setSubject(subject, "UTF-8");
-            msg.setSentDate(new Date());
+		MimeMessage msg = new MimeMessage(session);
+		try {
+			msg.setFrom(new InternetAddress(mailFrom, mailFromName));
+			if (StringHelper.isEmpty(toDisplayName)) {
+				msg.setRecipients(Message.RecipientType.TO, to);
+			} else {
+				msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to, toDisplayName));
+			}
+			msg.setSubject(subject, "UTF-8");
+			msg.setSentDate(new Date());
 
-            if (StringHelper.isEmpty(htmlMessage)) {
-                msg.setText(message + "\n", "UTF-8");
-            } else {
-                // Unformatted text version
-                final MimeBodyPart textPart = new MimeBodyPart();
-                textPart.setContent(message, "text/plain");
-                // HTML version
-                final MimeBodyPart htmlPart = new MimeBodyPart();
-                htmlPart.setContent(htmlMessage, "text/html");
+			if (StringHelper.isEmpty(htmlMessage)) {
+				msg.setText(message + "\n", "UTF-8", "text/plain");
+			} else {
+				// Unformatted text version
+				final MimeBodyPart textPart = new MimeBodyPart();
+				textPart.setText(message, "UTF-8", "text/plain");
+				// HTML version
+				final MimeBodyPart htmlPart = new MimeBodyPart();
+				htmlPart.setText(htmlMessage, "UTF-8", "text/html");
 
-                // Create the Multipart. Add BodyParts to it.
-                final Multipart mp = new MimeMultipart("alternative");
-                mp.addBodyPart(textPart);
-                mp.addBodyPart(htmlPart);
+				// Create the Multipart. Add BodyParts to it.
+				final Multipart mp = new MimeMultipart("alternative");
+				mp.addBodyPart(textPart);
+				mp.addBodyPart(htmlPart);
 
-                // Set Multipart as the message's content
-                msg.setContent(mp);
-            }
+				// Set Multipart as the message's content
+				msg.setContent(mp);
+			}
 
-            Transport.send(msg);
-        } catch (Exception ex) {
-            log.error("Failed to send message", ex);
-            return false;
-        }
+			Transport.send(msg);
+		} catch (Exception ex) {
+			log.error("Failed to send message", ex);
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    public long getConnectionTimeout() {
-        return connectionTimeout;
-    }
+	public long getConnectionTimeout() {
+		return connectionTimeout;
+	}
 
-    public void setConnectionTimeout(long connectionTimeout) {
-        this.connectionTimeout = connectionTimeout;
-    }
+	public void setConnectionTimeout(long connectionTimeout) {
+		this.connectionTimeout = connectionTimeout;
+	}
 
 }
