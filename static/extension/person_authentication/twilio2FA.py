39876fc79e4e627db130298a2502c12faff52d31
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2018-03-20 22:39:59 +0300

    Sync scripts from oxAuth

--- static/extension/person_authentication/twilio2FA.py
+++ static/extension/person_authentication/twilio2FA.py
@@ -1,22 +1,24 @@
 # oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
-# Copyright (c) 2016, Gluu
+# Copyright (c) 2018, Gluu
 #
-# Author: Michael Schwartz
-#
+# Author: Jose Gonzalez
 
-import random
-
-import com.twilio.sdk.TwilioRestClient as TwilioRestClient
-import java.util.ArrayList as ArrayList
-import org.apache.http.message.BasicNameValuePair as BasicNameValuePair
-from java.util import Arrays
-from org.xdi.model.custom.script.type.auth import PersonAuthenticationType
+from org.xdi.service.cdi.util import CdiUtil
 from org.xdi.oxauth.security import Identity
+from org.xdi.model.custom.script.type.auth import PersonAuthenticationType
 from org.xdi.oxauth.service import UserService, AuthenticationService
 from org.xdi.oxauth.util import ServerUtil
-from org.xdi.service.cdi.util import CdiUtil
-from org.xdi.util import StringHelper
+from org.xdi.util import StringHelper, ArrayHelper
+from java.util import Arrays
 
+import com.twilio.Twilio as Twilio
+import com.twilio.rest.api.v2010.account.Message as Message
+import com.twilio.type.PhoneNumber as PhoneNumber
+import org.codehaus.jettison.json.JSONArray as JSONArray
+
+import java
+import random
+import jarray
 
 class PersonAuthentication(PersonAuthenticationType):
     def __init__(self, currentTimeMillis):
@@ -25,10 +27,10 @@ class PersonAuthentication(PersonAuthenticationType):
     def init(self, configurationAttributes):
         print "Twilio SMS. Initialization"
 
-        self.ACCOUNT_SID = None    
-        self.AUTH_TOKEN = None    
+        self.ACCOUNT_SID = None
+        self.AUTH_TOKEN = None
         self.FROM_NUMBER = None
-        
+
         # Get Custom Properties
         try:
             self.ACCOUNT_SID = configurationAttributes.get("twilio_sid").getValue2()
@@ -50,7 +52,7 @@ class PersonAuthentication(PersonAuthenticationType):
 
         print "Twilio SMS. Initialized successfully"
 
-        return True    
+        return True
 
     def destroy(self, configurationAttributes):
         print "Twilio SMS. Destroy"
@@ -85,7 +87,7 @@ class PersonAuthentication(PersonAuthenticationType):
 
             user_name = credentials.getUsername()
             user_password = credentials.getPassword()
-            
+
             logged_in = False
             if StringHelper.isNotEmptyString(user_name) and StringHelper.isNotEmptyString(user_password):
                 logged_in = authenticationService.authenticate(user_name, user_password)
@@ -103,32 +105,22 @@ class PersonAuthentication(PersonAuthenticationType):
 
             try:
                 mobile_number = foundUser.getAttribute("phoneNumberVerified")
+                if isinstance(mobile_number, JSONArray):
+                    mobile_number=mobile_number.get(0)
+                    print "TwilioSMS, Message will be sent to number ", mobile_number
             except:
-                print 'TwilioSMS, Error finding mobile number for' % (user_name) 
+                print 'TwilioSMS, Error finding mobile number for' % (user_name)
                 return False
-                        
+
             # Generate Random six digit code and store it in array
             code = random.randint(100000, 999999)
-            
-            # Get code and save it in LDAP temporarily with special session entry 
+
+            # Get code and save it in LDAP temporarily with special session entry
             identity.setWorkingParameter("code", code)
 
-            # Store user phone number in authentication session 
-            identity.setWorkingParameter("mobile_number", mobile_number)
-
-            client = TwilioRestClient(self.ACCOUNT_SID, self.AUTH_TOKEN)
-            bodyParam = BasicNameValuePair("Body", str(code))
-            toParam = BasicNameValuePair("To", mobile_number)
-            fromParam = BasicNameValuePair("From", self.FROM_NUMBER)
-
-            params = ArrayList()
-            params.add(bodyParam)
-            params.add(toParam)
-            params.add(fromParam)
-
             try:
-                messageFactory = client.getAccount().getMessageFactory()
-                message = messageFactory.create(params)
+                Twilio.init(self.ACCOUNT_SID, self.AUTH_TOKEN);
+                message = Message.creator(PhoneNumber(mobile_number), PhoneNumber(self.FROM_NUMBER), str(code)).create();
 
                 print 'TwilioSMs, Message Sid: %s' % (message.getSid())
                 return True
@@ -142,15 +134,15 @@ class PersonAuthentication(PersonAuthenticationType):
             print "TwilioSMS. Step 2 SMS/OTP Authentication"
             code = session_attributes.get("code")
             print "TwilioSMS. Code: %s" % str(code)
-    
+
             if code is None:
                 print "TwilioSMS. Failed to find previously sent code"
-                return False 
-    
+                return False
+
             if form_passcode is None:
                 print "TwilioSMS. Passcode is empty"
-                return False 
-    
+                return False
+
             if len(form_passcode) != 6:
                 print "TwilioSMS. Passcode from response is not 6 digits: %s" % form_passcode
                 return False
@@ -161,7 +153,7 @@ class PersonAuthentication(PersonAuthenticationType):
 
             print "TwilioSMS. FAIL! User entered the wrong code! %s != %s" % (form_passcode, code)
 
-            return False            
+            return False
 
         print "TwilioSMS. ERROR: step param not found or != (1|2)"
 
@@ -178,7 +170,10 @@ class PersonAuthentication(PersonAuthenticationType):
         return False
 
     def getExtraParametersForStep(self, configurationAttributes, step):
-        return Arrays.asList("code", "mobile_number")
+        if step == 2:
+            return Arrays.asList("code")
+
+        return None
 
     def getCountAuthenticationSteps(self, configurationAttributes):
         return 2
