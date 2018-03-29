19af542f0e167b567764f24d134637efe5deeb7b
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2018-03-29 20:15:13 +0300

    Dynamic scope should contains list of allowed claims #519

--- Server/src/main/java/org/xdi/oxauth/service/external/ExternalDynamicScopeService.java
+++ Server/src/main/java/org/xdi/oxauth/service/external/ExternalDynamicScopeService.java
@@ -6,6 +6,8 @@
 
 package org.xdi.oxauth.service.external;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -18,6 +20,7 @@ import org.xdi.model.SimpleCustomProperty;
 import org.xdi.model.custom.script.CustomScriptType;
 import org.xdi.model.custom.script.conf.CustomScriptConfiguration;
 import org.xdi.model.custom.script.type.scope.DynamicScopeType;
+import org.xdi.oxauth.model.common.Scope;
 import org.xdi.oxauth.service.external.context.DynamicScopeExternalContext;
 import org.xdi.service.custom.script.ExternalScriptService;
 
@@ -53,6 +56,24 @@ public class ExternalDynamicScopeService extends ExternalScriptService {
 		return false;
 	}
 
+    public List<String> executeExternalGetSupportedClaimsMethod(CustomScriptConfiguration customScriptConfiguration) {
+        int apiVersion = executeExternalGetApiVersion(customScriptConfiguration);
+        
+        if (apiVersion > 1) {
+            try {
+                log.debug("Executing python 'get supported claims' method");
+                DynamicScopeType dynamicScopeType = (DynamicScopeType) customScriptConfiguration.getExternalType();
+                Map<String, SimpleCustomProperty> configurationAttributes = customScriptConfiguration.getConfigurationAttributes();
+                return dynamicScopeType.getSupportedClaims(configurationAttributes);
+            } catch (Exception ex) {
+                log.error(ex.getMessage(), ex);
+                saveScriptError(customScriptConfiguration.getCustomScript(), ex);
+            }
+        }
+        
+        return null;
+    }
+
 	private Set<CustomScriptConfiguration> getScriptsToExecute(DynamicScopeExternalContext context) {
         Set<String> allowedScripts = Sets.newHashSet();
 		for (org.xdi.oxauth.model.common.Scope scope : context.getScopes()) {
@@ -82,5 +103,19 @@ public class ExternalDynamicScopeService extends ExternalScriptService {
 
 		return result;
 	}
+
+    public List<String> executeExternalGetSupportedClaimsMethods(List<Scope> dynamicScope) {
+        DynamicScopeExternalContext context = new DynamicScopeExternalContext(dynamicScope, null, null);
+
+        Set<String> result = new HashSet<String>();
+        for (CustomScriptConfiguration customScriptConfiguration : getScriptsToExecute(context)) {
+            List<String> scriptResult = executeExternalGetSupportedClaimsMethod(customScriptConfiguration);
+            if (scriptResult != null) {
+                result.addAll(scriptResult);
+            }
+        }
+
+        return new ArrayList<String>(result);
+    }
 
 }
