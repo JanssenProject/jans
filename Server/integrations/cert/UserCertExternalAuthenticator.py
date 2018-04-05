abc07bd69ee03d97f12ea77998aef9d8bdb9c9cd
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2018-04-05 22:36:51 +0300

    Support configurable primary keys in various scripts

--- Server/integrations/cert/UserCertExternalAuthenticator.py
+++ Server/integrations/cert/UserCertExternalAuthenticator.py
@@ -296,7 +296,7 @@ class PersonAuthentication(PersonAuthenticationType):
         if (not logged_in):
             return None
 
-        find_user_by_uid = userService.getUser(user_name)
+        find_user_by_uid = authenticationService.getAuthenticatedUser()
         if (find_user_by_uid == None):
             print "Cert. Process basic authentication. Failed to find user '%s'" % user_name
             return None
