31dee2fb3b05b282b14fa9b803fd5a4667c5710c
yurem <Yuriy.Movchan@gmail.com>
2018-04-25 12:02:05 +0300

    Store custom data in U2F entry

--- Model/src/main/java/org/xdi/oxauth/model/fido/u2f/protocol/DeviceData.java
+++ Model/src/main/java/org/xdi/oxauth/model/fido/u2f/protocol/DeviceData.java
@@ -7,6 +7,7 @@
 package org.xdi.oxauth.model.fido.u2f.protocol;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.annotate.JsonProperty;
@@ -42,10 +43,13 @@ public class DeviceData implements Serializable {
 	@JsonProperty(value = "os_version")
 	private final String osVersion;
 
+    @JsonProperty(value = "custom_data")
+    private final List<String> customData;
+
 	public DeviceData(@JsonProperty(value = "uuid") String uuid, @JsonProperty(value = "token") String pushToken,
 			@JsonProperty(value = "type") String type, @JsonProperty(value = "platform") String platform,
 			@JsonProperty(value = "name") String name, @JsonProperty(value = "os_name") String osName,
-			@JsonProperty(value = "os_version") String osVersion) {
+			@JsonProperty(value = "os_version") String osVersion, List<String> customData) {
 		this.uuid = uuid;
 		this.pushToken = pushToken;
 		this.type = type;
@@ -53,6 +57,7 @@ public class DeviceData implements Serializable {
 		this.name = name;
 		this.osName = osName;
 		this.osVersion = osVersion;
+        this.customData = customData;
 	}
 
 	public String getUuid() {
@@ -83,12 +88,14 @@ public class DeviceData implements Serializable {
 		return osVersion;
 	}
 
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("DeviceData [uuid=").append(uuid).append(", pushToken=").append(pushToken).append(", type=").append(type).append(", platform=")
-				.append(platform).append(", name=").append(name).append(", osName=").append(osName).append(", osVersion=").append(osVersion).append("]");
-		return builder.toString();
-	}
+	public final List<String> getCustomData() {
+        return customData;
+    }
+
+    @Override
+    public String toString() {
+        return "DeviceData [uuid=" + uuid + ", pushToken=" + pushToken + ", type=" + type + ", platform=" + platform + ", name=" + name + ", osName="
+                + osName + ", osVersion=" + osVersion + ", customData=" + customData + "]";
+    }
 
 }
