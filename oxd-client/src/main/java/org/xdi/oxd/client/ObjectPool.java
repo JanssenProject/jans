acbf50c7fe0fe3a29e0b559e48acc9e7ba720a8a
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-05-17 00:35:13 +0300

    #94 - introduced pool for command client objects to re-use socket connections

---
+++ oxd-client/src/main/java/org/xdi/oxd/client/ObjectPool.java
@@ -0,0 +1,65 @@
+package org.xdi.oxd.client;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author yuriyz
+ */
+public abstract class ObjectPool<T> {
+
+    private long expirationTime;
+
+    private final Map<T, Long> locked = new HashMap<T, Long>();
+    private final Map<T, Long> unlocked = new HashMap<T, Long>();
+
+    public ObjectPool(int expirationInSeconds) {
+        expirationTime = expirationInSeconds * 1000;
+    }
+
+    protected abstract T create() throws IOException;
+
+    public abstract boolean validate(T o);
+
+    public abstract void expire(T o);
+
+    public synchronized T checkOut() throws IOException {
+        long now = System.currentTimeMillis();
+        T t = null;
+        if (unlocked.size() > 0) {
+            for (T obj : unlocked.keySet()) {
+                t = obj;
+                if ((now - unlocked.get(t)) > expirationTime) {
+                    // object has expired
+                    unlocked.remove(t);
+                    expire(t);
+                    t = null;
+                } else {
+                    if (validate(t)) {
+                        unlocked.remove(t);
+                        locked.put(t, now);
+                        return (t);
+                    } else {
+                        // object failed validation
+                        unlocked.remove(t);
+                        expire(t);
+                        t = null;
+                    }
+                }
+            }
+        }
+        // no objects available, create a new one
+        t = create();
+        locked.put(t, now);
+        return t;
+    }
+
+    public synchronized void checkIn(T t) {
+        if (t == null) {
+            return;
+        }
+        locked.remove(t);
+        unlocked.put(t, System.currentTimeMillis());
+    }
+}
