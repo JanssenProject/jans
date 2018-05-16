0f6e168d01bba7527e0a6633ef5a3bf7c32973fa
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-05-17 00:35:13 +0300

    #94 - introduced pool for command client objects to re-use socket connections

---
+++ oxd-client/src/main/java/org/xdi/oxd/client/CommandClientPool.java
@@ -0,0 +1,66 @@
+package org.xdi.oxd.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+/**
+ * @author yuriyz
+ */
+public class CommandClientPool extends ObjectPool<CommandClient> {
+
+    /**
+     * Logger
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(CommandClientPool.class);
+
+    private final ConcurrentSkipListSet<Integer> loggerNames = new ConcurrentSkipListSet<>();
+    private final String host;
+    private final int port;
+
+    public CommandClientPool(int expirationInSeconds, String host, int port) {
+        super(expirationInSeconds);
+        this.host = host;
+        this.port = port;
+    }
+
+    @Override
+    protected CommandClient create() throws IOException {
+        CommandClient client = new CommandClient(host, port);
+        client.setNameForLogger(nextLoggerName());
+        return client;
+    }
+
+    @Override
+    public boolean validate(CommandClient o) {
+        return o.isValid();
+    }
+
+    @Override
+    public void expire(CommandClient o) {
+        CommandClient.closeQuietly(o);
+        loggerNames.remove(o.getNameForLogger());
+    }
+
+    @Override
+    public synchronized CommandClient checkOut() {
+        try {
+            return super.checkOut();
+        } catch (IOException e) {
+            LOG.error("Failed to check out command client.", e);
+            return null;
+        }
+    }
+
+    private synchronized int nextLoggerName() {
+        for (int i = 1; i < 10000; i++) {
+            if (!loggerNames.contains(i)) {
+                loggerNames.add(i);
+                return i;
+            }
+        }
+        return -2;
+    }
+}
