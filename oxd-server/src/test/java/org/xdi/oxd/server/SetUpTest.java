67cda72908b06f1b24ac9cd0f1ddd47d964ccb03
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-05-21 13:35:19 +0300

    #181 : added jersey client proxy and started to move tests to https

--- oxd-server/src/test/java/org/xdi/oxd/server/SetUpTest.java
+++ oxd-server/src/test/java/org/xdi/oxd/server/SetUpTest.java
@@ -3,6 +3,9 @@
  */
 package org.xdi.oxd.server;
 
+import io.dropwizard.testing.ConfigOverride;
+import io.dropwizard.testing.DropwizardTestSupport;
+import io.dropwizard.testing.ResourceHelpers;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.xdi.oxd.common.CoreUtils;
@@ -18,18 +21,26 @@ import org.xdi.oxd.server.service.RpService;
 
 public class SetUpTest {
 
+    public static final DropwizardTestSupport<OxdServerConfiguration> SUPPORT =
+            new DropwizardTestSupport<OxdServerConfiguration>(OxdServerApplication.class,
+                    ResourceHelpers.resourceFilePath("oxd-server-jenkins.yaml"),
+                    ConfigOverride.config("server.applicationConnectors[0].port", "0") // Optional, if not using a separate testing-specific configuration file, use a randomly selected port
+            );
+
+
     @BeforeSuite
     public static void beforeSuite() {
         System.out.println("Running beforeSuite ...");
         ServerLauncher.setSetUpSuite(true);
+        SUPPORT.before();
+
         CoreUtils.createExecutor().execute(new Runnable() {
             @Override
             public void run() {
                 removeExistingRps();
-
-                ServerLauncher.configureServices(null);
             }
         });
+
         // from one side we should give time to start server, from other we can't start in current
         // thread because it will block suite thread, ugly but works...
         CoreUtils.sleep(10);
@@ -50,6 +61,7 @@ public class SetUpTest {
 
     @AfterSuite
     public static void afterSuite() {
+        SUPPORT.after();
         ServerLauncher.shutdown();
     }
 
