23dbd205949a65329668ab01b4e9b4b06f0cf5f5
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-05-22 12:19:53 +0300

    #181 : excluded log4j biding for slf4j

--- oxd-server/src/test/java/org/xdi/oxd/server/SetUpTest.java
+++ oxd-server/src/test/java/org/xdi/oxd/server/SetUpTest.java
@@ -26,7 +26,7 @@ public class SetUpTest {
 
     public static final DropwizardTestSupport<OxdServerConfiguration> SUPPORT =
             new DropwizardTestSupport<OxdServerConfiguration>(OxdServerApplication.class,
-                    ResourceHelpers.resourceFilePath("../src/test/resources/oxd-server-jenkins.yaml"),
+                    ResourceHelpers.resourceFilePath("./oxd-server/src/test/resources/oxd-server-jenkins.yaml"),
                     ConfigOverride.config("server.applicationConnectors[0].port", "0") // Optional, if not using a separate testing-specific configuration file, use a randomly selected port
             );
 
@@ -35,8 +35,11 @@ public class SetUpTest {
     @BeforeSuite
     public static void beforeSuite(String host, String opHost, String redirectUrl) {
         try {
-            System.out.println(">> " + ResourceHelpers.resourceFilePath("oxd-server-jenkins.yaml"));
-            System.out.println(">> " + ResourceHelpers.resourceFilePath("../src/test/resources/oxd-server-jenkins.yaml"));
+            System.out.println("PATH>> " + ResourceHelpers.resourceFilePath("oxd-server-jenkins.yaml"));
+            System.out.println("PATH>> " + ResourceHelpers.resourceFilePath("./oxd-server/src/test/resources/oxd-server-jenkins.yaml"));
+            LOG.debug("PATH>> " + ResourceHelpers.resourceFilePath("oxd-server-jenkins.yaml"));
+            LOG.debug("PATH>> " + ResourceHelpers.resourceFilePath("./oxd-server/src/test/resources/oxd-server-jenkins.yaml"));
+
             LOG.debug("Running beforeSuite ...");
             ServerLauncher.setSetUpSuite(true);
             SUPPORT.before();
