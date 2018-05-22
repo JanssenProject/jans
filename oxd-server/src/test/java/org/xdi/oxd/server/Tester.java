2aa7cf1142457f09533a822b7919c84ec79ef272
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-05-22 10:17:07 +0300

    #181 : http version of RegisterSiteTest

--- oxd-server/src/test/java/org/xdi/oxd/server/Tester.java
+++ oxd-server/src/test/java/org/xdi/oxd/server/Tester.java
@@ -1,5 +1,8 @@
 package org.xdi.oxd.server;
 
+import org.xdi.oxd.client.ClientInterface;
+import org.xdi.oxd.client.OxdClient;
+
 /**
  * @author Yuriy Zabrovarnyy
  * @version 0.9, 05/10/2015
@@ -8,5 +11,16 @@ package org.xdi.oxd.server;
 public class Tester {
 
     private Tester() {
+    }
+
+    public static ClientInterface newClient(String targetHost) {
+        if ("http://localhost".equalsIgnoreCase(targetHost) || "http://127.0.0.1".equalsIgnoreCase(targetHost) ) {
+            targetHost = targetHost + ":" + SetUpTest.SUPPORT.getLocalPort();
+        }
+        return OxdClient.newClient(targetHost);
+    }
+
+    public static String getAuthorization() {
+        return "";
     }
 }
