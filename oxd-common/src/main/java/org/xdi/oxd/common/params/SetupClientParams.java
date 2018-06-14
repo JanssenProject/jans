6a6ee21b55bf44acfb2cf5e3c94510d04b7326dd
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-06-14 13:51:23 +0300

    Added `setup_client_name` parameter to `setup_client` command https://github.com/GluuFederation/oxd/issues/200

--- oxd-common/src/main/java/org/xdi/oxd/common/params/SetupClientParams.java
+++ oxd-common/src/main/java/org/xdi/oxd/common/params/SetupClientParams.java
@@ -1,6 +1,7 @@
 package org.xdi.oxd.common.params;
 
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
 
 /**
  * @author Yuriy Zabrovarnyy
@@ -9,4 +10,15 @@ import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SetupClientParams extends RegisterSiteParams {
+
+    @JsonProperty(value = "setup_client_name")
+    private String setupClientName;
+
+    public String getSetupClientName() {
+        return setupClientName;
+    }
+
+    public void setSetupClientName(String setupClientName) {
+        this.setupClientName = setupClientName;
+    }
 }
