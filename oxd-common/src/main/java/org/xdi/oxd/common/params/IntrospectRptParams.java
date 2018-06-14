11b28a7504ec50ab2cad263ad5f73545e0a6f904
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-06-14 23:03:53 +0300

    Fixed reporting client usages when get-client-token command is executed. https://github.com/GluuFederation/ecommerce-python/issues/35

--- oxd-common/src/main/java/org/xdi/oxd/common/params/IntrospectRptParams.java
+++ oxd-common/src/main/java/org/xdi/oxd/common/params/IntrospectRptParams.java
@@ -7,12 +7,14 @@ import org.codehaus.jackson.annotate.JsonProperty;
  * @author yuriyz
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class IntrospectRptParams implements HasOxdIdParams {
+public class IntrospectRptParams implements HasProtectionAccessTokenParams {
 
     @JsonProperty(value = "oxd_id")
     private String oxdId;
     @JsonProperty(value = "rpt")
     private String rpt;
+    @JsonProperty(value = "protection_access_token")
+    private String protectionAccessToken;
 
     @Override
     public String getOxdId() {
@@ -29,5 +31,14 @@ public class IntrospectRptParams implements HasOxdIdParams {
 
     public void setRpt(String rpt) {
         this.rpt = rpt;
+    }
+
+    @Override
+    public String getProtectionAccessToken() {
+        return protectionAccessToken;
+    }
+
+    public void setProtectionAccessToken(String protectionAccessToken) {
+        this.protectionAccessToken = protectionAccessToken;
     }
 }
