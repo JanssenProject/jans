70fbce1b371a3d16df3ecae34a7e604bfd543e43
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-06-19 18:07:00 +0300

    fixed start-up scripts

--- oxd-server/src/main/bin/oxd-start.bat
+++ oxd-server/src/main/bin/oxd-start.bat
@@ -2,4 +2,4 @@
 set LIB=../lib
 set CONF=../conf/oxd-server.yml
 echo CONF=%CONF%
-start /b javaw -jar %LIB%/oxd-server.jar org.xdi.oxd.server.OxdServerApplication %CONF%
+start /b javaw -jar %LIB%/oxd-server.jar server %CONF%
