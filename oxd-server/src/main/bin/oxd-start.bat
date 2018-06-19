9f13964c77ffd2bdd03d8951249a95ecf783d75c
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-06-19 17:30:05 +0300

    correct oxd-server scripts

--- oxd-server/src/main/bin/oxd-start.bat
+++ oxd-server/src/main/bin/oxd-start.bat
@@ -1,5 +1,5 @@
 "cd /d %~dp0"
 set LIB=../lib
-set CONF=../conf/oxd-conf.json
+set CONF=../conf/oxd-server.yml
 echo CONF=%CONF%
-start /b javaw -Doxd.server.config=%CONF% -cp %LIB%/bcprov-jdk15on-1.54.jar;%LIB%/oxd-server-jar-with-dependencies.jar org.xdi.oxd.server.ServerLauncher
+start /b javaw -jar %LIB%/oxd-server.jar org.xdi.oxd.server.OxdServerApplication %CONF%
