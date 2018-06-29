4ffea40393a299bc4f88e74fc3b0fa633dc93266
yurem <Yuriy.Movchan@gmail.com>
2018-06-29 18:35:09 +0300

    Add dynamic persistence selection mechanism support

--- Server/src/main/java/org/xdi/oxauth/service/KeyGeneratorTimer.java
+++ Server/src/main/java/org/xdi/oxauth/service/KeyGeneratorTimer.java
@@ -100,7 +100,7 @@ public class KeyGeneratorTimer {
 	}
 
 	public String updateKeys() throws JSONException, Exception {
-		String dn = configurationFactory.getLdapConfiguration().getString("configurationEntryDN");
+		String dn = configurationFactory.getPersistenceConfiguration().getConfiguration().getString("configurationEntryDN");
 		Conf conf = ldapEntryManager.find(Conf.class, dn);
 
 		JSONObject jwks = new JSONObject(conf.getWebKeys());
