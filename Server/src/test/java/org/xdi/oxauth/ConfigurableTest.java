c643d9f71008de3d29d6aebe6a4d2b9667208f03
yurem <Yuriy.Movchan@gmail.com>
2018-07-12 11:35:29 +0300

    Add message about init test suite invocation

--- Server/src/test/java/org/xdi/oxauth/ConfigurableTest.java
+++ Server/src/test/java/org/xdi/oxauth/ConfigurableTest.java
@@ -44,7 +44,8 @@ public abstract class ConfigurableTest extends Arquillian {
 	@BeforeSuite
 	public void initTestSuite(ITestContext context) throws FileNotFoundException, IOException {
         Reporter.log("Invoked init test suite method", true);
-		String propertiesFile = context.getCurrentXmlTest().getParameter("propertiesFile");
+
+        String propertiesFile = context.getCurrentXmlTest().getParameter("propertiesFile");
 		if (StringHelper.isEmpty(propertiesFile)) {
 			propertiesFile = "target/test-classes/testng.properties";
 		}
