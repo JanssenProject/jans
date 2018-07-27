00aafdd8af07cd9323d83646acd1365e0186fe44
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-07-27 13:27:29 +0300

    #211 : fixed bug when introspection commands run successfully without protection_access_token.

--- oxd-common/src/main/java/org/xdi/oxd/common/params/GetAccessTokenByRefreshTokenParams.java
+++ oxd-common/src/main/java/org/xdi/oxd/common/params/GetAccessTokenByRefreshTokenParams.java
@@ -9,7 +9,7 @@ import java.util.List;
  * @author yuriyz
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class GetAccessTokenByRefreshTokenParams implements HasOxdIdParams {
+public class GetAccessTokenByRefreshTokenParams implements HasProtectionAccessTokenParams {
 
     @JsonProperty(value = "oxd_id")
     private String oxdId;
@@ -17,6 +17,17 @@ public class GetAccessTokenByRefreshTokenParams implements HasOxdIdParams {
     private String refreshToken;
     @JsonProperty(value = "scope")
     private List<String> scope;
+    @JsonProperty(value = "protection_access_token")
+    private String protectionAccessToken;
+
+    @Override
+    public String getProtectionAccessToken() {
+        return protectionAccessToken;
+    }
+
+    public void setProtectionAccessToken(String protectionAccessToken) {
+        this.protectionAccessToken = protectionAccessToken;
+    }
 
     public String getOxdId() {
         return oxdId;
