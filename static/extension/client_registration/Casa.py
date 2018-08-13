6bf48a6f63176090048ff4e7b18412a097cb910f
yurem <Yuriy.Movchan@gmail.com>
2018-08-13 12:12:33 +0300

    Supply more external methods for client operations oxAuth#787

--- static/extension/client_registration/Casa.py
+++ static/extension/client_registration/Casa.py
@@ -33,8 +33,8 @@ class ClientRegistration(ClientRegistrationType):
     #   registerRequest is org.xdi.oxauth.client.RegisterRequest
     #   client is org.xdi.oxauth.model.registration.Client
     #   configurationAttributes is java.util.Map<String, SimpleCustomProperty>
-    def updateClient(self, registerRequest, client, configurationAttributes):
-        print "Casa client registration. UpdateClient method"
+    def createClient(self, registerRequest, client, configurationAttributes):
+        print "Casa client registration. CreateClient method"
 
         redirectUris = client.getRedirectUris()
         print "Casa client registration. Redirect Uris: %s" % redirectUris
@@ -72,8 +72,17 @@ class ClientRegistration(ClientRegistrationType):
 
         return True
 
+    # Update client entry before persistent it
+    #   registerRequest is org.xdi.oxauth.client.RegisterRequest
+    #   client is org.xdi.oxauth.model.registration.Client
+    #   configurationAttributes is java.util.Map<String, SimpleCustomProperty>
+    def updateClient(self, registerRequest, client, configurationAttributes):
+        print "Casa client registration. UpdateClient method"
+
+        return True
+
     def getApiVersion(self):
-        return 1
+        return 2
 
     def prepareClientRedirectUris(self, configurationAttributes):
         clientRedirectUrisSet = HashSet()
