d289b5c2b3e8fca69a7a23c10b6b0f2499da1420
Jose G <bonustrack310@gmail.com>
2018-08-15 16:59:18 -0500

    Remove lifetime extend from script, only do scopes adjustments

--- static/extension/client_registration/Casa.py
+++ static/extension/client_registration/Casa.py
@@ -1,8 +1,7 @@
 # oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
 # Copyright (c) 2018, Gluu
 #
-# Author: Yuriy Movchan
-#
+# Author: Jose Gonzalez
 
 from org.xdi.model.custom.script.type.client import ClientRegistrationType
 from org.xdi.service.cdi.util import CdiUtil
@@ -18,9 +17,7 @@ class ClientRegistration(ClientRegistrationType):
 
     def init(self, configurationAttributes):
         print "Casa client registration. Initialization"
-        
         self.clientRedirectUrisSet = self.prepareClientRedirectUris(configurationAttributes)
-
         print "Casa client registration. Initialized successfully"
         return True   
 
@@ -34,41 +31,22 @@ class ClientRegistration(ClientRegistrationType):
     #   client is org.xdi.oxauth.model.registration.Client
     #   configurationAttributes is java.util.Map<String, SimpleCustomProperty>
     def createClient(self, registerRequest, client, configurationAttributes):
-        print "Casa client registration. CreateClient method"
 
+        print "Casa client registration. CreateClient method"
         redirectUris = client.getRedirectUris()
         print "Casa client registration. Redirect Uris: %s" % redirectUris
 
         credManagerClient = False
         for redirectUri in redirectUris:
-            if (self.clientRedirectUrisSet.contains(redirectUri)):
+            if self.clientRedirectUrisSet.contains(redirectUri):
                 credManagerClient = True
                 break
         
         if not credManagerClient:
             return True
 
-        print "Casa client registration. Client is Cred-manager"
-
-        newScopes = client.getScopes()
-        
-        scopeService = CdiUtil.bean(ScopeService)
-
-        requiredScopes = ["openid", "profile", "user_name", "clientinfo", "uma_protection"]
-        for scopeName in requiredScopes:
-            scope = scopeService.getScopeByDisplayName(scopeName)
-            if not scope.getIsDefault():
-                print "Casa client registration. Adding scope '%s'" % scopeName
-                newScopes = ArrayHelper.addItemToStringArray(newScopes, scope.getDn())
-
-        print "Casa client registration. Result scopes: %s" % newScopes
-        client.setScopes(newScopes)
-
-        #Extend client lifetime for one year
-        cal=GregorianCalendar()
-        cal.add(1,1)
-        client.setClientSecretExpiresAt(Date(cal.getTimeInMillis()))
-        #this style complains:  client.setClientSecretExpiresAt(Date(Date().getTime + 31536000000))
+        print "Casa client registration. Client is Gluu Casa"
+        self.setClientScopes(client, configurationAttributes.get("scopes"))
 
         return True
 
@@ -79,11 +57,39 @@ class ClientRegistration(ClientRegistrationType):
     def updateClient(self, registerRequest, client, configurationAttributes):
         print "Casa client registration. UpdateClient method"
 
+        #Extend client lifetime for one year
+        cal=GregorianCalendar()
+        cal.add(1,1)
+        client.setClientSecretExpiresAt(Date(cal.getTimeInMillis()))
+        #this style complains:  client.setClientSecretExpiresAt(Date(Date().getTime + 31536000000))
+        
+        self.setClientScopes(client, configurationAttributes.get("scopes"))
+        
         return True
 
     def getApiVersion(self):
         return 2
 
+    def setClientScopes(self, client, requiredScopes):
+        
+        if requiredScopes == None:
+            print "Casa client registration. No list of scopes was passed in script parameters"
+            return
+        
+        requiredScopes = StringHelper.split(requiredScopes.getValue2(), ",")
+        newScopes = client.getScopes()
+        scopeService = CdiUtil.bean(ScopeService)
+        
+        for scopeName in requiredScopes:
+            scope = scopeService.getScopeByDisplayName(scopeName)
+            if not scope.getIsDefault():
+                print "Casa client registration. Adding scope '%s'" % scopeName
+                newScopes = ArrayHelper.addItemToStringArray(newScopes, scope.getDn())
+
+        print "Casa client registration. Result scopes are: %s" % newScopes
+        client.setScopes(newScopes)
+        
+        
     def prepareClientRedirectUris(self, configurationAttributes):
         clientRedirectUrisSet = HashSet()
         if not configurationAttributes.containsKey("client_redirect_uris"):
