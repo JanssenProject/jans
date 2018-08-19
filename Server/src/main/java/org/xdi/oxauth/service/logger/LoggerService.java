d6994428e014095ca1b39700b6f6d630fc53f420
yurem <Yuriy.Movchan@gmail.com>
2018-08-19 15:04:39 +0300

    Log statements of level lower than INFO not shown after start oxTrust

--- Server/src/main/java/org/xdi/oxauth/service/logger/LoggerService.java
+++ Server/src/main/java/org/xdi/oxauth/service/logger/LoggerService.java
@@ -1,78 +1,37 @@
 package org.xdi.oxauth.service.logger;
 
-import java.io.File;
-import java.util.logging.LogManager;
-
-import javax.ejb.Stateless;
-import javax.enterprise.event.Event;
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.slf4j.Logger;
 import org.xdi.oxauth.model.configuration.AppConfiguration;
 import org.xdi.oxauth.util.ServerUtil;
-import org.xdi.service.cdi.event.ConfigurationUpdate;
 
 /**
- * Created by eugeniuparvan on 8/3/17.
+ * Logger service
+ *
+ * @author Yuriy Movchan Date: 08/19/2018
  */
-@Stateless
+@ApplicationScoped
 @Named
-public class LoggerService {
-
-    @Inject
-    private Logger log;
-
-    @Inject
-    private Event<AppConfiguration> configurationUpdateEvent;
+public class LoggerService extends org.xdi.service.logger.LoggerService {
 
     @Inject
     private AppConfiguration appConfiguration;
 
-    /**
-     * First trying to set external logger config from GluuAppliance.
-     * If there is no valid external path to log4j2.xml location then set default configuration.
-     */
-    public void updateLoggerConfigLocation() {
-        if (setExternalLoggerConfig())
-            return;
-        LoggerContext loggerContext = LoggerContext.getContext(false);
-        loggerContext.setConfigLocation(null);
-        loggerContext.reconfigure();
+    @Override
+    public boolean isDisableJdkLogger() {
+        return ServerUtil.isTrue(appConfiguration.getDisableJdkLogger());
     }
 
-    public void disableJdkLogger() {
-        LogManager.getLogManager().reset();
-        java.util.logging.Logger globalLogger = java.util.logging.Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
-        if (globalLogger != null) {
-            globalLogger.setLevel(java.util.logging.Level.OFF);
-        }
+    @Override
+    public String getLoggingLevel() {
+        return appConfiguration.getLoggingLevel();
     }
 
-    private boolean setExternalLoggerConfig() {
-        log.info("External log configuration: " + appConfiguration.getExternalLoggerConfiguration());
-        if (StringUtils.isEmpty(appConfiguration.getExternalLoggerConfiguration())) {
-            return false;
-        }
-        File log4jFile = new File(appConfiguration.getExternalLoggerConfiguration());
-        if (!log4jFile.exists()) {
-            log.info("External log configuration does not exist.");
-            return false;
-        }
-        LoggerContext loggerContext = LoggerContext.getContext(false);
-        loggerContext.setConfigLocation(log4jFile.toURI());
-        loggerContext.reconfigure();
-
-        configurationUpdateEvent.select(ConfigurationUpdate.Literal.INSTANCE).fire(this.appConfiguration);
-        return true;
+    @Override
+    public String getExternalLoggerConfiguration() {
+        return appConfiguration.getExternalLoggerConfiguration();
     }
 
-    public void configure() {
-        if (ServerUtil.isTrue(appConfiguration.getDisableJdkLogger())) {
-            disableJdkLogger();
-        }
-        updateLoggerConfigLocation();
-    }
 }
