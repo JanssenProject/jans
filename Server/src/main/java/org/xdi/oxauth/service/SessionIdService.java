a384509a1414a60a3bdff16e66afc4762952a44c
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2018-09-11 16:28:47 +0300

    Fix "Faces context returns null for http request object"

--- Server/src/main/java/org/xdi/oxauth/service/SessionIdService.java
+++ Server/src/main/java/org/xdi/oxauth/service/SessionIdService.java
@@ -18,7 +18,7 @@ import java.util.Map.Entry;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-import javax.ejb.Stateless;
+import javax.enterprise.context.RequestScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
@@ -63,7 +63,7 @@ import com.unboundid.ldap.sdk.ResultCode;
  * @author Javier Rojas Blum
  * @version August 9, 2017
  */
-@Stateless
+@RequestScoped
 @Named
 public class SessionIdService {
 
