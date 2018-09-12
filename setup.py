9f8dc4589df0f7208d8588dcc6d974a8dca81c27
aliaksander-samuseu <aliaksander-samuseu@users.noreply.github.com>
2018-09-12 18:09:13 +0300

    Update setup.py
    Adding lines binding OpenDJ's listeners to loopback interface only.

--- setup.py
+++ setup.py
@@ -2807,6 +2807,8 @@ class Setup(object):
                           ['set-log-publisher-prop', '--publisher-name', '"File-Based Audit Logger"', '--set', 'enabled:true'],
                           ['create-backend', '--backend-name', 'site', '--set', 'base-dn:o=site', '--type %s' % self.ldap_backend_type, '--set', 'enabled:true'],
                           ['set-connection-handler-prop', '--handler-name', '"LDAP Connection Handler"', '--set', 'enabled:false'],
+                          ['set-connection-handler-prop', '--handler-name', '"LDAPS Connection Handler"', '--set', 'enabled:true', '--set', 'listen-address:127.0.0.1'],
+                          ['set-administration-connector-prop', '--set', 'listen-address:127.0.0.1'],
                           ['set-access-control-handler-prop', '--remove', '%s' % opendj_prop_name],
                           ['set-global-configuration-prop', '--set', 'reject-unauthenticated-requests:true'],
                           ['set-password-policy-prop', '--policy-name', '"Default Password Policy"', '--set', 'default-password-storage-scheme:"Salted SHA-512"'],
