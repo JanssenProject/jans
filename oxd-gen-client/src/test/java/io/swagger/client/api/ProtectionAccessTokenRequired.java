fe648cadc24cb54d27852923bf0d50cca9143d9f
Shoeb Khan <shoebkhan09@gmail.com>
2018-09-19 10:22:38 +0530

    #233: Renamed annotation class

--- oxd-gen-client/src/test/java/io/swagger/client/api/ProtectionAccessTokenRequired.java
+++ oxd-gen-client/src/test/java/io/swagger/client/api/ProtectionAccessTokenRequired.java
@@ -12,5 +12,5 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface EnabledProtectionAccessToken {
+public @interface ProtectionAccessTokenRequired {
 }
