5f91c87474b3f73a6e3b01e1924dfeb066d84f44
Shoeb Khan <shoebkhan09@gmail.com>
2018-09-29 14:18:16 +0530

    #233: Swagger test class for Authorization URL

---
+++ oxd-gen-client/src/test/java/io/swagger/client/api/GetAuthorizationUrlTest.java
@@ -0,0 +1,25 @@
+package io.swagger.client.api;
+
+import io.swagger.client.model.GetAuthorizationUrlParams;
+import io.swagger.client.model.GetAuthorizationUrlResponse;
+import io.swagger.client.model.RegisterSiteResponseData;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertNotNull;
+
+public class GetAuthorizationUrlTest {
+    @Parameters({"redirectUrl", "opHost"})
+    @Test
+    public void test(String redirectUrl, String opHost) throws Exception {
+        DevelopersApi api = Tester.api();
+
+        final RegisterSiteResponseData site = RegisterSiteTest.registerSite(api, opHost, redirectUrl);
+        final GetAuthorizationUrlParams commandParams = new GetAuthorizationUrlParams();
+        commandParams.setOxdId(site.getOxdId());
+
+        final GetAuthorizationUrlResponse resp = api.getAuthorizationUrl(Tester.getAuthorization(), commandParams);
+        assertNotNull(resp);
+        Tester.notEmpty(resp.getData().getAuthorizationUrl());
+    }
+}
