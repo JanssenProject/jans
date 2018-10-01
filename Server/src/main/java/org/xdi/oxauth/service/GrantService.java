6dc3a19c8633827aa6672812f8613e25403bdf66
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2018-10-01 22:11:27 +0300

    Fix compilation issues after merge

--- Server/src/main/java/org/xdi/oxauth/service/GrantService.java
+++ Server/src/main/java/org/xdi/oxauth/service/GrantService.java
@@ -24,6 +24,7 @@ import javax.inject.Named;
 import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.lang.StringUtils;
 import org.gluu.persist.PersistenceEntryManager;
+import org.gluu.persist.exception.EntryPersistenceException;
 import org.gluu.persist.model.BatchOperation;
 import org.gluu.persist.model.ProcessBatchOperation;
 import org.gluu.persist.model.SearchScope;
