b35fa1fe7070e796c1c95564da16e289049b6804
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2018-10-02 09:28:29 +0300

    Sync with 3.1.4

--- setup.py
+++ setup.py
@@ -3176,7 +3176,7 @@ class Setup(object):
                     self.run([service_path, 'rsyslog', 'restart'])
                     self.run([service_path, 'solserver', 'start'])
             elif self.ldap_type == 'opendj':
-                self.run([service_path, 'opendj', 'restart'])
+                self.run_service_command('opendj', 'restart')
 
         # Jetty services
         # Iterate through all components and start installed
