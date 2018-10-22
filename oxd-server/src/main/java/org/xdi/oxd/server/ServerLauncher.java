1beadbf706489ada558721e7bdb49982e4348746
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-10-22 14:48:11 +0300

    more logs

--- oxd-server/src/main/java/org/xdi/oxd/server/ServerLauncher.java
+++ oxd-server/src/main/java/org/xdi/oxd/server/ServerLauncher.java
@@ -147,8 +147,10 @@ public class ServerLauncher {
                         hasBC = true;
                     }
                 }
+                LOG.debug("BC registered: " + hasBC);
                 if (!hasBC) {
                     Security.addProvider(new BouncyCastleProvider());
+                    LOG.debug("Registered BC successfully.");
                 }
             }
         } catch (Exception e) {
