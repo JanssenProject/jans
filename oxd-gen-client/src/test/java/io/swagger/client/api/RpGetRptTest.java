62a0caf94bbdbbab920e8907e869d6ace948f846
Shoeb Khan <shoebkhan09@gmail.com>
2018-11-08 15:35:06 +0530

    #233: Added RpGetRpt swagger test class

---
+++ oxd-gen-client/src/test/java/io/swagger/client/api/RpGetRptTest.java
@@ -0,0 +1,80 @@
+package io.swagger.client.api;
+
+import io.swagger.client.model.RegisterSiteResponse;
+import io.swagger.client.model.UmaRpGetRptParams;
+import io.swagger.client.model.UmaRpGetRptResponse;
+import io.swagger.client.model.UmaRsCheckAccessResponse;
+import org.apache.commons.lang.StringUtils;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static io.swagger.client.api.Tester.api;
+import static org.apache.commons.lang.StringUtils.isNotBlank;
+import static org.testng.Assert.*;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ * @author Shoeb
+ *
+ * @version 11/03/2018
+ */
+
+public class RpGetRptTest {
+
+    @Parameters({"opHost", "redirectUrl", "rsProtect"})
+    @Test
+    public void test(String opHost, String redirectUrl, String rsProtect) throws Exception {
+
+        final DevelopersApi client = api();
+
+        final RegisterSiteResponse site = RegisterSiteTest.registerSite(client, opHost, redirectUrl);
+        final UmaRpGetRptResponse response = requestRpt(client, site, rsProtect);
+
+        assertNotNull(response);
+
+    }
+
+
+    @Parameters({"opHost", "redirectUrl", "rsProtect"})
+    @Test
+    public void testWithSameRpt(String opHost, String redirectUrl, String rsProtect) throws Exception {
+
+        final DevelopersApi client = api();
+
+        final RegisterSiteResponse site = RegisterSiteTest.registerSite(client, opHost, redirectUrl);
+        final UmaRpGetRptResponse firstResponse = requestRpt(client, site, rsProtect);
+
+        final UmaRsCheckAccessResponse checkAccess = RsCheckAccessTest.checkAccess(client, site);
+
+        final UmaRpGetRptParams params = new UmaRpGetRptParams();
+        params.setOxdId(site.getOxdId());
+        params.setTicket(checkAccess.getTicket());
+        params.setRpt(firstResponse.getAccessToken());
+
+        final UmaRpGetRptResponse secondResponse = client.umaRpGetRpt(Tester.getAuthorization(), params);
+
+        assertNotNull(secondResponse);
+        assertEquals(secondResponse.getAccessToken(), firstResponse.getAccessToken());
+        assertFalse(firstResponse.isUpdated());
+        assertTrue(secondResponse.isUpdated());
+
+    }
+
+
+    private static UmaRpGetRptResponse requestRpt(DevelopersApi client, RegisterSiteResponse site, String rsProtect) throws Exception {
+        RsProtectTest.protectResources(client, site, UmaFullTest.resourceList(rsProtect));
+
+        final UmaRsCheckAccessResponse checkAccess = RsCheckAccessTest.checkAccess(client, site);
+
+        final UmaRpGetRptParams params = new UmaRpGetRptParams();
+        params.setOxdId(site.getOxdId());
+        params.setTicket(checkAccess.getTicket());
+
+        final UmaRpGetRptResponse response = client.umaRpGetRpt(Tester.getAuthorization(), params);
+
+        assertNotNull(response);
+        assertTrue(isNotBlank(response.getAccessToken()));
+        assertTrue(isNotBlank(response.getPct()));
+        return response;
+    }
+}
