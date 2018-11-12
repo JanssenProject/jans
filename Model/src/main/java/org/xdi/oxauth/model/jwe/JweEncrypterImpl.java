3e6262266a4fb69295d867d9aef614e0968891dc
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2018-11-12 08:51:37 +0300

    JWE fixes

--- Model/src/main/java/org/xdi/oxauth/model/jwe/JweEncrypterImpl.java
+++ Model/src/main/java/org/xdi/oxauth/model/jwe/JweEncrypterImpl.java
@@ -6,6 +6,10 @@
 
 package org.xdi.oxauth.model.jwe;
 
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.RSAEncrypter;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.RSAKey;
 import org.bouncycastle.crypto.BlockCipher;
 import org.bouncycastle.crypto.CipherParameters;
 import org.bouncycastle.crypto.InvalidCipherTextException;
@@ -17,17 +21,22 @@ import org.bouncycastle.crypto.params.KeyParameter;
 import org.xdi.oxauth.model.crypto.encryption.BlockEncryptionAlgorithm;
 import org.xdi.oxauth.model.crypto.encryption.KeyEncryptionAlgorithm;
 import org.xdi.oxauth.model.exception.InvalidJweException;
+import org.xdi.oxauth.model.exception.InvalidJwtException;
 import org.xdi.oxauth.model.exception.InvalidParameterException;
+import org.xdi.oxauth.model.jwt.JwtHeader;
 import org.xdi.oxauth.model.util.Base64Util;
 import org.xdi.oxauth.model.util.Pair;
 import org.xdi.oxauth.model.util.Util;
 
 import javax.crypto.*;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.security.*;
+import java.security.interfaces.RSAPublicKey;
+import java.text.ParseException;
 import java.util.Arrays;
 
 /**
@@ -49,6 +58,46 @@ public class JweEncrypterImpl extends AbstractJweEncrypter {
     public JweEncrypterImpl(KeyEncryptionAlgorithm keyEncryptionAlgorithm, BlockEncryptionAlgorithm blockEncryptionAlgorithm, PublicKey publicKey) {
         super(keyEncryptionAlgorithm, blockEncryptionAlgorithm);
         this.publicKey = publicKey;
+    }
+
+    @Override
+    public Jwe encrypt(Jwe jwe) throws InvalidJweException {
+        try {
+            RSAKey rsaKey = new RSAKey.Builder((RSAPublicKey) publicKey).build();
+            RSAEncrypter encrypter = new RSAEncrypter(rsaKey);
+
+            JWEObject jweObject = new JWEObject(
+                    JWEHeader.parse(jwe.getHeader().toJsonObject().toString()),
+                    new Payload(jwe.toString()));
+
+            jweObject.encrypt(encrypter);
+            String encryptedJwe = jweObject.serialize();
+
+            String[] jweParts = encryptedJwe.split("\\.");
+            if (jweParts.length != 5) {
+                throw new InvalidJwtException("Invalid JWS format.");
+            }
+
+            String encodedHeader = jweParts[0];
+            String encodedEncryptedKey = jweParts[1];
+            String encodedInitializationVector = jweParts[2];
+            String encodedCipherText = jweParts[3];
+            String encodedIntegrityValue = jweParts[4];
+
+            jwe.setEncodedHeader(encodedHeader);
+            jwe.setEncodedEncryptedKey(encodedEncryptedKey);
+            jwe.setEncodedInitializationVector(encodedInitializationVector);
+            jwe.setEncodedCiphertext(encodedCipherText);
+            jwe.setEncodedIntegrityValue(encodedIntegrityValue);
+
+            return jwe;
+        } catch (JOSEException e) {
+            throw new InvalidJweException(e);
+        } catch (InvalidJwtException e) {
+            throw new InvalidJweException(e);
+        } catch (ParseException e) {
+            throw new InvalidJweException(e);
+        }
     }
 
     @Override
@@ -136,6 +185,62 @@ public class JweEncrypterImpl extends AbstractJweEncrypter {
         try {
             if (getBlockEncryptionAlgorithm() == BlockEncryptionAlgorithm.A128GCM
                     || getBlockEncryptionAlgorithm() == BlockEncryptionAlgorithm.A256GCM) {
+                /*final SecretKey aesKey = new SecretKeySpec(contentMasterKey, "AES");
+
+                Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+
+                GCMParameterSpec gcmSpec = new GCMParameterSpec(128, initializationVector);
+                cipher.init(Cipher.ENCRYPT_MODE, aesKey, gcmSpec);
+
+                cipher.updateAAD(additionalAuthenticatedData);
+
+                byte[] cipherOutput = cipher.doFinal(plainText);
+
+                final int tagPos = cipherOutput.length - 16;
+
+                byte[] cipherText = subArray(cipherOutput, 0, tagPos);
+                byte[] authenticationTag = subArray(cipherOutput, tagPos, 16);
+
+                String encodedCipherText = Base64Util.base64urlencode(cipherText);
+                String encodedAuthenticationTag = Base64Util.base64urlencode(authenticationTag);
+
+                AlgorithmParameters algorithmParameters = cipher.getParameters();
+                GCMParameterSpec actualParams = algorithmParameters.getParameterSpec(GCMParameterSpec.class);
+                byte[] iv = actualParams.getIV();
+                int tLen = actualParams.getTLen();
+                initializationVector = iv;
+
+                return new Pair<String, String>(encodedCipherText, encodedAuthenticationTag);*/
+
+                /*SecureRandom secureRandom = new SecureRandom();
+                byte[] key = new byte[16];
+                secureRandom.nextBytes(key);
+                SecretKey secretKey = new SecretKeySpec(key, "AES");
+
+                byte[] iv = new byte[12];
+                secureRandom.nextBytes(iv);
+
+                final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+                GCMParameterSpec parameterSpec = new GCMParameterSpec(128, iv);
+                cipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec);
+
+                if (additionalAuthenticatedData != null) {
+                    cipher.updateAAD(additionalAuthenticatedData);
+                }
+
+                byte[] cipherOutput = cipher.doFinal(plainText);
+
+                final int tagPos = cipherOutput.length - 128 / 8;
+
+                byte[] cipherText = subArray(cipherOutput, 0, tagPos);
+                byte[] authenticationTag = subArray(cipherOutput, tagPos, 128 / 8);
+
+                String encodedCipherText = Base64Util.base64urlencode(cipherText);
+                String encodedAuthenticationTag = Base64Util.base64urlencode(authenticationTag);
+
+                return new Pair<String, String>(encodedCipherText, encodedAuthenticationTag);*/
+
+
                 SecretKey secretKey = new SecretKeySpec(contentMasterKey, "AES");
                 KeyParameter key = new KeyParameter(contentMasterKey);
                 final int MAC_SIZE_BITS = 128;
@@ -208,6 +313,15 @@ public class JweEncrypterImpl extends AbstractJweEncrypter {
             throw new InvalidJweException(e);
         } catch (InvalidParameterException e) {
             throw new InvalidJweException(e);
-        }
+        } /*catch (Exception e) {
+            throw new InvalidJweException(e);
+        }*/
     }
+
+    /*private byte[] subArray(byte[] byteArray, int beginIndex, int length) {
+
+        byte[] subArray = new byte[length];
+        System.arraycopy(byteArray, beginIndex, subArray, 0, subArray.length);
+        return subArray;
+    }*/
 }
