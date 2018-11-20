42072c250c4c0ac03ef8976ca508be73c70bd6f1
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2018-11-20 01:07:01 -0400

    Fix tests

--- Model/src/main/java/org/xdi/oxauth/model/jwe/AbstractJweEncrypter.java
+++ Model/src/main/java/org/xdi/oxauth/model/jwe/AbstractJweEncrypter.java
@@ -8,18 +8,10 @@ package org.xdi.oxauth.model.jwe;
 
 import org.xdi.oxauth.model.crypto.encryption.BlockEncryptionAlgorithm;
 import org.xdi.oxauth.model.crypto.encryption.KeyEncryptionAlgorithm;
-import org.xdi.oxauth.model.exception.InvalidJweException;
-import org.xdi.oxauth.model.exception.InvalidJwtException;
-import org.xdi.oxauth.model.util.Base64Util;
-import org.xdi.oxauth.model.util.Pair;
-import org.xdi.oxauth.model.util.Util;
-
-import java.io.UnsupportedEncodingException;
-import java.security.SecureRandom;
 
 /**
  * @author Javier Rojas Blum
- * @version July 31, 2016
+ * @version November 20, 2018
  */
 public abstract class AbstractJweEncrypter implements JweEncrypter {
 
@@ -38,41 +30,4 @@ public abstract class AbstractJweEncrypter implements JweEncrypter {
     public BlockEncryptionAlgorithm getBlockEncryptionAlgorithm() {
         return blockEncryptionAlgorithm;
     }
-
-    /*@Override
-    public Jwe encrypt(Jwe jwe) throws InvalidJweException {
-        try {
-            jwe.setEncodedHeader(jwe.getHeader().toBase64JsonObject());
-
-            byte[] contentMasterKey = new byte[blockEncryptionAlgorithm.getCmkLength() / 8];
-            SecureRandom random = new SecureRandom();
-            random.nextBytes(contentMasterKey);
-
-            String encodedEncryptedKey = generateEncryptedKey(contentMasterKey);
-            jwe.setEncodedEncryptedKey(encodedEncryptedKey);
-
-            byte[] initializationVector = new byte[blockEncryptionAlgorithm.getInitVectorLength() / 8];
-            random.nextBytes(initializationVector);
-            String encodedInitializationVector = Base64Util.base64urlencode(initializationVector);
-            jwe.setEncodedInitializationVector(encodedInitializationVector);
-
-            Pair<String, String> result = generateCipherTextAndIntegrityValue(contentMasterKey, initializationVector,
-                    jwe.getAdditionalAuthenticatedData().getBytes(Util.UTF8_STRING_ENCODING),
-                    jwe.getClaims().toBase64JsonObject().getBytes(Util.UTF8_STRING_ENCODING));
-            jwe.setEncodedCiphertext(result.getFirst());
-            jwe.setEncodedIntegrityValue(result.getSecond());
-
-            return jwe;
-        } catch (InvalidJwtException e) {
-            throw new InvalidJweException(e);
-        } catch (UnsupportedEncodingException e) {
-            throw new InvalidJweException(e);
-        }
-    }*/
-
-    public abstract String generateEncryptedKey(byte[] contentMasterKey) throws InvalidJweException;
-
-    public abstract Pair<String, String> generateCipherTextAndIntegrityValue(
-            byte[] contentMasterKey, byte[] initializationVector, byte[] additionalAuthenticatedData, byte[] plainText)
-            throws InvalidJweException;
 }
