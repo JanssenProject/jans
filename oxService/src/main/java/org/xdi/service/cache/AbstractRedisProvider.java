930776dcd7711b1f43e4b7b23d79b1f9f2d1baf7
Gasmyr <thomas@gluu.org>
2018-11-29 20:03:36 +0100

    Decrypt redisConfiguration password before using it for authentication #95

--- oxService/src/main/java/org/xdi/service/cache/AbstractRedisProvider.java
+++ oxService/src/main/java/org/xdi/service/cache/AbstractRedisProvider.java
@@ -1,50 +1,66 @@
 package org.xdi.service.cache;
 
+import javax.inject.Inject;
+
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xdi.util.security.StringEncrypter;
+import org.xdi.util.security.StringEncrypter.EncryptionException;
+
 import redis.clients.jedis.Jedis;
 
 /**
  * @author yuriyz
  */
 public abstract class AbstractRedisProvider {
+	private final static Logger LOG = LoggerFactory.getLogger(AbstractRedisProvider.class);
 
-    protected RedisConfiguration redisConfiguration;
+	protected RedisConfiguration redisConfiguration;
 
-    public AbstractRedisProvider(RedisConfiguration redisConfiguration) {
-        this.redisConfiguration = redisConfiguration;
-    }
+	@Inject
+	private StringEncrypter stringEncrypter;
 
-    public RedisConfiguration getRedisConfiguration() {
-        return redisConfiguration;
-    }
+	public AbstractRedisProvider(RedisConfiguration redisConfiguration) {
+		this.redisConfiguration = redisConfiguration;
+	}
 
-    public void testConnection() {
-        put(2, "testKey", "testValue");
-        if (!"testValue".equals(get("testKey"))) {
-            throw new RuntimeException("Failed to connect to redis server. Configuration: " + redisConfiguration);
-        }
-    }
+	public RedisConfiguration getRedisConfiguration() {
+		return redisConfiguration;
+	}
 
-    public void setAuthIfNeeded(Jedis jedis) {
-        if (StringUtils.isNotBlank(redisConfiguration.getPassword())) {
-            jedis.auth(redisConfiguration.getPassword());
-        }
-    }
+	public void testConnection() {
+		put(2, "testKey", "testValue");
+		if (!"testValue".equals(get("testKey"))) {
+			throw new RuntimeException("Failed to connect to redis server. Configuration: " + redisConfiguration);
+		}
+	}
 
-    public abstract void create();
+	public void setAuthIfNeeded(Jedis jedis) {
+		String encryptedPassword = redisConfiguration.getPassword();
+		if (StringUtils.isNotBlank(encryptedPassword)) {
+			try {
+				jedis.auth(stringEncrypter.decrypt(encryptedPassword));
+			} catch (EncryptionException e) {
+				LOG.error("Error during redis password decryption", e);
+			}
+		}
+	}
 
-    public abstract void destroy();
+	public abstract void create();
 
-    public abstract Object getDelegate();
+	public abstract void destroy();
 
-    public abstract Object get(String key);
+	public abstract Object getDelegate();
 
-    public abstract void remove(String key);
+	public abstract Object get(String key);
 
-    public abstract void clear();
+	public abstract void remove(String key);
 
-    public abstract void put(int expirationInSeconds, String key, Object object);
+	public abstract void clear();
 
-    public abstract void put(String key, Object object);
+	public abstract void put(int expirationInSeconds, String key, Object object);
+
+	public abstract void put(String key, Object object);
 
 }
