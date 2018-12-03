d80b5a595c393f83f3f66758645ca70746d7d951
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2018-12-03 10:23:20 +0300

    Sync with 3.1.5

--- oxService/src/main/java/org/xdi/service/security/SecurityExtension.java
+++ oxService/src/main/java/org/xdi/service/security/SecurityExtension.java
@@ -57,7 +57,7 @@ public class SecurityExtension implements Extension {
         }
     }
 
-    private <T> void collectAnnotations(Class<T> annotationType, AnnotatedMethod method, List<T> values) {
+    private <T> void collectAnnotations(Class<T> annotationType, AnnotatedMethod<?> method, List<T> values) {
         for (Annotation annotation : method.getAnnotations()) {
             collectAnnotations(annotationType, annotation, values);
         }
@@ -86,68 +86,5 @@ public class SecurityExtension implements Extension {
             return values;
         }
     }
-=======
-	private Map<Method, InterceptSecure> interceptSecureForMethods = new HashMap<Method, InterceptSecure>();
-
-	public <X> void processAnnotatedType(@Observes ProcessAnnotatedType<X> pat) {
-		// Wrap this to override the annotations of the class
-		final AnnotatedType<X> at = pat.getAnnotatedType();
-		final AnnotatedTypeConfigurator<X> cat = pat.configureAnnotatedType();
-
-		// Collect Secure annotation from the type
-		List<Secure> typeSecureAnnotations = new ArrayList<Secure>();
-		for (Annotation annotation : pat.getAnnotatedType().getAnnotations()) {
-			collectAnnotations(Secure.class, annotation, typeSecureAnnotations);
-		}
-
-		// Collect the Secure annotations from the methods
-		for (AnnotatedMethodConfigurator<? super X> methodConfiguration : cat.methods()) {
-			AnnotatedMethod<?> method = methodConfiguration.getAnnotated();
-			final List<Secure> methodAnnotations = new ArrayList<Secure>(typeSecureAnnotations);
-
-			collectAnnotations(Secure.class, method, methodAnnotations);
-
-			// Store in the map if we find Secure annotations
-			if (methodAnnotations.size() > 0) {
-				InterceptSecure is = new InterceptSecureImpl(methodAnnotations.toArray(new Secure[methodAnnotations.size()]));
-
-				// Add InterceptSecure annotation
-				methodConfiguration.add(is);
-
-				interceptSecureForMethods.put(method.getJavaMember(), is);
-			}
-		}
-	}
-
-	private <T> void collectAnnotations(Class<T> annotationType, AnnotatedMethod<?> method, List<T> values) {
-		for (Annotation annotation : method.getAnnotations()) {
-			collectAnnotations(annotationType, annotation, values);
-		}
-	}
-
-	private <T> void collectAnnotations(Class<T> annotationType, Annotation annotation, List<T> values) {
-		if (annotationType.isAssignableFrom(annotation.annotationType())) {
-			values.add((T) annotation);
-		}
-	}
-
-	public InterceptSecure getInterceptSecure(Method method) {
-		return interceptSecureForMethods.get(method);
-	}
-
-	private static class InterceptSecureImpl extends AnnotationLiteral<InterceptSecure> implements InterceptSecure {
-
-		private final Secure[] values;
-
-		InterceptSecureImpl(Secure[] values) {
-			this.values = values;
-		}
-
-		@Override
-		public Secure[] value() {
-			return values;
-		}
-	}
->>>>>>> 846fcaa... Exception handling CDI extension
 
 }
