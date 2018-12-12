356b22be32b53c4d121755d4e07058f25e341a50
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-12-12 19:13:37 +0200

    #930 : fixed bug in register request
    https://github.com/GluuFederation/oxAuth/issues/930

--- Client/src/main/java/org/xdi/oxauth/client/RegisterRequest.java
+++ Client/src/main/java/org/xdi/oxauth/client/RegisterRequest.java
@@ -1304,6 +1304,7 @@ public class RegisterRequest extends BaseRequest {
                 requestObject.getInt(ACCESS_TOKEN_LIFETIME.toString()) : null);
         result.setDefaultMaxAge(requestObject.has(DEFAULT_MAX_AGE.toString()) ?
                 requestObject.getInt(DEFAULT_MAX_AGE.toString()) : null);
+        result.setRptAsJwt(requestObject.optBoolean(RPT_AS_JWT.toString()));
         result.setAccessTokenAsJwt(requestObject.optBoolean(ACCESS_TOKEN_AS_JWT.toString()));
         result.setAccessTokenSigningAlg(SignatureAlgorithm.fromString(requestObject.optString(ACCESS_TOKEN_SIGNING_ALG.toString())));
         result.setIdTokenSignedResponseAlg(requestObject.has(ID_TOKEN_SIGNED_RESPONSE_ALG.toString()) ?
