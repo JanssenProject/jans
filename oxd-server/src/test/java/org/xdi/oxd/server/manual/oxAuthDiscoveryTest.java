d3286be7448854090260e9c4a072d44b516c8ab4
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-12-12 19:24:40 +0200

    switched oxd 4.0.beta to oxauth 3.1.5-SNAPSHOT (from 3.1.4-Final)

--- oxd-server/src/test/java/org/xdi/oxd/server/manual/oxAuthDiscoveryTest.java
+++ oxd-server/src/test/java/org/xdi/oxd/server/manual/oxAuthDiscoveryTest.java
@@ -8,6 +8,8 @@ import org.testng.annotations.Test;
 import org.xdi.oxauth.client.OpenIdConfigurationClient;
 import org.xdi.oxauth.client.OpenIdConfigurationResponse;
 
+import java.io.IOException;
+
 /**
  * @author Yuriy Zabrovarnyy
  * @version 0.9, 09/08/2013
@@ -16,7 +18,7 @@ import org.xdi.oxauth.client.OpenIdConfigurationResponse;
 public class oxAuthDiscoveryTest {
 
     @Test
-    public void discoveryCallByOxAuthClient() {
+    public void discoveryCallByOxAuthClient() throws IOException {
         String url = "https://ce-dev.gluu.org/.well-known/openid-configuration";
         OpenIdConfigurationClient client = new OpenIdConfigurationClient(url);
         OpenIdConfigurationResponse response = client.execOpenIdConfiguration();
