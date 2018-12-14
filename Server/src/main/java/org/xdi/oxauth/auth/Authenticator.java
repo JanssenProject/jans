b18ea886d8aee24c9a5db9d0d0813b5d44754f2e
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2018-12-14 22:33:06 +0300

    On empty acr or step return expired session result

--- Server/src/main/java/org/xdi/oxauth/auth/Authenticator.java
+++ Server/src/main/java/org/xdi/oxauth/auth/Authenticator.java
@@ -278,7 +278,7 @@ public class Authenticator {
 			initCustomAuthenticatorVariables(sessionIdAttributes);
 			if ((this.authStep == null) || StringHelper.isEmpty(this.authAcr)) {
 				logger.error("Failed to determine authentication mode");
-				return Constants.RESULT_FAILURE;
+				return Constants.RESULT_EXPIRED;
 			}
 
 			CustomScriptConfiguration customScriptConfiguration = externalAuthenticationService
