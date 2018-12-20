e1148c9b4551f3723fdc86bbf9e1402c0c272d49
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-12-20 15:52:20 +0200

    #868 (4.0): added introspection interception scripts support
    https://github.com/GluuFederation/oxAuth/issues/868
    
    (cherry picked from commit f3b57c8)

--- Server/src/main/java/org/xdi/oxauth/introspection/ws/rs/IntrospectionWebService.java
+++ Server/src/main/java/org/xdi/oxauth/introspection/ws/rs/IntrospectionWebService.java
@@ -6,44 +6,37 @@
 
 package org.xdi.oxauth.introspection.ws.rs;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.util.ArrayList;
-
-import javax.inject.Inject;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.StringUtils;
+import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.xdi.oxauth.model.authorize.AuthorizeErrorResponseType;
-import org.xdi.oxauth.model.common.AbstractToken;
-import org.xdi.oxauth.model.common.AccessToken;
-import org.xdi.oxauth.model.common.AuthorizationGrant;
-import org.xdi.oxauth.model.common.AuthorizationGrantList;
-import org.xdi.oxauth.model.common.IntrospectionResponse;
-import org.xdi.oxauth.model.common.TokenType;
-import org.xdi.oxauth.model.common.User;
+import org.xdi.oxauth.model.common.*;
 import org.xdi.oxauth.model.configuration.AppConfiguration;
 import org.xdi.oxauth.model.error.ErrorResponseFactory;
 import org.xdi.oxauth.model.uma.UmaScopeType;
 import org.xdi.oxauth.model.util.Util;
+import org.xdi.oxauth.service.AttributeService;
 import org.xdi.oxauth.service.ClientService;
+import org.xdi.oxauth.service.external.ExternalIntrospectionService;
+import org.xdi.oxauth.service.external.context.ExternalIntrospectionContext;
 import org.xdi.oxauth.service.token.TokenService;
 import org.xdi.oxauth.util.ServerUtil;
 import org.xdi.util.Pair;
 
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiResponse;
-import com.wordnik.swagger.annotations.ApiResponses;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
 
 /**
  * @author Yuriy Zabrovarnyy
@@ -56,7 +49,7 @@ import com.wordnik.swagger.annotations.ApiResponses;
         "   further authentication) and returns a JSON document representing the meta information surrounding the token.")
 public class IntrospectionWebService {
 
-    public static final Pair<AuthorizationGrant, Boolean> EMPTY = new Pair<AuthorizationGrant, Boolean>(null, false);
+    private static final Pair<AuthorizationGrant, Boolean> EMPTY = new Pair<AuthorizationGrant, Boolean>(null, false);
 
     @Inject
     private Logger log;
@@ -70,6 +63,10 @@ public class IntrospectionWebService {
     private AuthorizationGrantList authorizationGrantList;
     @Inject
     private ClientService clientService;
+    @Inject
+    private ExternalIntrospectionService externalIntrospectionService;
+    @Inject
+    private AttributeService attributeService;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -80,20 +77,24 @@ public class IntrospectionWebService {
     })
     public Response introspectGet(@HeaderParam("Authorization") String p_authorization,
                                   @QueryParam("token") String p_token,
-                                  @QueryParam("token_type_hint") String tokenTypeHint
+                                  @QueryParam("token_type_hint") String tokenTypeHint,
+                                  @Context HttpServletRequest httpRequest,
+                                  @Context HttpServletResponse httpResponse
     ) {
-        return introspect(p_authorization, p_token, tokenTypeHint);
+        return introspect(p_authorization, p_token, tokenTypeHint, httpRequest, httpResponse);
     }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     public Response introspectPost(@HeaderParam("Authorization") String p_authorization,
                                    @FormParam("token") String p_token,
-                                   @FormParam("token_type_hint") String tokenTypeHint) {
-        return introspect(p_authorization, p_token, tokenTypeHint);
+                                   @FormParam("token_type_hint") String tokenTypeHint,
+                                   @Context HttpServletRequest httpRequest,
+                                   @Context HttpServletResponse httpResponse) {
+        return introspect(p_authorization, p_token, tokenTypeHint, httpRequest, httpResponse);
     }
 
-    private Response introspect(String p_authorization, String p_token, String tokenTypeHint) {
+    private Response introspect(String p_authorization, String p_token, String tokenTypeHint, HttpServletRequest httpRequest, HttpServletResponse httpResponse) {
         try {
             log.trace("Introspect token, authorization: {}, token to introsppect: {}, tokenTypeHint:", p_authorization, p_token, tokenTypeHint);
             if (StringUtils.isNotBlank(p_authorization) && StringUtils.isNotBlank(p_token)) {
@@ -134,6 +135,14 @@ public class IntrospectionWebService {
                         } else {
                             log.error("Failed to find grant for access_token: " + p_token + ". Return 200 with active=false.");
                         }
+                        JSONObject responseAsJsonObject = new JSONObject(ServerUtil.asJson(response));
+
+                        ExternalIntrospectionContext context = new ExternalIntrospectionContext(authorizationGrant, httpRequest, httpResponse, appConfiguration, attributeService);
+                        if (externalIntrospectionService.executeExternalModifyResponse(responseAsJsonObject, context)) {
+                            log.trace("Successfully run extenal introspection scripts.");
+                            return Response.status(Response.Status.OK).entity(responseAsJsonObject).build();
+                        }
+
                         return Response.status(Response.Status.OK).entity(ServerUtil.asJson(response)).build();
                     } else {
                         log.error("Access token is not valid. Valid: " + (authorizationAccessToken != null && authorizationAccessToken.isValid()));
