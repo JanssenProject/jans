e9bb4ac0fb53a6020e89ada0452275a2a4ce6ddf
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2019-01-17 00:04:21 -0400

    oxAuth #784 Add support for Token Revocation https://github.com/GluuFederation/oxAuth/issues/784

--- Server/src/main/java/org/xdi/oxauth/model/audit/Action.java
+++ Server/src/main/java/org/xdi/oxauth/model/audit/Action.java
@@ -1,3 +1,9 @@
+/*
+ * oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2014, Gluu
+ */
+
 package org.xdi.oxauth.model.audit;
 
 public enum Action {
@@ -9,6 +15,7 @@ public enum Action {
     USER_INFO("USER_INFO"),
     TOKEN_REQUEST("TOKEN_REQUEST"),
     TOKEN_VALIDATE("TOKEN_VALIDATE"),
+    TOKEN_REVOCATION("TOKEN_REVOCATION"),
     SESSION_UNAUTHENTICATED("SESSION_UNAUTHENTICATED"),
     SESSION_AUTHENTICATED("SESSION_AUTHENTICATED"),
     SESSION_DESTROYED("SESSION_DESTROYED");
