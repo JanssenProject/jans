0290d3b26e2ec5984d96ebb2c1aac9d068dcad54
Jose G <bonustrack310@gmail.com>
2019-02-04 10:11:06 -0500

    Fixes for #993

--- Server/integrations/passport/PassportExternalAuthenticator.py
+++ Server/integrations/passport/PassportExternalAuthenticator.py
@@ -147,11 +147,10 @@ class PersonAuthentication(PersonAuthenticationType):
         identity = CdiUtil.bean(Identity)
 
         if step == 1:
-            # This param is needed in passportlogin.xhtml
-            identity.setWorkingParameter("behaviour", "social")
-
             #re-read the strategies config (for instance to know which strategies have enabled the email account linking)
             self.parseProviderConfigs()
+            identity.setWorkingParameter("externalProviders", json.dumps(self.registeredProviders))
+
             providerParam = self.customAuthzParameter
             url = None
 
@@ -190,7 +189,7 @@ class PersonAuthentication(PersonAuthenticationType):
     def getExtraParametersForStep(self, configurationAttributes, step):
         print "Passport. getExtraParametersForStep called"
         if step == 1:
-            return Arrays.asList("selectedProvider")
+            return Arrays.asList("selectedProvider", "externalProviders")
         elif step == 2:
             return Arrays.asList("passport_user_profile")
         return None
