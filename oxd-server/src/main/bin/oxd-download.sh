422396482bcf550ee54284fbf6c59612fc4921d5
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2019-02-10 19:40:07 +0200

    switch version 4.0.beta-SNAPSHOT -> 4.0-SNAPSHOT

--- oxd-server/src/main/bin/oxd-download.sh
+++ oxd-server/src/main/bin/oxd-download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-warName=oxd-server-4.0.beta-SNAPSHOT-distribution.zip
+warName=oxd-server-4.0-SNAPSHOT-distribution.zip
 distDir=oxd-dist
 
 # Clean up
@@ -8,5 +8,5 @@ rm -f $warName
 rm -f -r $destDir
 
 # Download and unzip
-wget http://ox.gluu.org/maven/org/xdi/oxd-server/4.0.beta-SNAPSHOT/$warName
+wget http://ox.gluu.org/maven/org/xdi/oxd-server/4.0-SNAPSHOT/$warName
 unzip $warName -d $distDir
