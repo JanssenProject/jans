9ba9c7e52cea6a6ae562535716028f2fc1cee490
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2019-02-14 19:33:29 -0400

    oxAuth #989, #997

---
+++ Model/src/main/java/org/xdi/oxauth/model/crypto/signature/AlgorithmFamily.java
@@ -0,0 +1,44 @@
+/*
+ * oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2014, Gluu
+ */
+
+package org.xdi.oxauth.model.crypto.signature;
+
+/**
+ * @author Javier Rojas Blum
+ * @version February 12, 2019
+ */
+public enum AlgorithmFamily {
+    HMAC("HMAC"),
+    RSA("RSA"),
+    EC("EC");
+
+    private final String value;
+
+    AlgorithmFamily(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    public static AlgorithmFamily fromString(String param) {
+        if (param != null) {
+            for (AlgorithmFamily gt : AlgorithmFamily.values()) {
+                if (param.equals(gt.value)) {
+                    return gt;
+                }
+            }
+        }
+
+        return null;
+    }
+}
