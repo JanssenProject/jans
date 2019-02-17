bcf3910a184567ddc1134d0dccb4b883c2fc7e8b
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2019-02-17 23:37:11 +0200

    oxauth : extracted MTLS into separate module
    https://github.com/GluuFederation/oxAuth/issues/946

--- Server/src/main/java/org/xdi/oxauth/util/CertUtil.java
+++ Server/src/main/java/org/xdi/oxauth/util/CertUtil.java
@@ -6,18 +6,13 @@
 
 package org.xdi.oxauth.util;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
-import org.bouncycastle.util.encoders.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xdi.oxauth.crypto.cert.CertificateParser;
-import org.xdi.oxauth.model.util.Base64Util;
 import org.xdi.util.StringHelper;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -65,30 +60,6 @@ public class CertUtil {
 		return certificates;
 	}
 
-    /**
-     *
-     * @param pem (e.g. "-----BEGIN CERTIFICATE-----MIICsDCCAZigAwIBAgIIdF+Wcca7gzkwDQYJKoZIhvcNAQELBQAwGDEWMBQGA1UEAwwNY2FvajdicjRpcHc2dTAeFw0xNzA4MDcxNDMyMzVaFw0xODA4MDcxNDMyMzZaMBgxFjAUBgNVBAMMDWNhb2o3YnI0aXB3NnUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCdrt40Otrveq46K3BzZuds6wDqsP0kZV+C3GdyTQWl53orBRtPIiEh6BauP17Rr19qadh7t4yFBb5thrXwBewseSNEL4j7sB0YoeNwRsmA29Fjfoe0yeNpLixFadL6dz7ej9xW2suPppIO6jA5SYgL6+S42ZlIauCnSQBKFcdP8QRvgDZBZ4A7CmuloRJst7GQzppa+YWR+Zg3V5reV8Ekrkjxhwgd+rMsGahxijY7Juf2zMgLOXwe68y41SGnn+1RwezAhnJgioGiwY2gP7z2m8yNZXhpUiX+KAP2xvYb60wNYOswuqfpya68rSmYT8mQjld1EPR21dBMjRQ8HfUBAgMBAAEwDQYJKoZIhvcNAQELBQADggEBAAIUlqltRlbqiolGETmAUF8AiC008UCUmI+IsnORbHFSaACKW04m1iFH0OlxuAE1ECj1mlTcKb4md6i7n+Fy+fdGXFL73yhlSiBLu7XW5uN1/dAkynA+mXC5BDFijmvkEAgNLKyh40u/U1u75v2SFS+kLyMeqmVxvUHA7qA8VgyHi/FZzXCfEvxK5jye4L8tkAR34x5j5MpPDMfLkwLegUG+ygX+h/f8luKiQAk7eD4C59c/F0PpigvzcMpyg8+SE9loIEuJ9dRaRaTwIzez3QA7PJtrhu9h0TooTtkmF/Zw9HARrO0qXgT8uNtQDcRXZCItt1Qr7cOJyx2IjTFR2rE=-----END CERTIFICATE-----";)
-     * @return x509 certificate
-     */
-	public static X509Certificate x509CertificateFromPem(String pem) {
-        pem = StringUtils.remove(pem, "-----BEGIN CERTIFICATE-----");
-        pem = StringUtils.remove(pem, "-----END CERTIFICATE-----");
-        return x509CertificateFromBytes(Base64.decode(pem));
-    }
-
-	public static X509Certificate x509CertificateFromBytes(byte[] cert) {
-		try {
-			CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
-			InputStream bais = new ByteArrayInputStream(cert);
-
-			return (X509Certificate) certFactory.generateCertificate(bais);
-		} catch (CertificateException ex) {
-			log.error("Failed to parse X.509 certificates from bytes", ex);
-		}
-		
-		return null;
-	}
-
 	public static X509Certificate parsePem(String pem) {
 		try {
 			return CertificateParser.parsePem(pem);
@@ -98,15 +69,4 @@ public class CertUtil {
 		
 		return null;
 	}
-
-	public static String confirmationMethodHashS256(String certificateAsPem) {
-	    if (StringUtils.isBlank(certificateAsPem)) {
-	        return "";
-        }
-	    certificateAsPem = StringUtils.remove(certificateAsPem, "-----BEGIN CERTIFICATE-----");
-	    certificateAsPem = StringUtils.remove(certificateAsPem, "-----END CERTIFICATE-----");
-	    certificateAsPem = StringUtils.replace(certificateAsPem, "\n", "");
-        return Base64Util.base64urlencode(DigestUtils.sha256(Base64.decode(certificateAsPem)));
-    }
-
 }
---
+++ oxauth-mtls/src/main/java/org/xdi/oxauth/auth/mtls/MTLSInterceptor.java
@@ -0,0 +1,115 @@
+package org.xdi.oxauth.auth.mtls;
+
+import com.google.common.base.Strings;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+import org.codehaus.jettison.json.JSONObject;
+import org.slf4j.Logger;
+import org.xdi.oxauth.interception.MTLSInterception;
+import org.xdi.oxauth.model.common.AuthenticationMethod;
+import org.xdi.oxauth.model.crypto.AbstractCryptoProvider;
+import org.xdi.oxauth.model.jwk.JSONWebKey;
+import org.xdi.oxauth.model.jwk.JSONWebKeySet;
+import org.xdi.oxauth.model.ref.AuthenticatorReference;
+import org.xdi.oxauth.model.ref.ClientReference;
+import org.xdi.oxauth.model.util.CertUtils;
+import org.xdi.oxauth.model.util.JwtUtil;
+
+import javax.annotation.Priority;
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.security.PublicKey;
+import java.security.cert.X509Certificate;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ */
+@Interceptor
+@MTLSInterception
+@Priority(2)
+public class MTLSInterceptor {
+
+    @Inject
+    private Logger log;
+
+    @AroundInvoke
+    public Object processMTLS(InvocationContext ctx) throws Exception {
+        log.debug("processMTLS...");
+        HttpServletRequest httpRequest = (HttpServletRequest) ctx.getParameters()[0];
+        HttpServletResponse httpResponse = (HttpServletResponse) ctx.getParameters()[1];
+        FilterChain filterChain = (FilterChain) ctx.getParameters()[2];
+        ClientReference client = (ClientReference) ctx.getParameters()[2];
+        AuthenticatorReference authenticator = (AuthenticatorReference) ctx.getParameters()[2];
+        AbstractCryptoProvider cryptoProvider = (AbstractCryptoProvider) ctx.getParameters()[2];
+        final boolean result = processMTLS(httpRequest, httpResponse, filterChain, client, authenticator, cryptoProvider);
+        ctx.proceed();
+        return result;
+    }
+
+    private boolean processMTLS(HttpServletRequest httpRequest, HttpServletResponse httpResponse, FilterChain filterChain,
+                                ClientReference client, AuthenticatorReference authenticator, AbstractCryptoProvider cryptoProvider) throws Exception {
+
+        log.debug("Trying to authenticate client {} via {} ...", client.getClientId(), client.getAuthenticationMethod());
+
+        final String clientCertAsPem = httpRequest.getHeader("X-ClientCert");
+        if (StringUtils.isBlank(clientCertAsPem)) {
+            log.debug("Client certificate is missed in `X-ClientCert` header, client_id: {}.", client.getClientId());
+            return false;
+        }
+
+        X509Certificate cert = CertUtils.x509CertificateFromPem(clientCertAsPem);
+        if (cert == null) {
+            log.debug("Failed to parse client certificate, client_id: {}.", client.getClientId());
+            return false;
+        }
+
+        if (client.getAuthenticationMethod() == AuthenticationMethod.TLS_CLIENT_AUTH) {
+
+            final String subjectDn = client.getAttributes().getTlsClientAuthSubjectDn();
+            if (StringUtils.isBlank(subjectDn)) {
+                log.debug("SubjectDN is not set for client {} which is required to authenticate it via `tls_client_auth`.", client.getClientId());
+                return false;
+            }
+
+            // we check only `subjectDn`, the PKI certificate validation is performed by apache/httpd
+            if (subjectDn.equals(cert.getSubjectDN().getName())) {
+                log.debug("Client {} authenticated via `tls_client_auth`.", client.getClientId());
+                authenticator.configureSessionClient();
+
+                filterChain.doFilter(httpRequest, httpResponse);
+                return true;
+            }
+        }
+
+        if (client.getAuthenticationMethod() == AuthenticationMethod.SELF_SIGNED_TLS_CLIENT_AUTH) { // disable it temporarily
+            final PublicKey publicKey = cert.getPublicKey();
+            final byte[] encodedKey = publicKey.getEncoded();
+
+            JSONObject jsonWebKeys = Strings.isNullOrEmpty(client.getJwks()) ?
+                    JwtUtil.getJSONWebKeys(client.getJwksUri()) :
+                    new JSONObject(client.getJwks());
+
+            if (jsonWebKeys == null) {
+                log.debug("Unable to load json web keys for client: {}, jwks_uri: {}, jks: {}", client.getClientId(), client.getJwksUri(), client.getJwks());
+                return false;
+            }
+
+            final JSONWebKeySet keySet = JSONWebKeySet.fromJSONObject(jsonWebKeys);
+            for (JSONWebKey key : keySet.getKeys()) {
+                if (ArrayUtils.isEquals(encodedKey, cryptoProvider.getPublicKey(key.getKid(), jsonWebKeys).getEncoded())) {
+                    log.debug("Client {} authenticated via `self_signed_tls_client_auth`, matched kid: {}.", client.getClientId(), key.getKid());
+                    authenticator.configureSessionClient();
+
+                    filterChain.doFilter(httpRequest, httpResponse);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
