7a842025cb3886fc25baec9c321098ae90f414e3
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2019-02-18 09:54:54 +0200

    oxauth : MTLS - introduced MTLSInterceptionInterface between interceptor and server to keep contract and not mess parameters (force same method). Removed redundant extension declaration.
    https://github.com/GluuFederation/oxAuth/issues/946

---
+++ common/src/main/java/org/xdi/oxauth/interception/MTLSInterceptionInterface.java
@@ -0,0 +1,17 @@
+package org.xdi.oxauth.interception;
+
+import org.xdi.oxauth.model.crypto.AbstractCryptoProvider;
+import org.xdi.oxauth.model.ref.AuthenticatorReference;
+import org.xdi.oxauth.model.ref.ClientReference;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ */
+public interface MTLSInterceptionInterface {
+    boolean processMTLS(HttpServletRequest httpRequest, HttpServletResponse httpResponse, FilterChain filterChain,
+                        ClientReference client, AuthenticatorReference authenticator, AbstractCryptoProvider cryptoProvider) throws Exception;
+}
