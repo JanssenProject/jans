d1b67c29ae76f0abc534f0ee531f9adf9d6f632b
Mohit Mali <mali@gluu.org>
2019-03-09 12:49:22 +0530

    alter changes as suggested

--- oxd-server/src/test/java/org/xdi/oxd/server/GetTokensByCodeTest.java
+++ oxd-server/src/test/java/org/xdi/oxd/server/GetTokensByCodeTest.java
@@ -1,6 +1,7 @@
 package org.xdi.oxd.server;
 
 import com.google.common.collect.Lists;
+import com.google.inject.Inject;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.xdi.oxd.client.ClientInterface;
@@ -11,10 +12,14 @@ import org.xdi.oxd.common.params.GetTokensByCodeParams;
 import org.xdi.oxd.common.response.GetClientTokenResponse;
 import org.xdi.oxd.common.response.GetTokensByCodeResponse;
 import org.xdi.oxd.common.response.RegisterSiteResponse;
+import org.xdi.oxd.server.service.ConfigurationService;
+import org.xdi.oxd.server.service.StateService;
 
+import javax.ws.rs.BadRequestException;
 import java.io.IOException;
 
 import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 import static org.xdi.oxd.server.TestUtils.notEmpty;
 
 /**
@@ -24,14 +29,28 @@ import static org.xdi.oxd.server.TestUtils.notEmpty;
 
 public class GetTokensByCodeTest {
 
+
     @Parameters({"host", "opHost", "redirectUrl", "userId", "userSecret"})
     @Test
-    public void test(String host, String opHost, String redirectUrl, String userId, String userSecret) throws IOException {
+    public void whenValidCodeIsUsed_shouldGetTokenInResponse(String host, String opHost, String redirectUrl, String userId, String userSecret) throws IOException {
         ClientInterface client = Tester.newClient(host);
         final RegisterSiteResponse site = RegisterSiteTest.registerSite(client, opHost, redirectUrl);
-        GetTokensByCodeResponse tokensResponse = whenValidCodeIsUsed_shouldGetTokenInResponse(client, site, userId, userSecret, CoreUtils.secureRandomString());
+        GetTokensByCodeResponse tokensResponse = tokenByCode(client, site, userId, userSecret, CoreUtils.secureRandomString());
         refreshToken(tokensResponse, client, site.getOxdId());
     }
+
+
+
+    @Parameters({"host", "opHost", "redirectUrl", "userId", "userSecret"})
+    @Test
+    public void whenInvalidCodeIsUsed_shouldGet400BadRequest(String host, String opHost, String redirectUrl, String userId, String userSecret) throws IOException {
+        ClientInterface client = Tester.newClient(host);
+        final RegisterSiteResponse site = RegisterSiteTest.registerSite(client, opHost, redirectUrl);
+        tokenByInvalidCode(client, site, userId, userSecret, CoreUtils.secureRandomString());
+    }
+
+
+
 
     public static GetClientTokenResponse refreshToken(GetTokensByCodeResponse resp, ClientInterface client, String oxdId) {
         notEmpty(resp.getRefreshToken());
@@ -51,7 +70,7 @@ public class GetTokensByCodeTest {
         return refreshResponse;
     }
 
-    public static GetTokensByCodeResponse whenValidCodeIsUsed_shouldGetTokenInResponse(ClientInterface client, RegisterSiteResponse site, String userId, String userSecret, String nonce) {
+    public static GetTokensByCodeResponse tokenByCode(ClientInterface client, RegisterSiteResponse site, String userId, String userSecret, String nonce) {
 
         final String state = CoreUtils.secureRandomString();
 
@@ -69,6 +88,37 @@ public class GetTokensByCodeTest {
         notEmpty(resp.getAccessToken());
         notEmpty(resp.getIdToken());
         notEmpty(resp.getRefreshToken());
+        return resp;
+    }
+
+
+    public static GetTokensByCodeResponse tokenByInvalidCode(ClientInterface client, RegisterSiteResponse site, String userId, String userSecret, String nonce) {
+
+        final String state = CoreUtils.secureRandomString();
+        codeRequest(client, site.getOxdId(), userId, userSecret, state, nonce);
+
+        final String code = CoreUtils.secureRandomString();
+
+        String testOxdId = site.getOxdId();
+
+        final GetTokensByCodeParams params = new GetTokensByCodeParams();
+        params.setOxdId(testOxdId);
+        params.setCode(code);
+        params.setState(state);
+
+        GetTokensByCodeResponse resp = null;
+
+        try {
+            resp = client.getTokenByCode(Tester.getAuthorization(), params);
+            assertNotNull(resp);
+            notEmpty(resp.getAccessToken());
+            notEmpty(resp.getIdToken());
+            notEmpty(resp.getRefreshToken());
+
+        } catch (BadRequestException ex) {
+            assertEquals(ex.getMessage(), "HTTP 400 Bad Request");
+        }
+
         return resp;
     }
 
