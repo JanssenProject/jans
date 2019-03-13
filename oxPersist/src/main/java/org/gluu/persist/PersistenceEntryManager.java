48855b4f812a66f5c97d92079d64cc7bc5564b16
Jose G <bonustrack310@gmail.com>
2019-03-13 08:43:11 -0500

    small refactoring

--- oxPersist/src/main/java/org/gluu/persist/PersistenceEntryManager.java
+++ oxPersist/src/main/java/org/gluu/persist/PersistenceEntryManager.java
@@ -19,34 +19,48 @@ import org.gluu.persist.model.SortOrder;
 import org.gluu.persist.operation.PersistenceOperationService;
 import org.gluu.search.filter.Filter;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.FlushModeType;
+import javax.persistence.LockModeType;
+import javax.persistence.Query;
+
 /**
  * Methods which Entry Manager must provide
  *
  * @author Yuriy Movchan Date: 01/29/2018
  */
-public interface PersistenceEntryManager {
+public interface PersistenceEntryManager extends EntityManager {
 
-    PersistenceOperationService getOperationService();
+    void addDeleteSubscriber(DeleteNotifier subscriber);
 
-    void persist(Object entry);
+    boolean authenticate(String bindDn, String password);
 
-    <T> T merge(T entry);
-
-    void remove(Object entry);
-
-    void removeRecursively(String dn);
-
-    boolean contains(Object entry);
+    boolean authenticate(String baseDN, String userName, String password);
 
     <T> boolean contains(Class<T> entryClass, String primaryKey);
 
     <T> boolean contains(String primaryKey, Class<T> entryClass, Filter filter);
 
-    <T> List<T> findEntries(Object entry);
+    <T> int countEntries(Object entry);
 
-    <T> T find(Class<T> entryClass, Object primaryKey);
+    <T> int countEntries(String baseDN, Class<T> entryClass, Filter filter);
+
+    <T> int countEntries(String baseDN, Class<T> entryClass, Filter filter,  SearchScope scope);
+
+    <T> List<T> createEntities(Class<T> entryClass, Map<String, List<AttributeData>> entriesAttributes);
+
+    Date decodeTime(String date);
+
+    boolean destroy();
+
+    String encodeTime(Date date);
+
+    String[] exportEntry(String dn);
 
     <T> T find(Class<T> entryClass, Object primaryKey, String[] ldapReturnAttributes);
+
+    <T> List<T> findEntries(Object entry);
 
     /**
      * Search by sample
@@ -66,46 +80,96 @@ public interface PersistenceEntryManager {
     <T> List<T> findEntries(String baseDN, Class<T> entryClass, Filter filter, String[] ldapReturnAttributes, int count);
 
     <T> List<T> findEntries(String baseDN, Class<T> entryClass, Filter filter, SearchScope scope, String[] ldapReturnAttributes,
-            int start, int count, int chunkSize);
+                            int start, int count, int chunkSize);
 
     <T> List<T> findEntries(String baseDN, Class<T> entryClass, Filter filter, SearchScope scope, String[] ldapReturnAttributes,
-            BatchOperation<T> batchOperation, int start, int count, int chunkSize);
+                            BatchOperation<T> batchOperation, int start, int count, int chunkSize);
 
     // TODO: Combine sortBy and SortOrder into Sort
     <T> PagedResult<T> findPagedEntries(String baseDN, Class<T> entryClass, Filter filter, String[] ldapReturnAttributes, String sortBy,
-            SortOrder sortOrder, int start, int count, int chunkSize);
-
-    boolean authenticate(String bindDn, String password);
-
-    boolean authenticate(String baseDN, String userName, String password);
-
-    <T> int countEntries(Object entry);
-
-    <T> int countEntries(String baseDN, Class<T> entryClass, Filter filter);
-
-    <T> int countEntries(String baseDN, Class<T> entryClass, Filter filter,  SearchScope scope);
+                                        SortOrder sortOrder, int start, int count, int chunkSize);
 
     int getHashCode(Object entry);
 
     String[] getObjectClasses(Object entry, Class<?> entryClass);
 
-    <T> List<T> createEntities(Class<T> entryClass, Map<String, List<AttributeData>> entriesAttributes);
+    PersistenceOperationService getOperationService();
 
-    String[] exportEntry(String dn);
-
-    void addDeleteSubscriber(DeleteNotifier subscriber);
+    <T> Map<T, List<T>> groupListByProperties(Class<T> entryClass, List<T> entries, boolean caseSensetive, String groupByProperties,
+                                              String sumByProperties);
 
     void removeDeleteSubscriber(DeleteNotifier subscriber);
 
-    boolean destroy();
-
-    String encodeTime(Date date);
-
-    Date decodeTime(String date);
+    void removeRecursively(String dn);
 
     <T> void sortListByProperties(Class<T> entryClass, List<T> entries, boolean caseSensetive, String... sortByProperties);
 
-    <T> Map<T, List<T>> groupListByProperties(Class<T> entryClass, List<T> entries, boolean caseSensetive, String groupByProperties,
-            String sumByProperties);
+    default void clear() {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default void close() {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default Query createNamedQuery(String name) {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default Query createNativeQuery(String sqlString) {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default Query createNativeQuery(String sqlString, @SuppressWarnings("rawtypes") Class resultClass) {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default Query createNativeQuery(String sqlString, String resultSetMapping) {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default Query createQuery(String qlString) {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default void flush() {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default Object getDelegate() {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default FlushModeType getFlushMode() {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default <T> T getReference(Class<T> entryClass, Object primaryKey) {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default EntityTransaction getTransaction() {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default boolean isOpen() {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default void joinTransaction() {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default void lock(Object entry, LockModeType lockMode) {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default void refresh(Object entry) {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
+
+    default void setFlushMode(FlushModeType flushMode) {
+        throw new UnsupportedOperationException("Method not implemented.");
+    }
 
 }
