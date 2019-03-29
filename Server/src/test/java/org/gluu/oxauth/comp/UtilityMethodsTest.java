d32b259f51333feb176bd02f77aa19b6bb8050aa
yurem <Yuriy.Movchan@gmail.com>
2019-03-29 20:47:23 +0300

    Change org.xdi -> org.gluu oxCore #58

---
+++ Server/src/test/java/org/gluu/oxauth/comp/UtilityMethodsTest.java
@@ -0,0 +1,29 @@
+/*
+ * oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2014, Gluu
+ */
+
+package org.gluu.oxauth.comp;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+import org.gluu.oxauth.model.common.AuthorizationGrantList;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ * @version 0.9, 09/01/2013
+ */
+
+public class UtilityMethodsTest {
+
+	@Test
+	public void test() {
+		final String dn = "uniqueIdentifier=fe6c2e7b-8c54-41d1-8cd1-c816371525dc,ou=token,inum=aa94b930-956a-4e3d-a150-d13735c3712b,ou=clients,o=gluu";
+		final String clientId = AuthorizationGrantList.extractClientIdFromTokenDn(dn);
+		assertEquals("aa94b930-956a-4e3d-a150-d13735c3712b", clientId);
+	}
+
+}
