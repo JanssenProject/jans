10454a32ae206de90479db442239ff8d15737eb8
yurem <Yuriy.Movchan@gmail.com>
2019-03-29 20:47:23 +0300

    Change org.xdi -> org.gluu oxCore #58

---
+++ Server/src/test/java/org/gluu/oxauth/BaseComponentTest.java
@@ -0,0 +1,26 @@
+/*
+ * oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2014, Gluu
+ */
+
+package org.gluu.oxauth;
+
+import org.gluu.oxauth.BaseTest;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ * @author Yuriy Movchan
+ * @version 0.9, 15/10/2012
+ */
+
+public abstract class BaseComponentTest extends BaseTest {
+
+	public static void sleepSeconds(int p_seconds) {
+		try {
+			Thread.sleep(p_seconds * 1000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
