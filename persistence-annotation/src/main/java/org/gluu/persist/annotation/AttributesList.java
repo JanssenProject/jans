d3506f121410939f8b08e66dacdebec57185498d
yurem <Yuriy.Movchan@gmail.com>
2019-04-12 10:48:49 +0300

    Persistence annotations should be DB neutral

--- persistence-annotation/src/main/java/org/gluu/persist/annotation/AttributesList.java
+++ persistence-annotation/src/main/java/org/gluu/persist/annotation/AttributesList.java
@@ -18,7 +18,7 @@ import java.lang.annotation.Target;
  */
 @Target({ ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
-public @interface @AttributesList {
+public @interface AttributesList {
 
     /**
      * (Required) The class property name which contains LDAP attribute name.
