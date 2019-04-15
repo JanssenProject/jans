23aaea082904bd14459f4ab440f99cec661432ac
yurem <Yuriy.Movchan@gmail.com>
2019-04-15 19:24:37 +0300

    Remove duplicate GluuAttributeDateType to use same bean from oxcore-model

--- oxModel/src/main/java/org/gluu/model/attribute/AttributeDataType.java
+++ oxModel/src/main/java/org/gluu/model/attribute/AttributeDataType.java
@@ -2,7 +2,8 @@
  * oxCore is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
  *
  * Copyright (c) 2014, Gluu
- */package org.gluu.model.attribute;
+ */
+package org.gluu.model.attribute;
 
 import java.util.HashMap;
 import java.util.Map;
