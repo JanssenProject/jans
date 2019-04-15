3e5a6c15f63dbc6f569ef774b4747a88c0504188
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2019-04-15 11:21:42 +0300

    fixed conflict between Jackson 1.x and Jackson 2.x
    https://github.com/GluuFederation/oxd/issues/305

--- oxd-server/src/test/java/org/gluu/oxd/server/manual/GoogleTest.java
+++ oxd-server/src/test/java/org/gluu/oxd/server/manual/GoogleTest.java
@@ -3,18 +3,16 @@ package org.gluu.oxd.server.manual;
 import com.google.common.base.Strings;
 import org.gluu.oxd.client.ClientInterface;
 import org.gluu.oxd.common.params.GetAuthorizationUrlParams;
-import org.gluu.oxd.common.params.GetTokensByCodeParams;
 import org.gluu.oxd.common.params.RegisterSiteParams;
 import org.gluu.oxd.common.response.GetAuthorizationUrlResponse;
-import org.gluu.oxd.common.response.GetTokensByCodeResponse;
 import org.gluu.oxd.common.response.RegisterSiteResponse;
 import org.gluu.oxd.server.Tester;
 
 import java.io.IOException;
 
+import static org.gluu.oxd.server.TestUtils.notEmpty;
 import static org.testng.AssertJUnit.assertNotNull;
 import static org.testng.AssertJUnit.assertTrue;
-import static org.gluu.oxd.server.TestUtils.notEmpty;
 
 /**
  * @author Yuriy Zabrovarnyy
@@ -32,23 +30,23 @@ public class GoogleTest {
     private static final String CLIENT_SECRET = "vJVYPI4Ybwc9a2YOesq2bRCa";
 
     public static void main(String[] args) throws IOException {
-        ClientInterface client = Tester.newClient(HOST);
-
-        final RegisterSiteResponse site = registerSite(client);
-        final String authorizationUrl = getAuthorizationUrl(client, site.getOxdId());
+//        ClientInterface client = Tester.newClient(HOST);
+//
+//        final RegisterSiteResponse site = registerSite(client);
+//        final String authorizationUrl = getAuthorizationUrl(client, site.getOxdId());
 
         // after successful login get redirect with code
         // https://mytestproduct.com/?state=af0ifjsldkj&code=4/2I5U130cxs7MObKVniVseBQkHQ0JQS0p5JfZm7NgZ-M&authuser=0&session_state=02bd0461002924877bee444d9dfd9f1279e44335..ae63&prompt=consent#
 //            String code = "4/2I5U130cxs7MObKVniVseBQkHQ0JQS0p5JfZm7NgZ-M";
 //            String code = "4/e8WrOQDMNRllHvMGAEKAovwWMnIy7k7oxRN4Xn9JPrg";
-        String code = "4/nWqDxBS-c7MDN_1Gq2WtW2L6B9KnA5rpOOYzGEdg7lM";
-
-        final GetTokensByCodeParams params = new GetTokensByCodeParams();
-        params.setOxdId(site.getOxdId());
-        params.setCode(code);
-
-        final GetTokensByCodeResponse resp = client.getTokenByCode(Tester.getAuthorization(), params);
-        System.out.println(resp);
+//        String code = "4/nWqDxBS-c7MDN_1Gq2WtW2L6B9KnA5rpOOYzGEdg7lM";
+//
+//        final GetTokensByCodeParams params = new GetTokensByCodeParams();
+//        params.setOxdId(site.getOxdId());
+//        params.setCode(code);
+//
+//        final GetTokensByCodeResponse resp = client.getTokenByCode(Tester.getAuthorization(), params);
+//        System.out.println(resp);
     }
 
     private static String getAuthorizationUrl(ClientInterface client, String oxdId) {
