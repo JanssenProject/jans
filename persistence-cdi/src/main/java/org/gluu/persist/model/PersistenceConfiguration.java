8cc603f6be6b841e7ee996bb5ccac79ebb4b2f94
yurem <Yuriy.Movchan@gmail.com>
2019-05-10 21:00:36 +0300

    Move Persistence Factory creation code from oxAuth/oxTrust to oxPersistence #84

---
+++ persistence-cdi/src/main/java/org/gluu/persist/model/PersistenceConfiguration.java
@@ -0,0 +1,42 @@
+package org.gluu.persist.model;
+
+import org.gluu.persist.PersistenceEntryManagerFactory;
+import org.gluu.util.properties.FileConfiguration;
+
+/**
+ * Persistence configuration
+ *
+ * @author Yuriy Movchan Date: 05/10/2019
+ */
+public class PersistenceConfiguration {
+
+	private final String fileName;
+	private final FileConfiguration configuration;
+	private final Class<? extends PersistenceEntryManagerFactory> entryManagerFactoryType;
+	private final long lastModifiedTime;
+
+	public PersistenceConfiguration(String fileName, FileConfiguration configuration,
+			Class<? extends PersistenceEntryManagerFactory> entryManagerFactoryType, long lastModifiedTime) {
+		this.fileName = fileName;
+		this.configuration = configuration;
+		this.entryManagerFactoryType = entryManagerFactoryType;
+		this.lastModifiedTime = lastModifiedTime;
+	}
+
+	public final String getFileName() {
+		return fileName;
+	}
+
+	public final FileConfiguration getConfiguration() {
+		return configuration;
+	}
+
+	public final Class<? extends PersistenceEntryManagerFactory> getEntryManagerFactoryType() {
+		return entryManagerFactoryType;
+	}
+
+	public final long getLastModifiedTime() {
+		return lastModifiedTime;
+	}
+
+}
