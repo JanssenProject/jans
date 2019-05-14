1f084304059d43a137c0e9764236b2c60b599126
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2019-05-14 00:17:05 -0400

    oxAuth #1071 Userinfo - Expired token must return 401 instead of 400 error code.

--- Client/src/test/java/org/gluu/oxauth/ws/rs/UserInfoRestWebServiceHttpTest.java
+++ Client/src/test/java/org/gluu/oxauth/ws/rs/UserInfoRestWebServiceHttpTest.java
@@ -36,7 +36,7 @@ import static org.testng.Assert.*;
  * Functional tests for User Info Web Services (HTTP)
  *
  * @author Javier Rojas Blum
- * @version March 9, 2019
+ * @version May 14, 2019
  */
 public class UserInfoRestWebServiceHttpTest extends BaseTest {
 
@@ -337,7 +337,7 @@ public class UserInfoRestWebServiceHttpTest extends BaseTest {
         UserInfoResponse response = userInfoClient.execUserInfo("INVALID_ACCESS_TOKEN");
 
         showClient(userInfoClient);
-        assertEquals(response.getStatus(), 400, "Unexpected response code: " + response.getStatus());
+        assertEquals(response.getStatus(), 401, "Unexpected response code: " + response.getStatus());
         assertNotNull(response.getErrorType(), "Unexpected result: errorType not found");
         assertNotNull(response.getErrorDescription(), "Unexpected result: errorDescription not found");
     }
@@ -389,6 +389,77 @@ public class UserInfoRestWebServiceHttpTest extends BaseTest {
         assertEquals(userInfoResponse.getStatus(), 403, "Unexpected response code: " + userInfoResponse.getStatus());
         assertNotNull(userInfoResponse.getErrorType(), "Unexpected result: errorType not found");
         assertNotNull(userInfoResponse.getErrorDescription(), "Unexpected result: errorDescription not found");
+    }
+
+    @Parameters({"redirectUris", "redirectUri", "userId", "userSecret", "sectorIdentifierUri"})
+    @Test
+    public void requestUserInfoExpiredAccessToken(final String redirectUris, final String redirectUri,
+                                                  final String userId, final String userSecret,
+                                                  final String sectorIdentifierUri) throws Exception {
+        showTitle("requestUserInfoRS256");
+
+        List<ResponseType> responseTypes = Arrays.asList(
+                ResponseType.TOKEN,
+                ResponseType.ID_TOKEN);
+
+        // 1. Dynamic Registration
+        RegisterRequest registerRequest = new RegisterRequest(ApplicationType.WEB, "oxAuth test app",
+                StringUtils.spaceSeparatedToList(redirectUris));
+        registerRequest.setResponseTypes(responseTypes);
+        registerRequest.setUserInfoSignedResponseAlg(SignatureAlgorithm.RS256);
+        registerRequest.setSubjectType(SubjectType.PAIRWISE);
+        registerRequest.setSectorIdentifierUri(sectorIdentifierUri);
+
+        RegisterClient registerClient = new RegisterClient(registrationEndpoint);
+        registerClient.setRequest(registerRequest);
+        RegisterResponse registerResponse = registerClient.exec();
+
+        showClient(registerClient);
+        assertEquals(registerResponse.getStatus(), 200, "Unexpected response code: " + registerResponse.getEntity());
+        assertNotNull(registerResponse.getClientId());
+        assertNotNull(registerResponse.getClientSecret());
+        assertNotNull(registerResponse.getRegistrationAccessToken());
+        assertNotNull(registerResponse.getClientSecretExpiresAt());
+
+        String clientId = registerResponse.getClientId();
+
+        AuthorizationResponse authorizationResponse = requestAuthorization(userId, userSecret, redirectUri, responseTypes, clientId);
+
+        String accessToken = authorizationResponse.getAccessToken();
+
+        {
+            // 3. Request user info
+            UserInfoClient userInfoClient = new UserInfoClient(userInfoEndpoint);
+            userInfoClient.setJwksUri(jwksUri);
+            UserInfoResponse userInfoResponse = userInfoClient.execUserInfo(accessToken);
+
+            showClient(userInfoClient);
+            assertEquals(userInfoResponse.getStatus(), 200, "Unexpected response code: " + userInfoResponse.getStatus());
+            assertNotNull(userInfoResponse.getClaim(JwtClaimName.SUBJECT_IDENTIFIER));
+            assertNotNull(userInfoResponse.getClaim(JwtClaimName.ISSUER));
+            assertNotNull(userInfoResponse.getClaim(JwtClaimName.AUDIENCE));
+            assertNotNull(userInfoResponse.getClaim(JwtClaimName.NAME));
+            assertNotNull(userInfoResponse.getClaim(JwtClaimName.GIVEN_NAME));
+            assertNotNull(userInfoResponse.getClaim(JwtClaimName.FAMILY_NAME));
+            assertNotNull(userInfoResponse.getClaim(JwtClaimName.PICTURE));
+            assertNotNull(userInfoResponse.getClaim(JwtClaimName.EMAIL));
+            assertNotNull(userInfoResponse.getClaim(JwtClaimName.ZONEINFO));
+            assertNotNull(userInfoResponse.getClaim(JwtClaimName.LOCALE));
+        }
+
+        Thread.sleep(61000);
+
+        {
+            // 3. Request user info with expired access token
+            UserInfoClient userInfoClient = new UserInfoClient(userInfoEndpoint);
+            userInfoClient.setJwksUri(jwksUri);
+            UserInfoResponse userInfoResponse = userInfoClient.execUserInfo(accessToken);
+
+            showClient(userInfoClient);
+            assertEquals(userInfoResponse.getStatus(), 401, "Unexpected response code: " + userInfoResponse.getStatus());
+            assertNotNull(userInfoResponse.getErrorType(), "Unexpected result: errorType not found");
+            assertNotNull(userInfoResponse.getErrorDescription(), "Unexpected result: errorDescription not found");
+        }
     }
 
     @Parameters({"userId", "userSecret", "redirectUris", "redirectUri", "sectorIdentifierUri"})
