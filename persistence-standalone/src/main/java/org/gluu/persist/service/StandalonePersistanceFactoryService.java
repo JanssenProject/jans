d216e91c21ed82e37856cc0b314b358426222a0d
yurem <Yuriy.Movchan@gmail.com>
2019-05-22 18:52:21 +0300

    Fix base types collection

--- persistence-standalone/src/main/java/org/gluu/persist/service/StandalonePersistanceFactoryService.java
+++ persistence-standalone/src/main/java/org/gluu/persist/service/StandalonePersistanceFactoryService.java
@@ -1,17 +1,24 @@
 package org.gluu.persist.service;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 
 import org.gluu.persist.PersistenceEntryManagerFactory;
+import org.gluu.persist.exception.PropertyNotFoundException;
+import org.gluu.persist.exception.operation.ConfigurationException;
 import org.gluu.persist.model.PersistenceConfiguration;
+import org.gluu.persist.reflect.util.ReflectHelper;
 
 /**
  * Factory which creates Persistence Entry Manager
  *
  * @author Yuriy Movchan Date: 05/10/2019
+ * @param <E>
  */
-public class StandalonePersistanceFactoryService extends PersistanceFactoryService {
+public class StandalonePersistanceFactoryService<E> extends PersistanceFactoryService {
 
 	private HashMap<String, PersistenceEntryManagerFactory> persistenceEntryManagerFactoryNames;
 	private HashMap<Class<? extends PersistenceEntryManagerFactory>, PersistenceEntryManagerFactory> persistenceEntryManagerFactoryTypes;
@@ -35,10 +42,6 @@ public class StandalonePersistanceFactoryService extends PersistanceFactoryServi
 
 	@Override
 	public PersistenceEntryManagerFactory getPersistenceEntryManagerFactory(String persistenceType) {
-		return getPersistenceEntryManagerFactoryImpl(persistenceType);
-	}
-
-	private PersistenceEntryManagerFactory getPersistenceEntryManagerFactoryImpl(String persistenceType) {
 		if (this.persistenceEntryManagerFactoryNames == null) {
 			initPersistenceManagerMaps();
 		}
@@ -56,12 +59,29 @@ public class StandalonePersistanceFactoryService extends PersistanceFactoryServi
 			     .setUrls(org.reflections.util.ClasspathHelper.forPackage("org.gluu.persist"))
 			     .setScanners(new org.reflections.scanners.SubTypesScanner()));
 		Set<Class<? extends PersistenceEntryManagerFactory>> classes = reflections.getSubTypesOf(PersistenceEntryManagerFactory.class);
+
+		getLog().info("Found '{}' PersistenceEntryManagerFactory", classes.size());
 		
-		for (Class<? extends PersistenceEntryManagerFactory> clazz : classes) {
-			PersistenceEntryManagerFactory persistenceEntryManagerFactory = getPersistenceEntryManagerFactory(clazz);
+		List<Class<? extends PersistenceEntryManagerFactory>> classesList = new ArrayList<Class<? extends PersistenceEntryManagerFactory>>(classes);
+		for (Class<? extends PersistenceEntryManagerFactory> clazz : classesList) {
+			getLog().info("Found PersistenceEntryManagerFactory '{}'", clazz);
+			PersistenceEntryManagerFactory persistenceEntryManagerFactory = createPersistenceEntryManagerFactoryImpl(clazz);
 			persistenceEntryManagerFactoryNames.put(persistenceEntryManagerFactory.getPersistenceType(), persistenceEntryManagerFactory);
 			persistenceEntryManagerFactoryTypes.put(clazz, persistenceEntryManagerFactory);
 		}
+	}
+
+	private PersistenceEntryManagerFactory createPersistenceEntryManagerFactoryImpl(Class<? extends PersistenceEntryManagerFactory> persistenceEntryManagerFactoryClass) {
+		PersistenceEntryManagerFactory persistenceEntryManagerFactory;
+		try {
+			persistenceEntryManagerFactory = ReflectHelper.createObjectByDefaultConstructor(persistenceEntryManagerFactoryClass);
+		} catch (PropertyNotFoundException | IllegalArgumentException | InstantiationException | IllegalAccessException
+				| InvocationTargetException e) {
+            throw new ConfigurationException(
+                    String.format("Failed to create PersistenceEntryManagerFactory by type '%s'!", persistenceEntryManagerFactoryClass));
+		}
+
+		return persistenceEntryManagerFactory;
 	}
 
 }
