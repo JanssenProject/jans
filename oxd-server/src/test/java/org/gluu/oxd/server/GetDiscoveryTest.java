46cfab4048165ce2be3758a050fc8718d8c5b116
Arnab Dutta <arnab.bdutta@gmail.com>
2019-05-22 04:45:11 -0500

    #291 - Missed OP discovery info to implement OpenID Connect GG plugin

--- oxd-server/src/test/java/org/gluu/oxd/server/GetDiscoveryTest.java
+++ oxd-server/src/test/java/org/gluu/oxd/server/GetDiscoveryTest.java
@@ -1,24 +1,24 @@
 package org.gluu.oxd.server;
 
 import org.gluu.oxd.client.ClientInterface;
-import org.gluu.oxd.common.params.GetOpDiscoveryConfigParams;
-import org.gluu.oxd.common.response.GetOpDiscoveryConfigResponse;
+import org.gluu.oxd.common.params.GetDiscoveryParams;
+import org.gluu.oxd.common.response.GetDiscoveryResponse;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import static org.testng.AssertJUnit.assertNotNull;
 
-public class GetOpDiscoveryConfigTest {
+public class GetDiscoveryTest {
     @Parameters({"host", "opHost", "opDiscoveryPath"})
     @Test
     public void test(String host, String opHost, String opDiscoveryPath) {
         ClientInterface client = Tester.newClient(host);
 
-        final GetOpDiscoveryConfigParams commandParams = new GetOpDiscoveryConfigParams();
+        final GetDiscoveryParams commandParams = new GetDiscoveryParams();
         commandParams.setOpHost(opHost);
         commandParams.setOpDiscoveryPath(opDiscoveryPath);
 
-        final GetOpDiscoveryConfigResponse resp = client.getOpDiscoveryConfig(commandParams);
+        final GetDiscoveryResponse resp = client.getDiscovery(commandParams);
         assertNotNull(resp);
         assertNotNull(resp.getIssuer());
     }
