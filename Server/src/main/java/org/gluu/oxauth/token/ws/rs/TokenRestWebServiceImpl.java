34959cb9c2760671092dbf76374a603a5d31e42e
yurem <Yuriy.Movchan@gmail.com>
2019-05-29 13:14:57 +0300

    Add configurable backward compatibility method to return id_token in refresh_token grant

--- Server/src/main/java/org/gluu/oxauth/token/ws/rs/TokenRestWebServiceImpl.java
+++ Server/src/main/java/org/gluu/oxauth/token/ws/rs/TokenRestWebServiceImpl.java
@@ -242,12 +242,20 @@ public class TokenRestWebServiceImpl implements TokenRestWebService {
 
                         AccessToken accToken = authorizationGrant.createAccessToken(request.getHeader("X-ClientCert")); // create token after scopes are checked
 
+                        IdToken idToken = null;
+                        if (appConfiguration.getOpenidScopeBackwardCompatibility() && authorizationGrant.getScopes().contains("openid")) {
+                            boolean includeIdTokenClaims = Boolean.TRUE.equals(
+                                    appConfiguration.getLegacyIdTokenClaims());
+                            idToken = authorizationGrant.createIdToken(
+                                    null, null, accToken, authorizationGrant, includeIdTokenClaims, idTokenTokingBindingPreprocessing);
+                        }
+
                         builder.entity(getJSonResponse(accToken,
                                 accToken.getTokenType(),
                                 accToken.getExpiresIn(),
                                 reToken,
                                 scope,
-                                null));
+                                idToken));
                         oAuth2AuditLog.updateOAuth2AuditLog(authorizationGrant, true);
                     } else {
                         builder = error(401, TokenErrorResponseType.INVALID_GRANT, "Unable to find grant object by refresh token.");
