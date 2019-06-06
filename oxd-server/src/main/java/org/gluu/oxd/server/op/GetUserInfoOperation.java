d91c2dd9ca950f79d0cff28b7efadb4a0bad4c22
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2019-06-06 20:05:18 +0300

    switched /get-user-info to Jackson 2
    https://github.com/GluuFederation/oxd/issues/183

--- oxd-server/src/main/java/org/gluu/oxd/server/op/GetUserInfoOperation.java
+++ oxd-server/src/main/java/org/gluu/oxd/server/op/GetUserInfoOperation.java
@@ -1,15 +1,14 @@
 package org.gluu.oxd.server.op;
 
 import com.google.inject.Injector;
-import org.codehaus.jackson.JsonNode;
 import org.gluu.oxauth.client.UserInfoClient;
 import org.gluu.oxauth.client.UserInfoRequest;
 import org.gluu.oxauth.client.UserInfoResponse;
 import org.gluu.oxd.common.Command;
-import org.gluu.oxd.common.CoreUtils;
 import org.gluu.oxd.common.params.GetUserInfoParams;
 import org.gluu.oxd.common.response.IOpResponse;
 import org.gluu.oxd.common.response.POJOResponse;
+import org.gluu.oxd.server.Jackson2;
 
 import java.io.IOException;
 
@@ -40,6 +39,6 @@ public class GetUserInfoOperation extends BaseOperation<GetUserInfoParams> {
         client.setRequest(new UserInfoRequest(params.getAccessToken()));
 
         final UserInfoResponse response = client.exec();
-        return new POJOResponse(CoreUtils.createJsonMapper().readValue(response.getEntity(), JsonNode.class));
+        return new POJOResponse(Jackson2.createJsonMapper().readTree(response.getEntity()));
     }
 }
