b27154c38bf67ef588cecf7a8e27910b71cfcd49
Arnab Dutta <arnab.bdutta@gmail.com>
2019-06-16 22:32:48 +0530

    #272 - Improve log message if certificate is not imported

--- oxd-common/src/main/java/org/gluu/oxd/common/ErrorResponseCode.java
+++ oxd-common/src/main/java/org/gluu/oxd/common/ErrorResponseCode.java
@@ -72,7 +72,8 @@ public enum ErrorResponseCode {
     FAILED_TO_GET_RPT(500, "internal_error", "Failed to get RPT."),
     FAILED_TO_REMOVE_SITE(500, "remove_site_failed", "Failed to remove site."),
     REDIRECT_URI_IS_NOT_REGISTERED(400, "redirect_uri_is_not_registered", "The authorization redirect uri is not registered."),
-    FAILED_TO_GET_DISCOVERY(500, "failed_to_get_discovery", "Failed to get OP discovery configuration.");
+    FAILED_TO_GET_DISCOVERY(500, "failed_to_get_discovery", "Failed to get OP discovery configuration."),
+    SSL_HANDSHAKE_ERROR(500, "ssl_handshake_error", "Unable to find valid certification path to requested target. Please check if key_store_path in oxd configuration is correct.");
 
     private final int httpStatus;
     private final String code;
