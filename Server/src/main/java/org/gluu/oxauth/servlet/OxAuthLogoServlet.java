efea5a4fd7adc1238fa266d33ca63b248ee117fa
Gasmyr <thomas@gluu.org>
2019-06-18 09:00:44 +0100

    Unified logo across all oxAuth pages #1099

---
+++ Server/src/main/java/org/gluu/oxauth/servlet/OxAuthLogoServlet.java
@@ -0,0 +1,76 @@
+package org.gluu.oxauth.servlet;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.inject.Inject;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.gluu.oxauth.model.GluuOrganization;
+import org.gluu.oxauth.service.OrganizationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet(urlPatterns = "/servlet/logo")
+public class OxAuthLogoServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 5445488800130871634L;
+
+	private static final Logger log = LoggerFactory.getLogger(OxAuthLogoServlet.class);
+
+	public static final String BASE_OXAUTH_LOGO_PATH = "/opt/gluu/jetty/oxauth/custom/static/logo/";
+
+	@Inject
+	private OrganizationService organizationService;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+		response.setContentType("image/jpg");
+		GluuOrganization organization = organizationService.getOrganization();
+		boolean hasSucceed = readCustomLogo(response, organization);
+		if (!hasSucceed) {
+			readDefaultLogo(response);
+		}
+	}
+
+	private boolean readDefaultLogo(HttpServletResponse response) {
+		String defaultLogoFileName = "img/logo.png";
+		try (InputStream in = getServletContext().getResourceAsStream(defaultLogoFileName);
+				OutputStream out = response.getOutputStream()) {
+			IOUtils.copy(in, out);
+			return true;
+		} catch (IOException e) {
+			log.debug("Error loading default logo: " + e.getMessage());
+			return false;
+		}
+	}
+
+	private boolean readCustomLogo(HttpServletResponse response, GluuOrganization organization) {
+		if (organization.getOxTrustLogoPath() == null || StringUtils.isEmpty(organization.getOxTrustLogoPath())) {
+			return false;
+		}
+		File directory = new File(BASE_OXAUTH_LOGO_PATH);
+		if (!directory.exists()) {
+			directory.mkdir();
+		}
+		File logoPath = new File(BASE_OXAUTH_LOGO_PATH + organization.getOxTrustLogoPath());
+		if (!logoPath.exists()) {
+			return false;
+		}
+		try (InputStream in = new FileInputStream(logoPath); OutputStream out = response.getOutputStream()) {
+			IOUtils.copy(in, out);
+			return true;
+		} catch (IOException e) {
+			log.debug("Error loading custom logo: " + e.getMessage());
+			return false;
+		}
+	}
+}
