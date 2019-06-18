96a2640d24e994aa8b793eacab92d4de6b5d72bc
Gasmyr <thomas@gluu.org>
2019-06-18 09:00:44 +0100

    Unified logo across all oxAuth pages #1099

---
+++ Server/src/main/java/org/gluu/oxauth/service/OrganizationService.java
@@ -0,0 +1,64 @@
+package org.gluu.oxauth.service;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+//import org.gluu.config.oxtrust.AppConfiguration;
+import org.gluu.oxauth.model.GluuOrganization;
+import org.gluu.persist.PersistenceEntryManager;
+import org.gluu.service.CacheService;
+import org.gluu.util.OxConstants;
+
+@Stateless
+@Named("organizationService")
+public class OrganizationService extends org.gluu.service.OrganizationService {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8966940469789981584L;
+
+	@Inject
+	private PersistenceEntryManager ldapEntryManager;
+
+	@Inject
+	private CacheService cacheService;
+
+	/**
+	 * Update organization entry
+	 * 
+	 * @param organization
+	 *            Organization
+	 */
+	public void updateOrganization(GluuOrganization organization) {
+		ldapEntryManager.merge(organization);
+
+	}
+
+	/**
+	 * Check if LDAP server contains organization with specified attributes
+	 * 
+	 * @return True if organization with specified attributes exist
+	 */
+	public boolean containsOrganization(GluuOrganization organization) {
+		return ldapEntryManager.contains(organization);
+	}
+
+	public GluuOrganization getOrganization() {
+		String key = OxConstants.CACHE_ORGANIZATION_KEY;
+		GluuOrganization organization = (GluuOrganization) cacheService.get(key);
+		if (organization == null) {
+			String orgDn = getDnForOrganization();
+			organization = ldapEntryManager.find(GluuOrganization.class, orgDn);
+			cacheService.put(key, organization);
+		}
+
+		return organization;
+	}
+
+	public String getDnForOrganization() {
+		return "ou=gluu";
+	}
+
+}
