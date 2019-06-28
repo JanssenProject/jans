79a2724fa82eac628e2f7c1e2c6d4a1366947f21
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2019-06-28 10:57:33 +0300

    oxauth : added support to transfer claims into accessToken JWT from introspection script response
    https://github.com/GluuFederation/oxAuth/issues/1098

--- Server/src/main/java/org/gluu/oxauth/service/external/context/ExternalIntrospectionContext.java
+++ Server/src/main/java/org/gluu/oxauth/service/external/context/ExternalIntrospectionContext.java
@@ -2,7 +2,9 @@ package org.gluu.oxauth.service.external.context;
 
 import org.gluu.model.custom.script.conf.CustomScriptConfiguration;
 import org.gluu.oxauth.model.common.AuthorizationGrant;
+import org.gluu.oxauth.model.common.User;
 import org.gluu.oxauth.model.configuration.AppConfiguration;
+import org.gluu.oxauth.model.jwt.Jwt;
 import org.gluu.oxauth.service.AttributeService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +20,9 @@ public class ExternalIntrospectionContext extends ExternalScriptContext {
     private final AttributeService attributeService;
 
     private CustomScriptConfiguration script;
+    private Jwt accessTokenAsJwt;
+    private boolean tranferIntrospectionPropertiesIntoJwtClaims = true;
+    private AuthorizationGrant grantOfIntrospectionToken;
 
     public ExternalIntrospectionContext(AuthorizationGrant tokenGrant, HttpServletRequest httpRequest, HttpServletResponse httpResponse,
                                         AppConfiguration appConfiguration, AttributeService attributeService) {
@@ -45,5 +50,33 @@ public class ExternalIntrospectionContext extends ExternalScriptContext {
 
     public void setScript(CustomScriptConfiguration script) {
         this.script = script;
+    }
+
+    public Jwt getAccessTokenAsJwt() {
+        return accessTokenAsJwt;
+    }
+
+    public void setAccessTokenAsJwt(Jwt accessTokenAsJwt) {
+        this.accessTokenAsJwt = accessTokenAsJwt;
+    }
+
+    public boolean isTranferIntrospectionPropertiesIntoJwtClaims() {
+        return tranferIntrospectionPropertiesIntoJwtClaims;
+    }
+
+    public void setTranferIntrospectionPropertiesIntoJwtClaims(boolean tranferIntrospectionPropertiesIntoJwtClaims) {
+        this.tranferIntrospectionPropertiesIntoJwtClaims = tranferIntrospectionPropertiesIntoJwtClaims;
+    }
+
+    public AuthorizationGrant getGrantOfIntrospectionToken() {
+        return grantOfIntrospectionToken;
+    }
+
+    public void setGrantOfIntrospectionToken(AuthorizationGrant grantOfIntrospectionToken) {
+        this.grantOfIntrospectionToken = grantOfIntrospectionToken;
+    }
+
+    public User getUser() {
+        return grantOfIntrospectionToken != null ? grantOfIntrospectionToken.getUser() : null;
     }
 }
