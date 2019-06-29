424fcd039b35570d4b98c38383a1e7068b8b6888
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2019-06-29 19:26:37 +0300

    Revert "Revert "changed protected->public for remove(key) method (needed for cleaner)""
    This reverts commit cdb21d6

--- persistence-couchbase/src/main/java/org/gluu/persist/couchbase/impl/CouchbaseEntryManager.java
+++ persistence-couchbase/src/main/java/org/gluu/persist/couchbase/impl/CouchbaseEntryManager.java
@@ -7,20 +7,13 @@
 
 package org.gluu.persist.couchbase.impl;
 
-import java.io.Serializable;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.function.Function;
-
+import com.couchbase.client.core.message.kv.subdoc.multi.Mutation;
+import com.couchbase.client.java.document.json.JsonArray;
+import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.query.consistency.ScanConsistency;
+import com.couchbase.client.java.query.dsl.Expression;
+import com.couchbase.client.java.query.dsl.Sort;
+import com.couchbase.client.java.subdoc.MutationSpec;
 import org.gluu.persist.couchbase.model.ConvertedExpression;
 import org.gluu.persist.couchbase.model.SearchReturnDataType;
 import org.gluu.persist.couchbase.operation.CouchbaseOperationService;
@@ -34,14 +27,8 @@ import org.gluu.persist.exception.operation.SearchException;
 import org.gluu.persist.impl.BaseEntryManager;
 import org.gluu.persist.key.impl.GenericKeyConverter;
 import org.gluu.persist.key.impl.model.ParsedKey;
-import org.gluu.persist.model.AttributeData;
-import org.gluu.persist.model.AttributeDataModification;
+import org.gluu.persist.model.*;
 import org.gluu.persist.model.AttributeDataModification.AttributeModificationType;
-import org.gluu.persist.model.BatchOperation;
-import org.gluu.persist.model.DefaultBatchOperation;
-import org.gluu.persist.model.PagedResult;
-import org.gluu.persist.model.SearchScope;
-import org.gluu.persist.model.SortOrder;
 import org.gluu.persist.reflect.property.PropertyAnnotation;
 import org.gluu.search.filter.Filter;
 import org.gluu.util.ArrayHelper;
@@ -49,13 +36,12 @@ import org.gluu.util.StringHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.couchbase.client.core.message.kv.subdoc.multi.Mutation;
-import com.couchbase.client.java.document.json.JsonArray;
-import com.couchbase.client.java.document.json.JsonObject;
-import com.couchbase.client.java.query.consistency.ScanConsistency;
-import com.couchbase.client.java.query.dsl.Expression;
-import com.couchbase.client.java.query.dsl.Sort;
-import com.couchbase.client.java.subdoc.MutationSpec;
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.function.Function;
 
 /**
  * Couchbase Entry Manager
@@ -259,7 +245,7 @@ public class CouchbaseEntryManager extends BaseEntryManager implements Serializa
     }
 
     @Override
-    protected void remove(String dn) {
+    public void remove(String dn) {
         // Remove entry
         try {
             for (DeleteNotifier subscriber : subscribers) {
