79b5c988bcd3afb571d88c4611e27a7f33cdea55
yurem <Yuriy.Movchan@gmail.com>
2019-08-15 10:56:52 +0300

    Add log message about request to demo endpoint

--- demo-cdi/src/main/java/org/gluu/demo/rest/DemoEndpoint.java
+++ demo-cdi/src/main/java/org/gluu/demo/rest/DemoEndpoint.java
@@ -1,15 +1,22 @@
 package org.gluu.demo.rest;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
+
 @Path("/demo")
 public class DemoEndpoint {
+
+	@Inject
+	private Logger log;
 
 	@GET
 	@Path("/status")
 	public Response listClients() {
+		log.info("Get request to demo endpoint");
 		return Response.ok("success").build();
 	}
 
