e008302690a163b5e243d41dcba1256b3cf2bc69
yurem <Yuriy.Movchan@gmail.com>
2019-08-17 23:12:39 +0300

    Add sample to show how to outject map

--- demo-cdi/src/main/java/org/gluu/demo/cdi/DemoApplicationService.java
+++ demo-cdi/src/main/java/org/gluu/demo/cdi/DemoApplicationService.java
@@ -1,9 +1,12 @@
 package org.gluu.demo.cdi;
 
+import java.util.HashMap;
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Initialized;
 import javax.enterprise.event.Observes;
+import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -27,6 +30,20 @@ public class DemoApplicationService {
 
 	public void applicationInitialized(@Observes @Initialized(ApplicationScoped.class) Object init) {
 		log.info("Initializing Demo application service");
+	}
+	
+	@Produces
+	@ApplicationScoped
+	@Named("ext_msgs")
+	public HashMap<String, String> buildExternalMessagesMap() {
+		HashMap<String, String> result = new HashMap<String, String>();
+		
+		// It's for demo to show how to outject Map
+		result.put("dummy_message_1", "Dummy message 1");
+		result.put("dummy_message_2", "Dummy message 2");
+		result.put("dummy_message_3", "Dummy message 3");
+		
+		return result;
 	}
 
 }
