6ece17168629cacd36d32a8730fcb540095fd529
gasmyr <gasmyrmougang@yahoo.fr>
2019-09-04 20:19:32 +0100

    Update ConfirmRegistrationSampleScript.py
    Update registration confirmation script

--- static/extension/user_registration/ConfirmRegistrationSampleScript.py
+++ static/extension/user_registration/ConfirmRegistrationSampleScript.py
@@ -19,14 +19,14 @@ class UserRegistration(UserRegistrationType):
         self.currentTimeMillis = currentTimeMillis
 
     def init(self, configurationAttributes):
-        print "User registration. Initialization"
-        print "User registration. Initialized successfully"
+        print "User Confirm registration. Initialization"
+        print "User Confirm registration. Initialized successfully"
 
         return True   
 
     def destroy(self, configurationAttributes):
-        print "User registration. Destroy"
-        print "User registration. Destroyed successfully"
+        print "User Confirm registration. Destroy"
+        print "User Confirm registration. Destroyed successfully"
         return True   
 
     # User registration init method
@@ -34,7 +34,7 @@ class UserRegistration(UserRegistrationType):
     #   requestParameters is java.util.Map<String, String[]>
     #   configurationAttributes is java.util.Map<String, SimpleCustomProperty>
     def initRegistration(self, user, requestParameters, configurationAttributes):
-        print "User registration. Init method"
+        print "User Confirm registration. Init method"
 
         return True
 
@@ -43,7 +43,7 @@ class UserRegistration(UserRegistrationType):
     #   requestParameters is java.util.Map<String, String[]>
     #   configurationAttributes is java.util.Map<String, SimpleCustomProperty>
     def preRegistration(self, user, requestParameters, configurationAttributes):
-        print "User registration. Pre method"
+        print "User Confirm registration. Pre method"
         
         userStatus = GluuStatus.INACTIVE
 
@@ -58,53 +58,54 @@ class UserRegistration(UserRegistrationType):
     #   requestParameters is java.util.Map<String, String[]>
     #   configurationAttributes is java.util.Map<String, SimpleCustomProperty>
     def postRegistration(self, user, requestParameters, configurationAttributes):
-        print "User registration. Post method"
+        print "User Confirm registration. Post method"
         appConfiguration = CdiUtil.bean(AppConfiguration)
-
-        hostName = appConfiguration.getApplianceUrl()
+        try:
+            hostName = appConfiguration.getApplianceUrl()
+        except:
+            hostName= requestParameters.get("hostName")[0];
+            
+        print "HostName: %s" % hostName   
         externalContext = CdiUtil.bean(ExternalContext)
         contextPath = externalContext.getRequest().getContextPath() 
-
         mailService = CdiUtil.bean(MailService)
         subject = "Registration confirmation"
-       
         activationLink = "%s%s/confirm/registration?code=%s" %(hostName, contextPath, self.guid)
         body = "<h2 style='margin-left:10%%;color: #337ab7;'>Welcome</h2><hr style='width:80%%;border: 1px solid #337ab7;'></hr><div style='text-align:center;'><p>Dear <span style='color: #337ab7;'>%s</span>,</p><p>Your Account has been created, welcome to <span style='color: #337ab7;'>%s</span>.</p><p>You are just one step way from activating your account on <span style='color: #337ab7;'>%s</span>.</p><p>Click the button and start using your account.</p></div><a class='btn' href='%s'><button style='background: #337ab7; color: white; margin-left: 30%%; border-radius: 5px; border: 0px; padding: 5px;' type='button'>Activate your account now!</button></a>"  % (user.getUid(), hostName, hostName, activationLink)
-
-        print "User registration. Post method. Attempting to send e-mail to '%s' message '%s'" % (user.getMail(), body)
+        print "User Confirm registration. Post method. Attempting to send e-mail to '%s' message '%s'" % (user.getMail(), body)
         mailService.sendMail(user.getMail(), None, subject, body, body);
         return True
 
     def confirmRegistration(self, user, requestParameters, configurationAttributes):
-        print "User registration. Confirm method"
+        print "User Confirm registration. Confirm method"
 
         code_array = requestParameters.get("code")
         if ArrayHelper.isEmpty(code_array):
-            print "User registration. Confirm method. code is empty"
+            print "User Confirm registration. Confirm method. code is empty"
             return False
 
         confirmation_code = code_array[0]
-        print "User registration. Confirm method. code: '%s'" % confirmation_code
+        print "User Confirm registration. Confirm method. code: '%s'" % confirmation_code
 
         if confirmation_code == None:
-            print "User registration. Confirm method. Confirmation code not exist in request"
+            print "User Confirm registration. Confirm method. Confirmation code not exist in request"
             return False
 
         personService = CdiUtil.bean(PersonService)
         user = personService.getPersonByAttribute("oxGuid", confirmation_code)
         if user == None:
-            print "User registration. Confirm method. There is no user by confirmation code: '%s'" % confirmation_code
+            print "User Confirm registration. Confirm method. There is no user by confirmation code: '%s'" % confirmation_code
             return False
 
         if confirmation_code == user.getGuid():
             user.setStatus(GluuStatus.ACTIVE)
             user.setGuid("")
             personService.updatePerson(user)
-            print "User registration. Confirm method. User '%s' confirmed his registration" % user.getUid()
+            print "User Confirm registration. Confirm method. User '%s' confirmed his registration" % user.getUid()
             return True
 
-        print "User registration. Confirm method. Confirmation code for user '%s' is invalid" % user.getUid()
-    	return False
+        print "User Confirm registration. Confirm method. Confirmation code for user '%s' is invalid" % user.getUid()
+        return False
 
     def getApiVersion(self):
         return 1
