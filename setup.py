9551f93c9e2df006c08b052df68cf8da20150fd5
Mustafa Baser <mbaser@mail.com>
2019-09-05 18:56:57 +0300

    implement cocuhbase static indexes

--- setup.py
+++ setup.py
@@ -4094,7 +4094,15 @@ class Setup(object):
                 W.write('CREATE INDEX %s ON `%s`(%s) USING GSI WITH {"defer_build":true};\n' % (index_name, bucket, ind))
                 index_names.append(index_name)
 
-            W.write('BUILD INDEX ON `%s` (%s) USING GSI;\n' % (bucket, ', '.join(index_names)))
+            if index_names:
+                W.write('BUILD INDEX ON `%s` (%s) USING GSI;\n' % (bucket, ', '.join(index_names)))
+
+            sic = 1
+            for attribs, wherec in index_list['static']:
+                attrquoted = ['`{}`'.format(a) for a in attribs]
+                attrquoteds = ', '.join(attrquoted)
+                query = 'CREATE INDEX `{0}_static_{1:02d}` ON `{0}`({2}) WHERE ({3})\n'.format(bucket, sic, attrquoteds, wherec)
+                W.write(query)
 
         self.couchbaseExecQuery(tmp_file)
 
