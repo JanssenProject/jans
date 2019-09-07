1d12bad11055f878f0cb1d5dda5eea83f2be6cf5
Arnab Dutta <arnab.bdutta@gmail.com>
2019-09-07 23:05:14 +0530

    #360 - Create stress/load test which should cover all APIs with mocked OP

--- oxd-server/src/main/java/org/gluu/oxd/server/op/OpClientFactoryImpl.java
+++ oxd-server/src/main/java/org/gluu/oxd/server/op/OpClientFactoryImpl.java
@@ -2,7 +2,14 @@ package org.gluu.oxd.server.op;
 
 import org.gluu.oxauth.client.*;
 import org.gluu.oxauth.model.jwt.Jwt;
+import org.gluu.oxd.rs.protect.resteasy.PatProvider;
+import org.gluu.oxd.rs.protect.resteasy.ResourceRegistrar;
+import org.gluu.oxd.rs.protect.resteasy.RptPreProcessInterceptor;
+import org.gluu.oxd.rs.protect.resteasy.ServiceProvider;
+import org.gluu.oxd.server.introspection.ClientFactory;
 import org.gluu.oxd.server.service.PublicOpKeyService;
+import org.jboss.resteasy.client.ClientExecutor;
+import org.jboss.resteasy.client.ClientRequest;
 
 public class OpClientFactoryImpl implements OpClientFactory {
 
@@ -10,6 +17,10 @@ public class OpClientFactoryImpl implements OpClientFactory {
     }
 
     public TokenClient createTokenClient(String url) {
+        return new TokenClient(url);
+    }
+
+    public TokenClient createTokenClientWithUmaProtectionScope(String url) {
         return new TokenClient(url);
     }
 
@@ -29,8 +40,24 @@ public class OpClientFactoryImpl implements OpClientFactory {
         return new AuthorizeClient(url);
     }
 
+    public ResourceRegistrar createResourceRegistrar(PatProvider patProvider, ServiceProvider serviceProvider) {
+        return new ResourceRegistrar(patProvider, serviceProvider);
+    }
+
+    public ClientFactory createClientFactory() {
+        return ClientFactory.instance();
+    }
+
     public Validator createValidator(Jwt idToken, OpenIdConfigurationResponse discoveryResponse, PublicOpKeyService keyService) {
         return new Validator(idToken, discoveryResponse, keyService);
+    }
+
+    public RptPreProcessInterceptor createRptPreProcessInterceptor(ResourceRegistrar resourceRegistrar) {
+        return new RptPreProcessInterceptor(resourceRegistrar);
+    }
+
+    public ClientRequest createClientRequest(String uriTemplate, ClientExecutor executor) throws Exception {
+        return new ClientRequest(uriTemplate, executor);
     }
 
 }
