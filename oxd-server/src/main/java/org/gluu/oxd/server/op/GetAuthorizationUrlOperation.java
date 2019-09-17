b49616aa066c7c33cc09ef35b0df48995b195bb2
Arnab Dutta <arnab.bdutta@gmail.com>
2019-09-17 17:48:53 +0530

    #114 - Hybrid flow : add ability to set response_type directly during authorization url request

--- oxd-server/src/main/java/org/gluu/oxd/server/op/GetAuthorizationUrlOperation.java
+++ oxd-server/src/main/java/org/gluu/oxd/server/op/GetAuthorizationUrlOperation.java
@@ -4,18 +4,19 @@ import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.google.inject.Injector;
 import org.apache.commons.lang.StringUtils;
-import org.gluu.oxd.common.ErrorResponseCode;
-import org.gluu.oxd.server.HttpException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.gluu.oxauth.model.authorize.AuthorizeRequestParam;
 import org.gluu.oxauth.model.util.Util;
 import org.gluu.oxd.common.Command;
+import org.gluu.oxd.common.ErrorResponseCode;
 import org.gluu.oxd.common.params.GetAuthorizationUrlParams;
+import org.gluu.oxd.common.params.UpdateSiteParams;
 import org.gluu.oxd.common.response.GetAuthorizationUrlResponse;
 import org.gluu.oxd.common.response.IOpResponse;
+import org.gluu.oxd.server.HttpException;
 import org.gluu.oxd.server.Utils;
 import org.gluu.oxd.server.service.Rp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -59,6 +60,11 @@ public class GetAuthorizationUrlOperation extends BaseOperation<GetAuthorization
             throw new HttpException(ErrorResponseCode.REDIRECT_URI_IS_NOT_REGISTERED);
         }
 
+        if (params.getResponseTypes() != null && !params.getResponseTypes().isEmpty()) {
+            site.setResponseTypes(params.getResponseTypes());
+            persistRp(site, createUpdateSiteParamsObject(params));
+        }
+
         String state = StringUtils.isNotBlank(params.getState()) ? getStateService().putState(Utils.encode(params.getState())) : getStateService().generateState();
         String redirectUri = StringUtils.isNotBlank(params.getRedirectUri()) ? params.getRedirectUri() : site.getRedirectUri();
 
@@ -100,5 +106,25 @@ public class GetAuthorizationUrlOperation extends BaseOperation<GetAuthorization
             LOG.error("acr value is null for site: " + site);
             return new ArrayList<>();
         }
+    }
+
+    private void persistRp(Rp rp, UpdateSiteParams params) {
+
+        try {
+            getUpdateRegisteredClientService().updateRegisteredClient(rp, params);
+            getRpService().update(rp);
+
+            LOG.info("RP updated: " + rp);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to persist RP, params: " + params, e);
+        }
+    }
+
+    private UpdateSiteParams createUpdateSiteParamsObject(GetAuthorizationUrlParams params) {
+        UpdateSiteParams updateSiteParams = new UpdateSiteParams();
+        updateSiteParams.setOxdId(params.getOxdId());
+        updateSiteParams.setResponseTypes(params.getResponseTypes());
+
+        return updateSiteParams;
     }
 }
