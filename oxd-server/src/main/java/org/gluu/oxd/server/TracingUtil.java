845cab6c1dd755459a3434b970cdc4e5109a67a6
Arnab Dutta <arnab.bdutta@gmail.com>
2019-10-29 18:25:43 +0530

    #182 - Add tracing metrics to oxd server

--- oxd-server/src/main/java/org/gluu/oxd/server/TracingUtil.java
+++ oxd-server/src/main/java/org/gluu/oxd/server/TracingUtil.java
@@ -19,30 +19,45 @@ import zipkin2.reporter.okhttp3.OkHttpSender;
 
 public class TracingUtil {
 
+    private TracingUtil() {
+    }
+
     public static boolean configureGlobalTracer(OxdServerConfiguration configuration, String componentName) {
+        GlobalTracer.register(createTracer(configuration, componentName));
+        return true;
+    }
+
+    private static Tracer createTracer(OxdServerConfiguration configuration, String componentName) {
         String tracerName = configuration.getTracer();
         Tracer tracer = null;
+
         if (!configuration.getEnableTracing() || Strings.isNullOrEmpty(tracerName)) {
             tracer = NoopTracerFactory.create();
         } else if ("jaeger".equals(tracerName)) {
             Configuration.SamplerConfiguration samplerConfig = new Configuration.SamplerConfiguration()
                     .withType(ConstSampler.TYPE)
                     .withParam(1);
+
             Configuration.SenderConfiguration senderConfig = new Configuration.SenderConfiguration()
                     .withAgentHost(configuration.getTracerHost())
                     .withAgentPort(configuration.getTracerPort());
+
             Configuration.ReporterConfiguration reporterConfig = new Configuration.ReporterConfiguration()
                     .withLogSpans(true)
                     .withFlushInterval(1000)
                     .withMaxQueueSize(10000)
                     .withSender(senderConfig);
-            tracer = new Configuration(componentName).withSampler(samplerConfig).withReporter(reporterConfig).getTracer();
+
+            tracer = new Configuration(componentName)
+                    .withSampler(samplerConfig)
+                    .withReporter(reporterConfig)
+                    .getTracer();
         } else if ("zipkin".equals(tracerName)) {
             OkHttpSender sender = OkHttpSender.create(
-                    "http://" +
-                            configuration.getTracerHost() + ":" +
-                            configuration.getTracerPort() + "/api/v1/spans");
+                    "http://" + configuration.getTracerHost() + ":" + configuration.getTracerPort() + "/api/v1/spans");
+
             Reporter<Span> reporter = AsyncReporter.builder(sender).build();
+
             tracer = BraveTracer.create(Tracing.newBuilder()
                     .localServiceName(componentName)
                     .spanReporter(reporter)
@@ -50,8 +65,8 @@ public class TracingUtil {
         } else {
             tracer = NoopTracerFactory.create();
         }
-        GlobalTracer.register(tracer);
-        return true;
+
+        return tracer;
     }
 
     private static Tracer getGlobalTracer() {
@@ -71,26 +86,26 @@ public class TracingUtil {
     }
 
     public static void setTag(String tagName, String tagValue) {
-        if(getGlobalTracer().activeSpan() != null) {
+        if (getGlobalTracer().activeSpan() != null) {
             getGlobalTracer().activeSpan().setTag(tagName, tagValue);
         }
     }
 
     public static void log(String message) {
-        if(getActiveSpan() != null) {
+        if (getActiveSpan() != null) {
             getActiveSpan().log(message);
         }
     }
 
     public static void errorLog(Exception e) {
-        if(getActiveSpan() != null) {
+        if (getActiveSpan() != null) {
             Tags.ERROR.set(getActiveSpan(), true);
             getActiveSpan().log("Error: " + e.getMessage());
         }
     }
 
     public static void errorLog(String message) {
-        if(getActiveSpan() != null) {
+        if (getActiveSpan() != null) {
             Tags.ERROR.set(getActiveSpan(), true);
             getActiveSpan().log("Error: " + message);
         }
