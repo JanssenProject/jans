a0bbd00b6df0222c3e26ae66928f737605c2433b
Milton BO <jmcm578@gmail.com>
2019-11-20 09:42:13 -0400

    sub claim is absent from id_token and userinfo response when certain attributes are used as source for it. Changed value for uid configuration. #1200

--- Server/src/main/java/org/gluu/oxauth/userinfo/ws/rs/UserInfoRestWebServiceImpl.java
+++ Server/src/main/java/org/gluu/oxauth/userinfo/ws/rs/UserInfoRestWebServiceImpl.java
@@ -76,6 +76,7 @@ import org.gluu.oxauth.service.external.context.DynamicScopeExternalContext;
 import org.gluu.oxauth.util.ServerUtil;
 import org.gluu.persist.PersistenceEntryManager;
 import org.gluu.persist.exception.EntryPersistenceException;
+import org.gluu.util.StringHelper;
 import org.gluu.util.security.StringEncrypter;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -479,11 +480,11 @@ public class UserInfoRestWebServiceImpl implements UserInfoRestWebService {
                 log.warn("Unable to calculate the pairwise subject identifier because the client hasn't a redirect uri. A public subject identifier will be used instead.");
             }
             String openidSubAttribute = appConfiguration.getOpenidSubAttribute();
-            String sub = authorizationGrant.getUser().getAttribute(openidSubAttribute);
-            if (sub != null && !sub.isEmpty())
-                jsonWebResponse.getClaims().setSubjectIdentifier(sub);
-            else
-                jsonWebResponse.getClaims().setSubjectIdentifier(authorizationGrant.getUser().getAttribute("inum"));
+            String subValue = authorizationGrant.getUser().getAttribute(openidSubAttribute);
+            if (StringHelper.equalsIgnoreCase(openidSubAttribute, "uid")) {
+                subValue = authorizationGrant.getUser().getUserId();
+            }
+            jsonWebResponse.getClaims().setSubjectIdentifier(subValue);
         }
 
         if ((dynamicScopes.size() > 0) && externalDynamicScopeService.isEnabled()) {
