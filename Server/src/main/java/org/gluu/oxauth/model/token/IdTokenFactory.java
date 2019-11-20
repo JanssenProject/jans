9fd58148e0e9fbd0f4a816fa2110d9aad0610bbe
miltonbo <32613743+miltonbo@users.noreply.github.com>
2019-11-20 10:13:02 -0400

    oxAuth#1200 (#1204)
    * sub claim is absent from id_token and userinfo response when certain attributes are used as source for it #1200
    
    * Restore test configuration #1200
    
    * sub claim is absent from id_token and userinfo response when certain attributes are used as source for it. Changed value for uid configuration. #1200

--- Server/src/main/java/org/gluu/oxauth/model/token/IdTokenFactory.java
+++ Server/src/main/java/org/gluu/oxauth/model/token/IdTokenFactory.java
@@ -65,6 +65,7 @@ import org.gluu.oxauth.service.ScopeService;
 import org.gluu.oxauth.service.external.ExternalAuthenticationService;
 import org.gluu.oxauth.service.external.ExternalDynamicScopeService;
 import org.gluu.oxauth.service.external.context.DynamicScopeExternalContext;
+import org.gluu.util.StringHelper;
 import org.gluu.util.security.StringEncrypter;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -274,9 +275,12 @@ public class IdTokenFactory {
             if (authorizationGrant.getClient().getSubjectType() != null && SubjectType.fromString(authorizationGrant.getClient().getSubjectType()).equals(SubjectType.PAIRWISE)) {
                 log.warn("Unable to calculate the pairwise subject identifier because the client hasn't a redirect uri. A public subject identifier will be used instead.");
             }
-
             String openidSubAttribute = appConfiguration.getOpenidSubAttribute();
-            jwt.getClaims().setSubjectIdentifier(authorizationGrant.getUser().getAttribute(openidSubAttribute));
+            String subValue = authorizationGrant.getUser().getAttribute(openidSubAttribute);
+            if (StringHelper.equalsIgnoreCase(openidSubAttribute, "uid")) {
+                subValue = authorizationGrant.getUser().getUserId();
+            }
+            jwt.getClaims().setSubjectIdentifier(subValue);
         }
 
         if ((dynamicScopes.size() > 0) && externalDynamicScopeService.isEnabled()) {
@@ -473,9 +477,12 @@ public class IdTokenFactory {
             if (authorizationGrant.getClient().getSubjectType() != null && SubjectType.fromString(authorizationGrant.getClient().getSubjectType()).equals(SubjectType.PAIRWISE)) {
                 log.warn("Unable to calculate the pairwise subject identifier because the client hasn't a redirect uri. A public subject identifier will be used instead.");
             }
-
             String openidSubAttribute = appConfiguration.getOpenidSubAttribute();
-            jwe.getClaims().setSubjectIdentifier(authorizationGrant.getUser().getAttribute(openidSubAttribute));
+            String subValue = authorizationGrant.getUser().getAttribute(openidSubAttribute);
+            if (StringHelper.equalsIgnoreCase(openidSubAttribute, "uid")) {
+                subValue = authorizationGrant.getUser().getUserId();
+            }
+            jwe.getClaims().setSubjectIdentifier(subValue);
         }
 
         if ((dynamicScopes.size() > 0) && externalDynamicScopeService.isEnabled()) {
