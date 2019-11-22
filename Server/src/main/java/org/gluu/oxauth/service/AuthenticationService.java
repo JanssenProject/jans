ec4c979e55c9dee31b6e9b3401547d51b240476b
yurem <Yuriy.Movchan@gmail.com>
2019-11-22 22:35:31 +0300

    Fix session loggin

--- Server/src/main/java/org/gluu/oxauth/service/AuthenticationService.java
+++ Server/src/main/java/org/gluu/oxauth/service/AuthenticationService.java
@@ -480,13 +480,13 @@ public class AuthenticationService {
 
 		User user = getAuthenticatedUser();
 
-		SessionId newSessionId;
-		if (sessionId == null) {
-			newSessionId = sessionIdService.generateAuthenticatedSessionId(getHttpRequest(), user.getDn(), sessionIdAttributes);
-		}
+		String sessionAuthUser = sessionIdAttributes.get(Constants.AUTHENTICATED_USER);
+		log.trace("configureSessionUser sessionId: '{}', sessionId.auth_user: '{}'", sessionId, sessionAuthUser);
+
+		SessionId newSessionId = sessionIdService.setSessionIdStateAuthenticated(getHttpRequest(), sessionId, user.getDn());
 
 		identity.setSessionId(sessionId);
-        newSessionId.setUser(user);
+	        newSessionId.setUser(user);
 
 		return newSessionId;
 	}
