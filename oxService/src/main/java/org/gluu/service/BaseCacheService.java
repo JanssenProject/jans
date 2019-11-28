ff81cce2d8f1599ba78ce6bce693b15c011d5e6f
yurem <Yuriy.Movchan@gmail.com>
2019-11-28 20:13:58 +0300

    Add trace messages to base cache

--- oxService/src/main/java/org/gluu/service/BaseCacheService.java
+++ oxService/src/main/java/org/gluu/service/BaseCacheService.java
@@ -31,10 +31,15 @@ public abstract class BaseCacheService implements CacheInterface {
     public Object get(String key) {
     	CacheProvider cacheProvider = getCacheProvider();
         if (cacheProvider == null) {
+        	log.error("Cache provider is invalid!");
             return null;
         }
 
-        return cacheProvider.get(key);
+    	log.trace("Request data, key '{}'", key);
+    	Object value = cacheProvider.get(key);
+    	log.trace("Loaded data, key '{}': '{}'", key, value);
+
+    	return value;
     }
 
     public <T> T getWithPut(String key, Supplier<T> loadFunction, int expirationInSeconds) {
@@ -45,23 +50,26 @@ public abstract class BaseCacheService implements CacheInterface {
     	CacheProvider cacheProvider = getCacheProvider();
 
     	if (CacheProviderType.NATIVE_PERSISTENCE == cacheProvider.getProviderType()) {
+        	log.trace("Loading data from DB without cache, key '{}'", key);
             return loadFunction.get();
         }
 
         final Object value = get(key);
         if (value != null) {
-            log.trace("Loaded from cache, key: " + key);
+            log.trace("Loaded from cache, key: '{}'", key);
             return (T) value;
         } else {
+            log.trace("Key not in cache. Searching value via load function, key: '{}'", key);
             final T loaded = loadFunction.get();
             if (loaded == null) {
+                log.trace("Key not in cache. There is no value, key: '{}'", key);
                 return null;
             }
 
             try {
                 put(expirationInSeconds, key, loaded);
             } catch (Exception e) {
-                log.error("Failed to put object into cache, key: " + key, e); // we don't want prevent returning loaded value due to failure with put
+                log.error("Failed to put object into cache, key: '{}'", key, e); // we don't want prevent returning loaded value due to failure with put
             }
             return loaded;
         }
@@ -69,37 +77,47 @@ public abstract class BaseCacheService implements CacheInterface {
 
 	public void put(int expirationInSeconds, String key, Object object) {
     	CacheProvider cacheProvider = getCacheProvider();
-
-    	if (cacheProvider != null) {
-			cacheProvider.put(expirationInSeconds, key, object);
+    	if (cacheProvider == null) {
+        	log.error("Cache provider is invalid!");
+			return;
 		}
+		
+    	log.trace("Put data, key '{}': '{}'", key, object);
+		cacheProvider.put(expirationInSeconds, key, object);
 	}
 
 	public void remove(String key) {
     	CacheProvider cacheProvider = getCacheProvider();
-
     	if (cacheProvider == null) {
+        	log.error("Cache provider is invalid!");
 			return;
 		}
 		
+    	log.trace("Remove data, key '{}'", key);
 		cacheProvider.remove(key);
 	}
 
 	public void clear() {
     	CacheProvider cacheProvider = getCacheProvider();
-
-    	if (cacheProvider != null) {
-			cacheProvider.clear();
+    	if (cacheProvider == null) {
+        	log.error("Cache provider is invalid!");
+			return;
 		}
+
+    	log.trace("Clear cache");
+		cacheProvider.clear();
 	}
 
     @Override
     public void cleanup(Date now) {
     	CacheProvider cacheProvider = getCacheProvider();
+    	if (cacheProvider == null) {
+        	log.error("Cache provider is invalid!");
+			return;
+		}
 
-    	if (cacheProvider != null) {
-            cacheProvider.cleanup(now);
-        }
+    	log.trace("Clean up cache");
+        cacheProvider.cleanup(now);
     }
 
     public void put(String key, Object object) {
