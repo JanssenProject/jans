8934c83e575607e5e612a645ea082e6db518aace
Mustafa Baser <mbaser@mail.com>
2019-12-09 17:11:44 +0300

    restart idendiy afte installing oxtrust api

--- oxtrustapi_setup.py
+++ oxtrustapi_setup.py
@@ -3,6 +3,7 @@ import ldap
 import xml.etree.ElementTree as ET
 import shutil
 import glob
+from setup import Setup
 
 ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_ALLOW)
 
@@ -30,8 +31,8 @@ oxtrust_api_server_version = '4.0.rc2'
 oxtrust_api_server_url = 'https://ox.gluu.org/maven/org/gluu/oxtrust-api-server/{0}/oxtrust-api-server-{0}.jar'.format(oxtrust_api_server_version)
 oxtrust_api_server_path = '/opt/gluu/jetty/identity/custom/libs/oxtrust-api-server.jar'
 
-
-os.system('wget {} -O {}'.format(oxtrust_api_server_url, oxtrust_api_server_path))
+print "Downloading oxtrust-api-server-{}.jar".format(oxtrust_api_server_version)
+os.system('wget -nv {} -O {}'.format(oxtrust_api_server_url, oxtrust_api_server_path))
 os.system('chown jetty:jetty {}'.format(oxtrust_api_server_path))
 
 for child in root:
@@ -39,6 +40,8 @@ for child in root:
         if 'oxtrust-api-server.jar' in child.text:
             break
 else:
+    print "Adding oxtrust-api-server-{}.jar to identity extraClasspath".format(oxtrust_api_server_version)
+    
     with open(identtiy_xml_fn) as f:
         identtiy_xml = f.readlines()
 
@@ -60,13 +63,19 @@ else:
         shutil.copyfile(identtiy_xml_fn+'~', identtiy_xml_fn)
         os.remove(identtiy_xml_fn+'~')
 
-
-ldap_conn = ldap.initialize('ldaps://{0}:1636'.format(ldap_server))
+# Enable custom script oxtrust_api_access_policy
+ldap_conn = ldap.initialize('ldaps://{}'.format(ldap_server))
 ldap_conn.simple_bind_s(ldap_binddn, ldap_password)
 
 basedn = 'inum=OO11-BAFE,ou=scripts,o=gluu'
 result = ldap_conn.search_s(basedn, ldap.SCOPE_BASE,  attrlist=['oxEnabled'])
 
 if result and result[0][1]['oxEnabled'][0].lower() != 'true':
+    print "Enabling custom script oxtrust_api_access_policy"
     ldap_conn.modify_s(basedn, [(ldap.MOD_REPLACE, 'oxEnabled',  'true')])
 
+print "Restarting identity, this will take a while"
+setupObject = Setup(os.path.dirname(os.path.realpath(__file__)))
+setupObject.os_initdaemon = setupObject.detect_initd()
+setupObject.os_type, setupObject.os_version = setupObject.detect_os_type()
+setupObject.run_service_command('identity', 'restart')
