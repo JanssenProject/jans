5ebe6728a6dd419c9abe77527852f7ec32d377c0
Arnab Dutta <arnab.bdutta@gmail.com>
2019-12-28 19:17:05 +0530

    #363 - Introduce new `/uma-rs-modify` command to be able modify existing resource

--- oxd-server/src/test/java/org/gluu/oxd/server/RsModifyTest.java
+++ oxd-server/src/test/java/org/gluu/oxd/server/RsModifyTest.java
@@ -3,12 +3,9 @@ package org.gluu.oxd.server;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.assertj.core.util.Lists;
 import org.gluu.oxd.client.ClientInterface;
-import org.gluu.oxd.client.RsProtectParams2;
-import org.gluu.oxd.common.Jackson2;
 import org.gluu.oxd.common.params.RsModifyParams;
 import org.gluu.oxd.common.response.RegisterSiteResponse;
 import org.gluu.oxd.common.response.RsModifyResponse;
-import org.gluu.oxd.common.response.RsProtectResponse;
 import org.gluu.oxd.rs.protect.RsResource;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -28,7 +25,7 @@ public class RsModifyTest {
 
         final RegisterSiteResponse site = RegisterSiteTest.registerSite(client, opHost, redirectUrls);
 
-        protectResources(client, site, UmaFullTest.resourceList(rsProtect).getResources());
+        RsProtectTest.protectResources(client, site, UmaFullTest.resourceList(rsProtect).getResources());
         RsCheckAccessTest.checkAccess(client, site);
         modifyResourcesWithScopes(client, site, UmaFullTest.resourceList(rsProtect).getResources());
     }
@@ -39,23 +36,9 @@ public class RsModifyTest {
         ClientInterface client = Tester.newClient(host);
 
         final RegisterSiteResponse site = RegisterSiteTest.registerSite(client, opHost, redirectUrls);
-        protectResources(client, site, UmaFullTest.resourceList(rsProtectScopeExpression).getResources());
+        RsProtectTest.protectResources(client, site, UmaFullTest.resourceList(rsProtectScopeExpression).getResources());
         RsCheckAccessTest.checkAccess(client, site);
         modifyResourcesWithScopeExpression(client, site, UmaFullTest.resourceList(rsProtectScopeExpression).getResources(), correctScopeExpression);
-    }
-
-    public static RsProtectResponse protectResources(ClientInterface client, RegisterSiteResponse site, List<RsResource> resources) {
-        final RsProtectParams2 params = new RsProtectParams2();
-        params.setOxdId(site.getOxdId());
-        try {
-            params.setResources(Jackson2.createJsonMapper().readTree(Jackson2.asJsonSilently(resources)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        final RsProtectResponse resp = client.umaRsProtect(Tester.getAuthorization(), params);
-        assertNotNull(resp);
-        return resp;
     }
 
     public static RsModifyResponse modifyResourcesWithScopes(ClientInterface client, RegisterSiteResponse site, List<RsResource> resources) {
