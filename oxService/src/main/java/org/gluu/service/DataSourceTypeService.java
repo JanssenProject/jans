74c9ed3eab88afa6c003d72a875b0e91f383b20d
Gasmyr <thomas@gluu.org>
2020-01-08 17:56:35 +0100

    Fix getSchema Method

---
+++ oxService/src/main/java/org/gluu/service/DataSourceTypeService.java
@@ -0,0 +1,24 @@
+package org.gluu.service;
+
+import org.gluu.persist.PersistenceEntryManager;
+import org.gluu.persist.ldap.impl.LdapEntryManagerFactory;
+
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@RequestScoped
+@Named
+public class DataSourceTypeService implements Serializable {
+
+    private static final long serialVersionUID = -1941135478226842653L;
+
+    @Inject
+    private PersistenceEntryManager entryManager;
+
+    public boolean isLDAP(String key) {
+        return entryManager.getPersistenceType(key).equals(LdapEntryManagerFactory.PERSISTANCE_TYPE);
+    }
+
+}
