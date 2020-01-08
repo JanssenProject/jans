c6537a392bb4dfae2d318ef6fe1c1d1aaebfb79a
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2020-01-08 20:00:47 +0200

    CIBA : removed noisy logs and fixed NPE

--- Model/src/main/java/org/gluu/oxauth/model/configuration/AppConfiguration.java
+++ Model/src/main/java/org/gluu/oxauth/model/configuration/AppConfiguration.java
@@ -1545,6 +1545,7 @@ public class AppConfiguration implements Configuration {
     }
 
     public List<String> getBackchannelTokenDeliveryModesSupported() {
+        if (backchannelTokenDeliveryModesSupported == null) backchannelTokenDeliveryModesSupported = Lists.newArrayList();
         return backchannelTokenDeliveryModesSupported;
     }
 
