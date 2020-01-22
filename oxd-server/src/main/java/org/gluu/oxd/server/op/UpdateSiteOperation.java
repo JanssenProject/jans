51cf8d643593f9c0a3c2bed8bf8ff2df6b6804db
Arnab Dutta <arnab.bdutta@gmail.com>
2020-01-22 19:08:58 +0530

    $409 - Add spontaneous scopes to oxd

--- oxd-server/src/main/java/org/gluu/oxd/server/op/UpdateSiteOperation.java
+++ oxd-server/src/main/java/org/gluu/oxd/server/op/UpdateSiteOperation.java
@@ -4,6 +4,7 @@ import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.inject.Injector;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
 import org.gluu.oxauth.client.RegisterClient;
@@ -351,6 +352,14 @@ public class UpdateSiteOperation extends BaseOperation<UpdateSiteParams> {
 
         if (!Strings.isNullOrEmpty(params.getSoftwareStatement())) {
             request.setSoftwareStatement(params.getSoftwareStatement());
+        }
+
+        if (params.getAllowSpontaneousScopes() != null) {
+            request.setAllowSpontaneousScopes(params.getAllowSpontaneousScopes());
+        }
+
+        if (CollectionUtils.isNotEmpty(params.getSpontaneousScopes())) {
+            request.setSpontaneousScopes(params.getSpontaneousScopes());
         }
 
         if (params.getCustomAttributes() != null && !params.getCustomAttributes().isEmpty()) {
