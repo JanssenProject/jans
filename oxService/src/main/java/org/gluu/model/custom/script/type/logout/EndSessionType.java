5e96895e442ddf2bec480fc57c41094c3262f19b
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2020-01-27 16:01:05 +0200

    oxcore : Added Connect front channel logout custom interception script
    https://github.com/GluuFederation/oxAuth/issues/1113

---
+++ oxService/src/main/java/org/gluu/model/custom/script/type/logout/EndSessionType.java
@@ -0,0 +1,13 @@
+package org.gluu.model.custom.script.type.logout;
+
+import org.gluu.model.custom.script.type.BaseExternalType;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ */
+public interface EndSessionType extends BaseExternalType {
+
+    String getFrontchannelHtml(Object context);
+
+    String getFrontchannelPage(Object context);
+}
