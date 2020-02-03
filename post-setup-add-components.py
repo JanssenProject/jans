baeb2f7835eaf0e4d5b212ce20466625b78d6978
Mustafa Baser <mbaser@mail.com>
2020-02-03 14:02:39 +0300

    closes #633 fix post installations for upgraded server

--- post-setup-add-components.py
+++ post-setup-add-components.py
@@ -4,6 +4,9 @@ import argparse
 import sys
 import subprocess
 import json
+import zipfile
+
+cur_dir = os.path.dirname(os.path.realpath(__file__))
 
 if not os.path.exists('setup.py'):
     print "This script should be run from /install/community-edition-setup/"
@@ -26,7 +29,6 @@ else:
 parser = argparse.ArgumentParser()
 parser.add_argument("-addshib", help="Install Shibboleth SAML IDP", action="store_true")
 parser.add_argument("-addpassport", help="Install Passport", action="store_true")
-parser.add_argument("-addradius", help="Install Radius", action="store_true")
 args = parser.parse_args()
 
 if  len(sys.argv)<2:
@@ -34,19 +36,112 @@ if  len(sys.argv)<2:
     parser.exit(1)
 
 
-from setup import *
+oxVersion = 0
+setup_properties_fn = '/install/community-edition-setup/setup.properties.last'
+install_dir = '.'
 
-setupObj = Setup('.')
+#Determine setup version
+with open(setup_properties_fn) as f:
+    for l in f:
+        ls = l.strip()
+        if ls.startswith('oxVersion'):
+            n = ls.find('=')
+            oxVersion_setup = ls[n+1:].strip()
+
+run_oxauth_war_fn = '/opt/gluu/jetty/oxauth/webapps/oxauth.war'
+
+os.system('cp -f {} /opt/dist/gluu'.format(run_oxauth_war_fn))
+
+#Determine gluu version
+war_zip = zipfile.ZipFile(run_oxauth_war_fn, 'r')
+menifest = war_zip.read('META-INF/MANIFEST.MF')
+
+for l in menifest.splitlines():
+    ls = l.strip()
+    n = ls.find(':')
+
+    if ls[:n].strip() == 'Implementation-Version':
+        oxVersion_current = ls[n+1:].strip()
+        gluu_version_list = oxVersion_current.split('.')
+        if not gluu_version_list[-1].isdigit():
+            gluu_version_list.pop(-1)
+
+        gluu_version = '.'.join(gluu_version_list)
+
+print "Current Gluu Version", gluu_version
+
+ces_version_l = []
+for ci in oxVersion_current.split('.'):
+    if ci.lower() == 'final' or ci.lower().startswith('sp') or ci.lower().startswith('patch'):
+        continue
+    ces_version_l.append(ci)
+    
+ces_version = '.'.join(ces_version_l)
+
+if os.path.exists('ces_current.back'):
+    os.system('rm -r -f ces_current.back')
+
+if os.path.exists('ces_current'):
+    os.system('mv ces_current ces_current.back')
+
+ces_url = 'https://github.com/GluuFederation/community-edition-setup/archive/version_{}.zip'.format(ces_version)
+
+print "Downloading Community Edition Setup {}".format(ces_version)
+
+os.system('wget -nv {} -O version_{}.zip'.format(ces_url, ces_version))
+print "Extracting package"
+os.system('unzip -o -qq version_{}.zip'.format(ces_version))
+os.system('mv community-edition-setup-version_{} ces_current'.format(ces_version))
+
+open('ces_current/__init__.py','w').close()
+
+sys.path.append('ces_current')
+
+from ces_current.setup import *
+install_dir = 'ces_current'
+
+setupObj = Setup(install_dir)
 
 setupObj.setup = setupObj
+
+setupObj.os_type, setupObj.os_version = setupObj.detect_os_type()
+setupObj.os_initdaemon = setupObj.detect_initd()
+
+
+if oxVersion != gluu_version:
+
+    keep_keys = ['idp3_war', 'idp3_cml_keygenerator', 'idp3_dist_jar', 'idp3Folder',
+                'idp3MetadataFolder', 'idp3MetadataCredentialsFolder', 'idp3LogsFolder',
+                'idp3LibFolder', 'idp3ConfFolder', 'idp3ConfAuthnFolder', 
+                'idp3CredentialsFolder', 'idp3WebappFolder', 'oxVersion',
+                'templateFolder', 'outputFolder',
+                'ldif_passport_config', 'ldif_passport', 'ldif_passport_clients',
+                ]
+    keep_dict = {}
+
+    for k in keep_keys:
+        v = getattr(setupObj, k)
+        if v:
+            keep_dict[k] = v
+
 setupObj.load_properties('/install/community-edition-setup/setup.properties.last')
+
+if oxVersion != gluu_version:
+    for k in keep_dict:
+        setattr(setupObj, k, keep_dict[k])
+
+
+setupObj.githubBranchName = oxVersion
 
 setupObj.log = os.path.join(setupObj.install_dir, 'post_setup.log')
 setupObj.logError = os.path.join(setupObj.install_dir, 'post_setup_error.log')
 
-attribDataTypes.startup('.')
+print "Log Files:", setupObj.log, setupObj.logError
 
-setupObj.cbm = CBM(setupObj.couchbase_hostname, setupObj.couchebaseClusterAdmin, setupObj.ldapPass)
+attribDataTypes.startup(install_dir)
+
+if oxVersion == gluu_version:
+    setupObj.cbm = CBM(setupObj.couchbase_hostname, setupObj.couchebaseClusterAdmin, setupObj.ldapPass)
 
 
 if not hasattr(setupObj, 'ldap_type'):
@@ -61,11 +156,19 @@ setupObj.ldapCertFn = setupObj.opendj_cert_fn
 
 def installSaml():
 
+    setupObj.run(['cp', '-f', os.path.join(setupObj.gluuOptFolder, 'jetty/identity/webapps/identity.war'), 
+                setupObj.distGluuFolder])
+
+    if not os.path.exists(setupObj.idp3Folder):
+        os.mkdir(setupObj.idp3Folder)
+
     if setupObj.idp3_metadata[0] == '/':
         setupObj.idp3_metadata = setupObj.idp3_metadata[1:]
 
     metadata_file = os.path.join(setupObj.idp3MetadataFolder, setupObj.idp3_metadata)
 
+    setupObj.run(['cp', '-f', './ces_current/templates/jetty.conf.tmpfiles.d',
+                            setupObj.templateFolder])
 
     if os.path.exists(metadata_file):
         print "Shibboleth is already installed on this system"
@@ -122,72 +225,77 @@ def installSaml():
         F.write(metadata)
     
     setupObj.run([setupObj.cmd_chown, '-R', 'jetty:jetty', setupObj.idp3Folder])
+    if not os.path.exists('/var/run/jetty'):
+        os.mkdir('/var/run/jetty')
+    setupObj.run([setupObj.cmd_chown, '-R', 'jetty:jetty', '/var/run/jetty'])
+    setupObj.enable_service_at_start('idp')
+
     print "Shibboleth installation done"
 
-def installRadius():
 
-    radius_dir = '/opt/gluu/radius'
-    logs_dir = os.path.join(radius_dir,'logs')
-    radius_jar = os.path.join(setupObj.distGluuFolder, 'super-gluu-radius-server.jar')
-    source_dir = os.path.join(setupObj.staticFolder, 'radius')
-    conf_dir = os.path.join(setupObj.gluuBaseFolder, 'conf/radius/')
-    radius_libs = os.path.join(setupObj.distGluuFolder, 'gluu-radius-libs.zip')
-    ldif_file_server = os.path.join(setupObj.outputFolder, 'gluu_radius_server.ldif')
-
-    if not os.path.exists(logs_dir):
-        setupObj.run([setupObj.cmd_mkdir, '-p', logs_dir])
-
-    setupObj.run(['unzip', '-n', '-q', radius_libs, '-d', radius_dir ])
-    setupObj.copyFile(radius_jar, radius_dir)
-
-    if setupObj.mappingLocations['default'] == 'ldap':
-        schema_ldif = os.path.join(source_dir, 'schema/98-radius.ldif')
-        setupObj.import_ldif_opendj([schema_ldif])
-        setupObj.import_ldif_opendj([ldif_file_server])
-    else:
-        setupObj.import_ldif_couchebase([ldif_file_server])
-
-    setupObj.createUser('radius', homeDir=radius_dir, shell='/bin/false')
-    setupObj.addUserToGroup('gluu', 'radius')
     
-    setupObj.copyFile(os.path.join(source_dir, 'etc/default/gluu-radius'), setupObj.osDefault)
-    setupObj.copyFile(os.path.join(source_dir, 'etc/gluu/conf/radius/gluu-radius-logging.xml'), conf_dir)
-    setupObj.copyFile(os.path.join(source_dir, 'scripts/gluu_common.py'), os.path.join(setupObj.gluuOptPythonFolder, 'libs'))
-
-    setupObj.copyFile(os.path.join(source_dir, 'etc/init.d/gluu-radius'), '/etc/init.d')
-    setupObj.run([setupObj.cmd_chmod, '+x', '/etc/init.d/gluu-radius'])
-    
-    if setupObj.os_type+setupObj.os_version == 'ubuntu16':
-        setupObj.run(['update-rc.d', 'gluu-radius', 'defaults'])
-    else:
-        setupObj.copyFile(os.path.join(source_dir, 'systemd/gluu-radius.service'), '/usr/lib/systemd/system')
-        setupObj.run([setupObj.systemctl, 'daemon-reload'])
-    
-    #create empty gluu-radius.private-key.pem
-    gluu_radius_private_key_fn = os.path.join(setupObj.certFolder, 'gluu-radius.private-key.pem')
-    setupObj.writeFile(gluu_radius_private_key_fn, '')
-    
-    setupObj.run([setupObj.cmd_chown, '-R', 'radius:gluu', radius_dir])
-    setupObj.run([setupObj.cmd_chown, '-R', 'root:gluu', conf_dir])
-    setupObj.run([setupObj.cmd_chown, 'root:gluu', os.path.join(setupObj.gluuOptPythonFolder, 'libs/gluu_common.py')])
-
-    setupObj.enable_service_at_start('gluu-radius')
-
-    setupObj.run([setupObj.cmd_chown, 'radius:gluu', os.path.join(setupObj.certFolder, 'gluu-radius.jks')])
-    setupObj.run([setupObj.cmd_chown, 'radius:gluu', os.path.join(setupObj.certFolder, 'gluu-radius.private-key.pem')])
-
-    setupObj.run([setupObj.cmd_chmod, '660', os.path.join(setupObj.certFolder, 'gluu-radius.jks')])
-    setupObj.run([setupObj.cmd_chmod, '660', os.path.join(setupObj.certFolder, 'gluu-radius.private-key.pem')])
-
 def installPassport():
     
     if os.path.exists('/opt/gluu/node/passport'):
         print "Passport is already installed on this system"
         sys.exit()
 
+    if oxVersion != gluu_version:
+
+        node_url = 'https://nodejs.org/dist/v{0}/node-v{0}-linux-x64.tar.xz'.format(setupObj.node_version)
+        nod_archive_fn = os.path.basename(node_url)
+
+        print "Downloading {}".format(nod_archive_fn)
+        setupObj.run(['wget', '-nv', node_url, '-O', os.path.join(setupObj.distAppFolder, nod_archive_fn)])
+        cur_node_dir = os.readlink('/opt/node')
+        setupObj.run(['unlink', '/opt/node'])
+        setupObj.run(['mv', cur_node_dir, cur_node_dir+'.back'])
+
+        print "Installing", nod_archive_fn
+        setupObj.installNode()
+
+        passport_url = 'https://ox.gluu.org/npm/passport/passport-{}.tgz'.format(gluu_version)
+        passport_modules_url = 'https://ox.gluu.org/npm/passport/passport-version_{}-node_modules.tar.gz'.format(gluu_version)
+        passport_fn = os.path.basename(passport_url)
+        passport_modules_fn = os.path.basename(passport_modules_url)
+
+        print "Downloading {}".format(passport_fn)
+        setupObj.run(['wget', '-nv', passport_url, '-O', os.path.join(setupObj.distGluuFolder, 'passport.tgz')])
+
+        print "Downloading {}".format(passport_modules_fn)
+        setupObj.run(['wget', '-nv', passport_modules_url, '-O', os.path.join(setupObj.distGluuFolder, 'passport-node_modules.tar.gz')])
+
+        if setupObj.os_initdaemon == 'systemd':
+            passport_syatemd_url = 'https://raw.githubusercontent.com/GluuFederation/community-edition-package/master/package/systemd/passport.service'
+            passport_syatemd_fn = os.path.basename(passport_syatemd_url)
+            
+            print "Downloading {}".format(passport_syatemd_fn)
+            setupObj.run(['wget', '-nv', passport_syatemd_url, '-O', '/usr/lib/systemd/system/passport.service'])
+
+
+
+    setupObj.installPassport = True
+    setupObj.calculate_selected_aplications_memory()
+    
+    setupObj.renderTemplateInOut(
+                    os.path.join(cur_dir, 'ces_current/templates/node/passport'),
+                    os.path.join(cur_dir, 'ces_current/templates/node'),
+                    os.path.join(cur_dir, 'ces_current/output/node')
+                    )
+
+
     print "Installing Passport ..."
+
+    if not os.path.exists(os.path.join(setupObj.configFolder, 'passport-inbound-idp-initiated.json')) and os.path.exists('ces_current/templates/passport-inbound-idp-initiated.json'):
+        setupObj.run(['cp', 'ces_current/templates/passport-inbound-idp-initiated.json', setupObj.configFolder])
+
+    proc = subprocess.Popen('echo "" | /opt/jre/bin/keytool -list -v -keystore /etc/certs/passport-rp.jks', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    alias_l=''
+    
     setupObj.generate_passport_configuration()
     setupObj.install_passport()
+    
+    
     print "Passport installation done"
 
 
@@ -196,8 +304,5 @@ if args.addshib:
 
 if args.addpassport:
     installPassport()
-
-if args.addradius:
-    installRadius()
 
 print "Please exit container and restart Gluu Server"
