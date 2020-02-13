f874dee1954bf3e5063b41a48c7c0ea50dd9e1d6
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2020-02-13 15:09:00 +0200

    jwr refactoring: JwrEncoder->JwrService #70

--- Server/src/main/java/org/gluu/oxauth/session/ws/rs/LogoutTokenFactory.java
+++ Server/src/main/java/org/gluu/oxauth/session/ws/rs/LogoutTokenFactory.java
@@ -2,14 +2,9 @@ package org.gluu.oxauth.session.ws.rs;
 
 import org.gluu.oxauth.model.config.WebKeysConfiguration;
 import org.gluu.oxauth.model.configuration.AppConfiguration;
-import org.gluu.oxauth.model.crypto.encryption.BlockEncryptionAlgorithm;
-import org.gluu.oxauth.model.crypto.encryption.KeyEncryptionAlgorithm;
-import org.gluu.oxauth.model.jwe.Jwe;
-import org.gluu.oxauth.model.jwt.JwtType;
 import org.gluu.oxauth.model.registration.Client;
 import org.gluu.oxauth.model.token.JsonWebResponse;
-import org.gluu.oxauth.model.token.JwrEncoder;
-import org.gluu.oxauth.model.token.JwtSigner;
+import org.gluu.oxauth.model.token.JwrService;
 import org.msgpack.core.Preconditions;
 import org.slf4j.Logger;
 
@@ -36,17 +31,17 @@ public class LogoutTokenFactory {
     private WebKeysConfiguration webKeysConfiguration;
 
     @Inject
-    private JwrEncoder jwrEncoder;
+    private JwrService jwrService;
 
     public JsonWebResponse createLogoutToken(Client client) {
         try {
             Preconditions.checkNotNull(client);
 
-            JsonWebResponse jwr = createJwr(client);
+            JsonWebResponse jwr = jwrService.createJwr(client);
 
             fillClaims(jwr);
 
-            jwrEncoder.encode(jwr, client);
+            jwrService.encode(jwr, client);
             return jwr;
         } catch (Exception e) {
             log.error("Failed to create logout_token for client:" + client.getClientId());
@@ -65,29 +60,7 @@ public class LogoutTokenFactory {
         jwr.getClaims().setIssuedAt(issuedAt);
         jwr.getClaims().setIssuer(appConfiguration.getIssuer());
 
+        // todo
         //jwr.getClaims().setSubjectIdentifier();
-    }
-
-    private JsonWebResponse createJwr(Client client) {
-        try {
-            if (client.getIdTokenEncryptedResponseAlg() != null
-                    && client.getIdTokenEncryptedResponseEnc() != null) {
-                Jwe jwe = new Jwe();
-
-                // Header
-                KeyEncryptionAlgorithm keyEncryptionAlgorithm = KeyEncryptionAlgorithm.fromName(client.getIdTokenEncryptedResponseAlg());
-                BlockEncryptionAlgorithm blockEncryptionAlgorithm = BlockEncryptionAlgorithm.fromName(client.getIdTokenEncryptedResponseEnc());
-                jwe.getHeader().setType(JwtType.JWT);
-                jwe.getHeader().setAlgorithm(keyEncryptionAlgorithm);
-                jwe.getHeader().setEncryptionMethod(blockEncryptionAlgorithm);
-                return jwe;
-            } else {
-                JwtSigner jwtSigner = JwtSigner.newJwtSigner(appConfiguration, webKeysConfiguration, client);
-                return jwtSigner.newJwt();
-            }
-        } catch (Exception e) {
-            log.error("Failed to create logout_token.", e);
-            return null;
-        }
     }
 }
