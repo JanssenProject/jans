67464e28e492b43137bae29a25a1fb26e9c2bd0a
Mustafa Baser <mbaser@mail.com>
2020-02-19 16:51:33 +0300

    add couchbase support for setup properties generation script

--- static/scripts/generate_ldap_properties.py
+++ static/scripts/generate_ldap_properties.py
@@ -23,6 +23,8 @@ from ldap.dn import explode_dn, str2dn, dn2str
 from urlparse import urlparse
 from pyDes import triple_des, ECB, PAD_PKCS5
 
+from cbm import CBM
+
 ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_ALLOW)
 
 setup_prop = Properties.Properties()
@@ -55,10 +57,33 @@ def get_ssl_subject(ssl_fn):
 
     return retDict
 
+def get_key_from(dn):
+    dns = []
+    for d in str2dn(dn):
+        for rd in d:
+            if rd[0] == 'o' and rd[1] == 'gluu':
+                continue
+            dns.append(rd[1])
 
+    dns.reverse(),
+    key = '_'.join(dns)
+
+    if not key:
+        key = '_'
+
+    return key
+
+def get_cb_result(n1ql):
+    result = cbm.exec_query(n1ql)
+    if result.ok:
+        data = result.json()
+        return data.get('results')
+
+
+default_storage = 'ldap'
 setup_prop['persistence_type'] = 'ldap'
 setup_prop['encode_salt'] = salt
-setup_prop['mappingLocations'] = json.dumps({'default': 'ldap', 'token': 'ldap', 'cache': 'ldap', 'user': 'ldap', 'site': 'ldap'})
+mappingLocations = {'default': 'ldap', 'token': 'ldap', 'cache': 'ldap', 'user': 'ldap', 'site': 'ldap'}
 
 
 #Determine gluu version
@@ -80,7 +105,11 @@ for l in menifest.splitlines():
 
 print "Gluu Version", gluu_version
 
-if gluu_version < '4.0.0':
+vers = gluu_version.split('.')
+
+gluu_3x = (vers[0] < '4') and (vers[1] < '0')
+
+if gluu_3x:
     gluu_ldap_prop = read_properties_file('/etc/gluu/conf/ox-ldap.properties')
     oxauth_ConfigurationEntryDN = gluu_ldap_prop['oxauth_ConfigurationEntryDN']
     oxtrust_ConfigurationEntryDN = gluu_ldap_prop['oxtrust_ConfigurationEntryDN']
@@ -90,19 +119,43 @@ if gluu_version < '4.0.0':
 else:
     gluu_prop = read_properties_file('/etc/gluu/conf/gluu.properties')
     setup_prop['persistence_type'] = gluu_prop['persistence.type']
-    
-    
     oxauth_ConfigurationEntryDN = gluu_prop['oxauth_ConfigurationEntryDN']
     oxtrust_ConfigurationEntryDN = gluu_prop['oxtrust_ConfigurationEntryDN']
+    oxidp_ConfigurationEntryDN = gluu_prop['oxidp_ConfigurationEntryDN']
+    gluu_ConfigurationDN = 'ou=configuration,o=gluu'
 
     if setup_prop['persistence_type'] == 'couchbase':
-        setup_prop['mappingLocations'] = json.dumps({'default': 'couchbase', 'token': 'couchbase', 'cache': 'couchbase', 'user': 'couchbase', 'site': 'couchbase'})
+        mappingLocations = {'default': 'couchbase', 'token': 'couchbase', 'cache': 'couchbase', 'user': 'couchbase', 'site': 'couchbase'}
+        default_storage = 'couchbase'
 
     if setup_prop['persistence_type'] in ('ldap', 'hybrid'):
+
         gluu_ldap_prop_fn = '/etc/gluu/conf/gluu-ldap.properties'
         if os.path.exists(gluu_ldap_prop_fn):
             gluu_ldap_prop = read_properties_file(gluu_ldap_prop_fn)
+        
+    if setup_prop['persistence_type'] != 'ldap':
+        gluu_cb_prop_fn = '/etc/gluu/conf/gluu-couchbase.properties'
+        if os.path.exists(gluu_cb_prop_fn):
+            gluu_cb_prop = read_properties_file(gluu_cb_prop_fn)
+            
+            setup_prop['couchebaseClusterAdmin'] = gluu_cb_prop['auth.userName']
+            setup_prop['encoded_cb_password'] = gluu_cb_prop['auth.userPassword']
+            setup_prop['cb_password'] = unobscure(setup_prop['encoded_cb_password'])
 
+            setup_prop['couchbase_bucket_prefix'] = gluu_cb_prop['bucket.default']
+
+            setup_prop['couchbase_hostname'] = gluu_cb_prop['servers'].split(',')[0].strip()
+            setup_prop['couchbaseTrustStorePass'] = unobscure(gluu_cb_prop['ssl.trustStore.pin'])
+
+            cbm = CBM(setup_prop['couchbase_hostname'], setup_prop['couchebaseClusterAdmin'], setup_prop['cb_password'])
+            cb_who = cbm.whoami()
+            if cb_who.get('roles'):
+                for rd in cb_who['roles']:
+                    for r in rd:
+                        if r == 'role' and rd[r] == 'admin':
+                            setup_prop['isCouchbaseUserAdmin'] = 'true'
+                            break
 
 if gluu_version < '4.1.0':
     jetty_services = {
@@ -122,18 +175,18 @@ else:
         'passport':  ('installPassport', 0.1),
     }
 
+if setup_prop['persistence_type'] != 'couchbase':
+    setup_prop['ldap_binddn'] = gluu_ldap_prop['bindDN']
+    setup_prop['ldapPass'] = unobscure(gluu_ldap_prop['bindPassword'])
+    setup_prop['opendj_p12_pass'] = unobscure(gluu_ldap_prop['ssl.trustStorePin'])
+    setup_prop['ldap_hostname'], setup_prop['ldaps_port']  = gluu_ldap_prop['servers'].split(',')[0].split(':')
 
-setup_prop['ldap_binddn'] = gluu_ldap_prop['bindDN']
-setup_prop['ldapPass'] = unobscure(gluu_ldap_prop['bindPassword'])
-setup_prop['opendj_p12_pass'] = unobscure(gluu_ldap_prop['ssl.trustStorePin'])
-setup_prop['ldap_hostname'], setup_prop['ldaps_port']  = gluu_ldap_prop['servers'].split(',')[0].split(':')
+    ldap_conn = ldap.initialize('ldaps://{0}:1636'.format(setup_prop['ldap_hostname']))
+    ldap_conn.simple_bind_s(setup_prop['ldap_binddn'], setup_prop['ldapPass'])
 
 
-ldap_conn = ldap.initialize('ldaps://{0}:1636'.format(setup_prop['ldap_hostname']))
-ldap_conn.simple_bind_s(setup_prop['ldap_binddn'], setup_prop['ldapPass'])
 
-
-if gluu_version < '4.0.0':
+if gluu_3x:
     result = ldap_conn.search_s('o=gluu',ldap.SCOPE_ONELEVEL)
     inumOrg = str(result[1][1]['o'][0])
     uma_rpt_policy_inum = '{}!0011!2DAF.F995'.format(inumOrg)
@@ -142,99 +195,168 @@ else:
     uma_rpt_policy_inum = '2DAF-F995'
     scim_access_policy_inum = '2DAF-F9A5'
 
-    result = ldap_conn.search_s('ou=oxradius,ou=configuration,o=gluu',ldap.SCOPE_BASE)
-    setup_prop['gluu_radius_client_id'] = result[0][1]['oxRadiusOpenidUsername'][0]
-    setup_prop['gluu_ro_pw'] = unobscure(result[0][1]['oxRadiusOpenidPassword'][0])
+    if default_storage == 'ldap':
 
-    result = ldap_conn.search_s('inum=5866-4202,ou=scripts,o=gluu',ldap.SCOPE_BASE, attrlist=['oxEnabled'])
-    if result[0][1]['oxEnabled'][0].lower() == 'true':
-        setup_prop['enableRadiusScripts'] = 'true' 
+        try:
+            result = ldap_conn.search_s('ou=oxradius,ou=configuration,o=gluu',ldap.SCOPE_BASE)
+            setup_prop['installGluuRadius'] = 'true'
+            setup_prop['gluu_radius_client_id'] = result[0][1]['oxRadiusOpenidUsername'][0]
+            setup_prop['gluu_ro_pw'] = unobscure(result[0][1]['oxRadiusOpenidPassword'][0])
+        except:
+            pass
 
-    result = ldap_conn.search_s('ou=clients,o=gluu',ldap.SCOPE_SUBTREE, '(inum=1402.*)', ['inum'])
+        result = ldap_conn.search_s('inum=5866-4202,ou=scripts,o=gluu',ldap.SCOPE_BASE, attrlist=['oxEnabled'])
+        if result[0][1]['oxEnabled'][0].lower() == 'true':
+            setup_prop['enableRadiusScripts'] = 'true' 
+
+        result = ldap_conn.search_s('ou=clients,o=gluu',ldap.SCOPE_SUBTREE, '(inum=1402.*)', ['inum'])
+        if result:
+            setup_prop['oxtrust_requesting_party_client_id'] = result[0][1]['inum'][0]
+
+    elif default_storage == 'couchbase':
+        n1ql = 'SELECT * from `{}` USE KEYS "configuration_oxradius"'.format(setup_prop['couchbase_bucket_prefix'])
+        result = get_cb_result(n1ql)
+        if result:
+            setup_prop['installGluuRadius'] = 'true'
+            setup_prop['gluu_radius_client_id'] = str(result[0]['gluu']['oxRadiusOpenidUsername'])
+            setup_prop['gluu_ro_pw'] = unobscure(result[0]['gluu']['oxRadiusOpenidPassword'])
+        
+        n1ql = 'SELECT oxEnabled from `{}` USE KEYS "scripts_5866-4202"'.format(setup_prop['couchbase_bucket_prefix'])
+        result = get_cb_result(n1ql)
+        if result and result[0]['oxEnabled']:
+            setup_prop['enableRadiusScripts'] = 'true' 
+
+        n1ql =  'SELECT inum from `{}` WHERE objectClass="oxAuthClient" AND inum LIKE "1402.%"'.format(setup_prop['couchbase_bucket_prefix'])
+        result = get_cb_result(n1ql)
+        if result:
+            setup_prop['oxtrust_requesting_party_client_id'] = str(result[0]['inum'])
+
+admin_dn = None
+if mappingLocations['user'] == 'ldap':
+    result = ldap_conn.search_s('o=gluu',ldap.SCOPE_SUBTREE, '(gluuGroupType=gluuManagerGroup)', ['member'])
+    if result and result[0][1]['member']:
+        admin_dn = result[0][1]['member'][0]
+
+
+if mappingLocations['user'] == 'couchbase':
+    bucket = '{}_user'.format(setup_prop['couchbase_bucket_prefix'])
+    n1ql = 'SELECT * from `{}` where objectClass="gluuGroup" and gluuGroupType="gluuManagerGroup"'.format(bucket)
+    result = get_cb_result(n1ql)
+    if result and result[0][bucket]['member']:
+       admin_dn = result[0][bucket]['member'][0]
+
+if admin_dn:
+    for d in str2dn(admin_dn):
+        for rd in d:
+            if rd[0] == 'inum':
+                setup_prop['admin_inum'] = str(rd[1])
+                break
+
+oxTrustConfApplication = None
+oxConfApplication = None
+oxAuthConfDynamic = None
+oxAuthConfDynamic = None
+
+if default_storage == 'ldap':
+
+    result = ldap_conn.search_s(gluu_ConfigurationDN, ldap.SCOPE_BASE,'(objectClass=*)')
+    setup_prop['ip'] = str(result[0][1]['gluuIpAddress'][0])
+    oxCacheConfiguration = json.loads(result[0][1]['oxCacheConfiguration'][0])
+
+    setup_prop['cache_provider_type'] = str(oxCacheConfiguration['cacheProviderType'])
+
+    result = ldap_conn.search_s(oxidp_ConfigurationEntryDN, ldap.SCOPE_BASE,'(objectClass=oxApplicationConfiguration)', ['oxConfApplication'])
+    oxConfApplication = json.loads(result[0][1]['oxConfApplication'][0])
+
+    result = ldap_conn.search_s(oxauth_ConfigurationEntryDN, ldap.SCOPE_BASE,'(objectClass=oxAuthConfiguration)', ['oxAuthConfDynamic'])
+    oxAuthConfDynamic = json.loads(result[0][1]['oxAuthConfDynamic'][0])
+
+    result = ldap_conn.search_s(oxtrust_ConfigurationEntryDN, ldap.SCOPE_BASE,'(objectClass=oxTrustConfiguration)', ['oxTrustConfApplication'])
+    oxTrustConfApplication = json.loads(result[0][1]['oxTrustConfApplication'][0])
+
+
+elif default_storage == 'couchbase':
+    bucket = setup_prop['couchbase_bucket_prefix']
+
+    s_key = get_key_from(gluu_ConfigurationDN)
+    n1ql = 'SELECT * FROM `{}` USE KEYS "{}"'.format(format(bucket), s_key)
+    result = get_cb_result(n1ql)
     if result:
-        setup_prop['oxtrust_requesting_party_client_id'] = result[0][1]['inum'][0]
+        setup_prop['ip'] = str(result[0][bucket]['gluuIpAddress'])
+        setup_prop['cache_provider_type'] = str(result[0][bucket]['oxCacheConfiguration']['cacheProviderType'])
 
-result = ldap_conn.search_s('o=gluu',ldap.SCOPE_SUBTREE, '(gluuGroupType=gluuManagerGroup)', ['member'])
-if result and result[0][1]['member']:
-    admin_dn = result[0][1]['member'][0]
-    tmp_ = explode_dn(admin_dn)
-    setup_prop['admin_inum'] = str(str2dn(tmp_[0])[0][0][1])
+    s_key = get_key_from(oxidp_ConfigurationEntryDN)
+    n1ql = 'SELECT oxConfApplication FROM `{}` USE KEYS "{}"'.format(format(bucket), s_key)
+    result = get_cb_result(n1ql)
+    if result:
+        oxConfApplication = result[0]['oxConfApplication']
 
-result = ldap_conn.search_s(gluu_ConfigurationDN, ldap.SCOPE_BASE,'(objectClass=*)')
-setup_prop['ip'] = str(result[0][1]['gluuIpAddress'][0])
-oxCacheConfiguration = json.loads(result[0][1]['oxCacheConfiguration'][0])
+    s_key = get_key_from(oxauth_ConfigurationEntryDN)
+    n1ql = 'SELECT oxAuthConfDynamic FROM `{}` USE KEYS "{}"'.format(format(bucket), s_key)
+    result = get_cb_result(n1ql)
+    if result:        
+        oxAuthConfDynamic = result[0]['oxAuthConfDynamic']
 
-setup_prop['cache_provider_type'] = str(oxCacheConfiguration['cacheProviderType'])
-
-
-result = ldap_conn.search_s(oxidp_ConfigurationEntryDN, ldap.SCOPE_BASE,'(objectClass=oxApplicationConfiguration)', ['oxConfApplication'])
-oxConfApplication = json.loads(result[0][1]['oxConfApplication'][0])
-
-setup_prop['idpClient_pw'] =  str(unobscure(oxConfApplication['openIdClientPassword']))
-setup_prop['idp_client_id'] =  str(oxConfApplication['openIdClientId'])
-
-
-result = ldap_conn.search_s(oxauth_ConfigurationEntryDN, ldap.SCOPE_BASE,'(objectClass=oxAuthConfiguration)', ['oxAuthConfDynamic'])
-oxAuthConfDynamic = json.loads(result[0][1]['oxAuthConfDynamic'][0])
-
-o_issuer = urlparse(oxAuthConfDynamic['issuer'])
-
-setup_prop['hostname'] = str(o_issuer.netloc)
-setup_prop['pairwiseCalculationSalt'] =  str(oxAuthConfDynamic['pairwiseCalculationSalt'])
-setup_prop['oxauth_openidScopeBackwardCompatibility'] =  str(oxAuthConfDynamic.get('openidScopeBackwardCompatibility', False)).lower()
-
-setup_prop['oxauth_legacyIdTokenClaims'] = str(oxAuthConfDynamic['legacyIdTokenClaims']).lower()
-
-setup_prop['pairwiseCalculationKey'] = str(oxAuthConfDynamic['pairwiseCalculationKey'])
-
-setup_prop['oxauth_openid_jks_fn'] = str(oxAuthConfDynamic['keyStoreFile'])
-setup_prop['oxauth_openid_jks_pass'] = str(oxAuthConfDynamic['keyStoreSecret'])
+    s_key = get_key_from(oxtrust_ConfigurationEntryDN)
+    n1ql = 'SELECT oxTrustConfApplication FROM `{}` USE KEYS "{}"'.format(format(bucket), s_key)
+    result = get_cb_result(n1ql)
+    if result:
+        oxTrustConfApplication = result[0]['oxTrustConfApplication']
+    
+if oxTrustConfApplication:
+    if 'apiUmaClientId' in oxTrustConfApplication:
+        setup_prop['oxtrust_resource_server_client_id'] =  str(oxTrustConfApplication['apiUmaClientId'])
 
 
+    if 'apiUmaClientKeyStorePassword' in oxTrustConfApplication:
+        setup_prop['api_rs_client_jks_pass'] =  str(unobscure(oxTrustConfApplication['apiUmaClientKeyStorePassword']))
+
+    if 'apiUmaResourceId' in oxTrustConfApplication:
+        setup_prop['oxtrust_resource_id'] =  str(oxTrustConfApplication['apiUmaResourceId'])
+
+    setup_prop['shibJksPass'] =  str(unobscure(oxTrustConfApplication['idpSecurityKeyPassword']))
+    setup_prop['admin_email'] =  str(oxTrustConfApplication['orgSupportEmail'])
+    setup_prop['orgName'] =  str(oxTrustConfApplication['organizationName'])
+    setup_prop['oxauth_client_id'] =  str(oxTrustConfApplication['oxAuthClientId'])
+    setup_prop['oxauthClient_pw'] = str(unobscure(oxTrustConfApplication['oxAuthClientPassword']))
+    setup_prop['scim_rs_client_id'] =  str(oxTrustConfApplication['scimUmaClientId'])
+    setup_prop['scim_resource_oxid'] =  str(oxTrustConfApplication['scimUmaResourceId'])
+    setup_prop['scimTestMode'] =  str(oxTrustConfApplication['scimTestMode'])
+
+    if 'apiUmaClientKeyStorePassword' in oxTrustConfApplication:
+        setup_prop['api_rp_client_jks_pass'] = unobscure(oxTrustConfApplication['apiUmaClientKeyStorePassword'])
+        setup_prop['api_rs_client_jks_fn'] = str(oxTrustConfApplication['apiUmaClientKeyStoreFile'])
+
+    if 'scimUmaClientKeyStorePassword' in oxTrustConfApplication:
+        setup_prop['scim_rs_client_jks_pass'] = unobscure(oxTrustConfApplication['scimUmaClientKeyStorePassword'])
+        setup_prop['scim_rs_client_jks_fn'] = str(oxTrustConfApplication['scimUmaClientKeyStoreFile'])
+
+
+if oxConfApplication:
+    setup_prop['idpClient_pw'] =  str(unobscure(oxConfApplication['openIdClientPassword']))
+    setup_prop['idp_client_id'] =  str(oxConfApplication['openIdClientId'])
+
+
+if oxAuthConfDynamic:
+    o_issuer = urlparse(oxAuthConfDynamic['issuer'])
+    setup_prop['hostname'] = str(o_issuer.netloc)
+    setup_prop['pairwiseCalculationSalt'] =  str(oxAuthConfDynamic['pairwiseCalculationSalt'])
+    setup_prop['oxauth_openidScopeBackwardCompatibility'] =  str(oxAuthConfDynamic.get('openidScopeBackwardCompatibility', False)).lower()
+    setup_prop['oxauth_legacyIdTokenClaims'] = str(oxAuthConfDynamic['legacyIdTokenClaims']).lower()
+    setup_prop['pairwiseCalculationKey'] = str(oxAuthConfDynamic['pairwiseCalculationKey'])
+    setup_prop['oxauth_openid_jks_fn'] = str(oxAuthConfDynamic['keyStoreFile'])
+    setup_prop['oxauth_openid_jks_pass'] = str(oxAuthConfDynamic['keyStoreSecret'])
 
 #to do: if radius is enabled set oxauth_openidScopeBackwardCompatibility to true
-
-
-result = ldap_conn.search_s(oxtrust_ConfigurationEntryDN, ldap.SCOPE_BASE,'(objectClass=oxTrustConfiguration)', ['oxTrustConfApplication'])
-oxTrustConfApplication = json.loads(result[0][1]['oxTrustConfApplication'][0])
-
-if 'apiUmaClientId' in oxTrustConfApplication:
-    setup_prop['oxtrust_resource_server_client_id'] =  str(oxTrustConfApplication['apiUmaClientId'])
-
-
-if 'apiUmaClientKeyStorePassword' in oxTrustConfApplication:
-    setup_prop['api_rs_client_jks_pass'] =  str(unobscure(oxTrustConfApplication['apiUmaClientKeyStorePassword']))
-
-if 'apiUmaResourceId' in oxTrustConfApplication:
-    setup_prop['oxtrust_resource_id'] =  str(oxTrustConfApplication['apiUmaResourceId'])
-
-setup_prop['shibJksPass'] =  str(unobscure(oxTrustConfApplication['idpSecurityKeyPassword']))
-setup_prop['admin_email'] =  str(oxTrustConfApplication['orgSupportEmail'])
-setup_prop['orgName'] =  str(oxTrustConfApplication['organizationName'])
-setup_prop['oxauth_client_id'] =  str(oxTrustConfApplication['oxAuthClientId'])
-setup_prop['oxauthClient_pw'] = str(unobscure(oxTrustConfApplication['oxAuthClientPassword']))
-setup_prop['scim_rs_client_id'] =  str(oxTrustConfApplication['scimUmaClientId'])
-setup_prop['scim_resource_oxid'] =  str(oxTrustConfApplication['scimUmaResourceId'])
-setup_prop['scimTestMode'] =  str(oxTrustConfApplication['scimTestMode'])
 
 ssl_subj = get_ssl_subject('/etc/certs/httpd.crt')
 setup_prop['countryCode'] = ssl_subj['C']
 setup_prop['state'] = ssl_subj['ST']
 setup_prop['city'] = ssl_subj['L']
 
-if 'apiUmaClientKeyStorePassword' in oxTrustConfApplication:
-    setup_prop['api_rp_client_jks_pass'] = unobscure(oxTrustConfApplication['apiUmaClientKeyStorePassword'])
-    setup_prop['api_rs_client_jks_fn'] = str(oxTrustConfApplication['apiUmaClientKeyStoreFile'])
-
-if 'scimUmaClientKeyStorePassword' in oxTrustConfApplication:
-    setup_prop['scim_rs_client_jks_pass'] = unobscure(oxTrustConfApplication['scimUmaClientKeyStorePassword'])
-    setup_prop['scim_rs_client_jks_fn'] = str(oxTrustConfApplication['scimUmaClientKeyStoreFile'])
-
-
 for service in jetty_services:
     if os.path.exists('/opt/gluu/jetty/{0}/webapps/{0}.war'.format(service)):
         setup_prop[jetty_services[service][0]] = 'true'
-
 
 if setup_prop['installSaml']:
     setup_prop['gluuSamlEnabled'] = 'true'
@@ -274,7 +396,8 @@ setup_prop['os_version'] = p[1].split('.')[0]
 https_gluu_fn = '/etc/httpd/conf.d/https_gluu.conf' if setup_prop['os_type'] in ('red', 'fedora', 'centos') else '/etc/apache2/sites-available/https_gluu.conf'
 setup_prop['installHTTPD'] = str(os.path.exists(https_gluu_fn)).lower()
 
-"""
+setup_prop['mappingLocations'] = json.dumps(mappingLocations)
+
 for p in setup_prop.keys():
     print p,":", setup_prop[p]
-"""
+
