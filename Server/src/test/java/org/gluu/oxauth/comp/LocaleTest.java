d592806cf31f6f4d7f52cc98c9170765f2033ee1
yurem <Yuriy.Movchan@gmail.com>
2020-02-28 14:42:48 +0300

    Use ui_locales_supported from confguration instead of JSF locales list oxAuth #1275

--- Server/src/test/java/org/gluu/oxauth/comp/LocaleTest.java
+++ Server/src/test/java/org/gluu/oxauth/comp/LocaleTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Locale;
 
 import org.gluu.oxauth.BaseTest;
-import org.gluu.oxauth.model.util.LocaleUtil;
+import org.gluu.util.ilocale.LocaleUtil;
 import org.testng.annotations.Test;
 
 /**
