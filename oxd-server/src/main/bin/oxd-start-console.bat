bfb7863a36920dc92318928c4267fd9aac18b9c1
Arnab Dutta <arnab.bdutta@gmail.com>
2020-03-16 23:49:35 +0530

    #430 - Add support for JDBC connection to be able connect to any RDBMS

--- oxd-server/src/main/bin/oxd-start-console.bat
+++ oxd-server/src/main/bin/oxd-start-console.bat
@@ -2,4 +2,4 @@ cd /d %~dp0
 set LIB=../lib
 set CONF=../conf/oxd-server.yml
 echo CONF=%CONF%
-java -Djava.net.preferIPv4Stack=true -cp %LIB%/* org.gluu.oxd.server.OxdServerApplication server %CONF%
+java -Djava.net.preferIPv4Stack=true -cp %LIB%/oxd-server.jar;%LIB%/* org.gluu.oxd.server.OxdServerApplication server %CONF%
