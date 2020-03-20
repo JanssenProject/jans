2b89fe7bd50d1a8859bbae45693e9fa41f6b0636
Milton BO <jmcm578@gmail.com>
2020-03-20 16:53:43 -0400

    Add Json configuration property to control JWKS endpoint algorithms. #1292

--- Server/src/main/java/org/gluu/oxauth/model/config/WebKeysConfiguration.java
+++ Server/src/main/java/org/gluu/oxauth/model/config/WebKeysConfiguration.java
@@ -6,11 +6,15 @@
 
 package org.gluu.oxauth.model.config;
 
-import javax.enterprise.inject.Vetoed;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.gluu.oxauth.model.configuration.AppConfiguration;
 import org.gluu.oxauth.model.configuration.Configuration;
+import org.gluu.oxauth.model.jwk.JSONWebKey;
 import org.gluu.oxauth.model.jwk.JSONWebKeySet;
+
+import javax.enterprise.inject.Vetoed;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Yuriy Movchan
@@ -19,4 +23,41 @@ import org.gluu.oxauth.model.jwk.JSONWebKeySet;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Vetoed
 public class WebKeysConfiguration extends JSONWebKeySet implements Configuration {
+
+    /**
+     * Configuration needed to get configuration done in LDAP about algorithms supported.
+     */
+    private AppConfiguration appConfiguration;
+
+    @Override
+    public List<JSONWebKey> getKeys() {
+        return filterKeys(super.getKeys());
+    }
+
+    /**
+     * Method responsible to filter all keys stored in LDAP with the list of
+     * algorithms that it is in Json config attribute called "jwksAlgorithmsSupported"
+     * @param allKeys All keys that are stored in LDAP
+     * @return Filtered list
+     */
+    private List<JSONWebKey> filterKeys(List<JSONWebKey> allKeys) {
+        if (allKeys != null && allKeys.size() > 0) {
+            List<JSONWebKey> filteredKeys = new ArrayList<>();
+            List<String> jwksAlgorithmsSupported = appConfiguration.getJwksAlgorithmsSupported();
+            if (jwksAlgorithmsSupported != null && jwksAlgorithmsSupported.size() > 0) {
+                for (JSONWebKey key : allKeys) {
+                    if (jwksAlgorithmsSupported.contains(key.getAlg().getParamName()))
+                        filteredKeys.add(key);
+                }
+                return filteredKeys;
+            } else
+                return allKeys;
+        } else
+            return allKeys;
+    }
+
+    public void setAppConfiguration(AppConfiguration appConfiguration) {
+        this.appConfiguration = appConfiguration;
+    }
+
 }
