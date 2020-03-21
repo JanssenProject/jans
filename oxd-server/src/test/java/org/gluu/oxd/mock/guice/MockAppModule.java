4ebb58c5a72c1b545d965a987bef5f5756737929
Arnab Dutta <arnab.bdutta@gmail.com>
2020-03-21 22:57:01 +0530

    #374 - Use cached mocked objects in OpClientFactoryMockImpl.

--- oxd-server/src/test/java/org/gluu/oxd/mock/guice/MockAppModule.java
+++ oxd-server/src/test/java/org/gluu/oxd/mock/guice/MockAppModule.java
@@ -22,7 +22,6 @@ public class MockAppModule extends AbstractModule {
         bind(RpService.class).in(Singleton.class);
         bind(HttpService.class).in(Singleton.class);
         bind(IntrospectionService.class).in(Singleton.class);
-        bind(SqlPersistenceProvider.class).to(H2PersistenceProvider.class).in(Singleton.class);
         bind(PersistenceService.class).to(PersistenceServiceImpl.class).in(Singleton.class);
         bind(MigrationService.class).in(Singleton.class);
         bind(DiscoveryService.class).in(Singleton.class);
