87202e2b34776484f1d8c8019d6bbb33ce3c8c0f
yurem <Yuriy.Movchan@gmail.com>
2020-04-14 10:12:57 +0300

    Store IDP/SP files in configurable document store oxCore #1939

---
+++ core-document-store/src/main/java/org/gluu/service/document/store/StandaloneDocumentStoreProviderFactory.java
@@ -0,0 +1,78 @@
+package org.gluu.service.document.store;
+
+import org.gluu.service.document.store.conf.DocumentStoreConfiguration;
+import org.gluu.service.document.store.conf.DocumentStoreType;
+import org.gluu.service.document.store.provider.DocumentStoreProvider;
+import org.gluu.service.document.store.provider.JcaDocumentStoreProvider;
+import org.gluu.service.document.store.provider.LocalDocumentStoreProvider;
+import org.gluu.service.document.store.provider.WebDavDocumentStoreProvider;
+import org.gluu.util.security.StringEncrypter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Yuriy Movchan on 04/10/2020
+ */
+public class StandaloneDocumentStoreProviderFactory {
+
+	private static final Logger LOG = LoggerFactory.getLogger(StandaloneDocumentStoreProviderFactory.class);
+
+	private StringEncrypter stringEncrypter;
+
+	public StandaloneDocumentStoreProviderFactory(StringEncrypter stringEncrypter) {
+		this.stringEncrypter = stringEncrypter;
+	}
+
+	public DocumentStoreProvider getDocumentStoreProvider(DocumentStoreConfiguration documentStoreConfiguration) {
+		DocumentStoreType documentStoreType = documentStoreConfiguration.getDocumentStoreType();
+
+		if (documentStoreType == null) {
+			LOG.error("Failed to initialize documentStoreProvider, documentStoreProviderType is null. Fallback to LOCAL type.");
+			documentStoreType = DocumentStoreType.LOCAL;
+		}
+
+		// Create bean
+		DocumentStoreProvider documentStoreProvider = null;
+		switch (documentStoreType) {
+		case LOCAL:
+			LocalDocumentStoreProvider localDocumentStoreProvider = new LocalDocumentStoreProvider();
+			localDocumentStoreProvider.configure(documentStoreConfiguration);
+			localDocumentStoreProvider.init();
+
+			documentStoreProvider = localDocumentStoreProvider;
+			break;
+		case JCA:
+			if (stringEncrypter == null) {
+				throw new RuntimeException("Factory is not initialized properly. stringEncrypter is not specified");
+			}
+
+			JcaDocumentStoreProvider jcaDocumentStoreProvider = new JcaDocumentStoreProvider();
+			jcaDocumentStoreProvider.configure(documentStoreConfiguration);
+			jcaDocumentStoreProvider.init();
+
+			documentStoreProvider = jcaDocumentStoreProvider;
+			break;
+		case WEB_DAV:
+			if (stringEncrypter == null) {
+				throw new RuntimeException("Factory is not initialized properly. stringEncrypter is not specified");
+			}
+
+			WebDavDocumentStoreProvider webDavDocumentStoreProvider = new WebDavDocumentStoreProvider();
+			webDavDocumentStoreProvider.configure(documentStoreConfiguration);
+			webDavDocumentStoreProvider.init();
+
+			documentStoreProvider = webDavDocumentStoreProvider;
+			break;
+		}
+
+		if (documentStoreProvider == null) {
+			throw new RuntimeException(
+					"Failed to initialize documentStoreProvider, documentStoreProviderType is unsupported: " + documentStoreType);
+		}
+
+		documentStoreProvider.create();
+
+		return documentStoreProvider;
+	}
+
+}
---
+++ core-document-store/src/main/java/org/gluu/service/document/store/conf/DocumentStoreConfiguration.java
@@ -0,0 +1,63 @@
+package org.gluu.service.document.store.conf;
+
+import java.io.Serializable;
+
+import javax.enterprise.inject.Vetoed;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * @author Yuriy Movchan on 04/10/2020
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Vetoed
+public class DocumentStoreConfiguration implements Serializable {
+
+	private static final long serialVersionUID = 2519892725606554887L;
+
+	private DocumentStoreType documentStoreType = DocumentStoreType.LOCAL;
+
+    private LocalDocumentStoreConfiguration localConfiguration = new LocalDocumentStoreConfiguration();
+
+    private JcaDocumentStoreConfiguration jcaConfiguration;
+
+    private WebDavDocumentStoreConfiguration webDavConfiguration;
+
+    public DocumentStoreType getDocumentStoreType() {
+		return documentStoreType;
+	}
+
+	public void setDocumentStoreType(DocumentStoreType documentStoreType) {
+		this.documentStoreType = documentStoreType;
+	}
+
+	public LocalDocumentStoreConfiguration getLocalConfiguration() {
+		return localConfiguration;
+	}
+
+	public void setLocalConfiguration(LocalDocumentStoreConfiguration localConfiguration) {
+		this.localConfiguration = localConfiguration;
+	}
+
+	public JcaDocumentStoreConfiguration getJcaConfiguration() {
+		return jcaConfiguration;
+	}
+
+	public void setJcaConfiguration(JcaDocumentStoreConfiguration jcaConfiguration) {
+		this.jcaConfiguration = jcaConfiguration;
+	}
+
+	public WebDavDocumentStoreConfiguration getWebDavConfiguration() {
+		return webDavConfiguration;
+	}
+
+	public void setWebDavConfiguration(WebDavDocumentStoreConfiguration webDavConfiguration) {
+		this.webDavConfiguration = webDavConfiguration;
+	}
+
+	@Override
+	public String toString() {
+		return "DocumentStoreConfiguration [documentStoreType=" + documentStoreType + ", localConfiguration=" + localConfiguration
+				+ ", jcaConfiguration=" + jcaConfiguration + ", webDavConfiguration=" + webDavConfiguration + "]";
+	}
+}
