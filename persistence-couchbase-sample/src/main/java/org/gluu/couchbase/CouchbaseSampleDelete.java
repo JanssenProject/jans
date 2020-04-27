8cf45100b94cf24e489db2d391550338f528b3d3
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2020-04-27 22:09:18 +0300

    Use `exp` instead of `oxAuthExpiration` all over the code
    https://github.com/GluuFederation/oxAuth/issues/1300

--- persistence-couchbase-sample/src/main/java/org/gluu/couchbase/CouchbaseSampleDelete.java
+++ persistence-couchbase-sample/src/main/java/org/gluu/couchbase/CouchbaseSampleDelete.java
@@ -1,13 +1,13 @@
 package org.gluu.couchbase;
 
-import java.util.Date;
-
 import org.gluu.persist.couchbase.impl.CouchbaseEntryManager;
 import org.gluu.persist.couchbase.operation.impl.CouchbaseConnectionProvider;
 import org.gluu.persist.model.base.DeletableEntity;
 import org.gluu.search.filter.Filter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Date;
 
 /**
  * @author Yuriy Movchan Date: 11/03/2016
@@ -29,7 +29,7 @@ public final class CouchbaseSampleDelete {
         String baseDn = "ou=cache,o=gluu";
 		Filter filter = Filter.createANDFilter(
 		        Filter.createEqualityFilter("del", true),
-				Filter.createLessOrEqualFilter("oxAuthExpiration", couchbaseEntryManager.encodeTime(baseDn, new Date()))
+				Filter.createLessOrEqualFilter("exp", couchbaseEntryManager.encodeTime(baseDn, new Date()))
         );
 
         int result = couchbaseEntryManager.remove(baseDn, DeletableEntity.class, filter, 100);
--- persistence-ldap-sample/src/main/java/org/gluu/ldap/LdapSampleDelete.java
+++ persistence-ldap-sample/src/main/java/org/gluu/ldap/LdapSampleDelete.java
@@ -1,23 +1,14 @@
 package org.gluu.ldap;
 
-import java.util.Date;
-import java.util.List;
-
 import org.apache.log4j.Logger;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.status.StatusLogger;
-import org.gluu.ldap.model.SimpleAttribute;
-import org.gluu.ldap.model.SimpleGrant;
-import org.gluu.ldap.model.SimpleSession;
-import org.gluu.ldap.model.SimpleUser;
 import org.gluu.log.LoggingHelper;
 import org.gluu.persist.ldap.impl.LdapEntryManager;
-import org.gluu.persist.model.PagedResult;
-import org.gluu.persist.model.SearchScope;
-import org.gluu.persist.model.SortOrder;
-import org.gluu.persist.model.base.CustomAttribute;
 import org.gluu.persist.model.base.DeletableEntity;
 import org.gluu.search.filter.Filter;
+
+import java.util.Date;
 
 /**
  * @author Yuriy Movchan Date: 11/03/2016
@@ -45,7 +36,7 @@ public final class LdapSampleDelete {
         String baseDn = "ou=cache,o=gluu";
 		Filter filter = Filter.createANDFilter(
 		        Filter.createEqualityFilter("del", true),
-				Filter.createLessOrEqualFilter("oxAuthExpiration", ldapEntryManager.encodeTime(baseDn, new Date(System.currentTimeMillis() + 2 * 24 * 60 * 60 * 1000)))
+				Filter.createLessOrEqualFilter("exp", ldapEntryManager.encodeTime(baseDn, new Date(System.currentTimeMillis() + 2 * 24 * 60 * 60 * 1000)))
         );
 
         int result = ldapEntryManager.remove(baseDn, DeletableEntity.class, filter, 100);
