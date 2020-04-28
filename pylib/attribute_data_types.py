d5aaadcdd9d7903729e37cb774e99a95f0d928a0
Mustafa Baser <mbaser@mail.com>
2020-04-28 15:49:09 +0300

    closes #662 fix package installation

--- pylib/attribute_data_types.py
+++ pylib/attribute_data_types.py
@@ -21,7 +21,6 @@ class ATTRUBUTEDATATYPES:
             self.startup(installDir)
 
     def startup(self, installDir):
-        print("Initilizing attribute data types")
         self.installDir = installDir
         opendjTypesFn = os.path.join(self.installDir, 'schema/opendj_types.json')
         self.attribTypes = json.load(open(opendjTypesFn))
