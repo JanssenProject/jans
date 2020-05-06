979158eca5810cd60b347bd85ff876fbc02a7174
Mustafa Baser <mbaser@mail.com>
2020-05-06 15:40:46 +0300

    move oxtrustapi_setup to ldap3 (ref: #661)

--- oxtrustapi_setup.py
+++ oxtrustapi_setup.py
@@ -3,14 +3,13 @@
 # Please place this script in the same directory as setup.py
 
 import os
-import ldap
 import xml.etree.ElementTree as ET
 import shutil
 import glob
-from setup import Setup
-from pylib.cbm import CBM
+import ldap3
 
-ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_ALLOW)
+from setup import Setup
+from pylib.dbutils import get_ldap_conn, get_cbm_conn
 
 scripts_location = 'ldap'
 
@@ -32,7 +31,7 @@ identtiy_xml_fn = '/opt/gluu/jetty/identity/webapps/identity.xml'
 tree = ET.parse(identtiy_xml_fn)
 root = tree.getroot()
 
-oxtrust_api_server_version = '4.0.rc2'
+oxtrust_api_server_version = '4.2.0-SNAPSHOT'
 oxtrust_api_server_url = 'https://ox.gluu.org/maven/org/gluu/oxtrust-api-server/{0}/oxtrust-api-server-{0}.jar'.format(oxtrust_api_server_version)
 oxtrust_api_server_path = '/opt/gluu/jetty/identity/custom/libs/oxtrust-api-server.jar'
 
@@ -71,44 +70,23 @@ else:
 print("Enabling custom script oxtrust_api_access_policy")
 
 if scripts_location == 'ldap':
-    # Obtain ldap binddn, server and password
-    for l in open(setupObject.ox_ldap_properties):
-        if l.startswith('bindPassword'):
-            crypted_passwd = l.split(':')[1].strip()
-            ldap_password = os.popen('/opt/gluu/bin/encode.py -D {}'.format(crypted_passwd)).read().strip()
-        elif l.startswith('servers'):
-            ls = l.strip()
-            n = ls.find(':')
-            s = ls[n+1:].strip()
-            servers_s = s.split(',')
-            ldap_server = servers_s[0].strip()
-        elif l.startswith('bindDN'):
-            ldap_binddn = l.split(':')[1].strip()
+    ldap_conn = get_ldap_conn()
 
-    # Enable custom script oxtrust_api_access_policy
-    ldap_conn = ldap.initialize('ldaps://{}'.format(ldap_server))
-    ldap_conn.simple_bind_s(ldap_binddn, ldap_password)
+    ldap_conn.search(
+                    search_base='inum=OO11-BAFE,ou=scripts,o=gluu', 
+                    search_scope=ldap3.BASE,
+                    search_filter='(objectclass=*)',
+                    attributes=["oxEnabled"]
+                    )
 
-    basedn = 'inum=OO11-BAFE,ou=scripts,o=gluu'
-    result = ldap_conn.search_s(basedn, ldap.SCOPE_BASE,  attrlist=['oxEnabled'])
-
-    if result and result[0][1]['oxEnabled'][0].lower() != 'true':
-        ldap_conn.modify_s(basedn, [(ldap.MOD_REPLACE, 'oxEnabled',  'true')])
+    if ldap_conn.response:
+        ldap_conn.modify(
+                        ldap_conn.response[0]['dn'], 
+                        {"oxEnabled": [ldap3.MODIFY_REPLACE, 'true']}
+                        )
 
 else:
-    # Obtain couchbase credidentals
-    for l in open(setupObject.gluuCouchebaseProperties):
-        ls = l.strip()
-        n = ls.find(':')
-        if ls.startswith('servers'):
-            server = ls[n+1:].strip().split(',')[0].strip()
-        elif ls.startswith('auth.userName'):
-            userName = ls[n+1:].strip()
-        elif ls.startswith('auth.userPassword'):
-            userPasswordEnc = ls[n+1:].strip()
-            userPassword = os.popen('/opt/gluu/bin/encode.py -D {}'.format(userPasswordEnc)).read().strip()
-    
-    cbm = CBM(server, userName, userPassword)
+    cbm = get_cbm_conn()
     result = cbm.exec_query('UPDATE `gluu` USE KEYS "scripts_OO11-BAFE" SET `oxEnabled`=true')
 
 print("Restarting identity, this will take a while")
