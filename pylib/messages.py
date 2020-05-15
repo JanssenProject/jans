3940b7680e1418222860222794b1e94730ff438b
Mustafa Baser <mbaser@mail.com>
2020-05-15 23:03:18 +0300

    tui: ask gluu storage for oxd

--- pylib/messages.py
+++ pylib/messages.py
@@ -90,6 +90,9 @@ class msg:
     ask_cb_install = "Couchbase Installation"
     cb_install_options = ["Don't Install","Install Locally","Use Remote Couchbase"]
     
+    ask_use_gluu_storage_oxd = "By default oxd uses its own db. Do you want to use Gluu Storage for Oxd?"
+    ask_use_gluu_storage_oxd_title = "Use Gluu Storage for Oxd?"
+    
     notify_select_backend = "Please select one of the backends either local install or remote" 
     weak_password = "Password for {} must be at least 6 characters and include one uppercase letter, one lowercase letter, one digit, and one special character."
     unselected_storages = "Note: Unselected storages will go Couchbase Server"
