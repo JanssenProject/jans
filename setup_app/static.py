773f245f5d8d16c90b3e943e7c45a9f8bb987d18
Mustafa Baser <mbaser@mail.com>
2020-05-23 02:02:44 +0300

    decompose and implement JreInstaller

---
+++ setup_app/static.py
@@ -0,0 +1,23 @@
+class InstallTypes:
+    NONE = 0
+    LOCAL = '1'
+    REMOTE = '2'
+
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    DANGER = '\033[31m'
+    
+COMPLETED = -99
+ERROR = -101
+
+suggested_mem_size = 3.7 # in GB
+suggested_number_of_cpu = 2
+suggested_free_disk_space = 40 #in GB
+file_max = 64000
