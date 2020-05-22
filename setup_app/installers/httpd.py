cf48c65ba959b8ff89a2db1988d5f1643e7753b2
Mustafa Baser <mbaser@mail.com>
2020-05-22 03:07:25 +0300

    initial commit of modular setup (ref: #494)

---
+++ setup_app/installers/httpd.py
@@ -0,0 +1,97 @@
+import os
+import glob
+
+
+from setup_app.utils.base import httpd_name, clone_type, \
+    os_initdaemon, os_type, determineApacheVersion
+
+from setup_app.config import Config
+from setup_app.utils.setup_utils import SetupUtils
+from setup_app.installers.base import BaseInstaller
+from setup_app import paths
+
+
+class HttpdInstaller(SetupUtils, BaseInstaller):
+
+    def __init__(self):
+        super().__init__()
+        self.service_name = httpd_name
+        self.apache_version = determineApacheVersion()
+
+    def configure(self):
+
+        self.stop()
+
+        # CentOS 7.* + systemd + apache 2.4
+        if self.service_name == 'httpd' and self.apache_version == "2.4":
+            self.copyFile(self.apache2_24_conf, '/etc/httpd/conf/httpd.conf')
+            self.copyFile(self.apache2_ssl_24_conf, '/etc/httpd/conf.d/https_gluu.conf')
+
+        if clone_type == 'rpm' and os_initdaemon == 'init':
+            self.copyFile(Config.apache2_conf, '/etc/httpd/conf/httpd.conf')
+            self.copyFile(Config.apache2_ssl_conf, '/etc/httpd/conf.d/https_gluu.conf')
+
+        if clone_type == 'deb':
+            self.copyFile(Config.apache2_ssl_conf, '/etc/apache2/sites-available/https_gluu.conf')
+            self.run([paths.cmd_ln, '-s', '/etc/apache2/sites-available/https_gluu.conf',
+                      '/etc/apache2/sites-enabled/https_gluu.conf'])
+
+        self.writeFile('/var/www/html/index.html', 'OK')
+
+        if clone_type == 'rpm':
+            icons_conf_fn = '/etc/httpd/conf.d/autoindex.conf'
+        else:
+            icons_conf_fn = '/etc/apache2/mods-available/alias.conf'
+
+        with open(icons_conf_fn[:]) as f:
+            icons_conf = f.readlines()
+
+        for i, l in enumerate(icons_conf[:]):
+            if l.strip().startswith('Alias') and ('/icons/' in l.strip().split()):
+                icons_conf[i] =  l.replace('Alias', '#Alias')
+
+        self.writeFile(icons_conf_fn, ''.join(icons_conf))
+
+        error_templates = glob.glob(os.path.join(Config.templateFolder,'apache/*'))
+        
+        for tmp_fn in error_templates:
+            self.copyFile(tmp_fn, '/var/www/html')
+
+        # we only need these modules
+        mods_enabled = ['env', 'proxy_http', 'access_compat', 'alias', 'authn_core', 'authz_core', 'authz_host', 'headers', 'mime', 'mpm_event', 'proxy', 'proxy_ajp', 'security2', 'reqtimeout', 'setenvif', 'socache_shmcb', 'ssl', 'unique_id']
+
+        if clone_type == 'rpm':
+
+            for mod_load_fn in glob.glob('/etc/httpd/conf.modules.d/*'):
+
+                with open(mod_load_fn) as f:
+                    mod_load_content = f.readlines()
+
+                modified = False
+                
+                for i, l in enumerate(mod_load_content[:]):
+                    ls = l.strip()
+                    
+                    if ls and not ls.startswith('#'):
+                        lsl = ls.split('/')
+                        module =  lsl[-1][4:-3]
+                        
+                        if not module in mods_enabled:
+                            mod_load_content[i] = l.replace('LoadModule', '#LoadModule')
+                            modified = True
+
+                if modified:
+                    self.writeFile(mod_load_fn, ''.join(mod_load_content))
+        else:
+
+            for mod_load_fn in glob.glob('/etc/apache2/mods-enabled/*'):
+                mod_load_base_name = os.path.basename(mod_load_fn)
+                f_name, f_ext = os.path.splitext(mod_load_base_name)
+
+                if not f_name in mods_enabled:
+                    self.run(['unlink', mod_load_fn])
+
+
+        self.enable()
+        self.start()
+        
