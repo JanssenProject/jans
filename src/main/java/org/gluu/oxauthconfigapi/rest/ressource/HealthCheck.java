e550a181885615a49721583d2a8691d6249d0724
Gasmyr <thomas@gluu.org>
2020-05-23 20:46:35 +0100

    Add jacoco for test coverage

--- src/main/java/org/gluu/oxauthconfigapi/rest/ressource/HealthCheck.java
+++ src/main/java/org/gluu/oxauthconfigapi/rest/ressource/HealthCheck.java
@@ -3,12 +3,13 @@ package org.gluu.oxauthconfigapi.rest.ressource;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
@@ -17,28 +18,22 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.gluu.oxauthconfigapi.rest.model.ApiHealth;
 import org.gluu.oxtrust.service.ClientService;
 
-import com.couchbase.client.core.message.ResponseStatus;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @Path("/health")
-@Consumes(APPLICATION_JSON)
 @Produces(APPLICATION_JSON)
 public class HealthCheck {
 
 	@Inject
 	ClientService clientService;
 
+	@Context
+	UriInfo uriInfo;
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Operation(summary = "Return json object containing the state of the api.")
 	@APIResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = ApiHealth.class, required = true)))
 	public Response health() {
-		try {
-			return Response.ok(new ObjectMapper().writeValueAsString(new ApiHealth(true, "running"))).build();
-		} catch (JsonProcessingException e) {
-			return Response.ok(ResponseStatus.INTERNAL_ERROR).build();
-		}
+		return Response.ok(new ApiHealth(true, "running")).build();
 	}
 
 }
--- src/test/java/org/gluu/oxauthconfigapi/HealthCheckTest.java
+++ src/test/java/org/gluu/oxauthconfigapi/HealthCheckTest.java
@@ -2,11 +2,13 @@ package org.gluu.oxauthconfigapi;
 
 import static io.restassured.RestAssured.given;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
+@Tag("integration")
 public class HealthCheckTest {
 
 	@Test
