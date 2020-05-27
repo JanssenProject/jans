59b81b51b6296621d016be69c6d6bf8d3f154eb3
Mustafa Baser <mbaser@mail.com>
2020-05-27 12:14:04 +0300

    implement oxauth-rp installation

--- setup_app/installers/base.py
+++ setup_app/installers/base.py
@@ -18,24 +18,26 @@ class BaseInstaller:
         self.install()
         self.copy_static()
 
-    def run_service_command(self, operation):
+    def run_service_command(self, operation, service):
+        if not service:
+            service = self.service_name
         try:
             if (clone_type == 'rpm' and os_initdaemon == 'systemd') or (os_name in ('ubuntu18','debian9','debian10')):
-                run([service_path, operation, self.service_name], None, None, True)
+                run([service_path, operation, service], None, None, True)
             else:
-                run([service_path, self.service_name, operation], None, None, True)
+                run([service_path, service, operation], None, None, True)
         except:
-            logIt("Error starting service '%s'" % operation)
+            logIt("Error running operation {} for service {}".format(operation, service))
             logIt(traceback.format_exc(), True)
 
-    def enable(self):
-        self.run_service_command('enable')
+    def enable(self, service=None):
+        self.run_service_command('enable', service)
 
-    def stop(self):
-        self.run_service_command('stop')
+    def stop(self, service=None):
+        self.run_service_command('stop', service)
 
-    def start(self):
-        self.run_service_command('start')
+    def start(self, service=None):       
+        self.run_service_command('start', service)
     
     def reload_daemon(self):
         if (clone_type == 'rpm' and os_initdaemon == 'systemd') or (os_name in ('ubuntu18','debian9','debian10')):
