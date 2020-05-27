ee773bd0b91eacf5613a238e9ee8982d8497f3bb
Milton BO <jmcm578@gmail.com>
2020-05-27 15:57:27 -0400

    Changed indentation to use spaces instead of tabs.

--- Server/src/main/java/org/gluu/oxauth/service/CibaRequestsProcessorJob.java
+++ Server/src/main/java/org/gluu/oxauth/service/CibaRequestsProcessorJob.java
@@ -40,138 +40,138 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @Named
 public class CibaRequestsProcessorJob {
 
-	@Inject
-	private Logger log;
+    @Inject
+    private Logger log;
 
-	@Inject
-	private AppConfiguration appConfiguration;
+    @Inject
+    private AppConfiguration appConfiguration;
 
-	@Inject
-	private Event<TimerEvent> processorEvent;
+    @Inject
+    private Event<TimerEvent> processorEvent;
 
-	@Inject
-	private CIBAPushErrorProxy cibaPushErrorProxy;
+    @Inject
+    private CIBAPushErrorProxy cibaPushErrorProxy;
 
-	@Inject
-	private CIBAPingCallbackProxy cibaPingCallbackProxy;
+    @Inject
+    private CIBAPingCallbackProxy cibaPingCallbackProxy;
 
-	@Inject
-	private AuthorizationGrantList authorizationGrantList;
+    @Inject
+    private AuthorizationGrantList authorizationGrantList;
 
-	@Inject
-	private GrantService grantService;
+    @Inject
+    private GrantService grantService;
 
-	private long lastFinishedTime;
+    private long lastFinishedTime;
 
-	private AtomicBoolean isActive;
+    private AtomicBoolean isActive;
 
-	private ExecutorService executorService;
+    private ExecutorService executorService;
 
-	/**
-	 * Method invoked from the appInitializer to start processing every some time.
-	 */
-	public void initTimer() {
-		log.debug("Initializing CIBA requests processor");
-		this.isActive = new AtomicBoolean(false);
-		int intervalSec = appConfiguration.getBackchannelRequestsProcessorJobIntervalSec();
+    /**
+     * Method invoked from the appInitializer to start processing every some time.
+     */
+    public void initTimer() {
+        log.debug("Initializing CIBA requests processor");
+        this.isActive = new AtomicBoolean(false);
+        int intervalSec = appConfiguration.getBackchannelRequestsProcessorJobIntervalSec();
 
-		// Schedule to start processor every N seconds
-		processorEvent.fire(new TimerEvent(new TimerSchedule(intervalSec, intervalSec),
-				new CibaRequestsProcessorEvent(), Scheduled.Literal.INSTANCE));
+        // Schedule to start processor every N seconds
+        processorEvent.fire(new TimerEvent(new TimerSchedule(intervalSec, intervalSec),
+                new CibaRequestsProcessorEvent(), Scheduled.Literal.INSTANCE));
 
-		this.lastFinishedTime = System.currentTimeMillis();
-		this.executorService = Executors.newCachedThreadPool(ServerUtil.daemonThreadFactory());
-	}
+        this.lastFinishedTime = System.currentTimeMillis();
+        this.executorService = Executors.newCachedThreadPool(ServerUtil.daemonThreadFactory());
+    }
 
-	@Asynchronous
-	public void process(@Observes @Scheduled CibaRequestsProcessorEvent cibaRequestsProcessorEvent) {
-		if (this.isActive.get()) {
-			return;
-		}
-		if (!this.isActive.compareAndSet(false, true)) {
-			return;
-		}
+    @Asynchronous
+    public void process(@Observes @Scheduled CibaRequestsProcessorEvent cibaRequestsProcessorEvent) {
+        if (this.isActive.get()) {
+            return;
+        }
+        if (!this.isActive.compareAndSet(false, true)) {
+            return;
+        }
 
-		try {
-			if (jobIsFree()) {
-				processImpl();
-				this.lastFinishedTime = System.currentTimeMillis();
-			} else {
-				log.trace("Starting conditions aren't reached for CIBA requestes processor");
-			}
-		} finally {
-			this.isActive.set(false);
-		}
-	}
+        try {
+            if (jobIsFree()) {
+                processImpl();
+                this.lastFinishedTime = System.currentTimeMillis();
+            } else {
+                log.trace("Starting conditions aren't reached for CIBA requestes processor");
+            }
+        } finally {
+            this.isActive.set(false);
+        }
+    }
 
-	/**
-	 * Defines whether the job is still in process or it is free according to the time interval defined.
-	 * @return True in case it is free to start a new process.
-	 */
-	private boolean jobIsFree() {
-		int interval = appConfiguration.getBackchannelRequestsProcessorJobIntervalSec();
-		if (interval < 0) {
-			log.info("CIBA Requests processor timer is disabled.");
-			log.warn("CIBA Requests processor timer Interval (cleanServiceInterval in oxauth configuration) is negative which turns OFF internal clean up by the server. Please set it to positive value if you wish internal CIBA Requests processor up timer run.");
-			return false;
-		}
+    /**
+     * Defines whether the job is still in process or it is free according to the time interval defined.
+     * @return True in case it is free to start a new process.
+     */
+    private boolean jobIsFree() {
+        int interval = appConfiguration.getBackchannelRequestsProcessorJobIntervalSec();
+        if (interval < 0) {
+            log.info("CIBA Requests processor timer is disabled.");
+            log.warn("CIBA Requests processor timer Interval (cleanServiceInterval in oxauth configuration) is negative which turns OFF internal clean up by the server. Please set it to positive value if you wish internal CIBA Requests processor up timer run.");
+            return false;
+        }
 
-		long timeDiffrence = System.currentTimeMillis() - this.lastFinishedTime;
-		return timeDiffrence >= interval * 1000;
-	}
+        long timeDiffrence = System.currentTimeMillis() - this.lastFinishedTime;
+        return timeDiffrence >= interval * 1000;
+    }
 
-	/**
-	 * Main process that process CIBA requests in cache.
-	 */
-	public void processImpl() {
-		try {
-			CIBACacheAuthReqIds cibaCacheAuthReqIds = grantService.getCacheCibaAuthReqIds();
-			for (Map.Entry<String, Long> entry : cibaCacheAuthReqIds.getAuthReqIds().entrySet()) {
-				Date now = new Date();
-				if (entry.getValue() < now.getTime()) {
-					CIBAGrant cibaGrant = authorizationGrantList.getCIBAGrant(entry.getKey());
-					if (cibaGrant != null) {
-						executorService.execute(() ->
-							processExpiredRequest(cibaGrant)
-						);
-					}
-					authorizationGrantList.removeCibaGrantFromProcessorCache(entry.getKey());
-				}
-			}
-		} catch (Exception e) {
-			log.error("Failed to process CIBA request from cache.", e);
-		}
-	}
+    /**
+     * Main process that process CIBA requests in cache.
+     */
+    public void processImpl() {
+        try {
+            CIBACacheAuthReqIds cibaCacheAuthReqIds = grantService.getCacheCibaAuthReqIds();
+            for (Map.Entry<String, Long> entry : cibaCacheAuthReqIds.getAuthReqIds().entrySet()) {
+                Date now = new Date();
+                if (entry.getValue() < now.getTime()) {
+                    CIBAGrant cibaGrant = authorizationGrantList.getCIBAGrant(entry.getKey());
+                    if (cibaGrant != null) {
+                        executorService.execute(() ->
+                            processExpiredRequest(cibaGrant)
+                        );
+                    }
+                    authorizationGrantList.removeCibaGrantFromProcessorCache(entry.getKey());
+                }
+            }
+        } catch (Exception e) {
+            log.error("Failed to process CIBA request from cache.", e);
+        }
+    }
 
-	/**
-	 * Method responsible to process expired CIBA grants, set them as expired in cache
-	 * and send callbacks to the client
-	 * @param cibaGrant Object containing data related to the CIBA grant.
-	 */
-	private void processExpiredRequest(CIBAGrant cibaGrant) {
-		if (cibaGrant.getUserAuthorization() != CIBAGrantUserAuthorization.AUTHORIZATION_PENDING
-				&& cibaGrant.getUserAuthorization() != CIBAGrantUserAuthorization.AUTHORIZATION_EXPIRED) {
-			return;
-		}
-		log.info("Authentication request id {} has expired", cibaGrant.getCIBAAuthenticationRequestId());
+    /**
+     * Method responsible to process expired CIBA grants, set them as expired in cache
+     * and send callbacks to the client
+     * @param cibaGrant Object containing data related to the CIBA grant.
+     */
+    private void processExpiredRequest(CIBAGrant cibaGrant) {
+        if (cibaGrant.getUserAuthorization() != CIBAGrantUserAuthorization.AUTHORIZATION_PENDING
+                && cibaGrant.getUserAuthorization() != CIBAGrantUserAuthorization.AUTHORIZATION_EXPIRED) {
+            return;
+        }
+        log.info("Authentication request id {} has expired", cibaGrant.getCIBAAuthenticationRequestId());
 
-		cibaGrant.setUserAuthorization(CIBAGrantUserAuthorization.AUTHORIZATION_EXPIRED);
-		cibaGrant.setTokensDelivered(false);
-		cibaGrant.save();
+        cibaGrant.setUserAuthorization(CIBAGrantUserAuthorization.AUTHORIZATION_EXPIRED);
+        cibaGrant.setTokensDelivered(false);
+        cibaGrant.save();
 
-		if (cibaGrant.getClient().getBackchannelTokenDeliveryMode() == BackchannelTokenDeliveryMode.PUSH) {
-			cibaPushErrorProxy.pushError(cibaGrant.getCIBAAuthenticationRequestId().getCode(),
-					cibaGrant.getClient().getBackchannelClientNotificationEndpoint(),
-					cibaGrant.getClientNotificationToken(),
-					PushErrorResponseType.EXPIRED_TOKEN,
-					"Request has expired and there was no answer from the end user.");
-		} else if (cibaGrant.getClient().getBackchannelTokenDeliveryMode() == BackchannelTokenDeliveryMode.PING) {
-			cibaPingCallbackProxy.pingCallback(
-					cibaGrant.getCIBAAuthenticationRequestId().getCode(),
-					cibaGrant.getClient().getBackchannelClientNotificationEndpoint(),
-					cibaGrant.getClientNotificationToken()
-			);
-		}
-	}
+        if (cibaGrant.getClient().getBackchannelTokenDeliveryMode() == BackchannelTokenDeliveryMode.PUSH) {
+            cibaPushErrorProxy.pushError(cibaGrant.getCIBAAuthenticationRequestId().getCode(),
+                    cibaGrant.getClient().getBackchannelClientNotificationEndpoint(),
+                    cibaGrant.getClientNotificationToken(),
+                    PushErrorResponseType.EXPIRED_TOKEN,
+                    "Request has expired and there was no answer from the end user.");
+        } else if (cibaGrant.getClient().getBackchannelTokenDeliveryMode() == BackchannelTokenDeliveryMode.PING) {
+            cibaPingCallbackProxy.pingCallback(
+                    cibaGrant.getCIBAAuthenticationRequestId().getCode(),
+                    cibaGrant.getClient().getBackchannelClientNotificationEndpoint(),
+                    cibaGrant.getClientNotificationToken()
+            );
+        }
+    }
 
 }
