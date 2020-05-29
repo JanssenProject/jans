ba99f20dc265c37564abbcf6c353aad8fdd3fcf7
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2020-05-29 00:19:03 -0400

    oxAuth #1316 CIBA: Conformance Testing for FAPI-CIBA Enabling private_key_jwt authentication for the backchannel authentication endpoint.

--- Client/src/main/java/org/gluu/oxauth/client/BackchannelAuthenticationRequest.java
+++ Client/src/main/java/org/gluu/oxauth/client/BackchannelAuthenticationRequest.java
@@ -7,22 +7,31 @@
 package org.gluu.oxauth.client;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.gluu.oxauth.model.ciba.BackchannelAuthenticationRequestParam;
 import org.gluu.oxauth.model.common.AuthenticationMethod;
+import org.gluu.oxauth.model.crypto.AbstractCryptoProvider;
+import org.gluu.oxauth.model.crypto.signature.SignatureAlgorithm;
+import org.gluu.oxauth.model.exception.InvalidJwtException;
+import org.gluu.oxauth.model.jwt.Jwt;
+import org.gluu.oxauth.model.jwt.JwtType;
+import org.gluu.oxauth.model.token.ClientAssertionType;
 import org.gluu.oxauth.model.util.Util;
 
 import javax.ws.rs.core.MediaType;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.List;
+import java.util.*;
 
 /**
  * Represents a CIBA backchannel authorization request to send to the authorization server.
  *
  * @author Javier Rojas Blum
- * @version August 20, 2019
+ * @version May 28, 2020
  */
 public class BackchannelAuthenticationRequest extends BaseRequest {
+
+    private static final Logger LOG = Logger.getLogger(TokenRequest.class);
 
     private List<String> scope;
     private String clientNotificationToken;
@@ -33,6 +42,12 @@ public class BackchannelAuthenticationRequest extends BaseRequest {
     private String bindingMessage;
     private String userCode;
     private Integer requestedExpiry;
+
+    private SignatureAlgorithm algorithm;
+    private String sharedKey;
+    private AbstractCryptoProvider cryptoProvider;
+    private String keyId;
+    private String audience;
 
     public BackchannelAuthenticationRequest() {
         setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -111,6 +126,93 @@ public class BackchannelAuthenticationRequest extends BaseRequest {
         this.requestedExpiry = requestedExpiry;
     }
 
+    public SignatureAlgorithm getAlgorithm() {
+        return algorithm;
+    }
+
+    public void setAlgorithm(SignatureAlgorithm algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public String getSharedKey() {
+        return sharedKey;
+    }
+
+    public void setSharedKey(String sharedKey) {
+        this.sharedKey = sharedKey;
+    }
+
+    public AbstractCryptoProvider getCryptoProvider() {
+        return cryptoProvider;
+    }
+
+    public void setCryptoProvider(AbstractCryptoProvider cryptoProvider) {
+        this.cryptoProvider = cryptoProvider;
+    }
+
+    public String getKeyId() {
+        return keyId;
+    }
+
+    public void setKeyId(String keyId) {
+        this.keyId = keyId;
+    }
+
+    public String getAudience() {
+        return audience;
+    }
+
+    public void setAudience(String audience) {
+        this.audience = audience;
+    }
+
+    public String getClientAssertion() {
+        if (cryptoProvider == null) {
+            LOG.error("Crypto provider is not specified");
+            return null;
+        }
+
+        Jwt clientAssertion = new Jwt();
+
+        if (algorithm == null) {
+            algorithm = SignatureAlgorithm.HS256;
+        }
+        GregorianCalendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+        Date issuedAt = calendar.getTime();
+        calendar.add(Calendar.MINUTE, 5);
+        Date expirationTime = calendar.getTime();
+
+        // Header
+        clientAssertion.getHeader().setType(JwtType.JWT);
+        clientAssertion.getHeader().setAlgorithm(algorithm);
+        if (StringUtils.isNotBlank(keyId)) {
+            clientAssertion.getHeader().setKeyId(keyId);
+        }
+
+        // Claims
+        clientAssertion.getClaims().setIssuer(getAuthUsername());
+        clientAssertion.getClaims().setSubjectIdentifier(getAuthUsername());
+        clientAssertion.getClaims().setAudience(audience);
+        clientAssertion.getClaims().setJwtId(UUID.randomUUID());
+        clientAssertion.getClaims().setExpirationTime(expirationTime);
+        clientAssertion.getClaims().setIssuedAt(issuedAt);
+
+        // Signature
+        try {
+            if (sharedKey == null) {
+                sharedKey = getAuthPassword();
+            }
+            String signature = cryptoProvider.sign(clientAssertion.getSigningInput(), keyId, sharedKey, algorithm);
+            clientAssertion.setEncodedSignature(signature);
+        } catch (InvalidJwtException e) {
+            LOG.error(e.getMessage(), e);
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
+
+        return clientAssertion.toString();
+    }
+
     @Override
     public String getQueryString() {
         StringBuilder queryStringBuilder = new StringBuilder();
@@ -154,6 +256,22 @@ public class BackchannelAuthenticationRequest extends BaseRequest {
             if (requestedExpiry != null) {
                 queryStringBuilder.append("&").append(BackchannelAuthenticationRequestParam.REQUESTED_EXPIRY)
                         .append("=").append(URLEncoder.encode(requestedExpiry.toString(), Util.UTF8_STRING_ENCODING));
+            }
+            if (getAuthenticationMethod() == AuthenticationMethod.CLIENT_SECRET_POST) {
+                if (getAuthUsername() != null && !getAuthUsername().isEmpty()) {
+                    queryStringBuilder.append("&").append(BackchannelAuthenticationRequestParam.CLIENT_ID)
+                            .append("=").append(URLEncoder.encode(getAuthUsername(), Util.UTF8_STRING_ENCODING));
+                }
+                if (getAuthPassword() != null && !getAuthPassword().isEmpty()) {
+                    queryStringBuilder.append("&").append(BackchannelAuthenticationRequestParam.CLIENT_SECRET)
+                            .append("=").append(URLEncoder.encode(getAuthPassword(), Util.UTF8_STRING_ENCODING));
+                }
+            } else if (getAuthenticationMethod() == AuthenticationMethod.CLIENT_SECRET_JWT ||
+                    getAuthenticationMethod() == AuthenticationMethod.PRIVATE_KEY_JWT) {
+                queryStringBuilder.append("&").append(BackchannelAuthenticationRequestParam.CLIENT_ASSERTION_TYPE)
+                        .append("=").append(URLEncoder.encode(ClientAssertionType.JWT_BEARER.toString(), Util.UTF8_STRING_ENCODING));
+                queryStringBuilder.append("&").append(BackchannelAuthenticationRequestParam.CLIENT_ASSERTION)
+                        .append("=").append(getClientAssertion());
             }
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
