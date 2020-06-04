a20ec52b2edb932928ce690baefd941adcc526eb
yurem <Yuriy.Movchan@gmail.com>
2020-06-04 09:04:21 +0300

    Allow to reuse persistence models

---
+++ model/src/main/java/org/gluu/fido2/ctap/AttestationConveyancePreference.java
@@ -0,0 +1,13 @@
+/*
+ * oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2020, Gluu
+ */
+
+package org.gluu.fido2.ctap;
+
+public enum AttestationConveyancePreference {
+
+	direct, indirect, none
+
+}
