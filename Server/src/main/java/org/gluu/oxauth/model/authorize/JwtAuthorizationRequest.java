f6e997e870b34187902b1b7cea2f1654b897600f
Milton BO <jmcm578@gmail.com>
2020-06-09 14:44:47 -0400

    Added CIBA JWT request, also some refactoring in order to reuse more code with current JwtAuthorizationRequest. #1387

--- Server/src/main/java/org/gluu/oxauth/model/authorize/JwtAuthorizationRequest.java
+++ Server/src/main/java/org/gluu/oxauth/model/authorize/JwtAuthorizationRequest.java
@@ -6,170 +6,53 @@
 
 package org.gluu.oxauth.model.authorize;
 
-import com.google.common.base.Strings;
-import com.google.common.collect.Lists;
 import org.apache.commons.lang.StringUtils;
 import org.gluu.oxauth.model.common.Display;
 import org.gluu.oxauth.model.common.Prompt;
 import org.gluu.oxauth.model.common.ResponseType;
 import org.gluu.oxauth.model.configuration.AppConfiguration;
 import org.gluu.oxauth.model.crypto.AbstractCryptoProvider;
-import org.gluu.oxauth.model.crypto.encryption.BlockEncryptionAlgorithm;
-import org.gluu.oxauth.model.crypto.encryption.KeyEncryptionAlgorithm;
-import org.gluu.oxauth.model.crypto.signature.SignatureAlgorithm;
-import org.gluu.oxauth.model.exception.InvalidJweException;
 import org.gluu.oxauth.model.exception.InvalidJwtException;
-import org.gluu.oxauth.model.jwe.Jwe;
-import org.gluu.oxauth.model.jwe.JweDecrypterImpl;
-import org.gluu.oxauth.model.jwt.JwtHeader;
-import org.gluu.oxauth.model.jwt.JwtHeaderName;
 import org.gluu.oxauth.model.registration.Client;
-import org.gluu.oxauth.model.util.Base64Util;
-import org.gluu.oxauth.model.util.JwtUtil;
 import org.gluu.oxauth.model.util.Util;
-import org.gluu.oxauth.service.ClientService;
-import org.gluu.service.cdi.util.CdiUtil;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.security.PrivateKey;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author Javier Rojas Blum
  * @version November 20, 2018
  */
-public class JwtAuthorizationRequest {
-
-    // Header
-    private String type;
-    private String algorithm;
-    private String encryptionAlgorithm;
-    private String keyId;
+public class JwtAuthorizationRequest extends JwtSignedRequest {
 
     // Payload
     private List<ResponseType> responseTypes;
     private String clientId;
-    private List<String> scopes;
     private String redirectUri;
     private String nonce;
     private String state;
-    private List<String> aud = Lists.newArrayList();
     private Display display;
     private List<Prompt> prompts;
     private UserInfoMember userInfoMember;
     private IdTokenMember idTokenMember;
-    private Integer exp;
-
-    private String encodedJwt;
-    private String payload;
-
-    private AppConfiguration appConfiguration;
-
-    public JwtAuthorizationRequest(AppConfiguration appConfiguration, AbstractCryptoProvider cryptoProvider, String encodedJwt, Client client) throws InvalidJwtException, InvalidJweException {
-        try {
-            this.appConfiguration = appConfiguration;
-            this.responseTypes = new ArrayList<ResponseType>();
-            this.scopes = new ArrayList<String>();
-            this.prompts = new ArrayList<Prompt>();
-            this.encodedJwt = encodedJwt;
-
-            if (StringUtils.isEmpty(encodedJwt)) {
-                throw new InvalidJwtException("The JWT is null or empty");
-            }
 
 
-            String[] parts = encodedJwt.split("\\.");
-
-            if (parts.length == 5) {
-                String encodedHeader = parts[0];
-
-                JwtHeader jwtHeader = new JwtHeader(encodedHeader);
-
-                keyId = jwtHeader.getKeyId();
-                KeyEncryptionAlgorithm keyEncryptionAlgorithm = KeyEncryptionAlgorithm.fromName(
-                        jwtHeader.getClaimAsString(JwtHeaderName.ALGORITHM));
-                BlockEncryptionAlgorithm blockEncryptionAlgorithm = BlockEncryptionAlgorithm.fromName(
-                        jwtHeader.getClaimAsString(JwtHeaderName.ENCRYPTION_METHOD));
-
-                JweDecrypterImpl jweDecrypter = null;
-                if ("RSA".equals(keyEncryptionAlgorithm.getFamily())) {
-                    PrivateKey privateKey = cryptoProvider.getPrivateKey(keyId);
-                    jweDecrypter = new JweDecrypterImpl(privateKey);
-                } else {
-                    ClientService clientService = CdiUtil.bean(ClientService.class);
-                    jweDecrypter = new JweDecrypterImpl(clientService.decryptSecret(client.getClientSecret()).getBytes(StandardCharsets.UTF_8));
-                }
-                jweDecrypter.setKeyEncryptionAlgorithm(keyEncryptionAlgorithm);
-                jweDecrypter.setBlockEncryptionAlgorithm(blockEncryptionAlgorithm);
-
-                Jwe jwe = jweDecrypter.decrypt(encodedJwt);
-
-                loadHeader(jwe.getHeader().toJsonString());
-                loadPayload(jwe.getClaims().toJsonString());
-            } else if (parts.length == 2 || parts.length == 3) {
-                String encodedHeader = parts[0];
-                String encodedClaim = parts[1];
-                String encodedSignature = StringUtils.EMPTY;
-                if (parts.length == 3) {
-                    encodedSignature = parts[2];
-                }
-
-                String signingInput = encodedHeader + "." + encodedClaim;
-                String header = new String(Base64Util.base64urldecode(encodedHeader), StandardCharsets.UTF_8);
-                String payload = new String(Base64Util.base64urldecode(encodedClaim), StandardCharsets.UTF_8);
-                payload = payload.replace("\\", "");
-
-                loadHeader(header);
-
-                SignatureAlgorithm sigAlg = SignatureAlgorithm.fromString(algorithm);
-                if (sigAlg == null) {
-                    throw new InvalidJwtException("The JWT algorithm is not supported");
-                }
-                if (sigAlg == SignatureAlgorithm.NONE && appConfiguration.getFapiCompatibility()) {
-                    throw new InvalidJwtException("None algorithm is not allowed for FAPI");
-                }
-                if (!validateSignature(cryptoProvider, sigAlg, client, signingInput, encodedSignature)) {
-                    throw new InvalidJwtException("The JWT signature is not valid");
-                }
-
-                loadPayload(payload);
-            } else {
-                throw new InvalidJwtException("The JWT is not well formed");
-            }
-
-        } catch (Exception e) {
-            throw new InvalidJwtException(e);
-        }
+    public JwtAuthorizationRequest(AppConfiguration appConfiguration,
+                                   AbstractCryptoProvider cryptoProvider,
+                                   String encodedJwt, Client client) throws InvalidJwtException {
+        super(appConfiguration, cryptoProvider, encodedJwt, client);
     }
 
-    public String getEncodedJwt() {
-        return encodedJwt;
-    }
-
-    private void loadHeader(String header) throws JSONException {
-        JSONObject jsonHeader = new JSONObject(header);
-
-        if (jsonHeader.has("typ")) {
-            type = jsonHeader.getString("typ");
-        }
-        if (jsonHeader.has("alg")) {
-            algorithm = jsonHeader.getString("alg");
-        }
-        if (jsonHeader.has("enc")) {
-            encryptionAlgorithm = jsonHeader.getString("enc");
-        }
-        if (jsonHeader.has("kid")) {
-            keyId = jsonHeader.getString("kid");
-        }
-    }
-
-    private void loadPayload(String payload) throws JSONException, UnsupportedEncodingException {
+    /**
+     * Method responsible to load corresponding data according to the type of request.
+     * @param payload Payload containing custom data related to every type of request.
+     */
+    @Override
+    void loadPayload(String payload) throws JSONException, UnsupportedEncodingException {
         JSONObject jsonPayload = new JSONObject(payload);
 
         if (jsonPayload.has("response_type")) {
@@ -247,35 +130,6 @@ public class JwtAuthorizationRequest {
         this.payload = payload;
     }
 
-    private boolean validateSignature(AbstractCryptoProvider cryptoProvider, SignatureAlgorithm signatureAlgorithm, Client client, String signingInput, String signature) throws Exception {
-        ClientService clientService = CdiUtil.bean(ClientService.class);
-        String sharedSecret = clientService.decryptSecret(client.getClientSecret());
-        JSONObject jwks = Strings.isNullOrEmpty(client.getJwks()) ?
-                JwtUtil.getJSONWebKeys(client.getJwksUri()) :
-                new JSONObject(client.getJwks());
-        return cryptoProvider.verifySignature(signingInput, signature, keyId, jwks, sharedSecret, signatureAlgorithm);
-    }
-
-    public String getKeyId() {
-        return keyId;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getAlgorithm() {
-        return algorithm;
-    }
-
-    public void setAlgorithm(String algorithm) {
-        this.algorithm = algorithm;
-    }
-
     public List<ResponseType> getResponseTypes() {
         return responseTypes;
     }
@@ -290,14 +144,6 @@ public class JwtAuthorizationRequest {
 
     public void setClientId(String clientId) {
         this.clientId = clientId;
-    }
-
-    public List<String> getScopes() {
-        return scopes;
-    }
-
-    public void setScopes(List<String> scopes) {
-        this.scopes = scopes;
     }
 
     public String getRedirectUri() {
@@ -356,24 +202,4 @@ public class JwtAuthorizationRequest {
         this.idTokenMember = idTokenMember;
     }
 
-    public Integer getExp() {
-        return exp;
-    }
-
-    public List<String> getAud() {
-        if (aud == null) aud = Lists.newArrayList();
-        return aud;
-    }
-
-    public void setAud(List<String> aud) {
-        this.aud = aud;
-    }
-
-    public String getPayload() {
-        return payload;
-    }
-
-    public void setPayload(String payload) {
-        this.payload = payload;
-    }
 }
