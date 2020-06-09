af1dbda7ab633fe77c116c3a94d9ac830f180300
Milton BO <jmcm578@gmail.com>
2020-06-09 15:30:23 -0400

    Implemented JWT request for CIBA authorization requests. #1387

--- Server/src/main/java/org/gluu/oxauth/bcauthorize/ws/rs/BackchannelAuthorizeRestWebService.java
+++ Server/src/main/java/org/gluu/oxauth/bcauthorize/ws/rs/BackchannelAuthorizeRestWebService.java
@@ -37,6 +37,7 @@ public interface BackchannelAuthorizeRestWebService {
             @FormParam("binding_message") String bindingMessage,
             @FormParam("user_code") String userCode,
             @FormParam("requested_expiry") Integer requestedExpiry,
+            @FormParam("request") String request,
             @Context HttpServletRequest httpRequest,
             @Context HttpServletResponse httpResponse,
             @Context SecurityContext securityContext
