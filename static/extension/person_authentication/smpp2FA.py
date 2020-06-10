c676ab35e379c9f0f1461025a0c1be522f833805
Mustafa Baser <mbaser@mail.com>
2020-06-10 17:05:36 +0300

    fix path for UserService in EncryptionService custom scripts

--- static/extension/person_authentication/smpp2FA.py
+++ static/extension/person_authentication/smpp2FA.py
@@ -13,7 +13,8 @@ from java.lang import Enum
 from org.gluu.service.cdi.util import CdiUtil
 from org.gluu.oxauth.security import Identity
 from org.gluu.model.custom.script.type.auth import PersonAuthenticationType
-from org.gluu.oxauth.service import UserService, AuthenticationService
+from org.gluu.oxauth.service import AuthenticationService
+from org.gluu.oxauth.service.common import UserService
 from org.gluu.oxauth.util import ServerUtil
 from org.gluu.util import StringHelper, ArrayHelper
 from javax.faces.application import FacesMessage
