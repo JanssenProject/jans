6d407780327ad3b42f9f9801e39b98d1504a544a
Arnab Dutta <arnab.bdutta@gmail.com>
2020-06-10 10:40:48 +0530

    #498 - Use WebFinger (RFC7033) and OpenID Provider Issuer Discovery to determine the location of the OpenID Provider

--- oxd-gen-client/src/test/java/io/swagger/client/api/GetIssuerTest.java
+++ oxd-gen-client/src/test/java/io/swagger/client/api/GetIssuerTest.java
@@ -1,7 +1,5 @@
 package io.swagger.client.api;
 
-import io.swagger.client.model.GetDiscoveryParams;
-import io.swagger.client.model.GetDiscoveryResponse;
 import io.swagger.client.model.GetIssuerParams;
 import io.swagger.client.model.GetIssuerResponse;
 import org.testng.annotations.Parameters;
@@ -11,52 +9,52 @@ import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertNotNull;
 
 public class GetIssuerTest {
-    private static final String EMAIL_ADDRESS_INPUT = "admin@jenkins-ldap.gluu.org";
-    private static final String URL_INPUT = "https://jenkins-ldap.gluu.org/admin";
-    private static final String HOSTNAME_PORT_INPUT = "jenkins-ldap.gluu.org";
 
+    @Parameters({"opHost", "emailWebfingerInput"})
     @Test
-    public void emailInputTest() throws Exception {
+    public void emailInputTest(String opHost, String emailWebfingerInput) throws Exception {
         DevelopersApi api = Tester.api();
 
         final GetIssuerParams commandParams = new GetIssuerParams();
-        commandParams.setResource(EMAIL_ADDRESS_INPUT);
+        commandParams.setResource(emailWebfingerInput);
 
         final GetIssuerResponse resp = api.getIssuer(commandParams);
         assertNotNull(resp);
-        assertEquals(resp.getSubject(), EMAIL_ADDRESS_INPUT);
+        assertEquals(resp.getSubject(), emailWebfingerInput);
         resp.getLinks().forEach((link) -> {
-            assertEquals(link.getHref(), "https://jenkins-ldap.gluu.org");
+            assertEquals(link.getHref(), opHost);
         });
     }
 
+    @Parameters({"opHost", "urlWebfingerInput"})
     @Test
-    public void urlInputTest() throws Exception {
+    public void urlInputTest(String opHost, String urlWebfingerInput) throws Exception {
         DevelopersApi api = Tester.api();
 
         final GetIssuerParams commandParams = new GetIssuerParams();
-        commandParams.setResource(URL_INPUT);
+        commandParams.setResource(urlWebfingerInput);
 
         final GetIssuerResponse resp = api.getIssuer(commandParams);
         assertNotNull(resp);
-        assertEquals(resp.getSubject(), URL_INPUT);
+        assertEquals(resp.getSubject(), urlWebfingerInput);
         resp.getLinks().forEach((link) -> {
-            assertEquals(link.getHref(), "https://jenkins-ldap.gluu.org");
+            assertEquals(link.getHref(), opHost);
         });
     }
 
+    @Parameters({"opHost", "hostnameWebfingerInput"})
     @Test
-    public void hostnameInputTest() throws Exception {
+    public void hostnameInputTest(String opHost, String hostnameWebfingerInput) throws Exception {
         DevelopersApi api = Tester.api();
 
         final GetIssuerParams commandParams = new GetIssuerParams();
-        commandParams.setResource(HOSTNAME_PORT_INPUT);
+        commandParams.setResource(hostnameWebfingerInput);
 
         final GetIssuerResponse resp = api.getIssuer(commandParams);
         assertNotNull(resp);
-        assertEquals(resp.getSubject(), HOSTNAME_PORT_INPUT);
+        assertEquals(resp.getSubject(), hostnameWebfingerInput);
         resp.getLinks().forEach((link) -> {
-            assertEquals(link.getHref(), "https://jenkins-ldap.gluu.org");
+            assertEquals(link.getHref(), opHost);
         });
     }
 }
