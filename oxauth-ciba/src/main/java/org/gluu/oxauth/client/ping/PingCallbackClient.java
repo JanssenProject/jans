fcb28a970b789549794a0935c7473bd798475676
Milton BO <jmcm578@gmail.com>
2020-06-16 12:05:59 -0400

    Changed the way to throw exceptions in CIBA JWT request validation method. Moved the method to create SSL clients using custom TLS and cipher values. #1316

--- oxauth-ciba/src/main/java/org/gluu/oxauth/client/ping/PingCallbackClient.java
+++ oxauth-ciba/src/main/java/org/gluu/oxauth/client/ping/PingCallbackClient.java
@@ -7,24 +7,12 @@
 package org.gluu.oxauth.client.ping;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.http.client.config.CookieSpecs;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.ssl.SSLContexts;
 import org.apache.log4j.Logger;
 import org.gluu.oxauth.client.BaseClient;
+import org.gluu.oxauth.util.ClientUtil;
 import org.jboss.resteasy.client.core.executors.ApacheHttpClient4Executor;
 import org.json.JSONObject;
 
-import javax.net.ssl.SSLContext;
 import javax.ws.rs.HttpMethod;
 import javax.ws.rs.core.MediaType;
 
@@ -83,34 +71,12 @@ public class PingCallbackClient extends BaseClient<PingCallbackRequest, PingCall
     }
 
     /**
-     * Creates an executor responsible to process rest calls using special SSL context.
+     * Creates an executor responsible to process rest calls using special SSL context defined in FAPI-CIBA specs.
      */
     private ApacheHttpClient4Executor getApacheHttpClient4ExecutorForMTLS() {
-        try {
-            // Ciphers accepted by FAPI-CIBA specs and OpenJDK.
-            String[] ciphers = new String[] { "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384" };
-            SSLContext sslContext = SSLContexts.createDefault();
-            SSLConnectionSocketFactory sslConnectionFactory = new SSLConnectionSocketFactory(sslContext,
-                    new String[] { "TLSv1.2" }, ciphers, NoopHostnameVerifier.INSTANCE);
-
-            Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory> create()
-                    .register("https", sslConnectionFactory)
-                    .register("http", new PlainConnectionSocketFactory())
-                    .build();
-
-            PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(registry);
-
-            CloseableHttpClient httpClient = HttpClients.custom()
-                    .setSSLContext(sslContext)
-                    .setDefaultRequestConfig(RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build())
-                    .setConnectionManager(cm)
-                    .build();
-
-            return new ApacheHttpClient4Executor(httpClient);
-        } catch (Exception e) {
-            LOG.error("Error creating Ping rest client, specific creating executor for SSL Context", e);
-            return null;
-        }
+        // Ciphers accepted by FAPI-CIBA specs and OpenJDK.
+        String[] ciphers = new String[] { "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384" };
+        return new ApacheHttpClient4Executor(ClientUtil.createHttpClient("TLSv1.2", ciphers));
     }
 
 }
