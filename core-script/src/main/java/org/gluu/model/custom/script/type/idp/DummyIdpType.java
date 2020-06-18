f32e59944c5cd9d4c7ec5fb28891a4ad20454a8d
yurem <Yuriy.Movchan@gmail.com>
2020-06-18 14:11:12 +0300

    Add Idp custom script type

--- core-script/src/main/java/org/gluu/model/custom/script/type/idp/DummyIdpType.java
+++ core-script/src/main/java/org/gluu/model/custom/script/type/idp/DummyIdpType.java
@@ -2,18 +2,18 @@
  * oxCore is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
  *
  * Copyright (c) 2020, Gluu
- */package org.gluu.model.custom.script.type.persistence;
+ */package org.gluu.model.custom.script.type.idp;
 
 import java.util.Map;
 
 import org.gluu.model.SimpleCustomProperty;
 import org.gluu.model.custom.script.model.CustomScript;
 /**
- * Dummy implementation of interface DynamicPeristanceType
+ * Dummy implementation of interface IdpType
  *
- * @author Yuriy Movchan Date: 06/04/2020
+ * @author Yuriy Movchan Date: 06/18/2020
  */
-public class DummyDynamicPeristenceType implements PersistenceType {
+public class DummyIdpType implements IdpType {
 
 	@Override
 	public boolean init(Map<String, SimpleCustomProperty> configurationAttributes) {
@@ -36,20 +36,7 @@ public class DummyDynamicPeristenceType implements PersistenceType {
 	}
 
 	@Override
-	public void onAfterCreate(Object context, Map<String, SimpleCustomProperty> configurationAttributes) {
-	}
-
-	@Override
-	public void onAfterDestroy(Object context, Map<String, SimpleCustomProperty> configurationAttributes) {
-	}
-
-	@Override
-	public String createHashedPassword(String credential) {
-		return credential;
-	}
-
-	@Override
-	public boolean compareHashedPasswords(String credential, String storedCredential) {
+	public boolean updateAttributes(Object context, Map<String, SimpleCustomProperty> configurationAttributes) {
 		return false;
 	}
 
