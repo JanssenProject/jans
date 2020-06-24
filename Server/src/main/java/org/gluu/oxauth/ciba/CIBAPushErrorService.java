b25cb92b9ebf9f04cd90a246da419c3799721d30
Milton BO <jmcm578@gmail.com>
2020-06-24 17:47:40 -0400

    Refactorization, removed CIBA interceptors, instead it uses Services

---
+++ Server/src/main/java/org/gluu/oxauth/ciba/CIBAPushErrorService.java
@@ -0,0 +1,44 @@
+/*
+ * oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2014, Gluu
+ */
+
+package org.gluu.oxauth.ciba;
+
+import org.gluu.oxauth.client.ciba.push.PushErrorClient;
+import org.gluu.oxauth.client.ciba.push.PushErrorRequest;
+import org.gluu.oxauth.client.ciba.push.PushErrorResponse;
+import org.gluu.oxauth.model.ciba.PushErrorResponseType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.Stateless;
+import javax.inject.Named;
+
+/**
+ * @author Javier Rojas Blum
+ * @version May 9, 2020
+ */
+@Stateless
+@Named
+public class CIBAPushErrorService {
+
+    private final static Logger log = LoggerFactory.getLogger(CIBAPushErrorService.class);
+
+    public void pushError(String authReqId, String clientNotificationEndpoint, String clientNotificationToken,
+                          PushErrorResponseType error, String errorDescription) {
+        PushErrorRequest pushErrorRequest = new PushErrorRequest();
+
+        pushErrorRequest.setClientNotificationToken(clientNotificationToken);
+        pushErrorRequest.setAuthReqId(authReqId);
+        pushErrorRequest.setErrorType(error);
+        pushErrorRequest.setErrorDescription(errorDescription);
+
+        PushErrorClient pushErrorClient = new PushErrorClient(clientNotificationEndpoint);
+        pushErrorClient.setRequest(pushErrorRequest);
+        PushErrorResponse pushErrorResponse = pushErrorClient.exec();
+
+        log.debug("CIBA: push error result status " + pushErrorResponse.getStatus());
+    }
+}
---
+++ Server/src/main/java/org/gluu/oxauth/ciba/CIBAPushTokenDeliveryService.java
@@ -0,0 +1,47 @@
+/*
+ * oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2014, Gluu
+ */
+
+package org.gluu.oxauth.ciba;
+
+import org.gluu.oxauth.client.ciba.push.PushTokenDeliveryClient;
+import org.gluu.oxauth.client.ciba.push.PushTokenDeliveryRequest;
+import org.gluu.oxauth.client.ciba.push.PushTokenDeliveryResponse;
+import org.gluu.oxauth.model.common.TokenType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.Stateless;
+import javax.inject.Named;
+
+/**
+ * @author Javier Rojas Blum
+ * @version September 4, 2019
+ */
+@Stateless
+@Named
+public class CIBAPushTokenDeliveryService {
+
+    private final static Logger log = LoggerFactory.getLogger(CIBAPushTokenDeliveryService.class);
+
+    public void pushTokenDelivery(String authReqId, String clientNotificationEndpoint, String clientNotificationToken,
+                                  String accessToken, String refreshToken, String idToken, Integer expiresIn) {
+        PushTokenDeliveryRequest pushTokenDeliveryRequest = new PushTokenDeliveryRequest();
+
+        pushTokenDeliveryRequest.setClientNotificationToken(clientNotificationToken);
+        pushTokenDeliveryRequest.setAuthReqId(authReqId);
+        pushTokenDeliveryRequest.setAccessToken(accessToken);
+        pushTokenDeliveryRequest.setTokenType(TokenType.BEARER);
+        pushTokenDeliveryRequest.setRefreshToken(refreshToken);
+        pushTokenDeliveryRequest.setExpiresIn(expiresIn);
+        pushTokenDeliveryRequest.setIdToken(idToken);
+
+        PushTokenDeliveryClient pushTokenDeliveryClient = new PushTokenDeliveryClient(clientNotificationEndpoint);
+        pushTokenDeliveryClient.setRequest(pushTokenDeliveryRequest);
+        PushTokenDeliveryResponse pushTokenDeliveryResponse = pushTokenDeliveryClient.exec();
+
+        log.debug("CIBA: push token delivery result status " + pushTokenDeliveryResponse.getStatus());
+    }
+}
