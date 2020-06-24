f687fd584a262785730d3f717570f3f60c6e01cb
yurem <Yuriy.Movchan@gmail.com>
2020-06-24 12:44:38 +0300

    Add translate attributes method to IDP script

--- core-script/src/test/java/org/gluu/service/custom/script/test/SampleIdpExternalScriptService.java
+++ core-script/src/test/java/org/gluu/service/custom/script/test/SampleIdpExternalScriptService.java
@@ -16,38 +16,70 @@ import org.gluu.service.custom.script.ExternalScriptService;
  */
 public class SampleIdpExternalScriptService extends ExternalScriptService {
 
-    private static final long serialVersionUID = -1316361273036208685L;
+	private static final long serialVersionUID = -1316361273036208685L;
 
-    public SampleIdpExternalScriptService() {
-        super(CustomScriptType.IDP);
-    }
+	public SampleIdpExternalScriptService() {
+		super(CustomScriptType.IDP);
+	}
 
-    public boolean executeExternalUpdateAttributesMethod(Object context, CustomScriptConfiguration customScriptConfiguration) {
-        try {
-            log.debug("Executing python 'updateAttributes' method");
-            IdpType idpType = (IdpType) customScriptConfiguration.getExternalType();
-            Map<String, SimpleCustomProperty> configurationAttributes = customScriptConfiguration.getConfigurationAttributes();
-            return idpType.updateAttributes(context, configurationAttributes);
-        } catch (Exception ex) {
-            log.error(ex.getMessage(), ex);
-            saveScriptError(customScriptConfiguration.getCustomScript(), ex);
-        }
+    //  boolean translateAttributes(Object context, Map<String, SimpleCustomProperty> configurationAttributes);
 
-        return false;
-    }
+	public boolean executeExternalTranslateAttributesMethod(Object context, CustomScriptConfiguration customScriptConfiguration) {
+		try {
+			log.debug("Executing python 'translateAttributes' method");
+			IdpType idpType = (IdpType) customScriptConfiguration.getExternalType();
+			Map<String, SimpleCustomProperty> configurationAttributes = customScriptConfiguration.getConfigurationAttributes();
+			return idpType.translateAttributes(context, configurationAttributes);
+		} catch (Exception ex) {
+			log.error(ex.getMessage(), ex);
+			saveScriptError(customScriptConfiguration.getCustomScript(), ex);
+		}
 
-    public boolean executeExternalUpdateAttributesMethods(Object context) {
-        boolean result = true;
-        for (CustomScriptConfiguration customScriptConfiguration : this.customScriptConfigurations) {
-            if (customScriptConfiguration.getExternalType().getApiVersion() > 1) {
-                result &= executeExternalUpdateAttributesMethod(context, customScriptConfiguration);
-                if (!result) {
-                    return result;
-                }
-            }
-        }
+		return false;
+	}
 
-        return result;
-    }
+	public boolean executeExternalTranslateAttributesMethod(Object context) {
+		boolean result = true;
+		for (CustomScriptConfiguration customScriptConfiguration : this.customScriptConfigurations) {
+			if (customScriptConfiguration.getExternalType().getApiVersion() > 1) {
+				result &= executeExternalTranslateAttributesMethod(context, customScriptConfiguration);
+				if (!result) {
+					return result;
+				}
+			}
+		}
+
+		return result;
+	}
+
+    //  boolean updateAttributes(Object context, Map<String, SimpleCustomProperty> configurationAttributes);
+
+	public boolean executeExternalUpdateAttributesMethod(Object context, CustomScriptConfiguration customScriptConfiguration) {
+		try {
+			log.debug("Executing python 'updateAttributes' method");
+			IdpType idpType = (IdpType) customScriptConfiguration.getExternalType();
+			Map<String, SimpleCustomProperty> configurationAttributes = customScriptConfiguration.getConfigurationAttributes();
+			return idpType.updateAttributes(context, configurationAttributes);
+		} catch (Exception ex) {
+			log.error(ex.getMessage(), ex);
+			saveScriptError(customScriptConfiguration.getCustomScript(), ex);
+		}
+
+		return false;
+	}
+
+	public boolean executeExternalUpdateAttributesMethods(Object context) {
+		boolean result = true;
+		for (CustomScriptConfiguration customScriptConfiguration : this.customScriptConfigurations) {
+			if (customScriptConfiguration.getExternalType().getApiVersion() > 1) {
+				result &= executeExternalUpdateAttributesMethod(context, customScriptConfiguration);
+				if (!result) {
+					return result;
+				}
+			}
+		}
+
+		return result;
+	}
 
 }
