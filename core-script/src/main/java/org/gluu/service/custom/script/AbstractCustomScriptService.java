87005675ee1e600102d5a5a04fc1273dc2fe9d80
yurem <Yuriy.Movchan@gmail.com>
2020-06-24 18:03:34 +0300

    Fix typo

--- core-script/src/main/java/org/gluu/service/custom/script/AbstractCustomScriptService.java
+++ core-script/src/main/java/org/gluu/service/custom/script/AbstractCustomScriptService.java
@@ -2,7 +2,8 @@
  * oxCore is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
  *
  * Copyright (c) 2014, Gluu
- */package org.gluu.service.custom.script;
+ */
+package org.gluu.service.custom.script;
 
 import java.io.Serializable;
 import java.util.ArrayList;
