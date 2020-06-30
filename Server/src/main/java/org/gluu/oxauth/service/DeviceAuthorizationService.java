0c15ab88f495390a68727490f3fc1bf28f589632
Milton BO <jmcm578@gmail.com>
2020-06-30 16:44:58 -0400

    Integrated interface with the Action in the server processing every request based on the user code. #141

--- Server/src/main/java/org/gluu/oxauth/service/DeviceAuthorizationService.java
+++ Server/src/main/java/org/gluu/oxauth/service/DeviceAuthorizationService.java
@@ -6,9 +6,10 @@
 
 package org.gluu.oxauth.service;
 
-import org.gluu.oxauth.model.common.CibaRequestCacheControl;
 import org.gluu.oxauth.model.common.DeviceAuthorizationCacheControl;
+import org.gluu.oxauth.model.common.GrantType;
 import org.gluu.oxauth.model.configuration.AppConfiguration;
+import org.gluu.oxauth.model.registration.Client;
 import org.gluu.service.CacheService;
 import org.slf4j.Logger;
 
@@ -31,16 +32,32 @@ public class DeviceAuthorizationService {
 
     public void saveInCache(DeviceAuthorizationCacheControl data) {
         cacheService.put(data.getExpiresIn(), data.getDeviceCode(), data);
+        cacheService.put(data.getExpiresIn(), data.getUserCode(), data);
         log.trace("Ciba request saved in cache, userCode: {}, deviceCode: {}, clientId: {}", data.getUserCode(), data.getDeviceCode(), data.getClient().getClientId());
     }
 
-    public DeviceAuthorizationCacheControl getDeviceAuthorizationCacheData(String deviceCode) {
-        Object cachedObject = cacheService.get(deviceCode);
+    public DeviceAuthorizationCacheControl getDeviceAuthorizationCacheData(String deviceCode, String userCode) {
+        String cacheKey = deviceCode != null ? deviceCode : userCode;
+        Object cachedObject = cacheService.get(cacheKey);
         if (cachedObject == null) {
             // retry one time : sometimes during high load cache client may be not fast enough
-            cachedObject = cacheService.get(deviceCode);
-            log.trace("Failed to fetch DeviceAuthorizationCacheControl request from cache, deviceCode: {}", deviceCode);
+            cachedObject = cacheService.get(cacheKey);
+            log.trace("Failed to fetch DeviceAuthorizationCacheControl request from cache, cacheKey: {}", cacheKey);
         }
         return cachedObject instanceof DeviceAuthorizationCacheControl ? (DeviceAuthorizationCacheControl) cachedObject : null;
+    }
+
+    /**
+     * Verifies whether a specific client has Device Code grant type compatibility.
+     *
+     * @param client Client to check.
+     */
+    public boolean hasDeviceCodeCompatibility(Client client) {
+        for (GrantType gt : client.getGrantTypes()) {
+            if (gt.getValue().equals(GrantType.DEVICE_CODE.getValue())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
