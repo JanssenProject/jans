dcc6e071a2fd3c6e2f7dc1fbab2bcb6e19a3c04d
Mustafa Baser <mbaser@mail.com>
2020-07-10 20:03:46 +0300

    closes #696 write os detection function

--- setup.py
+++ setup.py
@@ -21,6 +21,11 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+try:
+    import warnings
+    warnings.filterwarnings("ignore")
+except:
+    pass
 
 import readline
 import sys
