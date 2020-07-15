485fe76a3d005b326857c92d4da9fe1e273e9f1f
Milton BO <jmcm578@gmail.com>
2020-07-15 16:04:03 -0400

    Implemented different tests for WS to register device authz requests. #141

--- Client/src/test/java/org/gluu/oxauth/ws/rs/deviceauthz/DeviceAuthzDeviceRegistrationTest.java
+++ Client/src/test/java/org/gluu/oxauth/ws/rs/deviceauthz/DeviceAuthzDeviceRegistrationTest.java
@@ -8,9 +8,12 @@ package org.gluu.oxauth.ws.rs.deviceauthz;
 
 import org.gluu.oxauth.BaseTest;
 import org.gluu.oxauth.client.*;
+import org.gluu.oxauth.model.authorize.DeviceAuthzErrorResponseType;
+import org.gluu.oxauth.model.common.AuthenticationMethod;
 import org.gluu.oxauth.model.common.GrantType;
 import org.gluu.oxauth.model.common.ResponseType;
 import org.gluu.oxauth.model.register.ApplicationType;
+import org.gluu.oxauth.model.util.StringUtils;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -18,25 +21,29 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static org.gluu.oxauth.model.util.StringUtils.EASY_TO_READ_CHARACTERS;
 import static org.testng.Assert.*;
 
 /**
- * Tests for WS used to register device requests.
+ * Tests for WS used to register device authz requests.
  */
 public class DeviceAuthzDeviceRegistrationTest extends BaseTest {
 
-    @Parameters({"userId", "userSecret", "redirectUris", "redirectUri", "sectorIdentifierUri"})
+    /**
+     * Verifies normal flow with different scopes, AS should generate user_code, device_code and other data.
+     * It uses normal client_secret_basic authentication method.
+     */
     @Test
     public void deviceAuthzHappyFlow() {
         showTitle("deviceAuthzHappyFlow");
 
-        // 1. Register client
-        RegisterResponse registerResponse = registerClientForDeviceAuthz();
+        // Register client
+        RegisterResponse registerResponse = registerClientForDeviceAuthz(AuthenticationMethod.CLIENT_SECRET_BASIC,
+                Collections.singletonList(GrantType.DEVICE_CODE), null, null);
         String clientId = registerResponse.getClientId();
 
-        // 2. Request device authz
+        // 1. OpenId, profile, address and email scopes
         List<String> scopes = Arrays.asList("openid", "profile", "address", "email");
-
         DeviceAuthzRequest authorizationRequest = new DeviceAuthzRequest(clientId, scopes);
         authorizationRequest.setAuthUsername(clientId);
         authorizationRequest.setAuthPassword(registerResponse.getClientSecret());
@@ -47,25 +54,171 @@ public class DeviceAuthzDeviceRegistrationTest extends BaseTest {
         DeviceAuthzResponse response = deviceAuthzClient.exec();
 
         showClient(deviceAuthzClient);
+        validateResponse(response);
 
-        assertNotNull(response.getUserCode(), "User code is null");
-        assertNotNull(response.getDeviceCode(), "Device code is null");
-        assertNotNull(response.getInterval(), "Interval is null");
-        assertTrue(response.getInterval() > 0, "Interval is null");
-        assertNotNull(response.getVerificationUri(), "Verification Uri is null");
-        assertNotNull(response.getVerificationUriComplete(), "Verification Uri complete is null");
-        assertTrue(response.getVerificationUri().length() > 10, "Invalid verification_uri");
-        assertTrue(response.getVerificationUriComplete().length() > 10, "Invalid verification_uri_complete");
-        assertNotNull(response.getExpiresIn(), "expires_in is null");
-        assertTrue(response.getExpiresIn() > 0, "expires_in contains an invalid value");
+        // 2. Only openid scope
+        scopes = Collections.singletonList("openid");
+        authorizationRequest = new DeviceAuthzRequest(clientId, scopes);
+        authorizationRequest.setAuthUsername(clientId);
+        authorizationRequest.setAuthPassword(registerResponse.getClientSecret());
+
+        deviceAuthzClient = new DeviceAuthzClient(deviceAuthzEndpoint);
+        deviceAuthzClient.setRequest(authorizationRequest);
+
+        response = deviceAuthzClient.exec();
+
+        showClient(deviceAuthzClient);
+        validateResponse(response);
     }
 
-    private RegisterResponse registerClientForDeviceAuthz() {
+    /**
+     * Verifies normal flow with different scopes, AS should generate user_code, device_code and other data.
+     * It uses normal none authentication method, therefore no client authentication is required.
+     */
+    @Test
+    public void deviceAuthzHappyFlowPublicClient() {
+        showTitle("deviceAuthzHappyFlowPublicClient");
+
+        // Register client
+        RegisterResponse registerResponse = registerClientForDeviceAuthz(AuthenticationMethod.NONE,
+                Collections.singletonList(GrantType.DEVICE_CODE), null, null);
+        String clientId = registerResponse.getClientId();
+
+        // 1. OpenId, profile, address and email scopes
+        List<String> scopes = Arrays.asList("openid", "profile", "address", "email");
+        DeviceAuthzRequest authorizationRequest = new DeviceAuthzRequest(clientId, scopes);
+        authorizationRequest.setAuthenticationMethod(AuthenticationMethod.NONE);
+
+        DeviceAuthzClient deviceAuthzClient = new DeviceAuthzClient(deviceAuthzEndpoint);
+        deviceAuthzClient.setRequest(authorizationRequest);
+
+        DeviceAuthzResponse response = deviceAuthzClient.exec();
+
+        showClient(deviceAuthzClient);
+        validateResponse(response);
+
+        // 2. Only openid scope
+        scopes = Collections.singletonList("openid");
+        authorizationRequest = new DeviceAuthzRequest(clientId, scopes);
+        authorizationRequest.setAuthUsername(clientId);
+        authorizationRequest.setAuthPassword(registerResponse.getClientSecret());
+
+        deviceAuthzClient = new DeviceAuthzClient(deviceAuthzEndpoint);
+        deviceAuthzClient.setRequest(authorizationRequest);
+
+        response = deviceAuthzClient.exec();
+
+        showClient(deviceAuthzClient);
+        validateResponse(response);
+    }
+
+    /**
+     * Tests that the device authz request is rejected, since client doesnt support that grant type.
+     */
+    @Parameters({"redirectUris", "sectorIdentifierUri"})
+    @Test
+    public void deviceAuthzGrantTypeDoesntSupported(final String redirectUris, final String sectorIdentifierUri) {
+        showTitle("deviceAuthzGrantTypeDoesntSupported");
+
+        // Register client
+        RegisterResponse registerResponse = registerClientForDeviceAuthz(AuthenticationMethod.CLIENT_SECRET_BASIC,
+                Collections.singletonList(GrantType.AUTHORIZATION_CODE), redirectUris, sectorIdentifierUri);
+        String clientId = registerResponse.getClientId();
+
+        // Device authz request registration
+        List<String> scopes = Arrays.asList("openid", "profile", "address", "email");
+        DeviceAuthzRequest authorizationRequest = new DeviceAuthzRequest(clientId, scopes);
+        authorizationRequest.setAuthUsername(clientId);
+        authorizationRequest.setAuthPassword(registerResponse.getClientSecret());
+
+        DeviceAuthzClient deviceAuthzClient = new DeviceAuthzClient(deviceAuthzEndpoint);
+        deviceAuthzClient.setRequest(authorizationRequest);
+
+        DeviceAuthzResponse response = deviceAuthzClient.exec();
+
+        showClient(deviceAuthzClient);
+        validateErrorResponse(response, 400, DeviceAuthzErrorResponseType.INVALID_GRANT);
+    }
+
+    /**
+     * AS should authenticate client requests, however these tests are trying to pass device authz requests with
+     * wrong client authn data.
+     */
+    @Test
+    public void deviceAuthzNoPublicClientHoweverIncorrectAuthSent() {
+        showTitle("deviceAuthzNoPublicClientHoweverIncorrectAuthSent");
+
+        // Register client
+        RegisterResponse registerResponse = registerClientForDeviceAuthz(AuthenticationMethod.CLIENT_SECRET_BASIC,
+                Collections.singletonList(GrantType.DEVICE_CODE), null, null);
+        String clientId = registerResponse.getClientId();
+
+        // 1. No authentication data sent
+        List<String> scopes = Arrays.asList("openid", "profile", "address", "email");
+        DeviceAuthzRequest authorizationRequest = new DeviceAuthzRequest(clientId, scopes);
+        authorizationRequest.setAuthenticationMethod(AuthenticationMethod.NONE);
+
+        DeviceAuthzClient deviceAuthzClient = new DeviceAuthzClient(deviceAuthzEndpoint);
+        deviceAuthzClient.setRequest(authorizationRequest);
+
+        DeviceAuthzResponse response = deviceAuthzClient.exec();
+
+        showClient(deviceAuthzClient);
+        validateErrorResponse(response, 401, DeviceAuthzErrorResponseType.INVALID_CLIENT);
+
+        // 2. Invalid authentication
+        scopes = Arrays.asList("openid", "profile", "address", "email");
+        authorizationRequest = new DeviceAuthzRequest(clientId, scopes);
+        authorizationRequest.setAuthUsername(clientId);
+        authorizationRequest.setAuthPassword("invalid-client-id-" + System.currentTimeMillis());
+
+        deviceAuthzClient = new DeviceAuthzClient(deviceAuthzEndpoint);
+        deviceAuthzClient.setRequest(authorizationRequest);
+
+        response = deviceAuthzClient.exec();
+
+        showClient(deviceAuthzClient);
+        validateErrorResponse(response, 401, DeviceAuthzErrorResponseType.INVALID_CLIENT);
+    }
+
+    /**
+     * Client that doesnt require authn accept device authz requests even client sends authn data.
+     */
+    @Test
+    public void deviceAuthzPublicClientAndAuthSent() {
+        showTitle("deviceAuthzPublicClientAndAuthSent");
+
+        // Register client
+        RegisterResponse registerResponse = registerClientForDeviceAuthz(AuthenticationMethod.NONE,
+                Collections.singletonList(GrantType.DEVICE_CODE), null, null);
+        String clientId = registerResponse.getClientId();
+
+        // Device authz request
+        List<String> scopes = Arrays.asList("openid", "profile", "address", "email");
+        DeviceAuthzRequest authorizationRequest = new DeviceAuthzRequest(clientId, scopes);
+        authorizationRequest.setAuthUsername(clientId);
+        authorizationRequest.setAuthPassword(registerResponse.getClientSecret());
+
+        DeviceAuthzClient deviceAuthzClient = new DeviceAuthzClient(deviceAuthzEndpoint);
+        deviceAuthzClient.setRequest(authorizationRequest);
+
+        DeviceAuthzResponse response = deviceAuthzClient.exec();
+
+        showClient(deviceAuthzClient);
+        validateResponse(response);
+    }
+
+    private RegisterResponse registerClientForDeviceAuthz(AuthenticationMethod authenticationMethod,
+                                                          List<GrantType> grantTypes, String redirectUris,
+                                                          String sectorIdentifierUri) {
         List<ResponseType> responseTypes = Collections.singletonList(ResponseType.CODE);
 
-        RegisterRequest registerRequest = new RegisterRequest(ApplicationType.WEB, "oxAuth test app", null);
+        RegisterRequest registerRequest = new RegisterRequest(ApplicationType.WEB, "oxAuth test app",
+                StringUtils.spaceSeparatedToList(redirectUris));
         registerRequest.setResponseTypes(responseTypes);
-        registerRequest.setGrantTypes(Collections.singletonList(GrantType.DEVICE_CODE));
+        registerRequest.setGrantTypes(grantTypes);
+        registerRequest.setTokenEndpointAuthMethod(authenticationMethod);
+        registerRequest.setSectorIdentifierUri(sectorIdentifierUri);
 
         RegisterClient registerClient = new RegisterClient(registrationEndpoint);
         registerClient.setRequest(registerRequest);
@@ -80,6 +233,31 @@ public class DeviceAuthzDeviceRegistrationTest extends BaseTest {
         assertNotNull(registerResponse.getClientSecretExpiresAt());
 
         return registerResponse;
+    }
+
+    private void validateResponse(DeviceAuthzResponse response) {
+        final String regex = "[" + EASY_TO_READ_CHARACTERS + "]{4}-[" + EASY_TO_READ_CHARACTERS + "]{4}";
+        assertEquals(response.getStatus(), 200, "Unexpected response code: " + response.getEntity());
+        assertNotNull(response.getUserCode(), "User code is null");
+        assertNotNull(response.getDeviceCode(), "Device code is null");
+        assertNotNull(response.getInterval(), "Interval is null");
+        assertTrue(response.getInterval() > 0, "Interval is null");
+        assertNotNull(response.getVerificationUri(), "Verification Uri is null");
+        assertNotNull(response.getVerificationUriComplete(), "Verification Uri complete is null");
+        assertTrue(response.getVerificationUri().length() > 10, "Invalid verification_uri");
+        assertTrue(response.getVerificationUriComplete().length() > 10, "Invalid verification_uri_complete");
+        assertNotNull(response.getExpiresIn(), "expires_in is null");
+        assertTrue(response.getExpiresIn() > 0, "expires_in contains an invalid value");
+        assertTrue(response.getUserCode().matches(regex));
+    }
+
+    private void validateErrorResponse(DeviceAuthzResponse response, int status, DeviceAuthzErrorResponseType errorType) {
+        assertEquals(response.getStatus(), status, "Unexpected response code: " + response.getEntity());
+        assertNotNull(response.getErrorType(), "Error expected, however no error was found");
+        assertNotNull(response.getErrorDescription(), "Error description expected, however no error was found");
+        assertEquals(response.getErrorType(), errorType, "invalid_client error expected");
+        assertNull(response.getUserCode(), "User code must not be null");
+        assertNull(response.getDeviceCode(), "Device code must not be null");
     }
 
 }
