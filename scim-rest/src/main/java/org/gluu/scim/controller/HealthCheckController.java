03d175b11696c006b9beceaa1e0e5ec77807d30f
yurem <Yuriy.Movchan@gmail.com>
2020-07-24 14:44:07 +0300

    Add health-check controller to scim-server

---
+++ scim-rest/src/main/java/org/gluu/scim/controller/HealthCheckController.java
@@ -0,0 +1,28 @@
+package org.gluu.scim.controller;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Health check controller
+ * 
+ * @author Yuriy Movchan
+ * @version Jul 24, 2020
+ */
+@ApplicationScoped
+@Path("/")
+public class HealthCheckController {
+
+    @GET
+    @POST
+    @Path("/health-check")
+    @Produces(MediaType.APPLICATION_JSON)
+	public String healthCheckController() {
+        return "{\"status\":\"running\"}";
+	}
+
+}
