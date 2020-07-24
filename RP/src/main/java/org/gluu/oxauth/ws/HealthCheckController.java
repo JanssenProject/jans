1809748c98f09274aa5181c1885cc98411a23d41
yurem <Yuriy.Movchan@gmail.com>
2020-07-24 12:02:09 +0300

    Add health-check. Remove deprecated files. Move test classes to test folder.

--- RP/src/main/java/org/gluu/oxauth/ws/HealthCheckController.java
+++ RP/src/main/java/org/gluu/oxauth/ws/HealthCheckController.java
@@ -2,6 +2,7 @@ package org.gluu.oxauth.ws;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -13,10 +14,11 @@ import javax.ws.rs.core.MediaType;
  * @version Jul 24, 2020
  */
 @ApplicationScoped
-@Path("/fido2/attestation")
+@Path("/")
 public class HealthCheckController {
 
     @GET
+    @POST
     @Path("/health-check")
     @Produces(MediaType.APPLICATION_JSON)
 	public String healthCheckController() {
