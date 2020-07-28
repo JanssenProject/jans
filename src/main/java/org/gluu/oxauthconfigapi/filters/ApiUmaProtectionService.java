4d3e9e61693b8a32d0fe917c5a2ae384e30c5543
Gasmyr <thomas@gluu.org>
2020-07-28 15:16:53 +0100

    API Consumer fetches all OpenID clients #87

---
+++ src/main/java/org/gluu/oxauthconfigapi/filters/ApiUmaProtectionService.java
@@ -0,0 +1,135 @@
+/**
+ * 
+ */
+package org.gluu.oxauthconfigapi.filters;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.commons.lang.StringUtils;
+import org.gluu.config.oxtrust.AppConfiguration;
+import org.gluu.oxauth.client.ClientInfoClient;
+import org.gluu.oxauth.client.ClientInfoResponse;
+import org.gluu.oxtrust.auth.uma.BaseUmaProtectionService;
+import org.gluu.oxtrust.auth.uma.BindingUrls;
+import org.gluu.oxtrust.service.OpenIdService;
+import org.slf4j.Logger;
+
+import io.quarkus.arc.AlternativePriority;
+
+/**
+ * @author Mougang T.Gasmyr
+ *
+ */
+@ApplicationScoped
+@Named("apiUmaProtectionService")
+@AlternativePriority(value = 1)
+@BindingUrls({ "/api/v1" })
+public class ApiUmaProtectionService extends BaseUmaProtectionService implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6553095758559902245L;
+
+	@Inject
+	private Logger logger;
+
+	@Inject
+	private AppConfiguration appConfiguration;
+
+	@Inject
+	private OpenIdService openIdService;
+
+	@Override
+	protected String getClientId() {
+		return appConfiguration.getApiUmaClientId();
+	}
+
+	@Override
+	protected String getClientKeyStorePassword() {
+		return appConfiguration.getApiUmaClientKeyStorePassword();
+	}
+
+	@Override
+	protected String getClientKeyStoreFile() {
+		return appConfiguration.getApiUmaClientKeyStoreFile();
+	}
+
+	@Override
+	protected String getClientKeyId() {
+		return appConfiguration.getApiUmaClientKeyId();
+	}
+
+	@Override
+	public String getUmaResourceId() {
+		return appConfiguration.getApiUmaResourceId();
+	}
+
+	@Override
+	public String getUmaScope() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(appConfiguration.getApiUmaScopes()[0]);
+		buffer.append(" ");
+		buffer.append(appConfiguration.getApiUmaScopes()[1]);
+		return buffer.toString();
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return isEnabledUmaAuthentication();
+	}
+
+	@SuppressWarnings("resource")
+	@Override
+	public Response processAuthorization(HttpHeaders headers, ResourceInfo resourceInfo) {
+		Response authorizationResponse = null;
+		String authorization = headers.getHeaderString("Authorization");
+		logger.info("Authorization header {} found", StringUtils.isEmpty(authorization) ? "not" : "");
+		try {
+			if (appConfiguration.isOxTrustApiTestMode()) {
+				logger.info("================OXAUTH-CONFIG-API Test Mode is ACTIVE");
+				authorizationResponse = processTestModeAuthorization(authorization);
+			} else if (isEnabled()) {
+				logger.info("================OXAUTH-CONFIG-API is protected by UMA");
+				authorizationResponse = processUmaAuthorization(authorization, resourceInfo);
+			} else {
+				logger.info(
+						"Please activate UMA or test mode to protect your API endpoints. Read the Gluu API docs to learn more");
+				authorizationResponse = getErrorResponse(Response.Status.UNAUTHORIZED, "API not protected");
+			}
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			authorizationResponse = getErrorResponse(Response.Status.INTERNAL_SERVER_ERROR, e.getMessage());
+		}
+		return authorizationResponse;
+	}
+
+	private Response processTestModeAuthorization(String token) throws Exception {
+		Response response = null;
+		if (StringUtils.isNotEmpty(token)) {
+			token = token.replaceFirst("Bearer\\s+", "");
+			logger.debug("Validating token {}", token);
+			String clientInfoEndpoint = openIdService.getOpenIdConfiguration().getClientInfoEndpoint();
+			ClientInfoClient clientInfoClient = new ClientInfoClient(clientInfoEndpoint);
+			ClientInfoResponse clientInfoResponse = clientInfoClient.execClientInfo(token);
+			if ((clientInfoResponse.getStatus() != Response.Status.OK.getStatusCode())
+					|| (clientInfoResponse.getErrorType() != null)) {
+				response = getErrorResponse(Status.UNAUTHORIZED, "Invalid token " + token);
+				logger.debug("Error validating access token: {}", clientInfoResponse.getErrorDescription());
+			}
+		} else {
+			logger.info("Request is missing authorization header");
+			response = getErrorResponse(Status.INTERNAL_SERVER_ERROR, "No authorization header found");
+		}
+		return response;
+	}
+
+}
--- src/main/java/org/gluu/oxauthconfigapi/filters/AuthorizationFilter.java
+++ src/main/java/org/gluu/oxauthconfigapi/filters/AuthorizationFilter.java
@@ -8,12 +8,15 @@ import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
 
+//import org.gluu.oxtrust.auth.uma.ApiUmaProtectionService;
+import org.gluu.oxtrust.auth.uma.BaseUmaProtectionService;
 import org.slf4j.Logger;
 
 import io.vertx.core.http.HttpServerRequest;
@@ -34,6 +37,14 @@ public class AuthorizationFilter implements ContainerRequestFilter {
 	@Context
 	HttpServerRequest request;
 
+	@Context
+	private HttpHeaders httpHeaders;
+
+	@Context
+	private ResourceInfo resourceInfo;
+
+	BaseUmaProtectionService protectionService;
+
 	@Inject
 	Logger logger;
 
@@ -50,13 +61,14 @@ public class AuthorizationFilter implements ContainerRequestFilter {
 		}
 		String token = authorizationHeader.substring(AUTHENTICATION_SCHEME.length()).trim();
 		try {
-			validateToken(token,context);
-			logger.info("======AUTHORIZATION  GRANTED===========================================");	
+			validateToken(token, context);
+			logger.info("======AUTHORIZATION  GRANTED===========================================");
 		} catch (Exception e) {
+			logger.info("",e);
 			abortWithUnauthorized(context);
 			logger.info("======INVALID AUTHORIZATION TOKEN======================================");
 		}
-        
+
 	}
 
 	private boolean isTokenBasedAuthentication(String authorizationHeader) {
@@ -70,6 +82,8 @@ public class AuthorizationFilter implements ContainerRequestFilter {
 	}
 
 	private void validateToken(String token, ContainerRequestContext context) throws Exception {
+		protectionService = new ApiUmaProtectionService();
+		//protectionService.processAuthorization(httpHeaders, resourceInfo);
 		// Check if the token was issued by the server and if it's not expired
 		// Throw an Exception if the token is invalid
 	}
