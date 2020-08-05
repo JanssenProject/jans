0968ecd3c16994477cd97f6f05f27ea77286e784
yurem <Yuriy.Movchan@gmail.com>
2020-08-05 18:21:49 +0300

    Store issued tokens count metrics #1436

--- oxModel/src/main/java/org/gluu/model/metric/MetricType.java
+++ oxModel/src/main/java/org/gluu/model/metric/MetricType.java
@@ -30,7 +30,18 @@ public enum MetricType implements AttributeEnum {
     OXAUTH_USER_AUTHENTICATION_RATE("user_authentication_rate",
             "User authentication rate", TimerMetricData.class, TimerMetricEntry.class),
     DYNAMIC_CLIENT_REGISTRATION_RATE("dynamic_client_registration_rate",
-            "Dynamic client registration rate", TimerMetricData.class, TimerMetricEntry.class);
+            "Dynamic client registration rate", TimerMetricData.class, TimerMetricEntry.class),
+
+	OXAUTH_TOKEN_AUTHORIZATION_CODE_COUNT("tkn_authorization_code_count",
+            "Count successfull issued oxAuth authorization code tokens", CounterMetricData.class, CounterMetricEntry.class),
+	OXAUTH_TOKEN_ACCESS_TOKEN_COUNT("tkn_access_token_count",
+            "Count successfull issued oxAuth access tokens", CounterMetricData.class, CounterMetricEntry.class),
+	OXAUTH_TOKEN_ID_TOKEN_COUNT("tkn_id_token_count",
+            "Count successfull issued oxAuth id tokens", CounterMetricData.class, CounterMetricEntry.class),
+	OXAUTH_TOKEN_REFRESH_TOKEN_COUNT("tkn_refresh_token_count",
+            "Count successfull issued oxAuth refresh tokens", CounterMetricData.class, CounterMetricEntry.class),
+	OXAUTH_TOKEN_LONG_LIVED_ACCESS_TOKEN_COUNT("tkn_long_lived_access_token_count",
+            "Count successfull issued oxAuth long lived access tokens", CounterMetricData.class, CounterMetricEntry.class);
 
     private String value;
     private String displayName;
