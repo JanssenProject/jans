75c9de0fd5549aa22ab86376d54a93689cf24ea9
Gasmyr <thomas@gluu.org>
2020-08-10 14:57:58 +0100

    https://github.com/GluuFederation/oxauth-config/issues/25

--- src/main/java/org/gluu/oxauthconfigapi/rest/health/DatabaseConnectionHealthCheck.java
+++ src/main/java/org/gluu/oxauthconfigapi/rest/health/DatabaseConnectionHealthCheck.java
@@ -8,6 +8,7 @@ import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 import org.eclipse.microprofile.health.Readiness;
 import org.gluu.oxtrust.service.GroupService;
+import org.gluu.persist.PersistenceEntryManager;
 
 @Readiness
 @ApplicationScoped
@@ -16,19 +17,21 @@ public class DatabaseConnectionHealthCheck implements HealthCheck {
 	@Inject
 	GroupService groupService;
 
+	@Inject
+	PersistenceEntryManager persistenceEntryManager;
+
 	public HealthCheckResponse call() {
-		HealthCheckResponseBuilder responseBuilder = HealthCheckResponse
-				.named("oxauth-config-api readiness");
+		HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("oxauth-config-api readiness");
 		try {
 			checkDatabaseConnection();
 			responseBuilder.up();
-		} catch (IllegalStateException e) {
+		} catch (Exception e) {
 			responseBuilder.down().withData("error", e.getMessage());
 		}
 		return responseBuilder.build();
 	}
 
-	private void checkDatabaseConnection() {
+	private void checkDatabaseConnection() throws Exception {
 		groupService.countGroups();
 	}
 
