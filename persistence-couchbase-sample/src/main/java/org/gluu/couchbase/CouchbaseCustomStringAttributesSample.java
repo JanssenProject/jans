cf97f2e56b88ecc03bb422ae2faae185a6dcd5d7
yurem <Yuriy.Movchan@gmail.com>
2020-08-13 20:56:27 +0300

    Update sample to add user with oxExternalUid and search by this attribute

--- persistence-couchbase-sample/src/main/java/org/gluu/couchbase/CouchbaseCustomStringAttributesSample.java
+++ persistence-couchbase-sample/src/main/java/org/gluu/couchbase/CouchbaseCustomStringAttributesSample.java
@@ -1,15 +1,14 @@
 package org.gluu.couchbase;
 
 import java.util.Arrays;
-import java.util.Date;
+import java.util.List;
 
 import org.gluu.couchbase.model.SimpleCustomStringUser;
-import org.gluu.couchbase.model.SimpleUser;
 import org.gluu.couchbase.model.UserRole;
 import org.gluu.persist.couchbase.impl.CouchbaseEntryManager;
 import org.gluu.persist.couchbase.operation.impl.CouchbaseConnectionProvider;
 import org.gluu.persist.model.base.CustomAttribute;
-import org.gluu.persist.model.base.CustomObjectAttribute;
+import org.gluu.search.filter.Filter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,16 +29,15 @@ public final class CouchbaseCustomStringAttributesSample {
 		// Create Couchbase entry manager
 		CouchbaseEntryManager couchbaseEntryManager = couchbaseSampleEntryManager.createCouchbaseEntryManager();
 
+		String randomExternalUid = "otp:" + System.currentTimeMillis();
+
 		// Add dummy user
-		SimpleUser newUser = new SimpleUser();
+		SimpleCustomStringUser newUser = new SimpleCustomStringUser();
 		newUser.setDn(String.format("inum=%s,ou=people,o=gluu", System.currentTimeMillis()));
 		newUser.setUserId("sample_user_" + System.currentTimeMillis());
 		newUser.setUserPassword("test");
-		newUser.getCustomAttributes().add(new CustomObjectAttribute("streetAddress", Arrays.asList("London", "Texas", "Kiev")));
-		newUser.getCustomAttributes().add(new CustomObjectAttribute("test", "test_value"));
-		newUser.getCustomAttributes().add(new CustomObjectAttribute("birthdate", new Date()));
-		newUser.getCustomAttributes().add(new CustomObjectAttribute("enabled", false));
-		newUser.getCustomAttributes().add(new CustomObjectAttribute("age", 18));
+		newUser.getCustomAttributes().add(new CustomAttribute("streetAddress", Arrays.asList("London", "Texas", "Kiev")));
+		newUser.getCustomAttributes().add((new CustomAttribute("oxExternalUid", randomExternalUid)).setMultiValued(true));
 
 		newUser.setUserRole(UserRole.ADMIN);
 		newUser.setNotes(Arrays.asList("note 1", "note 2", "note 3"));
@@ -55,6 +53,13 @@ public final class CouchbaseCustomStringAttributesSample {
 		LOG.info("Custom attributes '{}'", foundUser.getCustomAttributes());
 		for (CustomAttribute customAttribute : foundUser.getCustomAttributes()) {
 			LOG.info("Found custom attribute '{}' with value '{}'", customAttribute.getName(), customAttribute.getValue());
+		}
+
+		// Find by oxExternalUid
+		Filter oxExternalUidFilter = Filter.createEqualityFilter("oxExternalUid", randomExternalUid).multiValued();
+		List<SimpleCustomStringUser> foundUsers = couchbaseEntryManager.findEntries("ou=people,o=gluu", SimpleCustomStringUser.class, oxExternalUidFilter);
+		for (SimpleCustomStringUser foundUser2 : foundUsers) {
+			LOG.info("Found User '{}' by oxExternalUid with uid '{}' and key '{}'", foundUser2, foundUser2.getUserId(), foundUser2.getDn());
 		}
 	}
 
