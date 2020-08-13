dc225da19a59a02e65fd4fe309f9abbb5accc1b9
yurem <Yuriy.Movchan@gmail.com>
2020-08-13 17:27:24 +0300

    Update methods to store oxExternalUid as multivalued by default #1442

--- common/src/main/java/org/gluu/oxauth/service/common/UserService.java
+++ common/src/main/java/org/gluu/oxauth/service/common/UserService.java
@@ -407,7 +407,7 @@ public abstract class UserService {
         	customAttribute.setValues(newAttributeValues);
         }
         
-        if (multiValued) {
+        if (multiValued != null) {
         	customAttribute.setMultiValued(multiValued);
         }
 
