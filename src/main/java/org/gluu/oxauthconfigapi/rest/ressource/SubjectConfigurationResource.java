fae2c98ea2d0a7ee36747b54f03f91107b3bebec
YuriyZ <yzabrovarniy@gmail.com>
2020-09-01 19:18:06 +0300

    Removed quarkus-smallrye-openapi dependency

--- src/main/java/org/gluu/oxauthconfigapi/rest/ressource/SubjectConfigurationResource.java
+++ src/main/java/org/gluu/oxauthconfigapi/rest/ressource/SubjectConfigurationResource.java
@@ -1,31 +1,18 @@
 package org.gluu.oxauthconfigapi.rest.ressource;
 
-import javax.inject.Inject;
-import javax.validation.Valid;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import org.slf4j.Logger;
-
 import com.couchbase.client.core.message.ResponseStatus;
-
-import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
-
 import org.gluu.oxauth.model.configuration.AppConfiguration;
-import org.gluu.oxtrust.service.JsonConfigurationService;
 import org.gluu.oxauthconfigapi.filters.ProtectedApi;
-import org.gluu.oxauthconfigapi.rest.model.ApiError;
 import org.gluu.oxauthconfigapi.rest.model.SubjectConfiguration;
 import org.gluu.oxauthconfigapi.util.ApiConstants;
+import org.gluu.oxtrust.service.JsonConfigurationService;
+import org.slf4j.Logger;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * @author Puja Sharma
@@ -59,11 +46,6 @@ public class SubjectConfigurationResource extends BaseResource {
 	}
 
 	@PUT
-	@Operation(summary = "Update Subject configuration properties.")
-	@APIResponses(value = {
-			@APIResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = Response.class, required = true, description = "Success"))),
-			@APIResponse(responseCode = "401", content = @Content(schema = @Schema(implementation = ApiError.class, required = true, description = "Unauthorized"))),
-			@APIResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ApiError.class)), description = "Server error") })
 	@ProtectedApi(scopes = { WRITE_ACCESS })
 	public Response updateSubjectConfiguration(@Valid SubjectConfiguration subjectConfiguration) {
 		try {
