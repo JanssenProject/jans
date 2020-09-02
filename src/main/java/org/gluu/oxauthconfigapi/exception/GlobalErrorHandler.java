fa7964e0e7206ee519f5fb72deb803b511ec1c57
YuriyZ <yzabrovarniy@gmail.com>
2020-09-02 13:21:11 +0300

    added WebApplicationException exclusion for global exception handler
    https://github.com/GluuFederation/oxauth-config/issues/160

--- src/main/java/org/gluu/oxauthconfigapi/exception/GlobalErrorHandler.java
+++ src/main/java/org/gluu/oxauthconfigapi/exception/GlobalErrorHandler.java
@@ -3,11 +3,12 @@
  */
 package org.gluu.oxauthconfigapi.exception;
 
+import org.gluu.oxauthconfigapi.rest.model.ApiError;
+
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-
-import org.gluu.oxauthconfigapi.rest.model.ApiError;
 
 /**
  * @author Mougang T.Gasmyr
@@ -15,10 +16,13 @@ import org.gluu.oxauthconfigapi.rest.model.ApiError;
  */
 @Provider
 public class GlobalErrorHandler implements ExceptionMapper<Exception> {
-	public Response toResponse(Exception exception) {
-		ApiError error = new ApiError.ErrorBuilder()
-				.withCode(String.valueOf(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()))
-				.withMessage("Internal Server error").andDescription(exception.getMessage()).build();
-		return Response.serverError().entity(error).build();
-	}
+    public Response toResponse(Exception exception) {
+        if (exception instanceof WebApplicationException && ((WebApplicationException) exception).getResponse() != null) {
+            return ((WebApplicationException) exception).getResponse();
+        }
+        ApiError error = new ApiError.ErrorBuilder()
+                .withCode(String.valueOf(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()))
+                .withMessage("Internal Server error").andDescription(exception.getMessage()).build();
+        return Response.serverError().entity(error).build();
+    }
 }
