968e76ede6102f49c79bc311b00b13689a62a756
Gasmyr <thomas@gluu.org>
2020-09-04 19:35:17 +0100

    API Consumer partially updates a Gluu attribute #42

--- src/main/java/org/gluu/configapi/rest/resource/ClientsResource.java
+++ src/main/java/org/gluu/configapi/rest/resource/ClientsResource.java
@@ -3,6 +3,26 @@
  */
 package org.gluu.configapi.rest.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import org.gluu.configapi.filters.ProtectedApi;
 import org.gluu.configapi.util.ApiConstants;
 import org.gluu.configapi.util.AttributeNames;
@@ -15,16 +35,6 @@ import org.gluu.oxtrust.service.ScopeService;
 import org.gluu.util.security.StringEncrypter.EncryptionException;
 import org.slf4j.Logger;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author Mougang T.Gasmyr
  *
@@ -33,7 +43,7 @@ import java.util.List;
 @Path(ApiConstants.BASE_API_URL + ApiConstants.CLIENTS)
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-@ApplicationScoped
+@RequestScoped
 public class ClientsResource extends BaseResource {
 	/**
 	 * 
