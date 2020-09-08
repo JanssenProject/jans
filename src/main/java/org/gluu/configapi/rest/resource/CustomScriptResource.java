45621b4dd98e3084bcc24313e0cb904212fdfaf7
Gasmyr <thomas@gluu.org>
2020-09-08 19:04:39 +0100

    Telemetry Data Consumer collects telemetry data from oxauth-config #26

--- src/main/java/org/gluu/configapi/rest/resource/CustomScriptResource.java
+++ src/main/java/org/gluu/configapi/rest/resource/CustomScriptResource.java
@@ -11,6 +11,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -32,51 +33,59 @@ import org.slf4j.Logger;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class CustomScriptResource extends BaseResource {
-	
+
+	/**
+	 * 
+	 */
+	private static final String CUSTOM_SCRIPT = "custom script";
+
+	/**
+	 * 
+	 */
+	private static final String PATH_SEPARATOR = "/";
+
 	@Inject
 	Logger logger;
-	
+
 	@Inject
 	CustomScriptService customScriptService;
-	
+
 	@GET
 	@ProtectedApi(scopes = { READ_ACCESS })
 	public Response getAllCustomScripts() {
 		List<CustomScript> customScripts = customScriptService.findAllCustomScripts(null);
 		return Response.ok(customScripts).build();
 	}
-	
-		
+
 	@GET
-	@Path("/" + ApiConstants.TYPE + ApiConstants.TYPE_PATH)
-	@ProtectedApi(scopes = { READ_ACCESS })	
-	public Response getCustomScriptsBygetCustomScriptsByTypePattern(@PathParam(ApiConstants.TYPE) @NotNull String type,
+	@Path(PATH_SEPARATOR + ApiConstants.TYPE + ApiConstants.TYPE_PATH)
+	@ProtectedApi(scopes = { READ_ACCESS })
+	public Response getCustomScriptsByTypePattern(@PathParam(ApiConstants.TYPE) @NotNull String type,
 			@DefaultValue("") @QueryParam(value = ApiConstants.PATTERN) String pattern,
 			@DefaultValue("50") @QueryParam(value = ApiConstants.LIMIT) int limit) {
-		List<CustomScript> customScripts = this.customScriptService.findScriptByPatternAndType(pattern,CustomScriptType.getByValue(type),limit);
-		if (customScripts!=null && !customScripts.isEmpty()) 
+		List<CustomScript> customScripts = this.customScriptService.findScriptByPatternAndType(pattern,
+				CustomScriptType.getByValue(type), limit);
+		if (customScripts != null && !customScripts.isEmpty())
 			return Response.ok(customScripts).build();
 		else
-			return Response.status(Response.Status.NOT_FOUND).build();		
+			return Response.status(Response.Status.NOT_FOUND).build();
 	}
 
-
 	@GET
-	@Path("/" +ApiConstants.INUM + "/" +ApiConstants.INUM_PATH)
+	@Path(ApiConstants.INUM_PATH)
 	@ProtectedApi(scopes = { READ_ACCESS })
 	public Response getCustomScriptByInum(@PathParam(ApiConstants.INUM) @NotNull String inum) {
 		CustomScript script = null;
 		try {
-			script = this.customScriptService.getScriptByInum(inum);			
-		}
-		catch(Exception ex)	{
-			if(ex.getMessage().contains("Failed to find entry")) {
+			script = this.customScriptService.getScriptByInum(inum);
+		} catch (Exception ex) {
+			if (ex.getMessage().contains("Failed to find entry")) {
 				return Response.status(Response.Status.NOT_FOUND).build();
-			}			
+			}
 		}
 		return Response.ok(script).build();
 	}
-	
+
 	@POST
 	@ProtectedApi(scopes = { WRITE_ACCESS })
 	public Response createPersonScript(@Valid CustomScript customScript) {
@@ -89,14 +98,11 @@ public class CustomScriptResource extends BaseResource {
 		customScript.setInum(inum);
 		customScriptService.add(customScript);
 		return Response.status(Response.Status.CREATED).entity(customScript).build();
-	}	
+	}
 
 	@PUT
 	@ProtectedApi(scopes = { WRITE_ACCESS })
-	public Response updatePersonScript(@Valid CustomScript customScript) {
-		Objects.requireNonNull(customScript, "Attempt to update null custom script");
-		String inum = customScript.getInum();
-		logger.info("Update custom script " + inum);
+	public Response updatePersonScript(@Valid @NotNull CustomScript customScript) {
 		CustomScript existingScript = customScriptService.getScriptByInum(customScript.getInum());
 		if (existingScript != null) {
 			customScript.setInum(existingScript.getInum());
@@ -111,20 +117,15 @@ public class CustomScriptResource extends BaseResource {
 	@Path(ApiConstants.INUM_PATH)
 	@ProtectedApi(scopes = { WRITE_ACCESS })
 	public Response deletePersonScript(@PathParam(ApiConstants.INUM) @NotNull String inum) {
-		logger.info("Delete custom script " + inum);
-		Objects.requireNonNull(inum);
-		CustomScript existingScript = null;
 		try {
-			existingScript = customScriptService.getScriptByInum(inum);
+			CustomScript existingScript = customScriptService.getScriptByInum(inum);
 			customScriptService.remove(existingScript);
-			
+			return Response.noContent().build();
+		} catch (Exception ex) {
+			logger.info("Error deleting script by inum " + inum, ex);
+			throw new NotFoundException(getNotFoundError(CUSTOM_SCRIPT));
 		}
-		catch(Exception ex)	{
-			if(ex.getMessage().contains("Failed to find entry")) {
-				return Response.status(Response.Status.NOT_FOUND).build();
-			}			
-		}
-		return Response.noContent().build();
+
 	}
 
 }
