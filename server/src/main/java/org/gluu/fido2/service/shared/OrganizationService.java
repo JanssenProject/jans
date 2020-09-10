a845aab2b180fd267e8160fbdd2e8fbe585c8558
yurem <Yuriy.Movchan@gmail.com>
2020-09-10 20:39:29 +0300

    Add missing service

---
+++ server/src/main/java/org/gluu/fido2/service/shared/OrganizationService.java
@@ -0,0 +1,16 @@
+/*
+ * oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2014, Gluu
+ */
+
+package org.gluu.fido2.service.shared;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class OrganizationService extends org.gluu.service.OrganizationService {
+
+	private static final long serialVersionUID = -3912721745240924111L;
+	
+}
