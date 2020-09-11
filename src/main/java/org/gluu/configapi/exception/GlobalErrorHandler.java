6c7855c78c81645a9229a6467f23a0ee70e3600d
YuriyZ <yzabrovarniy@gmail.com>
2020-09-11 09:55:00 +0300

    Log exception on error and do not expose exception internal outside.
    https://github.com/GluuFederation/oxauth-config/issues/164

--- src/main/java/org/gluu/configapi/exception/GlobalErrorHandler.java
+++ src/main/java/org/gluu/configapi/exception/GlobalErrorHandler.java
@@ -4,7 +4,9 @@
 package org.gluu.configapi.exception;
 
 import org.gluu.configapi.rest.model.ApiError;
+import org.slf4j.Logger;
 
+import javax.inject.Inject;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -16,13 +18,22 @@ import javax.ws.rs.ext.Provider;
  */
 @Provider
 public class GlobalErrorHandler implements ExceptionMapper<Exception> {
-    public Response toResponse(Exception exception) {
-        if (exception instanceof WebApplicationException && ((WebApplicationException) exception).getResponse() != null) {
-            return ((WebApplicationException) exception).getResponse();
+
+    @Inject
+    Logger log;
+
+    public Response toResponse(Exception e) {
+        if (e instanceof WebApplicationException && ((WebApplicationException) e).getResponse() != null) {
+            return ((WebApplicationException) e).getResponse();
         }
-        ApiError error = new ApiError.ErrorBuilder()
-                .withCode(String.valueOf(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()))
-                .withMessage("Internal Server error").andDescription(exception.getMessage()).build();
-        return Response.serverError().entity(error).build();
+        log.error(e.getMessage(), e);
+        return Response
+                .serverError()
+                .entity(new ApiError.ErrorBuilder()
+                        .withCode(String.valueOf(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()))
+                        .withMessage("Internal Server error")
+                        .andDescription("Internal error occurs, for more details please check log files.")
+                        .build())
+                .build();
     }
 }
