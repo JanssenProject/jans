941f24e3e86651285928adfd2bd464ea6715ab9f
pujavs <pujas.works@gmail.com>
2020-09-17 21:44:55 +0530

    LDAP configuration API removed oxTrust dependencies - #134 and 135

--- src/main/java/org/gluu/configapi/util/ConnectionStatus.java
+++ src/main/java/org/gluu/configapi/util/ConnectionStatus.java
@@ -1,82 +1,96 @@
 package org.gluu.configapi.util;
 
-import org.gluu.configapi.configuration.ConfigurationFactory;
-import org.gluu.model.SimpleProperty;
-import org.gluu.model.ldap.GluuLdapConfiguration;
-import org.gluu.persist.ldap.operation.impl.LdapConnectionProvider;
-import org.gluu.util.security.PropertiesDecrypter;
-import org.slf4j.Logger;
-
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
 
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 
+import org.gluu.model.SimpleProperty;
+import org.gluu.model.ldap.GluuLdapConfiguration;
+import org.gluu.persist.ldap.impl.LdapEntryManagerFactory;
+import org.gluu.persist.ldap.operation.impl.LdapConnectionProvider;
+import org.gluu.util.properties.FileConfiguration;
+import org.gluu.util.security.PropertiesDecrypter;
+import org.gluu.configapi.configuration.ConfigurationFactory;
 
 @ApplicationScoped
 public class ConnectionStatus {
 
-    @Inject
-    Logger logger;
+  @Inject
+  Logger logger;
 
-    @Inject
-    ConfigurationFactory configurationFactory;
+  @Inject
+  ConfigurationFactory configurationFactory;
 
+  public boolean isUp(GluuLdapConfiguration ldapConfiguration) {
+    // Properties properties = new Properties();
+    FileConfiguration configuration = loadFileConfiguration();
+    Properties properties = configuration.getProperties();
+    properties.setProperty("bindDN", ldapConfiguration.getBindDN());
+    properties.setProperty("bindPassword", ldapConfiguration.getBindPassword());
+    properties.setProperty("servers", buildServersString(getServers(ldapConfiguration)));
+    logger.info("\n\n\n ConnectionStatus:::isUp() - configurationFactory = " + configurationFactory + "\n\n\n");
+    logger.info("\n\n\n ConnectionStatus:::isUp() - configurationFactory.getCryptoConfigurationSalt() = "
+        + configurationFactory.getCryptoConfigurationSalt() + "\n\n\n");
+    // LdapConnectionProvider connectionProvider = new
+    // LdapConnectionProvider(PropertiesDecrypter.decryptProperties(properties,
+    // configurationFactory.getCryptoConfigurationSalt()));
 
-    public boolean isUp(GluuLdapConfiguration ldapConfiguration) {
+    LdapConnectionProvider connectionProvider = new LdapConnectionProvider(
+        PropertiesDecrypter.decryptProperties(properties, configurationFactory.getCryptoConfigurationSalt()));
 
-        Properties properties = new Properties();
-        properties.setProperty("bindDN", ldapConfiguration.getBindDN());
-        properties.setProperty("bindPassword", ldapConfiguration.getBindPassword());
-        properties.setProperty("servers", buildServersString(getServers(ldapConfiguration)));
-        logger.info("\n\n\n ConnectionStatus:::isUp() - configurationFactory = " + configurationFactory + "\n\n\n");
-        logger.info("\n\n\n ConnectionStatus:::isUp() - configurationFactory.getCryptoConfigurationSalt() = " + configurationFactory.getCryptoConfigurationSalt() + "\n\n\n");
-        LdapConnectionProvider connectionProvider = new LdapConnectionProvider(PropertiesDecrypter.decryptProperties(properties, configurationFactory.getCryptoConfigurationSalt()));
-
-
-        if (connectionProvider.getConnectionPool() != null) {
-            boolean isConnected = connectionProvider.isConnected();
-            logger.info("\n\n\n ConnectionStatus:::isUp() - isConnected_1 = " + isConnected + "\n\n\n");
-            connectionProvider.closeConnectionPool();
-            logger.info("\n\n\n ConnectionStatus:::isUp() - isConnected_2 = " + isConnected + "\n\n\n");
-            return isConnected;
-        }
-
-        return false;
+    if (connectionProvider.getConnectionPool() != null) {
+      boolean isConnected = connectionProvider.isConnected();
+      logger.info("\n\n\n ConnectionStatus:::isUp() - isConnected_1 = " + isConnected + "\n\n\n");
+      connectionProvider.closeConnectionPool();
+      logger.info("\n\n\n ConnectionStatus:::isUp() - isConnected_2 = " + isConnected + "\n\n\n");
+      return isConnected;
     }
 
+    return false;
+  }
+  
+  private FileConfiguration loadFileConfiguration() {
+    FileConfiguration configuration = new FileConfiguration(ConfigurationFactory.getAppPropertiesFile());
+    if (!configuration.isLoaded()) {
+        configuration = new FileConfiguration(LdapEntryManagerFactory.LDAP_DEFAULT_PROPERTIES_FILE);
+    }
+    return configuration;
+}
 
-    private List<String> getServers(GluuLdapConfiguration ldapConfiguration) {
-        logger.info("\n\n\n ConnectionStatus:::getServers() - ldapConfiguration.getServers() = " + ldapConfiguration.getServers() + "\n\n\n");
-        List<String> servers = new ArrayList<String>();
-        for (SimpleProperty server : ldapConfiguration.getServers())
-            servers.add(server.getValue());
-        logger.info("\n\n\n ConnectionStatus:::getServers() - servers = " + servers + "\n\n\n");
-        return servers;
+  private List<String> getServers(GluuLdapConfiguration ldapConfiguration) {
+    logger.info("\n\n\n ConnectionStatus:::getServers() - ldapConfiguration.getServers() = "
+        + ldapConfiguration.getServers() + "\n\n\n");
+    List<String> servers = new ArrayList<String>();
+    for (SimpleProperty server : ldapConfiguration.getServers())
+      servers.add(server.getValue());
+    logger.info("\n\n\n ConnectionStatus:::getServers() - servers = " + servers + "\n\n\n");
+    return servers;
+  }
+
+  private String buildServersString(List<String> servers) {
+    if (servers == null) {
+      return EMPTY;
     }
 
+    StringBuilder sb = new StringBuilder();
+    boolean first = true;
+    for (String server : servers) {
+      if (first) {
+        first = false;
+      } else {
+        sb.append(",");
+      }
 
-    private String buildServersString(List<String> servers) {
-        if (servers == null) {
-            return EMPTY;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        boolean first = true;
-        for (String server : servers) {
-            if (first) {
-                first = false;
-            } else {
-                sb.append(",");
-            }
-
-            sb.append(server);
-        }
-
-        return sb.toString();
+      sb.append(server);
     }
+
+    return sb.toString();
+  }
 
 }
