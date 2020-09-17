b3e4b14db49dba958cf8c51b2f6c53897421097a
Gasmyr <thomas@gluu.org>
2020-09-17 01:38:14 +0100

    Fix Resources implementation

--- src/main/java/org/gluu/configapi/rest/resource/ClientsResource.java
+++ src/main/java/org/gluu/configapi/rest/resource/ClientsResource.java
@@ -3,19 +3,50 @@
  */
 package org.gluu.configapi.rest.resource;
 
+import org.gluu.configapi.filters.ProtectedApi;
+import org.gluu.configapi.service.ScopeService;
+import org.gluu.configapi.util.ApiConstants;
+import org.gluu.configapi.util.AttributeNames;
+import org.gluu.configapi.util.Jackson;
+import org.gluu.oxauth.model.common.OxAuthApplicationType;
+import org.gluu.oxauth.model.common.OxAuthSubjectType;
+import org.gluu.oxauth.model.registration.Client;
+import org.gluu.oxauth.service.common.ClientService;
+import org.gluu.oxauth.service.common.EncryptionService;
+import org.gluu.util.security.StringEncrypter.EncryptionException;
 import org.slf4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.PATCH;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 
 /**
  * @author Mougang T.Gasmyr
  *
  */
 
-//@Path(ApiConstants.BASE_API_URL + ApiConstants.CLIENTS)
-//@Produces(MediaType.APPLICATION_JSON)
-//@Consumes(MediaType.APPLICATION_JSON)
-//@RequestScoped
+@Path(ApiConstants.BASE_API_URL + ApiConstants.CLIENTS)
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@RequestScoped
 public class ClientsResource extends BaseResource {
     /**
      *
@@ -24,7 +55,7 @@ public class ClientsResource extends BaseResource {
 
     @Inject
     Logger logger;
-/*
+
     @Inject
     ClientService clientService;
 
@@ -36,9 +67,10 @@ public class ClientsResource extends BaseResource {
 
     @GET
     @ProtectedApi(scopes = { READ_ACCESS })
-    public Response getOpenIdConnectClients(@DefaultValue(DEFAULT_LIST_SIZE) @QueryParam(value = ApiConstants.LIMIT) int limit,
+    public Response getOpenIdConnectClients(
+            @DefaultValue(DEFAULT_LIST_SIZE) @QueryParam(value = ApiConstants.LIMIT) int limit,
             @DefaultValue("") @QueryParam(value = ApiConstants.PATTERN) String pattern) {
-        List<OxAuthClient> clients = new ArrayList<OxAuthClient>();
+        List<Client> clients = new ArrayList<Client>();
         if (!pattern.isEmpty() && pattern.length() >= 2) {
             clients = clientService.searchClients(pattern, limit);
         } else {
@@ -51,14 +83,14 @@ public class ClientsResource extends BaseResource {
     @ProtectedApi(scopes = { READ_ACCESS })
     @Path(ApiConstants.INUM_PATH)
     public Response getOpenIdClientByInum(@PathParam(ApiConstants.INUM) @NotNull String inum) {
-        OxAuthClient client = clientService.getClientByInum(inum);
+        Client client = clientService.getClientByInum(inum);
         checkResourceNotNull(client, OPENID_CONNECT_CLIENT);
         return Response.ok(client).build();
     }
 
     @POST
     @ProtectedApi(scopes = { WRITE_ACCESS })
-    public Response createOpenIdConnect(@Valid OxAuthClient client) throws EncryptionException {
+    public Response createOpenIdConnect(@Valid Client client) throws EncryptionException {
         String inum = clientService.generateInumForNewClient();
         client.setInum(inum);
         checkNotNull(client.getDisplayName(), AttributeNames.DISPLAY_NAME);
@@ -68,32 +100,32 @@ public class ClientsResource extends BaseResource {
         if (client.getOxAuthAppType() == null) {
             client.setOxAuthAppType(OxAuthApplicationType.WEB);
         }
-        if (client.getSubjectType() == null) {
-            client.setSubjectType(OxAuthSubjectType.PUBLIC);
+        if (client.getOxAuthAppType() == null) {
+            client.setOxAuthSubjectType(OxAuthSubjectType.PUBLIC);
         }
         client.setDn(clientService.getDnForClient(inum));
-        client.setDeletable(client.getExp() != null);
+        client.setDeletable(client.getClientSecretExpiresAt() != null);
         clientService.addClient(client);
-        OxAuthClient result = clientService.getClientByInum(inum);
+        Client result = clientService.getClientByInum(inum);
         return Response.status(Response.Status.CREATED).entity(result).build();
     }
 
     @PUT
     @ProtectedApi(scopes = { WRITE_ACCESS })
-    public Response updateClient(@Valid OxAuthClient client) throws EncryptionException {
+    public Response updateClient(@Valid Client client) throws EncryptionException {
         String inum = client.getInum();
         checkNotNull(inum, AttributeNames.INUM);
         checkNotNull(client.getDisplayName(), AttributeNames.DISPLAY_NAME);
-        OxAuthClient existingClient = clientService.getClientByInum(inum);
+        Client existingClient = clientService.getClientByInum(inum);
         checkResourceNotNull(existingClient, OPENID_CONNECT_CLIENT);
         client.setInum(existingClient.getInum());
         client.setBaseDn(clientService.getDnForClient(inum));
-        client.setDeletable(client.getExp() != null);
+        client.setDeletable(client.getExpirationDate() != null);
         if (client.getOxAuthClientSecret() != null) {
             client.setEncodedClientSecret(encryptionService.encrypt(client.getOxAuthClientSecret()));
         }
         clientService.updateClient(client);
-        OxAuthClient result = clientService.getClientByInum(existingClient.getInum());
+        Client result = clientService.getClientByInum(existingClient.getInum());
         if (result.getEncodedClientSecret() != null) {
             result.setOxAuthClientSecret(encryptionService.decrypt(client.getEncodedClientSecret()));
         }
@@ -105,7 +137,7 @@ public class ClientsResource extends BaseResource {
     @ProtectedApi(scopes = { WRITE_ACCESS })
     @Path(ApiConstants.INUM_PATH)
     public Response patchClient(@PathParam(ApiConstants.INUM) @NotNull String inum, @NotNull String pathString) {
-        OxAuthClient existingClient = clientService.getClientByInum(inum);
+        Client existingClient = clientService.getClientByInum(inum);
         checkResourceNotNull(existingClient, OPENID_CONNECT_CLIENT);
         try {
             existingClient = Jackson.applyPatch(pathString, existingClient);
@@ -121,10 +153,10 @@ public class ClientsResource extends BaseResource {
     @Path(ApiConstants.INUM_PATH)
     @ProtectedApi(scopes = { WRITE_ACCESS })
     public Response deleteClient(@PathParam(ApiConstants.INUM) @NotNull String inum) {
-        OxAuthClient client = clientService.getClientByInum(inum);
+        Client client = clientService.getClientByInum(inum);
         checkResourceNotNull(client, OPENID_CONNECT_CLIENT);
         clientService.removeClient(client);
         return Response.noContent().build();
     }
-*/
+
 }
