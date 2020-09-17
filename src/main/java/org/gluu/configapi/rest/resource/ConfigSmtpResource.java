d96787630745342d05285d0a8e7d740c72b6d129
Gasmyr <thomas@gluu.org>
2020-09-17 01:38:14 +0100

    Fix Resources implementation

--- src/main/java/org/gluu/configapi/rest/resource/ConfigSmtpResource.java
+++ src/main/java/org/gluu/configapi/rest/resource/ConfigSmtpResource.java
@@ -3,15 +3,38 @@
  */
 package org.gluu.configapi.rest.resource;
 
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.gluu.configapi.filters.ProtectedApi;
+import org.gluu.configapi.util.ApiConstants;
+import org.gluu.model.SmtpConfiguration;
+import org.gluu.oxauth.service.common.ConfigurationService;
+import org.gluu.oxauth.service.common.EncryptionService;
+import org.gluu.service.MailService;
+import org.gluu.util.security.StringEncrypter.EncryptionException;
+import org.oxauth.persistence.model.configuration.GluuConfiguration;
+
 /**
  * @author Mougang T.Gasmyr
  *
  */
-//@Path(ApiConstants.BASE_API_URL + ApiConstants.CONFIG + ApiConstants.SMTP)
-//@Produces(MediaType.APPLICATION_JSON)
-//@Consumes(MediaType.APPLICATION_JSON)
+@Path(ApiConstants.BASE_API_URL + ApiConstants.CONFIG + ApiConstants.SMTP)
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ConfigSmtpResource extends BaseResource {
-/*
+
 	@Inject
 	ConfigurationService configurationService;
 
@@ -85,5 +108,5 @@ public class ConfigSmtpResource extends BaseResource {
 		configurationService.updateConfiguration(configurationUpdate);
 		return Response.noContent().build();
 	}
-*/
+
 }
--- src/main/java/org/gluu/configapi/rest/resource/OIDSectorResource.java
+++ src/main/java/org/gluu/configapi/rest/resource/OIDSectorResource.java
@@ -3,98 +3,130 @@
  */
 package org.gluu.configapi.rest.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.PATCH;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.gluu.configapi.filters.ProtectedApi;
+import org.gluu.configapi.util.ApiConstants;
+import org.gluu.configapi.util.AttributeNames;
+import org.gluu.configapi.util.Jackson;
+import org.gluu.oxauth.service.common.SectorService;
+import org.oxauth.persistence.model.SectorIdentifier;
+import org.slf4j.Logger;
+
 /**
  * @author Mougang T.Gasmyr
  *
  */
-//@Path(ApiConstants.BASE_API_URL + ApiConstants.OPENID + ApiConstants.SECTORS)
-//@Consumes(MediaType.APPLICATION_JSON)
-//@Produces(MediaType.APPLICATION_JSON)
+@Path(ApiConstants.BASE_API_URL + ApiConstants.OPENID + ApiConstants.SECTORS)
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class OIDSectorResource extends BaseResource {
 
     /**
      *
      */
     private static final String SECTOR_IDENTIFIER = "sector identifier";
-/*
-	@Inject
-	Logger logger;
 
-	@GET
-	@ProtectedApi(scopes = { READ_ACCESS })
-	public Response getSectorIdentifiers(@DefaultValue(DEFAULT_LIST_SIZE) @QueryParam(value = ApiConstants.LIMIT) int limit,
-			@DefaultValue("") @QueryParam(value = ApiConstants.PATTERN) String pattern) {
-		List<OxAuthSectorIdentifier> sectors = new ArrayList<OxAuthSectorIdentifier>();
-		if (!pattern.isEmpty()) {
-			sectors = sectorIdentifierService.searchSectorIdentifiers(pattern, limit);
-		} else {
-			sectors = sectorIdentifierService.getAllSectorIdentifiers();
-		}
-		return Response.ok().entity(sectors).build();
-	}
+    @Inject
+    Logger logger;
 
-	@GET
-	@ProtectedApi(scopes = { READ_ACCESS })
-	@Path(ApiConstants.INUM_PATH)
-	public Response getSectorByInum(@PathParam(ApiConstants.INUM) @NotNull String inum) {
-		OxAuthSectorIdentifier sectorIdentifier = sectorIdentifierService.getSectorIdentifierById(inum);
-		checkResourceNotNull(sectorIdentifier, SECTOR_IDENTIFIER);
-		return Response.ok().entity(sectorIdentifier).build();
-	}
+    @Inject
+    SectorService sectorIdentifierService;
 
-	@POST
-	@ProtectedApi(scopes = { WRITE_ACCESS })
-	public Response createNewOpenIDSector(@Valid OxAuthSectorIdentifier sectorIdentifier) {
-		checkNotNull(sectorIdentifier.getDescription(), AttributeNames.DESCRIPTION);
-		String oxId = sectorIdentifierService.generateIdForNewSectorIdentifier();
-		sectorIdentifier.setId(oxId);
-		sectorIdentifier.setBaseDn(sectorIdentifierService.getDnForSectorIdentifier(oxId));
-		sectorIdentifierService.addSectorIdentifier(sectorIdentifier);
-		OxAuthSectorIdentifier result = sectorIdentifierService.getSectorIdentifierById(oxId);
-		return Response.status(Response.Status.CREATED).entity(result).build();
-	}
+    @GET
+    @ProtectedApi(scopes = { READ_ACCESS })
+    public Response getSectorIdentifiers(
+            @DefaultValue(DEFAULT_LIST_SIZE) @QueryParam(value = ApiConstants.LIMIT) int limit,
+            @DefaultValue("") @QueryParam(value = ApiConstants.PATTERN) String pattern) {
+        List<SectorIdentifier> sectors = new ArrayList<SectorIdentifier>();
+        if (!pattern.isEmpty()) {
+            sectors = sectorIdentifierService.searchSectorIdentifiers(pattern, limit);
+        } else {
+            sectors = sectorIdentifierService.getAllSectorIdentifiers();
+        }
+        return Response.ok().entity(sectors).build();
+    }
 
-	@PUT
-	@ProtectedApi(scopes = { WRITE_ACCESS })
-	public Response updateSector(@Valid OxAuthSectorIdentifier sectorIdentifier) {
-		String inum = sectorIdentifier.getId();
-		checkNotNull(inum, AttributeNames.INUM);
-		checkNotNull(sectorIdentifier.getDescription(), AttributeNames.DESCRIPTION);
-		OxAuthSectorIdentifier existingSector = sectorIdentifierService.getSectorIdentifierById(inum);
-		checkResourceNotNull(existingSector, SECTOR_IDENTIFIER);
-		sectorIdentifier.setId(existingSector.getId());
-		sectorIdentifier.setBaseDn(sectorIdentifierService.getDnForSectorIdentifier(inum));
-		sectorIdentifierService.updateSectorIdentifier(sectorIdentifier);
-		OxAuthSectorIdentifier result = sectorIdentifierService.getSectorIdentifierById(existingSector.getId());
-		return Response.ok(result).build();
-	}
+    @GET
+    @ProtectedApi(scopes = { READ_ACCESS })
+    @Path(ApiConstants.INUM_PATH)
+    public Response getSectorByInum(@PathParam(ApiConstants.INUM) @NotNull String inum) {
+        SectorIdentifier sectorIdentifier = sectorIdentifierService.getSectorIdentifierById(inum);
+        checkResourceNotNull(sectorIdentifier, SECTOR_IDENTIFIER);
+        return Response.ok().entity(sectorIdentifier).build();
+    }
 
-	@PATCH
-	@Consumes(MediaType.APPLICATION_JSON_PATCH_JSON)
-	@ProtectedApi(scopes = { WRITE_ACCESS })
-	@Path(ApiConstants.INUM_PATH)
-	public Response patchScope(@PathParam(ApiConstants.INUM) @NotNull String inum, @NotNull String pathString) {
-		OxAuthSectorIdentifier existingSector = sectorIdentifierService.getSectorIdentifierById(inum);
-		checkResourceNotNull(existingSector, SECTOR_IDENTIFIER);
-		try {
-			existingSector = Jackson.applyPatch(pathString, existingSector);
-			sectorIdentifierService.updateSectorIdentifier(existingSector);
-			return Response.ok(existingSector).build();
-		} catch (Exception e) {
-			logger.error("", e);
-			throw new WebApplicationException(e.getMessage());
-		}
-	}
+    @POST
+    @ProtectedApi(scopes = { WRITE_ACCESS })
+    public Response createNewOpenIDSector(@Valid SectorIdentifier sectorIdentifier) {
+        checkNotNull(sectorIdentifier.getDescription(), AttributeNames.DESCRIPTION);
+        String oxId = sectorIdentifierService.generateIdForNewSectorIdentifier();
+        sectorIdentifier.setId(oxId);
+        sectorIdentifier.setBaseDn(sectorIdentifierService.getDnForSectorIdentifier(oxId));
+        sectorIdentifierService.addSectorIdentifier(sectorIdentifier);
+        SectorIdentifier result = sectorIdentifierService.getSectorIdentifierById(oxId);
+        return Response.status(Response.Status.CREATED).entity(result).build();
+    }
 
-	@DELETE
-	@Path(ApiConstants.INUM_PATH)
-	@ProtectedApi(scopes = { WRITE_ACCESS })
-	public Response deleteSector(@PathParam(ApiConstants.INUM) @NotNull String inum) {
-		OxAuthSectorIdentifier sectorIdentifier = sectorIdentifierService.getSectorIdentifierById(inum);
-		checkResourceNotNull(sectorIdentifier, SECTOR_IDENTIFIER);
-		sectorIdentifierService.removeSectorIdentifier(sectorIdentifier);
-		return Response.noContent().build();
+    @PUT
+    @ProtectedApi(scopes = { WRITE_ACCESS })
+    public Response updateSector(@Valid SectorIdentifier sectorIdentifier) {
+        String inum = sectorIdentifier.getId();
+        checkNotNull(inum, AttributeNames.INUM);
+        checkNotNull(sectorIdentifier.getDescription(), AttributeNames.DESCRIPTION);
+        SectorIdentifier existingSector = sectorIdentifierService.getSectorIdentifierById(inum);
+        checkResourceNotNull(existingSector, SECTOR_IDENTIFIER);
+        sectorIdentifier.setId(existingSector.getId());
+        sectorIdentifier.setBaseDn(sectorIdentifierService.getDnForSectorIdentifier(inum));
+        sectorIdentifierService.updateSectorIdentifier(sectorIdentifier);
+        SectorIdentifier result = sectorIdentifierService.getSectorIdentifierById(existingSector.getId());
+        return Response.ok(result).build();
+    }
 
-	}
-*/
+    @PATCH
+    @Consumes(MediaType.APPLICATION_JSON_PATCH_JSON)
+    @ProtectedApi(scopes = { WRITE_ACCESS })
+    @Path(ApiConstants.INUM_PATH)
+    public Response patchScope(@PathParam(ApiConstants.INUM) @NotNull String inum, @NotNull String pathString) {
+        SectorIdentifier existingSector = sectorIdentifierService.getSectorIdentifierById(inum);
+        checkResourceNotNull(existingSector, SECTOR_IDENTIFIER);
+        try {
+            existingSector = Jackson.applyPatch(pathString, existingSector);
+            sectorIdentifierService.updateSectorIdentifier(existingSector);
+            return Response.ok(existingSector).build();
+        } catch (Exception e) {
+            logger.error("", e);
+            throw new WebApplicationException(e.getMessage());
+        }
+    }
+
+    @DELETE
+    @Path(ApiConstants.INUM_PATH)
+    @ProtectedApi(scopes = { WRITE_ACCESS })
+    public Response deleteSector(@PathParam(ApiConstants.INUM) @NotNull String inum) {
+        SectorIdentifier sectorIdentifier = sectorIdentifierService.getSectorIdentifierById(inum);
+        checkResourceNotNull(sectorIdentifier, SECTOR_IDENTIFIER);
+        sectorIdentifierService.removeSectorIdentifier(sectorIdentifier);
+        return Response.noContent().build();
+
+    }
 }
