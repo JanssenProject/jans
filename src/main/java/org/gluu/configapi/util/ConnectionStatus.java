a65bd1ffe52119a90ccf95cae45b3ef6913beb0b
YuriyZ <yzabrovarniy@gmail.com>
2020-09-18 10:50:23 +0300

    Clean up
    https://github.com/GluuFederation/oxauth-config/issues/166

--- src/main/java/org/gluu/configapi/util/ConnectionStatus.java
+++ src/main/java/org/gluu/configapi/util/ConnectionStatus.java
@@ -1,90 +1,87 @@
 package org.gluu.configapi.util;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-
-import org.slf4j.Logger;
-
-import static org.apache.commons.lang3.StringUtils.EMPTY;
-
+import org.gluu.configapi.configuration.ConfigurationFactory;
 import org.gluu.model.SimpleProperty;
 import org.gluu.model.ldap.GluuLdapConfiguration;
 import org.gluu.persist.ldap.impl.LdapEntryManagerFactory;
 import org.gluu.persist.ldap.operation.impl.LdapConnectionProvider;
 import org.gluu.util.properties.FileConfiguration;
 import org.gluu.util.security.PropertiesDecrypter;
-import org.gluu.configapi.configuration.ConfigurationFactory;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 @ApplicationScoped
 public class ConnectionStatus {
 
-  @Inject
-  Logger logger;
+    @Inject
+    Logger logger;
 
-  @Inject
-  ConfigurationFactory configurationFactory;
+    @Inject
+    ConfigurationFactory configurationFactory;
 
-  public boolean isUp(GluuLdapConfiguration ldapConfiguration) {
-    Properties properties = new Properties();
-    FileConfiguration configuration = loadFileConfiguration();
-    if (configuration != null)
-      properties = configuration.getProperties();
-    properties.setProperty("bindDN", ldapConfiguration.getBindDN());
-    properties.setProperty("bindPassword", ldapConfiguration.getBindPassword());
-    properties.setProperty("servers", buildServersString(getServers(ldapConfiguration)));
-    properties.setProperty("useSSL", Boolean.toString(ldapConfiguration.isUseSSL()));
+    public boolean isUp(GluuLdapConfiguration ldapConfiguration) {
+        FileConfiguration configuration = loadFileConfiguration();
 
-    LdapConnectionProvider connectionProvider = new LdapConnectionProvider(
-        PropertiesDecrypter.decryptProperties(properties, configurationFactory.getCryptoConfigurationSalt()));
+        Properties properties = configuration.getProperties();
+        properties.setProperty("bindDN", ldapConfiguration.getBindDN());
+        properties.setProperty("bindPassword", ldapConfiguration.getBindPassword());
+        properties.setProperty("servers", buildServersString(getServers(ldapConfiguration)));
+        properties.setProperty("useSSL", Boolean.toString(ldapConfiguration.isUseSSL()));
 
-    if (connectionProvider.getConnectionPool() != null) {
-      boolean isConnected = connectionProvider.isConnected();
-      connectionProvider.closeConnectionPool();
-      return isConnected;
+        LdapConnectionProvider connectionProvider = new LdapConnectionProvider(
+                PropertiesDecrypter.decryptProperties(properties, configurationFactory.getCryptoConfigurationSalt()));
+
+        if (connectionProvider.getConnectionPool() != null) {
+            boolean isConnected = connectionProvider.isConnected();
+            connectionProvider.closeConnectionPool();
+            return isConnected;
+        }
+
+        return false;
     }
 
-    return false;
-  }
-
-  private FileConfiguration loadFileConfiguration() {
-    FileConfiguration configuration = null;
-    try {
-      configuration = new FileConfiguration(ConfigurationFactory.getAppPropertiesFile());
-    } catch (Exception ex) {
-      logger.error("ConnectionStatus:::loadFileConfiguration() - ****Exception**** = " + ex);
-      configuration = new FileConfiguration(LdapEntryManagerFactory.LDAP_DEFAULT_PROPERTIES_FILE);
-    }
-    return configuration;
-  }
-
-  private List<String> getServers(GluuLdapConfiguration ldapConfiguration) {
-    List<String> servers = new ArrayList<String>();
-    for (SimpleProperty server : ldapConfiguration.getServers())
-      servers.add(server.getValue());
-    return servers;
-  }
-
-  private String buildServersString(List<String> servers) {
-    if (servers == null) {
-      return EMPTY;
+    @NotNull
+    private FileConfiguration loadFileConfiguration() {
+        try {
+            return new FileConfiguration(ConfigurationFactory.getAppPropertiesFile());
+        } catch (Exception ex) {
+            logger.error("Failed to load configuration.", ex);
+            return new FileConfiguration(LdapEntryManagerFactory.LDAP_DEFAULT_PROPERTIES_FILE);
+        }
     }
 
-    StringBuilder sb = new StringBuilder();
-    boolean first = true;
-    for (String server : servers) {
-      if (first) {
-        first = false;
-      } else {
-        sb.append(",");
-      }
-
-      sb.append(server);
+    private static List<String> getServers(GluuLdapConfiguration ldapConfiguration) {
+        List<String> servers = new ArrayList<>();
+        for (SimpleProperty server : ldapConfiguration.getServers())
+            servers.add(server.getValue());
+        return servers;
     }
 
-    return sb.toString();
-  }
+    private static String buildServersString(List<String> servers) {
+        if (servers == null) {
+            return EMPTY;
+        }
 
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for (String server : servers) {
+            if (first) {
+                first = false;
+            } else {
+                sb.append(",");
+            }
+
+            sb.append(server);
+        }
+
+        return sb.toString();
+    }
 }
