4e09dbaf35b245c925977449bf0f9e1995d81d48
pujavs <pujas.works@gmail.com>
2020-10-02 13:18:06 +0530

    168 - OpenId Auth

--- src/main/java/org/gluu/configapi/auth/AuthorizationService.java
+++ src/main/java/org/gluu/configapi/auth/AuthorizationService.java
@@ -1,6 +1,7 @@
 package org.gluu.configapi.auth;
 
 import org.gluu.configapi.filters.ProtectedApi;
+
 import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.Response;
 import java.lang.reflect.Method;
@@ -14,12 +15,11 @@ public abstract class AuthorizationService implements Serializable {
 
     private static final long serialVersionUID = -8853549310376125248L;
 
-    public abstract Response processAuthorization(String token, ResourceInfo resourceInfo) throws Exception;
-    
+    public abstract void validateAuthorization(String token, ResourceInfo resourceInfo) throws Exception;
+
     protected Response getErrorResponse(Response.Status status, String detail) {
         return Response.status(status).entity(detail).build();
     }
-
 
     public List<String> getRequestedScopes(ResourceInfo resourceInfo) {
         Class<?> resourceClass = resourceInfo.getResourceClass();
@@ -40,6 +40,10 @@ public abstract class AuthorizationService implements Serializable {
         if (methodAnnotation != null) {
             scopes.addAll(Stream.of(methodAnnotation.scopes()).collect(Collectors.toList()));
         }
+    }
+
+    public boolean validateScope(List<String> authScopes, List<String> resourceScopes) {
+        return authScopes.containsAll(resourceScopes);
     }
 
 }
