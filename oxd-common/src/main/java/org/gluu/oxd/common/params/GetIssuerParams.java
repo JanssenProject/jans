10de43c3b90058885ba3bead8e9c551b0ca9c5f2
Arnab Dutta <arnab.bdutta@gmail.com>
2020-10-06 16:59:44 +0530

    #476 - Check if issuer from OpenID Provider Configuration Information matches with Webfinger issuer

--- oxd-common/src/main/java/org/gluu/oxd/common/params/GetIssuerParams.java
+++ oxd-common/src/main/java/org/gluu/oxd/common/params/GetIssuerParams.java
@@ -6,8 +6,41 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GetIssuerParams implements IParams {
 
+    @JsonProperty(value = "op_host")
+    private String op_host;
+
+    @JsonProperty(value = "op_discovery_path")
+    private String op_discovery_path;
+
+    @JsonProperty(value = "op_configuration_endpoint")
+    private String op_configuration_endpoint;
+
     @JsonProperty(value = "resource")
     private String resource;
+
+    public String getOpHost() {
+        return op_host;
+    }
+
+    public void setOpHost(String opHost) {
+        this.op_host = opHost;
+    }
+
+    public String getOpDiscoveryPath() {
+        return op_discovery_path;
+    }
+
+    public void setOpDiscoveryPath(String opDiscoveryPath) {
+        this.op_discovery_path = opDiscoveryPath;
+    }
+
+    public String getOpConfigurationEndpoint() {
+        return op_configuration_endpoint;
+    }
+
+    public void setOpConfigurationEndpoint(String op_configuration_endpoint) {
+        this.op_configuration_endpoint = op_configuration_endpoint;
+    }
 
     public String getResource() {
         return resource;
@@ -20,7 +53,10 @@ public class GetIssuerParams implements IParams {
     @Override
     public String toString() {
         return "WebfingerParams{" +
-                "resource='" + resource + '\'' +
+                "op_host='" + op_host + '\'' +
+                ", op_discovery_path='" + op_discovery_path + '\'' +
+                ", op_configuration_endpoint='" + op_configuration_endpoint + '\'' +
+                ", resource='" + resource + '\'' +
                 '}';
     }
 
