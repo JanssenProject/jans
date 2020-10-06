725a95a0288c582ebaba21e587da3b564f311637
YuriyZ <yzabrovarniy@gmail.com>
2020-10-06 15:26:42 +0300

    Missed files

---
+++ oxModel/src/main/java/io/jans/model/uma/ClaimDefinitionList.java
@@ -0,0 +1,24 @@
+package io.jans.model.uma;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * @author yuriyz on 06/16/2017.
+ */
+public class ClaimDefinitionList extends ArrayList<ClaimDefinition> {
+
+    @JsonIgnore
+    public ClaimDefinitionList addPermission(ClaimDefinition claim) {
+        add(claim);
+        return this;
+    }
+
+    public static ClaimDefinitionList instance(ClaimDefinition... claims) {
+        ClaimDefinitionList instance = new ClaimDefinitionList();
+        Collections.addAll(instance, claims);
+        return instance;
+    }
+}
