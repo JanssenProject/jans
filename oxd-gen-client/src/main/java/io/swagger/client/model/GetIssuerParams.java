39da0c07fc435b4eb0824fc0181ff3c6115e42a1
Arnab Dutta <arnab.bdutta@gmail.com>
2020-10-06 16:59:44 +0530

    #476 - Check if issuer from OpenID Provider Configuration Information matches with Webfinger issuer

--- oxd-gen-client/src/main/java/io/swagger/client/model/GetIssuerParams.java
+++ oxd-gen-client/src/main/java/io/swagger/client/model/GetIssuerParams.java
@@ -27,8 +27,71 @@ import java.io.IOException;
 
 
 public class GetIssuerParams {
+  @SerializedName("op_configuration_endpoint")
+  private String opConfigurationEndpoint = null;
+
+  @SerializedName("op_host")
+  private String opHost = null;
+
+  @SerializedName("op_discovery_path")
+  private String opDiscoveryPath = null;
+
   @SerializedName("resource")
   private String resource = null;
+
+  public GetIssuerParams opConfigurationEndpoint(String opConfigurationEndpoint) {
+    this.opConfigurationEndpoint = opConfigurationEndpoint;
+    return this;
+  }
+
+   /**
+   * The openid configuration endpoint URL. If missing, then &#x60;op_host&#x60; must be defined.
+   * @return opConfigurationEndpoint
+  **/
+  @Schema(example = "https://op.example.com/acme/.well-known/openid-configuration", required = true, description = "The openid configuration endpoint URL. If missing, then `op_host` must be defined.")
+  public String getOpConfigurationEndpoint() {
+    return opConfigurationEndpoint;
+  }
+
+  public void setOpConfigurationEndpoint(String opConfigurationEndpoint) {
+    this.opConfigurationEndpoint = opConfigurationEndpoint;
+  }
+
+  public GetIssuerParams opHost(String opHost) {
+    this.opHost = opHost;
+    return this;
+  }
+
+   /**
+   * Deprecated in favor of &#x60;op_configuration_endpoint&#x60;. It will be removed in future version(s). Provide the URL of OpenID Provider (OP) in this field. If missing, then &#x60;op_configuration_endpoint&#x60; must be defined.
+   * @return opHost
+  **/
+  @Schema(example = "https://<ophostname>", description = "Deprecated in favor of `op_configuration_endpoint`. It will be removed in future version(s). Provide the URL of OpenID Provider (OP) in this field. If missing, then `op_configuration_endpoint` must be defined.")
+  public String getOpHost() {
+    return opHost;
+  }
+
+  public void setOpHost(String opHost) {
+    this.opHost = opHost;
+  }
+
+  public GetIssuerParams opDiscoveryPath(String opDiscoveryPath) {
+    this.opDiscoveryPath = opDiscoveryPath;
+    return this;
+  }
+
+   /**
+   * Deprecated in favor of &#x60;op_configuration_endpoint&#x60;. It will be removed in future version(s). Provide path to the OpenID Connect Provider&#x27;s discovery document in this field. For example, if it is &#x27;https://example.com/.well-known/openid-configuration&#x27; then the path is blank. But if it is &#x27;https://example.com/oxauth/.well-known/openid-configuration&#x27; then the path is &#x27;/oxauth&#x27;
+   * @return opDiscoveryPath
+  **/
+  @Schema(example = "/oxauth", description = "Deprecated in favor of `op_configuration_endpoint`. It will be removed in future version(s). Provide path to the OpenID Connect Provider's discovery document in this field. For example, if it is 'https://example.com/.well-known/openid-configuration' then the path is blank. But if it is 'https://example.com/oxauth/.well-known/openid-configuration' then the path is '/oxauth'")
+  public String getOpDiscoveryPath() {
+    return opDiscoveryPath;
+  }
+
+  public void setOpDiscoveryPath(String opDiscoveryPath) {
+    this.opDiscoveryPath = opDiscoveryPath;
+  }
 
   public GetIssuerParams resource(String resource) {
     this.resource = resource;
@@ -58,12 +121,15 @@ public class GetIssuerParams {
       return false;
     }
     GetIssuerParams getIssuerParams = (GetIssuerParams) o;
-    return Objects.equals(this.resource, getIssuerParams.resource);
+    return Objects.equals(this.opConfigurationEndpoint, getIssuerParams.opConfigurationEndpoint) &&
+        Objects.equals(this.opHost, getIssuerParams.opHost) &&
+        Objects.equals(this.opDiscoveryPath, getIssuerParams.opDiscoveryPath) &&
+        Objects.equals(this.resource, getIssuerParams.resource);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(resource);
+    return Objects.hash(opConfigurationEndpoint, opHost, opDiscoveryPath, resource);
   }
 
 
@@ -72,6 +138,9 @@ public class GetIssuerParams {
     StringBuilder sb = new StringBuilder();
     sb.append("class GetIssuerParams {\n");
     
+    sb.append("    opConfigurationEndpoint: ").append(toIndentedString(opConfigurationEndpoint)).append("\n");
+    sb.append("    opHost: ").append(toIndentedString(opHost)).append("\n");
+    sb.append("    opDiscoveryPath: ").append(toIndentedString(opDiscoveryPath)).append("\n");
     sb.append("    resource: ").append(toIndentedString(resource)).append("\n");
     sb.append("}");
     return sb.toString();
