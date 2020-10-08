02f36ccf258d82f05185d378bff8771506f5dc7e
YuriyZ <yzabrovarniy@gmail.com>
2020-10-08 18:22:07 +0300

    Optimized imports

--- client/src/test/java/org/gluu/oxauth/ciba/BackchannelAuthenticationPollMode.java
+++ client/src/test/java/org/gluu/oxauth/ciba/BackchannelAuthenticationPollMode.java
@@ -7,13 +7,13 @@
 package org.gluu.oxauth.ciba;
 
 import org.apache.commons.lang.RandomStringUtils;
-import org.gluu.oxauth.model.ciba.BackchannelAuthenticationErrorResponseType;
-import org.gluu.oxauth.model.common.*;
-import org.json.JSONObject;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
 import org.gluu.oxauth.BaseTest;
 import org.gluu.oxauth.client.*;
+import org.gluu.oxauth.model.ciba.BackchannelAuthenticationErrorResponseType;
+import org.gluu.oxauth.model.common.AuthenticationMethod;
+import org.gluu.oxauth.model.common.BackchannelTokenDeliveryMode;
+import org.gluu.oxauth.model.common.GrantType;
+import org.gluu.oxauth.model.common.ResponseType;
 import org.gluu.oxauth.model.crypto.OxAuthCryptoProvider;
 import org.gluu.oxauth.model.crypto.encryption.BlockEncryptionAlgorithm;
 import org.gluu.oxauth.model.crypto.encryption.KeyEncryptionAlgorithm;
@@ -30,14 +30,18 @@ import org.gluu.oxauth.model.jwt.JwtHeaderName;
 import org.gluu.oxauth.model.register.ApplicationType;
 import org.gluu.oxauth.model.util.StringUtils;
 import org.gluu.oxauth.model.util.Util;
+import org.json.JSONObject;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
+import java.nio.charset.StandardCharsets;
 import java.security.PrivateKey;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-import static org.testng.Assert.*;
 import static org.gluu.oxauth.model.register.RegisterRequestParam.*;
+import static org.testng.Assert.*;
 
 /**
  * @author Javier Rojas Blum
@@ -2999,7 +3003,7 @@ public class BackchannelAuthenticationPollMode extends BaseTest {
         String idToken = authorizationResponse.getIdToken();
 
         // 3. Validate id_token
-        Jwe jwe = Jwe.parse(idToken, null, clientSecret.getBytes(Util.UTF8_STRING_ENCODING));
+        Jwe jwe = Jwe.parse(idToken, null, clientSecret.getBytes(StandardCharsets.UTF_8));
         assertNotNull(jwe.getHeader().getClaimAsString(JwtHeaderName.TYPE));
         assertNotNull(jwe.getHeader().getClaimAsString(JwtHeaderName.ALGORITHM));
         assertNotNull(jwe.getClaims().getClaimAsString(JwtClaimName.ISSUER));
@@ -3068,7 +3072,7 @@ public class BackchannelAuthenticationPollMode extends BaseTest {
         String idToken = authorizationResponse.getIdToken();
 
         // 3. Validate id_token
-        Jwe jwe = Jwe.parse(idToken, null, clientSecret.getBytes(Util.UTF8_STRING_ENCODING));
+        Jwe jwe = Jwe.parse(idToken, null, clientSecret.getBytes(StandardCharsets.UTF_8));
         assertNotNull(jwe.getHeader().getClaimAsString(JwtHeaderName.TYPE));
         assertNotNull(jwe.getHeader().getClaimAsString(JwtHeaderName.ALGORITHM));
         assertNotNull(jwe.getClaims().getClaimAsString(JwtClaimName.ISSUER));
