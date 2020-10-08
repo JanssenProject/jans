8ad56be66c256d8e707c08a3805906771a4a4184
YuriyZ <yzabrovarniy@gmail.com>
2020-10-08 23:03:34 +0300

    server module : Moved test sources to io.jans.as.server package

---
+++ server/src/test/java/io/jans/as/server/comp/GrantServiceTest.java
@@ -0,0 +1,69 @@
+/*
+ * Janssen Project software is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2020, Janssen Project
+ */
+
+package io.jans.as.server.comp;
+
+import io.jans.as.server.BaseComponentTest;
+import io.jans.as.server.model.ldap.TokenLdap;
+import io.jans.as.server.model.ldap.TokenType;
+import io.jans.as.server.service.GrantService;
+import io.jans.as.server.util.TokenHashUtil;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import javax.inject.Inject;
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ * @author Yuriy Movchan
+ * @version September 16, 2015
+ */
+
+public class GrantServiceTest extends BaseComponentTest {
+
+	private static final String TEST_TOKEN_CODE = UUID.randomUUID().toString();
+
+	@Inject
+	private GrantService grantService;
+
+	private static String m_clientId;
+
+	private static TokenLdap m_tokenLdap;
+
+	@Parameters(value = "clientId")
+	@Test
+	public void createTestToken(String clientId) {
+		this.m_clientId = clientId;
+		m_tokenLdap = createTestToken();
+		grantService.persist(m_tokenLdap);
+	}
+
+	@Test(dependsOnMethods = "createTestToken")
+	public void removeTestTokens() {
+		final TokenLdap t = grantService.getGrantByCode(TEST_TOKEN_CODE);
+		if (t != null) {
+			grantService.remove(t);
+		}
+	}
+
+	private TokenLdap createTestToken() {
+		final String grantId = GrantService.generateGrantId();
+		final String dn = grantService.buildDn(TokenHashUtil.hash(TEST_TOKEN_CODE));
+
+		final TokenLdap t = new TokenLdap();
+		t.setDn(dn);
+		t.setGrantId(grantId);
+		t.setClientId(m_clientId);
+		t.setTokenCode(TokenHashUtil.hash(TEST_TOKEN_CODE));
+		t.setTokenType(TokenType.ACCESS_TOKEN.getValue());
+		t.setCreationDate(new Date());
+		t.setExpirationDate(new Date());
+		return t;
+	}
+
+}
---
+++ server/src/test/java/io/jans/as/server/uma/ws/rs/ObtainPatWSTest.java
@@ -0,0 +1,47 @@
+/*
+ * Janssen Project software is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2020, Janssen Project
+ */
+
+package io.jans.as.server.uma.ws.rs;
+
+import io.jans.as.model.uma.wrapper.Token;
+import io.jans.as.client.BaseTest;
+import io.jans.as.server.model.uma.TUma;
+import io.jans.as.model.uma.UmaTestUtil;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.net.URI;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ * @version 0.9, 18/03/2013
+ */
+
+public class ObtainPatWSTest extends BaseTest {
+
+	@ArquillianResource
+	private URI url;
+
+	private static Token pat;
+
+	@Test
+	@Parameters({ "authorizePath", "tokenPath", "umaUserId", "umaUserSecret", "umaPatClientId", "umaPatClientSecret",
+			"umaRedirectUri" })
+	public void requestPat(String authorizePath, String tokenPath, String umaUserId, String umaUserSecret,
+			String umaPatClientId, String umaPatClientSecret, String umaRedirectUri) {
+		pat = TUma.requestPat(url, authorizePath, tokenPath, umaUserId, umaUserSecret, umaPatClientId,
+				umaPatClientSecret, umaRedirectUri);
+		UmaTestUtil.assert_(pat);
+	}
+
+	@Test(dependsOnMethods = "requestPat")
+	@Parameters({ "tokenPath", "umaPatClientId", "umaPatClientSecret" })
+	public void requestNewPatByRefreshTokne(String tokenPath, String umaPatClientId, String umaPatClientSecret) {
+		final Token newPat = TUma.newTokenByRefreshToken(url, tokenPath, pat, umaPatClientId, umaPatClientSecret);
+		UmaTestUtil.assert_(newPat);
+	}
+}
