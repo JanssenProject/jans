5198bece1bbb5d79f047e145ccddff09b9f655c8
YuriyZ <yzabrovarniy@gmail.com>
2020-10-08 17:56:38 +0300

    Correcting copyright text on source code

--- server/src/main/java/org/gluu/oxauth/uma/ws/rs/UmaGatheringWS.java
+++ server/src/main/java/org/gluu/oxauth/uma/ws/rs/UmaGatheringWS.java
@@ -1,7 +1,7 @@
 /*
  * Janssen Project software is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
  *
- * Copyright (c) 2015, Gluu
+ * Copyright (c) 2020, Janssen Project
  */
 
 package org.gluu.oxauth.uma.ws.rs;
