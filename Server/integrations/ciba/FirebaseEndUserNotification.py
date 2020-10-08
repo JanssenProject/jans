a42be8659565fea0485ae73d39977afb96e6556c
YuriyZ <yzabrovarniy@gmail.com>
2020-10-08 17:47:02 +0300

    Correcting copyright text on source code

--- Server/integrations/ciba/FirebaseEndUserNotification.py
+++ Server/integrations/ciba/FirebaseEndUserNotification.py
@@ -1,4 +1,4 @@
-# oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+# Janssen Project software is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
 # Copyright (c) 2018, Gluu
 #
 # Author: Milton BO
