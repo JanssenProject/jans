8001b33735260c0507cdc93df6517f4dd084fa31
pujavs <pujas.works@gmail.com>
2020-10-09 18:56:57 +0530

    To fix compile time error for karate test cases

--- src/main/java/io/jans/configapi/service/UmaService.java
+++ src/main/java/io/jans/configapi/service/UmaService.java
@@ -1,17 +1,23 @@
 package io.jans.configapi.service;
 
+import io.jans.as.client.uma.UmaMetadataService;
+import io.jans.as.client.uma.UmaPermissionService;
+import io.jans.as.client.uma.UmaRptIntrospectionService;
 import io.jans.as.model.uma.UmaMetadata;
-import io.jans.util.exception.ConfigurationException;
-import org.slf4j.Logger;
+import io.jans.configapi.auth.AuthClientFactory;
+import io.jans.exception.ConfigurationException;
+import io.jans.exception.OxIntializationException;
+import io.jans.util.init.Initializable;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
+import org.slf4j.Logger;
 
 @ApplicationScoped
-@Named("umaService")
-public class UmaService implements Serializable {
+public class UmaService extends Initializable implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -21,24 +27,55 @@ public class UmaService implements Serializable {
     Logger logger;
 
     @Inject
-    UmaService umaService;
+    ConfigurationService configurationService;
 
     private UmaMetadata umaMetadata;
-    
-    public UmaService() {
+    private UmaPermissionService umaPermissionService;
+    private UmaRptIntrospectionService umaRptIntrospectionService;
+
+    @Override
+    protected void initInternal() {
         try {
-            loadUmaConfiguration();
+            loadUmaConfigurationService();
         } catch (Exception ex) {
-            throw new ConfigurationException("Failed to load oxAuth UMA configuration");
-        }               
+            throw new ConfigurationException("Failed to load oxAuth UMA configuration", ex);
+        }
     }
-   
-    
-    public UmaMetadata getUmaMetadata() throws Exception{
+
+    public UmaMetadata getUmaMetadata() throws Exception {
+        init();
         return this.umaMetadata;
     }
 
-    public UmaMetadata loadUmaConfiguration() throws Exception {
-        return null;
+    public void loadUmaConfigurationService() throws Exception {
+        this.umaMetadata = getUmaMetadataConfiguration();
+        this.umaPermissionService = AuthClientFactory.getUmaPermissionService(this.umaMetadata, false);
+        this.umaRptIntrospectionService = AuthClientFactory.getUmaRptIntrospectionService(this.umaMetadata, false);
     }
+
+    @Produces
+    @ApplicationScoped
+    @Named("umaMetadataConfiguration")
+    public UmaMetadata getUmaMetadataConfiguration() throws OxIntializationException {
+
+        logger.info("##### Getting UMA Metadata Service ...");
+        logger.debug(
+                "\n\n UmaService::initUmaMetadataConfiguration() - configurationService.find().getUmaConfigurationEndpoint() = "
+                        + configurationService.find().getUmaConfigurationEndpoint());
+        UmaMetadataService umaMetadataService = AuthClientFactory
+                .getUmaMetadataService(configurationService.find().getUmaConfigurationEndpoint(), false);
+        logger.debug("\n\n UmaService::initUmaMetadataConfiguration() - umaMetadataService = " + umaMetadataService);
+
+        logger.info("##### Getting UMA Metadata ...");
+        UmaMetadata umaMetadata = umaMetadataService.getMetadata();
+        logger.debug("\n\n UmaService::initUmaMetadataConfiguration() - umaMetadata = " + umaMetadata);
+        logger.info("##### Getting UMA metadata ... DONE");
+
+        if (umaMetadata == null) {
+            throw new OxIntializationException("UMA meta data configuration is invalid!");
+        }
+
+        return umaMetadata;
+    }
+
 }
