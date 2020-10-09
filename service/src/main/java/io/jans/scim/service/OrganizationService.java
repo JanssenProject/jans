d44b5fcf8309ced04f2cfcaf03b1cf5063054c07
yurem <Yuriy.Movchan@gmail.com>
2020-10-09 18:31:03 +0300

    Clean up services

--- service/src/main/java/io/jans/scim/service/OrganizationService.java
+++ service/src/main/java/io/jans/scim/service/OrganizationService.java
@@ -6,32 +6,21 @@
 
 package io.jans.scim.service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.annotation.Priority;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.slf4j.Logger;
+
 import io.jans.config.oxtrust.AppConfiguration;
 import io.jans.config.oxtrust.LdapOxAuthConfiguration;
-import io.jans.model.GluuStatus;
-import io.jans.model.ProgrammingLanguage;
 import io.jans.orm.PersistenceEntryManager;
 import io.jans.orm.exception.BasePersistenceException;
-import io.jans.orm.model.base.GluuBoolean;
+import io.jans.scim.model.GluuOrganization;
 import io.jans.service.BaseCacheService;
 import io.jans.service.CacheService;
 import io.jans.service.LocalCacheService;
-import io.jans.util.ArrayHelper;
-import io.jans.util.StringHelper;
-import org.slf4j.Logger;
-
-import io.jans.scim.model.GluuOrganization;
-import io.jans.scim.util.OxTrustConstants;
 
 /**
  * Provides operations with organization
@@ -61,26 +50,6 @@ public class OrganizationService extends io.jans.service.OrganizationService {
 	private AppConfiguration appConfiguration;
 
 	/**
-	 * Update organization entry
-	 * 
-	 * @param organization
-	 *            Organization
-	 */
-	public void updateOrganization(GluuOrganization organization) {
-		persistenceEntryManager.merge(organization);
-
-	}
-
-	/**
-	 * Check if LDAP server contains organization with specified attributes
-	 * 
-	 * @return True if organization with specified attributes exist
-	 */
-	public boolean containsOrganization(String dn) {
-		return persistenceEntryManager.contains(dn, GluuOrganization.class);
-	}
-
-	/**
 	 * Get organization
 	 * 
 	 * @return Organization
@@ -97,10 +66,6 @@ public class OrganizationService extends io.jans.service.OrganizationService {
 		return organization;
 	}
 
-	public String getOrgName() {
-		return getOrganization().getDisplayName();
-	}
-
 	public String getDnForOrganization() {
 		return getDnForOrganization(appConfiguration.getBaseDN());
 	}
@@ -112,23 +77,6 @@ public class OrganizationService extends io.jans.service.OrganizationService {
 	 */
 	public String getBaseDn() {
 		return appConfiguration.getBaseDN();
-	}
-
-	public LdapOxAuthConfiguration getOxAuthSetting(String configurationDn) {
-		LdapOxAuthConfiguration ldapOxAuthConfiguration = null;
-		try {
-			configurationDn = configurationDn.replace("ou=oxtrust", "ou=oxauth");
-			ldapOxAuthConfiguration = persistenceEntryManager.find(LdapOxAuthConfiguration.class, configurationDn);
-			return ldapOxAuthConfiguration;
-		} catch (BasePersistenceException ex) {
-			log.error("Failed to load configuration from LDAP");
-		}
-
-		return null;
-	}
-
-	public void saveLdapOxAuthConfiguration(LdapOxAuthConfiguration ldapOxAuthConfiguration) {
-		persistenceEntryManager.merge(ldapOxAuthConfiguration);
 	}
 
 	private BaseCacheService getCacheService() {
