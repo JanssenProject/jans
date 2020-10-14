44fd25a8a2fef32243977cab346f4638b862c082
yurem <Yuriy.Movchan@gmail.com>
2020-10-14 12:23:58 +0300

    Fix license, properties and conform jans project

--- server/src/main/java/io/jans/fido2/service/processor/assertion/U2FAssertionFormatProcessor.java
+++ server/src/main/java/io/jans/fido2/service/processor/assertion/U2FAssertionFormatProcessor.java
@@ -1,6 +1,11 @@
 /*
+ * Janssen Project software is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2020, Janssen Project
+ */
+
+/*
  * Copyright (c) 2018 Mastercard
- * Copyright (c) 2020 Gluu
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
