a0bf642c74226700643eefaab213df8d23da054e
Mustafa Baser <mbaser@mail.com>
2020-10-15 14:09:15 +0300

    implement installer and fixes

--- setup_app/utils/package_utils.py
+++ setup_app/utils/package_utils.py
@@ -1,3 +1,4 @@
+import os
 
 from setup_app import paths
 from setup_app.config import Config
@@ -14,7 +15,7 @@ class PackageUtils(SetupUtils):
             query_command = 'dpkg-query -W -f=\'${{Status}}\' {} 2>/dev/null | grep -c "ok installed"'
             check_text = '0'
 
-        elif self.os_type in ('centos', 'red', 'fedora'):
+        elif base.clone_type == 'rpm':
             install_command = 'yum install -y {0}'
             update_command = 'yum install -y epel-release'
             query_command = 'rpm -q {0}'
@@ -28,18 +29,9 @@ class PackageUtils(SetupUtils):
 
         install_list = {'mondatory': [], 'optional': []}
 
-        package_list = {
-                'debian 10': {'mondatory': 'apache2 curl wget tar xz-utils unzip facter python3 rsyslog python3-ldap3 python3-requests python3-ruamel.yaml bzip2', 'optional': 'memcached'},
-                'debian 9': {'mondatory': 'apache2 curl wget tar xz-utils unzip facter python3 rsyslog python3-ldap3 python3-requests python3-ruamel.yaml bzip2', 'optional': 'memcached'},
-                'debian 8': {'mondatory': 'apache2 curl wget tar xz-utils unzip facter python3 rsyslog python3-ldap3 python3-requests python3-ruamel.yaml bzip2', 'optional': 'memcached'},
-                'ubuntu 16': {'mondatory': 'apache2 curl wget xz-utils unzip facter python3 rsyslog python3-ldap3 python3-requests python3-ruamel.yaml bzip2', 'optional': 'memcached'},
-                'ubuntu 18': {'mondatory': 'apache2 curl wget xz-utils unzip facter python3 rsyslog python3-ldap3 net-tools python3-requests python3-ruamel.yaml bzip2', 'optional': 'memcached'},
-                'centos 7': {'mondatory': 'httpd mod_ssl curl wget tar xz unzip facter python3 python3-ldap3 python3-ruamel-yaml rsyslog bzip2', 'optional': 'memcached'},
-                'red 7': {'mondatory': 'httpd mod_ssl curl wget tar xz unzip facter python3 rsyslog python3-ldap3 python3-requests python3-ruamel-yaml bzip2', 'optional': 'memcached'},
-                'fedora 22': {'mondatory': 'httpd mod_ssl curl wget tar xz unzip facter python3 rsyslog python3-ldap3 python3-requests python3-ruamel-yaml bzip2', 'optional': 'memcached'},
-                }
+        package_list = base.get_os_package_list()
 
-        os_type_version = self.os_type+' '+self.os_version
+        os_type_version = base.os_type + ' ' + base.os_version
 
         for install_type in install_list:
             for package in package_list[os_type_version][install_type].split():
@@ -60,32 +52,32 @@ class PackageUtils(SetupUtils):
             if install_list[install_type]:
                 packages = " ".join(install_list[install_type])
 
-                if not setupOptions['noPrompt']:
-                    if install_type == 'mondatory':
-                        print("The following packages are required for Jans Server")
-                        print(packages)
-                        r = input("Do you want to install these now? [Y/n] ")
-                        if r and r.lower()=='n':
-                            install[install_type] = False
-                            if install_type == 'mondatory':
-                                print("Can not proceed without installing required packages. Exiting ...")
-                                sys.exit()
 
-                    elif install_type == 'optional':
-                        print("You may need the following packages")
-                        print(packages)
-                        r = input("Do you want to install these now? [y/N] ")
-                        if r and r.lower()=='y':
-                            install[install_type] = True
+                if install_type == 'mondatory':
+                    print("The following packages are required for Jans Server")
+                    print(packages)
+                    r = input("Do you want to install these now? [Y/n] ")
+                    if r and r.lower()=='n':
+                        install[install_type] = False
+                        if install_type == 'mondatory':
+                            print("Can not proceed without installing required packages. Exiting ...")
+                            sys.exit()
+
+                elif install_type == 'optional':
+                    print("You may need the following packages")
+                    print(packages)
+                    r = input("Do you want to install these now? [y/N] ")
+                    if r and r.lower()=='y':
+                        install[install_type] = True
 
                 if install[install_type]:
                     self.logIt("Installing packages " + packages)
                     print("Installing packages", packages)
-                    if not self.os_type == 'fedora':
+                    if not base.os_type == 'fedora':
                         sout, serr = self.run(update_command, shell=True, get_stderr=True)
                     self.run(install_command.format(packages), shell=True)
 
-        if self.os_type in ('ubuntu', 'debian'):
+        if base.clone_type == 'deb':
             self.run('a2enmod ssl headers proxy proxy_http proxy_ajp', shell=True)
             default_site = '/etc/apache2/sites-enabled/000-default.conf'
             if os.path.exists(default_site):
