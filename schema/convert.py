0a500289ee2efb997dc92895578c9451f528f98a
Mustafa Baser <mbaser@mail.com>
2020-10-19 15:55:41 +0300

    exclude option for schma converter

--- schema/convert.py
+++ schema/convert.py
@@ -38,26 +38,45 @@ conversions = []
 def do_replace(eVal):
     if eVal in opendj_attributes:
         return eVal
-    for m in mapping:
+    for m in mapping['mappings']:
         if m in eVal:
-            eVal = eVal.replace(m, mapping[m])
+            eVal = eVal.replace(m, mapping['mappings'][m])
 
     return eVal
 
-for attribute in schema['attributeTypes']:
+for attribute in schema['attributeTypes'][:]:
+
+    skip = False
+    for name in attribute['names']:
+        if name in mapping['exclude']['attributeType']:
+            schema['attributeTypes'].remove(attribute)
+            skip = True
+    if skip:
+        continue
+
     for e in ('desc', 'equality', 'oid', 'x_origin'):
         if e in attribute:
             attribute[e] = do_replace(attribute[e])
 
-    for i in range(len((attribute['names']))):
-        cur = attribute['names'][i]
-        new = do_replace(cur)
-        if cur != new:
-            conversions.append((cur, new))
-        attribute['names'][i] = new
+    new_name_list = []
+    for name in (attribute['names']):
+        new = do_replace(name)
+        if not new in new_name_list:
+            conversions.append((name, new))
+            new_name_list.append(new)
+
+    attribute['names'] = new_name_list
 
 obj_conversions = []
-for obj in schema['objectClasses']:
+for obj in schema['objectClasses'][:]:
+    skip = False
+    for name in obj['names']:
+        if name in mapping['exclude']['objectClass']:
+            schema['objectClasses'].remove(obj)
+            skip = True
+    if skip:
+        continue
+
     for e in ('oid',  'x_origin'):
         if e in obj:
             obj[e] = do_replace(obj[e])
