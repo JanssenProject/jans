85c223b57dea2ecaefbc83aa7a350670f9695f30
Arnab Dutta <arnab.bdutta@gmail.com>
2020-10-20 09:33:28 +0530

    #2 - jans-client-api project setup

--- oxd-server/src/main/bin/oxd-start-console.bat
+++
@@ -1,5 +0,0 @@
-cd /d %~dp0
-set LIB=../lib
-set CONF=../conf/oxd-server.yml
-echo CONF=%CONF%
-java -Djava.net.preferIPv4Stack=true -cp %LIB%/oxd-server.jar;%LIB%/* org.gluu.oxd.server.OxdServerApplication server %CONF%
