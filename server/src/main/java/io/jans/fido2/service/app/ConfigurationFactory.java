868279887fc26485219aa17b896817ab753f3128
yurem <Yuriy.Movchan@gmail.com>
2020-10-20 11:59:22 +0300

    Use gluu.base to find jans.properties location

--- server/src/main/java/io/jans/fido2/service/app/ConfigurationFactory.java
+++ server/src/main/java/io/jans/fido2/service/app/ConfigurationFactory.java
@@ -77,8 +77,8 @@ public class ConfigurationFactory {
 	private final static int DEFAULT_INTERVAL = 30; // 30 seconds
 
 	static {
-		if (System.getProperty("gluu.base") != null) {
-			BASE_DIR = System.getProperty("gluu.base");
+		if (System.getProperty("jans.base") != null) {
+			BASE_DIR = System.getProperty("jans.base");
 		} else if ((System.getProperty("catalina.base") != null) && (System.getProperty("catalina.base.ignore") == null)) {
 			BASE_DIR = System.getProperty("catalina.base");
 		} else if (System.getProperty("catalina.home") != null) {
