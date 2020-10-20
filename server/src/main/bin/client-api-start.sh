0a35d712d4bc47514d048604651e119f0f5480e4
Arnab Dutta <arnab.bdutta@gmail.com>
2020-10-20 09:33:28 +0530

    #2 - jans-client-api project setup

---
+++ server/src/main/bin/client-api-start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Please run it with JDK 1.6 or higher
+
+BASEDIR=$(dirname $0)
+CONF=$BASEDIR/../conf/oxd-server.yml
+echo BASEDIR=$BASEDIR
+echo CONF=$CONF
+
+LIB=$BASEDIR/../lib
+javaExe=java
+$javaExe -Djava.net.preferIPv4Stack=true -cp $LIB/oxd-server.jar:$LIB/* OxdServerApplication server $CONF
