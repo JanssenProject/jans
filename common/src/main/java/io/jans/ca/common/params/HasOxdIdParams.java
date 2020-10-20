6215a622f69fff7564f7a79e510c8c7aad13a750
Arnab Dutta <arnab.bdutta@gmail.com>
2020-10-20 09:33:28 +0530

    #2 - jans-client-api project setup

---
+++ common/src/main/java/io/jans/ca/common/params/HasOxdIdParams.java
@@ -0,0 +1,11 @@
+package io.jans.ca.common.params;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ * @version 0.9, 23/05/2016
+ */
+
+public interface HasOxdIdParams extends IParams {
+
+    String getOxdId();
+}
