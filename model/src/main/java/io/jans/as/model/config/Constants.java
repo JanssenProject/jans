d995c52b06844d98aa7a45c5016ea0d39ea92c6d
YuriyZ <yzabrovarniy@gmail.com>
2020-10-21 23:31:42 +0300

    1. Fixed wrong references of oxauth_ConfigurationEntryDN to new jansAuth_ConfigurationEntryDN. 2. Added constants for property key references to be able reference it in other apps (e.g. jans-config-api)

---
+++ model/src/main/java/io/jans/as/model/config/Constants.java
@@ -0,0 +1,16 @@
+package io.jans.as.model.config;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ */
+public class Constants {
+
+    private Constants() {
+    }
+
+    public static final String SERVER_KEY_OF_CONFIGURATION_ENTRY = "jansAuth_ConfigurationEntryDN";
+
+    public static final String BASE_PROPERTIES_FILE_NAME = "jans.properties";
+    public static final String LDAP_PROPERTIES_FILE_NAME = "jans-ldap.properties";
+    public static final String COUCHBASE_PROPERTIES_FILE_NAME = "jans-couchbase.properties";
+}
