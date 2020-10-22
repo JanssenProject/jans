cff1cb634ac75279a5acd1f6a2dc7abf043c355c
YuriyZ <yzabrovarniy@gmail.com>
2020-10-22 11:54:46 +0300

    Switched protection to oauth2 and load UMA stuff only if uma protection is on.

--- src/main/java/io/jans/configapi/auth/UmaAuthorizationService.java
+++ src/main/java/io/jans/configapi/auth/UmaAuthorizationService.java
@@ -6,16 +6,23 @@
 
 package io.jans.configapi.auth;
 
-//import io.jans.as.client.uma.wrapper.UmaClient;
-import io.jans.as.client.uma.UmaRptIntrospectionService;
-import io.jans.as.model.uma.RptIntrospectionResponse;
+import io.jans.as.common.service.common.ApplicationFactory;
+import io.jans.as.model.config.StaticConfiguration;
 import io.jans.as.model.uma.UmaMetadata;
-import io.jans.as.model.uma.wrapper.Token;
 import io.jans.as.model.uma.persistence.UmaResource;
-import io.jans.as.common.service.common.EncryptionService;
+import io.jans.as.model.uma.wrapper.Token;
+import io.jans.configapi.configuration.ConfigurationFactory;
 import io.jans.configapi.service.UmaService;
+import io.jans.configapi.util.ApiConstants;
+import io.jans.exception.ConfigurationException;
+import io.jans.orm.PersistenceEntryManager;
+import io.jans.orm.search.filter.Filter;
+import io.jans.util.StringHelper;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.WebApplicationException;
@@ -26,29 +33,19 @@ import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 
-import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import io.jans.util.Pair;
-import io.jans.util.StringHelper;
-import io.jans.util.security.StringEncrypter.EncryptionException;
-
 @ApplicationScoped
 @Named("umaAuthorizationService")
 public class UmaAuthorizationService extends AuthorizationService implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private Token umaPat;
-    private long umaPatAccessTokenExpiration = 0l; // When the "accessToken" will expire;
+    private long umaPatAccessTokenExpiration = 0L; // When the "accessToken" will expire;
     private final ReentrantLock lock = new ReentrantLock();
 
     @Inject
-    private Logger logger;
+    private Logger log;
 
-    @Inject
-    private EncryptionService encryptionService;
-
-    @Inject
-    UmaResource configApiResource;
+    UmaResource umaResource;
 
     @Inject
     UmaService umaService;
@@ -56,48 +53,67 @@ public class UmaAuthorizationService extends AuthorizationService implements Ser
     @Inject
     private UmaMetadata umaMetadata;
 
+    @Inject
+    @Named(ApplicationFactory.PERSISTENCE_ENTRY_MANAGER_NAME)
+    Instance<PersistenceEntryManager> persistenceManager;
+
+    @Inject
+    StaticConfiguration staticConf;
+
     public void validateAuthorization(String token, ResourceInfo resourceInfo) throws Exception {
-        logger.debug(" UmaAuthorizationService::validateAuthorization() - token = " + token
+        log.debug(" UmaAuthorizationService::validateAuthorization() - token = " + token
                 + " , resourceInfo.getClass().getName() = " + resourceInfo.getClass().getName() + "\n");
 
         if (StringUtils.isBlank(token)) {
-            logger.info("Token is blank");
+            log.info("Token is blank");
             throw new WebApplicationException(Response.status(Response.Status.UNAUTHORIZED).build());
         }
 
         List<String> resourceScopes = getRequestedScopes(resourceInfo);
-        logger.debug(" UmaAuthorizationService::validateAuthorization() - resourceScopes = " + resourceScopes + "\n");
+        log.debug(" UmaAuthorizationService::validateAuthorization() - resourceScopes = " + resourceScopes + "\n");
 
         Token patToken = null;
         try {
             patToken = getPatToken(); // WIP ??
         } catch (Exception ex) {
-            logger.error("Failed to obtain PAT token", ex);
+            log.error("Failed to obtain PAT token", ex);
             throw new WebApplicationException("Failed to obtain PAT token", ex);
         }
 
-        logger.debug("this.umaService.getUmaMetadata() = " + this.umaService.getUmaMetadata());
+        log.debug("this.umaService.getUmaMetadata() = " + this.umaService.getUmaMetadata());
 
-        logger.debug("AuthClientFactory::createUmaMetadataService() - resourceScopes = " + resourceScopes + "\n\n");
+        log.debug("AuthClientFactory::createUmaMetadataService() - resourceScopes = " + resourceScopes + "\n\n");
 
         if (!resourceScopes.isEmpty()) {
             this.umaService.validateRptToken(patToken, token, getUmaResourceId(), resourceScopes);
         } else {
             this.umaService.validateRptToken(patToken, token, getUmaResourceId(), getUmaScope());
         }
+    }
 
+    public UmaResource getUmaResource() {
+        if (umaResource != null)
+            return umaResource;
+        umaResource = loadUmaResource();
+        return umaResource;
     }
 
     public String getUmaResourceId() {
-        return configApiResource.getId();
+        if (umaResource == null)
+            return null;
+
+        return umaResource.getId();
     }
 
     public String getUmaScope() {
-        String scopes = new String();
-        List<String> scopeList = configApiResource.getScopes();
+        if (umaResource == null)
+            return null;
+
+        String scopes = "";
+        List<String> scopeList = umaResource.getScopes();
         if (scopeList != null && !scopeList.isEmpty()) {
             scopes = scopeList.stream().map(s -> s.concat(" ")).collect(Collectors.joining());
-            logger.debug(scopes);
+            log.debug(scopes);
         }
 
         return scopes.trim();
@@ -138,7 +154,7 @@ public class UmaAuthorizationService extends AuthorizationService implements Ser
         try {
             return getPatToken() != null;
         } catch (Exception ex) {
-            logger.error("Failed to check UMA PAT token status", ex);
+            log.error("Failed to check UMA PAT token status", ex);
         }
 
         return false;
@@ -156,7 +172,7 @@ public class UmaAuthorizationService extends AuthorizationService implements Ser
         if (umaMetadata == null) {
             return;
         }
-        logger.debug("\n\n getClientKeyStoreFile() = " + getClientKeyStoreFile() + " , getClientKeyStorePassword() = "
+        log.debug("\n\n getClientKeyStoreFile() = " + getClientKeyStoreFile() + " , getClientKeyStorePassword() = "
                 + getClientKeyStorePassword() + " , getClientId() =" + getClientId() + " , getClientKeyId() = "
                 + getClientKeyId() + "\n\n");
         String umaClientKeyStoreFile = getClientKeyStoreFile();
@@ -193,6 +209,36 @@ public class UmaAuthorizationService extends AuthorizationService implements Ser
         // Get new access token only if is the previous one is missing or expired
         return !((validatePatToken == null) || (validatePatToken.getAccessToken() == null)
                 || (validatePatTokenExpiration <= now));
+    }
+
+    public UmaResource loadUmaResource() {
+        if (StringHelper.isEmpty(ConfigurationFactory.getApiResourceName())) {
+            log.trace("Config API Resource not defined");
+            return null;
+        }
+
+        try {
+            String[] targetArray = new String[] { ConfigurationFactory.getApiResourceName() };
+            Filter jsIdFilter = Filter.createSubstringFilter("jansId", null, targetArray, null);
+            // Filter jsIdFilter = Filter.createSubstringFilter("oxId", null, targetArray,
+            // null);
+            Filter displayNameFilter = Filter.createSubstringFilter(ApiConstants.DISPLAY_NAME, null, targetArray, null);
+            Filter searchFilter = Filter.createORFilter(jsIdFilter, displayNameFilter);
+
+            List<UmaResource> umaResourceList = persistenceManager.get()
+                    .findEntries(staticConf.getBaseDn().getUmaBase(), UmaResource.class, searchFilter);
+            log.trace(" \n umaResourceList = " + umaResourceList + "\n");
+
+            if (umaResourceList == null || umaResourceList.isEmpty()) {
+                log.trace("Unable to find UMA resource by name: " + ConfigurationFactory.getApiResourceName());
+                return null;
+            }
+
+            return umaResourceList.get(0);
+        } catch (Exception ex) {
+            log.error("Failed to load Config API Resource.", ex);
+            throw new ConfigurationException("Failed to load Config API Resource.", ex);
+        }
     }
 
 }
