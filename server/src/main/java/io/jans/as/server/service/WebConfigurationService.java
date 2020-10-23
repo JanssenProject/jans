d30f42d1b308e0a153bfe1e94ecdaa6c47b6cd0a
yurem <Yuriy.Movchan@gmail.com>
2020-10-23 12:26:36 +0300

    Fix web resources location

---
+++ server/src/main/java/io/jans/as/server/service/WebConfigurationService.java
@@ -0,0 +1,63 @@
+package io.jans.as.server.service;
+/*
+ * Janssen Project software is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2020, Janssen Project
+ */
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.ServletContext;
+
+import io.jans.as.model.configuration.AppConfiguration;
+import io.jans.util.StringHelper;
+
+/**
+ * OxAuthConfigurationService
+ *
+ * @author Oleksiy Tataryn Date: 08.07.2014
+ */
+@ApplicationScoped
+@Named
+public class WebConfigurationService {
+
+	@Inject
+	private AppConfiguration appConfiguration;
+	
+	@Inject
+	private ServletContext context;
+
+	public String getCssLocation() {
+		if (StringHelper.isEmpty(appConfiguration.getCssLocation())) {
+			FacesContext ctx = FacesContext.getCurrentInstance();
+			if (ctx == null) {
+				return "";
+			}
+			String contextPath = ctx.getExternalContext().getRequestContextPath();
+			return contextPath + "/stylesheet";
+		} else {
+			return appConfiguration.getCssLocation();
+		}
+	}
+
+	public String getJsLocation() {
+		if (StringHelper.isEmpty(appConfiguration.getJsLocation())) {
+			String contextPath = context.getContextPath();
+			return contextPath + "/js";
+		} else {
+			return appConfiguration.getJsLocation();
+		}
+	}
+
+	public String getImgLocation() {
+		if (StringHelper.isEmpty(appConfiguration.getImgLocation())) {
+			String contextPath = context.getContextPath();
+			return contextPath + "/img";
+		} else {
+			return appConfiguration.getImgLocation();
+		}
+	}
+
+}
