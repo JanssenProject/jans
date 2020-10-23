4a0a8fe7b554ee1a2e1921d5ae530609f118e0ec
yurem <Yuriy.Movchan@gmail.com>
2020-10-23 14:20:51 +0300

    Fix cache OC

--- cache/src/main/java/io/jans/service/cache/NativePersistenceCacheEntity.java
+++ cache/src/main/java/io/jans/service/cache/NativePersistenceCacheEntity.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 @DataEntry
-@ObjectClass(value = "cache")
+@ObjectClass(value = "jansCache")
 public class NativePersistenceCacheEntity extends DeletableEntity implements Serializable, Deletable {
 
     @DN
