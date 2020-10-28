1c342263c1f6ec2c1abe159a16775127ced7832e
Arnab Dutta <arnab.bdutta@gmail.com>
2020-10-28 21:33:53 +0530

    #2 - Fixing health-check test case

--- server/src/test/java/io/jans/ca/server/HealthCheckTest.java
+++ server/src/test/java/io/jans/ca/server/HealthCheckTest.java
@@ -1,7 +1,11 @@
 package io.jans.ca.server;
 
+import io.jans.ca.common.Jackson2;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Map;
 
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertNotNull;
@@ -11,11 +15,15 @@ public class HealthCheckTest {
 
     @Parameters({"host", "opHost"})
     @Test
-    public void testHealthCheck(String host, String opHost) {
+    public void testHealthCheck(String host, String opHost) throws IOException {
 
         String resp = Tester.newClient(host).healthCheck();
         assertNotNull(resp);
-        assertEquals("{\"status\":\"running\"}", resp);
+        Map<String,String> map = Jackson2.createRpMapper().readValue(resp, Map.class);
+
+        assertEquals(map.get("application"), "oxd");
+        assertEquals(map.get("status"), "running");
+        assertEquals(map.get("version"), System.getProperty("projectVersion"));
 
     }
 }
