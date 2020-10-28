4e630f64b8573fae771844ea41f1124f8680c7e4
yurem <Yuriy.Movchan@gmail.com>
2020-10-28 15:03:50 +0300

    Fix scope OC

--- persistence-model/src/main/java/io/jans/as/persistence/model/Scope.java
+++ persistence-model/src/main/java/io/jans/as/persistence/model/Scope.java
@@ -18,7 +18,7 @@ import java.util.List;
  * @author Yuriy Movchan Date: 06/30/2015
  */
 @DataEntry
-@ObjectClass(value = "jansCustomScope")
+@ObjectClass(value = "jansScope")
 public class Scope extends DeletableEntity implements Serializable {
 
     private static final long serialVersionUID = 4308826784917052508L;
