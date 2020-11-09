e3cb6c91f114d11b046ac13d340299e31b19d19e
yurem <Yuriy.Movchan@gmail.com>
2020-11-09 19:44:50 +0300

    Use Fido2 configuration

---
+++ server/src/main/java/io/jans/fido2/service/app/AttributeService.java
@@ -0,0 +1,28 @@
+/*
+ * Janssen Project software is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2020, Janssen Project
+ */
+
+package io.jans.fido2.service.app;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import io.jans.fido2.model.conf.AppConfiguration;
+
+/**
+ * @author Javier Rojas Blum
+ * @version May 30, 2018
+ */
+@ApplicationScoped
+public class AttributeService extends io.jans.as.common.service.AttributeService {
+
+	@Inject
+	private AppConfiguration appConfiguration;
+
+    protected boolean isUseLocalCache() {
+    	return appConfiguration.isUseLocalCache();
+    }
+
+}
---
+++ server/src/main/java/io/jans/fido2/service/app/OrganizationService.java
@@ -0,0 +1,26 @@
+/*
+ * Janssen Project software is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2020, Janssen Project
+ */
+
+package io.jans.fido2.service.app;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import io.jans.fido2.model.conf.AppConfiguration;
+
+@ApplicationScoped
+@Named("organizationService")
+public class OrganizationService extends io.jans.as.common.service.OrganizationService {
+
+	@Inject
+	private AppConfiguration appConfiguration;
+
+    protected boolean isUseLocalCache() {
+    	return appConfiguration.isUseLocalCache();
+    }
+
+}
