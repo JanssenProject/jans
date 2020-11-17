c5c6cdb590b5b26a9782f557df2a049d6d994afe
pujavs <pujas.works@gmail.com>
2020-11-17 22:44:34 +0530

    OpenId API Protection Code

--- src/main/java/io/jans/configapi/auth/service/OpenIdService.java
+++ src/main/java/io/jans/configapi/auth/service/OpenIdService.java
@@ -9,21 +9,17 @@ package io.jans.configapi.auth.service;
 import io.jans.as.client.service.IntrospectionService;
 import io.jans.as.model.common.IntrospectionResponse;
 import io.jans.configapi.auth.client.AuthClientFactory;
-import io.jans.configapi.auth.client.OpenIdClientService;
 import io.jans.configapi.service.ConfigurationService;
-import io.jans.util.exception.ConfigurationException;
-import io.jans.util.init.Initializable;
 import org.slf4j.Logger;
 
-import javax.ws.rs.Path;
-import javax.ws.rs.core.MediaType;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
 import javax.inject.Inject;
-import java.io.IOException;
 import java.io.Serializable;
 
 @ApplicationScoped
-public class OpenIdService extends Initializable implements Serializable {
+@Named("openIdService")
+public class OpenIdService implements Serializable {
 
     private static final long serialVersionUID = 4564959567069741194L;
 
@@ -36,7 +32,6 @@ public class OpenIdService extends Initializable implements Serializable {
     private IntrospectionService introspectionService;
 
     public IntrospectionService getIntrospectionService() {
-        init();
         return introspectionService;
     }
 
@@ -46,26 +41,6 @@ public class OpenIdService extends Initializable implements Serializable {
 
     public IntrospectionResponse getIntrospectionResponse(String header, String token) {
         return AuthClientFactory.getIntrospectionResponse(getIntrospectionEndpoint(), header, token, false);
-    }
-
-    @Override
-    protected void initInternal() {
-        try {
-            loadOpenIdConfiguration();
-        } catch (IOException ex) {
-            log.error("Failed to load oxAuth OpenId configuration", ex);
-            throw new ConfigurationException("Failed to load oxAuth OpenId configuration", ex);
-        }
-    }
-
-    private void loadOpenIdConfiguration() throws IOException {
-        log.debug("OpenIdService::loadOpenIdConfiguration() - configurationService.find().getIntrospectionEndpoint() = "
-                + configurationService.find().getIntrospectionEndpoint());
-        String introspectionEndpoint = configurationService.find().getIntrospectionEndpoint();
-        this.introspectionService = AuthClientFactory.getIntrospectionService(introspectionEndpoint, false);
-
-        log.debug("\n\n OpenIdService::loadOpenIdConfiguration() - introspectionService =" + introspectionService);
-        log.info("Successfully loaded oxAuth configuration");
     }
 
 }
