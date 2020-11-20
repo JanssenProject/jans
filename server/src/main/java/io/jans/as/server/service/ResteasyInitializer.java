9b998e8c75d8139035d1fbd9fb98bf356a81cf94
yurem <Yuriy.Movchan@gmail.com>
2020-11-20 20:39:46 +0300

    Enumerate only required endpoints to avoid scan for all pathes

--- server/src/main/java/io/jans/as/server/service/ResteasyInitializer.java
+++ server/src/main/java/io/jans/as/server/service/ResteasyInitializer.java
@@ -6,8 +6,37 @@
 
 package io.jans.as.server.service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+
+import io.jans.as.server.authorize.ws.rs.AuthorizeRestWebServiceImpl;
+import io.jans.as.server.authorize.ws.rs.DeviceAuthorizationRestWebServiceImpl;
+import io.jans.as.server.bcauthorize.ws.rs.BackchannelAuthorizeRestWebServiceImpl;
+import io.jans.as.server.bcauthorize.ws.rs.BackchannelDeviceRegistrationRestWebServiceImpl;
+import io.jans.as.server.clientinfo.ws.rs.ClientInfoRestWebServiceImpl;
+import io.jans.as.server.introspection.ws.rs.IntrospectionWebService;
+import io.jans.as.server.jans.ws.rs.JansConfigurationWS;
+import io.jans.as.server.jwk.ws.rs.JwkRestWebServiceImpl;
+import io.jans.as.server.register.ws.rs.RegisterRestWebServiceImpl;
+import io.jans.as.server.revoke.RevokeRestWebServiceImpl;
+import io.jans.as.server.revoke.RevokeSessionRestWebService;
+import io.jans.as.server.session.ws.rs.CheckSessionStatusRestWebServiceImpl;
+import io.jans.as.server.session.ws.rs.EndSessionRestWebServiceImpl;
+import io.jans.as.server.token.ws.rs.TokenRestWebServiceImpl;
+import io.jans.as.server.uma.ws.rs.UmaGatheringWS;
+import io.jans.as.server.uma.ws.rs.UmaMetadataWS;
+import io.jans.as.server.uma.ws.rs.UmaPermissionRegistrationWS;
+import io.jans.as.server.uma.ws.rs.UmaResourceRegistrationWS;
+import io.jans.as.server.uma.ws.rs.UmaRptIntrospectionWS;
+import io.jans.as.server.uma.ws.rs.UmaScopeIconWS;
+import io.jans.as.server.uma.ws.rs.UmaScopeWS;
+import io.jans.as.server.userinfo.ws.rs.UserInfoRestWebServiceImpl;
+import io.jans.as.server.ws.rs.fido.u2f.U2fAuthenticationWS;
+import io.jans.as.server.ws.rs.fido.u2f.U2fConfigurationWS;
+import io.jans.as.server.ws.rs.fido.u2f.U2fRegistrationWS;
 
 /**
  * Integration with Resteasy
@@ -17,4 +46,43 @@ import javax.ws.rs.core.Application;
  */
 @ApplicationPath("/restv1")
 public class ResteasyInitializer extends Application {	
+
+	@Override
+    public Set<Class<?>> getClasses() {
+        HashSet<Class<?>> classes = new HashSet<Class<?>>();
+        classes.add(JansConfigurationWS.class);
+
+        classes.add(AuthorizeRestWebServiceImpl.class);
+        classes.add(RegisterRestWebServiceImpl.class);
+        classes.add(ClientInfoRestWebServiceImpl.class);
+        classes.add(RevokeRestWebServiceImpl.class);
+        classes.add(RevokeSessionRestWebService.class);
+        classes.add(JwkRestWebServiceImpl.class);
+        classes.add(IntrospectionWebService.class);
+
+        classes.add(TokenRestWebServiceImpl.class);
+        classes.add(UserInfoRestWebServiceImpl.class);
+        classes.add(EndSessionRestWebServiceImpl.class);
+
+        classes.add(UmaMetadataWS.class);
+        classes.add(UmaGatheringWS.class);
+        classes.add(UmaPermissionRegistrationWS.class);
+        classes.add(UmaResourceRegistrationWS.class);
+        classes.add(UmaRptIntrospectionWS.class);
+        classes.add(UmaScopeIconWS.class);
+        classes.add(UmaScopeWS.class);
+
+        classes.add(U2fConfigurationWS.class);
+        classes.add(U2fAuthenticationWS.class);
+        classes.add(U2fRegistrationWS.class);
+
+        classes.add(CheckSessionStatusRestWebServiceImpl.class);
+
+        classes.add(DeviceAuthorizationRestWebServiceImpl.class);
+        classes.add(BackchannelAuthorizeRestWebServiceImpl.class);
+        classes.add(BackchannelDeviceRegistrationRestWebServiceImpl.class);
+
+        return classes;
+    }
+
 }
