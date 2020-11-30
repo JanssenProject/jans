41cb034d42e90c2ed8ce0d76d9b4686ad3e9f8df
YuriyZ <yzabrovarniy@gmail.com>
2020-11-30 20:22:48 +0200

    Changes copyright text:
    From:"Janssen Project software is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text."
    
    To:
    "Janssen Project software is available under the Apache License (2004). See http://www.apache.org/licenses/ for full text."

--- server/integrations.deprecatred/phonefactor/PhoneFactorExternalAuthenticator.py
+++ server/integrations.deprecatred/phonefactor/PhoneFactorExternalAuthenticator.py
@@ -1,4 +1,4 @@
-# Janssen Project software is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+# Janssen Project software is available under the Apache License (2004). See http://www.apache.org/licenses/ for full text.
 # Copyright (c) 2020, Janssen Project
 #
 # Author: Yuriy Movchan
