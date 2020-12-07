1e4d635a578ea67eb4c175f23316d68fd5fd840c
moabu <moabu@users.noreply.github.com>
2020-12-07 11:27:20 +0000

    feat(helm): Update Helm Chart

--- __version__.py
+++ __version__.py
@@ -1,8 +1,8 @@
 """
- License terms and conditions for Janssen Cloud Native Edition:
+ License terms and conditions for Gluu Cloud Native Edition:
  https://www.apache.org/licenses/LICENSE-2.0
 """
 
-__version__ = "5.0.0_a1"
-__previous_version__ = "5.0.0_a1"
+__version__ = "1.2.16"
+__previous_version__ = "1.2.15"
 
