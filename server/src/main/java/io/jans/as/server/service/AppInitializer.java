d9e1578dd13a76b81b0fd7401a20c4b7d1f3e4f9
yurem <Yuriy.Movchan@gmail.com>
2020-12-08 19:54:08 +0300

    Don't stop on unsuccessfull BC installation

--- server/src/main/java/io/jans/as/server/service/AppInitializer.java
+++ server/src/main/java/io/jans/as/server/service/AppInitializer.java
@@ -179,7 +179,11 @@ public class AppInitializer {
 
 	@PostConstruct
 	public void createApplicationComponents() {
-		SecurityProviderUtility.installBCProvider();
+		try {
+			SecurityProviderUtility.installBCProvider();
+		} catch (ClassCastException ex) {
+			log.error("Failed to install BC provider properly");
+		}
 	}
 
 	public void applicationInitialized(@Observes @Initialized(ApplicationScoped.class) Object init) {
