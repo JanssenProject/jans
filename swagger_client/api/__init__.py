ea9083af1bbb2ba2891d088e08e26526f22da014
Mustafa Baser <mbaser@mail.com>
2020-12-21 23:55:36 +0300

    initial commit of cli

---
+++ swagger_client/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.database___ldap_configuration_api import DatabaseLDAPConfigurationApi
+from swagger_client.api.o_auth___scopes_api import OAuthScopesApi
