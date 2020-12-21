0dff23f2f44e44859d8e2f71812378163521473e
YuriyZ <yzabrovarniy@gmail.com>
2020-12-21 10:29:42 +0200

    (jans) `sector_identifier` has to be based on host only. Also optimize redirect_uri's validation based on `sector_identifier_uri`
    https://github.com/GluuFederation/oxAuth/issues/1503

--- server/src/main/java/io/jans/as/server/service/LocalResponseCache.java
+++ server/src/main/java/io/jans/as/server/service/LocalResponseCache.java
@@ -12,6 +12,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -23,6 +24,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class LocalResponseCache {
 
     public static final int DEFAULT_DISCOVERY_LIFETIME = 60;
+    public static final int DEFAULT_SECTOR_IDENTIFIER_LIFETIME = 1440; // 1 day
+
     private static final String DISCOVERY_CACHE_KEY = "DISCOVERY_CACHE_KEY";
 
     @Inject
@@ -31,27 +34,46 @@ public class LocalResponseCache {
     private final AtomicBoolean rebuilding = new AtomicBoolean(false);
     private Cache<String, JSONObject> discoveryCache = CacheBuilder.newBuilder()
             .expireAfterWrite(DEFAULT_DISCOVERY_LIFETIME, TimeUnit.MINUTES).build();
+    private Cache<String, List<String>> sectorIdentifierCache = CacheBuilder.newBuilder()
+            .expireAfterWrite(DEFAULT_SECTOR_IDENTIFIER_LIFETIME, TimeUnit.MINUTES).build();
 
-    private int currentLifetime = DEFAULT_DISCOVERY_LIFETIME;
+    private int currentDiscoveryLifetime = DEFAULT_DISCOVERY_LIFETIME;
+    private int currentSectorIdentifierLifetime = DEFAULT_SECTOR_IDENTIFIER_LIFETIME;
 
     @Asynchronous
     public void reloadConfigurationTimerEvent(@Observes @Scheduled AuthConfigurationEvent authConfigurationEvent) {
-        if (currentLifetime != appConfiguration.getDiscoveryCacheLifetimeInMinutes()) {
-            try {
-                if (rebuilding.get())
-                    return;
+        try {
+            if (rebuilding.get())
+                return;
 
-                rebuilding.set(true);
-                currentLifetime = appConfiguration.getDiscoveryCacheLifetimeInMinutes();
-                discoveryCache = createDiscoveryCache(appConfiguration.getDiscoveryCacheLifetimeInMinutes());
-            } finally {
-                rebuilding.set(false);
+            rebuilding.set(true);
+
+            if (currentDiscoveryLifetime != appConfiguration.getDiscoveryCacheLifetimeInMinutes()) {
+                currentDiscoveryLifetime = appConfiguration.getDiscoveryCacheLifetimeInMinutes();
+                discoveryCache = CacheBuilder.newBuilder()
+                        .expireAfterWrite(appConfiguration.getDiscoveryCacheLifetimeInMinutes(), TimeUnit.MINUTES).build();
             }
+            if (currentSectorIdentifierLifetime != appConfiguration.getSectorIdentifierCacheLifetimeInMinutes()) {
+                currentSectorIdentifierLifetime = appConfiguration.getSectorIdentifierCacheLifetimeInMinutes();
+                sectorIdentifierCache = CacheBuilder.newBuilder()
+                        .expireAfterWrite(appConfiguration.getSectorIdentifierCacheLifetimeInMinutes(), TimeUnit.MINUTES).build();
+            }
+        } finally {
+            rebuilding.set(false);
         }
     }
 
-    private static Cache<String, JSONObject> createDiscoveryCache(int lifetimeInMinutes) {
-        return CacheBuilder.newBuilder().expireAfterWrite(lifetimeInMinutes, TimeUnit.MINUTES).build();
+    public List<String> getSectorRedirectUris(String sectorIdentifierUri) {
+        if (sectorIdentifierCache == null || rebuilding.get())
+            return null;
+        return sectorIdentifierCache.getIfPresent(sectorIdentifierUri);
+    }
+
+    public void putSectorRedirectUris(String sectorIdentifierUri, List<String> redirectUris) {
+        if (sectorIdentifierCache == null || rebuilding.get())
+            return;
+
+        sectorIdentifierCache.put(sectorIdentifierUri, redirectUris);
     }
 
     public JSONObject getDiscoveryResponse() {
