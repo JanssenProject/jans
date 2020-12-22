4a60e086b903844296fb54dbd9f33452088aaa9d
YuriyZ <yzabrovarniy@gmail.com>
2020-12-22 10:39:19 +0200

    (jans) oxcore : removed printing password from JcaDocumentStoreConfiguration and WebDavDocumentStoreConfiguration

--- document-store/src/main/java/io/jans/service/document/store/conf/JcaDocumentStoreConfiguration.java
+++ document-store/src/main/java/io/jans/service/document/store/conf/JcaDocumentStoreConfiguration.java
@@ -6,9 +6,9 @@
 
 package io.jans.service.document.store.conf;
 
-import java.io.Serializable;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
 
 /**
  * @author Yuriy Movchan on 04/10/2020
@@ -77,7 +77,7 @@ public class JcaDocumentStoreConfiguration implements Serializable {
 	@Override
 	public String toString() {
 		return "JcaDocumentStoreConfiguration [serverUrl=" + serverUrl + ", workspaceName=" + workspaceName + ", connectionTimeout="
-				+ connectionTimeout + ", userId=" + userId + ", password=" + password + ", decryptedPassword=" + decryptedPassword + "]";
+				+ connectionTimeout + ", userId=" + userId + "]";
 	}
 
 }
