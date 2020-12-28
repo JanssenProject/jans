7c37dba5212af15bd667d10f4ea9ce0aa338e0fa
YuriyZ <yzabrovarniy@gmail.com>
2020-12-28 10:24:20 +0200

    (jans) Added Stat and StatEntry entities.
    https://github.com/GluuFederation/oxAuth/issues/1512

---
+++ common/src/main/java/io/jans/as/common/model/stat/StatEntry.java
@@ -0,0 +1,61 @@
+package io.jans.as.common.model.stat;
+
+
+import io.jans.orm.annotation.*;
+
+import java.io.Serializable;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ */
+@DataEntry
+@ObjectClass(value = "jansStatEntry")
+public class StatEntry implements Serializable {
+
+    @DN
+    private String dn;
+
+    @AttributeName(name = "jansId")
+    private String id;
+
+    @AttributeName(name = "dat")
+    private String userHllData;
+
+    @AttributeName(name = "attr")
+    @JsonObject
+    private Stat stat;
+
+    public String getDn() {
+        return dn;
+    }
+
+    public void setDn(String dn) {
+        this.dn = dn;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserHllData() {
+        return userHllData;
+    }
+
+    public void setUserHllData(String userHllData) {
+        this.userHllData = userHllData;
+    }
+
+    public Stat getStat() {
+        if (stat == null)
+            stat = new Stat();
+        return stat;
+    }
+
+    public void setStat(Stat stat) {
+        this.stat = stat;
+    }
+}
