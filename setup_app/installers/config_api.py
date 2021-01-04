aa06c46babd75f93ebe5515287bfcbd4a7f857af
Mustafa Baser <mbaser@mail.com>
2021-01-04 21:08:52 +0300

    add displayName to jans config api client

--- setup_app/installers/config_api.py
+++ setup_app/installers/config_api.py
@@ -146,6 +146,7 @@ class ConfigApiInstaller(SetupUtils, BaseInstaller):
             'inum={},ou=clients,o=jans'.format(Config.jca_client_id), {
             'objectClass': ['top', 'jansClnt'],
             'del': ['false'],
+            'displayName': 'Jans Config Api Client',
             'inum': [Config.jca_client_id],
             'jansAccessTknAsJwt': ['false'],
             'jansAccessTknSigAlg': ['RS256'],
