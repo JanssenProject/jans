2ef1b511383bc8f5c10958584cf9c6d982389070
yurem <Yuriy.Movchan@gmail.com>
2021-01-07 20:41:36 +0300

    SCIM APIs not protected by default #2

--- server/src/main/java/io/jans/scim/service/init/ResteasyInitializer.java
+++ server/src/main/java/io/jans/scim/service/init/ResteasyInitializer.java
@@ -6,9 +6,37 @@
 
 package io.jans.scim.service.init;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import io.jans.scim.service.filter.AuthorizationProcessingFilter;
+import io.jans.scim.service.scim2.interceptor.ServiceMetadataFilter;
+import io.jans.scim.ws.rs.scim2.*;
+
 @ApplicationPath("/restv1")
 public class ResteasyInitializer extends Application {
+	@Override
+	public Set<Class<?>> getClasses() {
+		HashSet<Class<?>> classes = new HashSet<Class<?>>();
+		classes.add(ScimConfigurationWS.class);
+		classes.add(SchemaWebService.class);
+		classes.add(UserWebService.class);
+		classes.add(GroupWebService.class);
+		classes.add(ResourceTypeWS.class);
+		classes.add(SearchResourcesWebService.class);
+		classes.add(ScimResourcesUpdatedWebService.class);
+		classes.add(ServiceProviderConfigWS.class);
+		classes.add(BulkWebService.class);
+		classes.add(FidoDeviceWebService.class);
+		classes.add(Fido2DeviceWebService.class);
+
+		// Providers
+		classes.add(AuthorizationProcessingFilter.class);
+		classes.add( ServiceMetadataFilter.class);		
+
+		return classes;
+	}
 }
