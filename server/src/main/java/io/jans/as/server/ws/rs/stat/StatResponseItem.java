71974406ad33531622d64365a7b2fb952558bff0
YuriyZ <yzabrovarniy@gmail.com>
2021-01-08 10:32:56 +0200

    (jans) Added stat response item.
    https://github.com/GluuFederation/oxAuth/issues/1512

---
+++ server/src/main/java/io/jans/as/server/ws/rs/stat/StatResponseItem.java
@@ -0,0 +1,44 @@
+package io.jans.as.server.ws.rs.stat;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ */
+public class StatResponseItem {
+
+    @JsonProperty(value = "monthly_active_users")
+    private long monthlyActiveUsers;
+
+    @JsonProperty("token_count_per_granttype")
+    private Map<String, Map<String, Long>> tokenCountPerGrantType;
+
+    public long getMonthlyActiveUsers() {
+        return monthlyActiveUsers;
+    }
+
+    public void setMonthlyActiveUsers(long monthlyActiveUsers) {
+        this.monthlyActiveUsers = monthlyActiveUsers;
+    }
+
+    public Map<String, Map<String, Long>> getTokenCountPerGrantType() {
+        if (tokenCountPerGrantType == null) tokenCountPerGrantType = new HashMap<>();
+        return tokenCountPerGrantType;
+    }
+
+    public void setTokenCountPerGrantType(Map<String, Map<String, Long>> tokenCountPerGrantType) {
+        this.tokenCountPerGrantType = tokenCountPerGrantType;
+    }
+
+    @Override
+    public String toString() {
+        return "StatResponseItem{" +
+                "monthlyActiveUsers=" + monthlyActiveUsers +
+                ", tokenCountPerGrantType=" + tokenCountPerGrantType +
+                '}';
+    }
+}
+
