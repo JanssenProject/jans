c7909ce3dc3f8f3c4024a3e817f8f7c639a81781
Mustafa Baser <mbaser@mail.com>
2021-01-15 21:19:14 +0300

    rdbm preliminary works

---
+++ static/rdbm/sql_tables.py
@@ -0,0 +1,56 @@
+import json
+
+ldap_sql_data_type_mapping = {
+    '1.3.6.1.4.1.1466.115.121.1.7': 'SMALLINT',
+    '1.3.6.1.4.1.1466.115.121.1.12': 'VARCHAR(128)',
+    '1.3.6.1.4.1.1466.115.121.1.15': 'TINYTEXT',
+    '1.3.6.1.4.1.1466.115.121.1.24': 'DATETIME',
+    '1.3.6.1.4.1.1466.115.121.1.27': 'INT',
+    '1.3.6.1.4.1.1466.115.121.1.11': 'VARCHAR(2)', #country code
+    '1.3.6.1.4.1.1466.115.121.1.26': 'TEXT', #IA5 String
+    '1.3.6.1.4.1.1466.115.121.1.40': 'BINARY', #octed string
+    '1.3.6.1.4.1.1466.115.121.1.8': 'BLOB',
+    '1.3.6.1.4.1.1466.115.121.1.50': 'VARCHAR(20)', #phone number
+    '1.3.6.1.4.1.1466.115.121.1.22': 'VARCHAR(20)', #Facsimile Telephone Number
+    '1.3.6.1.4.1.1466.115.121.1.14': 'VARCHAR(50)', #Delivery Method
+    '1.3.6.1.4.1.1466.115.121.1.41': 'VARCHAR(128)', #Postal Address,
+    'JSON': 'JSON'
+    }
+
+with open('opendj_attributes_syntax.json') as f:
+    opendj_attributes_syntax = json.load(f)
+
+def get_attr_syntax(attrname, attributes):
+    for jans_attr in attributes:
+        if attrname in jans_attr['names']:
+            if jans_attr.get('multivalued'):
+                return 'JSON'
+            return jans_attr['syntax']
+    else:
+        opendj_syntax = opendj_attributes_syntax.get(attrname)
+        if opendj_syntax is None:
+            opendj_syntax = '1.3.6.1.4.1.1466.115.121.1.15'
+
+        return opendj_syntax
+
+if __name__ == "__main__": 
+
+    w = open('jans_tables.sql', 'w')
+
+    for jans_schema_fn in ('../../schema/jans_schema.json', '../../schema/custom_schema.json'):
+
+        with open(jans_schema_fn) as f:
+            jans_schema = json.load(f)
+
+        for obj in jans_schema['objectClasses']:
+            sql_tbl_name = obj['names'][0]
+            sql_tbl_cols = []
+
+            for attrname in obj['may']:
+                attr_syntax = get_attr_syntax(attrname, jans_schema['attributeTypes'])
+
+                sql_tbl_cols.append('`{}` {}'.format(attrname, ldap_sql_data_type_mapping[attr_syntax]))
+                
+            sql_cmd = 'CREATE TABLE `{}` (`id` int NOT NULL auto_increment, `doc_id` VARCHAR(48) NOT NULL UNIQUE, `objectClass` VARCHAR(48), dn VARCHAR(128), {}, PRIMARY KEY  (`id`, `doc_id`))'.format(sql_tbl_name.lower(), ', '.join(sql_tbl_cols))
+            w.write(sql_cmd+';\n')
+    w.close()
