b6daab71d9c66daa7b21297a433c936197b22ebe
Mustafa Baser <mbaser@mail.com>
2021-01-18 23:09:53 +0300

    impelement up to 5 in issue #11

--- setup_app/installers/scim.py
+++ setup_app/installers/scim.py
@@ -37,9 +37,6 @@ class ScimInstaller(JettyInstaller):
         self.ldif_config = os.path.join(self.output_folder, 'configuration.ldif')
         self.ldif_clients = os.path.join(self.output_folder, 'clients.ldif')
 
-        self.scim_rs_client_jks_fn = os.path.join(Config.certFolder, 'scim-rs.jks')
-        self.scim_rp_client_jks_fn = os.path.join(Config.certFolder, 'scim-rp.jks')
-
         self.enable()
 
     def installed(self):
@@ -47,34 +44,7 @@ class ScimInstaller(JettyInstaller):
 
     def generate_configuration(self):
         self.logIt("Generating {} configuration".format(self.service_name))
-        client_var_id_list = (
-                    ('scim_rs_client_id', '1201.'),
-                    ('scim_rp_client_id', '1202.'),
-                    )
-        self.check_clients(client_var_id_list)
-        self.check_clients([('scim_resource_oxid', '1203.')], resource=True)
 
-        if not Config.get('scim_rs_client_jks_pass'):
-            Config.scim_rs_client_jks_pass = self.getPW()
-        
-        Config.scim_rs_client_jks_pass_encoded = self.obscure(Config.scim_rs_client_jks_pass)
-
-        if not Config.get('scim_rp_client_jks_pass'):
-            Config.scim_rp_client_jks_pass = 'secret'
-
-        Config.scimTestMode = Config.get('scimTestMode', 'false')
-        Config.enable_scim_access_policy = 'true' if Config.installPassport else 'false'
-
-        #backup current jks files if exists
-        for jks_fn in (self.scim_rs_client_jks_fn, self.scim_rp_client_jks_fn):
-            if os.path.exists(jks_fn):
-                self.backupFile(jks_fn, move=True)
-
-        Config.scim_rs_client_jwks = self.gen_openid_jwks_jks_keys(self.scim_rs_client_jks_fn, Config.scim_rs_client_jks_pass)
-        Config.templateRenderingDict['scim_rs_client_base64_jwks'] = self.generate_base64_string(Config.scim_rs_client_jwks, 1)
-
-        Config.scim_rp_client_jwks = self.gen_openid_jwks_jks_keys(self.scim_rp_client_jks_fn, Config.scim_rp_client_jks_pass)
-        Config.templateRenderingDict['scim_rp_client_base64_jwks'] = self.generate_base64_string(Config.scim_rp_client_jwks, 1)
 
     def render_import_templates(self):
         
@@ -84,13 +54,10 @@ class ScimInstaller(JettyInstaller):
         Config.templateRenderingDict['scim_static_conf_base64'] = self.generate_base64_ldap_file(self.static_config_fn)
 
         self.renderTemplateInOut(self.ldif_config, self.templates_folder, self.output_folder)
-        self.renderTemplateInOut(self.ldif_clients, self.templates_folder, self.output_folder)
 
-        self.dbUtils.import_ldif([self.ldif_config, self.ldif_clients])
+        self.dbUtils.import_ldif([self.ldif_config])
 
         self.write_webapps_xml()
 
     def update_backend(self):
-        self.dbUtils.add_client2script('2DAF-F9A5', Config.scim_rp_client_id)
-        self.dbUtils.add_client2script('2DAF-F995', Config.scim_rp_client_id)
         self.dbUtils.enable_service('jansScimEnabled')
