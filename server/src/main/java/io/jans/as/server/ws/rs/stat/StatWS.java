3cf7f6f11ba5763e8fdecf0522fb2ff4e38b6299
YuriyZ <yzabrovarniy@gmail.com>
2021-01-25 11:17:10 +0200

    (jans) Added month validation and run validation methods to StatWS.
    https://github.com/GluuFederation/oxAuth/issues/1512

--- server/src/main/java/io/jans/as/server/ws/rs/stat/StatWS.java
+++ server/src/main/java/io/jans/as/server/ws/rs/stat/StatWS.java
@@ -2,9 +2,12 @@ package io.jans.as.server.ws.rs.stat;
 
 import io.jans.as.model.configuration.AppConfiguration;
 import io.jans.as.model.error.ErrorResponseFactory;
+import io.jans.as.model.token.TokenErrorResponseType;
 import io.jans.as.server.security.Identity;
 import io.jans.as.server.service.stat.StatService;
+import io.jans.as.server.util.ServerUtil;
 import io.jans.orm.PersistenceEntryManager;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -12,6 +15,8 @@ import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Yuriy Zabrovarnyy
@@ -56,5 +61,40 @@ public class StatWS {
 
     public Response stat(String month) {
         return null;
+    }
+
+    private List<String> validateMonth(String month) {
+        if (StringUtils.isBlank(month)) {
+            throw errorResponseFactory.createWebApplicationException(Response.Status.BAD_REQUEST, TokenErrorResponseType.INVALID_REQUEST, "`month` parameter can't be blank and should be in format yyyyMM (e.g. 202012)");
+        }
+
+        month = ServerUtil.urlDecode(month);
+
+        List<String> months = new ArrayList<>();
+        for (String m : month.split(" ")) {
+            m = m.trim();
+            if (m.length() == 6) {
+                months.add(m);
+            }
+        }
+
+        if (months.isEmpty()) {
+            throw errorResponseFactory.createWebApplicationException(Response.Status.BAD_REQUEST, TokenErrorResponseType.INVALID_REQUEST, "`month` parameter can't be blank and should be in format yyyyMM (e.g. 202012)");
+        }
+
+        return months;
+    }
+
+    private boolean allowToRun() {
+        int interval = appConfiguration.getStatWebServiceIntervalLimitInSeconds();
+        if (interval <= 0) {
+            interval = DEFAULT_WS_INTERVAL_LIMIT_IN_SECONDS;
+        }
+
+        long timerInterval = interval * 1000;
+
+        long timeDiff = System.currentTimeMillis() - lastProcessedAt;
+
+        return timeDiff >= timerInterval;
     }
 }
