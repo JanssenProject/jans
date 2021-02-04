2b1d46bf3a79f0569f9770897b9067578c2647ec
YuriyZ <yzabrovarniy@gmail.com>
2021-02-04 21:17:00 +0200

    (jans) added basic and post client authentication for stat
    https://github.com/GluuFederation/oxAuth/issues/1512

--- client/src/test/java/io/jans/as/client/ws/rs/StatWSTest.java
+++ client/src/test/java/io/jans/as/client/ws/rs/StatWSTest.java
@@ -1,6 +1,7 @@
 package io.jans.as.client.ws.rs;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import io.jans.as.client.BaseRequest;
 import io.jans.as.client.BaseTest;
 import io.jans.as.client.service.ClientFactory;
 import io.jans.as.client.service.StatService;
@@ -21,8 +22,24 @@ public class StatWSTest extends BaseTest {
     public void stat(final String umaPatClientId, final String umaPatClientSecret) throws Exception {
         final Token authorization = UmaClient.requestPat(tokenEndpoint, umaPatClientId, umaPatClientSecret);
 
-        final StatService service = ClientFactory.instance().createStatService(issuer + "/jans-auth/restv1/internal/stat");
+        final StatService service = ClientFactory.instance().createStatService(issuer + "/oxauth/restv1/internal/stat");
         final JsonNode node = service.stat("Bearer " + authorization.getAccessToken(), "202101");
-        assertTrue(node != null && node.isArray());
+        assertTrue(node != null && node.hasNonNull("response"));
+    }
+
+    @Test
+    @Parameters({"umaPatClientId", "umaPatClientSecret"})
+    public void statBasic(final String umaPatClientId, final String umaPatClientSecret) throws Exception {
+        final StatService service = ClientFactory.instance().createStatService(issuer + "/oxauth/restv1/internal/stat");
+        final JsonNode node = service.stat("Basic " + BaseRequest.getEncodedCredentials(umaPatClientId, umaPatClientSecret), "202101");
+        assertTrue(node != null && node.hasNonNull("response"));
+    }
+
+    @Test
+    @Parameters({"umaPatClientId", "umaPatClientSecret"})
+    public void statPost(final String umaPatClientId, final String umaPatClientSecret) throws Exception {
+        final StatService service = ClientFactory.instance().createStatService(issuer + "/oxauth/restv1/internal/stat");
+        final JsonNode node = service.stat(null, "202101", umaPatClientId, umaPatClientSecret);
+        assertTrue(node != null && node.hasNonNull("response"));
     }
 }
