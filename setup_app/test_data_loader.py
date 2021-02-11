4413a47ee6136b37e15bfbbc2d88f36f0de4e7eb
Mustafa Baser <mbaser@mail.com>
2021-02-11 23:40:13 +0300

    fix loading/searching entries

--- setup_app/test_data_loader.py
+++ setup_app/test_data_loader.py
@@ -286,7 +286,7 @@ class TestDataLoader(BaseInstaller, SetupUtils):
 
         self.dbUtils.ldap_conn.bind()
 
-        result = self.dbUtils.search('ou=configuration,o=jans', search_filter='(jansDbAuth=*)', search_scope=ldap3.BASE)
+        result = self.dbUtils.search('ou=configuration,o=jans', search_filter='(&(jansDbAuth=*)(objectClass=jansAppConf))', search_scope=ldap3.BASE)
 
         oxIDPAuthentication = json.loads(result['jansDbAuth'])
         oxIDPAuthentication['config']['servers'] = ['{0}:{1}'.format(Config.hostname, Config.ldaps_port)]
