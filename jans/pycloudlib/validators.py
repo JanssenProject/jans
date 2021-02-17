ecfcdac1835a8a04383554935f248e3a4fb61156
iromli <isman.firmansyah@gmail.com>
2021-02-18 01:09:40 +0700

    feat(persistence-type): add sql as valid type

--- jans/pycloudlib/validators.py
+++ jans/pycloudlib/validators.py
@@ -11,15 +11,11 @@ from jans.pycloudlib.constants import PERSISTENCE_TYPES
 from jans.pycloudlib.constants import PERSISTENCE_LDAP_MAPPINGS
 
 
-class ValidationError(Exception):
-    pass
-
-
 def validate_persistence_type(type_: str) -> NoReturn:
     if type_ not in PERSISTENCE_TYPES:
         types = ", ".join(PERSISTENCE_TYPES)
 
-        raise ValidationError(
+        raise ValueError(
             f"Unsupported persistence type {type_}; "
             f"please choose one of {types}"
         )
@@ -29,7 +25,7 @@ def validate_persistence_ldap_mapping(type_: str, ldap_mapping: str) -> NoReturn
     if type_ == "hybrid" and ldap_mapping not in PERSISTENCE_LDAP_MAPPINGS:
         mappings = ", ".join(PERSISTENCE_LDAP_MAPPINGS)
 
-        raise ValidationError(
+        raise ValueError(
             f"Unsupported persistence ldap mapping {ldap_mapping}; "
             f"please choose one of {mappings}"
         )
