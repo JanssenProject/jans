1cb54676da5034bd906185701b5eba7fbc977241
yurem <Yuriy.Movchan@gmail.com>
2021-02-17 19:41:49 +0300

    New Interception Script for id_token Jans-Auth #23

---
+++ script/src/main/java/io/jans/model/custom/script/type/token/DummyUpdateTokenType.java
@@ -0,0 +1,37 @@
+package io.jans.model.custom.script.type.token;
+
+import java.util.Map;
+import io.jans.model.SimpleCustomProperty;
+import io.jans.model.custom.script.model.CustomScript;
+
+/**
+ * @author Yuriy Movchan
+ */
+public class DummyUpdateTokenType implements UpdateTokenType {
+
+	@Override
+	public boolean init(Map<String, SimpleCustomProperty> configurationAttributes) {
+		return true;
+	}
+
+	@Override
+	public boolean init(CustomScript customScript, Map<String, SimpleCustomProperty> configurationAttributes) {
+		return true;
+	}
+
+	@Override
+	public boolean destroy(Map<String, SimpleCustomProperty> configurationAttributes) {
+		return true;
+	}
+
+	@Override
+	public int getApiVersion() {
+		return 1;
+	}
+
+	@Override
+	public boolean modifyIdToken(Object jwr, Object tokenContext) {
+		return false;
+	}
+
+}
