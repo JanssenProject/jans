2b10ba02a179426389c0ec98e087619b045161e5
yurem <Yuriy.Movchan@gmail.com>
2021-02-17 20:08:11 +0300

    New Interception Script for id_token #23

---
+++ server/src/main/java/io/jans/as/server/service/external/ExternalUpdateTokenService.java
@@ -0,0 +1,76 @@
+package io.jans.as.server.service.external;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
+import com.google.common.base.Function;
+
+import io.jans.as.model.token.JsonWebResponse;
+import io.jans.as.server.service.external.context.ExternalUpdateTokenContext;
+import io.jans.model.custom.script.CustomScriptType;
+import io.jans.model.custom.script.conf.CustomScriptConfiguration;
+import io.jans.model.custom.script.type.token.UpdateTokenType;
+import io.jans.service.custom.script.ExternalScriptService;
+
+/**
+ * @author Yuriy Movchan
+ */
+@ApplicationScoped
+public class ExternalUpdateTokenService extends ExternalScriptService {
+
+	private static final long serialVersionUID = -1033475075863270249L;
+
+	@Inject
+    private Logger log;
+
+    public ExternalUpdateTokenService() {
+        super(CustomScriptType.UPDATE_TOKEN);
+    }
+
+    public boolean modifyIdTokenMethod(CustomScriptConfiguration script, JsonWebResponse jsonWebResponse, ExternalUpdateTokenContext context) {
+        try {
+            log.trace("Executing python 'updateToken' method, script name: {}, jsonWebResponse: {}, context: {}", script.getName(), jsonWebResponse, context);
+            context.setScript(script);
+
+            UpdateTokenType updateTokenType = (UpdateTokenType) script.getExternalType();
+            final boolean result = updateTokenType.modifyIdToken(jsonWebResponse, context);
+            log.trace("Finished 'updateToken' method, script name: {}, jsonWebResponse: {}, context: {}, result: {}", script.getName(), jsonWebResponse, context, result);
+
+            return result;
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex);
+            saveScriptError(script.getCustomScript(), ex);
+        }
+
+        return false;
+    }
+
+    public boolean modifyIdTokenMethods(JsonWebResponse jsonWebResponse, ExternalUpdateTokenContext context) {
+        if (this.customScriptConfigurations.isEmpty()) {
+            return false;
+        }
+        log.trace("Executing {} update-token scripts.", this.customScriptConfigurations.size());
+
+        for (CustomScriptConfiguration script : this.customScriptConfigurations) {
+            if (!modifyIdTokenMethod(script, jsonWebResponse, context)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+	public Function<JsonWebResponse, Void> buildModifyIdTokenProcessor(final ExternalUpdateTokenContext context) {
+		return new Function<JsonWebResponse, Void>() {
+			@Override
+			public Void apply(JsonWebResponse jsonWebResponse) {
+				modifyIdTokenMethods(jsonWebResponse, context);
+
+				return null;
+			}
+		};
+	}
+
+}
