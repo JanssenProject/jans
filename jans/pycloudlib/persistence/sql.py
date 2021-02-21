727a18273f3818e5d3d3f169196db9531b5c095b
iromli <isman.firmansyah@gmail.com>
2021-02-22 05:19:06 +0700

    feat(sql): add helper to render jans-sql.properties

--- jans/pycloudlib/persistence/sql.py
+++ jans/pycloudlib/persistence/sql.py
@@ -3,6 +3,8 @@ import os
 
 from sqlalchemy import create_engine
 
+from jans.pycloudlib.utils import encode_text
+
 
 def get_sql_password() -> str:
     """Get password used for SQL database user.
@@ -45,3 +47,30 @@ class SQLClient:
             conn.execute("SELECT 1 AS is_alive")
             return True
         return False
+
+
+def render_sql_properties(manager, src: str, dest: str) -> None:
+    """Render file contains properties to connect to SQL database server.
+
+    :params manager: An instance of :class:`~jans.pycloudlib.manager._Manager`.
+    :params src: Absolute path to the template.
+    :params dest: Absolute path where generated file is located.
+    """
+
+    with open(src) as f:
+        txt = f.read()
+
+    with open(dest, "w") as f:
+        rendered_txt = txt % {
+            "rdbm_db": os.environ.get("CN_SQL_DB_NAME", "jans"),
+            "rdbm_type": os.environ.get("CN_SQL_DB_DIALECT", "mysql"),
+            "rdbm_host": os.environ.get("CN_SQL_DB_HOST", "localhost"),
+            "rdbm_port": os.environ.get("CN_SQL_DB_PORT", 3306),
+            "rdbm_user": os.environ.get("CN_SQL_DB_USER", "jans"),
+            "rdbm_password_enc": encode_text(
+                get_sql_password(),
+                manager.secret.get("encoded_salt"),
+            ).decode(),
+            "server_time_zone": os.environ.get("CN_SQL_DB_TIMEZONE", "UTC"),
+        }
+        f.write(rendered_txt)
