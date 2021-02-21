63a32b8e3da49e54acdba5b6a5b266ca20a84a44
iromli <isman.firmansyah@gmail.com>
2021-02-22 04:13:50 +0700

    feat(sql): conform to latest SQL schema

--- scripts/sql_setup.py
+++ scripts/sql_setup.py
@@ -1,13 +1,23 @@
+import contextlib
 import json
 import logging.config
 import os
 import re
+from collections import OrderedDict
+from collections import defaultdict
 
+from ldap3.utils import dn as dnutils
+from ldif3 import LDIFParser
 from sqlalchemy.exc import OperationalError
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.sql import text
 
 from jans.pycloudlib.persistence.sql import SQLClient
+from jans.pycloudlib.utils import as_boolean
 
 from settings import LOGGING_CONFIG
+from utils import prepare_template_ctx
+from utils import render_ldif
 
 FIELD_RE = re.compile(r"[^0-9a-zA-Z\s]+")
 
@@ -21,6 +31,10 @@ class SQLBackend:
 
         self.db_dialect = os.environ.get("CN_SQL_DB_DIALECT", "mysql")
         self.db_name = os.environ.get("CN_SQL_DB_NAME", "jans")
+        self.schema_files = [
+            "/app/static/jans_schema.json",
+            "/app/static/custom_schema.json",
+        ]
 
         self.client = SQLClient()
 
@@ -28,8 +42,7 @@ class SQLBackend:
             self.sql_data_types = json.loads(f.read())
 
         self.attr_types = []
-        for file_ in ("jans_schema.json", "custom_schema.json"):
-            fn = f"/app/static/{file_}"
+        for fn in self.schema_files:
             with open(fn) as f:
                 schema = json.loads(f.read())
             self.attr_types += schema["attributeTypes"]
@@ -42,6 +55,9 @@ class SQLBackend:
 
         with open("/app/static/sql/sql_index.json") as f:
             self.sql_indexes = json.loads(f.read())
+
+        # cached schemas that holds JSON columns
+        self.json_columns = defaultdict(list)
 
     def get_attr_syntax(self, attr):
         for attr_type in self.attr_types:
@@ -79,36 +95,61 @@ class SQLBackend:
         return data_type
 
     def create_tables(self, conn):
-        for file_ in ("jans_schema.json", "custom_schema.json"):
-            fn = f"/app/static/{file_}"
+        logger.info("Creating tables (if not exist)")
 
+        schemas = {}
+
+        for fn in self.schema_files:
             with open(fn) as f:
                 schema = json.loads(f.read())
 
             for oc in schema["objectClasses"]:
-                table = oc["names"][0]
-                table_cols = []
+                schemas[oc["names"][0]] = oc
 
-                for attr in oc["may"]:
-                    data_type = self.get_data_type(attr)
-                    col_def = f"`{attr}` {data_type}"
+        for table, oc in schemas.items():
+            if oc.get("sql", {}).get("ignore"):
+                continue
 
-                    if data_type.lower() == "json":
-                        col_def = f"""{col_def} NOT NULL DEFAULT ('{{"v": []}}')"""
-                    table_cols.append(col_def)
+            table_cols = []
 
-                sql_cmd = f"CREATE TABLE `{table}` (`id` INT NOT NULL auto_increment, `doc_id` VARCHAR(48) NOT NULL UNIQUE, `objectClass` VARCHAR(48), `dn` VARCHAR(128), {', '.join(table_cols)}, PRIMARY KEY (`id`, `doc_id`));"
+            if "sql" in oc:
+                oc["may"] += oc["sql"].get("include", [])
 
-                try:
-                    conn.execute(sql_cmd)
-                except OperationalError as exc:
-                    # the following code should be ignored
-                    # - 1050: table exists
-                    if exc.orig.args[0] in (1050,):
-                        continue
-                    logger.warning(f"Failed to execute query; reason={exc.orig.args}")
+                for inc_oc in oc["sql"].get("includeObjectClass", []):
+                    oc["may"] += schemas[inc_oc]["may"]
+
+            for attr in set(oc["may"]):
+                data_type = self.get_data_type(attr)
+                col_def = f"{attr} {data_type}"
+                table_cols.append(col_def)
+
+                if data_type.lower() == "json":
+                    self.json_columns[table].append(attr)
+
+            sql_cmd = (
+                f"CREATE TABLE {table} ("
+                "id INT NOT NULL auto_increment, "
+                "doc_id VARCHAR(48) NOT NULL UNIQUE, "
+                "objectClass VARCHAR(48), "
+                "dn VARCHAR(128), "
+                f"{', '.join(table_cols)}, "
+                "PRIMARY KEY (id, doc_id)"
+                ");"
+            )
+
+            try:
+                conn.execute(sql_cmd)
+                # self.tables.append(table)
+            except OperationalError as exc:
+                # the following code should be ignored
+                # - 1050: table exists
+                if exc.orig.args[0] in (1050,):
+                    continue
+                logger.warning(f"Failed to execute query; reason={exc.orig.args}")
 
     def create_indexes(self, conn):
+        logger.info("Creating table indexes (if not exist)")
+
         for table in self.sql_indexes[self.db_dialect]:
             for field in self.sql_indexes[self.db_dialect][table]["fields"]:
                 field_sub = FIELD_RE.sub("_", field)
@@ -135,7 +176,166 @@ class SQLBackend:
                         continue
                     logger.warning(f"Failed to execute query; reason={exc.orig.args}")
 
+    def import_ldif(self, conn):
+        ldif_mappings = {
+            "default": [
+                "base.ldif",
+                "attributes.ldif",
+                "scopes.ldif",
+                "scripts.ldif",
+                "configuration.ldif",
+                "jans-auth/configuration.ldif",
+                "jans-auth/clients.ldif",
+                "jans-fido2/configuration.ldif",
+                "jans-scim/configuration.ldif",
+                "jans-scim/scopes.ldif",
+                "jans-scim/clients.ldif",
+                "jans-config-api/scopes.ldif",
+                "jans-config-api/clients.ldif",
+                # "oxidp.ldif",
+                # "passport.ldif",
+                # "oxpassport-config.ldif",
+                # "gluu_radius_base.ldif",
+                # "gluu_radius_server.ldif",
+                # "clients.ldif",
+                "o_metric.ldif",
+                # "gluu_radius_clients.ldif",
+                # "passport_clients.ldif",
+                # "casa.ldif",
+                # "scripts_casa.ldif",
+            ],
+            "user": [
+                "people.ldif",
+                "groups.ldif",
+            ],
+            "site": [
+                "o_site.ldif",
+            ],
+            "cache": [],
+            "token": [],
+            "session": [],
+        }
+
+        ctx = prepare_template_ctx(self.manager)
+
+        for _, files in ldif_mappings.items():
+            # self.check_indexes(mapping)
+
+            for file_ in files:
+                logger.info(f"Importing {file_} file")
+                src = f"/app/templates/{file_}"
+                dst = f"/app/tmp/{file_}"
+                os.makedirs(os.path.dirname(dst), exist_ok=True)
+
+                render_ldif(src, dst, ctx)
+
+                # query_file = f"{dst}.sql"
+                # with open(query_file, "a+"):
+                for sql_cmd, data in self.ldif_to_sql(dst):
+                    try:
+                        conn.execute(text(sql_cmd), **data)
+                    except IntegrityError as exc:
+                        # the following code should be ignored
+                        # - 1061: duplicate key name
+                        if exc.orig.args[0] in (1062,):
+                            continue
+                        logger.warning(f"Failed to execute query; reason={exc.orig.args}")
+
     def initialize(self):
+        def is_initialized():
+            with self.client.engine.connect() as conn:
+                result = conn.execute(
+                    text("SELECT COUNT(id) FROM jansClnt WHERE doc_id = :doc_id"),
+                    **{"doc_id": self.manager.config.get("jca_client_id")}
+                )
+                return as_boolean(result.fetchone()[0])
+
+        should_skip = as_boolean(
+            os.environ.get("CN_PERSISTENCE_SKIP_EXISTING", True),
+        )
+
+        if should_skip and is_initialized():
+            logger.info("SQL backend already initialized")
+            return
+
         with self.client.engine.connect() as conn:
             self.create_tables(conn)
             self.create_indexes(conn)
+            self.import_ldif(conn)
+
+    def transform_value(self, key, values):
+        type_ = self.sql_data_types.get(key)
+
+        if not type_:
+            attr_syntax = self.get_attr_syntax(key)
+            type_ = self.sql_data_types_mapping[attr_syntax]
+
+        data_type = type_[self.db_dialect]["type"]
+
+        if data_type in ("SMALLINT",):
+            if values[0].lower() in ("1", "on", "true", "yes", "ok"):
+                return 1
+            return 0
+
+        if data_type == "INT":
+            return int(values[0])
+
+        if data_type in ("DATETIME(3)",):
+            dval = values[0].strip("Z")
+            return "{}-{}-{} {}:{}:{}{}".format(dval[0:4], dval[4:6], dval[6:8], dval[8:10], dval[10:12], dval[12:14], dval[14:17])
+
+        if data_type == "JSON":
+            return json.dumps({"v": values})
+
+        # fallback
+        return values[0]
+
+    def ldif_to_sql(self, filename):
+        with contextlib.ExitStack() as stack:
+            # will be automatically closed when exiting ``with`` block
+            fd = stack.enter_context(open(filename, "rb"))
+
+            parser = LDIFParser(fd)
+
+            for dn, entry in parser.parse():
+                parsed_dn = dnutils.parse_dn(dn)
+                # rdn_name = parsed_dn[0][0]
+                doc_id = parsed_dn[0][1]
+
+                oc = entry.get("objectClass") or entry.get("objectclass")
+                if oc:
+                    if "top" in oc:
+                        oc.remove("top")
+
+                    if len(oc) == 1 and oc[0].lower() in ("organizationalunit", "organization"):
+                        continue
+
+                table = oc[-1]
+
+                # entry.pop(rdn_name)
+
+                if "objectClass" in entry:
+                    entry.pop("objectClass")
+                elif "objectclass" in entry:
+                    entry.pop("objectclass")
+
+                attr_mapping = OrderedDict({
+                    "doc_id": doc_id,
+                    "objectClass": table,
+                    "dn": dn,
+                })
+
+                for attr in entry:
+                    value = self.transform_value(attr, entry[attr])
+                    attr_mapping[attr] = value
+
+                # populate existing JSON columns with default if value
+                # is not defined from ldif
+                for json_col in self.json_columns.get(table, []):
+                    if json_col not in attr_mapping:
+                        attr_mapping[json_col] = json.dumps({"v": []})
+
+                columns = ", ".join(attr_mapping.keys())
+                params = ", ".join(f":{column}" for column in attr_mapping.keys())
+                sql_cmd = f"INSERT INTO {table} ({columns}) VALUES ({params});"
+                yield sql_cmd, attr_mapping
