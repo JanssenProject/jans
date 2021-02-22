7f862b4a9d8b9b5c42be30c8092a756f1f170976
yurem <Yuriy.Movchan@gmail.com>
2021-02-22 17:39:10 +0300

    Fix UMA resources search

--- server/src/main/java/io/jans/as/server/uma/service/UmaResourceService.java
+++ server/src/main/java/io/jans/as/server/uma/service/UmaResourceService.java
@@ -133,7 +133,7 @@ public class UmaResourceService {
             prepareBranch();
 
             if (StringUtils.isNotBlank(associatedClientDn)) {
-                final Filter filter = Filter.createEqualityFilter("jansAssociatedClnt", associatedClientDn);
+                final Filter filter = Filter.createEqualityFilter("jansAssociatedClnt", associatedClientDn).multiValued();
                 return ldapEntryManager.findEntries(getBaseDnForResource(), UmaResource.class, filter);
             }
         } catch (Exception e) {
