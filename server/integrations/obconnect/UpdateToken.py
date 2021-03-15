b402b2e83e4d1a01240051c2bab951a794b11b6f
Madhumita <madhu@gluu.org>
2021-03-15 15:32:28 +0530

    Integration with jans-auth-server

---
+++ server/integrations/obconnect/UpdateToken.py
@@ -0,0 +1,62 @@
+from org.gluu.service.cdi.util import CdiUtil
+from org.gluu.model.custom.script.type.token import UpdateTokenType
+from org.gluu.oxauth.service import SessionIdService
+import java
+import sys
+import os
+
+class UpdateToken(UpdateTokenType):
+    def __init__(self, currentTimeMillis):
+        self.currentTimeMillis = currentTimeMillis
+
+    def init(self, customScript, configurationAttributes):
+        print "Update token obconnect script. Initializing ..."
+        print "Update token obconnect  script. Initialized successfully"
+
+        return True
+
+    def destroy(self, configurationAttributes):
+        print "Update token obconnect  script. Destroying ..."
+        print "Update token  obconnect  script. Destroyed successfully"
+        return True
+
+    def getApiVersion(self):
+        return 11
+
+    # Returns boolean, true - indicates that script applied changes
+    # This method is called after adding headers and claims. Hence script can override them
+    # Note :
+    # jsonWebResponse - is JwtHeader, you can use any method to manipulate JWT
+    # context is reference of org.gluu.oxauth.service.external.context.ExternalUpdateTokenContext (in https://github.com/GluuFederation/oxauth project, )
+    def modifyIdToken(self, jsonWebResponse, context):
+                print "Update token obconnect script. Modify idToken: %s" % jsonWebResponse
+		try :
+			sessionIdService = CdiUtil.bean(SessionIdService)
+			print "session id from context - %s" % context.getGrant().getSessionDn().strip("oxId=")
+
+			sessionId = sessionIdService.getSessionByDn(context.getGrant().getSessionDn()) # fetch from persistence
+			if sessionId is None:
+                print "Session Id is none"
+            else:
+                print "session id -%s " % sessionId.getId()
+
+                        
+			print "session id -%s " % sessionId.getSessionAttributes()
+			openbanking_intent_id = sessionId.getSessionAttributes().get("openbanking_intent_id")
+			acr = sessionId.getSessionAttributes().get("acr_ob")
+
+			#jsonWebResponse.getHeader().setClaim("custom_header_name", "custom_header_value")
+			
+			#custom claims
+			jsonWebResponse.getClaims().setClaim("openbanking_intent_id", openbanking_intent_id)
+			jsonWebResponse.getClaims().setClaim("acr", acr)
+			
+			#regular claims        
+			jsonWebResponse.getClaims().setClaim("sub", openbanking_intent_id)
+
+			print "Update token script. After modify idToken: %s" % jsonWebResponse
+		
+			return True
+		except:
+	        print "update token failure" , sys.exc_info()[1]
+	        return None
