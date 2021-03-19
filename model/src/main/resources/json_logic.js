7264f7df674ee3eb966974d9cbfb1ee65cd8c786
YuriyZ <yzabrovarniy@gmail.com>
2021-03-19 18:15:27 +0200

    refactor : correcting issues based on static analyzer.
    Issue: Use proper assert methods and follow js conventions during variable naming.

--- model/src/main/resources/json_logic.js
+++ model/src/main/resources/json_logic.js
@@ -126,13 +126,13 @@
             if(typeof a === "undefined" || a==="" || a===null) {
                 return data;
             }
-            var sub_props = String(a).split(".");
-            for(var i = 0; i < sub_props.length; i++) {
+            var subProps = String(a).split(".");
+            for(var i = 0; i < subProps.length; i++) {
                 if(data === null) {
                     return not_found;
                 }
                 // Descending into data
-                data = data[sub_props[i]];
+                data = data[subProps[i]];
                 if(data === undefined) {
                     return not_found;
                 }
