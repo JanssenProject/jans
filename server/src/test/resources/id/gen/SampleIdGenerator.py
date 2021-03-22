3b136826960502ae63b7fdba3f62f75252dd4182
Mustafa Baser <mbaser@mail.com>
2021-03-22 18:58:25 +0300

    code qualtiy work

--- server/src/test/resources/id/gen/SampleIdGenerator.py
+++ server/src/test/resources/id/gen/SampleIdGenerator.py
@@ -1,22 +1,29 @@
+from __future__ import print_function
+
 from io.jans.model.custom.script.type.id import IdGeneratorType
 from io.jans.util import StringHelper
 from java.util import UUID
 
 class IdGenerator(IdGeneratorType):
+
     def __init__(self, currentTimeMillis):
+        """
+        Args:
+            currentTimeMillis (int): current time in miliseconds
+        """
         self.currentTimeMillis = currentTimeMillis
 
     @classmethod
     def init(cls, configurationAttributes):
-        print "Id generator. Initialization"
-        print "Id generator. Initialized successfully"
+        print("Id generator. Initialization")
+        print("Id generator. Initialized successfully")
 
         return True
 
     @classmethod
     def destroy(cls, configurationAttributes):
-        print "Id generator. Destroy"
-        print "Id generator. Destroyed successfully"
+        print("Id generator. Destroy")
+        print("Id generator. Destroyed successfully")
         return True
 
     @classmethod
@@ -31,12 +38,12 @@ class IdGenerator(IdGeneratorType):
     #   configurationAttributes is java.util.Map<String, SimpleCustomProperty>
     @classmethod
     def generateId(cls, appId, idType, idPrefix, configurationAttributes):
-        print "Id generator. Generate Id"
-        print "Id generator. Generate Id. AppId: '", appId, "', IdType: '", idType, "', IdPrefix: '", idPrefix, "'"
+        print("Id generator. Generate Id")
+        print("Id generator. Generate Id. AppId: '", appId, "', IdType: '", idType, "', IdPrefix: '", idPrefix, "'")
 
         if StringHelper.equalsIgnoreCase(idType, "test"):
             newId = UUID.randomUUID().toString()
-            print "Id generator. New test id: " + newId
+            print("Id generator. New test id: " + newId)
             return newId
 
         return "invalid"
