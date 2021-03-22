d1fb25dd68bc50936e384d08b64ade16d81ed8b6
Mustafa Baser <mbaser@mail.com>
2021-03-22 14:15:07 +0300

    code qualtiy: SampleIdGenerator.py

--- server/src/test/resources/id/gen/SampleIdGenerator.py
+++ server/src/test/resources/id/gen/SampleIdGenerator.py
@@ -1,26 +1,27 @@
 from io.jans.model.custom.script.type.id import IdGeneratorType
-from io.jans.util import StringHelper, ArrayHelper
-from java.util import Arrays, ArrayList
+from io.jans.util import StringHelper
+from java.util import ArrayList
 from java.util import UUID
-
-import java
 
 class IdGenerator(IdGeneratorType):
     def __init__(self, currentTimeMillis):
         self.currentTimeMillis = currentTimeMillis
 
-    def init(self, configurationAttributes):
+    @classmethod
+    def init(cls, configurationAttributes):
         print "Id generator. Initialization"
         print "Id generator. Initialized successfully"
 
         return True   
 
-    def destroy(self, configurationAttributes):
+    @classmethod
+    def destroy(cls, configurationAttributes):
         print "Id generator. Destroy"
         print "Id generator. Destroyed successfully"
         return True   
 
-    def getApiVersion(self):
+    @classmethod
+    def getApiVersion(cls):
         return 1
 
     # Id generator init method
@@ -29,13 +30,14 @@ class IdGenerator(IdGeneratorType):
     #   idPrefix is Id Prefix
     #   user is org.gluu.oxtrust.model.GluuCustomPerson
     #   configurationAttributes is java.util.Map<String, SimpleCustomProperty>
-    def generateId(self, appId, idType, idPrefix, configurationAttributes):
+    @classmethod
+    def generateId(cls, appId, idType, idPrefix, configurationAttributes):
         print "Id generator. Generate Id"
         print "Id generator. Generate Id. AppId: '", appId, "', IdType: '", idType, "', IdPrefix: '", idPrefix, "'"
 
         if StringHelper.equalsIgnoreCase(idType, "test"):
-            id = UUID.randomUUID().toString()
-            print "Id generator. New test id: " + id
-            return id
+            newId = UUID.randomUUID().toString()
+            print "Id generator. New test id: " + newId
+            return newId
 
         return "invalid"
