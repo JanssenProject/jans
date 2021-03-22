1717f08a46b498ddb9932164f6e1d36085002d66
Mustafa Baser <mbaser@mail.com>
2021-03-22 13:22:53 +0300

    code qualtiy: postauthn.py

--- server/integrations/postauthn/postauthn.py
+++ server/integrations/postauthn/postauthn.py
@@ -6,31 +6,34 @@
 #
 
 from io.jans.model.custom.script.type.postauthn import PostAuthnType
-from java.lang import String
 
 class PostAuthn(PostAuthnType):
     def __init__(self, currentTimeMillis):
         self.currentTimeMillis = currentTimeMillis
 
-    def init(self, customScript, configurationAttributes):
+    @classmethod
+    def init(cls, customScript, configurationAttributes):
         print "Post Authn script. Initializing ..."
         print "Post Authn script. Initialized successfully"
 
         return True
 
-    def destroy(self, configurationAttributes):
+    @classmethod
+    def destroy(cls, configurationAttributes):
         print "Post Authn script. Destroying ..."
         print "Post Authn script. Destroyed successfully"
         return True
 
-    def getApiVersion(self):
+    @classmethod
+    def getApiVersion(cls):
         return 11
 
     # This method is called during Authorization Request at Authorization Endpoint.
     # If True is returned, session is set as unauthenticated and user is send for authentication.
     # Note :
     # context is reference of io.jans.as.server.service.external.context.ExternalPostAuthnContext(in https://github.com/GluuFederation/oxauth project, )
-    def forceReAuthentication(self, context):
+    @classmethod
+    def forceReAuthentication(cls, context):
         return False
 
     # This method is called during Authorization Request at Authorization Endpoint.
@@ -38,5 +41,6 @@ class PostAuthn(PostAuthnType):
     # This script has higher priority and can cancel Pre-Authorization and persisted authorizations.
     # Note :
     # context is reference of io.jans.as.server.service.external.context.ExternalPostAuthnContext(in https://github.com/GluuFederation/oxauth project, )
-    def forceAuthorization(self, context):
+    @classmethod
+    def forceAuthorization(cls, context):
         return False
