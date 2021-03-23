0e83eff76a0b42bff1931712f05208c69f8d7d12
YuriyZ <yzabrovarniy@gmail.com>
2021-03-23 14:12:49 +0200

    refactor : correcting issues based on static analyzer.
    Issue: Making static code analyzer happier.

--- client/src/main/java/io/jans/as/client/ciba/ping/PingCallbackResponse.java
+++ client/src/main/java/io/jans/as/client/ciba/ping/PingCallbackResponse.java
@@ -6,18 +6,14 @@
 
 package io.jans.as.client.ciba.ping;
 
-import org.apache.log4j.Logger;
-import org.jboss.resteasy.client.ClientResponse;
-
 import io.jans.as.client.BaseResponse;
+import org.jboss.resteasy.client.ClientResponse;
 
 /**
  * @author Javier Rojas Blum
  * @version December 21, 2019
  */
 public class PingCallbackResponse extends BaseResponse {
-
-    private static final Logger LOG = Logger.getLogger(PingCallbackResponse.class);
 
     public PingCallbackResponse(ClientResponse<String> clientResponse) {
         super(clientResponse);
