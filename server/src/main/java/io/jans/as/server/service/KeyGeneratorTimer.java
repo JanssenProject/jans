6d1c26ffdf7838d86b9573031fd81157e5b6c91c
YuriyZ <yzabrovarniy@gmail.com>
2021-03-23 13:54:40 +0200

    (4.3) Restricted keys generation by keyAlgsAllowedForGeneration configuration property.
    https://github.com/GluuFederation/oxAuth/issues/1525

--- server/src/main/java/io/jans/as/server/service/KeyGeneratorTimer.java
+++ server/src/main/java/io/jans/as/server/service/KeyGeneratorTimer.java
@@ -144,8 +144,7 @@ public class KeyGeneratorTimer {
     }
 
     private JSONObject updateKeys(JSONObject jwks) throws Exception {
-        JSONObject jsonObject = AbstractCryptoProvider.generateJwks(cryptoProvider, appConfiguration.getKeyRegenerationInterval(),
-                appConfiguration.getIdTokenLifetime());
+        JSONObject jsonObject = AbstractCryptoProvider.generateJwks(cryptoProvider, appConfiguration);
 
         JSONArray keys = jwks.getJSONArray(JSON_WEB_KEY_SET);
         for (int i = 0; i < keys.length(); i++) {
