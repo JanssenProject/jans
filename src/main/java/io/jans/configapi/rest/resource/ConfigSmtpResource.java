f2a46be46883831453e74d16c209d8b8e28646ca
pujavs <pujas.works@gmail.com>
2021-04-09 20:34:50 +0530

    fix(smtp): fixed smtp configuration test endpoint and realted spec

--- src/main/java/io/jans/configapi/rest/resource/ConfigSmtpResource.java
+++ src/main/java/io/jans/configapi/rest/resource/ConfigSmtpResource.java
@@ -97,16 +97,12 @@ public class ConfigSmtpResource extends BaseResource {
         SmtpConfiguration smtpConfiguration = configurationService.getConfiguration().getSmtpConfiguration();
         log.debug("\n ConfigSmtpResource::testSmtpConfiguration() - 1 - smtpConfiguration = "+smtpConfiguration+"\n\n");
         smtpConfiguration.setPasswordDecrypted(encryptionService.decrypt(smtpConfiguration.getPassword()));
-        boolean result = mailService.sendMail(smtpConfiguration, smtpConfiguration.getFromEmailAddress(),
+        boolean status = mailService.sendMail(smtpConfiguration, smtpConfiguration.getFromEmailAddress(),
                 smtpConfiguration.getFromName(), smtpConfiguration.getFromEmailAddress(), null,
                 "SMTP Configuration verification", "Mail to test smtp configuration",
                 "Mail to test smtp configuration");
-        log.debug("\n ConfigSmtpResource::testSmtpConfiguration() - 2 - result = "+result+"\n\n");
-        JsonObjectBuilder builder = Json.createObjectBuilder();
-        builder.add("service", "SMTP SERVER CONFIGURATION TEST");
-        builder.add("status", result ? "OKAY" : "FAILED");
-        log.debug("\n ConfigSmtpResource::testSmtpConfiguration() - 3 - builder="+builder+"\n\n");
-        return Response.ok(builder.build()).build();
+        log.debug("\n ConfigSmtpResource::testSmtpConfiguration() - 2 - status = "+status+"\n\n");
+        return Response.ok(status).build();
     }
 
     @DELETE
