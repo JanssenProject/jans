760deae0a5eb08ec097407ee1bb1ea097376b805
YuriyZ <yzabrovarniy@gmail.com>
2021-04-26 13:09:29 +0300

    feat: enable/disable `/health-check` endpoint based on enabled component
    https://github.com/JanssenProject/jans-auth-server/issues/82

--- server/src/main/java/io/jans/as/server/ws/rs/controller/HealthCheckController.java
+++ server/src/main/java/io/jans/as/server/ws/rs/controller/HealthCheckController.java
@@ -6,6 +6,10 @@
 
 package io.jans.as.server.ws.rs.controller;
 
+import io.jans.as.model.common.ComponentType;
+import io.jans.as.model.error.ErrorResponseFactory;
+import io.jans.orm.PersistenceEntryManager;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -13,8 +17,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import io.jans.orm.PersistenceEntryManager;
 
 /**
  * Health check controller
@@ -29,11 +31,15 @@ public class HealthCheckController {
 	@Inject
 	private PersistenceEntryManager persistenceEntryManager;
 
+	@Inject
+    private ErrorResponseFactory errorResponseFactory;
+
     @GET
     @POST
     @Path("/health-check")
     @Produces(MediaType.APPLICATION_JSON)
 	public String healthCheckController() {
+        errorResponseFactory.validateComponentEnabled(ComponentType.HEALTH_CHECK);
     	boolean isConnected = persistenceEntryManager.getOperationService().isConnected();
     	String dbStatus = isConnected ? "online" : "offline"; 
         return "{\"status\": \"running\", \"db_status\":\"" + dbStatus + "\"}";
