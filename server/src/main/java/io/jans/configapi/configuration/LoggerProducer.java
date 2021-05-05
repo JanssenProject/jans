9bce6e9d55f046d7b4734ae9931771b3a7a2135f
pujavs <pujas.works@gmail.com>
2021-05-05 23:14:10 +0530

    refactor(weld): replacing quarkus with weld issue#33

--- server/src/main/java/io/jans/configapi/configuration/LoggerProducer.java
+++ server/src/main/java/io/jans/configapi/configuration/LoggerProducer.java
@@ -10,11 +10,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Priority;
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Alternative;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.inject.Named;
 
+@ApplicationScoped
 @Named
 @Alternative
 @Priority(1)
