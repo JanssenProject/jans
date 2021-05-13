6d30974d1f55e37fa518ee45b453bb0e79cac58a
Mustafa Baser <mbaser@mail.com>
2021-05-13 22:03:44 +0300

    fix(spanner): indexs of del and exp

--- setup_app/installers/rdbm.py
+++ setup_app/installers/rdbm.py
@@ -232,14 +232,13 @@ class RDBMInstaller(BaseInstaller, SetupUtils):
                                 )
                         self.dbUtils.spanner.create_table(sql_cmd)
 
-                for i, custom_index in enumerate(sql_indexes['__common__'].get(tblCls, {}).get('custom', [])):
-                    sql_cmd = 'CREATE INDEX `{0}_custom_Idx{1}` ON `{0}` (`{2}`)'.format(
-                                tblCls,
-                                i,
-                                custom_index
+                for i, custom_index in enumerate(sql_indexes.get(tblCls, {}).get('custom', [])):
+                    sql_cmd = 'CREATE INDEX `{0}_CustomIdx{1}` ON {0} ({2})'.format(
+                                    tblCls,
+                                    i+1, 
+                                    custom_index
                                 )
                     self.dbUtils.spanner.create_table(sql_cmd)
-
 
         else:
             for tblCls in self.dbUtils.Base.classes.keys():
@@ -296,7 +295,7 @@ class RDBMInstaller(BaseInstaller, SetupUtils):
                         sql_cmd = 'ALTER TABLE {0}.{1} ADD INDEX `{2}` ({3});'.format(
                                         Config.rdbm_db,
                                         tblCls,
-                                        '{}_Idx{}'.format(tblCls, i+1),
+                                        '{}_CustomIdx{}'.format(tblCls, i+1),
                                         custom_index
                                     )
                         self.dbUtils.exec_rdbm_query(sql_cmd)
