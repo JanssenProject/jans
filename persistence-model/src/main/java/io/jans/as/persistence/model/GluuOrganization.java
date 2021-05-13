5b63b5dc33af77117210bbfa453a4fe52fb5d6e5
YuriyZ <yzabrovarniy@gmail.com>
2021-05-13 18:54:39 +0300

    style: no changes, code reformat only.

--- persistence-model/src/main/java/io/jans/as/persistence/model/GluuOrganization.java
+++ persistence-model/src/main/java/io/jans/as/persistence/model/GluuOrganization.java
@@ -20,55 +20,55 @@ import java.io.Serializable;
 
 /**
  * Group
- * 
+ *
  * @author Yuriy Movchan Date: 11.02.2010
  */
-@DataEntry(sortBy = { "displayName" })
+@DataEntry(sortBy = {"displayName"})
 @ObjectClass(value = "jansOrganization")
 @JsonInclude(Include.NON_NULL)
 public class GluuOrganization extends Entry implements Serializable {
 
-	private static final long serialVersionUID = -8284018077740582699L;
+    private static final long serialVersionUID = -8284018077740582699L;
 
-	@NotNull
-	@Size(min = 0, max = 60, message = "Length of the Display Name should not exceed 60")
-	@AttributeName
-	private String displayName;
+    @NotNull
+    @Size(min = 0, max = 60, message = "Length of the Display Name should not exceed 60")
+    @AttributeName
+    private String displayName;
 
-	@NotNull
-	@Size(min = 0, max = 60, message = "Length of the Description should not exceed 60")
-	@AttributeName
-	private String description;
+    @NotNull
+    @Size(min = 0, max = 60, message = "Length of the Description should not exceed 60")
+    @AttributeName
+    private String description;
 
-	@AttributeName(name = "memberOf")
-	private String member;
+    @AttributeName(name = "memberOf")
+    private String member;
 
-	@AttributeName(name = "c")
-	private String countryName;
+    @AttributeName(name = "c")
+    private String countryName;
 
-	@AttributeName(name = "o")
-	private String organization;
+    @AttributeName(name = "o")
+    private String organization;
 
-	@AttributeName
-	private String seeAlso;
+    @AttributeName
+    private String seeAlso;
 
-	@AttributeName(name = "jansStatus")
-	private GluuStatus status;
+    @AttributeName(name = "jansStatus")
+    private GluuStatus status;
 
-	@AttributeName(name = "jansManagerGroup")
-	private String managerGroup;
+    @AttributeName(name = "jansManagerGroup")
+    private String managerGroup;
 
-	@AttributeName(name = "jansThemeColor")
-	private String themeColor;
+    @AttributeName(name = "jansThemeColor")
+    private String themeColor;
 
-	@AttributeName(name = "jansOrgShortName")
-	private String shortName;
+    @AttributeName(name = "jansOrgShortName")
+    private String shortName;
 
-	@AttributeName(name = "jansCustomMessage")
-	private String[] customMessages;
+    @AttributeName(name = "jansCustomMessage")
+    private String[] customMessages;
 
-	@AttributeName(name = "title")
-	private String title;
+    @AttributeName(name = "title")
+    private String title;
 
     @AttributeName(name = "jansLogoPath")
     private String jsLogoPath;
@@ -93,106 +93,106 @@ public class GluuOrganization extends Entry implements Serializable {
     }
 
     public String getOrganizationTitle() {
-		if (title == null || title.trim().equals("")) {
-			return "Gluu";
-		}
-		return title;
-	}
+        if (title == null || title.trim().equals("")) {
+            return "Gluu";
+        }
+        return title;
+    }
 
-	public String getCountryName() {
-		return countryName;
-	}
+    public String getCountryName() {
+        return countryName;
+    }
 
-	public void setCountryName(String countryName) {
-		this.countryName = countryName;
-	}
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
 
-	public String[] getCustomMessages() {
-		return customMessages;
-	}
+    public String[] getCustomMessages() {
+        return customMessages;
+    }
 
-	public void setCustomMessages(String[] customMessages) {
-		this.customMessages = customMessages;
-	}
+    public void setCustomMessages(String[] customMessages) {
+        this.customMessages = customMessages;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public String getDisplayName() {
-		return displayName;
-	}
+    public String getDisplayName() {
+        return displayName;
+    }
 
-	public void setDisplayName(String displayName) {
-		this.displayName = displayName;
-	}
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
 
-	public String getManagerGroup() {
-		return managerGroup;
-	}
+    public String getManagerGroup() {
+        return managerGroup;
+    }
 
-	public void setManagerGroup(String managerGroup) {
-		this.managerGroup = managerGroup;
-	}
+    public void setManagerGroup(String managerGroup) {
+        this.managerGroup = managerGroup;
+    }
 
-	public String getMember() {
-		return member;
-	}
+    public String getMember() {
+        return member;
+    }
 
-	public void setMember(String member) {
-		this.member = member;
-	}
+    public void setMember(String member) {
+        this.member = member;
+    }
 
-	public String getOrganization() {
-		return organization;
-	}
+    public String getOrganization() {
+        return organization;
+    }
 
-	public void setOrganization(String organization) {
-		this.organization = organization;
-	}
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
 
-	public String getSeeAlso() {
-		return seeAlso;
-	}
+    public String getSeeAlso() {
+        return seeAlso;
+    }
 
-	public void setSeeAlso(String seeAlso) {
-		this.seeAlso = seeAlso;
-	}
+    public void setSeeAlso(String seeAlso) {
+        this.seeAlso = seeAlso;
+    }
 
-	public String getShortName() {
-		return shortName;
-	}
+    public String getShortName() {
+        return shortName;
+    }
 
-	public void setShortName(String shortName) {
-		this.shortName = shortName;
-	}
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
 
-	public GluuStatus getStatus() {
-		return status;
-	}
+    public GluuStatus getStatus() {
+        return status;
+    }
 
-	public void setStatus(GluuStatus status) {
-		this.status = status;
-	}
+    public void setStatus(GluuStatus status) {
+        this.status = status;
+    }
 
-	public String getThemeColor() {
-		return themeColor;
-	}
+    public String getThemeColor() {
+        return themeColor;
+    }
 
-	public void setThemeColor(String themeColor) {
-		this.themeColor = themeColor;
-	}
+    public void setThemeColor(String themeColor) {
+        this.themeColor = themeColor;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
 }
