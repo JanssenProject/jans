c78e7a3b2b6c2897f49d9507b0f6cfd4a8a9ab41
yurem <Yuriy.Movchan@gmail.com>
2021-05-14 19:26:45 +0300

    feat: add spanner test conf

--- setup_app/test_data_loader.py
+++ setup_app/test_data_loader.py
@@ -225,7 +225,7 @@ class TestDataLoader(BaseInstaller, SetupUtils):
                                     'sessionIdRequestParameterEnabled': True,
                                     'skipRefreshTokenDuringRefreshing': False,
                                     'enabledComponents': ['unknown', 'health_check', 'userinfo', 'clientinfo', 'id_generation', 'registration', 'introspection', 'revoke_token', 'revoke_session', 'end_session', 'status_session', 'jans_configuration', 'ciba', 'uma', 'u2f', 'device_authz', 'stat'],
-                                    'cleanServiceInterval':1200
+                                    'cleanServiceInterval':3600
                                     }
 
         if Config.get('config_patch_creds'):
