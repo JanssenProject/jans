1120d7b021cc0a5dab2a64e5cf67a3a7b32da56d
YuriyZ <yzabrovarniy@gmail.com>
2021-06-07 11:38:10 +0300

    feat(jans): made mtls service ignore order during subject matching
    https://github.com/JanssenProject/jans-auth-server/issues/116

--- model/src/test/java/io/jans/as/model/util/CertUtilsTest.java
+++ model/src/test/java/io/jans/as/model/util/CertUtilsTest.java
@@ -9,6 +9,9 @@ package io.jans.as.model.util;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
 /**
  * @author Yuriy Zabrovarnyy
  */
@@ -31,5 +34,21 @@ public class CertUtilsTest {
         Assert.assertEquals("A4DtL2JmUMhAsvJj5tKyn64SqzmuXbMrJa0n761y5v0", CertUtils.confirmationMethodHashS256(TEST_PEM_1));
         Assert.assertEquals("A4DtL2JmUMhAsvJj5tKyn64SqzmuXbMrJa0n761y5v0", CertUtils.confirmationMethodHashS256(TEST_PEM_2));
         Assert.assertEquals("A4DtL2JmUMhAsvJj5tKyn64SqzmuXbMrJa0n761y5v0", CertUtils.confirmationMethodHashS256(TEST_PEM_3));
+    }
+
+    @Test
+    public void equalsRdn_withCorrectValues_shouldReturnTrueAndIgnoreOrder() {
+        String r1 = "C=GB,O=OpenBanking,OU=0015800000jfFGuAAM,CN=1g7yUiOr3p0QFnAB1UvInE";
+        String r2 = "CN=1g7yUiOr3p0QFnAB1UvInE, OU=0015800000jfFGuAAM, O=OpenBanking, C=GB";
+
+        assertTrue(CertUtils.equalsRdn(r1, r2));
+    }
+
+    @Test
+    public void equalsRdn_withWrongValues_shouldReturnFalse() {
+        String r1 = "C=FAILGB,O=OpenBanking,OU=0015800000jfFGuAAM,CN=1g7yUiOr3p0QFnAB1UvInE";
+        String r2 = "CN=1g7yUiOr3p0QFnAB1UvInE, OU=0015800000jfFGuAAM, O=OpenBanking, C=GB";
+
+        assertFalse(CertUtils.equalsRdn(r1, r2));
     }
 }
