5f9099741635f0b168dbe9e6e42270eeea5ae220
YuriyZ <yzabrovarniy@gmail.com>
2021-06-30 15:41:48 +0300

    feat(jans): added par service
    https://github.com/JanssenProject/jans-auth-server/issues/101

---
+++ server/src/main/java/io/jans/as/server/par/ws/rs/ParService.java
@@ -0,0 +1,106 @@
+package io.jans.as.server.par.ws.rs;
+
+import io.jans.as.model.authorize.AuthorizeErrorResponseType;
+import io.jans.as.model.config.StaticConfiguration;
+import io.jans.as.model.error.ErrorResponseFactory;
+import io.jans.as.persistence.model.Par;
+import io.jans.orm.PersistenceEntryManager;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.UUID;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ */
+@ApplicationScoped
+public class ParService {
+
+    public static final String ID_REFIX = "urn:ietf:params:oauth:request_uri:";
+
+    @Inject
+    private Logger log;
+
+    @Inject
+    private StaticConfiguration staticConfiguration;
+
+    @Inject
+    private PersistenceEntryManager entryManager;
+
+    @Inject
+    private ErrorResponseFactory errorResponseFactory;
+
+    public boolean isPar(String requestUri) {
+        if (StringUtils.isBlank(requestUri)) {
+            return false;
+        }
+        return requestUri.startsWith(ID_REFIX);
+    }
+
+    public void persist(Par par) {
+        setIdAndDnIfNeeded(par);
+
+        entryManager.persist(par);
+    }
+
+    public Par getPar(String id) {
+        return getParByDn(dn(id));
+    }
+
+    public Par getParByDn(String dn) {
+        try {
+            return entryManager.find(Par.class, dn);
+        } catch (Exception e) {
+            log.trace(e.getMessage(), e);
+            return null;
+        }
+    }
+
+    private void setIdAndDnIfNeeded(Par par) {
+        if (StringUtils.isBlank(par.getId())) {
+            par.setId(ID_REFIX + UUID.randomUUID().toString());
+        }
+
+        if (StringUtils.isBlank(par.getDn())) {
+            par.setDn(dn(par.getId()));
+        }
+    }
+
+    public String dn(String id) {
+        if (StringUtils.isBlank(id)) {
+            throw new IllegalArgumentException("PAR id is null or blank.");
+        }
+        return String.format("jansPar=%s,%s", id, branchBaseDn());
+    }
+
+    public String branchBaseDn() {
+        return staticConfiguration.getBaseDn().getPar(); // "ou=par,o=jans"
+    }
+
+    public Par getParAndValidateForAuthorizationRequest(String id, String state, String clientIdInRequest) {
+        Par par = getPar(id);
+
+        if (par == null) {
+            log.debug("Failed to find PAR by request_uri (id): " + id );
+            throw new WebApplicationException(Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity(errorResponseFactory.getErrorAsJson(AuthorizeErrorResponseType.INVALID_REQUEST, state, "Failed to find par by request_uri"))
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .build());
+        }
+        if (StringUtils.isBlank(clientIdInRequest) || !clientIdInRequest.equals(par.getAttributes().getClientId())) {
+            log.debug("client_id does not match to PAR's client_id (used during PAR registration). Reject request. PAR clientId: " + par.getAttributes().getClientId() + ", request's clientId: " + clientIdInRequest);
+            throw new WebApplicationException(Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity(errorResponseFactory.getErrorAsJson(AuthorizeErrorResponseType.INVALID_REQUEST, state, "client_id does not match to PAR's client_id"))
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .build());
+        }
+        return par;
+    }
+}
