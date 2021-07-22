aa253f3e1689fbe89061c008a4afeed45ad24a9a
pujavs <pujas.works@gmail.com>
2021-07-22 19:07:48 +0530

    feat(stats) : user stat jsonnode tested

--- server/src/main/java/io/jans/configapi/security/client/StatClient.java
+++ server/src/main/java/io/jans/configapi/security/client/StatClient.java
@@ -1,5 +1,6 @@
 package io.jans.configapi.security.client;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -12,7 +13,7 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 public interface StatClient {
 
     @GET
-    @Consumes({ MediaType.APPLICATION_JSON })
-    @Produces({ MediaType.APPLICATION_JSON })
-    public Response statGet(@HeaderParam("Authorization") String authorization, @QueryParam("month") String month);
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response statGet(@HeaderParam("Authorization") String authorization, @QueryParam("month") String month, @QueryParam("format") String format);
+
 }
