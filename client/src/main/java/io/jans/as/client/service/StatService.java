de0aeac4719f06abe9cac96e7ab4e4597f6daa4c
YuriyZ <yzabrovarniy@gmail.com>
2021-07-23 11:50:58 +0300

    feat: added method to query stat with format format

--- client/src/main/java/io/jans/as/client/service/StatService.java
+++ client/src/main/java/io/jans/as/client/service/StatService.java
@@ -1,5 +1,7 @@
 package io.jans.as.client.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
@@ -8,8 +10,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-import com.fasterxml.jackson.databind.JsonNode;
-
 /**
  * @author Yuriy Zabrovarnyy
  */
@@ -17,6 +17,9 @@ public interface StatService {
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     JsonNode stat(@HeaderParam("Authorization") String authorization, @QueryParam("month") String month);
+
+    @GET
+    String stat(@HeaderParam("Authorization") String authorization, @QueryParam("month") String month, @QueryParam("format") String format);
 
     @POST
     @Produces({MediaType.APPLICATION_JSON})
