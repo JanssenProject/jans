08007dd6268c073d2aede26830fdce603ddbf20f
YuriyZ <yzabrovarniy@gmail.com>
2021-07-23 14:17:03 +0300

    feat(jans): forced stat scope for statistic endpoint
    https://github.com/GluuFederation/oxAuth/issues/1554

--- client/src/test/java/io/jans/as/client/ws/rs/StatWSTest.java
+++ client/src/test/java/io/jans/as/client/ws/rs/StatWSTest.java
@@ -1,18 +1,15 @@
 package io.jans.as.client.ws.rs;
 
-import static org.testng.Assert.assertTrue;
-
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-
 import com.fasterxml.jackson.databind.JsonNode;
-
-import io.jans.as.client.BaseRequest;
 import io.jans.as.client.BaseTest;
 import io.jans.as.client.service.ClientFactory;
 import io.jans.as.client.service.StatService;
 import io.jans.as.client.uma.wrapper.UmaClient;
 import io.jans.as.model.uma.wrapper.Token;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
 
 /**
  * @author Yuriy Zabrovarnyy
@@ -21,29 +18,22 @@ public class StatWSTest extends BaseTest {
 
     private static final String STAT_PATH = "/jans-auth/restv1/internal/stat";
 
-    @Test
+    @Test(enabled = false)
     @Parameters({"umaPatClientId", "umaPatClientSecret"})
     public void stat(final String umaPatClientId, final String umaPatClientSecret) throws Exception {
         final Token authorization = UmaClient.requestPat(tokenEndpoint, umaPatClientId, umaPatClientSecret);
 
         final StatService service = ClientFactory.instance().createStatService(issuer + STAT_PATH);
-        final JsonNode node = service.stat("Bearer " + authorization.getAccessToken(), "202101");
+        final JsonNode node = service.stat("Bearer " + authorization.getAccessToken(), "202101", null);
         assertTrue(node != null && node.hasNonNull("response"));
     }
 
-    @Test
-    @Parameters({"umaPatClientId", "umaPatClientSecret"})
-    public void statBasic(final String umaPatClientId, final String umaPatClientSecret) throws Exception {
-        final StatService service = ClientFactory.instance().createStatService(issuer + "/jans-auth/restv1/internal/stat");
-        final JsonNode node = service.stat("Basic " + BaseRequest.getEncodedCredentials(umaPatClientId, umaPatClientSecret), "202101");
-        assertTrue(node != null && node.hasNonNull("response"));
-    }
-
-    @Test
+    @Test(enabled = false)
     @Parameters({"umaPatClientId", "umaPatClientSecret"})
     public void statPost(final String umaPatClientId, final String umaPatClientSecret) throws Exception {
+        final Token authorization = UmaClient.requestPat(tokenEndpoint, umaPatClientId, umaPatClientSecret);
         final StatService service = ClientFactory.instance().createStatService(issuer + "/jans-auth/restv1/internal/stat");
-        final JsonNode node = service.stat(null, "202101", umaPatClientId, umaPatClientSecret);
+        final JsonNode node = service.stat(authorization.getAccessToken(), "202101", null);
         assertTrue(node != null && node.hasNonNull("response"));
     }
 }
