d13b8c5bb09993c3cbb28e7281a5f857f042637f
pujavs <pujas.works@gmail.com>
2021-08-04 13:30:40 +0530

    fix(cors): cors issue 49

--- server/src/main/java/io/jans/configapi/filters/CorsFilter.java
+++ server/src/main/java/io/jans/configapi/filters/CorsFilter.java
@@ -15,9 +15,7 @@ import io.jans.configapi.model.configuration.CorsConfiguration;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Pattern;
 
 import javax.inject.Inject;
@@ -33,147 +31,154 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 
-@WebFilter(filterName = "CorsFilter", asyncSupported = true, urlPatterns = { "*" })
-public class CorsFilter implements Filter {
+@WebFilter(filterName = "CorsFilter", asyncSupported = true, urlPatterns = { "/*" })
+public class CorsFilter extends AbstractCorsFilter {
 
     @Inject
     private Logger log;
 
     @Inject
-    private CorsConfiguration corsConfiguration;
+    ConfigurationFactory configurationFactory;
 
-    //@Inject
-    //ConfigurationFactory configurationFactory;
+    private boolean filterEnabled;
 
-    private static final Pattern COMMA_SEPARATED_SPLIT_REGEX = Pattern.compile("\\s*,\\s*");
-    public static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
-    public static final String ACCESS_CONTROL_REQUEST_HEADERS = "Access-Control-Request-Headers";
-    public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
-    public static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
-    public static final String ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
-    public static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    public CorsFilter() {
+        super();
+    }
 
-    /**
-     * The Access-Control-Allow-Headers header indicates, as part of the
-     * response to a preflight request, which header field names can be used
-     * during the actual request.
-     */
-    public static final String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_HEADERS =
-            "Access-Control-Allow-Headers";
-    
     @Override
-    public void init(final FilterConfig filterConfig) throws ServletException {       
+    public void init(final FilterConfig filterConfig) throws ServletException {
+
+        log.debug("\n\n\n *** CorsFilter::init() - configurationFactory = " + configurationFactory + " *** \n\n\n");
+        // Initialize defaults
+        parseAndStore(DEFAULT_ALLOWED_ORIGINS, DEFAULT_ALLOWED_HTTP_METHODS, DEFAULT_ALLOWED_HTTP_HEADERS,
+                DEFAULT_EXPOSED_HEADERS, DEFAULT_SUPPORTS_CREDENTIALS, DEFAULT_PREFLIGHT_MAXAGE,
+                DEFAULT_DECORATE_REQUEST);
+
+        ApiAppConfiguration apiAppConfiguration = configurationFactory.getApiAppConfiguration();
+
+        log.debug("\n\n\n *** CorsFilter::init() - apiAppConfiguration = " + apiAppConfiguration + " *** \n\n\n");
+
+        if (filterConfig != null) {
+            String filterName = filterConfig.getFilterName();
+            CorsFilterConfig corsFilterConfig = new CorsFilterConfig(filterName, apiAppConfiguration);
+
+            String configEnabled = corsFilterConfig.getInitParameter(PARAM_CORS_ENABLED);
+            String configAllowedOrigins = corsFilterConfig.getInitParameter(PARAM_CORS_ALLOWED_ORIGINS);
+            String configAllowedHttpMethods = corsFilterConfig.getInitParameter(PARAM_CORS_ALLOWED_METHODS);
+            String configAllowedHttpHeaders = corsFilterConfig.getInitParameter(PARAM_CORS_ALLOWED_HEADERS);
+            String configExposedHeaders = corsFilterConfig.getInitParameter(PARAM_CORS_EXPOSED_HEADERS);
+            String configSupportsCredentials = corsFilterConfig.getInitParameter(PARAM_CORS_SUPPORT_CREDENTIALS);
+            String configPreflightMaxAge = corsFilterConfig.getInitParameter(PARAM_CORS_PREFLIGHT_MAXAGE);
+            String configDecorateRequest = corsFilterConfig.getInitParameter(PARAM_CORS_REQUEST_DECORATE);
+
+            if (configEnabled != null) {
+                this.filterEnabled = Boolean.parseBoolean(configEnabled);
+            }
+
+            log.debug("\n\n\n *** CorsFilter::init() - configEnabled = "+configEnabled+" , configAllowedOrigins = " + configAllowedOrigins
+                    + " ,configAllowedHttpMethods = " + configAllowedHttpMethods + " *** \n\n\n");
+            parseAndStore(configAllowedOrigins, configAllowedHttpMethods, configAllowedHttpHeaders,
+                    configExposedHeaders, configSupportsCredentials, configPreflightMaxAge, configDecorateRequest);
+            
+            log.debug("\n\n\n *** CorsFilter::init() - configEnabled = "+configEnabled+" , configAllowedOrigins = " + configAllowedOrigins
+                    + " ,configAllowedHttpMethods = " + configAllowedHttpMethods + " *** \n\n\n");
+        }
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
-        log.error("CorsFilter::doFilter() - Entry - corsConfiguration = "+corsConfiguration);
         Collection<String> globalAllowedOrigins = null;
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        
-        String origin = request.getHeader("Origin");     
-        log.debug("CorsFilter::doFilter() - Entry - origin = " + origin);
-        
-        if ( (StringUtils.isNotBlank(origin)) || (corsConfiguration!=null && !corsConfiguration.isEnabled()) ) {
-               
-                //Method check
-                final String requestedMethods = request.getMethod();
-                log.debug("CorsFilter::doFilter() requestedMethods = "+requestedMethods);
-                if (StringUtils.isNotBlank(requestedMethods)) {
-                    processMethods(response, requestedMethods);
-                }
-
-                //Header check
-                final String requestedHeaders = request.getHeader(ACCESS_CONTROL_REQUEST_HEADERS);
-                log.debug("CorsFilter::doFilter() requestedHeaders = "+requestedHeaders);
-                if (StringUtils.isNotBlank(requestedHeaders)) {
-                    processRequestedHeaders(response, requestedHeaders);
-                }
-
-                //Origin check
-                boolean allowsOrigin =  (corsConfiguration.getAllowedOrigins()==null || corsConfiguration.getAllowedOrigins().size() ==0 || corsConfiguration.isOriginAllowed(origin));
-                log.debug("CorsFilter::doFilter() allowsOrigin = "+allowsOrigin);
-                if (allowsOrigin) {
-                    response.addHeader(ACCESS_CONTROL_ALLOW_ORIGIN, origin);
-                }
-
-                boolean allowCredentials = ( (corsConfiguration.isSupportsCredentials()) || 
-                       (corsConfiguration.getAllowedOrigins()!=null && corsConfiguration.isOriginAllowed(origin) && !corsConfiguration.getAllowedOrigins().contains("*")) );
-
-                response.addHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, String.valueOf(allowCredentials));
-
-                final Collection<String> exposedHeaders = corsConfiguration.getExposedHeaders();
-
-                if (exposedHeaders !=null && exposedHeaders.size()>0) {
-                    response.addHeader(ACCESS_CONTROL_EXPOSE_HEADERS,
-                            String.join(",", exposedHeaders));
-                }
-/*
-                if (request.method().equals(HttpMethod.OPTIONS)) {
-                    if ((requestedHeaders != null || requestedMethods != null) && corsConfiguration.accessControlMaxAge.isPresent()) {
-                        response.putHeader(HttpHeaders.ACCESS_CONTROL_MAX_AGE,
-                                String.valueOf(corsConfiguration.accessControlMaxAge.get().getSeconds()));
-                    }
-                    response.end();
-                } else {
-                    event.next();
-                }
-            */
+        String origin = request.getHeader("Origin");
+        log.debug("\n\n\n *** CorsFilter::doFilter() - Entry - this.filterEnabled = "+this.filterEnabled+" ,origin = " + origin + " *** \n\n\n");
+        Collection<String> allowedOrigins = null;
+        if (this.filterEnabled) {
+            try {
+                allowedOrigins = doFilterImpl(servletRequest,origin);
+                log.debug("\n\n\n *** CorsFilter::doFilter() - allowedOrigins = " + allowedOrigins
+                + " *** \n\n\n");
+            } catch (Exception ex) {
+                log.error("Failed to process request", ex);
+            }
+            super.doFilter(servletRequest, servletResponse, filterChain);
+            setAllowedOrigins(allowedOrigins);
         } else {
+            log.debug("\n\n\n *** CorsFilter::doFilter() - filterEnabled disabled  *** \n\n\n");
             filterChain.doFilter(servletRequest, servletResponse);
         }
-    }
-    
-    @Override
-    public void destroy() {
+        
+        log.debug("\n\n\n *** CorsFilter::doFilter() - 3 - getHeader(Origin) = " + request.getHeader("Origin")
+                + " *** \n\n\n");
     }
 
-    private void processRequestedHeaders(HttpServletResponse response, String allowHeadersValue) {
-        log.debug("CorsFilter::processRequestedHeaders() - Entry - allowHeadersValue = "+allowHeadersValue);
-        if (corsConfiguration.getAllowedHttpHeaders()== null || corsConfiguration.getAllowedHttpHeaders().size()==0) {
-            response.addHeader(ACCESS_CONTROL_ALLOW_HEADERS, allowHeadersValue);
+    protected Collection<String> doFilterImpl(ServletRequest servletRequest,String origin )
+            throws UnsupportedEncodingException, IOException, ServletException {
+         
+        Collection<String> globalAllowedOrigins = getAllowedOrigins();
+        log.debug("\n\n\n *** CorsFilter::doFilterImpl() - New - globalAllowedOrigins = " + globalAllowedOrigins
+        + " *** \n\n\n");
+
+        if (getAllowedOrigins() == null) {
+            log.debug("\n\n\n *** CorsFilter::doFilterImpl() getAllowedOrigins() - Is Null  ");
+            List<String> authorizedOrigins = new ArrayList();
+            authorizedOrigins.add(origin);
+            setAllowedOrigins(authorizedOrigins);
+        }else if (getAllowedOrigins().size() ==0 || getAllowedOrigins().contains("*")){
+            log.debug("\n\n\n *** CorsFilter::doFilterImpl() getAllowedOrigins() - Empty OR Contains * ");
+            globalAllowedOrigins.add(origin);
+            setAllowedOrigins(globalAllowedOrigins);
+        }
+         
+         return getAllowedOrigins();
+         /*
+        if (getAllowedOrigins() != null && getAllowedOrigins().size() != 0) {
+            log.debug("\n\n\n *** CorsFilter::doFilterImpl() getAllowedOrigins() !=null ");
+            return getAllowedOrigins();
         } else {
-            List<String> requestedHeaders = new ArrayList<>();
-            for (String requestedHeader : COMMA_SEPARATED_SPLIT_REGEX.split(allowHeadersValue)) {
-                requestedHeaders.add(requestedHeader.toLowerCase());
-            }
+            log.debug("\n\n\n *** CorsFilter::doFilterImpl() getAllowedOrigins() =====null ");
+            return getDefaultOrigin(servletRequest, "*");
+        }
+        */
+    }
 
-            List<String> validRequestedHeaders = new ArrayList<>();
-            for (String configHeader : corsConfiguration.getAllowedHttpHeaders()) {
-                if (requestedHeaders.contains(configHeader.toLowerCase())) {
-                    validRequestedHeaders.add(configHeader);
-                }
-            }
+    private Collection<String> getDefaultOrigin(ServletRequest servletRequest, String origin)
+            throws UnsupportedEncodingException, IOException, ServletException {
+        Collection<String> defaultAllowedOrigins = new HashSet<String>();
+        Set<String> setAllowedOrigins = parseStringToSet(origin);
+        defaultAllowedOrigins.addAll(setAllowedOrigins);
+        log.debug("\n\n\n *** CorsFilter::getDefaultOrigin() - defaultAllowedOrigins = "+defaultAllowedOrigins);
+        return defaultAllowedOrigins;
+    }
 
-            if (!validRequestedHeaders.isEmpty()) {
-                response.addHeader(ACCESS_CONTROL_ALLOW_HEADERS, String.join(",", validRequestedHeaders));
+    private Collection<String> addOrigin(Collection<String> globalAllowedOrigins, String origin)
+            throws UnsupportedEncodingException, IOException, ServletException {
+        log.debug("\n\n\n *** CorsFilter::addOrigin() - globalAllowedOrigins = "+globalAllowedOrigins+" , origin = "+origin);
+        if (globalAllowedOrigins != null && globalAllowedOrigins.contains("*")) {
+            globalAllowedOrigins.add(origin);
+        }
+        log.debug("\n\n\n *** CorsFilter::addOrigin() - globalAllowedOrigins = "+globalAllowedOrigins);
+        return globalAllowedOrigins;
+    }
+
+    private Set<String> parseStringToSet(final String data) {
+        String[] splits;
+
+        if (data != null && data.length() > 0) {
+            splits = data.split(",");
+        } else {
+            splits = new String[] {};
+        }
+
+        Set<String> set = new HashSet<String>();
+        if (splits.length > 0) {
+            for (String split : splits) {
+                set.add(split.trim());
             }
         }
+
+        return set;
     }
 
-    private void processMethods(HttpServletResponse response, String allowMethodsValue) {
-        if (corsConfiguration.getAllowedHttpMethods()==null | corsConfiguration.getAllowedHttpMethods().size()==0) {
-            response.addHeader(ACCESS_CONTROL_ALLOW_METHODS, allowMethodsValue);
-        } else {
-            List<String> requestedMethods = new ArrayList<>();
-            for (String requestedMethod : COMMA_SEPARATED_SPLIT_REGEX.split(allowMethodsValue)) {
-                requestedMethods.add(requestedMethod.toLowerCase());
-            }
-
-            List<String> validRequestedMethods = new ArrayList<>();
-            for (String configMethod : corsConfiguration.getAllowedHttpMethods()) {
-                if (requestedMethods.contains(configMethod.toLowerCase())) {
-                    validRequestedMethods.add(configMethod);
-                }
-            }
-
-            if (!validRequestedMethods.isEmpty()) {
-                response.addHeader(ACCESS_CONTROL_ALLOW_METHODS, String.join(",", validRequestedMethods));
-            }
-        }
-    }
-    
-    
 }
