f2e25acb07376c7e1975574ab5ea0329654145d8
Mustafa Baser <mbaser@mail.com>
2021-09-06 23:30:21 +0300

    fix: update jca swagger client

--- cli/jca/swagger_client/api/health___check_api.py
+++ cli/jca/swagger_client/api/health___check_api.py
@@ -32,13 +32,13 @@ class HealthCheckApi(object):
             api_client = ApiClient()
         self.api_client = api_client
 
-    def get_config_cache(self, **kwargs):  # noqa: E501
+    def get_config_health(self, **kwargs):  # noqa: E501
         """Returns application health status.  # noqa: E501
 
         Returns application health status.  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
-        >>> thread = api.get_config_cache(async_req=True)
+        >>> thread = api.get_config_health(async_req=True)
         >>> result = thread.get()
 
         :param async_req bool
@@ -48,18 +48,18 @@ class HealthCheckApi(object):
         """
         kwargs['_return_http_data_only'] = True
         if kwargs.get('async_req'):
-            return self.get_config_cache_with_http_info(**kwargs)  # noqa: E501
+            return self.get_config_health_with_http_info(**kwargs)  # noqa: E501
         else:
-            (data) = self.get_config_cache_with_http_info(**kwargs)  # noqa: E501
+            (data) = self.get_config_health_with_http_info(**kwargs)  # noqa: E501
             return data
 
-    def get_config_cache_with_http_info(self, **kwargs):  # noqa: E501
+    def get_config_health_with_http_info(self, **kwargs):  # noqa: E501
         """Returns application health status.  # noqa: E501
 
         Returns application health status.  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
-        >>> thread = api.get_config_cache_with_http_info(async_req=True)
+        >>> thread = api.get_config_health_with_http_info(async_req=True)
         >>> result = thread.get()
 
         :param async_req bool
@@ -79,7 +79,7 @@ class HealthCheckApi(object):
             if key not in all_params:
                 raise TypeError(
                     "Got an unexpected keyword argument '%s'"
-                    " to method get_config_cache" % key
+                    " to method get_config_health" % key
                 )
             params[key] = val
         del params['kwargs']
@@ -112,6 +112,180 @@ class HealthCheckApi(object):
             post_params=form_params,
             files=local_var_files,
             response_type='HealthStatus',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
+    def get_config_health_live(self, **kwargs):  # noqa: E501
+        """Returns application liveness status.  # noqa: E501
+
+        Returns application liveness status.  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_config_health_live(async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :return: HealthStatusItem
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.get_config_health_live_with_http_info(**kwargs)  # noqa: E501
+        else:
+            (data) = self.get_config_health_live_with_http_info(**kwargs)  # noqa: E501
+            return data
+
+    def get_config_health_live_with_http_info(self, **kwargs):  # noqa: E501
+        """Returns application liveness status.  # noqa: E501
+
+        Returns application liveness status.  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_config_health_live_with_http_info(async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :return: HealthStatusItem
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = []  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method get_config_health_live" % key
+                )
+            params[key] = val
+        del params['kwargs']
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = []  # noqa: E501
+
+        return self.api_client.call_api(
+            '/jans-config-api/api/v1/health/live', 'GET',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='HealthStatusItem',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
+    def get_config_health_ready(self, **kwargs):  # noqa: E501
+        """Returns application readiness status.  # noqa: E501
+
+        Returns application readiness status.  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_config_health_ready(async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :return: HealthStatusItem
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.get_config_health_ready_with_http_info(**kwargs)  # noqa: E501
+        else:
+            (data) = self.get_config_health_ready_with_http_info(**kwargs)  # noqa: E501
+            return data
+
+    def get_config_health_ready_with_http_info(self, **kwargs):  # noqa: E501
+        """Returns application readiness status.  # noqa: E501
+
+        Returns application readiness status.  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_config_health_ready_with_http_info(async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :return: HealthStatusItem
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = []  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method get_config_health_ready" % key
+                )
+            params[key] = val
+        del params['kwargs']
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = []  # noqa: E501
+
+        return self.api_client.call_api(
+            '/jans-config-api/api/v1/health/ready', 'GET',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='HealthStatusItem',  # noqa: E501
             auth_settings=auth_settings,
             async_req=params.get('async_req'),
             _return_http_data_only=params.get('_return_http_data_only'),
