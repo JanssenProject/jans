f64a26154321f60a6c6db91722938c1ac039a5bd
root <root@10debian64-ws4.SMAN-WG>
2021-09-16 02:24:10 +0300

    some fixes of code (usage of internal functions, changing the perm access to /var/www/...);

--- setup_app/test_data_loader.py
+++ setup_app/test_data_loader.py
@@ -69,14 +69,12 @@ class TestDataLoader(BaseInstaller, SetupUtils):
         apache_user = 'www-data' if base.clone_type == 'deb' else 'apache'
 
         www_keys_dir='/var/www/html/jans-auth-client/test/resources'
-
-        try:
-            os.makedirs(www_keys_dir)
-        except Exception as ex:
-            self.logIt(ex, True, False)
+        
+        self.createDirs(www_keys_dir)
 
         shutil.copyfile(keys_json_fn, os.path.join(www_keys_dir, 'jwks.json'))
-        self.run([paths.cmd_chown, '-R', 'root:'+apache_user, '/var/www/html/jans-auth-client'])        
+        self.run([paths.cmd_chown, '-R', 'root:'+apache_user, '/var/www/html/jans-auth-client'])
+        self.run([paths.cmd_chmod, '-R', '0770', '/var/www/html/jans-auth-client'])
 
     def load_test_data(self):
         Config.pbar.progress(self.service_name, "Loading Test Data", False)
