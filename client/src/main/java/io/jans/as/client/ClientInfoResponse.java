ffe7509d87b9741077a50dfa0ab25486ddfd4788
YuriyZ <yzabrovarniy@gmail.com>
2021-09-21 21:15:55 +0300

    refactor: get rid of old duplicated code

--- client/src/main/java/io/jans/as/client/ClientInfoResponse.java
+++ client/src/main/java/io/jans/as/client/ClientInfoResponse.java
@@ -6,33 +6,35 @@
 
 package io.jans.as.client;
 
+import io.jans.as.model.userinfo.UserInfoErrorResponseType;
+import org.jboss.resteasy.client.ClientResponse;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import io.jans.as.model.userinfo.UserInfoErrorResponseType;
 
 /**
  * Represents an client info response received from the authorization server.
  *
  * @author Javier Rojas Blum Date: 07.19.2012
  */
-public class ClientInfoResponse extends BaseResponse {
+public class ClientInfoResponse extends BaseResponseWithErrors<UserInfoErrorResponseType> {
 
     private Map<String, List<String>> claims;
-
-    private UserInfoErrorResponseType errorType;
-    private String errorDescription;
-    private String errorUri;
 
     /**
      * Constructs a Client Info response.
      *
      * @param status The response status code.
      */
-    public ClientInfoResponse(int status) {
-        super(status);
-        claims = new HashMap<String, List<String>>();
+    public ClientInfoResponse(ClientResponse<String> clientResponse) {
+        super(clientResponse);
+        claims = new HashMap<>();
+    }
+
+    @Override
+    public UserInfoErrorResponseType fromString(String str) {
+        return UserInfoErrorResponseType.fromString(str);
     }
 
     public Map<String, List<String>> getClaims() {
@@ -41,69 +43,6 @@ public class ClientInfoResponse extends BaseResponse {
 
     public void setClaims(Map<String, List<String>> claims) {
         this.claims = claims;
-    }
-
-    /**
-     * Returns the error code when the request fails, otherwise will return <code>null</code>.
-     *
-     * @return The error code when the request fails.
-     */
-    public UserInfoErrorResponseType getErrorType() {
-        return errorType;
-    }
-
-    /**
-     * Sets the error code when the request fails, otherwise will return
-     * <code>null</code>.
-     *
-     * @param errorType The error code when the request fails.
-     */
-    public void setErrorType(UserInfoErrorResponseType errorType) {
-        this.errorType = errorType;
-    }
-
-    /**
-     * Returns a human-readable UTF-8 encoded text providing additional
-     * information, used to assist the client developer in understanding the
-     * error that occurred.
-     *
-     * @return The error description.
-     */
-    public String getErrorDescription() {
-        return errorDescription;
-    }
-
-    /**
-     * Sets a human-readable UTF-8 encoded text providing additional
-     * information, used to assist the client developer in understanding the
-     * error that occurred.
-     *
-     * @param errorDescription The error description.
-     */
-    public void setErrorDescription(String errorDescription) {
-        this.errorDescription = errorDescription;
-    }
-
-    /**
-     * Returns a URI identifying a human-readable web page with information
-     * about the error, used to provide the client developer with additional
-     * information about the error.
-     *
-     * @return A URI with information about the error.
-     */
-    public String getErrorUri() {
-        return errorUri;
-    }
-
-    /**
-     * Sets a URI identifying a human-readable web page with information about
-     * the error, used to provide the client developer with additional
-     * information about the error.
-     *
-     * @param errorUri A URI with information about the error.
-     */
-    public void setErrorUri(String errorUri) {
-        this.errorUri = errorUri;
     }
 
     public List<String> getClaim(String claimName) {
