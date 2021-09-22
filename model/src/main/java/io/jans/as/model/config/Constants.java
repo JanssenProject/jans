382a67475e60a8f220d6a2064839d52152fdb0a7
YuriyZ <yzabrovarniy@gmail.com>
2021-09-22 19:01:46 +0300

    fix: fixing bugs reported by sonar

--- model/src/main/java/io/jans/as/model/config/Constants.java
+++ model/src/main/java/io/jans/as/model/config/Constants.java
@@ -28,4 +28,6 @@ public class Constants {
 
     public static final String STATE = "state";
     public static final String REDIRECT = "redirect";
+    public static final String AUTH_STEP = "auth_step";
+    public static final String PROMPT = "prompt";
 }
