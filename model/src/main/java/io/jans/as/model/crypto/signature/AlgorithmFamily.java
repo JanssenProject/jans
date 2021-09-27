aef32e097339824b0d593d758e7701afbd6cf937
SMan <smansoft@ukr.net>
2021-09-27 17:07:04 +0000

    class JwkUtil has been removed; configuration files have been restored;

--- model/src/main/java/io/jans/as/model/crypto/signature/AlgorithmFamily.java
+++ model/src/main/java/io/jans/as/model/crypto/signature/AlgorithmFamily.java
@@ -9,29 +9,37 @@ package io.jans.as.model.crypto.signature;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
+import io.jans.as.model.jwk.KeyType;
+
 /**
  * @author Javier Rojas Blum
  * @author Sergey Manoylo
  * @version September 13, 2021
  */
 public enum AlgorithmFamily {
-    NONE("none"),
-    HMAC("HMAC"),
-    RSA("RSA"),
-    EC("EC"),
-    ED("ED"),
-    AES("AES"),
-    PASSW("PASSW"),
-    DIR("DIR");
+    NONE("none", null),
+    HMAC("HMAC", KeyType.OCT),
+    RSA("RSA", KeyType.RSA),
+    EC("EC", KeyType.EC),
+    ED("ED", KeyType.OKP),
+    AES("AES", KeyType.OCT),
+    PASSW("PASSW", KeyType.OCT),
+    DIR("DIR", null);
 
     private final String value;
+    private final KeyType keyType;
 
-    AlgorithmFamily(String value) {
+    AlgorithmFamily(final String value, final KeyType keyType) {
         this.value = value;
+        this.keyType = keyType;
     }
 
     public String getValue() {
         return value;
+    }
+
+    public KeyType getKeyType() {
+        return keyType;
     }
 
     @Override
