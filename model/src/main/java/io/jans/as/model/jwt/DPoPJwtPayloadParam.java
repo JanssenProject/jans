880de77ff3c18ced378b5a712ea5b45df1b28dcb
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2021-10-12 19:08:16 -0400

    fix: sonarcloud issue fixing

--- model/src/main/java/io/jans/as/model/jwt/DPoPJwtPayloadParam.java
+++ model/src/main/java/io/jans/as/model/jwt/DPoPJwtPayloadParam.java
@@ -9,33 +9,33 @@ package io.jans.as.model.jwt;
  * @author Javier Rojas Blum
  * @version September 30, 2021
  */
-public interface DPoPJwtPayloadParam {
+public final class DPoPJwtPayloadParam {
 
     /**
      * Unique identifier for the DPoP proof JWT.
      * The value must be assigned such that there is a negligible probability that the same value will be assigned
      * to any other DPoP proof used in the same context during the time window of validity.
      */
-    String JTI = "jti";
+    public static final String JTI = "jti";
 
     /**
      * The HTTP method for the request to which the JWT is attached.
      */
-    String HTM = "htm";
+    public static final String HTM = "htm";
 
     /**
      * The HTTP URI used for the request, without query and fragment parts.
      */
-    String HTU = "htu";
+    public static final String HTU = "htu";
 
     /**
      * Time at which the JWT was created.
      */
-    String IAT = "iat";
+    public static final String IAT = "iat";
 
     /**
      * Hash of the access token. Required when the DPoP proof is used in conjunction with the presentation of an
      * access token.
      */
-    String ATH = "ath";
+    public static final String ATH = "ath";
 }
