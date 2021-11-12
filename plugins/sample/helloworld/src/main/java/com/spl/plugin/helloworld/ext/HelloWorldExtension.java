b4acde65bfa5a6b98533669f40c9d885f0b77aad
pujavs <pujas.works@gmail.com>
2021-11-12 21:46:51 +0530

    feat: plugin shared lib wip

---
+++ plugins/sample/helloworld/src/main/java/com/spl/plugin/helloworld/ext/HelloWorldExtension.java
@@ -0,0 +1,20 @@
+package com.spl.plugins.helloworld.ext;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AfterDeploymentValidation;
+import javax.enterprise.inject.spi.Extension;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorldExtension implements Extension {
+    
+    private static final Logger log = LoggerFactory.getLogger(HelloWorldExtension.class.getName());
+    
+    void AfterDeploymentValidation(
+            @Observes AfterDeploymentValidation adv) {
+        log.debug("HelloWorldExtension::AfterDeploymentValidation() ********* ");
+     }
+
+    
+}
