cd2dedbb561834eb8dc6a6afc66d913a918273e1
pujavs <pujas.works@gmail.com>
2021-11-12 21:46:51 +0530

    feat: plugin shared lib wip

---
+++ plugins/sample/helloworld/src/main/java/com/spl/plugin/helloworld/rest/HelloWorldApplication.java
@@ -0,0 +1,20 @@
+package com.spl.plugins.helloworld.rest;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("/api/v1")
+public class HelloWorldApplication extends Application {
+    
+        @Override
+        public Set<Class<?>> getClasses() {
+            HashSet<Class<?>> classes = new HashSet<Class<?>>();
+
+            // General
+            classes.add(HelloWorldResource.class);
+             return classes;
+        }
+    }
---
+++ plugins/sample/helloworld/src/main/java/com/spl/plugin/helloworld/rest/HelloWorldResource.java
@@ -0,0 +1,32 @@
+package com.spl.plugins.helloworld.rest;
+
+import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import org.slf4j.Logger;
+
+import com.spl.plugins.helloworld.util.Constants;
+
+@Path("hello")
+public class HelloWorldResource {    
+    
+    @Inject
+    Logger log;
+        
+    @GET
+    public Response sayHelloWorld() {
+        return Response.ok("Hello World!").build();
+    }
+    
+    
+    @GET
+    @Path(Constants.NAME_PATH)
+    public Response sayHelloWithName(@PathParam(Constants.NAME) @NotNull String name) {
+        return Response.ok("Hello World!").build();
+    }
+
+    
+
+}
