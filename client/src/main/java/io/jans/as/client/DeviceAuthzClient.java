1a8c4677f5e4c00235a05f2f6d315f6292a83efd
yurem <Yuriy.Movchan@gmail.com>
2021-11-17 21:14:00 +0300

    chore: sonar fixes

--- client/src/main/java/io/jans/as/client/DeviceAuthzClient.java
+++ client/src/main/java/io/jans/as/client/DeviceAuthzClient.java
@@ -55,6 +55,7 @@ public class DeviceAuthzClient extends BaseClient<DeviceAuthzRequest, DeviceAuth
         return _exec();
     }
 
+    @SuppressWarnings("deprecation")
     @Deprecated
     public DeviceAuthzResponse exec(ClientHttpEngine engine) {
         resteasyClient = ((ResteasyClientBuilder) ClientBuilder.newBuilder()).httpEngine(engine).build();
