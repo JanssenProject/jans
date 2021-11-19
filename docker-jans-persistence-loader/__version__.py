6a8ab00e5addcaf41af12f8c008926279bbda459
moabu <47318409+moabu@users.noreply.github.com>
2021-11-19 09:47:43 -0500

    ci: push initial release strategy

--- docker-jans-persistence-loader/__version__.py
+++
@@ -1,7 +0,0 @@
-"""
- License terms and conditions for Janssen Cloud Native Edition:
- https://www.apache.org/licenses/LICENSE-2.0
- This is only used by Github actions: release.yaml workflow.
-"""
-__version__ = "1.0.0_b12"
-__previous_version__ = "1.0.0_b11"
--- jans-pycloudlib/CHANGES.md
+++
@@ -1,7 +0,0 @@
-# Changelog
-
-Here you can see an overview of changes between each release.
-
-## Version 1.0-dev
-
-Unreleased.
