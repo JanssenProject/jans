6be70c250f7427f219e7dc8d53fd56f946a2b4c5
Mustafa Baser <mbaser@mail.com>
2021-11-23 16:34:19 +0300

    fix: search scope

--- setup_app/installers/base.py
+++ setup_app/installers/base.py
@@ -92,9 +92,9 @@ class BaseInstaller:
 
 
     def check_scope(self, scope_id):
-        result = self.dbUtils.search('ou=scopes,o=jans', '(jansId={})'.format(scope_id))
-        if result:
-            return result['dn']
+        search_filter = '(&(objectClass=jansScope)(jansId={}))'.format(scope_id)
+        result = self.dbUtils.search('ou=scopes,o=jans', search_filter=search_filter)
+        return result.get('dn')
 
 
     def run_service_command(self, operation, service):
