07b77728824db6552f2ceb5922a656aa59e1389e
Puja Sharma <pujas.works@gmail.com>
2021-12-15 15:10:09 +0530

    feat: configapi shared lib

--- plugins/sample/helloworld/src/main/java/com/spl/plugin/helloworld/rest/HelloWorldResource.java
+++ plugins/sample/helloworld/src/main/java/com/spl/plugin/helloworld/rest/HelloWorldResource.java
@@ -3,13 +3,16 @@ package com.spl.plugin.helloworld.rest;
 import javax.inject.Inject;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 
-import com.spl.plugins.helloworld.util.Constants;
+import com.spl.plugin.helloworld.util.Constants;
 
 @Path("hello")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class HelloWorldResource {    
     
     @Inject
@@ -18,13 +21,12 @@ public class HelloWorldResource {
     @GET
     public Response sayHelloWorld() {
         return Response.ok("Hello World!").build();
-    }
-    
+    }    
     
     @GET
     @Path(Constants.NAME_PATH)
     public Response sayHelloWithName(@PathParam(Constants.NAME) @NotNull String name) {
-        return Response.ok("Hello World!").build();
+        return Response.ok("Hello World!"+name).build();
     }
 
     
