aa802998e10a89258bbf3492eada2a1c911ba73f
Puja Sharma <pujas.works@gmail.com>
2021-12-15 12:42:19 +0530

    feat: api shared lib

---
+++ shared/src/main/java/io/jans/configapi/core/service/ClientService.java
@@ -0,0 +1,92 @@
+/*
+ * Janssen Project software is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2020, Janssen Project
+ */
+
+package io.jans.configapi.core.service;
+
+import static io.jans.as.model.util.Util.escapeLog;
+import io.jans.as.common.model.registration.Client;
+import io.jans.as.common.service.OrganizationService;
+import io.jans.as.common.service.common.InumService;
+import io.jans.as.common.util.AttributeConstants;
+import io.jans.as.model.common.SubjectType;
+import io.jans.as.model.crypto.signature.SignatureAlgorithm;
+import io.jans.as.model.register.ApplicationType;
+import io.jans.configapi.rest.model.SearchRequest;
+import io.jans.orm.PersistenceEntryManager;
+import io.jans.orm.model.PagedResult;
+import io.jans.orm.model.SortOrder;
+import io.jans.orm.search.filter.Filter;
+import io.jans.util.StringHelper;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+
+@ApplicationScoped
+public class ClientService implements Serializable {
+
+    private static final long serialVersionUID = 7912416439116338984L;
+
+    @Inject
+    private PersistenceEntryManager persistenceEntryManager;
+
+    @Inject
+    private Logger logger;
+
+   // @Inject
+   // private OrgService orgService;
+
+    public boolean contains(String clientDn) {
+        return persistenceEntryManager.contains(clientDn, Client.class);
+    }
+
+    public Client getClientByInum(String inum) {
+        Client result = null;
+        try {
+            result = persistenceEntryManager.find(Client.class, getDnForClient(inum));
+           } catch (Exception ex) {
+            logger.error("Failed to load client entry", ex);
+        }
+        return result;
+    }
+
+    public Client getClientByDn(String dn) {
+        try {
+            return persistenceEntryManager.find(Client.class, dn);
+        } catch (Exception e) {
+            logger.warn("", e);
+            return null;
+        }
+    }
+    
+    public String getDnForClient(String inum) {
+        String orgDn = getDnForOrganization(null);
+        if (StringHelper.isEmpty(inum)) {
+            return String.format("ou=clients,%s", orgDn);
+        }
+        return String.format("inum=%s,ou=clients,%s", inum, orgDn);
+    }
+    
+    public String getDnForOrganization(String baseDn) {
+        if (baseDn == null) {
+            baseDn = "o=jans";
+        }
+        return baseDn;
+    }
+
+ /*   public String getDnForClient(String inum) {
+        String orgDn = orgService.getDnForOrganization();
+        if (StringHelper.isEmpty(inum)) {
+            return String.format("ou=clients,%s", orgDn);
+        }
+        return String.format("inum=%s,ou=clients,%s", inum, orgDn);
+    }*/
+
+}
---
+++ shared/src/main/java/io/jans/configapi/core/util/AuthUtil.java
@@ -0,0 +1,58 @@
+package io.jans.configapi.core.util;
+
+import io.jans.as.common.model.registration.Client;
+import io.jans.configapi.core.service.ConfService;
+import io.jans.configapi.core.service.ClientService;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.slf4j.Logger;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Response;
+
+@ApplicationScoped
+public class AuthUtil {
+
+    @Inject
+    Logger log;
+
+    @Inject
+    ConfService confService;
+
+    @Inject
+    ClientService clientService;
+
+    public String getOpenIdConfigurationEndpoint() {
+        return this.confService.find().getOpenIdConfigurationEndpoint();
+    }
+
+    public String getIssuer() {
+        return this.confService.find().getIssuer();
+    }
+
+    public Client getClient(String clientId) {
+        return clientService.getClientByInum(clientId);
+    }
+
+    public List<String> findMissingElements(List<String> list1, List<String> list2) {
+        List<String> unavailable = list1.stream().filter(e -> !list2.contains(e)).collect(Collectors.toList());
+        return unavailable;
+    }
+
+    public boolean isEqualCollection(List<String> list1, List<String> list2) {
+        return CollectionUtils.isEqualCollection(list1, list2);
+    }
+
+}
