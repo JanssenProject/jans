2fe7e167b7f8e0ece376dec624a4e280a763be01
Arnab Dutta <arnab.bdutta@gmail.com>
2021-12-27 11:08:35 +0530

    feat: Plugin logs management in config api using  Composite Configuration

---
+++ plugins/admin-ui-plugin/src/main/java/io/jans/ca/plugin/adminui/model/auth/UserInfoRequest.java
@@ -0,0 +1,28 @@
+package io.jans.ca.plugin.adminui.model.auth;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserInfoRequest {
+    private String code;
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
