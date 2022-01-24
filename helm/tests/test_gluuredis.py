a3c34a5a6380bcb110909f09edbbf6e55f67fc12
Mohammad Abudayyeh <47318409+moabu@users.noreply.github.com>
2022-01-24 03:41:38 -0500

    chore: remove helm py installation and move to flex (#676)

--- helm/tests/test_gluuredis.py
+++
@@ -1,9 +0,0 @@
-import pygluu.kubernetes.redis as module0
-from pygluu.kubernetes.redis import Redis
-
-
-def test_base_exception():
-    try:
-        var0 = module0.Redis()
-    except BaseException:
-        pass
