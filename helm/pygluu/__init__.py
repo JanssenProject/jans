32252cd0ebcd320bb55bd1bb2d8356515e100511
Mohammad Abudayyeh <47318409+moabu@users.noreply.github.com>
2022-01-24 03:41:38 -0500

    chore: remove helm py installation and move to flex (#676)

--- helm/pygluu/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
- License terms and conditions for Gluu Cloud Native Edition:
- https://www.apache.org/licenses/LICENSE-2.0
-"""
-
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
