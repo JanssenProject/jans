594de1b79f5e093667e3559bac5b18507cf760d5
Arnab Dutta <arnab.bdutta@gmail.com>
2022-03-10 18:14:07 +0530

    fix:remove deprecated SubTypesScanner as reflection code not working #1006

--- jans-orm/standalone/src/main/java/io/jans/orm/service/StandalonePersistanceFactoryService.java
+++ jans-orm/standalone/src/main/java/io/jans/orm/service/StandalonePersistanceFactoryService.java
@@ -27,69 +27,69 @@ import static org.reflections.scanners.Scanners.SubTypes;
  */
 public class StandalonePersistanceFactoryService extends PersistanceFactoryService {
 
-    private HashMap<String, PersistenceEntryManagerFactory> persistenceEntryManagerFactoryNames;
-    private HashMap<Class<? extends PersistenceEntryManagerFactory>, PersistenceEntryManagerFactory> persistenceEntryManagerFactoryTypes;
+	private HashMap<String, PersistenceEntryManagerFactory> persistenceEntryManagerFactoryNames;
+	private HashMap<Class<? extends PersistenceEntryManagerFactory>, PersistenceEntryManagerFactory> persistenceEntryManagerFactoryTypes;
 
-    @Override
-    public PersistenceEntryManagerFactory getPersistenceEntryManagerFactory(PersistenceConfiguration persistenceConfiguration) {
+	@Override
+	public PersistenceEntryManagerFactory getPersistenceEntryManagerFactory(PersistenceConfiguration persistenceConfiguration) {
         return getPersistenceEntryManagerFactory(persistenceConfiguration.getEntryManagerFactoryType());
     }
 
-    @Override
-    public PersistenceEntryManagerFactory getPersistenceEntryManagerFactory(Class<? extends PersistenceEntryManagerFactory> persistenceEntryManagerFactoryClass) {
-        if (this.persistenceEntryManagerFactoryTypes == null) {
-            initPersistenceManagerMaps();
-        }
+	@Override
+	public PersistenceEntryManagerFactory getPersistenceEntryManagerFactory(Class<? extends PersistenceEntryManagerFactory> persistenceEntryManagerFactoryClass) {
+		if (this.persistenceEntryManagerFactoryTypes == null) {
+			initPersistenceManagerMaps();
+		}
 
-        PersistenceEntryManagerFactory persistenceEntryManagerFactory = this.persistenceEntryManagerFactoryTypes
-                .get(persistenceEntryManagerFactoryClass);
+		PersistenceEntryManagerFactory persistenceEntryManagerFactory = this.persistenceEntryManagerFactoryTypes
+				.get(persistenceEntryManagerFactoryClass);
 
-        return persistenceEntryManagerFactory;
-    }
+		return persistenceEntryManagerFactory;
+	}
 
-    @Override
-    public PersistenceEntryManagerFactory getPersistenceEntryManagerFactory(String persistenceType) {
-        if (this.persistenceEntryManagerFactoryNames == null) {
-            initPersistenceManagerMaps();
-        }
+	@Override
+	public PersistenceEntryManagerFactory getPersistenceEntryManagerFactory(String persistenceType) {
+		if (this.persistenceEntryManagerFactoryNames == null) {
+			initPersistenceManagerMaps();
+		}
 
-        PersistenceEntryManagerFactory persistenceEntryManagerFactory = this.persistenceEntryManagerFactoryNames.get(persistenceType);
+		PersistenceEntryManagerFactory persistenceEntryManagerFactory = this.persistenceEntryManagerFactoryNames.get(persistenceType);
 
-        return persistenceEntryManagerFactory;
-    }
+		return persistenceEntryManagerFactory;
+	}
 
-    private void initPersistenceManagerMaps() {
-        this.persistenceEntryManagerFactoryNames = new HashMap<String, PersistenceEntryManagerFactory>();
-        this.persistenceEntryManagerFactoryTypes = new HashMap<Class<? extends PersistenceEntryManagerFactory>, PersistenceEntryManagerFactory>();
+	private void initPersistenceManagerMaps() {
+		this.persistenceEntryManagerFactoryNames = new HashMap<String, PersistenceEntryManagerFactory>();
+		this.persistenceEntryManagerFactoryTypes = new HashMap<Class<? extends PersistenceEntryManagerFactory>, PersistenceEntryManagerFactory>();
 
-        org.reflections.Reflections reflections = new org.reflections.Reflections(new org.reflections.util.ConfigurationBuilder()
-                .setUrls(org.reflections.util.ClasspathHelper.forPackage("io.jans.orm"))
-                .setScanners(SubTypes));
-        Set<Class<? extends PersistenceEntryManagerFactory>> classes = reflections.getSubTypesOf(PersistenceEntryManagerFactory.class);
+		org.reflections.Reflections reflections = new org.reflections.Reflections(new org.reflections.util.ConfigurationBuilder()
+			     .setUrls(org.reflections.util.ClasspathHelper.forPackage("io.jans.orm"))
+                 .setScanners(SubTypes));
+		Set<Class<? extends PersistenceEntryManagerFactory>> classes = reflections.getSubTypesOf(PersistenceEntryManagerFactory.class);
 
-        getLog().info("Found '{}' PersistenceEntryManagerFactory", classes.size());
+		getLog().info("Found '{}' PersistenceEntryManagerFactory", classes.size());
 
-        List<Class<? extends PersistenceEntryManagerFactory>> classesList = new ArrayList<Class<? extends PersistenceEntryManagerFactory>>(classes);
-        for (Class<? extends PersistenceEntryManagerFactory> clazz : classesList) {
-            getLog().info("Found PersistenceEntryManagerFactory '{}'", clazz);
-            PersistenceEntryManagerFactory persistenceEntryManagerFactory = createPersistenceEntryManagerFactoryImpl(clazz);
-            persistenceEntryManagerFactoryNames.put(persistenceEntryManagerFactory.getPersistenceType(), persistenceEntryManagerFactory);
-            persistenceEntryManagerFactoryTypes.put(clazz, persistenceEntryManagerFactory);
-        }
-    }
+		List<Class<? extends PersistenceEntryManagerFactory>> classesList = new ArrayList<Class<? extends PersistenceEntryManagerFactory>>(classes);
+		for (Class<? extends PersistenceEntryManagerFactory> clazz : classesList) {
+			getLog().info("Found PersistenceEntryManagerFactory '{}'", clazz);
+			PersistenceEntryManagerFactory persistenceEntryManagerFactory = createPersistenceEntryManagerFactoryImpl(clazz);
+			persistenceEntryManagerFactoryNames.put(persistenceEntryManagerFactory.getPersistenceType(), persistenceEntryManagerFactory);
+			persistenceEntryManagerFactoryTypes.put(clazz, persistenceEntryManagerFactory);
+		}
+	}
 
-    private PersistenceEntryManagerFactory createPersistenceEntryManagerFactoryImpl(Class<? extends PersistenceEntryManagerFactory> persistenceEntryManagerFactoryClass) {
-        PersistenceEntryManagerFactory persistenceEntryManagerFactory;
-        try {
-            persistenceEntryManagerFactory = ReflectHelper.createObjectByDefaultConstructor(persistenceEntryManagerFactoryClass);
-            persistenceEntryManagerFactory.initStandalone(this);
-        } catch (PropertyNotFoundException | IllegalArgumentException | InstantiationException | IllegalAccessException
-                | InvocationTargetException e) {
+	private PersistenceEntryManagerFactory createPersistenceEntryManagerFactoryImpl(Class<? extends PersistenceEntryManagerFactory> persistenceEntryManagerFactoryClass) {
+		PersistenceEntryManagerFactory persistenceEntryManagerFactory;
+		try {
+			persistenceEntryManagerFactory = ReflectHelper.createObjectByDefaultConstructor(persistenceEntryManagerFactoryClass);
+			persistenceEntryManagerFactory.initStandalone(this);
+		} catch (PropertyNotFoundException | IllegalArgumentException | InstantiationException | IllegalAccessException
+				| InvocationTargetException e) {
             throw new ConfigurationException(
                     String.format("Failed to create PersistenceEntryManagerFactory by type '%s'!", persistenceEntryManagerFactoryClass));
-        }
+		}
 
-        return persistenceEntryManagerFactory;
-    }
+		return persistenceEntryManagerFactory;
+	}
 
 }
