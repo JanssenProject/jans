0a77a81a4cc90966cfdb48dd850461c399ef2a37
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-03-15 12:42:45 -0300

    feat(jans-auth-server): force signed request object
    Signed-off-by: Javier Rojas Blum <javier.rojas.blum@gmail.com>

--- jans-auth-server/client/src/test/java/io/jans/as/client/ws/rs/ForceSignedRequestObjectTest.java
+++ jans-auth-server/client/src/test/java/io/jans/as/client/ws/rs/ForceSignedRequestObjectTest.java
@@ -8,13 +8,9 @@ package io.jans.as.client.ws.rs;
 
 import io.jans.as.client.*;
 import io.jans.as.client.client.AssertBuilder;
-import io.jans.as.client.client.Asserter;
 import io.jans.as.client.model.authorize.Claim;
 import io.jans.as.client.model.authorize.ClaimValue;
 import io.jans.as.client.model.authorize.JwtAuthorizationRequest;
-import io.jans.as.model.common.AuthenticationMethod;
-import io.jans.as.model.common.GrantType;
-import io.jans.as.model.common.Prompt;
 import io.jans.as.model.common.ResponseType;
 import io.jans.as.model.crypto.AbstractCryptoProvider;
 import io.jans.as.model.crypto.AuthCryptoProvider;
@@ -29,13 +25,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-import static org.testng.Assert.*;
-
 /**
- * Functional tests for OpenID Request Object (HTTP)
- *
  * @author Javier Rojas Blum
- * @version February 12, 2019
+ * @version March 15, 2022
  */
 public class ForceSignedRequestObjectTest extends BaseTest {
 
@@ -573,175 +565,92 @@ public class ForceSignedRequestObjectTest extends BaseTest {
                 .check();
     }
 
-//    @Parameters({"userId", "userSecret", "redirectUris", "redirectUri", "sectorIdentifierUri"})
-//    @Test
-//    public void requestAuthorizationWithoutRequestObjectFail(
-//            final String userId, final String userSecret, final String redirectUris, final String redirectUri,
-//            final String sectorIdentifierUri) throws Exception {
-//        showTitle("requestAuthorizationWithoutRequestObjectFail");
-//
-//        List<ResponseType> responseTypes = Arrays.asList(
-//                ResponseType.CODE,
-//                ResponseType.ID_TOKEN);
-//        List<String> scopes = Arrays.asList("openid", "profile", "address", "email", "phone", "user_name");
-//
-//        // 1. Register client
-//        RegisterResponse registerResponse = registerClient(redirectUris, responseTypes, scopes, sectorIdentifierUri);
-//
-//        String clientId = registerResponse.getClientId();
-//        String clientSecret = registerResponse.getClientSecret();
-//
-//        // 2. Request authorization and receive the authorization code.
-//        String nonce = UUID.randomUUID().toString();
-//        String state = UUID.randomUUID().toString();
-//
-//        AuthorizationRequest authorizationRequest = new AuthorizationRequest(responseTypes, clientId, scopes, redirectUri, nonce);
-//        authorizationRequest.setState(state);
-//
-//        AuthorizeClient authorizeClient = new AuthorizeClient(authorizationEndpoint);
-//        authorizeClient.setRequest(authorizationRequest);
-//        AuthorizationResponse authorizationResponse = authorizeClient.exec();
-//
-//        AssertBuilder.authorizationResponse(authorizationResponse).check();
-//
-//        String scope = authorizationResponse.getScope();
-//        String authorizationCode = authorizationResponse.getCode();
-//        String idToken = authorizationResponse.getIdToken();
-//
-//        // 3. Request access token using the authorization code.
-//        TokenRequest tokenRequest = new TokenRequest(GrantType.AUTHORIZATION_CODE);
-//        tokenRequest.setCode(authorizationCode);
-//        tokenRequest.setRedirectUri(redirectUri);
-//        tokenRequest.setAuthUsername(clientId);
-//        tokenRequest.setAuthPassword(clientSecret);
-//        tokenRequest.setAuthenticationMethod(AuthenticationMethod.CLIENT_SECRET_BASIC);
-//
-//        TokenClient tokenClient1 = newTokenClient(tokenRequest);
-//        tokenClient1.setRequest(tokenRequest);
-//        TokenResponse tokenResponse1 = tokenClient1.exec();
-//
-//        showClient(tokenClient1);
-//        AssertBuilder.tokenResponse(tokenResponse1)
-//                .notNullRefreshToken()
-//                .check();
-//
-//        String refreshToken = tokenResponse1.getRefreshToken();
-//
-//        // 4. Validate id_token
-//        AssertBuilder.jwtParse(idToken)
-//                .validateSignatureRSAClientEngine(jwksUri, SignatureAlgorithm.RS256)
-//                .claimsPresence(JwtClaimName.CODE_HASH)
-//                .notNullAuthenticationTime()
-//                .notNullOxOpenIDConnectVersion()
-//                .notNullAuthenticationContextClassReference()
-//                .notNullAuthenticationMethodReferences()
-//                .check();
-//
-//        // 5. Request new access token using the refresh token.
-//        TokenClient tokenClient2 = new TokenClient(tokenEndpoint);
-//        tokenClient2.setExecutor(clientEngine(true));
-//        TokenResponse tokenResponse2 = tokenClient2.execRefreshToken(scope, refreshToken, clientId, clientSecret);
-//
-//        showClient(tokenClient2);
-//        AssertBuilder.tokenResponse(tokenResponse2)
-//                .notNullRefreshToken()
-//                .notNullScope()
-//                .check();
-//        String accessToken = tokenResponse2.getAccessToken();
-//
-//        // 6. Request user info
-//        UserInfoClient userInfoClient = new UserInfoClient(userInfoEndpoint);
-//        userInfoClient.setExecutor(clientEngine(true));
-//        UserInfoResponse userInfoResponse = userInfoClient.execUserInfo(accessToken);
-//
-//        showClient(userInfoClient);
-//        AssertBuilder.userInfoResponse(userInfoResponse)
-//                .notNullClaimsPersonalData()
-//                .claimsPresence(JwtClaimName.EMAIL, JwtClaimName.BIRTHDATE, JwtClaimName.GENDER, JwtClaimName.MIDDLE_NAME)
-//                .claimsPresence(JwtClaimName.NICKNAME, JwtClaimName.PREFERRED_USERNAME, JwtClaimName.PROFILE)
-//                .claimsPresence(JwtClaimName.WEBSITE, JwtClaimName.EMAIL_VERIFIED, JwtClaimName.PHONE_NUMBER)
-//                .claimsPresence(JwtClaimName.PHONE_NUMBER_VERIFIED, JwtClaimName.ADDRESS, JwtClaimName.USER_NAME)
-//                .claimsNoPresence("org_name", "work_phone")
-//                .check();
-//    }
-//
-//    @Parameters({"userId", "userSecret", "redirectUri", "redirectUris", "sectorIdentifierUri"})
-//    @Test
-//    public void requestParameterMethodAlgNoneFail(
-//            final String userId, final String userSecret, final String redirectUri, final String redirectUris,
-//            final String sectorIdentifierUri) {
-//        try {
-//            showTitle("requestParameterMethodAlgNoneFail");
-//
-//            List<ResponseType> responseTypes = Arrays.asList(ResponseType.TOKEN, ResponseType.ID_TOKEN);
-//
-//            // 1. Dynamic Client Registration
-//            RegisterRequest registerRequest = new RegisterRequest(ApplicationType.WEB, "jans test app",
-//                    StringUtils.spaceSeparatedToList(redirectUris));
-//            registerRequest.setResponseTypes(responseTypes);
-//            registerRequest.setRequestObjectSigningAlg(SignatureAlgorithm.NONE);
-//            registerRequest.addCustomAttribute("jansTrustedClnt", "true");
-//            registerRequest.setSectorIdentifierUri(sectorIdentifierUri);
-//
-//            RegisterClient registerClient = new RegisterClient(registrationEndpoint);
-//            registerClient.setRequest(registerRequest);
-//            RegisterResponse response = registerClient.exec();
-//
-//            showClient(registerClient);
-//            AssertBuilder.registerResponse(response).created().check();
-//
-//            String clientId = response.getClientId();
-//
-//            // 2. Request authorization
-//            AbstractCryptoProvider cryptoProvider = createCryptoProviderWithAllowedNone();
-//
-//            List<String> scopes = Arrays.asList("openid", "profile", "address", "email");
-//            String nonce = UUID.randomUUID().toString();
-//            String state = UUID.randomUUID().toString();
-//
-//            AuthorizationRequest request = new AuthorizationRequest(responseTypes, clientId, scopes, redirectUri, nonce);
-//            request.setState(state);
-//            request.setAuthUsername(userId);
-//            request.setAuthPassword(userSecret);
-//            request.getPrompts().add(Prompt.NONE);
-//
-//            JwtAuthorizationRequest jwtAuthorizationRequest = new JwtAuthorizationRequest(request, SignatureAlgorithm.NONE, cryptoProvider);
-//            jwtAuthorizationRequest.addUserInfoClaim(new Claim(JwtClaimName.NAME, ClaimValue.createNull()));
-//            jwtAuthorizationRequest.addUserInfoClaim(new Claim(JwtClaimName.NICKNAME, ClaimValue.createEssential(false)));
-//            jwtAuthorizationRequest.addUserInfoClaim(new Claim(JwtClaimName.EMAIL, ClaimValue.createNull()));
-//            jwtAuthorizationRequest.addUserInfoClaim(new Claim(JwtClaimName.EMAIL_VERIFIED, ClaimValue.createNull()));
-//            jwtAuthorizationRequest.addUserInfoClaim(new Claim(JwtClaimName.PICTURE, ClaimValue.createEssential(false)));
-//            jwtAuthorizationRequest.addIdTokenClaim(new Claim(JwtClaimName.AUTHENTICATION_TIME, ClaimValue.createNull()));
-//            jwtAuthorizationRequest.addIdTokenClaim(new Claim(JwtClaimName.AUTHENTICATION_CONTEXT_CLASS_REFERENCE, ClaimValue.createValueList(new String[]{ACR_VALUE})));
-//            jwtAuthorizationRequest.getIdTokenMember().setMaxAge(86400);
-//            String authJwt = jwtAuthorizationRequest.getEncodedJwt();
-//            request.setRequest(authJwt);
-//
-//            AuthorizeClient authorizeClient = new AuthorizeClient(authorizationEndpoint);
-//            authorizeClient.setRequest(request);
-//            AuthorizationResponse response1 = authorizeClient.exec();
-//
-//            showClient(authorizeClient);
-//            assertEquals(response1.getStatus(), 302, "Unexpected response code: " + response1.getStatus());
-//            assertNotNull(response1.getLocation(), "The location is null");
-//            assertNotNull(response1.getAccessToken(), "The accessToken is null");
-//            assertNotNull(response1.getTokenType(), "The tokenType is null");
-//            assertNotNull(response1.getIdToken(), "The idToken is null");
-//            assertNotNull(response1.getState(), "The state is null");
-//
-//            String accessToken = response1.getAccessToken();
-//
-//            // 3. Request user info
-//            UserInfoClient userInfoClient = new UserInfoClient(userInfoEndpoint);
-//            UserInfoResponse response3 = userInfoClient.execUserInfo(accessToken);
-//
-//            showClient(userInfoClient);
-//            AssertBuilder.userInfoResponse(response3)
-//                    .notNullClaimsPersonalData()
-//                    .claimsPresence(JwtClaimName.EMAIL, JwtClaimName.ADDRESS)
-//                    .check();
-//        } catch (Exception e) {
-//            fail(e.getMessage(), e);
-//        }
-//    }
+    @Parameters({"redirectUris", "redirectUri", "sectorIdentifierUri"})
+    @Test(enabled = false) // Set forceSignedRequestObject to true to run this test
+    public void requestAuthorizationWithoutRequestObjectFail(
+            final String redirectUris, final String redirectUri, final String sectorIdentifierUri) {
+        showTitle("requestAuthorizationWithoutRequestObjectFail");
+
+        List<ResponseType> responseTypes = Arrays.asList(
+                ResponseType.CODE,
+                ResponseType.ID_TOKEN);
+        List<String> scopes = Arrays.asList("openid", "profile", "address", "email");
+
+        // 1. Register client
+        RegisterResponse registerResponse = registerClient(redirectUris, responseTypes, scopes, sectorIdentifierUri);
+
+        String clientId = registerResponse.getClientId();
+
+        // 2. Request authorization and receive the authorization code.
+        String nonce = UUID.randomUUID().toString();
+        String state = UUID.randomUUID().toString();
+
+        AuthorizationRequest authorizationRequest = new AuthorizationRequest(responseTypes, clientId, scopes, redirectUri, nonce);
+        authorizationRequest.setState(state);
+
+        AuthorizeClient authorizeClient = new AuthorizeClient(authorizationEndpoint);
+        authorizeClient.setRequest(authorizationRequest);
+        AuthorizationResponse authorizationResponse = authorizeClient.exec();
+
+        showClient(authorizeClient);
+        AssertBuilder.authorizationResponse(authorizationResponse)
+                .status(302)
+                .check();
+    }
+
+    @Parameters({"redirectUri", "redirectUris", "sectorIdentifierUri"})
+    @Test(enabled = false) // Set forceSignedRequestObject to true to run this test
+    public void requestParameterMethodAlgNoneFail(
+            final String redirectUri, final String redirectUris, final String sectorIdentifierUri) throws Exception {
+        showTitle("requestParameterMethodAlgNoneFail");
+
+        List<ResponseType> responseTypes = Arrays.asList(ResponseType.TOKEN, ResponseType.ID_TOKEN);
+
+        // 1. Dynamic Client Registration
+        RegisterRequest registerRequest = new RegisterRequest(ApplicationType.WEB, "jans test app",
+                StringUtils.spaceSeparatedToList(redirectUris));
+        registerRequest.setResponseTypes(responseTypes);
+        registerRequest.setRequestObjectSigningAlg(SignatureAlgorithm.NONE);
+        registerRequest.setSectorIdentifierUri(sectorIdentifierUri);
+
+        RegisterClient registerClient = new RegisterClient(registrationEndpoint);
+        registerClient.setRequest(registerRequest);
+        RegisterResponse response = registerClient.exec();
+
+        showClient(registerClient);
+        AssertBuilder.registerResponse(response).created().check();
+
+        String clientId = response.getClientId();
+
+        // 2. Request authorization
+        AbstractCryptoProvider cryptoProvider = createCryptoProviderWithAllowedNone();
+
+        List<String> scopes = Arrays.asList("openid", "profile", "address", "email");
+        String nonce = UUID.randomUUID().toString();
+        String state = UUID.randomUUID().toString();
+
+        AuthorizationRequest request = new AuthorizationRequest(responseTypes, clientId, scopes, redirectUri, nonce);
+        request.setState(state);
+
+        JwtAuthorizationRequest jwtAuthorizationRequest = new JwtAuthorizationRequest(request, SignatureAlgorithm.NONE, cryptoProvider);
+        jwtAuthorizationRequest.addUserInfoClaim(new Claim(JwtClaimName.NAME, ClaimValue.createNull()));
+        jwtAuthorizationRequest.addUserInfoClaim(new Claim(JwtClaimName.NICKNAME, ClaimValue.createEssential(false)));
+        jwtAuthorizationRequest.addUserInfoClaim(new Claim(JwtClaimName.EMAIL, ClaimValue.createNull()));
+        jwtAuthorizationRequest.addUserInfoClaim(new Claim(JwtClaimName.EMAIL_VERIFIED, ClaimValue.createNull()));
+        jwtAuthorizationRequest.addUserInfoClaim(new Claim(JwtClaimName.PICTURE, ClaimValue.createEssential(false)));
+        jwtAuthorizationRequest.addIdTokenClaim(new Claim(JwtClaimName.AUTHENTICATION_TIME, ClaimValue.createNull()));
+        jwtAuthorizationRequest.addIdTokenClaim(new Claim(JwtClaimName.AUTHENTICATION_CONTEXT_CLASS_REFERENCE, ClaimValue.createValueList(new String[]{ACR_VALUE})));
+        jwtAuthorizationRequest.getIdTokenMember().setMaxAge(86400);
+        String authJwt = jwtAuthorizationRequest.getEncodedJwt();
+        request.setRequest(authJwt);
+
+        AuthorizeClient authorizeClient = new AuthorizeClient(authorizationEndpoint);
+        authorizeClient.setRequest(request);
+        AuthorizationResponse authorizationResponse = authorizeClient.exec();
+
+        showClient(authorizeClient);
+        AssertBuilder.authorizationResponse(authorizationResponse)
+                .status(302)
+                .check();
+    }
 }
