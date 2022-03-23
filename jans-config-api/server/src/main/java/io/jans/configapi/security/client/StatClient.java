8dd81c3c59ed69b96539ea31256dab5d81c827ff
Puja Sharma <pujas.works@gmail.com>
2022-03-23 21:48:10 +0530

    feat(jans-config-api): jetty upgrade changes

--- jans-config-api/server/src/main/java/io/jans/configapi/security/client/StatClient.java
+++ jans-config-api/server/src/main/java/io/jans/configapi/security/client/StatClient.java
@@ -1,15 +1,12 @@
 package io.jans.configapi.security.client;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient
-@ApplicationScoped
 public interface StatClient {
 
     @GET
