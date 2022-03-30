3f2a55294915be4fd71a4337e72c5ed97b6d9c2f
manojs1978 <manojsurya78@gmail.com>
2022-03-30 10:17:23 +0530

    setup file updated

--- automation/tests/setup.sh
+++ automation/tests/setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash -x
 ##!/usr/bin/expect  -d
 
+mkdir logs
+LOG_LOCATION=./logs
+exec > >(tee -i $LOG_LOCATION/install.log)
+exec 2>&1
 
 helpFunction()
 {
@@ -29,29 +33,29 @@ if [ -z ${HOST} ] && [ -z ${USERNAME} ] && [ -z ${PASSWD} ]
 	helpFunction
 fi 
 # Begin script in case all parameters are correct
-echo "$HOST"
-echo "${USERNAME}"
-echo "${PASSWD}"
+echo "your host is $HOST"
+echo "your username is ${USERNAME}"
+echo "your passwd is ${PASSWD}"
 
 #USER=(root root)
 
-
-ssh-keygen  -t rsa -P '' -f ~/.ssh/id_rsa &> /dev/null
 expect <<EOF
+spawn ssh-keygen  -t rsa -P '' -f ~/.ssh/id_rsa
+expect { "Overwrite (y/n)?" {send "y\r"} }
 spawn ssh-copy-id -f -i ${USERNAME}@${HOST}
 expect {
   "yes/no" { send "yes\n";exp_continue }
-  "password" { send "${PASSWD}\n" }
+  "password" { send "${PASSWD}\r" }
 }
 expect eof
 EOF
 
-HOST_OS=`ssh ${USERNAME}@${HOST} 'cat /etc/os-release | grep PRETTY_NAME | cut -d" " -f-1,2 | cut -d"\"" -f2,3'`
+HOST_OS=`ssh ${USERNAME}@${HOST} 'cat /etc/os-release | grep PRETTY_NAME | cut -d" " -f-1,2 | cut -d"\"" -f2,3'` 
 echo $HOST_OS
-OSTYPE=`echo $HOST_OS |cut -d" " -f1`
+OSTYPE=`echo $HOST_OS |cut -d" " -f1` 
 echo $OSTYPE
 
-OSVERSION=`echo $HOST_OS |cut -d" " -f2`
+OSVERSION=`echo $HOST_OS |cut -d" " -f2` 
 
 echo $OSVERSION
 
@@ -65,12 +69,12 @@ echo $OSVERSION
 	
 				echo " login to Ubuntu Host"
 				
-				./install.exp ${HOST} ${USERNAME}
+				./install.exp ${HOST} ${USERNAME} 
 				break;;
 			Suse)
 				echo "Login to Suse Host"
 				
-				./install.exp ${HOST} ${USERNAME}
+				./install.exp ${HOST} ${USERNAME} 2>&1 >log/installation.log
                                 break;;
 
 
@@ -78,8 +82,9 @@ echo $OSVERSION
 				echo "Login to RHEL8 Host"
 				
 
-				./install.exp ${HOST} ${USERNAME}
+				./install.exp ${HOST} ${USERNAME} 2>&1 >log/installation.log
                                 break;;
 
 		esac
+	
 	done
