fc919ea424dccacda5e4dd238005630402cef060
Puja Sharma <pujas.works@gmail.com>
2022-03-30 15:36:13 +0530

    feat(jans-orm): orm jetty upgrade changed

--- jans-orm/spanner-sample/src/test/java/io/jans/cloud/spanner/test/SessionId.java
+++ jans-orm/spanner-sample/src/test/java/io/jans/cloud/spanner/test/SessionId.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import java.util.UUID;
 
 import javax.annotation.Nonnull;
-import javax.persistence.Transient;
+import jakarta.persistence.Transient;
 
 import org.apache.commons.lang.StringUtils;
 
