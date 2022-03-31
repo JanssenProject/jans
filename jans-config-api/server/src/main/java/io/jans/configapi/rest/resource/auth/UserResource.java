f386b5071a47fcb4bfa5b6a34df6064ebb00b0a0
Puja Sharma <pujas.works@gmail.com>
2022-03-31 19:25:29 +0530

    feat(jans-config-api): user mgmt endpoint

--- jans-config-api/server/src/main/java/io/jans/configapi/rest/resource/auth/UserResource.java
+++ jans-config-api/server/src/main/java/io/jans/configapi/rest/resource/auth/UserResource.java
@@ -8,23 +8,20 @@ package io.jans.configapi.rest.resource.auth;
 
 import com.github.fge.jsonpatch.JsonPatchException;
 import static io.jans.as.model.util.Util.escapeLog;
-import  io.jans.as.common.model.common.User;
-//import io.jans.configapi.rest.model.user.User;
-import io.jans.as.common.service.common.EncryptionService;
-import io.jans.as.common.service.common.InumService;
+import io.jans.as.common.model.common.User;
 import io.jans.configapi.core.rest.ProtectedApi;
 import io.jans.configapi.rest.model.SearchRequest;
 import io.jans.configapi.service.auth.UserService;
-import io.jans.configapi.service.auth.ConfigurationService;
 import io.jans.configapi.util.ApiAccessConstants;
 import io.jans.configapi.util.ApiConstants;
-import io.jans.configapi.util.AttributeNames;
 import io.jans.configapi.core.util.Jackson;
-import io.jans.orm.PersistenceEntryManager;
 import io.jans.orm.model.PagedResult;
 import io.jans.util.StringHelper;
 import io.jans.util.security.StringEncrypter.EncryptionException;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.validation.Valid;
@@ -32,20 +29,16 @@ import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+
 
 import org.slf4j.Logger;
-
 
 @Path(ApiConstants.USER)
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @ApplicationScoped
 public class UserResource extends BaseResource {
-    
+
     private static final String USER = "user";
 
     @Inject
@@ -53,15 +46,6 @@ public class UserResource extends BaseResource {
 
     @Inject
     UserService userSrv;
-    
-    @Inject
-    ConfigurationService configurationService;
-    
-    @Inject
-    private InumService inumService;
-
-    @Inject
-    EncryptionService encryptionService;
 
     @GET
     @ProtectedApi(scopes = { ApiAccessConstants.USER_READ_ACCESS })
@@ -76,107 +60,73 @@ public class UserResource extends BaseResource {
                     escapeLog(limit), escapeLog(pattern), escapeLog(startIndex), escapeLog(sortBy),
                     escapeLog(sortOrder));
         }
+        logger.error("User serach param - limit:{}, pattern:{}, startIndex:{}, sortBy:{}, sortOrder:{}",
+                escapeLog(limit), escapeLog(pattern), escapeLog(startIndex), escapeLog(sortBy), escapeLog(sortOrder));
 
-        SearchRequest searchReq = createSearchRequest(userSrv.getPeopleBaseDn(), pattern, sortBy, sortOrder,
-                startIndex, limit, null, null);
+        SearchRequest searchReq = createSearchRequest(userSrv.getPeopleBaseDn(), pattern, sortBy, sortOrder, startIndex,
+                limit, null, null);
 
         final List<User> users = this.doSearch(searchReq);
-        logger.trace("User serach result:{}", users);
-        return Response.ok(getUsers(users)).build();
+        logger.error("User serach result:{}", users);
+        return Response.ok(users).build();
     }
 
     @GET
-    @ProtectedApi(scopes = { ApiAccessConstants.OPENID_CLIENTS_READ_ACCESS })
+    @ProtectedApi(scopes = { ApiAccessConstants.USER_WRITE_ACCESS })
     @Path(ApiConstants.INUM_PATH)
     public Response getUserByInum(@PathParam(ApiConstants.INUM) @NotNull String inum) {
         if (logger.isDebugEnabled()) {
             logger.debug("User serach by inum:{}", escapeLog(inum));
         }
         User user = userSrv.getUserByInum(inum);
-        checkResourceNotNull(user, USER);
+        logger.error("Based on inum:{}, user:{}", inum, user);
         return Response.ok(user).build();
     }
 
-   /* @POST
-    @ProtectedApi(scopes = { ApiAccessConstants.OPENID_CLIENTS_WRITE_ACCESS })
+    @POST
+    @ProtectedApi(scopes = { ApiAccessConstants.USER_WRITE_ACCESS })
     public Response createOpenIdConnect(@Valid User user) throws EncryptionException {
         if (logger.isDebugEnabled()) {
             logger.debug("User details to be added - user:{}", escapeLog(user));
         }
-        String inum = user.getUserId();
-        if (inum == null || inum.isEmpty() || inum.isBlank()) {
-            inum = inumService.generateClientInum();
-            user.setUserId(inum);
-        }
-        checkNotNull(user., AttributeNames.DISPLAY_NAME);
-        String clientSecret = client.getClientSecret();
 
-        if (StringHelper.isEmpty(clientSecret)) {
-            clientSecret = generatePassword();
-        }
-
-        client.setClientSecret(encryptionService.encrypt(clientSecret));
-        client.setDn(userSrv.getDnForClient(inum));
-        client.setDeletable(client.getClientSecretExpiresAt() != null);
-        ignoreCustomObjectClassesForNonLDAP(client);       
-        
-        logger.debug("Final Client details to be added - client:{}", client);      
-        userSrv.addClient(user);
-        User result = userSrv.getUserByInum(inum);
-        result.setClientSecret(encryptionService.decrypt(result.getClientSecret()));
-
-        return Response.status(Response.Status.CREATED).entity(result).build();
+        user = userSrv.addUser(user, true);
+        logger.error("User created {}", user);
+        return Response.status(Response.Status.CREATED).entity(user).build();
     }
 
     @PUT
-    @ProtectedApi(scopes = { ApiAccessConstants.OPENID_CLIENTS_WRITE_ACCESS })
+    @ProtectedApi(scopes = { ApiAccessConstants.USER_WRITE_ACCESS })
     public Response updateUser(@Valid User user) throws EncryptionException {
         if (logger.isDebugEnabled()) {
             logger.debug("User details to be updated - user:{}", escapeLog(user));
-        }
-        String inum = client.getClientId();
-        checkNotNull(inum, AttributeNames.INUM);
-        checkNotNull(client.getClientName(), AttributeNames.DISPLAY_NAME);
-        Client existingClient = userSrv.getClientByInum(inum);
-        checkResourceNotNull(existingClient, USER);
-        client.setClientId(existingClient.getClientId());
-        client.setBaseDn(userSrv.getDnForClient(inum));
-        client.setDeletable(client.getExpirationDate() != null);
-        if (client.getClientSecret() != null) {
-            client.setClientSecret(encryptionService.encrypt(client.getClientSecret()));
-        }
-        ignoreCustomObjectClassesForNonLDAP(client);
-   
-        logger.debug("Final Client details to be updated - user:{}", user);      
-        userSrv.updateClient(client);
-        User result = userSrv.getClientByInum(existingClient.getClientId());
-        result.setClientSecret(encryptionService.decrypt(client.getClientSecret()));
+        }              
+        user = userSrv.updateUser(user);      
+        logger.debug("Updated user:{}", user);
 
-        return Response.ok(result).build();
+        return Response.ok(user).build();
     }
 
     @PATCH
     @Consumes(MediaType.APPLICATION_JSON_PATCH_JSON)
-    @ProtectedApi(scopes = { ApiAccessConstants.OPENID_CLIENTS_WRITE_ACCESS })
+    @ProtectedApi(scopes = { ApiAccessConstants.USER_WRITE_ACCESS })
     @Path(ApiConstants.INUM_PATH)
     public Response patchUser(@PathParam(ApiConstants.INUM) @NotNull String inum, @NotNull String pathString)
             throws JsonPatchException, IOException {
         if (logger.isDebugEnabled()) {
-            logger.debug("User details to be patched - inum:{}, pathString:{}", escapeLog(inum),
-                    escapeLog(pathString));
+            logger.debug("User details to be patched - inum:{}, pathString:{}", escapeLog(inum), escapeLog(pathString));
         }
-        User existingUser = userSrv.getClientByInum(inum);
+        User existingUser = userSrv.getUserByInum(inum);
         checkResourceNotNull(existingUser, USER);
 
         existingUser = Jackson.applyPatch(pathString, existingUser);
-        userSrv.updateClient(existingUser);
+        userSrv.updateUser(existingUser);
         return Response.ok(existingUser).build();
     }
 
-*/
     @DELETE
     @Path(ApiConstants.INUM_PATH)
-    @ProtectedApi(scopes = { ApiAccessConstants.OPENID_CLIENTS_DELETE_ACCESS })
+    @ProtectedApi(scopes = { ApiAccessConstants.USER_DELETE_ACCESS })
     public Response deleteUser(@PathParam(ApiConstants.INUM) @NotNull String inum) {
         if (logger.isDebugEnabled()) {
             logger.debug("User to be deleted - inum:{} ", escapeLog(inum));
@@ -185,19 +135,6 @@ public class UserResource extends BaseResource {
         checkResourceNotNull(user, USER);
         userSrv.removeUser(user);
         return Response.noContent().build();
-    }
-
-    private List<User> getUsers(List<User> users) throws EncryptionException {
-        if (users != null && !users.isEmpty()) {
-            for (User user : users) {
-                //user.setClientSecret(encryptionService.decrypt(user.));
-            }
-        }
-        return users;
-    }
-
-    private String generatePassword() {
-        return UUID.randomUUID().toString();
     }
 
     private List<User> doSearch(SearchRequest searchReq) {
@@ -220,6 +157,5 @@ public class UserResource extends BaseResource {
         }
         return users;
     }
-    
 
 }
