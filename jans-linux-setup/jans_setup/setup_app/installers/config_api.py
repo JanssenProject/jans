80f6bf5adcb5f5bb444aadb6b79364549e22c915
Mustafa Baser <mbaser@mail.com>
2022-04-26 04:15:21 +0300

    fix: jans-linux-setup url of config api scim plugin

--- jans-linux-setup/jans_setup/setup_app/installers/config_api.py
+++ jans-linux-setup/jans_setup/setup_app/installers/config_api.py
@@ -41,7 +41,7 @@ class ConfigApiInstaller(JettyInstaller):
 
         self.source_files = [
                 (os.path.join(Config.distJansFolder, 'jans-config-api.war'), os.path.join(Config.app_info['JANS_MAVEN'], 'maven/io/jans/jans-config-api-server/{0}/jans-config-api-server-{0}.war').format(Config.oxVersion)),
-                (os.path.join(Config.distJansFolder, 'scim-plugin.jar'), os.path.join(Config.app_info['JANS_MAVEN'], 'maven/io/jans/scim-plugin/{0}/scim-plugin-{0}-distribution.jar').format(Config.oxVersion)),
+                (os.path.join(Config.distJansFolder, 'scim-plugin.jar'), os.path.join(Config.app_info['JANS_MAVEN'], 'maven/io/jans/jans-config-api/plugins/scim-plugin/{0}/scim-plugin-{0}-distribution.jar').format(Config.oxVersion)),
                 (os.path.join(Config.distJansFolder, 'facter'), 'https://raw.githubusercontent.com/GluuFederation/gluu-snap/master/facter/facter'),
                 (os.path.join(Config.install_dir, 'jans_setup/data/jans-config-api-swagger.yaml'), 'https://raw.githubusercontent.com/JanssenProject/jans/main/jans-config-api/docs/jans-config-api-swagger.yaml'),
                 (os.path.join(Config.distJansFolder, 'user-mgt-plugin.jar'), os.path.join(Config.app_info['JANS_MAVEN'], 'maven/io/jans/jans-config-api/plugins/user-mgt-plugin/{0}/user-mgt-plugin-{0}-distribution.jar').format(Config.oxVersion)),
