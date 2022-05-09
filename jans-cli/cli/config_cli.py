2c1934a00e3f10b4c5d1a5544be8dc35981a8811
Mustafa Baser <mbaser@mail.com>
2022-05-09 15:57:30 +0300

    feat: jans-cli tabulate scopes

--- jans-cli/cli/config_cli.py
+++ jans-cli/cli/config_cli.py
@@ -44,6 +44,7 @@ tabulate_endpoints = {
     'jca.get-user': ['inum', 'userId', 'mail','sn', 'givenName', 'jansStatus'],
     'jca.get-attributes': ['inum', 'name', 'displayName', 'status', 'dataType', 'claimName'],
     'jca.get-oauth-openid-clients': ['inum', 'displayName', 'clientName', 'applicationType'],
+    'jca.get-oauth-scopes': ['inum', 'displayName', 'scopeType']
 }
 
 my_op_mode = 'scim' if 'scim' in os.path.basename(sys.argv[0]) else 'jca'
