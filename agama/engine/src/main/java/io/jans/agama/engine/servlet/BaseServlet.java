d784aab0bbb90c99c8bc792d80f5a2abe1c36675
Jose <bonustrack310@gmail.com>
2022-05-31 06:42:13 -0500

    chore: avoid carrying http request variable in servlet methods #1456 Former-commit-id: 9661e5c7b87e3a8b5dca08fc04b09425c744657f

--- agama/engine/src/main/java/io/jans/agama/engine/servlet/BaseServlet.java
+++ agama/engine/src/main/java/io/jans/agama/engine/servlet/BaseServlet.java
@@ -6,16 +6,22 @@ import io.jans.agama.engine.page.BasicTemplateModel;
 import io.jans.agama.engine.page.Page;
 import io.jans.agama.engine.service.TemplatingService;
 import io.jans.agama.model.EngineConfig;
+import io.jans.util.Pair;
 
 import jakarta.inject.Inject;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
 import java.io.IOException;
+import java.io.StringWriter;
+
+import org.slf4j.Logger;
 
 public abstract class BaseServlet extends HttpServlet {
+    
+    @Inject
+    protected Logger logger;
     
     @Inject
     protected FlowUtils flowUtils;
@@ -29,62 +35,88 @@ public abstract class BaseServlet extends HttpServlet {
     @Inject
     protected Page page;
 
-    protected boolean isJsonRequest(HttpServletRequest request) {
+    @Inject
+    protected HttpServletRequest request;
+
+    protected boolean isJsonRequest() {
         return MediaType.APPLICATION_JSON.equals(request.getContentType());
     }
-    
+
     protected void sendNotAvailable(HttpServletResponse response) throws IOException {
         response.sendError(HttpServletResponse.SC_SERVICE_UNAVAILABLE,
                 "Flow engine not available. Check if Agama has been enabled in your configuration.");
     }
 
-    protected void sendFlowTimeout(HttpServletResponse response, boolean jsonResponse, String message)
-            throws IOException {
+    protected void sendFlowTimeout(HttpServletResponse response, String message) throws IOException {
 
         String errorPage = engineConf.getInterruptionErrorPage();
-        page.setTemplatePath(jsonResponse ? engineConf.getJsonErrorPage(errorPage) : errorPage);
+        page.setTemplatePath(errorPath(errorPage));
         page.setDataModel(new BasicTemplateModel(message));
         sendPageContents(response);
 
     }
     
-    protected void sendFlowCrashed(HttpServletResponse response, boolean jsonResponse, String error)
-            throws IOException {
+    protected void sendFlowCrashed(HttpServletResponse response, String error) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 
         String errorPage = engineConf.getCrashErrorPage();
-        page.setTemplatePath(jsonResponse ? engineConf.getJsonErrorPage(errorPage) : errorPage);
+        page.setTemplatePath(errorPath(errorPage));
         page.setRawDataModel(new BasicTemplateModel(error));
         sendPageContents(response);
         
     }
     
-    protected void sendPageMismatch(HttpServletResponse response, boolean jsonResponse, String url)
+    protected void sendPageMismatch(HttpServletResponse response, String message, String flowQname)
             throws IOException {
-        
-        String errorPage = engineConf.getPageMismatchErrorPage();        
-        page.setTemplatePath(jsonResponse ? engineConf.getJsonErrorPage(errorPage) : errorPage);
-        page.setDataModel(new BasicTemplateModel(url));
+
+        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+        String errorPage = engineConf.getPageMismatchErrorPage();
+        page.setTemplatePath(errorPath(errorPage));
+        page.setDataModel(new BasicTemplateModel(message, flowQname));
         sendPageContents(response);
 
     }
 
     protected void sendPageContents(HttpServletResponse response) throws IOException {
-        processTemplate(page.getTemplatePath(), page.getDataModel(), response);        
-    }
-    
-    protected void processTemplate(String path, Object dataModel, HttpServletResponse response)
-            throws IOException {
-
+        
         try {
-            engineConf.getDefaultResponseHeaders().forEach((h, v) -> response.setHeader(h, v));
-            String mimeType = templatingService.process(path, dataModel, response.getWriter(), false);
-            if (mimeType != null) {
-                response.setHeader(HttpHeaders.CONTENT_TYPE, mimeType);
-            }
+            processTemplate(response, page.getTemplatePath(), page.getDataModel());
         } catch (TemplateProcessingException e) {
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+
+            try {
+                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                String templatePath = errorPath(engineConf.getCrashErrorPage());
+                processTemplate(response, templatePath, new BasicTemplateModel(e.getMessage()));
+
+            } catch (TemplateProcessingException e2) {
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e2.getMessage());
+            }
         }
 
     }
     
+    private String errorPath(String page) {
+        return isJsonRequest() ? engineConf.getJsonErrorPage(page) : page;
+    }
+    
+    private void processTemplate(HttpServletResponse response, String path, Object dataModel)
+            throws TemplateProcessingException, IOException {
+
+        StringWriter sw = new StringWriter();
+        Pair<String, String> contentType = templatingService.process(path, dataModel, sw, false);
+        
+        //encoding MUST be set before calling getWriter 
+        response.setCharacterEncoding(contentType.getSecond());
+        response.getWriter().write(sw.toString());
+
+        engineConf.getDefaultResponseHeaders().forEach((h, v) -> response.setHeader(h, v));
+        String mediaType = contentType.getFirst();
+        if (mediaType != null) {
+            response.setContentType(mediaType);
+        }
+
+    }
+
 }
