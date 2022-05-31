f87fdd2248b29936fea52b4e242a91f0780b8423
pujavs <43700552+pujavs@users.noreply.github.com>
2022-05-31 16:25:40 +0530

    feat(jans-config-api): enhancement to expose user inum at root level of response (#1477)
    Former-commit-id: 1e4b6bc9955a0cd91d6dff000a860ca96b6bd822

--- jans-config-api/plugins/user-mgt-plugin/src/main/java/io/jans/configapi/plugin/mgt/model/user/CustomUser.java
+++ jans-config-api/plugins/user-mgt-plugin/src/main/java/io/jans/configapi/plugin/mgt/model/user/CustomUser.java
@@ -9,46 +9,69 @@ public class CustomUser extends User {
  
     private static final long serialVersionUID = 1L;
 
+    private String inum;
     private String mail;
     private String displayName;
     private String jansStatus;
     private String givenName;
     private String userPassword;
     
+        
+    public String getInum() {
+        return inum;
+    }
+    
+    public void setInum(String inum) {
+        this.inum = inum;
+    }
+    
     public String getMail() {
         return mail;
     }
+    
     public void setMail(String mail) {
         this.mail = mail;
     }
+    
     public String getDisplayName() {
         return displayName;
     }
+    
     public void setDisplayName(String displayName) {
         this.displayName = displayName;
     }
+    
     public String getJansStatus() {
         return jansStatus;
     }
+    
     public void setJansStatus(String jansStatus) {
         this.jansStatus = jansStatus;
     }
+    
     public String getGivenName() {
         return givenName;
     }
+    
     public void setGivenName(String givenName) {
         this.givenName = givenName;
     }
+    
     public String getUserPassword() {
         return userPassword;
     }
+    
     public void setUserPassword(String userPassword) {
         this.userPassword = userPassword;
     }
+    
     @Override
     public String toString() {
-        return "CustomUser [mail=" + mail + ", displayName=" + displayName + ", jansStatus=" + jansStatus + ", givenName="
-                + givenName + ", userPassword= XXXXX ]";
+        return "CustomUser [inum=" + inum + ", mail=" + mail + ", displayName=" + displayName + ", jansStatus="
+                + jansStatus + ", givenName=" + givenName + ", userPassword=" + userPassword + "]";
     }
+    
+    
+    
     
 }
