4582ae563ddf2492c519fdbc7685af2ce3c5529d
Isman Firmansyah <iromli@users.noreply.github.com>
2022-06-14 01:53:55 +0700

    feat: add newly redesigned jans-client-api (#1540)

--- docker-jans-client-api/scripts/entrypoint.sh
+++ docker-jans-client-api/scripts/entrypoint.sh
@@ -5,12 +5,23 @@ set -e
 python3 /app/scripts/wait.py
 python3 /app/scripts/bootstrap.py
 
-# run the server
-# customized `/opt/client-api/bin/client-api-start.sh`
+# run jans-client-api
+cd /opt/jans/jetty/jans-client-api
+
+# shellcheck disable=SC2086
 exec java \
-    -Djava.net.preferIPv4Stack=true \
+    -server \
+    -XX:+DisableExplicitGC \
     -XX:+UseContainerSupport \
     -XX:MaxRAMPercentage=$CN_MAX_RAM_PERCENTAGE \
+    -Djava.net.preferIPv4Stack=true \
+    -Djans.base=/etc/jans \
+    -Dserver.base=/opt/jans/jetty/jans-client-api \
+    -Dlog.base=/opt/jans/jetty/jans-client-api \
+    -Djava.io.tmpdir=/tmp \
+    -Dpython.home=/opt/jython \
+    -Dlog4j2.configurationFile=resources/log4j2.xml \
     ${CN_JAVA_OPTIONS} \
-    -cp /opt/client-api/client-api.jar:/opt/client-api/lib/* \
-    io.jans.ca.server.RpServerApplication server /opt/client-api/conf/client-api-server.yml
+    -jar /opt/jetty/start.jar \
+        jetty.deploy.scanInterval=0 \
+        jetty.httpConfig.sendServerVersion=false
