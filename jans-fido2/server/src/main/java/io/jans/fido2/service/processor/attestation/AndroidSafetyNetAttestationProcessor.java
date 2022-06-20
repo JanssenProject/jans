95246d8610f42a88cfbcb192b94f9fc2775c6a2a
YuriyZ <yzabrovarniy@gmail.com>
2022-06-20 23:04:44 +0300

    chore(jans-auth-server): resolve cycle dependency between fido2 and jans-auth
    Moved some fido2 model classes to jans-orm-model.
    
    https://github.com/JanssenProject/jans/issues/1499
    
    Former-commit-id: 59a77e3a46314392bd0851022828f6e6dd390096

--- jans-fido2/server/src/main/java/io/jans/fido2/service/processor/attestation/AndroidSafetyNetAttestationProcessor.java
+++ jans-fido2/server/src/main/java/io/jans/fido2/service/processor/attestation/AndroidSafetyNetAttestationProcessor.java
@@ -35,7 +35,7 @@ import io.jans.fido2.google.safetynet.AttestationStatement;
 import io.jans.fido2.google.safetynet.OfflineVerify;
 import io.jans.fido2.model.auth.AuthData;
 import io.jans.fido2.model.auth.CredAndCounterData;
-import io.jans.fido2.model.entry.Fido2RegistrationData;
+import io.jans.orm.model.fido2.Fido2RegistrationData;
 import io.jans.fido2.service.Base64Service;
 import io.jans.fido2.service.mds.AttestationCertificateService;
 import io.jans.fido2.service.processors.AttestationFormatProcessor;
