b409633dca7fdd394421d105cf33b9210d29d38e
Puja Sharma <pujas.works@gmail.com>
2022-07-28 22:08:37 +0530

    feat(jans-config-api): scim config endpoint enhancment

--- jans-config-api/plugins/scim-plugin/src/main/java/io/jans/configapi/plugin/scim/rest/ScimConfigResource.java
+++ jans-config-api/plugins/scim-plugin/src/main/java/io/jans/configapi/plugin/scim/rest/ScimConfigResource.java
@@ -2,14 +2,13 @@ package io.jans.configapi.plugin.scim.rest;
 
 import com.github.fge.jsonpatch.JsonPatchException;
 
+import io.jans.scim.model.conf.Conf;
 import io.jans.configapi.core.rest.ProtectedApi;
 import io.jans.configapi.plugin.scim.service.ScimConfigService;
-import io.jans.configapi.plugin.scim.model.config.ScimAppConfiguration;
-import io.jans.configapi.plugin.scim.model.config.ScimConf;
+import io.jans.scim.model.conf.AppConfiguration;
 import io.jans.configapi.core.util.Jackson;
 import io.jans.configapi.plugin.scim.util.Constants;
-
-import org.slf4j.Logger;
+import io.jans.configapi.util.ApiAccessConstants;
 
 import java.io.IOException;
 import jakarta.inject.Inject;
@@ -17,6 +16,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import org.slf4j.Logger;
 
 @Path(Constants.SCIM_CONFIG)
 @Produces(MediaType.APPLICATION_JSON)
@@ -32,29 +33,49 @@ public class ScimConfigResource {
     @GET
     @ProtectedApi(scopes = { "https://jans.io/scim/config.readonly" })
     public Response getAppConfiguration() {
-        ScimAppConfiguration appConfiguration = scimConfigService.find();
+        AppConfiguration appConfiguration = scimConfigService.find();
         log.debug("SCIM appConfiguration:{}", appConfiguration);
         return Response.ok(appConfiguration).build();
     }
-    
+
+    @PATCH
+    @Consumes(MediaType.APPLICATION_JSON_PATCH_JSON)
+    @ProtectedApi(scopes = { ApiAccessConstants.JANS_AUTH_CONFIG_WRITE_ACCESS })
+    public Response patchAppConfigurationProperty(@NotNull String requestString) throws JsonPatchException , IOException{
+        log.debug("AUTH CONF details to patch - requestString:{} ", requestString);
+       
+        Conf conf = scimConfigService.findConf();
+        AppConfiguration appConfiguration = scimConfigService.find();
+        log.debug("AUTH CONF details BEFORE patch - appConfiguration :{}", appConfiguration);
+        appConfiguration = Jackson.applyPatch(requestString, conf.getDynamicConf());
+        log.debug("AUTH CONF details BEFORE patch merge - appConfiguration:{}", appConfiguration);
+        conf.setDynamicConf(appConfiguration);
+        
+        
+        scimConfigService.merge(conf);
+        appConfiguration = scimConfigService.find();
+        log.debug("AUTH CONF details AFTER patch merge - appConfiguration:{}", appConfiguration);
+        return Response.ok(appConfiguration).build();
+    }
+    /*
     @PATCH
     @Consumes(MediaType.APPLICATION_JSON_PATCH_JSON)
     @ProtectedApi(scopes = { "https://jans.io/scim/config.write" })
     public Response patchAppConfigurationProperty(@NotNull String requestString) throws IOException,JsonPatchException {
         log.debug("AUTH CONF details to patch - requestString:{}", requestString);
-        ScimConf conf = scimConfigService.findConf();
-        ScimAppConfiguration appConfiguration = conf.getDynamicConf();
-        log.trace("AUTH CONF details BEFORE patch - conf:{}, appConfiguration:{}", conf, appConfiguration);
+        AppConfiguration appConfiguration = scimConfigService.find();
+        //AppConfiguration appConfiguration = conf.getDynamicConf();
+        log.trace("AUTH CONF details BEFORE patch - appConfiguration:{}", appConfiguration);
         
         appConfiguration = Jackson.applyPatch(requestString, appConfiguration);
         log.trace("AUTH CONF details BEFORE patch merge - appConfiguration:{}" ,appConfiguration);
-        conf.setDynamicConf(appConfiguration);
+        //conf.setDynamicConf(appConfiguration);
 
         scimConfigService.merge(conf);
         appConfiguration = scimConfigService.find();
         log.debug("AUTH CONF details AFTER patch merge - appConfiguration:{}", appConfiguration);
         return Response.ok(appConfiguration).build();
     }
-
+*/
     
 }
