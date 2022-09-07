bf273dd7de99d17a25d236730aff81acbce3a0b9
Puja Sharma <pujas.works@gmail.com>
2022-09-07 14:53:45 +0530

    feat(config-api): auto generation of swagger spec

--- jans-config-api/plugins/user-mgt-plugin/src/main/java/io/jans/configapi/plugin/mgt/rest/ApiApplication.java
+++ jans-config-api/plugins/user-mgt-plugin/src/main/java/io/jans/configapi/plugin/mgt/rest/ApiApplication.java
@@ -1,11 +1,36 @@
-	package io.jans.configapi.plugin.mgt.rest;
+package io.jans.configapi.plugin.mgt.rest;
 
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @ApplicationPath("/mgt")
+@OpenAPIDefinition(info = @Info(title = "Jans Config API - User-Mgt", version = "1.0", contact = @Contact(name = "Gluu Support", url = "https://support.gluu.org", email = "xxx@gluu.org"),
+
+license = @License(name = "Apache 2.0", url = "https://github.com/JanssenProject/jans/blob/main/LICENSE")),
+
+tags = { @Tag(name = "Configuration – User Management") },
+
+servers = { @Server(url = "https://jans.io/", description = "The Jans server") })
+
+@SecurityScheme(name = "oauth2", type = SecuritySchemeType.OAUTH2, flows = @OAuthFlows(clientCredentials = @OAuthFlow(tokenUrl = "https://{op-hostname}/.../token", scopes = {
+@OAuthScope(name = "https://jans.io/oauth/config/user.readonly", description = "View user related information"),
+@OAuthScope(name = "https://jans.io/oauth/config/user.write", description = "Manage user related information"),
+@OAuthScope(name = "https://jans.io/oauth/config/user.delete", description = "Delete user related information")}
+)))
 public class ApiApplication extends Application {
 
     @Override
