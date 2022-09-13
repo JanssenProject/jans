d0f2822849fe83a18c4a12159bba40f55f3ed6e9
AbdelwahabAdam <abdelwahabosama.1@gmail.com>
2022-09-13 09:21:07 -0700

    fix:jans-cli up and down keys + selectOne

--- jans-cli-tui/wui_components/jans_drop_down.py
+++ jans-cli-tui/wui_components/jans_drop_down.py
@@ -12,6 +12,7 @@ from prompt_toolkit.layout.dimension import D
 class JansSelectBox:
     """_summary_
     """
+
     def __init__(self, values=[], value=None, height=4, rotatable_up=True, rotatable_down=True):
         """_summary_
 
@@ -22,25 +23,29 @@ class JansSelectBox:
             rotatable_up (bool, optional): _description_. Defaults to True.
             rotatable_down (bool, optional): _description_. Defaults to True.
         """
+
         self.values = values
+
         self.set_value(value)
 
+        # if not self.values :
+
         # --------------------------------------------------- #
-        self.height=min(len(self.values), height)
+        self.height = min(len(self.values), height)
         self.rotatable_up = rotatable_up
         self.rotatable_down = rotatable_down
         # --------------------------------------------------- #
 
-        self.container =HSplit(children=[ Window(
+        self.container = HSplit(children=[Window(
             content=FormattedTextControl(
                 text=self._get_formatted_text,
                 focusable=True,
             ),
             height=self.height,
             cursorline=False,
-            width=D(),  #15,
+            width=D(),  # 15,
             style='bg:#4D4D4D',
-            right_margins=[ScrollbarMargin(display_arrows=True),],
+            right_margins=[ScrollbarMargin(display_arrows=True), ],
             wrap_lines=True,
             allow_scroll_beyond_bottom=True,
         )])
@@ -69,14 +74,15 @@ class JansSelectBox:
         result = []
         for i, entry in enumerate(self.values):
             if i == self.selected_line:
-                result.append(HTML('<style fg="ansired" bg="{}">{}</style>'.format('#ADD8E6', entry[1])))
+                result.append(
+                    HTML('<style fg="ansired" bg="{}">{}</style>'.format('#ADD8E6', entry[1])))
             else:
                 result.append(HTML('<b>{}</b>'.format(entry[1])))
             result.append("\n")
 
         return merge_formatted_text(result)
 
-    def shift(self,seq, n):
+    def shift(self, seq, n):
         """_summary_
 
         Args:
@@ -91,27 +97,25 @@ class JansSelectBox:
     def up(self):
         """_summary_
         """
-        if self.selected_line == 0 :
-            if self.rotatable_up and  self.values[self.selected_line] == self.values[0]:
+        if self.selected_line == 0:
+            if self.rotatable_up and self.values[self.selected_line] == self.values[0]:
                 pass
-            else :
-                self.values = self.shift(self.values,-1) 
-        else :
+            else:
+                self.values = self.shift(self.values, -1)
+        else:
             self.selected_line = (self.selected_line - 1) % (self.height)
-
 
     def down(self):
         """_summary_
         """
 
-        if self.selected_line +1 == (self.height):
-            if self.rotatable_down and  self.values[self.selected_line] == self.values[-1]:
+        if self.selected_line + 1 == (self.height):
+            if self.rotatable_down and self.values[self.selected_line] == self.values[-1]:
                 pass
             else:
                 self.values = self.shift(self.values, 1)
-        else :
+        else:
             self.selected_line = (self.selected_line + 1) % (self.height)
-
 
     def __pt_container__(self):
         return self.container
@@ -120,6 +124,7 @@ class JansSelectBox:
 class DropDownWidget:
     """This is a Combobox widget (drop down) to select single from multi choices
     """
+
     def __init__(self, values=[], value=None):
         """init for DropDownWidget
         Args:
@@ -132,6 +137,7 @@ class DropDownWidget:
                 value=self.data.get('tokenEndpointAuthMethodsSupported'))
         """
         self.values = values
+        values.insert(0, (None, 'Select One'))
         for val in values:
             if val[0] == value:
                 self.text = val[1]
@@ -144,16 +150,18 @@ class DropDownWidget:
             content=FormattedTextControl(
                 text=self._get_text,
                 focusable=True,
-                 key_bindings=self._get_key_bindings(),
-            ), height=D()) #5  ## large sized enties get >> (window too small)
+                key_bindings=self._get_key_bindings(),
+            ), height=D())  # 5  ## large sized enties get >> (window too small)
 
-        self.select_box = JansSelectBox(values=self.values, value=value, rotatable_down=True, rotatable_up=True, height=4)
-        self.select_box_float = Float(content=self.select_box, xcursor=True, ycursor=True)
+        self.select_box = JansSelectBox(
+            values=self.values, value=value, rotatable_down=True, rotatable_up=True, height=4)
+        self.select_box_float = Float(
+            content=self.select_box, xcursor=True, ycursor=True)
 
     @property
     def value(self):
         """Getter for the value property
-        
+
         Returns:
             str: The selected value
         """
@@ -186,10 +194,13 @@ class DropDownWidget:
 
         @kb.add("enter")
         def _enter(event) -> None:
-
             if self.select_box_float not in get_app().layout.container.floats:
                 get_app().layout.container.floats.append(self.select_box_float)
             else:
+                # if self.select_box.values[self.select_box.selected_line][1] == "Select One":
+                #     self.text = "Enter to Select"
+                #     get_app().layout.container.floats.remove(self.select_box_float)
+                # else :
                 self.text = self.select_box.values[self.select_box.selected_line][1]
                 get_app().layout.container.floats.remove(self.select_box_float)
 
