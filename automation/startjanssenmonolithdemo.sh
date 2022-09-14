7e0c04e33a1bf327e79f2a8b85f3c1f7afa22c03
Mohammad Abudayyeh <47318409+moabu@users.noreply.github.com>
2022-09-14 10:02:55 +0300

    feat(docker-jans-monolith): add docker jans monolith (#2323)
    * feat: add monolith image for test and dev
    
    * feat: finish Dockerfile and docker compose
    
    * docs: add table of info
    
    * ci: add quick run
    
    * fix: volumes
    
    * fix: scim env name
    
    * docs: add quick start command
    
    * fix: add clean up command to the quick start
    
    * fix: clean up
    
    * fix: conform to hadolint rules
    
    * fix: update linux installer
    
    * feat: add installation check
    
    * fix: add gitignore rules for volumes
    
    * fix(monolith): remove deployment flag volume
    
    Co-authored-by: iromli <isman.firmansyah@gmail.com>
    Former-commit-id: ba511c3e23230da4f924ec48ea5d8fb9e02027f9

---
+++ automation/startjanssenmonolithdemo.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -eo pipefail
+
+JANS_FQDN=$1
+JANS_PERSISTENCE=$2
+
+if [[ ! "$JANS_FQDN" ]]; then
+  read -rp "Enter Hostname [demoexample.jans.io]:                           " JANS_FQDN
+fi
+if [[ ! "$JANS_PERSISTENCE" ]]; then
+  read -rp "Enter persistence type [LDAP(NOT SUPPORTED YET)|MYSQL]:                            " JANS_PERSISTENCE
+fi
+
+if [[ -z $EXT_IP ]]; then
+  EXT_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+fi
+
+sudo apt-get update
+# Install Docker and Docker compose plugin
+sudo apt-get remove docker docker-engine docker.io containerd runc -y || echo "Docker doesn't exist..installing.."
+sudo apt-get update
+sudo apt-get install \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+WORKING_DIRECTORY=$PWD
+# note that as we're pulling from a monorepo (with multiple project in it)
+# we are using partial-clone and sparse-checkout to get the docker-jans-monolith code
+rm -rf /tmp/jans || echo "/tmp/jans doesn't exist"
+git clone --filter blob:none --no-checkout https://github.com/janssenproject/jans /tmp/jans \
+    && cd /tmp/jans \
+    && git sparse-checkout init --cone \
+    && git checkout main \
+    && git sparse-checkout set docker-jans-monolith \
+    && cd "$WORKING_DIRECTORY"
+
+if [[ $JANS_PERSISTENCE == "MYSQL" ]]; then
+  docker compose -f /tmp/jans/docker-jans-monolith/mysql-docker-compose.yml up -d
+fi
+echo "$EXT_IP $JANS_FQDN" | sudo tee -a /etc/hosts > /dev/null
+echo "Waiting for the Janssen server to come up. Depending on the  resources it may take 3-5 mins for the services to be up."
+sleep 180
+cat << EOF > testendpoints.sh
+echo -e "Testing openid-configuration endpoint.. \n"
+curl -k https://$JANS_FQDN/.well-known/openid-configuration
+echo -e "Testing scim-configuration endpoint.. \n"
+curl -k https://$JANS_FQDN/.well-known/scim-configuration
+echo -e "Testing fido2-configuration endpoint.. \n"
+curl -k https://$JANS_FQDN/.well-known/fido2-configuration
+EOF
+sudo bash testendpoints.sh
+echo -e "You may re-execute bash testendpoints.sh to do a quick test to check the configuration endpoints."
+echo -e "Add the following record to your local computers' hosts file to engage with the services $EXT_IP $JANS_FQDN"
+echo -e "To clean up run:"
+echo -e "docker compose -f /tmp/jans/docker-jans-monolith/mysql-docker-compose.yml down && rm -rf /tmp/jans"
