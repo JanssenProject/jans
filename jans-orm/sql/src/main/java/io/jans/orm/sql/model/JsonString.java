0f551bc575cbb7c164a60814225dc0dbb0d2a7f6
Devrim <devrimyatar@gluu.org>
2022-09-16 20:01:28 +0300

    feat: jans-linux-setup postgresql support (#2409)
    * feat: jans-linux-setup postgresql support
    
    * feat: add PostgreSQL support
    
    Co-authored-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    Former-commit-id: 3d961516e6077034f0057b5b17e08ddb58bf1e0c

---
+++ jans-orm/sql/src/main/java/io/jans/orm/sql/model/JsonString.java
@@ -0,0 +1,25 @@
+package io.jans.orm.sql.model;
+
+/**
+ * PostgreSQL JSON column type support
+ *
+ * @author Yuriy Movchan Date: 09/01/2022
+ */
+
+public class JsonString {
+
+	private String value;
+
+	public JsonString(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
