d053d52f5f30e9708ddd51dce05d6458f8f189af
AbdelwahabAdam <abdelwahabosama.1@gmail.com>
2022-09-28 04:41:31 -0700

    fix:jans-cli add other tabs, save, search, deleted, and title

--- jans-cli-tui/plugins/010_oxauth/edit_scope_dialog.py
+++ jans-cli-tui/plugins/010_oxauth/edit_scope_dialog.py
@@ -55,7 +55,7 @@ from multi_lang import _
 class EditScopeDialog(DialogUtils):
     """The Main scope Dialog that contain every thing related to The scope
     """
-    def __init__(self, parent, data, save_handler=None):
+    def __init__(self, parent, title, data, save_handler=None):
         """init for `EditscopeDialog`, inherits from two diffrent classes `JansGDialog` and `DialogUtils`
             
         JansGDialog (dialog): This is the main dialog Class Widget for all Jans-cli-tui dialogs except custom dialogs like dialogs with navbar
@@ -71,6 +71,7 @@ class EditScopeDialog(DialogUtils):
         self.myparent = parent
         self.save_handler = save_handler
         self.data = data
+        self.title=title
         self.future = Future()
 
         self.showInConfigurationEndpoint = self.data.get('attributes',{}).get('showInConfigurationEndpoint','')
@@ -97,7 +98,17 @@ class EditScopeDialog(DialogUtils):
                 data[item_data['key']] = item_data['value']
 
         self.myparent.logger.debug('DATA: ' + str(data))
+        self.data = data    
+        if 'attributes' in self.data.keys():    
+            self.data['attributes'] = {'showInConfigurationEndpoint':self.data['attributes']}
 
+
+        self.myparent.logger.debug('handler: '+str(self.save_handler))
+        close_me = True
+        if self.save_handler:
+            close_me = self.save_handler(self)
+        if close_me:
+            self.future.set_result(DialogResult.ACCEPT)
 
     def create_window(self):
         """This method creat the dialog it self
@@ -107,10 +118,10 @@ class EditScopeDialog(DialogUtils):
 
 
         self.dialog = Dialog(
-            title=_("Add scope"),
+            title=self.title,
             body = HSplit([
                         self.myparent.getTitledRadioButton(
-                                _("Scope Type"), 
+                                _("Scope Type")+'\n \n \n \n', 
                                 name='scopeType', 
                                 values=[('oauth', 'AUuth'), ('openid', 'OpenID'), ('dynamic', 'Dynamic'), ('spontaneous', 'Spontaneous'), ('uma', 'UMA')], 
                                 on_selection_changed=self.scope_selection_changed,
@@ -122,14 +133,13 @@ class EditScopeDialog(DialogUtils):
                         self.myparent.getTitledText(_("Description"), name='description', value=self.data.get('description',''), style='green'),
                         DynamicContainer(lambda: self.alt_tabs[self.sope_type]),
 
-                        ], width=self.myparent.dialog_width,
+                        ], width=self.myparent.dialog_width
                     ),
-            buttons = [Button(_("Save"), handler=self.save), Button(_("Cancel"), self.cancel)]
+            buttons = [Button(_("Save"), handler=self.save), Button(_("Cancel"), self.cancel)],
         )
 
-
     def set_scope_type(self, scope_type):
-        self.sope_type = sope_type
+        self.sope_type = scope_type
 
     def prepare_tabs(self):
         """Prepare the tabs for Edil scope Dialogs
@@ -157,7 +167,6 @@ class EditScopeDialog(DialogUtils):
                                     style='green',
                             )
                         ])
-
 
         self.alt_tabs['openid'] = HSplit([
 
@@ -193,8 +202,50 @@ class EditScopeDialog(DialogUtils):
 
                             ])
 
+        self.alt_tabs['dynamic'] = HSplit([
+                        
+                        self.myparent.getTitledText(_("Dynamic Scope Script"),
+                            name='dynamicScopeScripts',
+                            value='\n'.join(self.data.get('dynamicScopeScripts', [])),
+                            height=3, 
+                            style='green'),
 
+
+                         self.myparent.getTitledText(_("Claims"),
+                            name='claims',
+                            value='\n'.join(self.data.get('claims', [])),
+                            height=3, 
+                            style='green'),
+
+                        # Label(text=_("Claims"),style='red'),  ## name = claims TODO 
+
+                        ],width=D(),
+                    )
+
+        self.alt_tabs['spontaneous'] = HSplit([
+                    self.myparent.getTitledText(_("Associated Client"), name='none', value=self.data.get('none',''), style='green',read_only=True,height=3,),## Not fount
+                    self.myparent.getTitledText(_("Creationg time"), name='creationDate', value=self.data.get('creationDate',''), style='green',read_only=True,),
+
+                                                ],width=D(),
+                    )
+
+        self.alt_tabs['uma'] = HSplit([
+                    self.myparent.getTitledText(_("IconURL"), name='iconUrl', value=self.data.get('iconUrl',''), style='green'),
+                    
+
+                    self.myparent.getTitledText(_("Authorization Policies"),
+                            name='umaAuthorizationPolicies',
+                            value='\n'.join(self.data.get('umaAuthorizationPolicies', [])),
+                            height=3, 
+                            style='green'),
+
+                    self.myparent.getTitledText(_("Associated Client"), name='none', value=self.data.get('none',''), style='green',read_only=True,height=3,), ## Not fount
+                    self.myparent.getTitledText(_("Creationg time"), name='description', value=self.data.get('description',''), style='green',read_only=True,),
+                    self.myparent.getTitledText(_("Creator"), name='Creator', value=self.data.get('Creator',''), style='green',read_only=True,),
+                        
+                                                ],width=D(),
+                    )
+        
 
     def __pt_container__(self):
         return self.dialog
-
