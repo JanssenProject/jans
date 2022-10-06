63adea1b7d429639eff9427b60007db538d3bb2b
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2022-10-06 18:44:27 +0300

    feat: merge ORM from Gluu (#2468)

--- jans-orm/sql/src/main/java/io/jans/orm/sql/dsl/template/PostgreSQLJsonTemplates.java
+++ jans-orm/sql/src/main/java/io/jans/orm/sql/dsl/template/PostgreSQLJsonTemplates.java
@@ -1,5 +1,10 @@
-package io.jans.orm.sql.dsl.template;
+/*
+ * Janssen Project software is available under the Apache License (2004). See http://www.apache.org/licenses/ for full text.
+ *
+ * Copyright (c) 2020, Janssen Project
+ */
 
+package io.jans.orm.sql.dsl.template;
 
 import com.querydsl.sql.PostgreSQLTemplates;
 import com.querydsl.sql.SQLTemplates;
