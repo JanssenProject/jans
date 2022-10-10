23fb97e171ab99421cc241af808346785b55ddbe
Mustafa Baser <mbaser@mail.com>
2022-10-10 14:11:14 +0300

    fix: jans-cli Scripts plugin skeleton

--- jans-cli-tui/plugins/060_scripts/main.py
+++ jans-cli-tui/plugins/060_scripts/main.py
@@ -1,30 +1,48 @@
 import os
 import sys
+
+import threading
+from asyncio import ensure_future
+from functools import partial
+
+import prompt_toolkit
+from prompt_toolkit.application.current import get_app
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.key_binding.bindings.focus import focus_next, focus_previous
 from prompt_toolkit.layout.containers import (
     HSplit,
     VSplit,
+    HorizontalAlign,
     DynamicContainer,
-    Window
 )
-from prompt_toolkit.widgets import (
-    Button,
-    Dialog,
-    VerticalLine,
-)
-from prompt_toolkit.layout.containers import HSplit
 from prompt_toolkit.layout.dimension import D
-from prompt_toolkit.widgets import Button, Label, Frame
+from prompt_toolkit.widgets import (
+    Box,
+    Button,
+    Label,
+    Frame
+)
+from static import DialogResult
 
+from cli import config_cli
+from wui_components.jans_nav_bar import JansNavBar
 from wui_components.jans_side_nav_bar import JansSideNavBar
+from wui_components.jans_vetrical_nav import JansVerticalNav
+from wui_components.jans_dialog import JansDialog
+from wui_components.jans_dialog_with_nav import JansDialogWithNav
+from wui_components.jans_drop_down import DropDownWidget
+from wui_components.jans_data_picker import DateSelectWidget
+
+from edit_client_dialog import EditClientDialog
+from edit_scope_dialog import EditScopeDialog
 
 from multi_lang import _
 import cli_style
-
 class Plugin():
     """This is a general class for plugins 
     """
     def __init__(self, app):
-        """init for Plugin class "scripts"
+        """init for Plugin class "oxauth"
 
         Args:
             app (_type_): _description_
@@ -33,11 +51,7 @@ class Plugin():
         self.pid = 'scripts'
         self.name = 'Scripts'
 
-        self.data = {}
-        self.script_containers = {}
-        self.script_prepare_navbar()
-        self.script_prepare_containers()
-        self.script_nav_selection_changed(self.script_navbar.navbar_entries[0][0])
+        self.scripts_prepare_containers()
 
     def process(self):
         pass
@@ -45,112 +59,33 @@ class Plugin():
     def set_center_frame(self):
         """center frame content
         """
-        self.app.center_container = self.script_main_container
+        self.app.center_container = self.scripts_main_area
 
-    def script_prepare_navbar(self):
-        """prepare the navbar for the current Plugin 
-        """
-        self.script_navbar = JansSideNavBar(
-                    entries=['Person Authentication', 'Consent Gathering',
-                     'Post Authentication', 'id_token', 'Password Grant',
-                      'CIBA End User Notification', 'OpenID Configuration', 
-                      'Dynamic Scope', 'Spontaneous Scope', 'Application Session',
-                       'End Session', 'Client Registration', 'Introspection', 
-                       'Update Token'],
-                    selection_changed=self.script_nav_selection_changed,
-                    select=0,
-                    entries_color='class:script-navbar-bgcolor' 
-                    )
-
-    def script_nav_selection_changed(self, selection):
-        """This method for the selection change
-
-        Args:
-            selection (str): the current selected tab
-        """
-        self.app.logger.debug('OXUATH NAV: %s', selection)
-        if selection in self.script_containers:
-            self.script_main_area = self.script_containers[selection]
-        else:
-            self.script_main_area = self.not_implemented
-
-    def script_prepare_containers(self):
+    def scripts_prepare_containers(self):
         """prepare the main container (tabs) for the current Plugin 
         """
-        self.script_data_container = {
-            'clients' :HSplit([],width=D()),
-            'scopes' :HSplit([],width=D()),
 
-        } 
-        self.script_main_area = HSplit([],width=D())
+        self.scripts_list_container = HSplit([],width=D(), height=D())
 
-        self.not_implemented =  Frame(
-                            body=HSplit([Label(text=_("Not imlemented yet")), Button(text=_("MyButton"))], width=D()),
-                            height=D(),)
-
-        self.script_containers['Person Authentication'] = Frame(
-        body=HSplit([
-            self.app.getTitledCheckBox(_("Enabled"), name='enabled', checked= not self.data.get('enabled'), style='class:script-checkbox'),
-            self.app.getTitledText(_("Name"), name='name', value=self.data.get('name',''), style='class:script-titledtext'),
-            self.app.getTitledText(_("Description"), name='description', value=self.data.get('description',''), style='class:script-titledtext'),
-            VSplit([
-                Label(text=_("Location"),style='class:script-label',width=len(_("Location"))),
-                HSplit([
-                    self.app.getTitledCheckBox(_("Database"), name='database', checked= not self.data.get('database'), style='class:script-checkbox'),
+        self.scripts_main_area = HSplit([
                     VSplit([
-                        self.app.getTitledCheckBox(_("File System"), name='filesystem', checked= not self.data.get('filesystem'), style='class:script-checkbox'),
-                        self.app.getTitledText(_(""), name='filesystem', value=self.data.get('filesystem',''), style='class:script-titledtext'),
-                        ]),
-                    ]),
-                ],
-                padding=3,
-                width=D(),
-            ),
-            Label(text=_("Level"),style='class:script-label',width=len(_("Level"))),
-            Label(text=_("Properties"),style='class:script-label',width=len(_("Properties"))),
-            Label(text=_("Script"),style='class:script-label',width=len(_("Script"))),
-            ],height=D(),width=D(),),
-
-                    height=D())
-        
-        HSplit([
-            self.app.getTitledCheckBox(_("Enabled"), name='enabled', checked= not self.data.get('enabled'), style='class:script-checkbox'),
-            self.app.getTitledText(_("Name"), name='name', value=self.data.get('name',''), style='class:script-titledtext'),
-            self.app.getTitledText(_("Description"), name='description', value=self.data.get('description',''), style='class:script-titledtext'),
-            VSplit([
-                Label(text=_("Location"),style='bold',width=len(_("Location"))),
-                HSplit([
-                    self.app.getTitledCheckBox(_("Database"), name='database', checked= not self.data.get('database'), style='class:script-checkbox'),
-                    VSplit([
-                        self.app.getTitledCheckBox(_("File System"), name='filesystem', checked= not self.data.get('filesystem'), style='class:script-checkbox'),
-                        self.app.getTitledText(_(""), name='filesystem', value=self.data.get('filesystem',''), style='class:script-titledtext'),
-                        ]),
-                ]),
-            Label(text=_("Level"),style='class:script-label',width=len(_("Level"))),
-            Label(text=_("Properties"),style='class:script-label',width=len(_("Properties"))),
-            Label(text=_("Script"),style='class:script-label',width=len(_("Script"))),
-                ],
-                padding=3,
-                width=D(),
-            ),
-            DynamicContainer(lambda: self.script_data_container['scopes'])
-            ])
-
-
-        self.script_main_container = Frame(
-            body=VSplit([
-                        HSplit([self.script_navbar,],style='class:script-sidenav'),
-                        
-                        Window(
-                            char="\u2502", style="class:line,vertical-line", width=1
-                        ),
-                        HSplit([DynamicContainer(lambda: self.script_main_area),],style='class:script-mainarea'),
-                        
+                        self.app.getButton(text=_("Get Scripts"), name='scripts:get', jans_help=_("Retreive first %d Scripts") % (20), handler=self.scrips_get_scripts),
+                        self.app.getTitledText(_("Search: "), name='scripts:search', jans_help=_("Press enter to perform search"), accept_handler=self.search_scripts, style='class:outh_containers_scopes.text'),
+                        self.app.getButton(text=_("Add Sscript"), name='scripts:add', jans_help=_("To add a new scope press this button"), handler=self.add_script),
                         ],
-                        style='class:script_maincontainer',
-                    height=D(),
-                    
-                    ))
+                        padding=3,
+                        width=D(),
+                    ),
+                    DynamicContainer(lambda: self.scripts_list_container)
+                    ],style='class:outh_containers_scopes')
 
 
-                                
+
+    def scrips_get_scripts(self):
+        pass
+
+    def search_scripts(self):
+        pass
+
+    def add_script(self):
+        pass
