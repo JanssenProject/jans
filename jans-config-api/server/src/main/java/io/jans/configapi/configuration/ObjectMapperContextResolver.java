312149393337ff2b2c794053a729c0f0919caa31
pujavs <43700552+pujavs@users.noreply.github.com>
2022-11-03 19:20:40 +0530

    fix(config-api): fixes for client creation, enum handling (#2854)
    * fix(config-api): client creation exception
    
    * fix(config-api): client creations exception
    
    * fix(config-api): issues fixed 2743, 2755 and 2689
    
    * fix(config-api): change to use fido2 app config model
    
    * fix(config-api): change to use fido2 app config model

--- jans-config-api/server/src/main/java/io/jans/configapi/configuration/ObjectMapperContextResolver.java
+++ jans-config-api/server/src/main/java/io/jans/configapi/configuration/ObjectMapperContextResolver.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 
 import java.text.SimpleDateFormat;
 import jakarta.inject.Singleton;
@@ -29,8 +30,10 @@ public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper
         return defaultObjectMapper;
     }
 
+    @SuppressWarnings("deprecation")
     public static ObjectMapper createDefaultMapper() {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
