5f23d33e3fda696451a9f6cae1426f148a543a86
pujavs <43700552+pujavs@users.noreply.github.com>
2022-11-03 19:20:40 +0530

    fix(config-api): fixes for client creation, enum handling (#2854)
    * fix(config-api): client creation exception
    
    * fix(config-api): client creations exception
    
    * fix(config-api): issues fixed 2743, 2755 and 2689
    
    * fix(config-api): change to use fido2 app config model
    
    * fix(config-api): change to use fido2 app config model
    
    Former-commit-id: 7061c0b3aa8feca98d36f6fea131a1f6610bc8ea

--- jans-config-api/plugins/admin-ui-plugin/src/main/java/io/jans/ca/plugin/adminui/service/user/UserManagementService.java
+++ jans-config-api/plugins/admin-ui-plugin/src/main/java/io/jans/ca/plugin/adminui/service/user/UserManagementService.java
@@ -45,7 +45,7 @@ public class UserManagementService {
             List<AdminRole> roles = adminConf.getDynamic().getRoles().stream().filter(ele -> ele.getRole().equals(role)).collect(Collectors.toList());
             if (roles.isEmpty()) {
                 log.error(ErrorResponse.ROLE_NOT_FOUND.getDescription());
-                throw new ApplicationException(Response.Status.BAD_REQUEST.getStatusCode(), ErrorResponse.ROLE_NOT_FOUND.getDescription());
+                throw new ApplicationException(Response.Status.NOT_FOUND.getStatusCode(), ErrorResponse.ROLE_NOT_FOUND.getDescription());
             }
             return roles.stream().findFirst().get();
         } catch (ApplicationException e) {
@@ -160,7 +160,7 @@ public class UserManagementService {
             List<AdminPermission> permissions = adminConf.getDynamic().getPermissions().stream().filter(ele -> ele.getPermission().equals(permission)).collect(Collectors.toList());
             if (permissions.isEmpty()) {
                 log.error(ErrorResponse.ROLE_NOT_FOUND.getDescription());
-                throw new ApplicationException(Response.Status.BAD_REQUEST.getStatusCode(), ErrorResponse.ROLE_NOT_FOUND.getDescription());
+                throw new ApplicationException(Response.Status.NOT_FOUND.getStatusCode(), ErrorResponse.PERMISSION_NOT_FOUND.getDescription());
             }
             return permissions.stream().findFirst().get();
         } catch (ApplicationException e) {
@@ -336,9 +336,12 @@ public class UserManagementService {
 
             if (roleScopeMapping.isEmpty()) {
                 log.error(ErrorResponse.ROLE_PERMISSION_MAP_NOT_FOUND.getDescription());
-                throw new ApplicationException(Response.Status.BAD_REQUEST.getStatusCode(), ErrorResponse.ROLE_PERMISSION_MAP_NOT_FOUND.getDescription());
+                throw new ApplicationException(Response.Status.NOT_FOUND.getStatusCode(), ErrorResponse.ROLE_PERMISSION_MAP_NOT_FOUND.getDescription());
             }
             return roleScopeMapping.stream().findFirst().get();
+        } catch (ApplicationException e) {
+            log.error(ErrorResponse.GET_ADMIUI_PERMISSIONS_ERROR.getDescription());
+            throw e;
         } catch (Exception e) {
             log.error(ErrorResponse.ERROR_READING_ROLE_PERMISSION_MAP.getDescription(), e);
             throw new ApplicationException(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), ErrorResponse.ERROR_READING_ROLE_PERMISSION_MAP.getDescription());
