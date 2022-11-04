ff17f3ea0c745f64e17783762cab412f86106100
ossdhaval <343411+ossdhaval@users.noreply.github.com>
2022-11-04 16:12:26 +0530

    feat: add default value

--- jans-core/util/src/main/java/io/jans/util/properties/JansConfigProperty.java
+++ jans-core/util/src/main/java/io/jans/util/properties/JansConfigProperty.java
@@ -8,6 +8,9 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.SOURCE)
 public @interface JansConfigProperty {
-    String optionalType() default "Optional";
     String description() default "None";
+
+    boolean isMandatory() default false;
+
+    String defaultValue() default "None";
 }
