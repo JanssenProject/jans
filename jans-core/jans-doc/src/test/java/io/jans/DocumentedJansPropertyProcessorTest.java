3d6c26ddb312bc589235f3b08cd1ad35156579e9
ossdhaval <343411+ossdhaval@users.noreply.github.com>
2022-11-09 20:05:33 +0530

    feat: fix test class errors

--- jans-core/jans-doc/src/test/java/io/jans/DocumentedJansPropertyProcessorTest.java
+++ jans-core/jans-doc/src/test/java/io/jans/DocumentedJansPropertyProcessorTest.java
@@ -1,38 +1,9 @@
 package io.jans;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class DocumentedJansPropertyProcessorTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+    // TODO: Write TestNG tests
 }
