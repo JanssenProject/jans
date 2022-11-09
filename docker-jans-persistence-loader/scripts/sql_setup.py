dcd9cc9d237d62c760be67a9221b27e0f3ec7c74
Isman Firmansyah <iromli@users.noreply.github.com>
2022-11-09 16:45:41 +0700

    chore(image): sync missing localized attributes for sql and spanner (#2927)
    * chore(image): sync missing localized attributes for sql and spanner
    
    * chore(image): sync jans-schema.json template

--- docker-jans-persistence-loader/scripts/sql_setup.py
+++ docker-jans-persistence-loader/scripts/sql_setup.py
@@ -369,6 +369,11 @@ class SQLBackend:
             ("jansStatEntry", "jansData"),
             ("jansSessId", "deviceSecret"),
             ("jansSsa", "jansState"),
+            ("jansClnt", "jansClntURILocalized"),
+            ("jansClnt", "jansLogoURILocalized"),
+            ("jansClnt", "jansPolicyURILocalized"),
+            ("jansClnt", "jansTosURILocalized"),
+            ("jansClnt", "displayNameLocalized"),
         ]:
             add_column(mod[0], mod[1])
 
