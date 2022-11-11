b0f1d9bd632ecbd95c46e12858f921b1c9211793
ossdhaval <343411+ossdhaval@users.noreply.github.com>
2022-11-11 15:45:50 +0530

    feat: write file under classes output dir

--- jans-core/doc/src/main/java/io/jans/doc/annotation/DocumentedJansPropertyProcessor.java
+++ jans-core/doc/src/main/java/io/jans/doc/annotation/DocumentedJansPropertyProcessor.java
@@ -5,8 +5,12 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.tools.FileObject;
+import javax.tools.JavaFileManager;
+import javax.tools.StandardLocation;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 import java.util.Set;
@@ -46,15 +50,16 @@ public class DocumentedJansPropertyProcessor extends AbstractProcessor {
             // for each property add a row in table and add content for the details section
             for (Element jansProperty : sortedProperties)
             {
+//                System.out.println(jansProperty.getEnclosingElement().toString());
                 DocumentedJansProperty propertyAnnotation = jansProperty.getAnnotation(DocumentedJansProperty.class);
-
                 addToTable(propTable, jansProperty, propertyAnnotation);
                 addToDetails(propDetails, jansProperty, propertyAnnotation);
 
             }
             propTable.append("\n\n");
+//            String annotatedClassName = ((TypeElement)annotation.getEnclosingElement()).getQualifiedName().toString();
 
-            createAndWriteDoc(propTable.append(propDetails.toString()));
+            createAndWriteDoc(propTable.append(propDetails.toString()), "");
 
             // This would be replaced by code to write into a markdown file
             System.out.println(propTable.toString()+"\n\n");
@@ -63,11 +68,24 @@ public class DocumentedJansPropertyProcessor extends AbstractProcessor {
         return false;
     }
 
-    private void createAndWriteDoc(StringBuilder docContent) {
-        try(PrintWriter docWriter = new PrintWriter(new File("out.put"))) {
+    private void createAndWriteDoc(StringBuilder docContent, String className) {
+//        String userDirectory = System.getProperty("user.dir");
+//        System.out.println("dir ->"+userDirectory);
+
+
+        try{
+            System.out.println("Paths -->");
+            System.out.println(StandardLocation.CLASS_OUTPUT.getName());
+            System.out.println(StandardLocation.SOURCE_OUTPUT);
+            System.out.println(StandardLocation.CLASS_PATH);
+            System.out.println(StandardLocation.ANNOTATION_PROCESSOR_PATH);
+            System.out.println("Paths --<");
+//            FileObject docFile = processingEnv.getFiler().createSourceFile("out.put");
+            FileObject docFile = processingEnv.getFiler().createResource(StandardLocation.CLASS_OUTPUT,"", "out.put");
+            PrintWriter docWriter = new PrintWriter(docFile.openWriter());
             docWriter.write(docContent.toString());
             docWriter.flush();
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             // log to system output at compile time and exit
             System.out.println("Failed to create file for property documentation. Exiting the process");
             e.printStackTrace();
