34bd41defb52c25f38a328ac6496ff9298cd15d8
pujavs <43700552+pujavs@users.noreply.github.com>
2022-11-30 20:58:20 +0530

    jans(config-api): static scope id and feature and admin level scopes (#3126)
    * fix(config-api): fix for swagger spec for scope creation and sessoin endpoint filter
    
    * ci: add yurem to linux setup codeowners
    
    * chore(image): sync missing localized attributes for sql and spanner (#2927)
    
    * chore(image): sync missing localized attributes for sql and spanner
    
    * chore(image): sync jans-schema.json template
    
    * Update restarting-services.md (#2941)
    
    Restart command and output were merged which made "copy" / "paste" hard....
    
    * Update restarting-services.md (#2942)
    
    Removed "$" sign from command....
    
    * Update restarting-services.md (#2943)
    
    Adding some other service related info...
    
    * feat: allow to use like with lower together (#2944)
    
    Co-authored-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    * chore: remove unused merthod (#2945)
    
    Co-authored-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    * feat(jans-auth-server): corrected GluuOrganization - refactor getOrganizationName() #2947 (#2948)
    
    * feat(config-api): super scope implementation
    
    * feat(config-api): static scope
    
    * feat(config-api): comprehensive claims for authurization
    
    * feat(config-api): endpoint group and admin scope
    
    * feat(config-api): sync with main
    
    * feat(config-api): endpoint group and admin scope
    
    * feat(config-api): scope change - wip
    
    * feat(config-api): scope change - wip
    
    * feat(config-api): scope enhancements
    
    * feat(config-api): scope enhancement
    
    * feat(config-api): scope enhancement wip
    
    * feat(config-api): scope enhancement- wip
    
    * feat(config-api): scope enhancement- wip
    
    * feat: jans-linux-setup config-api scope creation with static inum (ref: #3097)
    
    * feat(config-api): scope enhancement - wip
    
    * fix: jans-linux-setup create scope if inum exists (ref: #3097)
    
    * feat: jans-linux-setup config-api scope creation (ref: #3097)
    
    * feat(config-api): scope enhancement - wip
    
    * feat(config-api): scope enhancement
    
    Co-authored-by: moabu <47318409+moabu@users.noreply.github.com>
    Co-authored-by: Isman Firmansyah <iromli@users.noreply.github.com>
    Co-authored-by: mzico <mohib@gluu.org>
    Co-authored-by: Yuriy M <95305560+yuremm@users.noreply.github.com>
    Co-authored-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    Co-authored-by: YuriyZ <yzabrovarniy@gmail.com>
    Co-authored-by: Mustafa Baser <mbaser@mail.com>

--- jans-config-api/server/src/main/java/io/jans/configapi/security/api/ApiProtectionCache.java
+++ jans-config-api/server/src/main/java/io/jans/configapi/security/api/ApiProtectionCache.java
@@ -6,9 +6,12 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.Maps;
 import io.jans.as.persistence.model.Scope;
+import io.jans.configapi.core.util.ProtectionScopeType;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Named;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -22,8 +25,17 @@ public class ApiProtectionCache {
     private static final Cache<String, Scope> scopeCache = CacheBuilder.newBuilder()
             .expireAfterWrite(CACHE_LIFETIME, TimeUnit.MINUTES).build();
 
-    private static final Cache<String, List<Scope>> resourceCache = CacheBuilder.newBuilder()
+    private static final Cache<String, Scope> groupScopeCache = CacheBuilder.newBuilder()
             .expireAfterWrite(CACHE_LIFETIME, TimeUnit.MINUTES).build();
+
+    private static final Cache<String, Scope> superScopeCache = CacheBuilder.newBuilder()
+            .expireAfterWrite(CACHE_LIFETIME, TimeUnit.MINUTES).build();
+
+    private static final Cache<String, Map<ProtectionScopeType, List<Scope>>> resourceCache = CacheBuilder.newBuilder()
+            .expireAfterWrite(CACHE_LIFETIME, TimeUnit.MINUTES).build();
+
+    ApiProtectionCache() {
+    }
 
     // Scope
     public static void removeAllScopes() {
@@ -39,35 +51,130 @@ public class ApiProtectionCache {
 
     public static void putScope(Scope scope) {
         Preconditions.checkNotNull(scope);
-        if (scopeCache.getIfPresent(scope.getId()) == null) {
-            scopeCache.put(scope.getId(), scope);
+        if (scopeCache.getIfPresent(scope.getInum()) == null) {
+            scopeCache.put(scope.getInum(), scope);
         }
     }
 
-    public static Map<String, Scope> getAllScopes() {
+    public static Map<String, Scope> getScopes() {
         return Maps.newHashMap(scopeCache.asMap());
     }
 
+    // Group Scope
+    public static Scope getGroupScope(String scopeName) {
+        Preconditions.checkNotNull(scopeName);
+        Preconditions.checkState(!Strings.isNullOrEmpty(scopeName));
+        return groupScopeCache.getIfPresent(scopeName);
+
+    }
+
+    public static void putGroupScope(Scope scope) {
+        Preconditions.checkNotNull(scope);
+        if (groupScopeCache.getIfPresent(scope.getInum()) == null) {
+            groupScopeCache.put(scope.getInum(), scope);
+        }
+    }
+
+    public static void removeGroupScopes() {
+        groupScopeCache.invalidateAll();
+    }
+
+    public static Map<String, Scope> getGroupScopes() {
+        return Maps.newHashMap(groupScopeCache.asMap());
+    }
+
+    // Super Scope
+    public static Scope getSuperScope(String scopeName) {
+        Preconditions.checkNotNull(scopeName);
+        Preconditions.checkState(!Strings.isNullOrEmpty(scopeName));
+        return superScopeCache.getIfPresent(scopeName);
+
+    }
+
+    public static void putSuperScope(Scope scope) {
+        Preconditions.checkNotNull(scope);
+        if (superScopeCache.getIfPresent(scope.getInum()) == null) {
+            superScopeCache.put(scope.getInum(), scope);
+        }
+    }
+
+    public static void removeSuperScopes() {
+        superScopeCache.invalidateAll();
+    }
+
+    public static Map<String, Scope> getSuperScopes() {
+        return Maps.newHashMap(superScopeCache.asMap());
+    }
+
+    // All Scope
+    public static Map<String, Scope> getAllTypesOfScopes() {
+        Map<String, Scope> scopes = Maps.newHashMap(scopeCache.asMap());
+        scopes.putAll(Maps.newHashMap(groupScopeCache.asMap()));
+        scopes.putAll(Maps.newHashMap(superScopeCache.asMap()));
+        return scopes;
+    }
+
     // Resource
-    public static void removeAllResources() {
+    public static void raemoveAllResources() {
         resourceCache.invalidateAll();
     }
 
-    public static List<Scope> getResourceScopes(String resourceName) {
+    public static void putResource(String resourceName, Map<ProtectionScopeType, List<Scope>> scopeMap) {
+        Preconditions.checkNotNull(resourceName);
+        resourceCache.put(resourceName, scopeMap);
+    }
+
+    public static void putResourceScopeByType(String resourceName, ProtectionScopeType protectionScopeType,
+            List<Scope> scopes) {
+        Preconditions.checkNotNull(resourceName);
+        Preconditions.checkNotNull(protectionScopeType);
+        Map<ProtectionScopeType, List<Scope>> scopeMap = resourceCache.getIfPresent(resourceName);
+        if (scopeMap == null) {
+            scopeMap = new HashMap<>();
+        }
+        scopeMap.put(protectionScopeType, scopes);
+        resourceCache.put(resourceName, scopeMap);
+    }
+
+    public static Map<String, Map<ProtectionScopeType, List<Scope>>> getAllResources() {
+        return Maps.newHashMap(resourceCache.asMap());
+    }
+
+    public static Map<ProtectionScopeType, List<Scope>> getResourceScopes(String resourceName) {
         Preconditions.checkNotNull(resourceName);
         Preconditions.checkState(!Strings.isNullOrEmpty(resourceName));
         return resourceCache.getIfPresent(resourceName);
 
     }
 
-    public static void putResource(String resourceName, List<Scope> scopeList) {
+    public static List<Scope> getResourceScopeByType(String resourceName, ProtectionScopeType protectionScopeType) {
         Preconditions.checkNotNull(resourceName);
-        resourceCache.put(resourceName, scopeList);
-
+        Preconditions.checkNotNull(protectionScopeType);
+        Map<ProtectionScopeType, List<Scope>> scopeMap = resourceCache.getIfPresent(resourceName);
+        if (scopeMap == null) {
+            return Collections.emptyList();
+        }
+        return scopeMap.get(protectionScopeType);
     }
 
-    public static Map<String, List<Scope>> getAllResources() {
-        return Maps.newHashMap(resourceCache.asMap());
-    }
+    public static void addScope(String resourceName, ProtectionScopeType protectionScopeType, Scope scope) {
+        Preconditions.checkNotNull(resourceName);
+        Preconditions.checkNotNull(protectionScopeType);
+        Preconditions.checkNotNull(scope);
 
+        switch (protectionScopeType) {
+        case GROUP:
+            putGroupScope(scope);
+            break;
+
+        case SUPER:
+            putSuperScope(scope);
+            break;
+
+        default:
+            putScope(scope);
+            break;
+
+        }
+    }
 }
