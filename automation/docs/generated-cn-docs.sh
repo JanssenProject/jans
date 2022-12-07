e5f48b4fb8dfc1c29fa4a70445665c3483e81f28
moabu <47318409+moabu@users.noreply.github.com>
2022-12-07 11:17:40 +0000

    ci: fix charts main readme location
    Former-commit-id: e855a6f7d661acb1ab5d390b314ebdefa56d6f61

--- automation/docs/generated-cn-docs.sh
+++ automation/docs/generated-cn-docs.sh
@@ -11,7 +11,7 @@ tar xvf helm-docs_"${VERSION}"_Linux_x86_64.tar.gz
 sudo cp helm-docs /usr/local/bin/
 # Generate Helm docs
 helm-docs charts/
-cp ./charts/README.md ./docs/admin/reference/kubernetes/helm-chart.md
+cp ./charts/janssen/README.md ./docs/admin/reference/kubernetes/helm-chart.md
 sed -i '1 s/^/---\ntags:\n  - administration\n  - reference\n  - kubernetes\n---\n/' ./docs/admin/reference/kubernetes/helm-chart.md
 # cp docker-monolith main README.md
 cp ./docker-jans-monolith/README.md  ./docs/admin/install/docker-install/compose.md
