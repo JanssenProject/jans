3b91666eca592906536a55ce79003b7ce213069a
Mohammad Abudayyeh <47318409+moabu@users.noreply.github.com>
2022-12-08 20:47:36 +0200

    ci: fix overriding readme

--- automation/docs/generated-cn-docs.sh
+++ automation/docs/generated-cn-docs.sh
@@ -5,12 +5,16 @@ echo "Copies generated/duplicate cn docs such as the helm main readme to the mai
 
 # Generate Helm docs
 # Install helm docs
+mkdir helmtemp
+cd helmtemp
 VERSION=$(curl "https://api.github.com/repos/norwoodj/helm-docs/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c2-)
 curl -sSL https://github.com/norwoodj/helm-docs/releases/download/v"${VERSION}"/helm-docs_"${VERSION}"_Linux_x86_64.tar.gz  -o helm-docs_"${VERSION}"_Linux_x86_64.tar.gz
 tar xvf helm-docs_"${VERSION}"_Linux_x86_64.tar.gz
 sudo cp helm-docs /usr/local/bin/
+cd ..
 # Generate Helm docs
 helm-docs charts/
+rm -rf helmtemp
 cp ./charts/janssen/README.md ./docs/admin/reference/kubernetes/helm-chart.md
 sed -i '1 s/^/---\ntags:\n  - administration\n  - reference\n  - kubernetes\n---\n/' ./docs/admin/reference/kubernetes/helm-chart.md
 # cp docker-monolith main README.md
