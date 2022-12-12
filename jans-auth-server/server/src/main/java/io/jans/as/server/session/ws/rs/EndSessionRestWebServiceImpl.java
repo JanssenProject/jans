3f3bf60999a8ef08433de3d18d72dc7b9d2fb021
YuriyZ <yzabrovarniy@gmail.com>
2022-12-12 14:28:03 +0200

    fix(jans-auth-server): sonar complains

--- jans-auth-server/server/src/main/java/io/jans/as/server/session/ws/rs/EndSessionRestWebServiceImpl.java
+++ jans-auth-server/server/src/main/java/io/jans/as/server/session/ws/rs/EndSessionRestWebServiceImpl.java
@@ -312,7 +312,7 @@ public class EndSessionRestWebServiceImpl implements EndSessionRestWebService {
         return null;
     }
 
-    protected Jwt validateIdTokenHint(String idTokenHint, SessionId sidSession, String postLogoutRedirectUri) {
+    private Jwt validateIdTokenHint(String idTokenHint, SessionId sidSession, String postLogoutRedirectUri) {
         final boolean isIdTokenHintRequired = isTrue(appConfiguration.getForceIdTokenHintPrecense());
 
         if (isIdTokenHintRequired && StringUtils.isBlank(idTokenHint)) { // must be present for logout tests #1279
@@ -372,6 +372,7 @@ public class EndSessionRestWebServiceImpl implements EndSessionRestWebService {
         if (isTrue(appConfiguration.getAllowEndSessionWithUnmatchedSid())) {
             return;
         }
+
         final String sidClaim = jwt.getClaims().getClaimAsString("sid");
         if (sidSession != null && StringUtils.equals(sidSession.getOutsideSid(), sidClaim)) {
             return;
