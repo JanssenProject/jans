3e29868ddac32aff368f723f7bb16740c4a5a570
AbdelwahabAdam <abdelwahabosama.1@gmail.com>
2022-12-17 00:13:17 -0800

    fix:jans-cli-tui fix import error

--- jans-cli-tui/cli_tui/utils/utils.py
+++ jans-cli-tui/cli_tui/utils/utils.py
@@ -5,9 +5,11 @@ import prompt_toolkit
 
 from cli_style import style
 from wui_components.jans_drop_down import DropDownWidget
-from cli_tui.wui_components.jans_date_picker import DateSelectWidget
 from wui_components.jans_spinner import Spinner
+import sys
 
+sys.path.append("./")
+from cli_tui.wui_components.jans_date_picker import DateSelectWidget
 
 common_data = SimpleNamespace()
 
