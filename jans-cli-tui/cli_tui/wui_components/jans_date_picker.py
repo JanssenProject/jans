d5e60dc8213dd9446f059a1ddbd77385a37799bd
AbdelwahabAdam <abdelwahabosama.1@gmail.com>
2022-12-19 06:36:32 -0800

    fix:jans-cli-tui remove unused imports

--- jans-cli-tui/cli_tui/wui_components/jans_date_picker.py
+++ jans-cli-tui/cli_tui/wui_components/jans_date_picker.py
@@ -1,29 +1,20 @@
 
 from prompt_toolkit.application.current import get_app
 from prompt_toolkit.key_binding import KeyBindings
-from prompt_toolkit.layout.containers import Float, HSplit, Window
 from prompt_toolkit.layout.controls import FormattedTextControl
 from prompt_toolkit.formatted_text import HTML, merge_formatted_text
 from prompt_toolkit.layout.margins import ScrollbarMargin
 from prompt_toolkit.key_binding.bindings.focus import focus_next, focus_previous
 from prompt_toolkit.layout.dimension import D
-from prompt_toolkit.layout.containers import Float, FloatContainer, HSplit, Window, VSplit
-from prompt_toolkit.widgets import Button, Label, TextArea
+from prompt_toolkit.widgets import Button, Label
 from prompt_toolkit.layout.containers import (
-    ConditionalContainer,
     Float,
     HSplit,
     VSplit,
-    VerticalAlign,
-    HorizontalAlign,
     DynamicContainer,
-    FloatContainer,
     Window
 )
-from prompt_toolkit.layout.containers import (
-    AnyContainer,
-)
-from typing import Optional, Sequence, Union
+from typing import Optional
 from prompt_toolkit.formatted_text import AnyFormattedText
 from prompt_toolkit.widgets import Button, Dialog
 from prompt_toolkit.key_binding.key_bindings import KeyBindings, KeyBindingsBase
