fcc8a23c60bc36d78ddbf59b9b7d44ba6076491b
Isman Firmansyah <iromli@users.noreply.github.com>
2022-12-29 01:42:52 +0700

    chore(prometheus): exclude optional prometheus exporter from images (#3439)
    Former-commit-id: d5b457729ade923315687aafdc4a002fed50c209

--- docker-jans-config-api/scripts/entrypoint.sh
+++ docker-jans-config-api/scripts/entrypoint.sh
@@ -22,6 +22,17 @@ get_prometheus_opt() {
     echo "${prom_opt}"
 }
 
+get_prometheus_lib() {
+    if [ -n "${CN_PROMETHEUS_PORT}" ]; then
+        prom_agent_version="0.17.2"
+
+        if [ ! -f /opt/prometheus/jmx_prometheus_javaagent.jar ]; then
+            wget -q https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/${prom_agent_version}/jmx_prometheus_javaagent-${prom_agent_version}.jar -O /opt/prometheus/jmx_prometheus_javaagent.jar
+        fi
+    fi
+}
+
+get_prometheus_lib
 python3 /app/scripts/wait.py
 python3 /app/scripts/bootstrap.py
 python3 /app/scripts/upgrade.py
