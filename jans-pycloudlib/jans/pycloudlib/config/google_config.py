dab570d73202815f00c114f9913557d728d687c0
iromli <isman.firmansyah@gmail.com>
2023-01-03 02:15:15 +0700

    refactor(pycloudlib): simplify Google Secret integration

--- jans-pycloudlib/jans/pycloudlib/config/google_config.py
+++ jans-pycloudlib/jans/pycloudlib/config/google_config.py
@@ -2,23 +2,18 @@
 
 from __future__ import annotations
 
-import sys
-import logging
-import os
 import json
+import logging
+import sys
+import os
 import typing as _t
 from functools import cached_property
 
 from google.cloud import secretmanager
 from google.api_core.exceptions import AlreadyExists, NotFound
 
-from jans.pycloudlib.utils import safe_value
 from jans.pycloudlib.config.base_config import BaseConfig
-
-if _t.TYPE_CHECKING:  # pragma: no cover
-    # imported objects for function type hint, completion, etc.
-    # these won't be executed in runtime
-    from google.cloud import secretmanager_v1
+from jans.pycloudlib.utils import safe_value
 
 logger = logging.getLogger(__name__)
 
@@ -30,17 +25,40 @@ class GoogleConfig(BaseConfig):
 
     Supported environment variables:
 
-    - `CN_CONFIG_GOOGLE_SECRET_VERSION_ID`: Janssen configuration secret version ID in Google Secret Manager. Defaults to `latest`, which is recommended.
-    - `CN_CONFIG_GOOGLE_SECRET_NAME_PREFIX`: Prefix for Janssen configuration secret in Google Secret Manager. Defaults to `jans`. If left intact `jans-configuration` secret will be created.
+    - `CN_SECRET_GOOGLE_SECRET_VERSION_ID`: Deprecated in favor of `CN_GOOGLE_SECRET_VERSION_ID`.
+    - `CN_SECRET_GOOGLE_SECRET_NAME_PREFIX`: Deprecated in favor of `CN_GOOGLE_SECRET_NAME_PREFIX`.
+
     - `GOOGLE_APPLICATION_CREDENTIALS`: JSON file (contains Google credentials) that should be injected into container.
     - `GOOGLE_PROJECT_ID`: ID of Google project.
+    - `CN_GOOGLE_SECRET_VERSION_ID`: Janssen secret version ID in Google Secret Manager. Defaults to `latest`, which is recommended.
+    - `CN_GOOGLE_SECRET_NAME_PREFIX`: Prefix for Janssen secret in Google Secret Manager. Defaults to `jans`. If left `jans-secret` secret will be created.
     """
 
     def __init__(self) -> None:
         self.project_id = os.getenv("GOOGLE_PROJECT_ID", "")
-        self.version_id = os.getenv("CN_CONFIG_GOOGLE_SECRET_VERSION_ID", "latest")
+
+        if "CN_SECRET_GOOGLE_SECRET_VERSION_ID" in os.environ:
+            logger.warning(
+                "Found CN_SECRET_GOOGLE_SECRET_VERSION_ID environment variable. "
+                "Note that this environment variable is deprecated in favor of "
+                "CN_GOOGLE_SECRET_VERSION_ID and soon will be removed."
+            )
+            self.version_id = os.environ["CN_SECRET_GOOGLE_SECRET_VERSION_ID"] or "latest"
+        else:
+            self.version_id = os.getenv("CN_GOOGLE_SECRET_VERSION_ID", "latest")
+
+        if "CN_SECRET_GOOGLE_SECRET_NAME_PREFIX" in os.environ:
+            logger.warning(
+                "Found CN_SECRET_GOOGLE_SECRET_NAME_PREFIX environment variable. "
+                "Note that this environment variable is deprecated in favor of "
+                "CN_GOOGLE_SECRET_NAME_PREFIX and soon will be removed."
+            )
+            prefix = os.environ["CN_SECRET_GOOGLE_SECRET_NAME_PREFIX"] or "jans"
+        else:
+            prefix = os.getenv("CN_GOOGLE_SECRET_NAME_PREFIX", "jans")
+
         # secrets key value by default
-        self.google_secret_name = os.getenv("CN_CONFIG_GOOGLE_SECRET_NAME_PREFIX", "jans") + "-configuration"
+        self.google_secret_name = f"{prefix}-configuration"
 
     @cached_property
     def client(self) -> secretmanager.SecretManagerServiceClient:
@@ -58,29 +76,32 @@ class GoogleConfig(BaseConfig):
         # Try to get the latest resource name. Used in initialization. If the latest version doesn't exist
         # its a state where the secret and initial version must be created
         name = f"projects/{self.project_id}/secrets/{self.google_secret_name}/versions/latest"
+
         try:
             self.client.access_secret_version(request={"name": name})
         except NotFound:
             logger.warning("Secret may not exist or have any versions created yet")
             self.create_secret()
             self.add_secret_version(safe_value({}))
+
         # Build the resource name of the secret version.
         name = f"projects/{self.project_id}/secrets/{self.google_secret_name}/versions/{self.version_id}"
         data = {}
+
         try:
             # Access the secret version.
             response = self.client.access_secret_version(request={"name": name})
-            logger.info(f"Secret {self.google_secret_name} has been found. Accessing version {self.version_id}.")
+            # logger.info(f"Secret {self.google_secret_name} has been found. Accessing version {self.version_id}.")
             payload = response.payload.data.decode("UTF-8")
             data = json.loads(payload)
         except NotFound:
-            logger.warning("Secret may not exist or have any versions created. "
-                           "Make sure CN_SECRET_GOOGLE_SECRET_VERSION_ID, and "
-                           "CN_SECRET_GOOGLE_SECRET_NAME_PREFIX are set correctly. "
-                           "In Google secrets manager, "
-                           "a secret with the name jans-secret would have CN_SECRET_GOOGLE_SECRET_NAME_PREFIX"
-                           " set to jans.")
-
+            logger.warning(
+                "Secret may not exist or have any versions created. Make sure "
+                "CN_GOOGLE_SECRET_VERSION_ID and CN_GOOGLE_SECRET_NAME_PREFIX "
+                "environment variables are set correctly. In Google secrets manager, "
+                "a secret with the name jans-secret would have "
+                "CN_GOOGLE_SECRET_NAME_PREFIX set to jans."
+            )
         return data
 
     def get(self, key: str, default: _t.Any = "") -> _t.Any:
@@ -108,7 +129,9 @@ class GoogleConfig(BaseConfig):
         """
         all_ = self.get_all()
         all_[key] = safe_value(value)
-        _ = self.create_secret()
+
+        self.create_secret()
+
         logger.info(f'Adding key {key} to google secret manager')
         logger.info(f'Size of secret payload : {sys.getsizeof(safe_value(all_))} bytes')
         secret_version_bool = self.add_secret_version(safe_value(all_))
@@ -124,26 +147,25 @@ class GoogleConfig(BaseConfig):
             A boolean to mark whether config is set or not.
         """
         all_ = {}
+
         for k, v in data.items():
             all_[k] = safe_value(v)
-        _ = self.create_secret()
+
+        self.create_secret()
+
         logger.info(f'Size of secret payload : {sys.getsizeof(safe_value(all_))} bytes')
         secret_version_bool = self.add_secret_version(safe_value(all_))
         return secret_version_bool
 
-    def create_secret(self) -> _t.Union[secretmanager_v1.types.Secret, None]:
+    def create_secret(self) -> None:
         """Create a new secret with the given name.
 
         A secret is a logical wrapper around a collection of secret versions.
         Secret versions hold the actual secret material.
-
-        Returns:
-            `google.cloud.secretmanager_v1.types.Secret` instead of boolean.
         """
         # Build the resource name of the parent project.
         parent = f"projects/{self.project_id}"
 
-        response = None
         try:
             # Create the secret.
             response = self.client.create_secret(
@@ -156,20 +178,19 @@ class GoogleConfig(BaseConfig):
             logger.info(f"Created secret: {response.name}")
         except AlreadyExists:
             logger.warning(f'Secret {self.google_secret_name} already exists. A new version will be created.')
-        return response
 
     def add_secret_version(self, payload: _t.AnyStr) -> bool:
         """Add a new secret version to the given secret with the provided payload.
 
         Args:
-            payload: payload
+            payload: Payload string or bytes.
         """
         # Build the resource name of the parent secret.
         parent = self.client.secret_path(self.project_id, self.google_secret_name)
 
         if isinstance(payload, str):
-            # Convert the string payload into a bytes (if it's a string).
-            # This step can be omitted if you pass in bytes instead of a str for the payload argument.
+            # Convert the string payload into a bytes. This step can be omitted if you
+            # pass in bytes instead of a str for the payload argument.
             payload_bytes = payload.encode("UTF-8")
         else:
             payload_bytes = payload
