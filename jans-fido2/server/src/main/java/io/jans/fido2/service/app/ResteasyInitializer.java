20b9d8d4d97077f527c996531b84178b531ebd2b
jmunozherbas <jorgemunozherbas@gmail.com>
2023-01-11 02:13:17 -0400

    fix(jans-fido2):remove endpoint u2f fido-configuration

--- jans-fido2/server/src/main/java/io/jans/fido2/service/app/ResteasyInitializer.java
+++ jans-fido2/server/src/main/java/io/jans/fido2/service/app/ResteasyInitializer.java
@@ -6,20 +6,16 @@
 
 package io.jans.fido2.service.app;
 
-import java.util.HashSet;
-
-import java.util.Set;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
 import io.jans.fido2.ws.rs.controller.AssertionController;
 import io.jans.fido2.ws.rs.controller.AttestationController;
 import io.jans.fido2.ws.rs.controller.ConfigurationController;
-
 import io.jans.fido2.ws.rs.controller.u2f.U2fAuthenticationWS;
-import io.jans.fido2.ws.rs.controller.u2f.U2fConfigurationWS;
 import io.jans.fido2.ws.rs.controller.u2f.U2fRegistrationWS;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Integration with Resteasy
@@ -37,7 +33,6 @@ public class ResteasyInitializer extends Application {
         classes.add(AssertionController.class);
         classes.add(AttestationController.class);
 
-        classes.add(U2fConfigurationWS.class);
         classes.add(U2fAuthenticationWS.class);
         classes.add(U2fRegistrationWS.class);
         return classes;
