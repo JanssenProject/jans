8f3f47cb43a492954fd2711549e3b801844e1fde
YuriyZ <yzabrovarniy@gmail.com>
2023-01-31 20:19:06 +0200

    feat(jans-auth-server): introduced key_ops for granular map of crypto service to rotation profile #3415 (#3642)
    * feat(jans-auth-server): introduced key_ops for granular map of crypto service to rotation profile #3415
    
    * fix(jans-auth-server): fixed key rotation and added test to key generator context #3415
    
    * minor code improvements #3415
    
    Former-commit-id: ff43871fa500642df62c9ad388389927a1dc71e5

--- jans-auth-server/model/src/main/java/io/jans/as/model/crypto/ElevenCryptoProvider.java
+++ jans-auth-server/model/src/main/java/io/jans/as/model/crypto/ElevenCryptoProvider.java
@@ -9,6 +9,7 @@ package io.jans.as.model.crypto;
 import io.jans.as.model.crypto.signature.SignatureAlgorithm;
 import io.jans.as.model.exception.CryptoProviderException;
 import io.jans.as.model.jwk.Algorithm;
+import io.jans.as.model.jwk.KeyOps;
 import io.jans.eleven.client.DeleteKeyClient;
 import io.jans.eleven.client.DeleteKeyRequest;
 import io.jans.eleven.client.DeleteKeyResponse;
@@ -74,6 +75,11 @@ public class ElevenCryptoProvider extends AbstractCryptoProvider {
         } else {
             throw new CryptoProviderException(response.getEntity());
         }
+    }
+
+    @Override
+    public JSONObject generateKey(Algorithm algorithm, Long expirationTime, int keyLength, KeyOps keyOps) throws CryptoProviderException {
+        return generateKey(algorithm, expirationTime, keyLength, KeyOps.CONNECT);
     }
 
     @Override
