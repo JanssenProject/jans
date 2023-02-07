6354df1025f1a5031b35255b12c32c7a7a5d0a14
YuriyZ <yzabrovarniy@gmail.com>
2023-02-07 10:12:41 +0200

    feat(jans-auth-server): renamed "key_ops" -> "key_ops_type" #3790 (#3792)
    Some frameworks does not allow custom key_ops like "ssa" (e.g. nimbus)
    
    Former-commit-id: 7a6bcba5ca3597f7556d406e4a572c76a229bbdf

--- docker-jans-certmanager/scripts/utils.py
+++ docker-jans-certmanager/scripts/utils.py
@@ -37,7 +37,7 @@ def generate_openid_keys(passwd, jks_path, jwks_path, dn, exp=365, sig_keys=DEFA
         f"-enc_keys {enc_keys} -sig_keys {sig_keys} "
         f"-dnname '{dn}' -expiration_hours {exp} "
         f"-keystore {jks_path} -keypasswd {passwd} "
-        "-key_ops connect"
+        "-key_ops_type connect"
     )
 
     out, err, retcode = exec_cmd(cmd)
