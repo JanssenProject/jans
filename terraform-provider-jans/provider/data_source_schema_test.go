08329e113c108599eb25db0129a86a103202e69f
Mohammad Abudayyeh <47318409+moabu@users.noreply.github.com>
2023-02-13 14:51:07 +0000

    ci: fix go test (#3842)
    * ci: fix go test
    
    * ci: fix go test source
    
    * ci: fix go test source

--- terraform-provider-jans/provider/data_source_schema_test.go
+++ terraform-provider-jans/provider/data_source_schema_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/moabu/terraform-provider-jans/jans"
+	"github.com/jans/terraform-provider-jans/jans"
 )
 
 func TestDataSourceSchema_Mapping(t *testing.T) {
