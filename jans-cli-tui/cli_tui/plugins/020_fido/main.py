e29139a0e74cf0e5c84942babc9f78552b51cbee
Devrim <devrimyatar@gluu.org>
2023-02-22 21:27:26 +0300

    feat(jans-cli-tui): enable super gluu option (#3970)

--- jans-cli-tui/cli_tui/plugins/020_fido/main.py
+++ jans-cli-tui/cli_tui/plugins/020_fido/main.py
@@ -126,6 +126,7 @@ class Plugin(DialogUtils):
                                             style='class:outh-scope-text'
                                             ,widget_style=cli_style.scim_widget
                                             ),
+                                self.app.getTitledCheckBox(_("Enable Super Gluu"), name='superGluuEnabled', checked=self.data.get('superGluuEnabled'), jans_help=self.app.get_help_from_schema(self.schema, 'superGluuEnabled'), style=cli_style.check_box, widget_style=cli_style.scim_widget),
                                 Window(height=1),
                                 VSplit([Window(),
                                 HSplit([Button(_("Save"), handler=self.save_config)]),
