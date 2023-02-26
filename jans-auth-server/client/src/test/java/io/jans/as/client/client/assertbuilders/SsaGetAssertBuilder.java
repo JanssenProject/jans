f9a897b406553cb75b3268a440e05da2e5454c89
Milton Ch <j.milton.chambi.m@gmail.com>
2023-02-26 01:23:27 -0400

    fix(jans-auth-server): does not create ssa if it fails to generate jwt

--- jans-auth-server/client/src/test/java/io/jans/as/client/client/assertbuilders/SsaGetAssertBuilder.java
+++ jans-auth-server/client/src/test/java/io/jans/as/client/client/assertbuilders/SsaGetAssertBuilder.java
@@ -64,6 +64,9 @@ public class SsaGetAssertBuilder extends BaseAssertBuilder {
                     assertTrue(ssaJson.has(SOFTWARE_ROLES.getName()));
                     assertTrue(ssaJson.has(GRANT_TYPES.getName()));
                     assertTrue(ssaJson.has(EXP.getName()));
+                    assertTrue(ssaJson.has(DESCRIPTION.getName()));
+                    assertTrue(ssaJson.has(ONE_TIME_USE.getName()));
+                    assertTrue(ssaJson.has(ROTATE_SSA.getName()));
                 });
             }
         }
