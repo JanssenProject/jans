668829f1ec979949049adc7a86e91748077ca144
moabu <47318409+moabu@users.noreply.github.com>
2023-03-06 13:38:17 +0000

    ci: update doc generation procedures
    Former-commit-id: d1ce9e5353284e3f3cad7d9b92df466101c59b45

--- automation/docs/generate-swagger-specs.sh
+++ automation/docs/generate-swagger-specs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -euo pipefail
-
+# The below variable represents the top level directory of the repository
+MAIN_DIRECTORY_LOCATION=$1
 echo "Generate Swagger yaml SPEC"
 
 # Compile jans-config-api to generate new Swagger SPECs from API annotations
-mvn -q -f jans-config-api/pom.xml -DskipTests clean compile
+mvn -q -f "$MAIN_DIRECTORY_LOCATION"/jans-config-api/pom.xml -DskipTests clean compile
