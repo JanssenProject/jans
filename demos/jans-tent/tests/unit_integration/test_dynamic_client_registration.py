c1b1e97cec7bc32df680e9daf9c2e86754f445b9
Christian <59786962+christian-hawk@users.noreply.github.com>
2023-03-08 12:24:04 -0300

    test(tent): restructure and uncouple tests  (#4042)
    * test(register): mock register_client method
    
    was causing this test to break
    
    Signed-off-by: christian <eland.christian@gmail.com>
    
    * test(tent): mock clienthandler methods
    
    Signed-off-by: christian <eland.christian@gmail.com>
    
    * test(tent): move mocks to a separate helper method
    
    Signed-off-by: christian <eland.christian@gmail.com>
    
    * chore(tent): bump deps
    
    httlib2, certifi, Authlib
    
    Signed-off-by: christian <eland.christian@gmail.com>
    
    * test(tent): restore stashed register_client
    
    Signed-off-by: christian <eland.christian@gmail.com>
    
    * test(tent): remove internet dependant
    
    Mocking also does not make sense
    
    Signed-off-by: christian <eland.christian@gmail.com>
    
    * test(tent): remove login endpoint integration
    
    tests are unmockable
    
    Signed-off-by: christian <eland.christian@gmail.com>
    
    * Set stable to v1.0.6
    
    * ci: delete helm index file
    
    shouldn't be committed here
    
    * ci: remove helm package from misplaced folder
    
    ---------
    
    Signed-off-by: christian <eland.christian@gmail.com>
    Co-authored-by: mo-auto <54212639+mo-auto@users.noreply.github.com>
    Co-authored-by: Mohammad Abudayyeh <47318409+moabu@users.noreply.github.com>

--- demos/jans-tent/tests/unit_integration/test_dynamic_client_registration.py
+++ demos/jans-tent/tests/unit_integration/test_dynamic_client_registration.py
@@ -8,7 +8,6 @@ import helper
 
 ClientHandler = client_handler.ClientHandler
 
-
 # helper
 def get_class_instance(op_url='https://t1.techno24x7.com',
                        client_url='https://mock.test.com'):
@@ -18,12 +17,26 @@ def get_class_instance(op_url='https://t1.techno24x7.com',
 
 class TestDynamicClientRegistration(TestCase):
 
+    def setUp(self) -> None:
+        self.register_client_stash = ClientHandler.register_client
+        self.discover_stash = ClientHandler.discover
+
+    @staticmethod
+    def mock_methods():
+        ClientHandler.register_client = MagicMock(name='register_client')
+        ClientHandler.register_client.return_value = helper.REGISTER_CLIENT_RESPONSE
+        ClientHandler.discover = MagicMock(name='discover')
+        ClientHandler.discover.return_value = helper.OP_DATA_DICT_RESPONSE
+
+    def restore_stashed_mocks(self):
+        ClientHandler.discover = self.discover_stash
+        ClientHandler.register_client = self.register_client_stash
+
     def test_if_registration_is_imported_in_sys(self):
         self.assertIn('flask_oidc.registration', sys.modules,
                       'flask_oidc.registration not found in sys')
 
     def test_if_registration_exists(self):
-
         self.assertTrue(hasattr(client_handler, 'registration'))
 
     def test_if_registration_is_flask_oidc_package(self):
@@ -32,7 +45,6 @@ class TestDynamicClientRegistration(TestCase):
             'registration is not from flask_oidc package')
 
     def test_if_discovery_is_imported(self):
-
         self.assertIn('flask_oidc.discovery', sys.modules)
 
     def test_if_discovery_is_flask_oidc_package(self):
@@ -70,7 +82,6 @@ class TestDynamicClientRegistration(TestCase):
             'register_client does not receive expected args')
 
     def test_if_register_client_params_are_expected_type(self):
-
         insp = inspect.getfullargspec(ClientHandler.register_client)
         self.assertTrue(
             insp.annotations['op_data'] == Optional[dict]
@@ -93,7 +104,6 @@ class TestDynamicClientRegistration(TestCase):
             'ClientHandler does not have initial attrs')
 
     def test_if_discover_exists(self):
-
         self.assertTrue(hasattr(ClientHandler, 'discover'),
                         'discover does not exists in ClientHandler')
 
@@ -135,14 +145,14 @@ class TestDynamicClientRegistration(TestCase):
             'revocation_endpoint', 'registration_endpoint'
         }
 
+        self.mock_methods()
         op_data = ClientHandler.discover(ClientHandler,
                                          'https://t1.techno24x7.com')
-
         self.assertTrue(main_keys <= set(op_data),
                         'discovery return data does not have main keys')
+        self.restore_stashed_mocks()
 
     def test_if_get_client_dict_exists(self):
-
         self.assertTrue(hasattr(ClientHandler, 'get_client_dict'),
                         'get_client_dict does not exists in ClientHandler')
 
@@ -167,26 +177,38 @@ class TestDynamicClientRegistration(TestCase):
                               dict, 'get_client_dict is not returning a dict')
 
     def test_class_init_should_set_op_url(self):
+        self.mock_methods()
+
         op_url = 'https://t1.techno24x7.com'
 
         client_handler_obj = get_class_instance(op_url)
+
+        self.restore_stashed_mocks()
 
         self.assertEqual(client_handler_obj.__dict__['_ClientHandler__op_url'],
                          op_url)
 
     def test_class_init_should_set_client_url(self):
+        self.mock_methods()
+
         op_url = 'https://t1.techno24x7.com'
         client_url = 'https://mock.test.com'
         client_handler_obj = ClientHandler(op_url, client_url)
-        # import ipdb; ipdb.set_trace()
+
+        self.restore_stashed_mocks()
+
         self.assertEqual(
             client_handler_obj.__dict__['_ClientHandler__client_url'],
             client_url)
 
     def test_class_init_should_set_metadata_url(self):
+        self.mock_methods()
 
         op_url = 'https://t1.techno24x7.com'
+
         client_handler_obj = get_class_instance(op_url)
+
+        self.restore_stashed_mocks()
 
         expected_metadata_url = op_url + '/.well-known/openid-configuration'
 
@@ -205,8 +227,12 @@ class TestDynamicClientRegistration(TestCase):
             'client_secret',
         ]
 
+        self.mock_methods()
+
         client_handler_obj = get_class_instance()
         client_dict = client_handler_obj.get_client_dict()
+
+        self.restore_stashed_mocks()
 
         self.assertTrue(
             all(key in client_dict.keys() for key in expected_keys),
@@ -214,37 +240,55 @@ class TestDynamicClientRegistration(TestCase):
             % (str(expected_keys), str(client_dict.keys())))
 
     def test_get_client_dict_values_cannot_be_none(self):
+        self.mock_methods()
 
         op_url = 'https://t1.techno24x7.com'
-
         client_handler_obj = get_class_instance(op_url)
-
         client_dict = client_handler_obj.get_client_dict()
+
+        self.restore_stashed_mocks()
 
         for key in client_dict.keys():
             self.assertIsNotNone(client_dict[key],
                                  'get_client_dict[%s] cannot be None!' % key)
 
     def test_get_client_dict_should_return_url_metadata_value(self):
+        self.mock_methods()
+
         client_handler_obj = get_class_instance()
+
+        self.restore_stashed_mocks()
+
         self.assertEqual(
             client_handler_obj.get_client_dict()['op_metadata_url'],
             client_handler_obj._ClientHandler__metadata_url)
 
     def test_get_client_dict_should_return_client_id_value(self):
+        self.mock_methods()
+
         client_handler_obj = get_class_instance()
+
+        self.restore_stashed_mocks()
+
         self.assertEqual(
             client_handler_obj.get_client_dict()['client_id'],
             client_handler_obj._ClientHandler__client_id
         )
 
     def test_init_should_call_discover_once(self):
-        ClientHandler.discover = MagicMock(name='discover')
-        ClientHandler.discover.return_value = helper.OP_DATA_DICT_RESPONSE
-        client_handler_obj = get_class_instance()
+        self.mock_methods()
+
+        get_class_instance()
+
         ClientHandler.discover.assert_called_once()
 
+        self.restore_stashed_mocks()
+
     def test_init_should_call_register_client_once(self):
-        ClientHandler.register_client = MagicMock(name='register_client')
-        client_handler_obj = get_class_instance()
+        self.mock_methods()
+
+        get_class_instance()
         ClientHandler.register_client.assert_called_once()
+
+        self.restore_stashed_mocks()
+
