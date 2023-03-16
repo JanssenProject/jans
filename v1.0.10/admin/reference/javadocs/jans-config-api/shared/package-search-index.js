9438225749161ef6aec53fdb97edc56c25d64e27
moauto <54212639+mo-auto@users.noreply.github.com>
2023-03-16 13:52:43 +0000

    Deployed a8fb6116fc to v1.0.10 with MkDocs 1.4.1 and mike 1.1.2

---
+++ v1.0.10/admin/reference/javadocs/jans-config-api/shared/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.jans.configapi.core.interceptor"},{"l":"io.jans.configapi.core.model"},{"l":"io.jans.configapi.core.protect"},{"l":"io.jans.configapi.core.rest"},{"l":"io.jans.configapi.core.service"},{"l":"io.jans.configapi.core.util"}]
