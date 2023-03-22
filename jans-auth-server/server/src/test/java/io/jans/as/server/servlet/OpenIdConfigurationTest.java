6084e8e2eee741c9a93a91aec209e500bb436250
YuriyZ <yzabrovarniy@gmail.com>
2023-03-22 10:26:06 +0200

    fix(jans-auth-server): simple_password_auth is missed in acr_values_supported #4258

--- jans-auth-server/server/src/test/java/io/jans/as/server/servlet/OpenIdConfigurationTest.java
+++ jans-auth-server/server/src/test/java/io/jans/as/server/servlet/OpenIdConfigurationTest.java
@@ -4,6 +4,9 @@ import io.jans.as.model.configuration.AppConfiguration;
 import org.json.JSONObject;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertTrue;
 import static org.testng.AssertJUnit.assertFalse;
 
@@ -11,6 +14,12 @@ import static org.testng.AssertJUnit.assertFalse;
  * @author Yuriy Z
  */
 public class OpenIdConfigurationTest {
+
+    @Test
+    public void getAcrValuesList_whenCalled_shouldContainInternalAuthnAlias() {
+        final List<String> acrValuesList = OpenIdConfiguration.getAcrValuesList(new ArrayList<>());
+        assertTrue(acrValuesList.contains("simple_password_auth"));
+    }
 
     @Test
     public void filterOutKeys_whenKeyIsInDentiedList_mustRemoveThemFromJson() {
