89ba6b07a22409f475ebf5d275afc0c2634530d1
Christian <59786962+christian-hawk@users.noreply.github.com>
2023-03-23 21:24:30 -0300

    fix(tent): use params according to OpenID specs (#4281)
    * fix(tent): require correct params only
    
    * fix(tent): remove unused params from `config.py`
    
    * docs(tent): remove unused `username` scope
    
    * docs(tent): edit config.py instead of copy
    
    * fix(tent): remove config_dummy

--- demos/jans-tent/clientapp/config.py
+++ demos/jans-tent/clientapp/config.py
@@ -16,37 +16,29 @@ See the License for the specific language governing permissions and
 limitations under the License.
 '''
 
-CLIENT_ID = "YOUR-CLIENT-ID"
-CLIENT_SECRET = "YOUR-CLIENT-SECRET"
-SERVER_META_URL = 'https://YOUR_OP_HOST/.well-known/openid-configuration'
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+SERVER_META_URL = 'https://op-hostname/.well-known/openid-configuration'
 REDIRECT_URIS = [
     'https://localhost:9090/oidc_callback'
 ]
-
-USERINFO_URI = "https://your-userinfo-endpoint"
-
-ISSUER = "https://your-server-fqdn"
-
 
 # Token authentication method can be
 # client_secret_basic
 # client_secret_post
 # none
-
 SERVER_TOKEN_AUTH_METHOD = "client_secret_post"
 
-# for gluu
-ACR_VALUES = 'agama'
-PRE_SELECTED_PROVIDER = False
-PRE_SELECTED_PROVIDER_ID = ''
-HAS_PROVIDER_HOST = False
-PROVIDER_HOST_STRING = None
+# ACR VALUES
+# Examples:
+# ACR_VALUES = "agama"
+# ACR_VALUES = 'simple_password_auth'
+ACR_VALUES = None
 
 # ADDITIONAL PARAMS TO CALL AUTHORIZE ENDPOINT, WITHOUT BASE64 ENCODING. USE DICT {'param': 'value'}
 # ADDITIONAL_PARAMS = {'paramOne': 'valueOne', 'paramTwo': 'valueTwo'}
 ADDITIONAL_PARAMS = None
 
-
 # SYSTEM SETTINGS
-# use with caution, unsecure requests, for develpment environments
+# use with caution, unsecure requests, for development environments
 SSL_VERIFY = False
