f3d0073542ca5118c7e0a8046056342251894825
christian <eland.christian@gmail.com>
2023-04-03 20:03:00 -0300

    feat(logger): add external lib loggers

--- demos/jans-tent/clientapp/utils/logger.py
+++ demos/jans-tent/clientapp/utils/logger.py
@@ -3,8 +3,12 @@ from logging.handlers import TimedRotatingFileHandler
 
 
 def setup_logger() -> None:
-    FORMATTER = logging.Formatter("[%(asctime)s] %(levelname)s %(name)s in %(module)s : %(message)s")
-    LOG_FILE = "test-client.log"
-    file_handler = TimedRotatingFileHandler(LOG_FILE, when='midnight')
-    file_handler.setFormatter(FORMATTER)
+    formatter = logging.Formatter("[%(asctime)s] %(levelname)s %(name)s in %(module)s : %(message)s")
+    log_file = "test-client.log"
+    file_handler = TimedRotatingFileHandler(log_file, when='midnight')
+    file_handler.setFormatter(formatter)
+    logging.getLogger("oic")
+    logging.getLogger("oauth")
+    logging.getLogger("flask-oidc")
+    logging.getLogger("urllib3")
     logging.basicConfig(level=logging.DEBUG, handlers=[file_handler])
