dd1e323096b5d5593475773bb9b9fe070c42f048
christian <eland.christian@gmail.com>
2023-04-03 12:46:41 -0300

    feat(dcr): variables loads from config file

--- demos/jans-tent/tests/unit_integration/test_dcr_from_config.py
+++ demos/jans-tent/tests/unit_integration/test_dcr_from_config.py
@@ -1,4 +1,6 @@
 from clientapp.utils import dcr_from_config
+from clientapp import config as cfg
+
 class TestDrcFromConfig():
     def test_if_setup_logging_exists(self):
         assert hasattr(dcr_from_config, 'setup_logging')
@@ -6,3 +8,7 @@ class TestDrcFromConfig():
     def test_if_static_variables_exists(self):
         assert hasattr(dcr_from_config, 'OP_URL')
         assert hasattr(dcr_from_config, 'REDIRECT_URIS')
+
+    def test_if_static_variables_from_config(self):
+        assert dcr_from_config.OP_URL == cfg.ISSUER
+        assert dcr_from_config.REDIRECT_URIS == cfg.REDIRECT_URIS
