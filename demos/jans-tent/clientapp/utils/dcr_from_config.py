67b06339d1f88aadbeee8fcbfb20324aaec082ff
christian <eland.christian@gmail.com>
2023-04-03 12:44:55 -0300

    feat(dcr): ensure static variables from cfg

--- demos/jans-tent/clientapp/utils/dcr_from_config.py
+++ demos/jans-tent/clientapp/utils/dcr_from_config.py
@@ -1,5 +1,14 @@
 import logging
+from clientapp import config as cfg
 
+OP_URL = cfg.ISSUER
+REDIRECT_URIS = cfg.REDIRECT_URIS
 
 def setup_logging() -> None:
-    ...
+    logging.getLogger('oic')
+    logging.getLogger('urllib3')
+    logging.basicConfig(
+        level=logging.DEBUG,
+        handlers=[logging.StreamHandler(), logging.FileHandler('register_new_client.log')],
+        format='[%(asctime)s] %(levelname)s %(name)s in %(module)s : %(message)s')
+
--- demos/jans-tent/tests/unit_integration/test_dcr_from_config.py
+++ demos/jans-tent/tests/unit_integration/test_dcr_from_config.py
@@ -2,3 +2,7 @@ from clientapp.utils import dcr_from_config
 class TestDrcFromConfig():
     def test_if_setup_logging_exists(self):
         assert hasattr(dcr_from_config, 'setup_logging')
+
+    def test_if_static_variables_exists(self):
+        assert hasattr(dcr_from_config, 'OP_URL')
+        assert hasattr(dcr_from_config, 'REDIRECT_URIS')
