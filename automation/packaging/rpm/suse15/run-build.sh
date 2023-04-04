9d10c161c6df924a1fe28730e136972bbbc4c9f4
moabu <47318409+moabu@users.noreply.github.com>
2023-04-04 13:11:57 +0000

    ci: add jans.spec

--- automation/packaging/rpm/suse15/run-build.sh
+++ automation/packaging/rpm/suse15/run-build.sh
@@ -2,7 +2,7 @@
 
 VERSION=$(echo "%VERSION%" | awk -F '-' {'print $1'})
 REL=$(echo "%VERSION%" | sed "s/^${VERSION}//g" | sed "s/^-//g")
-current_dir=`pwd`
+current_dir=$PWD
 sed -i "s/%VER%/$VERSION/g" jans.spec
 if [ -z "$REL" ]; then
         RELEASE="suse15"
@@ -11,13 +11,13 @@ else
 fi
 sed -i "s/%RELEASE%/$RELEASE/g" jans.spec
 rpmbuild_path="$current_dir/rpmbuild"
-mkdir -p $rpmbuild_path/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+mkdir -p "$rpmbuild_path"/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 specfile=jans.spec
-cp $current_dir/$specfile $rpmbuild_path/SPECS/.
-mv jans-src jans-$VERSION
-tar cvfz jans-$VERSION.tar.gz jans-$VERSION
-cp jans-$VERSION.tar.gz $rpmbuild_path/SOURCES/.
+cp "$current_dir"/$specfile "$rpmbuild_path"/SPECS/.
+mv jans-src jans-"$VERSION"
+tar cvfz jans-"$VERSION".tar.gz jans-"$VERSION"
+cp jans-"$VERSION".tar.gz "$rpmbuild_path"/SOURCES/.
 rm -rf rpmbuild/RPMS/x86_64/*
-rpmbuild -bb --define "_topdir $rpmbuild_path" $rpmbuild_path/SPECS/$specfile
-chmod a+w rpmbuild/RPMS/x86_64/jans-$VERSION-$RELEASE.x86_64.rpm
+rpmbuild -bb --define "_topdir $rpmbuild_path" "$rpmbuild_path"/SPECS/$specfile
+chmod a+w rpmbuild/RPMS/x86_64/jans-"$VERSION"-"$RELEASE".x86_64.rpm
 chmod a+w rpmbuild/RPMS/x86_64
