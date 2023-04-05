c2c24799d5346d0962fbd320e799baf7a25ff051
YuriyZ <yzabrovarniy@gmail.com>
2023-04-05 20:03:58 +0300

    feat(jans-auth-server): show error with clear message if exception occurs authz check #4449

--- jans-auth-server/server/src/main/java/io/jans/as/server/authorize/ws/rs/AuthorizeAction.java
+++ jans-auth-server/server/src/main/java/io/jans/as/server/authorize/ws/rs/AuthorizeAction.java
@@ -8,6 +8,8 @@ package io.jans.as.server.authorize.ws.rs;
 
 import io.jans.as.common.model.common.User;
 import io.jans.as.common.model.registration.Client;
+import io.jans.as.common.model.session.SessionId;
+import io.jans.as.common.model.session.SessionIdState;
 import io.jans.as.model.authorize.AuthorizeErrorResponseType;
 import io.jans.as.model.common.Prompt;
 import io.jans.as.model.common.SubjectType;
@@ -19,6 +21,7 @@ import io.jans.as.model.jwt.JwtClaimName;
 import io.jans.as.model.util.Base64Util;
 import io.jans.as.model.util.JwtUtil;
 import io.jans.as.model.util.Util;
+import io.jans.as.persistence.model.ClientAuthorization;
 import io.jans.as.persistence.model.Scope;
 import io.jans.as.server.auth.Authenticator;
 import io.jans.as.server.i18n.LanguageBean;
@@ -28,11 +31,8 @@ import io.jans.as.server.model.authorize.JwtAuthorizationRequest;
 import io.jans.as.server.model.authorize.ScopeChecker;
 import io.jans.as.server.model.common.CibaRequestCacheControl;
 import io.jans.as.server.model.common.DefaultScope;
-import io.jans.as.common.model.session.SessionId;
-import io.jans.as.common.model.session.SessionIdState;
 import io.jans.as.server.model.config.Constants;
 import io.jans.as.server.model.exception.AcrChangedException;
-import io.jans.as.persistence.model.ClientAuthorization;
 import io.jans.as.server.security.Identity;
 import io.jans.as.server.service.*;
 import io.jans.as.server.service.ciba.CibaRequestService;
@@ -49,14 +49,7 @@ import io.jans.service.net.NetworkService;
 import io.jans.util.OxConstants;
 import io.jans.util.StringHelper;
 import io.jans.util.ilocale.LocaleUtil;
-import org.apache.commons.lang.BooleanUtils;
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.text.StringEscapeUtils;
-import org.apache.logging.log4j.util.Strings;
-import org.slf4j.Logger;
-
 import jakarta.enterprise.context.RequestScoped;
-import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.ExternalContext;
 import jakarta.faces.context.FacesContext;
 import jakarta.inject.Inject;
@@ -67,6 +60,12 @@ import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.apache.commons.lang.BooleanUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.text.StringEscapeUtils;
+import org.apache.logging.log4j.util.Strings;
+import org.slf4j.Logger;
+
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
@@ -229,7 +228,16 @@ public class AuthorizeAction {
         }
     }
 
-    public void checkPermissionGranted() throws IOException {
+    public void checkPermissionGranted() {
+        try {
+            checkPermissionGrantedInternal();
+        } catch (Exception e) {
+            log.error("Failed to perform checkPermissionGranted()", e);
+            permissionDenied();
+        }
+    }
+
+    public void checkPermissionGrantedInternal() throws IOException {
         if ((clientId == null) || clientId.isEmpty()) {
             log.debug("Permission denied. client_id should be not empty.");
             permissionDenied();
@@ -813,8 +821,8 @@ public class AuthorizeAction {
         return sessionId;
     }
 
-    public void setSessionId(String p_sessionId) {
-        sessionId = p_sessionId;
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
     }
 
     public void permissionGranted() {
@@ -830,11 +838,6 @@ public class AuthorizeAction {
     public void permissionDenied() {
         final SessionId session = getSession();
         authorizeService.permissionDenied(session);
-    }
-
-    private void authenticationFailedSessionInvalid() {
-        facesMessages.add(FacesMessage.SEVERITY_ERROR, "login.errorSessionInvalidMessage");
-        facesService.redirect("/error.xhtml");
     }
 
     public void invalidRequest() {
