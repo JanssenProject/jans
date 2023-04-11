35be21d536460516feddb752262ecd3624789692
YuriyZ <yzabrovarniy@gmail.com>
2023-04-11 12:37:27 +0300

    fix(jans-auth-server) - client registration request defaults to code flow #4296 (#4540)
    * fix(jans-auth-server) - client registration request defaults to code flow #4296
    
    * test(jans-auth-server) - added more tests for registration response and grant types #4296
    
    Former-commit-id: 3f76ab64808fb4182710844462c1d676731243bd

--- jans-auth-server/server/src/test/java/io/jans/as/server/register/ws/rs/RegisterServiceTest.java
+++ jans-auth-server/server/src/test/java/io/jans/as/server/register/ws/rs/RegisterServiceTest.java
@@ -4,7 +4,10 @@ import com.beust.jcommander.internal.Lists;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Sets;
 import io.jans.as.common.service.AttributeService;
+import io.jans.as.model.common.GrantType;
+import io.jans.as.model.common.ResponseType;
 import io.jans.as.model.configuration.AppConfiguration;
 import io.jans.as.model.error.ErrorResponseFactory;
 import io.jans.as.server.ciba.CIBARegisterClientMetadataService;
@@ -17,6 +20,9 @@ import org.mockito.testng.MockitoTestNGListener;
 import org.slf4j.Logger;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertTrue;
@@ -48,6 +54,88 @@ public class RegisterServiceTest {
 
     @Mock
     private CIBARegisterClientMetadataService cibaRegisterClientMetadataService;
+
+    @Test
+    public void identifyResponseType_whenResponseTypeIsBlank_shouldFallbackToCodeValue() {
+        when(appConfiguration.getAllResponseTypesSupported()).thenReturn(Sets.newHashSet(ResponseType.values()));
+
+        final Set<ResponseType> result = registerService.identifyResponseTypes(new ArrayList<>(), new ArrayList<>());
+        assertTrue(result.contains(ResponseType.CODE));
+    }
+
+    @Test
+    public void identifyResponseType_whenResponseTypeIsNotSupported_shouldRemoveIt() {
+        when(appConfiguration.getAllResponseTypesSupported()).thenReturn(Sets.newHashSet());
+
+        final Set<ResponseType> result = registerService.identifyResponseTypes(new ArrayList<>(), new ArrayList<>());
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void identifyResponseType_whenResponseTypeIsAbsentButAutofixEnabledForAuthorizationCode_shouldFix() {
+        when(appConfiguration.getAllResponseTypesSupported()).thenReturn(Sets.newHashSet(ResponseType.values()));
+        when(appConfiguration.getGrantTypesAndResponseTypesAutofixEnabled()).thenReturn(true);
+
+        final Set<ResponseType> result = registerService.identifyResponseTypes(new ArrayList<>(), Lists.newArrayList(GrantType.AUTHORIZATION_CODE));
+        assertTrue(result.contains(ResponseType.CODE));
+    }
+
+    @Test
+    public void identifyResponseType_whenResponseTypeIsAbsentButAutofixEnabledForToken_shouldFix() {
+        when(appConfiguration.getAllResponseTypesSupported()).thenReturn(Sets.newHashSet(ResponseType.values()));
+        when(appConfiguration.getGrantTypesAndResponseTypesAutofixEnabled()).thenReturn(true);
+
+        final Set<ResponseType> result = registerService.identifyResponseTypes(new ArrayList<>(), Lists.newArrayList(GrantType.IMPLICIT));
+        assertTrue(result.contains(ResponseType.TOKEN));
+    }
+
+    @Test
+    public void identifyGrantType_whenGrantTypeIsBlank_shouldFallbackToCodeValue() {
+        when(appConfiguration.getGrantTypesSupported()).thenReturn(Sets.newHashSet(GrantType.values()));
+        when(appConfiguration.getDynamicGrantTypeDefault()).thenReturn(Sets.newHashSet(GrantType.values()));
+
+        final Set<GrantType> result = registerService.identifyGrantTypes(new ArrayList<>(), new ArrayList<>());
+        assertTrue(result.contains(GrantType.AUTHORIZATION_CODE));
+    }
+
+    @Test
+    public void identifyGrantType_whenGrantTypeIsNotSupported_shouldRemoveIt() {
+        when(appConfiguration.getGrantTypesSupported()).thenReturn(Sets.newHashSet());
+        when(appConfiguration.getDynamicGrantTypeDefault()).thenReturn(Sets.newHashSet(GrantType.values()));
+
+        final Set<GrantType> result = registerService.identifyGrantTypes(new ArrayList<>(), new ArrayList<>());
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void identifyGrantType_whenGrantTypeIsAbsentButAutofixEnabledForAuthorizationCode_shouldFix() {
+        when(appConfiguration.getGrantTypesSupported()).thenReturn(Sets.newHashSet(GrantType.values()));
+        when(appConfiguration.getDynamicGrantTypeDefault()).thenReturn(Sets.newHashSet(GrantType.values()));
+        when(appConfiguration.getGrantTypesAndResponseTypesAutofixEnabled()).thenReturn(true);
+
+        final Set<GrantType> result = registerService.identifyGrantTypes(Lists.newArrayList(ResponseType.CODE), new ArrayList<>());
+        assertTrue(result.contains(GrantType.AUTHORIZATION_CODE));
+    }
+
+    @Test
+    public void identifyGrantType_whenGrantTypeIsAbsentButAutofixEnabledForToken_shouldFix() {
+        when(appConfiguration.getGrantTypesSupported()).thenReturn(Sets.newHashSet(GrantType.values()));
+        when(appConfiguration.getDynamicGrantTypeDefault()).thenReturn(Sets.newHashSet(GrantType.values()));
+        when(appConfiguration.getGrantTypesAndResponseTypesAutofixEnabled()).thenReturn(true);
+
+        final Set<GrantType> result = registerService.identifyGrantTypes(Lists.newArrayList(ResponseType.TOKEN), new ArrayList<>());
+        assertTrue(result.contains(GrantType.IMPLICIT));
+    }
+
+    @Test
+    public void identifyGrantType_whenGrantTypeIsAbsentButAutofixEnabledForIdToken_shouldFix() {
+        when(appConfiguration.getGrantTypesSupported()).thenReturn(Sets.newHashSet(GrantType.values()));
+        when(appConfiguration.getDynamicGrantTypeDefault()).thenReturn(Sets.newHashSet(GrantType.values()));
+        when(appConfiguration.getGrantTypesAndResponseTypesAutofixEnabled()).thenReturn(true);
+
+        final Set<GrantType> result = registerService.identifyGrantTypes(Lists.newArrayList(ResponseType.ID_TOKEN), new ArrayList<>());
+        assertTrue(result.contains(GrantType.IMPLICIT));
+    }
 
     @Test
     public void addDefaultCustomAttributes_whenCalledWithExistingConfigurationData_shouldPopulateRequestObject() throws JsonProcessingException {
