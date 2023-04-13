3dd993497e4b332715435500107b1f3b6b3ab44e
Christian <59786962+christian-hawk@users.noreply.github.com>
2023-04-13 14:58:29 -0300

    test(tent): mock auto register on server start (#4569)
    * tests(dcr-from-config): mock file open
    
    as is called on app init
    
    * test(factory): mocks without starting server
    
    * test: move flask base to helper
    
    avoid duplicates
    
    Signed-off-by: christian <eland.christian@gmail.com>
    
    * style(init): add blank line
    
    Signed-off-by: christian <eland.christian@gmail.com>
    
    ---------
    
    Signed-off-by: christian <eland.christian@gmail.com>

--- demos/jans-tent/clientapp/utils/dcr_from_config.py
+++ demos/jans-tent/clientapp/utils/dcr_from_config.py
@@ -6,6 +6,7 @@ import json
 OP_URL = cfg.ISSUER
 REDIRECT_URIS = cfg.REDIRECT_URIS
 
+
 def setup_logging() -> None:
     logging.getLogger('oic')
     logging.getLogger('urllib3')
@@ -17,10 +18,9 @@ def setup_logging() -> None:
 
 def register() -> None:
     logger = logging.getLogger(__name__)
-    logger.info('Trying to auto-register client from configuration')
     client_handler = ClientHandler(OP_URL, REDIRECT_URIS)
     json_client_info = json.dumps(client_handler.get_client_dict(), indent=4)
     with open('client_info.json', 'w') as outfile:
-        logger.info('Writing registered client information to clien_info.json')
+        logger.info('Writing registered client information to client_info.json')
         outfile.write(json_client_info)
 
