8d7acf9ddad26ff3bde8b8fdf0e99363f907c841
Mohammad Abudayyeh <47318409+moabu@users.noreply.github.com>
2023-04-17 07:18:16 +0000

    feat: change state handling of global configs that support patch updates and switched location type from ldap to db
    Signed-off-by: moabu <47318409+moabu@users.noreply.github.com>
    Former-commit-id: 599c20f4f448892a782f1159ab96e2ae6b3f637c

--- terraform-provider-jans/provider/resource_scim_app_configuration_test.go
+++ terraform-provider-jans/provider/resource_scim_app_configuration_test.go
@@ -42,6 +42,15 @@ func TestResourceScimAppConfiguration_Mapping(t *testing.T) {
 
 	newCfg := jans.ScimAppConfigurations{}
 
+	patches, err := patchFromResourceData(data, &newCfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(patches) != 16 {
+		t.Errorf("Got %d patches, expected 16", len(patches))
+	}
+
 	if err := fromSchemaResource(data, &newCfg); err != nil {
 		t.Fatal(err)
 	}
