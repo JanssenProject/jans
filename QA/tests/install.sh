27362e2a61eaa2fbc9c2d98fb4d18076c371f9d0
manoj suryawanshi <manojsurya78@gmail.com>
2023-04-20 12:21:10 +0530

    Update install.sh

--- QA/tests/install.sh
+++ QA/tests/install.sh
@@ -15,14 +15,14 @@
 
 install_jans() {
 
-sudo ssh -i ~/private.pem ${USERNAME}@${IPADDRESS} << EOF
+sudo ssh -i ~/private_ubuntu22.pem ${USERNAME}@${IPADDRESS} << EOF
 sudo python3 install.py -uninstall -y
 exit
 EOF
 
 rm setup.properties
 # IP_ADDRESS=$HOST
-# HOSTNAME=`sudo ssh -i ~/private.pem ${USERNAME}@${IPADDRESS} hostname`
+# HOSTNAME=`sudo ssh -i ~/private_ubuntu22.pem ${USERNAME}@${IPADDRESS} hostname`
  
 # echo "$USERNAME"
 # echo "$HOST"
@@ -70,93 +70,137 @@ if [[ $DB == opendj ]]
 		echo "please select any DB"
 fi
 		
-		curl https://raw.githubusercontent.com/JanssenProject/jans/v${BRANCH}/jans-linux-setup/jans_setup/install.py > install.py
+		curl https://raw.githubusercontent.com/JanssenProject/jans/v${VERSION}/jans-linux-setup/jans_setup/install.py > install.py
 		echo "install downloaded"
-		sudo scp  -i ~/private.pem  setup.properties install.py ${USERNAME}@${IPADDRESS}:~/
+		sudo scp  -i ~/private_ubuntu22.pem  setup.properties install.py ${USERNAME}@${IPADDRESS}:~/
 		rm setup.properties install.py
-		sudo ssh -i ~/private.pem ${USERNAME}@${IPADDRESS} << EOF
-if [[ $OS == ubuntu ]];
+		
+if [[ $OS == ubuntu22 ]] || [[ $OS == ubuntu20 ]];
     then
+		sudo ssh -i ~/private_ubuntu22.pem ${USERNAME}@${IPADDRESS} << EOF
  		sudo apt install  python3-pip -y
+		echo "package download started"
+		wget https://github.com/JanssenProject/jans/releases/download/v${VERSION}.nightly/jans_${VERSION}.nightly.${OS}.04_amd64.deb
+		echo "package downloaded"
+EOF
 	elif [[ $OS == rhel ]] || [[ $OS == centos ]];
     then
+			sudo ssh -i ~/private_ubuntu22.pem ${USERNAME}@${IPADDRESS} << EOF
 			sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
-			sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-
-			sudo /usr/bin/crb enable
-	 		sudo dnf module enable mod_auth_openidc
+			sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+			sudo yum -y module enable mod_auth_openidc 
 			sudo yum update -y
-			sudo yum install python3.8 -y
 			sudo yum install  python3-pip -y
+			sudo yum  install python3-certifi python3-ldap3 python3-prompt-toolkit python3-ruamel-yaml
+			wget https://github.com/JanssenProject/jans/releases/download/v${VERSION}.nightly/jans-${VERSION}.nightly-el8.x86_64.rpm
+			
+			sudo reboot
+			wait 300	
+EOF
 	elif [[ $OS == suse ]];
 	then
-			sudo zypper install python3-pip
-			sudo pip3 install python3-PyMySQL PyMySQL ruamel.yaml
+			sudo ssh -i ~/private_ubuntu22.pem ${USERNAME}@${IPADDRESS} << EOF			
+			#sudo zypper update -y
+			#sudo reboot		
+#EOF
+#sleep 360
+			#sudo ssh -i ~/private_ubuntu22.pem ${USERNAME}@${IPADDRESS} << EOF
+			echo "downloading package"
+#			wget https://github.com/JanssenProject/jans/releases/download/v${VERSION}.nightly/jans-${VERSION}.nightly-suse15.x86_64.rpm
+			
+#			sudo zypper addrepo https://download.opensuse.org/repositories/home:frispete:python/15.4/home:frispete:python.repo"
+	echo "in shell"
+			sudo zypper --non-interactive --gpg-auto-import-keys refresh
+			sudo zypper --non-interactive --gpg-auto-import-keys install python3-PyMySQL
+			#/usr/bin/expect -c 'spawn sudo zypper install python3-PyMySQL;expect -re "(.*)";send -- "y\r";expect eof'
+
+#send -- "a\r"
+#expect -re "(.*)\n"
+
+EOF
+	
 	else
 		echo "db not selected"
 fi
-	if [[ $OS == rhel ]] || [[ $OS == centos ]] || [[ $OS == ubuntu ]] || [[ $OS == suse ]] && [[ $DB == couchbase ]] ;
-	then
-                        sudo mkdir -p /opt/dist/couchbase
-                        cd /opt/dist/couchbase
-						echo "***********************************************************$PWD"
-			if [[ $OS == rhel ]] || [[ $OS == centos ]]
-			then
- 			sudo wget https://packages.couchbase.com/releases/7.1.1/couchbase-server-enterprise-7.1.1-rhel8.x86_64.rpm
-			cd ~
-			elif [[ $OS == ubuntu ]]
-			then 
-			echo "$PWD"
-			sudo wget https://packages.couchbase.com/releases/7.1.1/couchbase-server-enterprise_7.1.1-ubuntu20.04_amd64.deb
-			cd ~
-			elif [[ $OS == suse ]]
-			then 
-			sudo wget https://packages.couchbase.com/releases/7.1.1/couchbase-server-enterprise-7.1.1-suse15.x86_64.rpm
-			else
-			echo " no couchbase DB rpm found "
-			fi
-	else
-			echo " OS not found"
-fi
-exit
-EOF
-sudo ssh -i ~/private.pem ${USERNAME}@${IPADDRESS} << EOF
-	echo " installation started"
-	sleep 20
-	if [[ ${FLEX_OR_JANS} == "jans" ]];
-	then 
-	sudo python3 install.py -y --args="-f setup.properties -c -n --cli-test-client"
-	echo " installation ended"
-	elif [[ ${FLEX_OR_JANS} == "flex" ]];
-	then
-		wget https://raw.githubusercontent.com/GluuFederation/flex/main/flex-linux-setup/flex_linux_setup/flex_setup.py  -O flex_setup.py
-		python3 flex_setup.py -f setup.properties -n -c
-		cd /opt/jans/jetty/casa
-		touch .administrable
 
+# 	if [[ $OS == rhel ]] || [[ $OS == centos ]] || [[ $OS == ubuntu ]] || [[ $OS == suse ]] && [[ $DB == couchbase ]] ;
+# 	then
+#                         sudo mkdir -p /opt/dist/couchbase
+#                         cd /opt/dist/couchbase
+# 						echo "***********************************************************$PWD"
+# 			if [[ $OS == rhel ]] || [[ $OS == centos ]]
+# 			then
+#  			sudo wget https://packages.couchbase.com/releases/7.1.1/couchbase-server-enterprise-7.1.1-rhel8.x86_64.rpm
+# 			cd ~
+# 			elif [[ $OS == ubuntu ]]
+# 			then 
+# 			echo "$PWD"
+# 			sudo wget https://packages.couchbase.com/releases/7.1.1/couchbase-server-enterprise_7.1.1-ubuntu20.04_amd64.deb
+# 			cd ~
+# 			elif [[ $OS == suse ]]
+# 			then 
+# 			sudo wget https://packages.couchbase.com/releases/7.1.1/couchbase-server-enterprise-7.1.1-suse15.x86_64.rpm
+# 			else
+# 			echo " no couchbase DB rpm found "
+# 			fi
+# 	else
+# 			echo " OS not found"
+# fi
+
+
+	echo " installation started"
+	if [[ ${PACKAGE_OR_ONLINE} == "online" ]];
+	then 
+	sudo ssh -i ~/private_ubuntu22.pem ${USERNAME}@${IPADDRESS} << EOF 
+	sudo python3 install.py -y --args="-f setup.properties -c -n --cli-test-client" 
+EOF
+	echo " installation ended"
+	elif [[ ${PACKAGE_OR_ONLINE} == "package" ]];
+	then
+		
+		if [[ ${OS} == "suse"  ]]
+		then
+		echo "package installation started"
+		sudo ssh -i ~/private_ubuntu22.pem ${USERNAME}@${IPADDRESS} << EOF
+			sudo zypper --no-gpg-checks install -y ./jans-${VERSION}.nightly-suse15.x86_64.rpm
+			sudo python3 /opt/jans/jans-setup/setup.py -f setup.properties -n
+EOF
+		fi
+		if [[ ${OS} == "rhel"  ]]
+		then
+		echo "package installation started"
+		sudo ssh -i ~/private_ubuntu22.pem ${USERNAME}@${IPADDRESS} << EOF
+			sudo yum install ./jans-${VERSION}.nightly-suse15.x86_64.rpm
+			sudo python3 /opt/jans/jans-setup/setup.py -f setup.properties -n
+EOF
+		fi
+		if [[ ${OS} == "ubuntu20"  ]] || [[ ${OS} == "ubuntu22"  ]]
+		then
+			echo "package installation started"
+			sudo ssh -i ~/private_ubuntu22.pem ${USERNAME}@${IPADDRESS} << EOF
+			sudo apt install -y ./jans_${VERSION}.nightly.${OS}.04_amd64.deb
+			sudo python3 /opt/jans/jans-setup/setup.py -f setup.properties -n
+EOF
+		fi
 	else
 		echo " please enter which product you want to install"
 	
 	fi
 
 
-exit
-EOF
-
-echo "i am here"
 }	
 
 
 helpFunction()
 {
    echo ""
-   echo "Usage:  -i ipaddress -h hostname -u username -d db -o os -f flex or jans -b branch"
-   echo -e "\t -i ipaddress -h hostname -u username -d db -o os -f flex or jans  -b branch"
+   echo "Usage: ./install.sh -i ipaddress -h hostname -u username -d db -o os  -b VERSION -p PACKAGE_OR_ONLINE"
+   echo -e "\t ./install.sh  3.10.10.22  manojs1978-pleasing-goldfish.gluu.info  ec2-user  ldap  ubuntu22 or suse or rhel or ubuntu20  1.0.12  package"
    exit 1 # Exit script after printing help
 }
 
-unset IPADDRESS HOSTNAME USERNAME DB OS FLEX_OR_JANS
-while getopts i:h:u:d:o:f:b: option
+unset IPADDRESS HOSTNAME USERNAME DB OS  PACKAGE_OR_ONLINE
+while getopts i:h:u:d:o:f:b:p: option
 do
 case "${option}"
 in
@@ -165,34 +209,34 @@ h) HOSTNAME=${OPTARG};;
 u) USERNAME=${OPTARG};;
 d) DB=${OPTARG};;
 o) OS=${OPTARG};;
-f) FLEX_OR_JANS=${OPTARG};;
-b) BRANCH=${BRANCH};;
-
+b) VERSION=${VERSION};;
+p) PACKAGE_OR_ONLINE=${PACKAGE_OR_ONLINE};;
 esac
 done
-#BRANCH=main
+#VERSION=main
 # Print helpFunction in case parameters are empty
-# sudo ssh -i ~/private.pem $USERNAME@$HOST
+# sudo ssh -i ~/private_ubuntu22.pem $USERNAME@$HOST
 
  IPADDRESS=$1
  HOSTNAME=$2
  USERNAME=$3
  DB=$4
  OS=$5
- FLEX_OR_JANS=$6
- BRANCH=$7
+ VERSION=$6
+ PACKAGE_OR_ONLINE=$7
+
 # Begin script in case all parameters are correct
 echo "your ip address is ${IPADDRESS}"
 echo "your hostname is ${HOSTNAME}"
 echo "your username is ${USERNAME}"
 echo "your DB is ${DB}"
 echo "your  OS is  ${OS}"
-echo "you want to install  ${FLEX_OR_JANS}"
-echo "your BRANCH is ${BRANCH}"
+echo "your VERSION is ${VERSION}"
+echo "installation type is ${PACKAGE_OR_ONLINE}"
 
-if [ -z ${IPADDRESS} ] && [ -z ${HOSTNAME} ] && [ -z ${USERNAME} ] && [ -z ${DB} ] && [ -z ${OS} ] && [ -z ${FLEX_OR_JANS} ] && [ -z ${BRANCH} ]
+if [ -z ${IPADDRESS} ] && [ -z ${HOSTNAME} ] && [ -z ${USERNAME} ] && [ -z ${DB} ] && [ -z ${OS} ] && [ -z ${VERSION} ] && [ -z ${PACKAGE_OR_ONLINE} ]
   then
-	echo " host parameter is empty or username or passwd is not added"
+	echo " some parameter are empty please check help"
 	helpFunction
 	else
 	install_jans
