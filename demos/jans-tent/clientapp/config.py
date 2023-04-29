b5cdea681f8796e44974bfdbca1947978fbc9f9f
christian <eland.christian@gmail.com>
2023-04-29 15:55:48 -0300

    fix(config): revert last commit for this file
    It was added by mistake
    
    Signed-off-by: christian <eland.christian@gmail.com>

--- demos/jans-tent/clientapp/config.py
+++ demos/jans-tent/clientapp/config.py
@@ -1,6 +1,6 @@
 # REQUIRED
 # Replace op_hostname
-ISSUER = 'https://christian-hawk-awake-grub.gluu.info'
+ISSUER = 'https://op_hostname'
 
 # Tent redirect uri
 REDIRECT_URIS = [
@@ -17,13 +17,13 @@ SERVER_TOKEN_AUTH_METHOD = 'client_secret_post'
 
 # ACR VALUES
 # Examples:
-ACR_VALUES = "agama"
+# ACR_VALUES = "agama"
 # ACR_VALUES = 'simple_password_auth'
-# ACR_VALUES = None
+ACR_VALUES = None
 
 # ADDITIONAL PARAMS TO CALL AUTHORIZE ENDPOINT, WITHOUT BASE64 ENCODING. USE DICT {'param': 'value'}
 # ADDITIONAL_PARAMS = {'paramOne': 'valueOne', 'paramTwo': 'valueTwo'}
-ADDITIONAL_PARAMS = {'agama_flow': 'com.chris.basic'}
+ADDITIONAL_PARAMS = None
 
 # SYSTEM SETTINGS
 # use with caution, unsecure requests, for development environments
