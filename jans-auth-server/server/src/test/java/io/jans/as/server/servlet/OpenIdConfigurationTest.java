7cbd10928a7dc59275c2ec3b4d43a64bffde86d8
YuriyZ <yzabrovarniy@gmail.com>
2023-05-10 13:56:27 +0300

    fix(jans-auth-server): Illegal op_policy_uri parameter: - exclude entries with blank values from discovery response (oxauth counterpart) #4888 (#4934)

--- jans-auth-server/server/src/test/java/io/jans/as/server/servlet/OpenIdConfigurationTest.java
+++ jans-auth-server/server/src/test/java/io/jans/as/server/servlet/OpenIdConfigurationTest.java
@@ -9,11 +9,43 @@ import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertEquals;
 
 /**
  * @author Yuriy Z
  */
 public class OpenIdConfigurationTest {
+
+    @Test
+    public void filterOutKeys_withBlankValues_shouldRemoveKeys() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("key1", "");
+        jsonObject.put("key2", "value2");
+        jsonObject.put("key3", "  ");
+
+        OpenIdConfiguration.filterOutKeys(jsonObject, new AppConfiguration());
+
+        assertEquals("value2", jsonObject.get("key2"));
+        assertFalse(jsonObject.has("key1"));
+        assertFalse(jsonObject.has("key3"));
+    }
+
+    @Test
+    public void filterOutKeys_withBlankValuesAndAllowedBlankValuesInConfig_shouldNotRemoveKeys() {
+        final AppConfiguration appConfiguration = new AppConfiguration();
+        appConfiguration.setAllowBlankValuesInDiscoveryResponse(true);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("key1", "");
+        jsonObject.put("key2", "value2");
+        jsonObject.put("key3", "  ");
+
+        OpenIdConfiguration.filterOutKeys(jsonObject, appConfiguration);
+
+        assertEquals("value2", jsonObject.get("key2"));
+        assertTrue(jsonObject.has("key1"));
+        assertTrue(jsonObject.has("key3"));
+    }
 
     @Test
     public void getAcrValuesList_whenCalled_shouldContainInternalAuthnAlias() {
