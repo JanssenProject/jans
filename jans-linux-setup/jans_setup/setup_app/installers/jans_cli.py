f0130b2a7aeff01545784335a0e67489c9bf4eb9
Mustafa Baser <mbaser@mail.com>
2023-05-13 12:59:07 +0300

    feat(jans-linux-setup): typo

--- jans-linux-setup/jans_setup/setup_app/installers/jans_cli.py
+++ jans-linux-setup/jans_setup/setup_app/installers/jans_cli.py
@@ -80,7 +80,7 @@ class JansCliInstaller(BaseInstaller, SetupUtils):
 
         # extract yaml files
         base.extract_file(base.current_app.jans_zip, 'jans-config-api/docs/jans-config-api-swagger.yaml', os.path.join(ops_dir, 'jca', 'jans-config-api-swagger.yaml'), ren=True)
-        plugin_yamls = ['fido2-plugin-swagger.yaml', 'jans-admin-ui-plugin-swagger.yaml', 'scim-plugin-swagger.yaml', 'user-mgt-plugin-swagger.yaml')
+        plugin_yamls = ['fido2-plugin-swagger.yaml', 'jans-admin-ui-plugin-swagger.yaml', 'scim-plugin-swagger.yaml', 'user-mgt-plugin-swagger.yaml']
 
         if Config.install_cache_refresh:
             plugin_yamls.append('cache-refresh-plugin-swagger.yaml')
