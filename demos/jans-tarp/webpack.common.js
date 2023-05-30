8f9c4f06dbf40587bc0a784c089d36343833eba2
Arnab Dutta <arnab.bdutta@gmail.com>
2023-05-30 17:57:30 +0530

    feat(jans-tarp): migrate tarp from vanilla javascript to react.js and typescript #5123 (#5124)
    * feat: migrate tarp from vanilla javascript to raect.js and tyescript #5123
    
    * feat: add loader
    
    * feat: add loader
    
    * feat: address code review comments
    
    * feat: address code review comments
    
    Former-commit-id: 13ab94f2af8eb63bb3ddf263cef55e05584772cc

---
+++ demos/jans-tarp/webpack.common.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const tailwindcss = require('tailwindcss')
+const autoprefixer = require('autoprefixer')
+
+module.exports = {
+    entry: {
+        popup: path.resolve('src/popup/index.tsx'),
+        options: path.resolve('src/options/index.tsx'),
+        background: path.resolve('src/background/background.ts'),
+        contentScript: path.resolve('src/contentScript/contentScript.ts'),
+    },
+    module: {
+        rules: [
+            {
+                use: 'ts-loader',
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader', // postcss loader needed for tailwindcss
+                        options: {
+                            postcssOptions: {
+                                ident: 'postcss',
+                                plugins: [tailwindcss, autoprefixer],
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                type: 'assets/resource',
+                test: /\.(png|jpg|jpeg|gif|woff|woff2|tff|eot|svg)$/,
+            },
+        ]
+    },
+    "plugins": [
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false
+        }),
+        new CopyPlugin({
+            patterns: [{
+                from: path.resolve('src/static'),
+                to: path.resolve('dist')
+            }]
+        }),
+        ...getHtmlPlugins([
+            'popup',
+            'options',
+            'newTab'
+        ])
+    ],
+    resolve: {
+        extensions: ['.tsx', '.js', '.ts']
+    },
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, 'dist')
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        }
+    }
+}
+
+function getHtmlPlugins(chunks) {
+    return chunks.map(chunk => new HtmlPlugin({
+        title: 'React Extension',
+        filename: `${chunk}.html`,
+        chunks: [chunk]
+    }))
+}
