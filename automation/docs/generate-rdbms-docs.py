a1a87c08661e0ac0272c3327ef620bea9ffac69e
Mustafa Baser <mbaser@mail.com>
2023-06-01 15:12:02 +0300

    docs: rewrite rdbm doc schema generator for mysql and pgsql

--- automation/docs/generate-rdbms-docs.py
+++ automation/docs/generate-rdbms-docs.py
@@ -1,72 +1,135 @@
 import sys
+import argparse
 import pymysql
+import psycopg2
+import psycopg2.extras
+
+parser = argparse.ArgumentParser(description="This script generates rdbm schema doc for Jansssen RDBM backend")
+parser.add_argument('-hostname', help="Hostname or IP address of server", default='localhost')
+parser.add_argument('-username', help="RDBM username", default='jans')
+parser.add_argument('-password', help="Password for RDBM user")
+parser.add_argument('-database', help="RDBM database name", default='jansdb')
+parser.add_argument('-schema-file', help="Path of schema file")
+parser.add_argument('-schema-indexes-file', help="Path of schema indexes file")
+parser.add_argument('-rdbm-type', help="Type of RDBM", choices=['mysql', 'pgsql'], default='mysql')
+argsp = parser.parse_args()
 
 
-def connectToMySQL(serverIP, serverUser, serverUserPwd, database, characterSet):
-    mySQLConnection = pymysql.connect(host=serverIP,
-                                      user=serverUser,
-                                      password=serverUserPwd,
-                                      database=database,
-                                      charset=characterSet)
-    cursorObject = mySQLConnection.cursor()
-    return cursorObject
+
+class RDBMSchemaGenerator:
+
+    def __init__(self):
+        if argsp.rdbm_type == 'mysql':
+            self.table_titles = ('Field', 'Type', 'Null', 'Key', 'Default', 'Comment')
+            self.index_titles = ('Table', 'Non_unique', 'Key_name', 'Seq_in_index', 'Column_name', 'Null', 'Comment', 'Index_comment')
+        else:
+            self.table_titles = ('Field', 'Type', 'Character Maximum Length', 'Null', 'Default', 'Comment')
+            self.index_titles = ('tablename', 'indexname', 'indexdef')
+
+        self.schema_file = argsp.schema_file or argsp.rdbm_type + '-schema.md'
+        self.schema_indexes_file = argsp.schema_file or argsp.rdbm_type + '-schema-indexes.md'
+
+    def connect(self):
+        getattr(self, 'connect_to_' + argsp.rdbm_type)()
+
+    def connect_to_mysql(self):
+        self.connection = pymysql.connect(host=argsp.hostname,
+                                          user=argsp.username,
+                                          password=argsp.password,
+                                          database=argsp.database,
+                                          charset='utf8')
+        self.cursor = self.connection.cursor()
+
+    def connect_to_pgsql(self):
+        self.connection = psycopg2.connect(host=argsp.hostname,
+                                    user=argsp.username,
+                                    password=argsp.password,
+                                    dbname= argsp.database)
+        self.cursor = self.connection.cursor()
 
 
-def getTables(cursorObject, database):
-    cursorObject.execute("SHOW tables FROM {}".format(database))
-    tables = cursorObject.fetchall()
-    return tables  # returns a list of tuples   [('table1',), ('table2',), ('table3',)]
+    def get_tables(self):
+        if argsp.rdbm_type == 'mysql':
+            cmd = "SHOW tables FROM {}".format(argsp.database)
+        else:
+            cmd = "SELECT table_name FROM information_schema.tables where table_schema='public' AND  table_catalog='{}' AND table_type='BASE TABLE';".format(argsp.database)
+
+        self.cursor.execute(cmd)
+        tables = self.cursor.fetchall()
+        self.tables = [table[0] for table in tables]
+
+    def get_dict_cursor_object(self):
+        return self.connection.cursor(pymysql.cursors.DictCursor) if argsp.rdbm_type == 'mysql' else self.connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+    def get_table_fields(self, table):
+        if argsp.rdbm_type == 'mysql':
+            cmd = "SHOW FULL COLUMNS FROM {}".format(table)
+        else:
+            cmd = '''
+                            select
+                                c.column_name as "Field",
+                                c.data_type as "Type",
+                                c.character_maximum_length as "Character Maximum Length",
+                                c.is_nullable as "Null",
+                                c.column_default as "Default",
+                                pgd.description as "Comment"
+                            from pg_catalog.pg_statio_all_tables as st
+                            inner join pg_catalog.pg_description pgd on (
+                                pgd.objoid = st.relid
+                            )
+                            inner join information_schema.columns c on (
+                                pgd.objsubid   = c.ordinal_position and
+                                c.table_schema = st.schemaname and
+                                c.table_name   = st.relname
+                            ) where c.table_name='{}';
+                            '''.format(table)
+
+        with self.get_dict_cursor_object() as cursor:
+            cursor.execute(cmd)
+            fields = cursor.fetchall()
+            return fields
+
+    def get_indexes(self, table):
+        if argsp.rdbm_type == 'mysql':
+            cmd = "SHOW INDEX FROM {}".format(table)
+        else:
+            cmd = "SELECT * FROM pg_indexes WHERE tablename = '{}'".format(table)
+
+        with self.get_dict_cursor_object() as cursor:
+            cursor.execute(cmd)
+            indexes = cursor.fetchall()
+            return indexes
+
+    def get_list_from_dict(self, data_dict, key_list):
+        return [str(data_dict.get(key, '')) for key in key_list]
+
+    def print_tables(self):
+        with open(self.schema_file, 'w') as w:
+            for table in self.tables:
+                w.write('\n\n**' + table + '**\n')
+                w.write('|' + '|'.join(self.table_titles) + '|\n')
+                w.write('|-'*len(self.table_titles)+'|\n')
+                fields = self.get_table_fields(table)
+                for field in fields:
+                    w.write("|" + "|".join(self.get_list_from_dict(field, self.table_titles)) + '|\n')
 
 
-def getTableFields(cursorObject, database, table):
-    cursorObject.execute("SHOW COLUMNS FROM {} FROM {}".format(table, database))
-    fields = cursorObject.fetchall()
-    return fields  # returns a list of tuples   [('field1',), ('field2',), ('field3',)]
-
-
-def getIndexes(cursorObject, database, table):
-    cursorObject.execute("SHOW INDEX FROM {} FROM {}".format(table, database))
-    indexes = cursorObject.fetchall()
-    return indexes  # returns a list of tuples   [('index1',), ('index2',), ('index3',)]
-
-
-def printTables(fileName, cursorObject, database, tables):
-    with open(fileName, 'w') as f:
-        for table in tables:
-            f.write("\n\n**" + table[0] + "**\n")
-            f.write("|Field|Type|Null|Key|Default|Extra|\n")
-            f.write("|-|-|-|-|-|-|")  # Markdown table header row separator
-            fields = getTableFields(cursorObject, database, table[0])  # get the fields for the table
-            for field in fields:
-                f.write("\n|" + "|".join(map(str, field)))
-
-
-def printIndexes(fileName, cursorObject, database, tables):
-    with open(fileName, 'w') as f:
-        for table in tables:
-            f.write("\n\n**" + table[0] + "**\n")
-            indexes = getIndexes(cursorObject, database, table[0])
-            f.write(
-                "|Table|Non_unique|Key_name|Seq_in_index|Column_name|Collation|Cardinality|Sub_part|Packed|Null|Index_type"
-                "|Comment| Index_comment | Visible | Expression|\n")
-            f.write("|-|-|-|-|-|-|-|-|-|-|-|-| - | - | -|")
-            for index in indexes:
-                f.write("\n|" + "|".join(map(str, index)))
+    def print_indexes(self):
+        with open(self.schema_indexes_file, 'w') as w:
+            for table in self.tables:
+                w.write("\n\n**" + table + "**\n")
+                w.write('|' + '|'.join(self.index_titles) + '|\n')
+                w.write('|-'*len(self.index_titles)+'|\n')
+                indexes = self.get_indexes(table)
+                for index in indexes:
+                    w.write("|" + "|".join(self.get_list_from_dict(index, self.index_titles)) + '|\n')
 
 def main():
-    serverIP = sys.argv[1] or "localhost"
-    serverUser = sys.argv[2] or "jans"
-    serverUserPwd = sys.argv[3] or "abcde"
-    database = sys.argv[4] or "jansdb"
-    characterSet = sys.argv[5] or "utf8"
-    schemaFileName = sys.argv[6] or "mysql-schema.md"
-    schemaIndexesFileName = sys.argv[7] or "mysql-schema-indexes.md"
-
-    cursorObject = connectToMySQL(serverIP, serverUser, serverUserPwd, database, characterSet)
-    tables = getTables(cursorObject, database)
-    printTables(schemaFileName, cursorObject, database, tables)
-    printIndexes(schemaIndexesFileName, cursorObject, database, tables)
-
+    schema_generator = RDBMSchemaGenerator()
+    schema_generator.connect()
+    schema_generator.get_tables()
+    schema_generator.print_tables()
+    schema_generator.print_indexes()
 
 if __name__ == '__main__':
     main()
