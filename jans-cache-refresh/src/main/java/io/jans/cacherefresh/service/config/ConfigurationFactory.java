2b2441a2d22027502b04db594de6e823a7c68818
shekhar16 <shekharlaad1609@gmail.com>
2023-06-05 23:54:06 +0530

    fix(jans-cache-refresh): removed unused vars

--- jans-cache-refresh/src/main/java/io/jans/cacherefresh/service/config/ConfigurationFactory.java
+++ jans-cache-refresh/src/main/java/io/jans/cacherefresh/service/config/ConfigurationFactory.java
@@ -90,7 +90,6 @@ public class ConfigurationFactory {
 	private static final String DIR = BASE_DIR + File.separator + "conf" + File.separator;
 
 	private static final String BASE_PROPERTIES_FILE = DIR + "jans.properties";
-	private static final String APP_PROPERTIES_FILE = DIR + "fido2.properties";
 
 	private final String SALT_FILE_NAME = "salt";
 
