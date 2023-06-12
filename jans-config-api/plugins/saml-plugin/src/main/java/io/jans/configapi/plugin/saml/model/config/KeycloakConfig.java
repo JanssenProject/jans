f6bafd68aba852fc6e1c6cfb328df4d49592993d
pujavs <pujas.works@gmail.com>
2023-06-12 22:59:08 +0530

    feat(config-api): saml plugin wip

---
+++ jans-config-api/plugins/saml-plugin/src/main/java/io/jans/configapi/plugin/saml/model/config/KeycloakConfig.java
@@ -0,0 +1,59 @@
+package io.jans.configapi.plugin.saml.model.config;
+
+
+import io.jans.exception.ConfigurationException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.CreatedResponseUtil;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UserResource;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+
+
+
+@ApplicationScoped
+public class KeycloakConfig {
+
+    private static Logger log = LoggerFactory.getLogger(SamlConfigSource.class);
+    
+    private static String serverUrl= "http://localhost:8080";
+    private static String realm = "master";
+    private static String clientId = "https://samltest.id/saml/sp";
+    private static String grantType = OAuth2Constants.PASSWORD;
+    private static String username = "admin1";
+    private static String password = "admin123";
+
+        
+    @Inject
+    SamlConfigSource samlConfigSource;
+    
+    public Keycloak getInstance() {
+        log.error("Keycloak instance entry - samlConfigSource:{}", samlConfigSource);
+        log.error("samlConfigSource.getProperties():{}, samlConfigSource.getPropertyNames():{} ", samlConfigSource.getProperties(), samlConfigSource.getPropertyNames());
+        return getInstance(serverUrl, realm, username, password, clientId, grantType);
+      }
+    
+    public Keycloak getInstance(String serverUrl, String realm, String username, String password, String clientId,String grantType) {
+        log.error("Keycloak instance param serverUrl:{}, realm:{}, username:{}, password:{}, clientId:{}, grantType:{} ",serverUrl, realm, username, password, clientId, grantType);
+        log.error("samlConfigSource.getProperties():{}, samlConfigSource.getPropertyNames():{} ", samlConfigSource.getProperties(), samlConfigSource.getPropertyNames());
+        return Keycloak.getInstance(serverUrl,  realm,  username,  password,  clientId);
+      }
+}
--- jans-config-api/plugins/saml-plugin/src/main/java/io/jans/configapi/plugin/saml/rest/SamlTrustRelationshipResource.java
+++ jans-config-api/plugins/saml-plugin/src/main/java/io/jans/configapi/plugin/saml/rest/SamlTrustRelationshipResource.java
@@ -6,6 +6,7 @@ import io.jans.configapi.plugin.saml.util.Constants;
 import io.jans.configapi.util.ApiAccessConstants;
 import io.jans.configapi.util.ApiConstants;
 import io.jans.configapi.plugin.saml.model.config.SAMLTrustRelationship;
+import io.jans.configapi.plugin.saml.model.config.KeycloakConfig;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -22,9 +23,13 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.slf4j.Logger;
+
 
 import java.util.*;
+import java.util.stream.*;
+
+import org.keycloak.representations.idm.UserRepresentation;
+import org.slf4j.Logger;
 
 @Path(Constants.TRUST_RELATIONSHIP)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -33,6 +38,9 @@ public class SamlTrustRelationshipResource extends BaseResource {
 
     @Inject
     Logger logger;
+    
+    @Inject
+    KeycloakConfig keycloakConfig;
 
 
     @Operation(summary = "Get details of of Trust Relationship", description = "Get details of of Trust Relationship", operationId = "get-saml-trust-relationship", tags = {
@@ -46,6 +54,35 @@ public class SamlTrustRelationshipResource extends BaseResource {
     @ProtectedApi(scopes = {Constants.SAML_READ_ACCESS})
     public Response findAllRegistered() {
         List entries = new ArrayList();
+        
+        
         return Response.ok(entries).build();
     }
+    
+    @Operation(summary = "Get details of of Trust Relationship by username", description = "Get details of of Trust Relationship", operationId = "get-saml-trust-relationship", tags = {
+    "SAML - Trust Relationship" }, security = @SecurityRequirement(name = "oauth2", scopes = {
+            Constants.SAML_READ_ACCESS }))
+    @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = MediaType.APPLICATION_JSON, array = @ArraySchema(schema = @Schema(implementation = SAMLTrustRelationship.class)))),
+    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+    @ApiResponse(responseCode = "500", description = "InternalServerError") })
+    @GET
+    @ProtectedApi(scopes = {Constants.SAML_READ_ACCESS})
+    public Response findAllRegistered(String username, boolean exact) {
+        List entries = new ArrayList();
+        
+        logger.info("Searching by username: {} (exact {})", username, exact);
+        List<UserRepresentation> users = keycloakConfig.getInstance().realm("master")
+          .users()
+          .searchByUsername(username, exact);
+
+      
+        logger.info("Users found by username:{}, users:{}", username, users);
+
+        return Response.ok(users).build();
+    }
+    
+
+        
+
 }
