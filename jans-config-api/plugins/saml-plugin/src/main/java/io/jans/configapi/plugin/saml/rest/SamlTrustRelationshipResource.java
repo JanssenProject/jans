56549daffc140c656852efb5f73e746884de30cd
pujavs <pujas.works@gmail.com>
2023-06-20 18:00:04 +0530

    feat(config-api): saml plugin - client endpoints

--- jans-config-api/plugins/saml-plugin/src/main/java/io/jans/configapi/plugin/saml/rest/SamlTrustRelationshipResource.java
+++ jans-config-api/plugins/saml-plugin/src/main/java/io/jans/configapi/plugin/saml/rest/SamlTrustRelationshipResource.java
@@ -47,13 +47,10 @@ public class SamlTrustRelationshipResource extends BaseResource {
     Logger logger;
 
     @Inject
-    KeycloakConfig keycloakConfig;
-
-    @Inject
     SamlService samlService;
 
     @Operation(summary = "Get all Clients", description = "Get all Clients.", operationId = "get-saml-client", tags = {
-            "SAML - Trust Relationship" }, security = @SecurityRequirement(name = "oauth2", scopes = {
+            "SAML - Client" }, security = @SecurityRequirement(name = "oauth2", scopes = {
                     Constants.SAML_READ_ACCESS }))
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = MediaType.APPLICATION_JSON, array = @ArraySchema(schema = @Schema(implementation = SAMLTrustRelationship.class)))),
@@ -63,23 +60,14 @@ public class SamlTrustRelationshipResource extends BaseResource {
     @ProtectedApi(scopes = { Constants.SAML_READ_ACCESS })
     public Response getAllClients() {
 
-        // UsersResource UsersResource =
-        // keycloakConfig.getInstance().realm("master").users();
-        RealmResource realmResource = keycloakConfig.getInstance().realm("master");
-        logger.error("Searching by realmResource:{})", realmResource);
-
-        ClientsResource clientsResource = realmResource.clients();
-        logger.error("Searching by clientsResource:{})", clientsResource);
-
-        List<ClientRepresentation> clientList = clientsResource.findAll();
+        List<ClientRepresentation> clientList = samlService.getAllClients();
 
         logger.error("All clientList:{}", clientList);
         return Response.ok(clientList).build();
     }
 
-    @Operation(summary = "Get all Users", description = "Get all Users.", operationId = "get-saml-client", tags = {
-            "SAML - Trust Relationship" }, security = @SecurityRequirement(name = "oauth2", scopes = {
-                    Constants.SAML_READ_ACCESS }))
+    @Operation(summary = "Get all users", description = "Get all users", operationId = "get-saml-user", tags = {
+            "SAML - User" }, security = @SecurityRequirement(name = "oauth2", scopes = { Constants.SAML_READ_ACCESS }))
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = MediaType.APPLICATION_JSON, array = @ArraySchema(schema = @Schema(implementation = SAMLTrustRelationship.class)))),
             @ApiResponse(responseCode = "401", description = "Unauthorized"),
@@ -90,21 +78,15 @@ public class SamlTrustRelationshipResource extends BaseResource {
     public Response getAllUsers() {
 
         logger.error("Searching users()");
-//UsersResource UsersResource = keycloakConfig.getInstance().realm("master").users();
-        RealmResource realmResource = keycloakConfig.getInstance().realm("master");
-        logger.error("Searching by realmResource:{})", realmResource);
 
-        UsersResource usersResource = realmResource.users();
-        logger.error("Searching by usersResource:{})", usersResource);
-
-        List<UserRepresentation> userList = usersResource.list();
+        List<UserRepresentation> userList = samlService.getAllUsers();
 
         logger.error("All userList:{}", userList);
         return Response.ok(userList).build();
     }
 
-    @Operation(summary = "Get client by username", description = "Get client by username", operationId = "get-saml-client-by-name", tags = {
-            "SAML - Trust Relationship" }, security = @SecurityRequirement(name = "oauth2", scopes = {
+    @Operation(summary = "Get client by name", description = "Get client by name", operationId = "get-saml-client-by-name", tags = {
+            "SAML - Client" }, security = @SecurityRequirement(name = "oauth2", scopes = {
                     Constants.SAML_READ_ACCESS }))
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = MediaType.APPLICATION_JSON, array = @ArraySchema(schema = @Schema(implementation = SAMLTrustRelationship.class)))),
@@ -114,30 +96,21 @@ public class SamlTrustRelationshipResource extends BaseResource {
     @ProtectedApi(scopes = { Constants.SAML_READ_ACCESS })
     @Path(Constants.NAME_PARAM_PATH)
     public Response searchClient(
-            @Parameter(description = "Client name") @PathParam(Constants.NAME) @NotNull String name, boolean exact) {
-        logger.info("Searching by username: {} , exact: {})", name, exact);
-        // UsersResource UsersResource =
-        // keycloakConfig.getInstance().realm("master").users();
+            @Parameter(description = "Client name") @PathParam(Constants.NAME) @NotNull String name) {
+        logger.info("Searching client by name: {}", name);
 
-        logger.info("Searching by username: {} , exact: {})", name, exact);
-        RealmResource realmResource = keycloakConfig.getInstance().realm("master");
-        logger.info("Searching by realmResource:{})", realmResource);
+        List<ClientRepresentation> clients = samlService.serachClients(name);
 
-        ClientsResource clientsResource = realmResource.clients();
-        logger.info("Searching by clientsResource:{})", clientsResource);
+        logger.info("Clients found by name:{}, clients:{}", name, clients);
 
-        List<ClientRepresentation> users = clientsResource.query(name);
-
-        logger.info("Users found by username:{}, users:{}", name, users);
-
-        return Response.ok(users).build();
+        return Response.ok(clients).build();
     }
 
-    @Operation(summary = "Create Client", description = "Create Client", operationId = "get-saml-trust-relationship", tags = {
-            "SAML - Trust Relationship" }, security = @SecurityRequirement(name = "oauth2", scopes = {
+    @Operation(summary = "Create Client", description = "Create Client", operationId = "post-client", tags = {
+            "SAML - Client" }, security = @SecurityRequirement(name = "oauth2", scopes = {
                     Constants.SAML_WRITE_ACCESS }))
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = MediaType.APPLICATION_JSON, array = @ArraySchema(schema = @Schema(implementation = SAMLTrustRelationship.class)))),
+            @ApiResponse(responseCode = "201", description = "clientList", content = @Content(mediaType = MediaType.APPLICATION_JSON, array = @ArraySchema(schema = @Schema(implementation = SAMLTrustRelationship.class)))),
             @ApiResponse(responseCode = "401", description = "Unauthorized"),
             @ApiResponse(responseCode = "500", description = "InternalServerError") })
     @POST
@@ -145,13 +118,52 @@ public class SamlTrustRelationshipResource extends BaseResource {
     public Response createClient(ClientRepresentation clientRepresentation) throws Exception {
 
         logger.info("Create client clientRepresentation:{}", clientRepresentation);
-        
-        //TO-DO validation of client
+
+        // TO-DO validation of client
         ClientRepresentation client = samlService.createClient(clientRepresentation);
-        
+
         logger.info("Create created by client:{}", client);
+        return Response.status(Response.Status.CREATED).entity(client).build();
+    }
+
+    @Operation(summary = "Update client", description = "Update client", operationId = "put-client", tags = {
+            "SAML - Client" }, security = @SecurityRequirement(name = "oauth2", scopes = {
+                    Constants.SAML_WRITE_ACCESS }))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = MediaType.APPLICATION_JSON, array = @ArraySchema(schema = @Schema(implementation = SAMLTrustRelationship.class)))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "500", description = "InternalServerError") })
+    @PUT
+    @ProtectedApi(scopes = { Constants.SAML_WRITE_ACCESS })
+    public Response updateClient(ClientRepresentation clientRepresentation) throws Exception {
+
+        logger.info("Update client:{}", clientRepresentation);
+
+        //TO-DO validation of client
+        ClientRepresentation client = samlService.updateClient(clientRepresentation);
+
+        logger.info("Post update client:{}", client);
 
         return Response.ok(client).build();
+    }
+
+    @Operation(summary = "Delete client", description = "Delete client", operationId = "put-client", tags = {
+            "SAML - Client" }, security = @SecurityRequirement(name = "oauth2", scopes = {
+                    Constants.SAML_WRITE_ACCESS }))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "No Content", content = @Content(mediaType = MediaType.APPLICATION_JSON, array = @ArraySchema(schema = @Schema(implementation = SAMLTrustRelationship.class)))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "500", description = "InternalServerError") })
+    @DELETE
+    @ProtectedApi(scopes = { Constants.SAML_WRITE_ACCESS })
+    public Response deleteClient(String id) throws Exception {
+
+        logger.info("Delete client identified by id:{}", id);
+
+        // TO-DO validation of client
+        samlService.deleteClient(id);
+
+        return Response.noContent().build();
     }
 
 }
--- jans-config-api/plugins/saml-plugin/src/main/java/io/jans/configapi/plugin/saml/service/SamlService.java
+++ jans-config-api/plugins/saml-plugin/src/main/java/io/jans/configapi/plugin/saml/service/SamlService.java
@@ -18,6 +18,7 @@ import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.CreatedResponseUtil;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.admin.client.resource.ClientResource;
 import org.keycloak.admin.client.resource.ClientsResource;
 import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.admin.client.resource.UserResource;
@@ -34,8 +35,6 @@ import jakarta.inject.Inject;
 import java.util.*;
 import jakarta.ws.rs.core.Response;
 
-
-
 @ApplicationScoped
 public class SamlService {
 
@@ -47,73 +46,138 @@ public class SamlService {
 
     @Inject
     ConfigurationFactory configurationFactory;
-    
+
     @Inject
     KeycloakConfig keycloakConfig;
-    
+
     public RealmResource getRealmResource(String realm) {
         logger.error("Get RealmResource for realm:{})", realm);
-        if(StringUtils.isBlank(realm)) {
+        if (StringUtils.isBlank(realm)) {
             realm = "master";
         }
         RealmResource realmResource = keycloakConfig.getInstance().realm(realm);
         logger.error("realmResource:{})", realmResource);
         return realmResource;
     }
-    
-    
+
     public ClientsResource getClientsResource(String realm) {
         logger.error("Get ClientsResource for realm:{})", realm);
         RealmResource realmResource = this.getRealmResource(realm);
-        logger.error("realmResource:{})", realmResource);
-        
+        logger.error("realm-resource:{})", realmResource);
+
         ClientsResource clientsResource = realmResource.clients();
         logger.error(" clientsResource:{})", clientsResource);
-        
+
         return clientsResource;
     }
-    
-    
-    public List<ClientRepresentation> getClient(String realm)  {
+
+    public UsersResource getUsersResource(String realm) {
+        logger.error("Get UsersResource for realm:{})", realm);
         RealmResource realmResource = this.getRealmResource(realm);
-        logger.error("Searching by realmResource:{})", realmResource);
-        
-        ClientsResource clientsResource = realmResource.clients();
-        logger.error("Searching by clientsResource:{})", clientsResource);
-        
-        List<ClientRepresentation>  clientList = clientsResource.findAll();
-     
+        logger.error("realmResource:{})", realmResource);
+
+        UsersResource usersResource = realmResource.users();
+        logger.error(" usersResource:{})", usersResource);
+
+        return usersResource;
+    }
+
+    public List<ClientRepresentation> getAllClients() {
+        List<ClientRepresentation> clientList = getClients(null);
+        logger.error("All clients - clientList:{}", clientList);
+        return clientList;
+    }
+
+    public List<UserRepresentation> getAllUsers() {
+        List<UserRepresentation> userList = getUsers(null);
+        logger.error("All userList:{}", userList);
+        return userList;
+    }
+
+    public List<ClientRepresentation> getClients(String realm) {
+        logger.error("Serach client in realm:{})", realm);
+
+        List<ClientRepresentation> clients = getClientsResource(null).findAll();
+
+        logger.error("clients:{}", clients);
+        return clients;
+    }
+
+    public List<ClientRepresentation> serachClients(String name) {
+        logger.error("Searching client by name:{}", name);
+
+        List<ClientRepresentation> clientList = serachClients(name, null);
+
+        logger.error("Clients by name:{} are clientList:{}", name, clientList);
+        return clientList;
+    }
+
+    public List<ClientRepresentation> serachClients(String name, String realm) {
+        logger.error("Searching client by name:{} in realm:{})", name, realm);
+
+        List<ClientRepresentation> clientList = getClientsResource(null).query(name);
+
         logger.error("All clientList:{}", clientList);
         return clientList;
     }
 
-    public ClientRepresentation createClient(ClientRepresentation clientRepresentation) throws Exception {
-        logger.error(" SamlService::createClient() - clientRepresentation:{}",clientRepresentation);
-             
-        //Get Keycloak/SAML implmentation instance
-        Keycloak keycloak = keycloakConfig.getInstance();
-        
-        logger.error(" SamlService::createClient() - keycloak:{}",keycloak);
+    public List<UserRepresentation> getUsers(String realm) {
+        logger.error("Fetching users in realm:{})", realm);
+        List<UserRepresentation> userList = getUsersResource(null).list();
+        logger.error("All userList:{}", userList);
+        return userList;
+    }
 
-        RealmResource realmResource = this.getRealmResource(null);
-        logger.error("Create client in realmResource:{})", realmResource);
-        
-        ClientsResource clientsResource = realmResource.clients();
-        logger.error("clientsResource:{})", clientsResource);
-        
+    public ClientRepresentation createClient(ClientRepresentation clientRepresentation)  {
+        logger.error(" createClient() - clientRepresentation:{}", clientRepresentation);
+
+        ClientsResource clientsResource = getClientsResource(null);
+
         // Create client (requires manage-users role)
         Response response = clientsResource.create(clientRepresentation);
-        logger.error(" SamlService::createClient() - response:{}",response);
-        
-        logger.error("Repsonse: %s %s%n", response.getStatus(), response.getStatusInfo());
-        logger.error("response.getLocation():{}",response.getLocation());
-        String clienId = CreatedResponseUtil.getCreatedId(response);
+        logger.error(" createClient() - response:{}", response);
 
-        logger.error(" SamlService::createClient() - response.getStatus():{}, response.getStatusInfo():{}, response.getLocation():{}, clienId:{}",response.getStatus(), response.getStatusInfo(), response.getLocation(),clienId);
-        System.out.printf("User created with clienId: %s%n", clienId);
+        logger.error(
+                " createClient() - response.getStatus():{}, response.getStatusInfo():{}, response.getLocation():{}",
+                response.getStatus(), response.getStatusInfo(), response.getLocation());
+        logger.error("response.getLocation():{}", response.getLocation());
+        String id = CreatedResponseUtil.getCreatedId(response);
 
-     
-        
-        return clientRepresentation;
+        logger.error("New client created with id:{}", id);
+
+        ClientResource clientResource = clientsResource.get(id);
+        ClientRepresentation client = clientResource.toRepresentation();
+        logger.error("New client created with client:{}", client);
+
+        return client;
     }
+
+    public ClientRepresentation updateClient(ClientRepresentation clientRepresentation) {
+        logger.error(" updateClient() - clientRepresentation:{}", clientRepresentation);
+
+        ClientsResource clientsResource = getClientsResource(null);
+
+        ClientResource clientResource = clientsResource.get(clientRepresentation.getId());
+        clientResource.update(clientRepresentation);
+
+        ClientRepresentation client = clientResource.toRepresentation();
+        logger.error("Updated client:{}", client);
+        return client;
+    }
+
+    public void deleteClient(String id)  {
+        logger.error(" deleteClient() - id:{}", id);
+
+        ClientsResource clientsResource = getClientsResource(null);
+        logger.error("clientsResource:{})", clientsResource);
+
+        ClientResource clientResource = clientsResource.get(id);
+
+        clientResource.remove();
+
+        ClientRepresentation client = clientResource.toRepresentation();
+        logger.error("Updated client:{}", client);
+
+    }
+
 }
