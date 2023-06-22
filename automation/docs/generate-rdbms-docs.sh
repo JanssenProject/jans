1b33e39780f4d89be8f60427af27d602a93a0ed9
moabu <47318409+moabu@users.noreply.github.com>
2023-06-22 07:23:38 +0000

    ci: add persistence docs

--- automation/docs/generate-rdbms-docs.sh
+++ automation/docs/generate-rdbms-docs.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -euo pipefail
+PERSISTENCE=$1
 git pull origin main
 docker cp automation/docs/generate-rdbms-docs.py docker-jans-monolith-jans-1:/opt/generate-rdbms-docs.py
-docker exec docker-jans-monolith-jans-1 python3 /opt/generate-rdbms-docs.py mysql jans 1t5Fin3#security jans utf8 mysql-schema.md mysql-schema-indexes.md
-docker cp docker-jans-monolith-jans-1:/opt/mysql-schema.md docs/admin/reference/database/mysql-schema.md
-docker cp docker-jans-monolith-jans-1:/opt/mysql-schema-indexes.md docs/admin/reference/database/mysql-schema-indexes.md
+docker exec docker-jans-monolith-jans-1 python3 /opt/generate-rdbms-docs.py -hostname localhost -username "jans" -password "1t5Fin3#security" -database "jans" -rdbm-type "$PERSISTENCE"
+docker cp docker-jans-monolith-jans-1:/opt/mysql-schema.md docs/admin/reference/database/"$PERSISTENCE"-schema.md
+docker cp docker-jans-monolith-jans-1:/opt/mysql-schema-indexes.md docs/admin/reference/database/"$PERSISTENCE"-schema-indexes.md
 git add docs/* && git update-index --refresh
 git commit -m "docs: update rdbms docs"
 git diff-index --quiet HEAD -- || git commit -S -m "docs: update rdbms docs" && git push
