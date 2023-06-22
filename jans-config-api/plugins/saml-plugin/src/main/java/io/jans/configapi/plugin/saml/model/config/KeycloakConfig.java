a35ab0d9c7f1ddf75e7689d7bf07af8b4f6349e2
pujavs <pujas.works@gmail.com>
2023-06-22 18:24:47 +0530

    feat(config-api): saml-plugin wip

--- jans-config-api/plugins/saml-plugin/src/main/java/io/jans/configapi/plugin/saml/model/config/KeycloakConfig.java
+++ jans-config-api/plugins/saml-plugin/src/main/java/io/jans/configapi/plugin/saml/model/config/KeycloakConfig.java
@@ -34,43 +34,31 @@ import org.keycloak.representations.info.ServerInfoRepresentation;
 @ApplicationScoped
 public class KeycloakConfig {
 
-    private static Logger log = LoggerFactory.getLogger(SamlConfigSource.class);
-    
-    private static String serverUrl= "http://localhost:8180";
-    private static String realm = "master";
-    private static String clientId = "jans-client-1";
-    private static String clientSecret = "yxsKi8ah9pU7ANyjH7HBwJh4XTLYN4x3";
-    private static String grantType = OAuth2Constants.PASSWORD;
-    private static String username = "admin1";
-    private static String password = "admin123";
-
+    private static Logger log = LoggerFactory.getLogger(KeycloakConfig.class);
         
     @Inject
     SamlConfigSource samlConfigSource;
 
 
     public Keycloak getInstance() {
-        log.error("Keycloak instance entry - samlConfigSource:{}", samlConfigSource);
-        log.error("samlConfigSource.getProperties():{}, samlConfigSource.getPropertyNames():{} ", samlConfigSource.getProperties(), samlConfigSource.getPropertyNames());
-        log.error("Keycloak instance entry - samlConfigSource:{}", samlConfigSource);
-        log.error("Keycloak instance entry - samlConfigSource.getValue(\"saml.server.url\"):{}", samlConfigSource.getValue("saml.server.url"));
-        log.error("Keycloak instance entry - samlConfigSource.getValue(\"saml.realm.name\"):{}", samlConfigSource.getValue("saml.realm.name"));
-        log.error("Keycloak instance entry - samlConfigSource.getValue(\"saml.client.id\"):{}", samlConfigSource.getValue("saml.client.id"));
-        log.error("Keycloak instance entry - samlConfigSource.getValue(\"saml.grant.type\"):{}", samlConfigSource.getValue("saml.grant.type"));
-        log.error("Keycloak instance entry - samlConfigSource.getValue(\"saml.admin.username\"):{}", samlConfigSource.getValue("saml.admin.username"));
-        log.error("Keycloak instance entry - samlConfigSource.getValue(\"saml.admin.password\"):{}", samlConfigSource.getValue("saml.admin.password"));
-        log.error("Keycloak instance entry - samlConfigSource.getValue(\"saml.client.id\"):{}", samlConfigSource.getValue("saml.client.id"));
-        log.error("Keycloak instance entry - samlConfigSource.getValue(\"saml.client.secret\"):{}", samlConfigSource.getValue("saml.client.secret"));
-        return getInstance(samlConfigSource.getValue("saml.server.url"), realm, username, password, clientId, clientSecret);
+        log.info("Keycloak instance entry - samlConfigSource:{}", samlConfigSource);
+        log.info("samlConfigSource.getProperties():{}, samlConfigSource.getPropertyNames():{} ", samlConfigSource.getProperties(), samlConfigSource.getPropertyNames());
+        log.info("Keycloak instance entry - samlConfigSource:{}", samlConfigSource);
+        log.info("Keycloak instance entry - samlConfigSource.getValue(\"saml.server.url\"):{}", samlConfigSource.getValue("saml.server.url"));
+        log.info("Keycloak instance entry - samlConfigSource.getValue(\"saml.realm.name\"):{}", samlConfigSource.getValue("saml.realm.name"));
+        log.info("Keycloak instance entry - samlConfigSource.getValue(\"saml.client.id\"):{}", samlConfigSource.getValue("saml.client.id"));
+        log.info("Keycloak instance entry - samlConfigSource.getValue(\"saml.grant.type\"):{}", samlConfigSource.getValue("saml.grant.type"));
+        log.info("Keycloak instance entry - samlConfigSource.getValue(\"saml.admin.username\"):{}", samlConfigSource.getValue("saml.admin.username"));
+        log.info("Keycloak instance entry - samlConfigSource.getValue(\"saml.admin.password\"):{}", samlConfigSource.getValue("saml.admin.password"));
+        log.info("Keycloak instance entry - samlConfigSource.getValue(\"saml.client.id\"):{}", samlConfigSource.getValue("saml.client.id"));
+        log.info("Keycloak instance entry - samlConfigSource.getValue(\"saml.client.secret\"):{}", samlConfigSource.getValue("saml.client.secret"));
+        return getInstance(samlConfigSource.getValue("saml.server.url"), samlConfigSource.getValue("saml.realm.name"), samlConfigSource.getValue("saml.admin.username"), samlConfigSource.getValue("saml.admin.password"), samlConfigSource.getValue("saml.client.id"), samlConfigSource.getValue("saml.client.secret"));
       }
     
     public Keycloak getInstance(String serverUrl, String realm, String username, String password, String clientId, String clientSecret) {
-        log.error("Keycloak instance param serverUrl:{}, realm:{}, username:{}, password:{}, clientId:{}, clientSecret:{} ",serverUrl, realm, username, password, clientId, clientSecret);
-        log.error("samlConfigSource.getProperties():{}, samlConfigSource.getPropertyNames():{} ", samlConfigSource.getProperties(), samlConfigSource.getPropertyNames());
-        
+        log.info("Keycloak instance param serverUrl:{}, realm:{}, username:{}, password:{}, clientId:{}, clientSecret:{} ",serverUrl, realm, username, password, clientId, clientSecret);
         Keycloak keycloak = Keycloak.getInstance(serverUrl,  realm,  username,  password,  clientId, clientSecret);
-        log.error("keycloak:{} ", keycloak);
-       
+        log.info("keycloak:{} ", keycloak);       
         return keycloak;
       }
     
