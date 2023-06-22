7c5eaabe177ee68833aa16938949ca45a574c3f3
moabu <47318409+moabu@users.noreply.github.com>
2023-06-22 08:30:11 +0000

    ci: use PR method

--- automation/docs/generate-rdbms-docs.sh
+++ automation/docs/generate-rdbms-docs.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 PERSISTENCE=$1
-git pull origin main
+echo "Generate RDBMS docs"
+git checkout -b cn-jans-update-rdbms-auto-generated-docs
+git pull origin cn-jans-update-rdbms-auto-generated-docs || echo "Nothing to pull"
 docker cp automation/docs/generate-rdbms-docs.py docker-jans-monolith-jans-1:/opt/generate-rdbms-docs.py
 docker exec docker-jans-monolith-jans-1 python3 /opt/generate-rdbms-docs.py -hostname localhost -username "jans" -password "1t5Fin3#security" -database "jans" -rdbm-type "$PERSISTENCE"
 docker cp docker-jans-monolith-jans-1:/opt/mysql-schema.md docs/admin/reference/database/"$PERSISTENCE"-schema.md
 docker cp docker-jans-monolith-jans-1:/opt/mysql-schema-indexes.md docs/admin/reference/database/"$PERSISTENCE"-schema-indexes.md
 git add docs/* && git update-index --refresh
-git commit -m "docs: update rdbms docs"
-git diff-index --quiet HEAD -- || git commit -S -m "docs: update rdbms docs" && git push
+git commit -m "docs: update rdbms docs"]
+git push --set-upstream origin cn-jans-update-rdbms-auto-generated-docs
+MESSAGE="fix(docs): autogenerate RDBMS docs"
+gh pr create --body "Auto generated RDBMS docs" --title "${MESSAGE}"
