4d7df7dee474c109b33fcf211899b27d1df0798e
moabu <47318409+moabu@users.noreply.github.com>
2023-06-23 08:45:47 +0000

    ci: fix docker command

--- automation/docs/generate-rdbms-docs.sh
+++ automation/docs/generate-rdbms-docs.sh
@@ -8,7 +8,7 @@ docker cp automation/docs/generate-rdbms-docs.py docker-jans-monolith-jans-1:/op
 docker exec docker-jans-monolith-jans-1 python3 /opt/generate-rdbms-docs.py -hostname "$PERSISTENCE" -username "jans" -password "1t5Fin3#security" -database "jans" -rdbm-type "$PERSISTENCE" -schema-file "/opt/$PERSISTENCE-schema.md" -schema-indexes-file "/opt/$PERSISTENCE-schema-indexes.md"
 docker exec docker-jans-monolith-jans-1 ls -l /opt/
 docker cp docker-jans-monolith-jans-1:/opt/"$PERSISTENCE"-schema.md ./docs/admin/reference/database/"$PERSISTENCE"-schema.md || echo "No schema file found"
-docker cp docker-jans-monolith-jans-1:/opt/"$PERSISTENCE"-schema-indexes.md ./docs/admin/reference/database/"$PERSISTENCE"-schema-indexes.md echo "No schema indexes file found"
+docker cp docker-jans-monolith-jans-1:/opt/"$PERSISTENCE"-schema-indexes.md ./docs/admin/reference/database/"$PERSISTENCE"-schema-indexes.md || echo "No schema indexes file found"
 git add ./docs/* && git update-index --refresh || echo "generating rdbms docs failed for ${{ matrix.persistence-backends }}!!!"
 git commit -m "docs: update rdbms docs"] || echo "generating rdbms docs failed for ${{ matrix.persistence-backends }}!!!"
 git push --set-upstream origin cn-jans-update-rdbms-auto-generated-docs || echo "generating rdbms docs failed for ${{ matrix.persistence-backends }}!!!"
