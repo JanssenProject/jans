6e5ad71a497f73e794e41804d38a097918ae279d
moabu <47318409+moabu@users.noreply.github.com>
2023-06-23 07:26:36 +0000

    ci: fix output

--- automation/docs/generate-rdbms-docs.sh
+++ automation/docs/generate-rdbms-docs.sh
@@ -7,9 +7,10 @@ git pull origin cn-jans-update-rdbms-auto-generated-docs || echo "Nothing to pul
 docker cp automation/docs/generate-rdbms-docs.py docker-jans-monolith-jans-1:/opt/generate-rdbms-docs.py
 docker exec docker-jans-monolith-jans-1 python3 /opt/generate-rdbms-docs.py -hostname "$PERSISTENCE" -username "jans" -password "1t5Fin3#security" -database "jans" -rdbm-type "$PERSISTENCE" -schema-file "/opt/$PERSISTENCE-schema.md" -schema-indexes-file "/opt/$PERSISTENCE-schema-indexes.md"
 docker exec docker-jans-monolith-jans-1 ls -l /opt/
-docker cp docker-jans-monolith-jans-1:/opt/"$PERSISTENCE"-schema.md docs/admin/reference/database/"$PERSISTENCE"-schema.md
-docker cp docker-jans-monolith-jans-1:/opt/"$PERSISTENCE"-schema-indexes.md docs/admin/reference/database/"$PERSISTENCE"-schema-indexes.md
-git add docs/* && git update-index --refresh
+ls -l
+docker cp docker-jans-monolith-jans-1:/opt/"$PERSISTENCE"-schema.md ./docs/admin/reference/database/"$PERSISTENCE"-schema.md
+docker cp docker-jans-monolith-jans-1:/opt/"$PERSISTENCE"-schema-indexes.md ./docs/admin/reference/database/"$PERSISTENCE"-schema-indexes.md
+git add ./docs/* && git update-index --refresh
 git commit -m "docs: update rdbms docs"]
 git push --set-upstream origin cn-jans-update-rdbms-auto-generated-docs
 MESSAGE="fix(docs): autogenerate RDBMS docs"
