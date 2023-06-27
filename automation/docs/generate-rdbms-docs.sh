993573bc233cdd72791d7973b6e42c303823055a
moabu <47318409+moabu@users.noreply.github.com>
2023-06-27 11:20:15 +0000

    ci: fix commit
    Former-commit-id: d614c229ff9ec1614e81f55b1f0e6e35d2e60a8a

--- automation/docs/generate-rdbms-docs.sh
+++ automation/docs/generate-rdbms-docs.sh
@@ -11,8 +11,8 @@ docker exec docker-jans-monolith-jans-1 python3 /opt/generate-rdbms-docs.py -hos
 docker exec docker-jans-monolith-jans-1 ls -l /opt/
 docker cp docker-jans-monolith-jans-1:/opt/"$PERSISTENCE"-schema.md ./docs/admin/reference/database/"$PERSISTENCE"-schema.md || echo "No schema file found"
 docker cp docker-jans-monolith-jans-1:/opt/"$PERSISTENCE"-schema-indexes.md ./docs/admin/reference/database/"$PERSISTENCE"-schema-indexes.md || echo "No schema indexes file found"
-git add . && git update-index --refresh || echo "generating rdbms docs failed !!!"
-git commit -m -S "docs: update rdbms docs" || echo "generating rdbms docs failed !!!"
+git add . || echo "generating rdbms docs failed !!!"
+git commit -a -S -m "docs: auto-generated $PERSISTENCE docs" || echo "Nothing to commit"
 git push --set-upstream origin cn-jans-update-"$PERSISTENCE"-auto-generated-docs || echo "generating rdbms docs failed !!!"
 MESSAGE="fix(docs): autogenerate $PERSISTENCE RDBMS docs"
 gh pr create --body "Auto generated RDBMS docs" --title "${MESSAGE}" || echo "PR exists"
