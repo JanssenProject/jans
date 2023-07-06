4d2f79d294da17583cf3b21d17923114ea552534
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2023-07-06 21:58:30 +0300

    Merge linkupdates (#5435)
    * fix(jans-link): fix for lastupdate
    
    * refactor(jans-link): refactor changes
    
    * refactor(jans-link): code cleanup
    
    * refactor(jans-link): refactor changes
    
    ---------
    
    Co-authored-by: shekhar16 <shekharlaad1609@gmail.com>
    Co-authored-by: shekhar16 <shekhar16@users.noreply.github.com>

---
+++ jans-link/src/test/java/io/jans/link/test/LdapSample.java
@@ -0,0 +1,49 @@
+/*
+ * oxCore is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2014, Gluu
+ */
+
+package io.jans.link.test;
+
+import io.jans.link.service.config.ConfigurationFactory;
+import io.jans.link.timer.CacheRefreshTimer;
+import io.jans.orm.PersistenceEntryManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Yuriy Movchan Date: 11/03/2016
+ */
+public final class LdapSample {
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(LdapSample.class);
+
+    private LdapSample() {
+    }
+
+    public static void main(String[] args) {
+        // Prepare sample connection details
+        LdapEntryManagerSample entryManagerSample = new LdapEntryManagerSample();
+
+        // Create LDAP entry manager
+        PersistenceEntryManager entryManager = entryManagerSample.createLdapEntryManager();
+
+        //gluucacherefresh
+        //GluuConfiguration GluuConfiguration = new GluuConfiguration();
+        CacheRefreshTimer cacheRefreshTimer = new CacheRefreshTimer();
+        cacheRefreshTimer.setLdapEntryManager(entryManager);
+        ConfigurationFactory configurationFactory = new ConfigurationFactory();
+        //configurationFactory.setPersistenceEntryManagerInstance(entryManager);
+        configurationFactory.create();
+        cacheRefreshTimer.setConfigurationFactory(configurationFactory);
+        /*ConfigurationService configurationService = new ConfigurationService();
+        configurationService.setPersistenceEntryManager(entryManager);
+        cacheRefreshTimer.setConfigurationService(configurationService);
+        cacheRefreshTimer.processInt();*/
+
+
+    }
+
+}
