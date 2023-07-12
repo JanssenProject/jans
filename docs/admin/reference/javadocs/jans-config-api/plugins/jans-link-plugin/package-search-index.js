bfe248dbca735367b9e34f0460f0a270b48e8121
mo-auto <54212639+mo-auto@users.noreply.github.com>
2023-07-12 14:48:11 +0000

    fix(docs): autogenerate docs (#5515)
    * docs: auto-generated property docs
    
    * docs: generate and add javadocs

---
+++ docs/admin/reference/javadocs/jans-config-api/plugins/jans-link-plugin/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.jans.configapi.plugin.link.extensions"},{"l":"io.jans.configapi.plugin.link.model.config"},{"l":"io.jans.configapi.plugin.link.rest"},{"l":"io.jans.configapi.plugin.link.service"},{"l":"io.jans.configapi.plugin.link.util"}]
