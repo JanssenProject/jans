1db6dd036cf113d389eb292c458632a65fa27e1a
Devrim <devrimyatar@gluu.org>
2023-07-28 16:52:54 +0300

    fix(jans-linux-setup): non-document entry (#5677)
    Signed-off-by: Mustafa Baser <mbaser@mail.com>

--- jans-linux-setup/jans_setup/setup_app/utils/db_utils.py
+++ jans-linux-setup/jans_setup/setup_app/utils/db_utils.py
@@ -356,8 +356,9 @@ class DBUtils:
             base.logIt("Querying LDAP for dn {}".format(dn))
             result = self.ldap_conn.search(search_base=dn, search_filter='(objectClass=*)', search_scope=ldap3.BASE, attributes=['*'])
             if result:
-                key, document = ldif_utils.get_document_from_entry(self.ldap_conn.response[0]['dn'], self.ldap_conn.response[0]['attributes'])
-                return document
+                key_document = ldif_utils.get_document_from_entry(self.ldap_conn.response[0]['dn'], self.ldap_conn.response[0]['attributes'])
+                if key_document:
+                    return key_document[1]
 
         else:
             bucket = self.get_bucket_for_dn(dn)
