49b423731e612dfa958f31038c9b0c0781c606ab
Isman Firmansyah <iromli@users.noreply.github.com>
2023-08-09 14:04:29 +0700

    refactor(docker-jans-persistence-loader): reusable assets for custom image (#5792)
    Former-commit-id: de01ee27f9422e26f860f855ae59e7bfcfc69d53

--- docker-jans-persistence-loader/scripts/spanner_setup.py
+++ docker-jans-persistence-loader/scripts/spanner_setup.py
@@ -13,7 +13,7 @@ from hooks import get_ldif_mappings_hook
 FIELD_RE = re.compile(r"[^0-9a-zA-Z\s]+")
 
 logging.config.dictConfig(LOGGING_CONFIG)
-logger = logging.getLogger("spanner_setup")
+logger = logging.getLogger("persistence-loader")
 
 
 class SpannerBackend:
