a8ca57cf358bd2c90a0716c5de7de95413e65b7b
moabu <47318409+moabu@users.noreply.github.com>
2023-09-18 15:17:35 +0000

    docs: remove 1.0.14 nightly

--- v1.0.14/admin/reference/javadocs/jans-fido2/model/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.jans.entry"},{"l":"io.jans.fido2.ctap"},{"l":"io.jans.fido2.model.conf"},{"l":"io.jans.fido2.model.u2f.error"},{"l":"io.jans.fido2.sg"}]
