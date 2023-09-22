1fe5f49b25580b3cc94c1dae4560036b89ed8326
pujavs <pujas.works@gmail.com>
2023-09-22 22:56:08 +0530

    feat(jans-keycloak): added kc code
    Signed-off-by: pujavs <pujas.works@gmail.com>

---
+++ jans-keycloak/user-storage-spi/src/main/java/io/jans/jans-keycloak/App.java
@@ -0,0 +1,13 @@
+package io.jans.jans-keycloak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
