0df0c86a684827a2fb932c1fd80d6cf472d3b355
Devrim <devrimyatar@gluu.org>
2023-10-05 13:37:07 +0300

    fix(jans-linux-setup): casa url (#6187)
    * fix(jans-linux-setup): casa url
    
    * fix(jans-linux-setup): variable rename

--- jans-linux-setup/jans_setup/setup_app/installers/jans_link.py
+++ jans-linux-setup/jans_setup/setup_app/installers/jans_link.py
@@ -13,7 +13,7 @@ from setup_app.installers.jetty import JettyInstaller
 class JansLinkInstaller(JettyInstaller):
 
     source_files = [
-            (os.path.join(Config.dist_jans_dir, 'jans-link.war'), os.path.join(base.current_app.app_info['JANS_MAVEN'], 'maven/io/jans/jans-link-server/{0}/jans-link-server-{0}.war').format(base.current_app.app_info['ox_version']))
+            (os.path.join(Config.dist_jans_dir, 'jans-link.war'), os.path.join(base.current_app.app_info['JANS_MAVEN'], 'maven/io/jans/jans-link-server/{0}/jans-link-server-{0}.war').format(base.current_app.app_info['jans_version']))
             ]
 
     def __init__(self):
