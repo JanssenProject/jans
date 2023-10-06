474fc086ea05b97af936ea87ac6b4bd4aef64fc7
Isman Firmansyah <iromli@users.noreply.github.com>
2023-10-06 14:40:01 +0700

    feat(docker-jans-casa): migrate Casa image from Flex (#6194)
    * feat(docker-jans-casa): migrate Casa image from Flex
    
    * feat(docker-jans-casa): add casa plugins
    
    * feat(docker-jans-casa): handle updating URIs from previous version
    
    * fix(chart): change casa base URL
    
    Signed-off-by: Mustafa Baser <mbaser@mail.com>

---
+++ docker-jans-casa/scripts/entrypoint.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+set -e
+
+# get script directory
+basedir=$(dirname "$(readlink -f -- "$0")")
+
+get_prometheus_opt() {
+    prom_opt=""
+
+    if [ -n "${CN_PROMETHEUS_PORT}" ]; then
+        prom_opt="
+            -javaagent:/opt/prometheus/jmx_prometheus_javaagent.jar=${CN_PROMETHEUS_PORT}:/opt/prometheus/prometheus-config.yaml
+        "
+    fi
+    echo "${prom_opt}"
+}
+
+get_prometheus_lib() {
+    if [ -n "${CN_PROMETHEUS_PORT}" ]; then
+        prom_agent_version="0.17.2"
+
+        if [ ! -f /opt/prometheus/jmx_prometheus_javaagent.jar ]; then
+            wget -q https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/${prom_agent_version}/jmx_prometheus_javaagent-${prom_agent_version}.jar -O /opt/prometheus/jmx_prometheus_javaagent.jar
+        fi
+    fi
+}
+
+get_java_options() {
+    if [ -n "${CN_CASA_JAVA_OPTIONS}" ]; then
+        echo " ${CN_CASA_JAVA_OPTIONS} "
+    else
+        # backward-compat
+        echo " ${CN_JAVA_OPTIONS} "
+    fi
+}
+
+get_max_ram_percentage() {
+    if [ -n "${CN_MAX_RAM_PERCENTAGE}" ]; then
+        echo " -XX:MaxRAMPercentage=$CN_MAX_RAM_PERCENTAGE "
+    fi
+}
+
+touch "$CN_CASA_ADMIN_LOCK_FILE"
+get_prometheus_lib
+python3 "$basedir/wait.py"
+python3 "$basedir/bootstrap.py"
+python3 "$basedir/mod_context.py" jans-casa
+python3 "$basedir/upgrade.py"
+# python3 "$basedir/jca_sync.py" &
+python3 "$basedir/auth_conf.py"
+
+cd /opt/jans/jetty/jans-casa
+# shellcheck disable=SC2046
+exec java \
+    -server \
+    -XX:+DisableExplicitGC \
+    -XX:+UseContainerSupport \
+    -Djans.base=/etc/jans \
+    -Dserver.base=/opt/jans/jetty/jans-casa \
+    -Dlog.base=/opt/jans/jetty/jans-casa \
+    -Djava.io.tmpdir=/opt/jetty/temp \
+    -Dlog4j2.configurationFile=resources/log4j2.xml \
+    -Dpython.home=/opt/jython \
+    -Dadmin.lock=${CN_CASA_ADMIN_LOCK_FILE} \
+    -Dcom.nimbusds.jose.jwk.source.RemoteJWKSet.defaultHttpSizeLimit=${CN_CASA_JWKS_SIZE_LIMIT} \
+    $(get_max_ram_percentage) \
+    $(get_prometheus_opt) \
+    $(get_java_options) \
+    -jar /opt/jetty/start.jar \
+        jetty.http.host="${CN_CASA_JETTY_HOST}" \
+        jetty.http.port="${CN_CASA_JETTY_PORT}" \
+        jetty.deploy.scanInterval=0 \
+        jetty.httpConfig.sendServerVersion=false
---
+++ docker-jans-casa/scripts/upgrade.py
@@ -0,0 +1,314 @@
+import json
+import logging.config
+import os
+from collections import namedtuple
+from contextlib import suppress
+from urllib.parse import urlparse
+from urllib.parse import urlunparse
+
+from jans.pycloudlib import get_manager
+from jans.pycloudlib.persistence import CouchbaseClient
+from jans.pycloudlib.persistence import LdapClient
+from jans.pycloudlib.persistence import SpannerClient
+from jans.pycloudlib.persistence import SqlClient
+from jans.pycloudlib.persistence import PersistenceMapper
+from jans.pycloudlib.persistence import doc_id_from_dn
+from jans.pycloudlib.persistence import id_from_dn
+
+from settings import LOGGING_CONFIG
+
+logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger("casa")
+
+Entry = namedtuple("Entry", ["id", "attrs"])
+
+
+class LDAPBackend:
+    def __init__(self, manager):
+        self.manager = manager
+        self.client = LdapClient(manager)
+        self.type = "ldap"
+
+    def format_attrs(self, attrs):
+        _attrs = {}
+        for k, v in attrs.items():
+            if len(v) < 2:
+                v = v[0]
+            _attrs[k] = v
+        return _attrs
+
+    def get_entry(self, key, filter_="", attrs=None, **kwargs):
+        filter_ = filter_ or "(objectClass=*)"
+
+        entry = self.client.get(key, filter_=filter_, attributes=attrs)
+        if not entry:
+            return None
+        return Entry(entry.entry_dn, self.format_attrs(entry.entry_attributes_as_dict))
+
+    def modify_entry(self, key, attrs=None, **kwargs):
+        attrs = attrs or {}
+        del_flag = kwargs.get("delete_attr", False)
+
+        if del_flag:
+            mod = self.client.MODIFY_DELETE
+        else:
+            mod = self.client.MODIFY_REPLACE
+
+        for k, v in attrs.items():
+            if not isinstance(v, list):
+                v = [v]
+            attrs[k] = [(mod, v)]
+        return self.client.modify(key, attrs)
+
+
+class SQLBackend:
+    def __init__(self, manager):
+        self.manager = manager
+        self.client = SqlClient(manager)
+        self.type = "sql"
+
+    def get_entry(self, key, filter_="", attrs=None, **kwargs):
+        table_name = kwargs.get("table_name")
+        entry = self.client.get(table_name, key, attrs)
+
+        if not entry:
+            return None
+        return Entry(key, entry)
+
+    def modify_entry(self, key, attrs=None, **kwargs):
+        attrs = attrs or {}
+        table_name = kwargs.get("table_name")
+        return self.client.update(table_name, key, attrs), ""
+
+
+class CouchbaseBackend:
+    def __init__(self, manager):
+        self.manager = manager
+        self.client = CouchbaseClient(manager)
+        self.type = "couchbase"
+
+    def get_entry(self, key, filter_="", attrs=None, **kwargs):
+        bucket = kwargs.get("bucket")
+        req = self.client.exec_query(
+            f"SELECT META().id, {bucket}.* FROM {bucket} USE KEYS '{key}'"
+        )
+        if not req.ok:
+            return None
+
+        try:
+            _attrs = req.json()["results"][0]
+            id_ = _attrs.pop("id")
+            entry = Entry(id_, _attrs)
+        except IndexError:
+            entry = None
+        return entry
+
+    def modify_entry(self, key, attrs=None, **kwargs):
+        bucket = kwargs.get("bucket")
+        del_flag = kwargs.get("delete_attr", False)
+        attrs = attrs or {}
+
+        if del_flag:
+            kv = ",".join(attrs.keys())
+            mod_kv = f"UNSET {kv}"
+        else:
+            kv = ",".join([
+                "{}={}".format(k, json.dumps(v))
+                for k, v in attrs.items()
+            ])
+            mod_kv = f"SET {kv}"
+
+        query = f"UPDATE {bucket} USE KEYS '{key}' {mod_kv}"
+        req = self.client.exec_query(query)
+
+        if req.ok:
+            resp = req.json()
+            status = bool(resp["status"] == "success")
+            message = resp["status"]
+        else:
+            status = False
+            message = req.text or req.reason
+        return status, message
+
+
+class SpannerBackend:
+    def __init__(self, manager):
+        self.manager = manager
+        self.client = SpannerClient(manager)
+        self.type = "spanner"
+
+    def get_entry(self, key, filter_="", attrs=None, **kwargs):
+        table_name = kwargs.get("table_name")
+        entry = self.client.get(table_name, key, attrs)
+
+        if not entry:
+            return None
+        return Entry(key, entry)
+
+    def modify_entry(self, key, attrs=None, **kwargs):
+        attrs = attrs or {}
+        table_name = kwargs.get("table_name")
+        return self.client.update(table_name, key, attrs), ""
+
+
+BACKEND_CLASSES = {
+    "sql": SQLBackend,
+    "couchbase": CouchbaseBackend,
+    "spanner": SpannerBackend,
+    "ldap": LDAPBackend,
+}
+
+
+class Upgrade:
+    def __init__(self, manager):
+        self.manager = manager
+
+        mapper = PersistenceMapper()
+
+        backend_cls = BACKEND_CLASSES[mapper.mapping["default"]]
+        self.backend = backend_cls(manager)
+
+    def invoke(self):
+        logger.info("Running upgrade process (if required)")
+        self.update_client_scopes()
+        self.update_client_uris()
+        self.update_conf_app()
+
+    def update_client_scopes(self):
+        kwargs = {}
+        client_id = self.manager.config.get("casa_client_id")
+        id_ = f"inum={client_id},ou=clients,o=jans"
+
+        if self.backend.type in ("sql", "spanner"):
+            kwargs = {"table_name": "jansClnt"}
+            id_ = doc_id_from_dn(id_)
+        elif self.backend.type == "couchbase":
+            kwargs = {"bucket": os.environ.get("CN_COUCHBASE_BUCKET_PREFIX", "jans")}
+            id_ = id_from_dn(id_)
+
+        entry = self.backend.get_entry(id_, **kwargs)
+
+        if not entry:
+            return
+
+        if self.backend.type == "sql" and self.backend.client.dialect == "mysql":
+            client_scopes = entry.attrs["jansScope"]["v"]
+        else:
+            client_scopes = entry.attrs["jansScope"]
+
+        if not isinstance(client_scopes, list):
+            client_scopes = [client_scopes]
+
+        # all potential new scopes for client
+        with open("/app/templates/jans-casa/scopes.json") as f:
+            new_client_scopes = [
+                f"inum={scope['inum']},ou=scopes,o=jans"
+                for scope in json.loads(f.read())
+            ]
+
+        # find missing scopes from the client
+        diff = list(set(new_client_scopes).difference(client_scopes))
+
+        if diff:
+            if self.backend.type == "sql" and self.backend.client.dialect == "mysql":
+                entry.attrs["jansScope"]["v"] = client_scopes + diff
+            else:
+                entry.attrs["jansScope"] = client_scopes + diff
+            self.backend.modify_entry(entry.id, entry.attrs, **kwargs)
+
+    def update_conf_app(self):
+        kwargs = {}
+        id_ = "ou=casa,ou=configuration,o=jans"
+
+        if self.backend.type in ("sql", "spanner"):
+            kwargs = {"table_name": "jansAppConf"}
+            id_ = doc_id_from_dn(id_)
+        elif self.backend.type == "couchbase":
+            kwargs = {"bucket": os.environ.get("CN_COUCHBASE_BUCKET_PREFIX", "jans")}
+            id_ = id_from_dn(id_)
+
+        entry = self.backend.get_entry(id_, **kwargs)
+
+        if not entry:
+            return
+
+        should_update = False
+
+        if self.backend.type != "couchbase":
+            with suppress(json.decoder.JSONDecodeError):
+                entry.attrs["jansConfApp"] = json.loads(entry.attrs["jansConfApp"])
+
+        for key in ["authz_redirect_uri", "post_logout_uri", "frontchannel_logout_uri"]:
+            parsed_url = urlparse(entry.attrs["jansConfApp"]["oidc_config"][key])
+
+            url_paths = [
+                pth for pth in parsed_url.path.rsplit("/")
+                if pth
+            ]
+
+            if url_paths[0] != "jans-casa":
+                url_paths[0] = "jans-casa"
+                parsed_url = parsed_url._replace(path="/".join(url_paths))
+                entry.attrs["jansConfApp"]["oidc_config"][key] = urlunparse(parsed_url)
+                should_update = True
+
+        if should_update:
+            if self.backend.type != "couchbase":
+                entry.attrs["jansConfApp"] = json.dumps(entry.attrs["jansConfApp"])
+            self.backend.modify_entry(entry.id, entry.attrs, **kwargs)
+
+    def update_client_uris(self):
+        kwargs = {}
+        client_id = self.manager.config.get("casa_client_id")
+        id_ = f"inum={client_id},ou=clients,o=jans"
+
+        if self.backend.type in ("sql", "spanner"):
+            kwargs = {"table_name": "jansClnt"}
+            id_ = doc_id_from_dn(id_)
+        elif self.backend.type == "couchbase":
+            kwargs = {"bucket": os.environ.get("CN_COUCHBASE_BUCKET_PREFIX", "jans")}
+            id_ = id_from_dn(id_)
+
+        entry = self.backend.get_entry(id_, **kwargs)
+
+        if not entry:
+            return
+
+        should_update = False
+        hostname = self.manager.config.get("hostname")
+        uri_mapping = {
+            "jansLogoutURI": f"https://{hostname}/jans-casa/autologout",
+            "jansPostLogoutRedirectURI": f"https://{hostname}/jans-casa/bye.zul",
+            "jansRedirectURI": f"https://{hostname}/jans-casa",
+        }
+
+        for key, uri in uri_mapping.items():
+            if self.backend.type == "sql" and self.backend.client.dialect == "mysql":
+                client_uris = entry.attrs[key]["v"]
+            else:
+                client_uris = entry.attrs[key]
+
+            if not isinstance(client_uris, list):
+                client_uris = [client_uris]
+
+            if uri not in client_uris:
+                client_uris.append(uri)
+
+                if self.backend.type == "sql" and self.backend.client.dialect == "mysql":
+                    entry.attrs[key]["v"] = client_uris
+                else:
+                    entry.attrs[key] = client_uris
+                should_update = True
+
+        if should_update:
+            self.backend.modify_entry(entry.id, entry.attrs, **kwargs)
+
+
+def main():
+    manager = get_manager()
+    upgrade = Upgrade(manager)
+    upgrade.invoke()
+
+
+if __name__ == "__main__":
+    main()
