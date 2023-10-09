d52aa25ae2d166d2447de52585633df91c583304
Rolain Djeumen <uprightech@gmail.com>
2023-10-09 17:27:24 +0100

    chore(keycloak-integration): renamed project and plugin internal names
    Signed-off-by: Rolain Djeumen <uprightech@gmail.com>

--- keycloak-integration/authenticator/src/main/java/io/jans/kc/spi/ProviderIDs.java
+++ keycloak-integration/authenticator/src/main/java/io/jans/kc/spi/ProviderIDs.java
@@ -1,6 +1,6 @@
 package io.jans.kc.spi;
 
 public class ProviderIDs {
-    public static final String JANS_AUTHENTICATOR_PROVIDER = "janssen-auth";
-    public static final String JANS_AUTH_RESPONSE_REST_PROVIDER = "janssen-auth-bridge";
+    public static final String JANS_AUTHENTICATOR_PROVIDER = "kc-jans-authn-plugin";
+    public static final String JANS_AUTH_RESPONSE_REST_PROVIDER = "kc-jans-authn-rest-bridge";
 }
