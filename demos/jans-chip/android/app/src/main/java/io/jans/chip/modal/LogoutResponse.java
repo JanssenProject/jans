e74f55cce8dff45c640a4007539cf4bf14c6159e
Arnab Dutta <arnab.bdutta@gmail.com>
2023-10-09 15:51:19 +0530

    feat: use Android MVVM Design Pattern #6105 (#6206)
    * feat: use Android MVVM Design Pattern #6105
    
    Signed-off-by: Arnab Dutta <arnab.bdutta@gmail.com>
    
    * feat: correcting validation #6105
    
    Signed-off-by: Arnab Dutta <arnab.bdutta@gmail.com>
    
    * feat: adding tag  #6105
    
    Signed-off-by: Arnab Dutta <arnab.bdutta@gmail.com>
    
    ---------
    
    Signed-off-by: Arnab Dutta <arnab.bdutta@gmail.com>

--- demos/jans-chip/android/app/src/main/java/io/jans/chip/modal/LogoutResponse.java
+++ demos/jans-chip/android/app/src/main/java/io/jans/chip/modal/LogoutResponse.java
@@ -1,32 +1,15 @@
 package io.jans.chip.modal;
 
-import com.fasterxml.jackson.databind.JsonNode;
-
-import org.json.JSONObject;
-
-public class UserInfoResponse {
-    Object reponse;
+public class LogoutResponse {
     private boolean isSuccessful;
     private OperationError operationError;
 
-    public UserInfoResponse(Object reponse) {
-        this.reponse = reponse;
+    public LogoutResponse() {
     }
 
-    public UserInfoResponse(boolean isSuccessful, OperationError operationError) {
+    public LogoutResponse(boolean isSuccessful, OperationError operationError) {
         this.isSuccessful = isSuccessful;
         this.operationError = operationError;
-    }
-
-    public UserInfoResponse() {
-    }
-
-    public Object getReponse() {
-        return reponse;
-    }
-
-    public void setReponse(Object reponse) {
-        this.reponse = reponse;
     }
 
     public boolean isSuccessful() {
