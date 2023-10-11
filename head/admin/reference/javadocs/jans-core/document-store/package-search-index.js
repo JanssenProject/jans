47fcafc91631418e35e1df9f89f193438e5d3370
moauto <54212639+mo-auto@users.noreply.github.com>
2023-10-11 10:32:48 +0000

    Deployed 8a8540c27b to head with MkDocs 1.4.1 and mike 1.1.2

---
+++ head/admin/reference/javadocs/jans-core/document-store/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.jans.service.document.store"},{"l":"io.jans.service.document.store.conf"},{"l":"io.jans.service.document.store.provider"},{"l":"io.jans.service.document.store.service"}]
