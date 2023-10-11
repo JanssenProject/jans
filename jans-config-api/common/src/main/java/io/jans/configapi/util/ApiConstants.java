e74b598b8fe9456ca61469cd7985f6a114c0c022
pujavs <pujas.works@gmail.com>
2023-10-11 21:47:20 +0530

    feat(config-api): endpoint to fetch custom types
    Signed-off-by: pujavs <pujas.works@gmail.com>

--- jans-config-api/common/src/main/java/io/jans/configapi/util/ApiConstants.java
+++ jans-config-api/common/src/main/java/io/jans/configapi/util/ApiConstants.java
@@ -102,6 +102,7 @@ public class ApiConstants {
     public static final String ID = "id";
     public static final String SCOPE_INUM = "scope_inum";
     public static final String TYPE = "type";
+    public static final String TYPES = "types";
     public static final String NAME = "name";
     public static final String DISPLAY_NAME = "displayName";
     public static final String KID = "kid";
