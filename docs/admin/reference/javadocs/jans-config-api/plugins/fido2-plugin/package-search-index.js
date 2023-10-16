409b957c3f41199fe348e39cf2f374a679717c57
moabu <47318409+moabu@users.noreply.github.com>
2023-10-16 08:49:12 +0000

    docs: clean up javadocs from main repo
    Signed-off-by: Mustafa Baser <mbaser@mail.com>

--- docs/admin/reference/javadocs/jans-config-api/plugins/fido2-plugin/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.jans.configapi.plugin.fido2.extensions"},{"l":"io.jans.configapi.plugin.fido2.model.config"},{"l":"io.jans.configapi.plugin.fido2.rest"},{"l":"io.jans.configapi.plugin.fido2.service"},{"l":"io.jans.configapi.plugin.fido2.util"}]
--- docs/admin/reference/javadocs/jans-core/java-ext/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"io.jans.i18n","l":"ExtendedResourceBundleControlProvider.CustomControl"},{"p":"io.jans.i18n","l":"ExtendedResourceBundle"},{"p":"io.jans.i18n","l":"ExtendedResourceBundleControlProvider"}]
