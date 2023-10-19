486688b3dcb00b0bdfe57ee5f4b696e5d727fbb5
Jose Gonzalez <bonustrack310@gmail.com>
2023-10-19 09:40:31 -0500

    chore: refactor 2FA requirements related to password (#6342)

--- jans-casa/app/src/main/java/io/jans/casa/ui/vm/admin/PassResetViewModel.java
+++ jans-casa/app/src/main/java/io/jans/casa/ui/vm/admin/PassResetViewModel.java
@@ -1,8 +1,6 @@
 package io.jans.casa.ui.vm.admin;
 
-import io.jans.casa.core.PersistenceService;
 import io.jans.casa.core.PasswordStatusService;
-import io.jans.casa.misc.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zkoss.bind.annotation.Init;
@@ -15,20 +13,13 @@ public class PassResetViewModel extends MainViewModel {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    private PersistenceService persistenceService;
-
     @WireVariable("passwordStatusService")
     private PasswordStatusService pst;
 
     private boolean passResetEnabled;
-    private boolean passResetImpossible;
 
     public boolean isPassResetEnabled() {
         return passResetEnabled;
-    }
-
-    public boolean isPassResetImpossible() {
-        return passResetImpossible;
     }
 
     public void setPassResetEnabled(boolean passResetEnabled) {
@@ -37,9 +28,7 @@ public class PassResetViewModel extends MainViewModel {
 
     @Init
     public void init() {
-        persistenceService = Utils.managedBean(PersistenceService.class);
-        passResetImpossible = persistenceService.isBackendLdapEnabled();
-        passResetEnabled = !passResetImpossible && getSettings().isEnablePassReset();
+        passResetEnabled = getSettings().isEnablePassReset();
     }
 
     public void change() {
