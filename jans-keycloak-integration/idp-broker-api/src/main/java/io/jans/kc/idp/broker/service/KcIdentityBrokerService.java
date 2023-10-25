72535ea66f44b93c244f543d0389b4262c24ab85
pujavs <pujas.works@gmail.com>
2023-10-25 22:09:39 +0530

    feat(keycloak): kc broker classes
    Signed-off-by: pujavs <pujas.works@gmail.com>

--- jans-keycloak-integration/idp-broker-api/src/main/java/io/jans/kc/idp/broker/service/KcIdentityBrokerService.java
+++ jans-keycloak-integration/idp-broker-api/src/main/java/io/jans/kc/idp/broker/service/KcIdentityBrokerService.java
@@ -15,55 +15,55 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.keycloak.util.JsonSerialization;
 
-public class ScimService {
+public class KcIdentityBrokerService {
 
-    private static Logger LOG = LoggerFactory.getLogger(ScimService.class);
+    private static Logger LOG = LoggerFactory.getLogger(KcIdentityBrokerService.class);
     private static JansUtil jansUtil = new JansUtil();
 
     private String getScimUserEndpoint() {
         String scimUserEndpoint = jansUtil.getScimUserEndpoint();
-        LOG.info("ScimService::getScimUserEndpoint() - scimUserEndpoint:{}", scimUserEndpoint);
+        LOG.info("KcIdentityBrokerService::getScimUserEndpoint() - scimUserEndpoint:{}", scimUserEndpoint);
         return scimUserEndpoint;
     }
 
     private String getScimUserSearchEndpoint() {
         String scimUserSearchEndpoint = jansUtil.getScimUserSearchEndpoint();
-        LOG.info("ScimService::getScimUserSearchEndpoint() - scimUserSearchEndpoint:{}", scimUserSearchEndpoint);
+        LOG.info("KcIdentityBrokerService::getScimUserSearchEndpoint() - scimUserSearchEndpoint:{}", scimUserSearchEndpoint);
         return scimUserSearchEndpoint;
     }
 
     private String requestAccessToken() {
-        LOG.info("ScimService::requestAccessToken()");
+        LOG.info("KcIdentityBrokerService::requestAccessToken()");
         String token = null;
 
         try {
             token = jansUtil.requestScimAccessToken();
-            LOG.info("ScimService::requestAccessToken() -  token:{}", token);
+            LOG.info("KcIdentityBrokerService::requestAccessToken() -  token:{}", token);
         } catch (Exception ex) {
-            LOG.error("ScimService::requestAccessToken() - Error while generating access token for SCIM endpoint is:{}",
+            LOG.error("KcIdentityBrokerService::requestAccessToken() - Error while generating access token for SCIM endpoint is:{}",
                     ex);
             throw new WebApplicationException(
-                    "ScimService::requestAccessToken() - Error while generating access token for SCIM endpoint is = "
+                    "KcIdentityBrokerService::requestAccessToken() - Error while generating access token for SCIM endpoint is = "
                             + ex);
         }
         return token;
     }
 
     public UserResource getUserById(String inum) {
-        LOG.info(" ScimService::getUserById() - inum:{}", inum);
+        LOG.info(" KcIdentityBrokerService::getUserById() - inum:{}", inum);
         try {
             return getData(getScimUserEndpoint() + "/" + inum, this.requestAccessToken());
         } catch (Exception ex) {
             ex.printStackTrace();
             LOG.error(
-                    "ScimService::getUserById() - Error fetching user based on inum:{} from external service is:{} - {} ",
+                    "KcIdentityBrokerService::getUserById() - Error fetching user based on inum:{} from external service is:{} - {} ",
                     inum, ex.getMessage(), ex);
         }
         return null;
     }
 
     public UserResource getUserByName(String username) {
-        LOG.info("ScimService::getUserByName() - username:{}", username);
+        LOG.info("KcIdentityBrokerService::getUserByName() - username:{}", username);
         try {
 
             String filter = "userName eq \"" + username + "\"";
@@ -71,23 +71,23 @@ public class ScimService {
         } catch (Exception ex) {
             ex.printStackTrace();
             LOG.error(
-                    "ScimService::getUserByName() - Error fetching user based on username:{} from external service is:{} - {} ",
+                    "KcIdentityBrokerService::getUserByName() - Error fetching user based on username:{} from external service is:{} - {} ",
                     username, ex.getMessage(), ex);
         }
         return null;
     }
 
     public UserResource getUserByEmail(String email) {
-        LOG.info(" ScimService::getUserByEmail() - email:{}", email);
+        LOG.info(" KcIdentityBrokerService::getUserByEmail() - email:{}", email);
         try {
 
             String filter = "emails[value eq \"" + email + "\"]";
-            LOG.info(" ScimService::getUserByEmail() - filter:{}", filter);
+            LOG.info(" KcIdentityBrokerService::getUserByEmail() - filter:{}", filter);
             return postData(this.getScimUserSearchEndpoint(), this.requestAccessToken(), filter);
         } catch (Exception ex) {
             ex.printStackTrace();
             LOG.error(
-                    " ScimService::getUserByEmail() - Error fetching user based on email:{} from external service is:{} - {} ",
+                    " KcIdentityBrokerService::getUserByEmail() - Error fetching user based on email:{} from external service is:{} - {} ",
                     email, ex.getMessage(), ex);
 
         }
@@ -96,94 +96,94 @@ public class ScimService {
 
     public UserResource postData(String uri, String accessToken, String filter) {
         UserResource user = null;
-        LOG.info("ScimService::postData() - uri:{}, accessToken:{}, filter:{}", uri, accessToken, filter);
+        LOG.info("KcIdentityBrokerService::postData() - uri:{}, accessToken:{}, filter:{}", uri, accessToken, filter);
         try {
             HttpClient client = HttpClientBuilder.create().build();
 
             SearchRequest searchRequest = createSearchRequest(filter);
-            LOG.info("ScimService::postData() - client:{}, searchRequest:{}, accessToken:{}", client, searchRequest,
+            LOG.info("KcIdentityBrokerService::postData() - client:{}, searchRequest:{}, accessToken:{}", client, searchRequest,
                     accessToken);
 
             JsonNode jsonNode = SimpleHttp.doPost(uri, client).auth(accessToken).json(searchRequest).asJson();
 
-            LOG.info("\n\n  ScimService::postData() - jsonNode:{}", jsonNode);
+            LOG.info("\n\n  KcIdentityBrokerService::postData() - jsonNode:{}", jsonNode);
 
             user = getUserResourceFromList(jsonNode);
 
-            LOG.info("ScimService::postData() - user:{}", user);
+            LOG.info("KcIdentityBrokerService::postData() - user:{}", user);
 
         } catch (Exception ex) {
             ex.printStackTrace();
-            LOG.error("\n\n ScimService::postData() - Error while fetching data is ex:{}", ex);
+            LOG.error("\n\n KcIdentityBrokerService::postData() - Error while fetching data is ex:{}", ex);
         }
         return user;
     }
 
     public UserResource getData(String uri, String accessToken) {
         UserResource user = null;
-        LOG.info("ScimService::getData() - uri:{}, accessToken:{}", uri, accessToken);
+        LOG.info("KcIdentityBrokerService::getData() - uri:{}, accessToken:{}", uri, accessToken);
         try {
             HttpClient client = HttpClientBuilder.create().build();
 
             JsonNode jsonNode = SimpleHttp.doGet(uri, client).auth(accessToken).asJson();
 
-            LOG.info("\n\n  ScimService::getData() - jsonNode:{}", jsonNode);
+            LOG.info("\n\n  KcIdentityBrokerService::getData() - jsonNode:{}", jsonNode);
 
             user = getUserResource(jsonNode);
 
-            LOG.info("ScimService::getData() - user:{}", user);
+            LOG.info("KcIdentityBrokerService::getData() - user:{}", user);
 
         } catch (Exception ex) {
             ex.printStackTrace();
-            LOG.error("\n\n ScimService::getData() - Error while fetching data is ex:{}", ex);
+            LOG.error("\n\n KcIdentityBrokerService::getData() - Error while fetching data is ex:{}", ex);
         }
         return user;
     }
 
     private SearchRequest createSearchRequest(String filter) {
-        LOG.info("ScimService::createSearchRequest() - createSearchRequest() - filter:{}", filter);
+        LOG.info("KcIdentityBrokerService::createSearchRequest() - createSearchRequest() - filter:{}", filter);
         SearchRequest searchRequest = new SearchRequest();
         searchRequest.setFilter(filter);
 
-        LOG.info(" ScimService::createSearchRequest() - searchRequest:{}", searchRequest);
+        LOG.info(" KcIdentityBrokerService::createSearchRequest() - searchRequest:{}", searchRequest);
 
         return searchRequest;
     }
 
     private UserResource getUserResourceFromList(JsonNode jsonNode) {
-        LOG.info(" \n\n ScimService::getUserResourceFromList() - jsonNode:{}", jsonNode);
+        LOG.info(" \n\n KcIdentityBrokerService::getUserResourceFromList() - jsonNode:{}", jsonNode);
 
         UserResource user = null;
         try {
             if (jsonNode != null) {
                 if (jsonNode.get("Resources") != null) {
                     JsonNode value = jsonNode.get("Resources").get(0);
-                    LOG.info("\n\n *** ScimService::getUserResourceFromList() - value:{}, value.getClass():{}", value,
+                    LOG.info("\n\n *** KcIdentityBrokerService::getUserResourceFromList() - value:{}, value.getClass():{}", value,
                             value.getClass());
                     user = JsonSerialization.readValue(JsonSerialization.writeValueAsBytes(value), UserResource.class);
-                    LOG.info(" ScimService::getUserResourceFromList() - user:{}, user.getClass():{}", user,
+                    LOG.info(" KcIdentityBrokerService::getUserResourceFromList() - user:{}, user.getClass():{}", user,
                             user.getClass());
                 }
             }
         } catch (Exception ex) {
             ex.printStackTrace();
-            LOG.error("\n\n ScimService::getUserResourceFromList() - Error while fetching data is ex:{}", ex);
+            LOG.error("\n\n KcIdentityBrokerService::getUserResourceFromList() - Error while fetching data is ex:{}", ex);
         }
         return user;
     }
 
     private UserResource getUserResource(JsonNode jsonNode) {
-        LOG.info(" \n\n ScimService::getUserResource() - jsonNode:{}", jsonNode);
+        LOG.info(" \n\n KcIdentityBrokerService::getUserResource() - jsonNode:{}", jsonNode);
 
         UserResource user = null;
         try {
             if (jsonNode != null) {
                 user = JsonSerialization.readValue(JsonSerialization.writeValueAsBytes(jsonNode), UserResource.class);
-                LOG.info(" ScimService::getUserResource() - user:{}, user.getClass():{}", user, user.getClass());
+                LOG.info(" KcIdentityBrokerService::getUserResource() - user:{}, user.getClass():{}", user, user.getClass());
             }
         } catch (Exception ex) {
             ex.printStackTrace();
-            LOG.error("\n\n ScimService::getUserResource() - Error while fetching data is ex:{}", ex);
+            LOG.error("\n\n KcIdentityBrokerService::getUserResource() - Error while fetching data is ex:{}", ex);
         }
         return user;
     }
