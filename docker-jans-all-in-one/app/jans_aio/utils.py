ae44c665e8f161fa3eb7c11198503ba4d4c30d6b
Isman Firmansyah <iromli@users.noreply.github.com>
2023-10-26 13:05:04 +0700

    feat(docker-jans-all-in-one): implement plugin system (#6392)
    Signed-off-by: Mustafa Baser <mbaser@mail.com>

--- docker-jans-all-in-one/app/jans_aio/utils.py
+++ docker-jans-all-in-one/app/jans_aio/utils.py
@@ -1,70 +1,33 @@
-import os
-import re
-from pathlib import Path
-
-from jans.pycloudlib.persistence import render_couchbase_properties
-from jans.pycloudlib.persistence import render_base_properties
-from jans.pycloudlib.persistence import render_hybrid_properties
-from jans.pycloudlib.persistence import render_ldap_properties
-from jans.pycloudlib.persistence import render_salt
-from jans.pycloudlib.persistence import sync_couchbase_truststore
-from jans.pycloudlib.persistence import sync_ldap_truststore
-from jans.pycloudlib.persistence import render_sql_properties
-from jans.pycloudlib.persistence import render_spanner_properties
-from jans.pycloudlib.persistence.utils import PersistenceMapper
-from jans.pycloudlib.utils import cert_to_truststore
+import sys
 
 
-def get_template_paths(path: str) -> tuple[str, str]:
-    tmpl = Path("/app/templates").joinpath(path.lstrip("/"))
-    return str(tmpl), path
+def import_from_string(import_name):
+    # force the import name to automatically convert to strings
+    # __import__ is not able to handle unicode strings in the fromlist
+    # if the module is a package
+    import_name = str(import_name).replace(":", ".")
 
+    try:
+        try:
+            __import__(import_name)
+        except ImportError:
+            if "." not in import_name:
+                raise
+        else:
+            return sys.modules[import_name]
 
-def render_persistence_props(manager):
-    persistence_type = os.environ.get("CN_PERSISTENCE_TYPE", "ldap")
+        module_name, obj_name = import_name.rsplit('.', 1)
+        try:
+            module = __import__(module_name, None, None, [obj_name])
+        except ImportError:
+            # support importing modules not yet set up by the parent module
+            # (or package for that matter)
+            module = import_from_string(module_name)
 
-    render_salt(manager, *get_template_paths("/etc/jans/conf/salt"))
-    render_base_properties(*get_template_paths("/etc/jans/conf/jans.properties"))
+        try:
+            return getattr(module, obj_name)
+        except AttributeError as exc:
+            raise ImportError(exc)
 
-    mapper = PersistenceMapper()
-    persistence_groups = mapper.groups()
-
-    if persistence_type == "hybrid":
-        render_hybrid_properties("/etc/jans/conf/jans-hybrid.properties")
-
-    if "ldap" in persistence_groups:
-        render_ldap_properties(
-            manager,
-            *get_template_paths("/etc/jans/conf/jans-ldap.properties")
-        )
-        sync_ldap_truststore(manager)
-
-    if "couchbase" in persistence_groups:
-        render_couchbase_properties(
-            manager,
-            *get_template_paths("/etc/jans/conf/jans-couchbase.properties")
-        )
-        sync_couchbase_truststore(manager)
-
-    if "sql" in persistence_groups:
-        db_dialect = os.environ.get("CN_SQL_DB_DIALECT", "mysql")
-        src, dst = get_template_paths(f"/etc/jans/conf/jans-{db_dialect}.properties")
-        render_sql_properties(manager, src, dst.replace(db_dialect, "sql"))
-
-    if "spanner" in persistence_groups:
-        render_spanner_properties(
-            manager,
-            *get_template_paths("/etc/jans/conf/jans-spanner.properties")
-        )
-
-
-def import_cert_to_truststore(manager):
-    if not os.path.isfile("/etc/certs/web_https.crt"):
-        manager.secret.to_file("ssl_cert", "/etc/certs/web_https.crt")
-
-    cert_to_truststore(
-        "web_https",
-        "/etc/certs/web_https.crt",
-        "/opt/java/lib/security/cacerts",
-        "changeit",
-    )
+    except ImportError as exc:
+        raise exc
