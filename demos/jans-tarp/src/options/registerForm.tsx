abb34fbf9e673e9b498f70e46d979a178ca79163
Arnab Dutta <arnab.bdutta@gmail.com>
2023-11-07 22:44:41 +0530

    feat: user should be allowed to paste an SSA (or specify a file from disk) in DCR form #6161
    Signed-off-by: Arnab Dutta <arnab.bdutta@gmail.com>

--- demos/jans-tarp/src/options/registerForm.tsx
+++ demos/jans-tarp/src/options/registerForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, KeyboardEventHandler } from 'react'
+import React, { useState, KeyboardEventHandler, useCallback, useEffect } from 'react'
 import axios from 'axios';
 import './options.css'
 import { v4 as uuidv4 } from 'uuid';
@@ -9,6 +9,8 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import moment from 'moment';
 import { ILooseObject } from './ILooseObject';
+import StyledDropzone from './StyledDropzone';
+import { jwtDecode } from "jwt-decode";
 const components = {
     DropdownIndicator: null,
 };
@@ -24,10 +26,180 @@ const RegisterForm = (data) => {
     const [inputValueIssuer, setInputValueIssuer] = useState('');
     const [inputValueScope, setInputValueScope] = useState('');
     const [isLoading, setIsLoading] = useState(false);
+    const [uploadSsa, setUploadSsa] = useState(false);
     const [showClientExpiry, setShowClientExpiry] = useState(false);
     const [issuerOption, setIssuerOption] = useState<readonly IOption[]>([]);
     const [scopeOption, setScopeOption] = useState<readonly IOption[]>([createOption('openid')]);
     const [clientExpiryDate, setClientExpiryDate] = useState(moment().add(1, 'days').toDate());
+    const [ssaJwt, setSsaJwt] = useState(null)
+    const REGISTRATION_ERROR = 'Error in registration. Check web console for logs.'
+
+    const readJWTFile = (selectedFile) => {
+        const reader = new FileReader()
+
+        reader.onload = () => {
+            const token = reader.result
+            setSsaJwt(token)
+        }
+
+        const blob = new Blob([selectedFile])
+        reader.readAsText(blob)
+    }
+
+    async function triggerClientRegistration() {
+        try {
+            if (uploadSsa) {
+                const response = await processForClientRegiWithSSA()
+                if (response.result !== 'success') {
+                    setError(REGISTRATION_ERROR);
+                }
+                setPageLoading(false);
+            } else if (validate()) {
+
+                setPageLoading(true);
+                const response = await processForClientRegiWithoutSSA()
+                if (response.result !== 'success') {
+                    setError(REGISTRATION_ERROR);
+                }
+                setPageLoading(false);
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    async function processForClientRegiWithSSA() {
+        try {
+
+            const ssaJson = jwtDecode(ssaJwt)
+
+            if (ssaJson.iss == null) {
+                setError('Issuer not found in SSA.')
+            }
+
+            const issuer = ssaJson.iss
+
+            const configurationUrl = generateOpenIdConfigurationURL(issuer)
+            const openapiConfig = await getOpenidConfiguration(configurationUrl)
+
+            if (openapiConfig != undefined) {
+                chrome.storage.local.set({ opConfiguration: openapiConfig.data }).then(() => {
+                    console.log("openapiConfig is set to " + openapiConfig);
+                });
+
+                const registrationUrl = openapiConfig.data.registration_endpoint;
+
+                var registerObj: ILooseObject = {
+                    redirect_uris: [issuer],
+                    software_statement: ssaJwt,
+                    response_types: ['code'],
+                    grant_types: ['authorization_code'],
+                    application_type: 'web',
+                    client_name: 'jans-tarp-' + uuidv4(),
+                    token_endpoint_auth_method: 'client_secret_basic',
+                    post_logout_redirect_uris: [chrome.runtime.getURL('options.html')]
+                }
+
+                const registrationResp = await registerOIDCClient(registrationUrl, registerObj)
+
+                if (registrationResp !== undefined) {
+                    chrome.storage.local.set({
+                        oidcClient: {
+                            'op_host': issuer,
+                            'client_id': registrationResp.data.client_id,
+                            'client_secret': registrationResp.data.client_secret,
+                            'scope': registerObj.scope,
+                            'redirect_uri': registerObj.redirect_uris,
+                            'authorization_endpoint': openapiConfig.data.authorization_endpoint,
+                            'response_type': registerObj.response_types,
+                            'post_logout_redirect_uris': registerObj.post_logout_redirect_uris,
+                            'expire_at': clientExpiryDate.getTime(),
+                            'showClientExpiry': showClientExpiry
+
+                        }
+                    })
+                    console.log('OIDC client registered successfully!')
+                    console.log("oidcClient is set for client_id: " + registrationResp.data.client_id)
+
+                    return await { result: "success", message: "Regstration successful!" }
+
+                } else {
+                    return await { result: "error", message: REGISTRATION_ERROR }
+                }
+            } else {
+                return await { result: "error", message: "Error in fetching Openid configuration!" }
+            }
+        } catch (err) {
+            console.error(err)
+            return { result: "error", message: REGISTRATION_ERROR + err.message}
+        }
+    }
+
+    async function processForClientRegiWithoutSSA() {
+        try {
+            const opConfigurationEndpoint = generateOpenIdConfigurationURL(issuerOption.map((iss) => iss.value)[0]);
+            const opConfigurationEndpointURL = new URL(opConfigurationEndpoint);
+            const issuer = opConfigurationEndpointURL.protocol + '//' + opConfigurationEndpointURL.hostname;
+            const scope = scopeOption.map((ele) => ele.value).join(" ");
+
+            const openapiConfig = await getOpenidConfiguration(opConfigurationEndpoint);
+
+            if (openapiConfig != undefined) {
+                chrome.storage.local.set({ opConfiguration: openapiConfig.data }).then(() => {
+                    console.log("openapiConfig is set to " + openapiConfig);
+                });
+
+                const registrationUrl = openapiConfig.data.registration_endpoint;
+
+                var registerObj: ILooseObject = {
+                    redirect_uris: [issuer],
+                    scope: scope,
+                    post_logout_redirect_uris: [chrome.runtime.getURL('options.html')],
+                    response_types: ['code'],
+                    grant_types: ['authorization_code'],
+                    application_type: 'web',
+                    client_name: 'jans-tarp-' + uuidv4(),
+                    token_endpoint_auth_method: 'client_secret_basic'
+                };
+
+                if (showClientExpiry) {
+                    registerObj.lifetime = ((clientExpiryDate.getTime() - moment().toDate().getTime()) / 1000);
+                }
+
+                const registrationResp = await registerOIDCClient(registrationUrl, registerObj);
+
+                if (registrationResp !== undefined) {
+                    chrome.storage.local.set({
+                        oidcClient: {
+                            'op_host': issuer,
+                            'client_id': registrationResp.data.client_id,
+                            'client_secret': registrationResp.data.client_secret,
+                            'scope': registerObj.scope,
+                            'redirect_uri': registerObj.redirect_uris,
+                            'authorization_endpoint': openapiConfig.data.authorization_endpoint,
+                            'response_type': registerObj.response_types,
+                            'post_logout_redirect_uris': registerObj.post_logout_redirect_uris,
+                            'expire_at': clientExpiryDate.getTime(),
+                            'showClientExpiry': showClientExpiry
+
+                        }
+                    })
+                    console.log('OIDC client registered successfully!')
+                    console.log("oidcClient is set for client_id: " + registrationResp.data.client_id);
+
+                    return await { result: "success", message: "Regstration successful!" };
+
+                } else {
+                    return await { result: "error", message: REGISTRATION_ERROR };
+                }
+            } else {
+                return await { result: "error", message: "Error in fetching Openid configuration!" };
+            }
+        } catch (err) {
+            console.error(err)
+            return { result: "error", message: REGISTRATION_ERROR };
+        }
+    }
 
     const handleKeyDown: KeyboardEventHandler = async (event) => {
         const inputId = (event.target as HTMLInputElement).id;
@@ -114,86 +286,6 @@ const RegisterForm = (data) => {
         return true;
     }
 
-    async function registerClient() {
-        if (validate()) {
-            try {
-                setPageLoading(true);
-                const response = await register()
-                if (response.result !== 'success') {
-                    setError('Error in registration.');
-                }
-                setPageLoading(false);
-            } catch (err) {
-                console.error(err)
-            }
-        }
-    }
-
-    async function register() {
-        try {
-            const opConfigurationEndpoint = generateOpenIdConfigurationURL(issuerOption.map((iss) => iss.value)[0]);
-            const opConfigurationEndpointURL = new URL(opConfigurationEndpoint);
-            const issuer = opConfigurationEndpointURL.protocol + '//' + opConfigurationEndpointURL.hostname;
-            const scope = scopeOption.map((ele) => ele.value).join(" ");
-            const openapiConfig = await getOpenidConfiguration(opConfigurationEndpoint);
-
-            if (openapiConfig != undefined) {
-                chrome.storage.local.set({ opConfiguration: openapiConfig.data }).then(() => {
-                    console.log("openapiConfig is set to " + openapiConfig);
-                });
-
-                const registrationUrl = openapiConfig.data.registration_endpoint;
-
-                var registerObj: ILooseObject = {
-                    redirect_uris: [issuer],
-                    scope: scope,
-                    post_logout_redirect_uris: [chrome.runtime.getURL('options.html')],
-                    response_types: ['code'],
-                    grant_types: ['authorization_code', 'client_credentials'],
-                    application_type: 'web',
-                    client_name: 'Gluu-RP-' + uuidv4(),
-                    token_endpoint_auth_method: 'client_secret_basic'
-                };
-
-                if (showClientExpiry) {
-                    registerObj.lifetime = ((clientExpiryDate.getTime() - moment().toDate().getTime()) / 1000);
-                }
-
-                const registrationResp = await registerOIDCClient(registrationUrl, registerObj);
-
-                if (registrationResp !== undefined) {
-                    chrome.storage.local.set({
-                        oidcClient: {
-                            'op_host': issuer,
-                            'client_id': registrationResp.data.client_id,
-                            'client_secret': registrationResp.data.client_secret,
-                            'scope': registerObj.scope,
-                            'redirect_uri': registerObj.redirect_uris,
-                            'authorization_endpoint': openapiConfig.data.authorization_endpoint,
-                            'response_type': registerObj.response_types,
-                            'post_logout_redirect_uris': registerObj.post_logout_redirect_uris,
-                            'expire_at': clientExpiryDate.getTime(),
-                            'showClientExpiry': showClientExpiry
-
-                        }
-                    })
-                    console.log('OIDC client registered successfully!')
-                    console.log("oidcClient is set for client_id: " + registrationResp.data.client_id);
-
-                    return await { result: "success", message: "Regstration successful!" };
-
-                } else {
-                    return await { result: "error", message: "Error in registration!" };
-                }
-            } else {
-                return await { result: "error", message: "Error in fetching Openid configuration!" };
-            }
-        } catch (err) {
-            console.error(err)
-            return { result: "error", message: "Error in registration!" };
-        }
-    }
-
     async function registerOIDCClient(registration_endpoint, registerObj) {
         try {
             const registerReqOptions = {
@@ -228,55 +320,64 @@ const RegisterForm = (data) => {
             <legend><span className="number">O</span> Register OIDC Client</legend>
             {error.length > 0 ? <legend><span className="redFont">{error}</span></legend> : ""}
             <WindmillSpinner loading={pageLoading} color="#00ced1" />
-            <label><b>Issuer</b><span className="required">*</span> <span style={{ fontSize: 12 }}>(Enter OpenID Provider URL and press ENTER to validate)</span> :</label>
-            <CreatableSelect
-                inputId="issuer"
-                components={components}
-                inputValue={inputValueIssuer}
-                isClearable
-                isMulti
-                isLoading={isLoading}
-                menuIsOpen={false}
-                onChange={(newValue) => setIssuerOption(newValue)}
-                onInputChange={(newValue) => setInputValueIssuer(newValue)}
-                onKeyDown={handleKeyDown}
-                placeholder="Type something and press enter..."
-                value={issuerOption}
-                className="inputText"
-            />
-
-            <label><input type="checkbox" onChange={() => setShowClientExpiry(!showClientExpiry)} /><b>Set Client Expiry</b></label>
-            {showClientExpiry ?
+            <label><input type="checkbox" onChange={() => setUploadSsa(!uploadSsa)} /><b>Register using SSA</b></label>
+            {uploadSsa ?
                 <>
-                    <label><b>Client expiry date</b><span className="required">*</span> <span style={{ fontSize: 12 }}>(Select the date)</span> :</label>
-                    <DatePicker
-                        showTimeSelect
-                        selected={clientExpiryDate}
-                        onChange={(date) => setClientExpiryDate(date)}
-                        minDate={new Date()}
-                        className="inputText inputStyle"
-                        dateFormat="yyyy/MM/dd h:mm aa"
+                    <StyledDropzone submitFile={readJWTFile} />
+                </>
+                :
+                <>
+                    <label><b>Issuer</b><span className="required">*</span> <span style={{ fontSize: 12 }}>(Enter OpenID Provider URL and press ENTER to validate)</span> :</label>
+                    <CreatableSelect
+                        inputId="issuer"
+                        components={components}
+                        inputValue={inputValueIssuer}
+                        isClearable
+                        isMulti
+                        isLoading={isLoading}
+                        menuIsOpen={false}
+                        onChange={(newValue) => setIssuerOption(newValue)}
+                        onInputChange={(newValue) => setInputValueIssuer(newValue)}
+                        onKeyDown={handleKeyDown}
+                        placeholder="Type something and press enter..."
+                        value={issuerOption}
+                        className="inputText"
                     />
-                </> : ''}
-            <label><b>Scopes</b><span className="required">*</span> <span style={{ fontSize: 12 }}>(Type and press enter)</span> :</label>
 
-            <CreatableSelect
-                inputId='scope'
-                components={components}
-                inputValue={inputValueScope}
-                isClearable
-                isMulti
-                menuIsOpen={false}
-                onChange={(newValue) => setScopeOption(newValue)}
-                onInputChange={(newValue) => setInputValueScope(newValue)}
-                onKeyDown={handleKeyDown}
-                placeholder="Type something and press enter..."
-                value={scopeOption}
-                className="inputText"
-            />
+                    <label><input type="checkbox" onChange={() => setShowClientExpiry(!showClientExpiry)} /><b>Set Client Expiry</b></label>
+                    {showClientExpiry ?
+                        <>
+                            <label><b>Client expiry date</b><span className="required">*</span> <span style={{ fontSize: 12 }}>(Select the date)</span> :</label>
+                            <DatePicker
+                                showTimeSelect
+                                selected={clientExpiryDate}
+                                onChange={(date) => setClientExpiryDate(date)}
+                                minDate={new Date()}
+                                className="inputText inputStyle"
+                                dateFormat="yyyy/MM/dd h:mm aa"
+                            />
+                        </> : ''}
+                    <label><b>Scopes</b><span className="required">*</span> <span style={{ fontSize: 12 }}>(Type and press enter)</span> :</label>
 
-            <legend><span className="redFont">{error}</span></legend>
-            <button id="sbmtButton" onClick={registerClient}>Register</button>
+                    <CreatableSelect
+                        inputId='scope'
+                        components={components}
+                        inputValue={inputValueScope}
+                        isClearable
+                        isMulti
+                        menuIsOpen={false}
+                        onChange={(newValue) => setScopeOption(newValue)}
+                        onInputChange={(newValue) => setInputValueScope(newValue)}
+                        onKeyDown={handleKeyDown}
+                        placeholder="Type something and press enter..."
+                        value={scopeOption}
+                        className="inputText"
+                    />
+
+                    <legend><span className="redFont">{error}</span></legend>
+                </>
+            }
+            <button id="sbmtButton" onClick={triggerClientRegistration}>Register</button>
         </div>
     )
 };
