6ce4e70efabb1ffa2253abada56b18a59c89ffd8
Devrim <devrimyatar@gluu.org>
2023-11-13 14:47:10 +0300

    feat(jans-cli-tui): replace list by label container for scope claims (#6521)
    * fix(jans-cli-tui): replace list by label container for scope claims
    
    * fix(jans-cli-tui): fixes after claims label container change
    
    * fix(jans-cli-tui): code smell
    
    * fix(jans-cli-tui): typo
    
    Signed-off-by: Mustafa Baser <mbaser@mail.com>

---
+++ jans-cli-tui/cli_tui/utils/background_tasks.py
@@ -0,0 +1,34 @@
+from prompt_toolkit.eventloop import get_event_loop
+
+from utils.utils import common_data
+from utils.static import common_strings
+
+
+async def get_attributes_coroutine(app) -> None:
+
+    common_data.jans_attributes = []
+    start_index = 1
+    limit = 100
+
+    while True:
+
+        cli_args = {'operation_id': 'get-attributes', 'endpoint_args': f'limit:{limit},startIndex:{start_index}'}
+        response = await app.loop.run_in_executor(app.executor, app.cli_requests, cli_args)
+
+        if response.status_code == 200:
+            try:
+                data = response.json()
+                if data.get('entriesCount'):
+                    common_data.jans_attributes += data['entries']
+                    app.logger.info("%d attributes retreived", data['entriesCount'])
+                else:
+                    break
+
+                start_index += limit
+
+            except Exception as e:
+                app.logger.error("Failed tor retreive attributes %s", e)
+                break
+        else:
+            break
+
