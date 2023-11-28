5764c6ce97a36557cc1825ad22d85c3497628725
Mustafa Baser <mbaser@mail.com>
2023-11-28 19:27:17 +0300

    fix(jans-cli-tui): import error

--- jans-cli-tui/cli_tui/utils/background_tasks.py
+++ jans-cli-tui/cli_tui/utils/background_tasks.py
@@ -2,6 +2,7 @@ from prompt_toolkit.eventloop import get_event_loop
 
 from utils.utils import common_data
 from utils.static import common_strings
+from utils.multi_lang import _
 
 
 async def get_attributes_coroutine(app) -> None:
@@ -40,7 +41,7 @@ async def retrieve_enabled_scripts() -> None:
     response = await common_data.app.loop.run_in_executor(common_data.app.executor, common_data.app.cli_requests, cli_args)
 
     if response.status_code not in (200, 201):
-        common_data.app.show_message(_("Error getting scriots"), str(response.text), tobefocused=common_data.app.center_frame)
+        common_data.app.show_message(_("Error getting scriotp"), str(response.text), tobefocused=common_data.app.center_frame)
         return
 
     result = response.json()
