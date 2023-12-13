370f7ec561bc72d2c8f515f3ce10e922c0bdd9f2
Yuriy M <95305560+yuremm@users.noreply.github.com>
2023-12-13 14:26:18 +0300

    feat: add Jans lock (#7074)
    * feat: add lock project and modules
    
    * chore: samples
    
    * chore: add dependecies
    
    * chore: allow to start lock as server or as jans-auth plugin
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    * chore: call each plugin configuration factory
    
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    * chore: update version
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    * chore: call each plugin configuration factory
    
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    * chore: call each plugin configuration factory
    
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    ---------
    
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    Co-authored-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    Co-authored-by: YuriyZ <yzabrovarniy@gmail.com>
    Signed-off-by: Mustafa Baser <mbaser@mail.com>

---
+++ jans-core/script/src/main/java/io/jans/model/custom/script/type/lock/DummyLockExtensionType.java
@@ -0,0 +1,35 @@
+/*
+ * Janssen Project software is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2023, Janssen Project
+ */
+
+package io.jans.model.custom.script.type.lock;
+
+import java.util.Map;
+
+import io.jans.model.SimpleCustomProperty;
+import io.jans.model.custom.script.model.CustomScript;
+
+public class DummyLockExtensionType implements LockExtensionType {
+    @Override
+    public boolean init(Map<String, SimpleCustomProperty> configurationAttributes) {
+        return true;
+    }
+
+    @Override
+    public boolean init(CustomScript customScript, Map<String, SimpleCustomProperty> configurationAttributes) {
+        return true;
+    }
+
+    @Override
+    public boolean destroy(Map<String, SimpleCustomProperty> configurationAttributes) {
+        return true;
+    }
+
+    @Override
+    public int getApiVersion() {
+        return 1;
+    }
+
+}
