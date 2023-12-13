4552846a7c9655995336d0775fa80e490c3a6dfc
Yuriy M <95305560+yuremm@users.noreply.github.com>
2023-12-13 14:26:18 +0300

    feat: add Jans lock (#7074)
    * feat: add lock project and modules
    
    * chore: samples
    
    * chore: add dependecies
    
    * chore: allow to start lock as server or as jans-auth plugin
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    * chore: call each plugin configuration factory
    
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    * chore: update version
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    * chore: call each plugin configuration factory
    
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    * chore: call each plugin configuration factory
    
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    ---------
    
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    Co-authored-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    Co-authored-by: YuriyZ <yzabrovarniy@gmail.com>

---
+++ jans-core/script/src/main/java/io/jans/model/custom/script/type/lock/LockExtensionType.java
@@ -0,0 +1,13 @@
+/*
+ * Janssen Project software is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2023, Janssen Project
+ */
+
+package io.jans.model.custom.script.type.lock;
+
+import io.jans.model.custom.script.type.BaseExternalType;
+
+public interface LockExtensionType extends BaseExternalType {
+
+}
