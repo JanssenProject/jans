39c582c782d6626ee20d9bbbcf6b00da18b3e925
YuriyZ <yzabrovarniy@gmail.com>
2023-12-21 12:34:02 +0200

    feat(jans-auth-server): support for OAuth 2.0 Rich Authorization Requests (#7145)
    * feat(jans-auth-server): rar - added authorizationDetailsTypesSupported
    
    https://github.com/JanssenProject/jans/issues/6292
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): added supported authz details types to discovery #6933
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): added authorization_details custom interception script and dynamic registration #6933
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): used in templates title jans instead of oxauth #6933
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): authorization_details authorization #6292
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): added authz details service with check/save ability #6292
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): added authz details support to token endpoint #6292
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): added authz details to PAR #6292
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): added authz details to introspection #6292
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * chore(jans-auth-server): authz details related refactoring #6292
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * chore(jans-auth-server): authz details related refactoring #6292
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * test(jans-auth-server): added AuthzDetailsServiceTest #6292
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * test(jans-auth-server): added integration test for Rich Authorization Requests #6292
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * doc(jans-auth-server): added documentation for rich authorization requests #6292
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    ---------
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>

--- jans-auth-server/model/src/main/java/io/jans/as/model/token/TokenErrorResponseType.java
+++ jans-auth-server/model/src/main/java/io/jans/as/model/token/TokenErrorResponseType.java
@@ -60,6 +60,18 @@ public enum TokenErrorResponseType implements IErrorType {
     INVALID_SCOPE("invalid_scope"),
 
     /**
+     * invalid_authorization_details is returned to the client if any of the
+     * following are true of the objects in the authorization_details structure:
+     *
+     * - contains an unknown authorization details type value,
+     * - is an object of known type but containing unknown fields,
+     * - contains fields of the wrong type for the authorization details type,
+     * - contains fields with invalid values for the authorization details type, or
+     * - is missing required fields for the authorization details type.
+     */
+    INVALID_AUTHORIZATION_DETAILS("invalid_authorization_details "),
+
+    /**
      * CIBA. The authorization request is still pending as the end-user hasn't yet been authenticated.
      */
     AUTHORIZATION_PENDING("authorization_pending"),
