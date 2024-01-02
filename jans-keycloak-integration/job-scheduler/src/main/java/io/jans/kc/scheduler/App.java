2a35184f20bc3ad58db736d91ed0a6005b585bf2
Rolain Djeumen <uprightech@gmail.com>
2024-01-02 15:08:09 +0100

    feat(keycloak-integration): application startup #6390 * added scheduler to application startup * made relevant modifications to the application configuration
    Signed-off-by: Rolain Djeumen <uprightech@gmail.com>

--- jans-keycloak-integration/job-scheduler/src/main/java/io/jans/kc/scheduler/App.java
+++ jans-keycloak-integration/job-scheduler/src/main/java/io/jans/kc/scheduler/App.java
@@ -1,10 +1,3 @@
-package io.jans.kc.scheduler;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import io.jans.kc.scheduler.config.Configuration;
-import io.jans.kc.scheduler.config.ConfigurationException;
 /**
  * Background scheduler application 
  * This application runs and monitors various background jobs that are 
@@ -12,31 +5,59 @@ import io.jans.kc.scheduler.config.ConfigurationException;
  * E.g. configuration synchronization 
  * Author : Rolain Djeumen <rolain@gluu.org> 
  */
+
+package io.jans.kc.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.jans.kc.scheduler.config.Configuration;
+import io.jans.kc.scheduler.config.ConfigurationException;
+
+import io.jans.kc.scheduler.job.*;
+import io.jans.kc.scheduler.job.service.JobScheduler;
+import io.jans.kc.scheduler.job.service.JobSchedulerException;
+import io.jans.kc.scheduler.job.service.impl.QuartzJobScheduler;
+
 public class App {
     
     private static final String APP_DISPLAY_NAME = "Keycloak";
     private static final String PROP_APP_CFG_FILE = "app.config";
     private static final String DEFAULT_APP_CFG_FILEPATH = "/opt/kc-scheduler/conf/config.properties";
     private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    private static JobScheduler jobScheduler = null;
+    private static boolean running = false;
     /*
      * Entry point 
      */
-    public static void main(String[] args) {
-        
-        log.info("Application starting ...");
+    public static void main(String[] args) throws InterruptedException {
 
+        log.info("Application starting ...");
         Configuration config = null;
         try {
             log.debug("Loading application configuration");
             config = loadApplicationConfiguration();
             log.debug("Application configuration loaded successfully. {}",config.toString());
+
+            log.debug("Initializing scheduler ");
+            jobScheduler = createJobScheduler(config);
+            startJobScheduler(jobScheduler);
+
+            log.debug("Performing post-startup operations");
+            performPostStartupOperations();
+
+            log.debug("Application startup successful");
+            while(running) {
+                Thread.sleep(1000);
+            }
+            log.debug("Application shutting down");
         }catch(ConfigurationException e) {
             log.error("Application startup failed. ",e);
             System.exit(-1);
             return;
         }
 
-        log.info("Application Startup Successful.");
     }
 
     private static final String getAppConfigFileName() {
@@ -53,5 +74,70 @@ public class App {
         }
         log.debug("Application configuration file: {} ",config_file_name);
         return Configuration.fromFile(config_file_name);
+    }
+
+    private static final JobScheduler createJobScheduler(Configuration configuration) {
+
+        return createQuartzJobSchedulerFromConfiguration(configuration);
+    }
+
+    private static final void startJobScheduler(JobScheduler jobScheduler) {
+
+        jobScheduler.start();
+    }
+
+    private static final JobScheduler createQuartzJobSchedulerFromConfiguration(Configuration configuration) {
+        
+        try {
+            return QuartzJobScheduler.builder()
+                .name(configuration.quatzSchedulerName())
+                .instanceId(configuration.quartzSchedulerInstanceId())
+                .threadPoolSize(configuration.quartzSchedulerThreadPoolSize())
+                .build();
+        }catch(ConfigurationException e) {
+            throw new StartupError("Could not create quartz job scheduler",e);
+        }
+    }
+
+    private static final void performPostStartupOperations() {
+
+        running = true;
+        registerShutdownHook();
+    }
+
+    private static final void registerShutdownHook() {
+
+        Runtime.getRuntime().addShutdownHook(new ShutdownHook());
+    }
+
+    
+    public static class ShutdownHook extends Thread  {
+        
+        
+        @Override
+        public void run() {
+
+            try {
+                log.debug("Shutting down application");
+                if (jobScheduler != null) {
+                    jobScheduler.stop();
+                }
+            }catch(Exception e) {
+                log.warn("Something unexpected happened while stopping the scheduler",e);
+            }
+
+            running = false;
+        }
+    }
+
+    public static class StartupError extends RuntimeException {
+
+        public StartupError(String msg) {
+            super(msg);
+        }
+
+        public StartupError(String msg, Throwable cause) {
+            super(msg,cause);
+        }
     }
 }
--- jans-keycloak-integration/job-scheduler/src/main/java/io/jans/kc/scheduler/config/Configuration.java
+++ jans-keycloak-integration/job-scheduler/src/main/java/io/jans/kc/scheduler/config/Configuration.java
@@ -11,6 +11,9 @@ public class Configuration {
     private static final String SYS_PROP_APP_VERSION = "app.version";
 
     private static final String CFG_PROP_APP_VERSION = SYS_PROP_APP_VERSION;
+    private static final String CFG_PROP_QUARTZ_SCHEDULER_NAME = "app.scheduler.quartz.name";
+    private static final String CFG_PROP_QUARTZ_SCHEDULER_INSTANCEID = "app.scheduler.quartz.instanceid";
+    private static final String CFG_PROP_QUARTZ_SCHEDULER_THREAD_POOL_SIZE = "app.scheduler.quartz.threadpoolsize";
 
     private final Properties configProperties;
 
@@ -19,10 +22,44 @@ public class Configuration {
         this.configProperties = configProperties;
     }
 
-    public final String getAppVersion() {
+    public String appVersion() {
 
-        return configProperties.getProperty(CFG_PROP_APP_VERSION);
+        return getStringEntry(CFG_PROP_APP_VERSION);
     }
+
+    public String quatzSchedulerName() {
+
+        return getStringEntry(CFG_PROP_QUARTZ_SCHEDULER_NAME);
+    }
+
+    public String quartzSchedulerInstanceId() {
+
+        return getStringEntry(CFG_PROP_QUARTZ_SCHEDULER_INSTANCEID);
+    }
+
+    public Integer quartzSchedulerThreadPoolSize() {
+
+        return getIntEntry(CFG_PROP_QUARTZ_SCHEDULER_THREAD_POOL_SIZE);
+    }
+
+    private String getStringEntry(String entry) {
+
+        return configProperties.getProperty(entry);
+    }
+
+    private Integer getIntEntry(String entry) {
+
+        String strvalue = configProperties.getProperty(entry);
+        if(strvalue == null || strvalue.isEmpty() ) {
+            return null;
+        }
+        try {
+            return Integer.parseInt(strvalue);
+        }catch(NumberFormatException e) {
+            throw new ConfigurationException("Unable to get specified configuration entryQue Dor",e);
+        }
+    }
+
 
     @Override
     public String toString() {
@@ -34,7 +71,7 @@ public class Configuration {
         StringBuilder sb = new StringBuilder();
         sb.append(newline);
         sb.append(header+newline);
-        sb.append("+ Application version: " + getAppVersion()+" "+newline);
+        sb.append("+ Application version: " + appVersion()+" "+newline);
         sb.append(footer);
         
         return sb.toString();
