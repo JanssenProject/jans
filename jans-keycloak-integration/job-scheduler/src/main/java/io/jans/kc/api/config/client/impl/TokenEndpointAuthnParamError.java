5ae5a3d68c89c29f7be1d350462698c9ad4989f5
Rolain Djeumen <uprightech@gmail.com>
2024-01-09 16:53:19 +0100

    feat(keycloak-integration): added interface to obtain config-api credentials #6390
    Signed-off-by: Rolain Djeumen <uprightech@gmail.com>

---
+++ jans-keycloak-integration/job-scheduler/src/main/java/io/jans/kc/api/config/client/impl/TokenEndpointAuthnParamError.java
@@ -0,0 +1,8 @@
+package io.jans.kc.api.config.client.impl;
+
+public class TokenEndpointAuthnParamError extends RuntimeException {
+    
+    public TokenEndpointAuthnParamError(String message) {
+        super(message);
+    }
+}
