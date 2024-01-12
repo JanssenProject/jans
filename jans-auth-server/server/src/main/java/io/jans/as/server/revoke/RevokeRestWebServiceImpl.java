a8ae5fe4aeffe2c814a0de1a6b911d00a12386b0
YuriyZ <yzabrovarniy@gmail.com>
2024-01-12 12:46:05 +0200

    feat(jans-auth-server): added support to revoke tx token #6291
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>

--- jans-auth-server/server/src/main/java/io/jans/as/server/revoke/RevokeRestWebServiceImpl.java
+++ jans-auth-server/server/src/main/java/io/jans/as/server/revoke/RevokeRestWebServiceImpl.java
@@ -195,6 +195,7 @@ public class RevokeRestWebServiceImpl implements RevokeRestWebService {
         for (TokenEntity token : tokens) {
             if (tth == null ||
                     (tth == TokenTypeHint.ACCESS_TOKEN && token.getTokenTypeEnum() == TokenType.ACCESS_TOKEN) ||
+                    (tth == TokenTypeHint.TX_TOKEN && token.getTokenTypeEnum() == TokenType.TX_TOKEN) ||
                     (tth == TokenTypeHint.REFRESH_TOKEN && token.getTokenTypeEnum() == TokenType.REFRESH_TOKEN)) {
                 grantService.removeSilently(token);
             }
@@ -202,7 +203,7 @@ public class RevokeRestWebServiceImpl implements RevokeRestWebService {
     }
 
     private AuthorizationGrant findAuthorizationGrant(String token, TokenTypeHint tth) {
-        if (tth == TokenTypeHint.ACCESS_TOKEN) {
+        if (tth == TokenTypeHint.ACCESS_TOKEN || tth == TokenTypeHint.TX_TOKEN) {
             return authorizationGrantList.getAuthorizationGrantByAccessToken(token);
         }
 
