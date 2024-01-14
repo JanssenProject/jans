20d69da3a22c8a6572ceccd7aaf441d7ddfdf101
Rolain Djeumen <uprightech@gmail.com>
2024-01-14 22:22:45 +0100

    feat(keycloak-integration): added config-api wrapper #6390
    Signed-off-by: Rolain Djeumen <uprightech@gmail.com>

---
+++ jans-keycloak-integration/job-scheduler/src/main/java/io/jans/kc/api/config/client/model/JansTrustRelationship.java
@@ -0,0 +1,13 @@
+package io.jans.kc.api.config.client.model;
+
+import io.jans.config.api.client.model.TrustRelationship;
+
+public class JansTrustRelationship {
+    
+    private TrustRelationship tr; 
+
+    public JansTrustRelationship(TrustRelationship tr) {
+
+        this.tr = tr;
+    }
+}
