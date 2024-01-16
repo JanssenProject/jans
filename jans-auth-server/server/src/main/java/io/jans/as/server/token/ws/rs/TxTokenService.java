9edf2df9765629b3639bb93b4b7bbcbb300d05e6
YuriyZ <yzabrovarniy@gmail.com>
2024-01-16 10:06:04 +0200

    feat(jans-auth-server): completed tx token service implementation #6291
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>

--- jans-auth-server/server/src/main/java/io/jans/as/server/token/ws/rs/TxTokenService.java
+++ jans-auth-server/server/src/main/java/io/jans/as/server/token/ws/rs/TxTokenService.java
@@ -1,31 +1,55 @@
 package io.jans.as.server.token.ws.rs;
 
+import io.jans.as.common.claims.Audience;
+import io.jans.as.common.model.common.User;
+import io.jans.as.common.model.registration.Client;
+import io.jans.as.common.util.CommonUtils;
 import io.jans.as.model.common.ExchangeTokenType;
 import io.jans.as.model.common.SubjectTokenType;
+import io.jans.as.model.common.TokenType;
+import io.jans.as.model.config.WebKeysConfiguration;
 import io.jans.as.model.configuration.AppConfiguration;
 import io.jans.as.model.crypto.AbstractCryptoProvider;
+import io.jans.as.model.crypto.encryption.BlockEncryptionAlgorithm;
+import io.jans.as.model.crypto.encryption.KeyEncryptionAlgorithm;
+import io.jans.as.model.crypto.signature.SignatureAlgorithm;
 import io.jans.as.model.error.ErrorResponseFactory;
-import io.jans.as.model.exception.InvalidJwtException;
+import io.jans.as.model.exception.InvalidJweException;
+import io.jans.as.model.jwe.Jwe;
+import io.jans.as.model.jwe.JweEncrypter;
+import io.jans.as.model.jwe.JweEncrypterImpl;
+import io.jans.as.model.jwk.Algorithm;
+import io.jans.as.model.jwk.JSONWebKeySet;
+import io.jans.as.model.jwk.KeyOpsType;
+import io.jans.as.model.jwk.Use;
 import io.jans.as.model.jwt.Jwt;
+import io.jans.as.model.jwt.JwtType;
+import io.jans.as.model.token.JsonWebResponse;
 import io.jans.as.model.token.TokenErrorResponseType;
 import io.jans.as.server.audit.ApplicationAuditLogger;
 import io.jans.as.server.model.audit.OAuth2AuditLog;
-import io.jans.as.server.model.common.AbstractToken;
-import io.jans.as.server.model.common.AuthorizationGrant;
-import io.jans.as.server.model.common.AuthorizationGrantList;
-import io.jans.as.server.model.common.ExecutionContext;
+import io.jans.as.server.model.common.*;
+import io.jans.as.server.model.ldap.TokenEntity;
+import io.jans.as.server.model.token.JwtSigner;
+import io.jans.as.server.service.ClientService;
+import io.jans.as.server.service.ServerCryptoProvider;
 import io.jans.as.server.util.ServerUtil;
-import io.jans.as.server.util.TokenHashUtil;
+import io.jans.util.security.StringEncrypter;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.apache.tika.utils.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
 import org.slf4j.Logger;
+
+import java.nio.charset.StandardCharsets;
+import java.security.PublicKey;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * @author Yuriy Z
@@ -47,30 +71,171 @@ public class TxTokenService {
     private ErrorResponseFactory errorResponseFactory;
 
     @Inject
-    private AuthorizationGrantList authorizationGrantList;
+    private TxTokenValidator txTokenValidator;
 
     @Inject
     private AbstractCryptoProvider cryptoProvider;
 
-    public Response processTxToken(ExecutionContext executionContext) {
+    @Inject
+    private ClientService clientService;
+
+    @Inject
+    private WebKeysConfiguration webKeysConfiguration;
+
+    @Inject
+    private AuthorizationGrantList authorizationGrantList;
+
+    public Response processTxToken(ExecutionContext executionContext) throws Exception {
         final JSONObject responseJson = process(executionContext);
         final String entity = responseJson.toString();
+        log.trace("Created TxToken: {}", entity);
         return response(Response.ok().entity(entity), executionContext.getAuditLog());
     }
 
-    private JSONObject process(ExecutionContext executionContext) {
+    private JSONObject process(ExecutionContext executionContext) throws Exception {
         final String requestedTokenType = executionContext.getHttpRequest().getParameter("requested_token_type");
         final String subjectToken = executionContext.getHttpRequest().getParameter("subject_token");
         final String subjectTokenType = executionContext.getHttpRequest().getParameter("subject_token_type");
         final String audience = executionContext.getHttpRequest().getParameter("audience");
         final String requestContext = executionContext.getHttpRequest().getParameter("rctx");
+        final String scope = executionContext.getHttpRequest().getParameter("scope");
 
-        validateRequestedTokenType(requestedTokenType, executionContext.getAuditLog());
-        SubjectTokenType subjectTokenTypeEnum = validateSubjectTokenType(subjectTokenType, executionContext.getAuditLog());
-        validateSubjectToken(subjectToken, subjectTokenTypeEnum, executionContext.getAuditLog());
+        final Client client = executionContext.getClient();
+
+        txTokenValidator.validateRequestedTokenType(requestedTokenType, executionContext.getAuditLog());
+        SubjectTokenType subjectTokenTypeEnum = txTokenValidator.validateSubjectTokenType(subjectTokenType, executionContext.getAuditLog());
+        AuthorizationGrant subjectGrant = txTokenValidator.validateSubjectToken(subjectToken, subjectTokenTypeEnum, executionContext.getAuditLog());
+
+        TxTokenGrant txTokenGrant = authorizationGrantList.createTxTokenGrant(new User(), client);
+        txTokenGrant.checkScopesPolicy(scope);
+
+        executionContext.setGrant(txTokenGrant);
+
+        final JsonWebResponse jwr = createTxTokenJwr(audience, requestContext, executionContext, subjectGrant);
+        final String jwrString = jwr.toString();
+
+        TxToken txToken = new TxToken(getTxTokenLifetime(client));
+        txToken.setCode(jwrString);
+
+        final TokenEntity tokenEntity = txTokenGrant.asToken(txToken);
+        txTokenGrant.persist(tokenEntity);
 
         JSONObject responseJson = new JSONObject();
+        responseJson.put("issued_token_type", ExchangeTokenType.TX_TOKEN.getName());
+        responseJson.put("token_type", TokenType.TX_TOKEN.getName());
+        responseJson.put("access_token", jwrString);
         return responseJson;
+    }
+
+    private JsonWebResponse createTxTokenJwr(String audience, String requestContext, ExecutionContext executionContext, AuthorizationGrant authorizationGrant) throws Exception {
+        final Client client = executionContext.getClient();
+
+        KeyEncryptionAlgorithm keyAlgorithm = KeyEncryptionAlgorithm.fromName(client.getAttributes().getTxTokenEncryptedResponseAlg());
+        BlockEncryptionAlgorithm blockAlgorithm = BlockEncryptionAlgorithm.fromName(client.getAttributes().getTxTokenEncryptedResponseEnc());
+
+        if (keyAlgorithm != null && blockAlgorithm != null) {
+            log.trace("Preparing encrypted TxToken with keyEncryptionAlgorithm {}, blockEncryptionAlgorithm: {}", keyAlgorithm, blockAlgorithm);
+            Jwe jwe = new Jwe();
+
+            // Header
+            jwe.getHeader().setType(JwtType.TX_TOKEN);
+            jwe.getHeader().setAlgorithm(keyAlgorithm);
+            jwe.getHeader().setEncryptionMethod(blockAlgorithm);
+
+            // Claims
+            fillPayload(jwe, audience, requestContext, executionContext, authorizationGrant);
+
+            // nested signed jwt payload
+            JwtSigner jwtSigner = newJwtSigner(client);
+            Jwt jwt = jwtSigner.newJwt();
+            jwt.setClaims(jwe.getClaims());
+            jwe.setSignedJWTPayload(signJwt(jwt, client));
+
+            if (keyAlgorithm == KeyEncryptionAlgorithm.RSA_OAEP || keyAlgorithm == KeyEncryptionAlgorithm.RSA1_5) {
+                JSONObject jsonWebKeys = CommonUtils.getJwks(client);
+                String keyId = new ServerCryptoProvider(cryptoProvider).getKeyId(JSONWebKeySet.fromJSONObject(jsonWebKeys),
+                        Algorithm.fromString(keyAlgorithm.getName()),
+                        Use.ENCRYPTION, KeyOpsType.CONNECT);
+                PublicKey publicKey = cryptoProvider.getPublicKey(keyId, jsonWebKeys, null);
+                jwe.getHeader().setKeyId(keyId);
+
+                if (publicKey == null) {
+                    throw new InvalidJweException("The public key is not valid");
+                }
+
+                JweEncrypter jweEncrypter = new JweEncrypterImpl(keyAlgorithm, blockAlgorithm, publicKey);
+                return jweEncrypter.encrypt(jwe);
+            }
+            if (keyAlgorithm == KeyEncryptionAlgorithm.A128KW || keyAlgorithm == KeyEncryptionAlgorithm.A256KW) {
+                byte[] sharedSymmetricKey = clientService.decryptSecret(client.getClientSecret()).getBytes(StandardCharsets.UTF_8);
+                JweEncrypter jweEncrypter = new JweEncrypterImpl(keyAlgorithm, blockAlgorithm, sharedSymmetricKey);
+                return jweEncrypter.encrypt(jwe);
+            }
+        }
+
+        log.trace("Preparing signed TxToken, client {}", client.getClientId());
+
+        final JwtSigner jwtSigner = newJwtSigner(client);
+        final Jwt jwt = jwtSigner.newJwt();
+        fillPayload(jwt, audience, requestContext, executionContext, authorizationGrant);
+        return jwtSigner.sign();
+    }
+
+    private void fillPayload(JsonWebResponse jwr, String audience, String requestContext, ExecutionContext executionContext, AuthorizationGrant authorizationGrant) {
+        Client client = executionContext.getClient();
+
+        Calendar calendar = Calendar.getInstance();
+        Date issuedAt = calendar.getTime();
+        calendar.add(Calendar.SECOND, getTxTokenLifetime(client));
+        Date expiration = calendar.getTime();
+
+        jwr.getClaims().setIssuer(appConfiguration.getIssuer());
+        jwr.getClaims().setExpirationTime(expiration);
+        jwr.getClaims().setIssuedAt(issuedAt);
+        jwr.setClaim("txn", UUID.randomUUID().toString());
+        jwr.setClaim("sub_id", UUID.randomUUID().toString());
+
+        Audience.setAudience(jwr.getClaims(), client);
+        if (StringUtils.isNotBlank(audience)) {
+            jwr.getClaims().addAudience(audience);
+        }
+
+        if (StringUtils.isNotBlank(requestContext)) {
+            jwr.getClaims().setClaim("req_ctx", new JSONObject(requestContext));
+        }
+
+        if (authorizationGrant != null) {
+            jwr.setClaim("sub_id", authorizationGrant.getSub());
+        }
+
+        JSONObject azd = new JSONObject();
+        azd.put("client_id", client.getClientId());
+        jwr.getClaims().setClaim("azd", azd);
+    }
+
+    private int getTxTokenLifetime(Client client) {
+        if (client.getAttributes().getTxTokenLifetime() != null && client.getAttributes().getTxTokenLifetime() > 0) {
+            log.trace("Override TxToken lifetime with value {} from client: {}", client.getAttributes().getTxTokenLifetime(), client.getClientId());
+            return client.getAttributes().getTxTokenLifetime();
+        }
+        return appConfiguration.getTxTokenLifetime();
+    }
+
+    private Jwt signJwt(Jwt jwt, Client client) throws Exception {
+        JwtSigner jwtSigner = newJwtSigner(client);
+
+        jwtSigner.setJwt(jwt);
+        jwtSigner.sign();
+        return jwt;
+    }
+
+    private JwtSigner newJwtSigner(Client client) throws StringEncrypter.EncryptionException {
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.fromString(appConfiguration.getDefaultSignatureAlgorithm());
+        if (client.getAttributes().getTxTokenSignedResponseAlg() != null) {
+            signatureAlgorithm = SignatureAlgorithm.fromString(client.getAttributes().getTxTokenSignedResponseAlg());
+        }
+
+        return new JwtSigner(appConfiguration, webKeysConfiguration, signatureAlgorithm, client.getClientId(), clientService.decryptSecret(client.getClientSecret()));
     }
 
     private Response response(Response.ResponseBuilder builder, OAuth2AuditLog oAuth2AuditLog) {
@@ -82,113 +247,16 @@ public class TxTokenService {
         return builder.build();
     }
 
-    public void validateRequestedTokenType(String requestedTokenType, OAuth2AuditLog auditLog) {
-        if (isTxTokenFlow(requestedTokenType)) {
-            return;
-        }
-
-        log.trace("Invalid requested_token_type.");
-        throw new WebApplicationException(response(error(400, TokenErrorResponseType.INVALID_REQUESTED_TOKEN_TYPE, "Unknown requested_token_type. For transaction tokens value must be " + ExchangeTokenType.TX_TOKEN.getName()), auditLog));
-    }
-
-    public SubjectTokenType validateSubjectTokenType(String subjectTokenType, OAuth2AuditLog auditLog) {
-        SubjectTokenType result = SubjectTokenType.fromString(subjectTokenType);
-        if (result != null) {
-            return result;
-        }
-
-        log.trace("Invalid subject_token_type.");
-        throw new WebApplicationException(response(error(400, TokenErrorResponseType.INVALID_SUBJECT_TOKEN_TYPE, "Unknown subject_token_type."), auditLog));
-    }
-
-    private void validateSubjectToken(String subjectToken, SubjectTokenType subjectTokenTypeEnum, OAuth2AuditLog auditLog) {
-        if (StringUtils.isBlank(subjectToken)) {
-            log.trace("Invalid subject_token. Blank value is not allowed.");
-            throw new WebApplicationException(response(error(400, TokenErrorResponseType.INVALID_GRANT, "Invalid subject_token."), auditLog));
-        }
-        if (subjectTokenTypeEnum == SubjectTokenType.ACCESS_TOKEN) {
-            validateAccessToken(subjectToken, auditLog);
-        } else if (subjectTokenTypeEnum == SubjectTokenType.ID_TOKEN) {
-            validateIdToken(subjectToken, auditLog);
-        }
-
-        log.trace("Invalid subject_token. subject_token_type is not supported.");
-        throw new WebApplicationException(response(error(400, TokenErrorResponseType.INVALID_GRANT, "Invalid subject_token. subject_token_type is not supported."), auditLog));
-    }
-
-    private void validateAccessToken(String subjectToken, OAuth2AuditLog auditLog) {
-        AuthorizationGrant authorizationGrant = authorizationGrantList.getAuthorizationGrantByAccessToken(subjectToken);
-
-        if (authorizationGrant == null) {
-            log.trace("Failed to find authorization grant by subject_token: {}", subjectToken);
-            throw new WebApplicationException(response(error(400, TokenErrorResponseType.INVALID_GRANT, "Failed to find authorization grant by subject_token."),auditLog));
-        }
-
-        final AbstractToken authorizationAccessToken = authorizationGrant.getAccessToken(subjectToken);
-
-        if ((authorizationAccessToken == null || !authorizationAccessToken.isValid())) {
-            log.error("Access token is not valid. Valid: {}", (authorizationAccessToken != null && authorizationAccessToken.isValid()));
-            throw new WebApplicationException(response(error(400, TokenErrorResponseType.INVALID_GRANT, "Access token is not valid."),auditLog));
-        }
-
-        log.trace("Invalid subject_token. Unable to validate subject_token");
-        throw new WebApplicationException(response(error(400, TokenErrorResponseType.INVALID_GRANT, "Invalid subject_token. Unable to validate subject_token."), auditLog));
-    }
-
-    private void validateIdToken(String subjectToken, OAuth2AuditLog auditLog) {
-        try {
-            final AuthorizationGrant idTokenGrant = getIdTokenGrant(subjectToken);
-
-            if (idTokenGrant != null) { // id_token is in db
-                log.debug("Found subject_token in db.");
-                return;
-            }
-
-            final Jwt jwt = Jwt.parse(subjectToken);
-
-            // verify jwt signature if we can't find it in db
-            if (!cryptoProvider.verifySignature(jwt.getSigningInput(), jwt.getEncodedSignature(), jwt.getHeader().getKeyId(),
-                    null, null, jwt.getHeader().getSignatureAlgorithm())) {
-                log.error("id_token signature verification failed.");
-                throw new WebApplicationException(response(error(400, TokenErrorResponseType.INVALID_GRANT, "Invalid subject_token. id_token signature verification failed."), auditLog));
-            }
-
-            log.debug("subject_token is validated successfully as id_token.");
-        } catch (InvalidJwtException e) {
-            log.error("Unable to parse subject_token as JWT.", e);
-            throw new WebApplicationException(response(error(400, TokenErrorResponseType.INVALID_GRANT, "Invalid subject_token. Unable to parse subject_token as JWT."), auditLog));
-        } catch (WebApplicationException e) {
-            throw e;
-        } catch (Exception e) {
-            log.error("Unable to validate subject_token as id_token JWT.", e);
-            throw new WebApplicationException(response(error(400, TokenErrorResponseType.INVALID_GRANT, "Invalid subject_token. Unable to validate subject_token as id_token JWT."), auditLog));
-        }
-    }
-
     public boolean isTxTokenFlow(HttpServletRequest httpRequest) {
         return isTxTokenFlow(httpRequest.getParameter("requested_token_type"));
     }
 
-    public boolean isTxTokenFlow(String requestedTokenType) {
+    public static boolean isTxTokenFlow(String requestedTokenType) {
         final ExchangeTokenType exchangeTokenType = ExchangeTokenType.fromString(requestedTokenType);
         return exchangeTokenType == ExchangeTokenType.TX_TOKEN;
     }
 
     public Response.ResponseBuilder error(int status, TokenErrorResponseType type, String reason) {
         return Response.status(status).type(MediaType.APPLICATION_JSON_TYPE).entity(errorResponseFactory.errorAsJson(type, reason));
-    }
-
-    protected AuthorizationGrant getIdTokenGrant(String idTokenHint) {
-        if (org.apache.commons.lang.StringUtils.isBlank(idTokenHint)) {
-            return null;
-        }
-
-        AuthorizationGrant authorizationGrant = authorizationGrantList.getAuthorizationGrantByIdToken(TokenHashUtil.hash(idTokenHint));
-        if (authorizationGrant != null) {
-            return authorizationGrant;
-        }
-
-        authorizationGrant = authorizationGrantList.getAuthorizationGrantByIdToken(idTokenHint);
-        return authorizationGrant;
     }
 }
