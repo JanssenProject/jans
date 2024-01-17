57b638720f19a6fe558516f2a4c2bd47276e49ca
Devrim <devrimyatar@gluu.org>
2024-01-17 18:46:49 +0300

    feat(jans-cli-tui): inbound SAML (#7147)
    Signed-off-by: Mustafa Baser <mbaser@mail.com>
    Former-commit-id: c857e490efcb17f8854a2ae99c9e0e6d4cabdf2a

--- jans-cli-tui/cli_tui/plugins/100_saml/main.py
+++ jans-cli-tui/cli_tui/plugins/100_saml/main.py
@@ -21,6 +21,7 @@ from wui_components.jans_path_browser import jans_file_browser_dialog, BrowseTyp
 from wui_components.jans_label_container import JansLabelContainer
 
 from edit_tr_dialog import EditTRDialog
+from edit_identity_provider_dialog import EditIdentityProvideDialog
 from utils.multi_lang import _
 from utils.utils import DialogUtils
 from utils.static import cli_style
@@ -39,7 +40,7 @@ class Plugin(DialogUtils):
         """
         self.app = app
         self.pid = 'saml'
-        self.name = 'Jans Sa[m]l'
+        self.name = 'Jans SA[M]L'
         self.server_side_plugin = True
         self.page_entered = False
 
@@ -127,6 +128,30 @@ class Plugin(DialogUtils):
                         width=D()
                     )
 
+        self.provider_container = JansVerticalNav(
+                myparent=self.app,
+                headers=['inum', _("Display Name"), _("Status")],
+                preferred_size= self.app.get_column_sizes(.3, .5, .2),
+                data=[],
+                on_enter=self.edit_identity_provider,
+                on_display=self.app.data_display_dialog,
+                on_delete=self.delete_identity_provider,
+                selectes=0,
+                headerColor=cli_style.navbar_headcolor,
+                entriesColor=cli_style.navbar_entriescolor,
+                all_data=[],
+                hide_headers=False
+            )
+
+
+        add_provider_button_label = _("Add Identity Provider")
+        self.tabs['identity_providers'] = HSplit([
+                                self.provider_container,
+                                Window(height=1),
+                                VSplit([Button(add_provider_button_label, handler=self.edit_identity_provider, width=len(add_provider_button_label)+4)], align=HorizontalAlign.CENTER),
+                                ],
+                                width=D()
+                                )
 
         self.nav_selection_changed(list(self.tabs)[0])
 
@@ -193,6 +218,7 @@ class Plugin(DialogUtils):
                     entries=[
                             ('trust_relationships', '[T]rust Relationships'),
                             ('configuration', 'C[o]nfiguration'),
+                            ('identity_providers', '[I]dentity Providers'),
                             ],
                     selection_changed=self.nav_selection_changed,
                     select=0,
@@ -293,3 +319,14 @@ class Plugin(DialogUtils):
                 tobefocused=self.tr_container
                 )
 
+    def edit_identity_provider(self, **kwargs: Any) -> None:
+        """This is method for editing/creating identity provider 
+        """
+        edit_identity_provider_dialog = EditIdentityProvideDialog(self, data=kwargs.get('data', {}))
+        self.app.show_jans_dialog(edit_identity_provider_dialog)
+
+
+    def delete_identity_provider(self, **kwargs: Any) -> None:
+        """This is method for the deleting trust relationship 
+        """
+        pass
