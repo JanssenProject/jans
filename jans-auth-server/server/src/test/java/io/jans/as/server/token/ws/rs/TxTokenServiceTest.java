3e30ab7c9e3512d8844f2dfd4227be6c57e928c9
YuriyZ <yzabrovarniy@gmail.com>
2024-01-18 12:12:02 +0200

    feat(jans-auth-server): updated TxTokenServiceTest #6291
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>

--- jans-auth-server/server/src/test/java/io/jans/as/server/token/ws/rs/TxTokenServiceTest.java
+++ jans-auth-server/server/src/test/java/io/jans/as/server/token/ws/rs/TxTokenServiceTest.java
@@ -1,14 +1,13 @@
 package io.jans.as.server.token.ws.rs;
 
 import io.jans.as.model.common.ExchangeTokenType;
-import io.jans.as.model.common.SubjectTokenType;
+import io.jans.as.model.config.WebKeysConfiguration;
 import io.jans.as.model.configuration.AppConfiguration;
 import io.jans.as.model.crypto.AbstractCryptoProvider;
 import io.jans.as.model.error.ErrorResponseFactory;
 import io.jans.as.server.audit.ApplicationAuditLogger;
-import io.jans.as.server.model.audit.OAuth2AuditLog;
 import io.jans.as.server.model.common.AuthorizationGrantList;
-import jakarta.ws.rs.WebApplicationException;
+import io.jans.as.server.service.ClientService;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.testng.MockitoTestNGListener;
@@ -16,15 +15,14 @@ import org.slf4j.Logger;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
 /**
  * @author Yuriy Z
  */
 @Listeners(MockitoTestNGListener.class)
 public class TxTokenServiceTest {
-
-    public static final OAuth2AuditLog AUDIT_LOG = new OAuth2AuditLog("", null);
 
     @Mock
     private Logger log;
@@ -39,45 +37,30 @@ public class TxTokenServiceTest {
     private ErrorResponseFactory errorResponseFactory;
 
     @Mock
-    private AuthorizationGrantList authorizationGrantList;
+    private TxTokenValidator txTokenValidator;
 
     @Mock
     private AbstractCryptoProvider cryptoProvider;
+
+    @Mock
+    private ClientService clientService;
+
+    @Mock
+    private WebKeysConfiguration webKeysConfiguration;
+
+    @Mock
+    private AuthorizationGrantList authorizationGrantList;
 
     @InjectMocks
     private TxTokenService txTokenService;
 
     @Test
     public void isTxTokenFlow_ifRequestedTokenTypeIsNotTxToken_shouldReturnFalse() {
-        assertFalse(txTokenService.isTxTokenFlow("access_token"));
+        assertFalse(TxTokenService.isTxTokenFlow("access_token"));
     }
 
     @Test
     public void isTxTokenFlow_ifRequestedTokenTypeIsTxToken_shouldReturnTrue() {
-        assertTrue(txTokenService.isTxTokenFlow(ExchangeTokenType.TX_TOKEN.getName()));
-    }
-
-    @Test
-    public void validateRequestedTokenType_forValidValue_shouldNotRaiseException() {
-        txTokenService.validateRequestedTokenType("urn:ietf:params:oauth:token-type:txn_token", AUDIT_LOG);
-    }
-
-    @Test(expectedExceptions = WebApplicationException.class)
-    public void validateRequestedTokenType_forInvalidValue_shouldNotRaiseException() {
-        txTokenService.validateRequestedTokenType("dummy_token", AUDIT_LOG);
-    }
-
-    @Test
-    public void validateSubjectTokenType_forValidValue_shouldNotRaiseException() {
-        final SubjectTokenType validated1 = txTokenService.validateSubjectTokenType(SubjectTokenType.ID_TOKEN.getName(), AUDIT_LOG);
-        final SubjectTokenType validated2 = txTokenService.validateSubjectTokenType(SubjectTokenType.ACCESS_TOKEN.getName(), AUDIT_LOG);
-
-        assertEquals(validated1, SubjectTokenType.ID_TOKEN);
-        assertEquals(validated2, SubjectTokenType.ACCESS_TOKEN);
-    }
-
-    @Test(expectedExceptions = WebApplicationException.class)
-    public void validateSubjectTokenType_forInvalidValue_shouldNotRaiseException() {
-        txTokenService.validateSubjectTokenType("dummy_token", AUDIT_LOG);
+        assertTrue(TxTokenService.isTxTokenFlow(ExchangeTokenType.TX_TOKEN.getName()));
     }
 }
