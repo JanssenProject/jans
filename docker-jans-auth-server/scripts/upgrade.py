eeadf69bec5c23ecc760f1ce096449e29d5e2fe1
Isman Firmansyah <iromli@users.noreply.github.com>
2024-02-19 21:44:51 +0700

    chore(docker-jans): add prefix jans to stdout logs (#7762)
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    Co-authored-by: Mohammad Abudayyeh <47318409+moabu@users.noreply.github.com>

--- docker-jans-auth-server/scripts/upgrade.py
+++ docker-jans-auth-server/scripts/upgrade.py
@@ -18,7 +18,7 @@ from jans.pycloudlib.utils import as_boolean
 from settings import LOGGING_CONFIG
 
 logging.config.dictConfig(LOGGING_CONFIG)
-logger = logging.getLogger("auth")
+logger = logging.getLogger("jans-auth")
 
 Entry = namedtuple("Entry", ["id", "attrs"])
 
