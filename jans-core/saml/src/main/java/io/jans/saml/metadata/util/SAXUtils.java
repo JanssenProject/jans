2b32ff6d7d4b959bce2e933095c01efbbb7295e9
Rolain Djeumen <uprightech@gmail.com>
2024-02-27 18:23:11 +0100

    fix(keycloak-integration): saml metadata parsing #7777
    Signed-off-by: Rolain Djeumen <uprightech@gmail.com>

--- jans-core/saml/src/main/java/io/jans/saml/metadata/util/SAXUtils.java
+++ jans-core/saml/src/main/java/io/jans/saml/metadata/util/SAXUtils.java
@@ -16,35 +16,25 @@ import org.xml.sax.SAXException;
 
 public class SAXUtils  {
 
-    private static final Source [] SCHEMA_SOURCES = new Source [] {
+    private static final Source [] schemasources = new Source [] {
         new StreamSource(SAXUtils.class.getResourceAsStream("/schema/saml/saml-schema-metadata-2.0.xsd"))
     };
-
-    private static final SchemaFactory instance;
-
-    static {
-
-        try {
-            instance = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            instance.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA,"");
-            instance.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING,true);
-            instance.setResourceResolver(new LSResourceResolverImpl());
-        }catch(SAXException e) {
-            throw new RuntimeException("Could not properly initialize SAX",e);
-        }
-    }
 
 
     public static final SAXParser createParser() throws ParserConfigurationException, SAXException {
         SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setSchema(createSchema());
+        factory.setSchema(newSchemaFactory().newSchema(schemasources));
         factory.setNamespaceAware(true);
         return factory.newSAXParser();
     }
 
-    private static final Schema createSchema() throws SAXException {
-
-        return instance.newSchema(SCHEMA_SOURCES);
+    private static final SchemaFactory newSchemaFactory() throws SAXException {
+        
+        final SchemaFactory schemafactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        schemafactory.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA,"");
+        schemafactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING,true);
+        schemafactory.setResourceResolver(new LSResourceResolverImpl());
+        return schemafactory;
     }
 
     private SAXUtils() {
