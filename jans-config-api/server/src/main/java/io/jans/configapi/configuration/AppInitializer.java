bc0d4275a9aa4a4f2043cf703ccc8a7456a60bdd
pujavs <43700552+pujavs@users.noreply.github.com>
2024-03-12 17:09:02 +0530

    fix(config-api) code optimization and SMAL IDP delete scope rectification (#8022)
    * fix(config-api): user attribute validation and SAML TR update file name fix
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * fix(config-api): user mgmt added search filter for givenName, middleName, nickName and sn
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * fix(config-api): user mgmt validation for custom attributes
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * fix(config-api): sysnc with main
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * fix(config-api): sysnc with main
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): SAML TR removing chk for metadata file on update
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * fix(config-api): out of memory issue 8021
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * fix(config-api) saml IDP delete scope issue#8031
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    ---------
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    Signed-off-by: pujavs <43700552+pujavs@users.noreply.github.com>

--- jans-config-api/server/src/main/java/io/jans/configapi/configuration/AppInitializer.java
+++ jans-config-api/server/src/main/java/io/jans/configapi/configuration/AppInitializer.java
@@ -10,7 +10,6 @@ import io.jans.as.common.service.common.ApplicationFactory;
 import io.jans.configapi.security.api.ApiProtectionService;
 import io.jans.configapi.security.service.AuthorizationService;
 import io.jans.configapi.security.service.OpenIdAuthorizationService;
-import io.jans.configapi.service.status.StatusCheckerTimer;
 import io.jans.configapi.service.logger.LoggerService;
 import io.jans.exception.ConfigurationException;
 import io.jans.exception.OxIntializationException;
@@ -72,9 +71,6 @@ public class AppInitializer {
     private Instance<AuthorizationService> authorizationServiceInstance;
 
     @Inject
-    StatusCheckerTimer statusCheckerTimer;
-
-    @Inject
     private LoggerService loggerService;
 
     @Inject
@@ -111,9 +107,6 @@ public class AppInitializer {
 
         // Initialize custom Script
         initCustomScripts();
-
-        // Stats timer
-        statusCheckerTimer.initTimer();
 
         // Schedule timer tasks
         configurationFactory.initTimer();        
