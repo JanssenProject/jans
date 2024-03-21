027669c7ff0e248d219e5be5aba7ac81c469d066
pujavs <43700552+pujavs@users.noreply.github.com>
2024-03-21 14:28:03 +0530

    feat(config-api): asset mgt endpoint (#8096)
    * feat(config-api): asset endpoint
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset mgt endpoints wip
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset endpoint wip
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): setup issues fix
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): setup issues fix
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): sync sith main
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): sync sith main
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset mgt wip
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset endpoint wip
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset uplaod wip
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset mgt wipo
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset mgt wip
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset mgmt wip
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset mgt wip
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset revision update changes
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset endpoint spec modification
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asswt mgt wip
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset endpoint changes for revision
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset endpoint changes for revision
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset mgt wip
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): added get asset stream endpoint
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): added get asset stream endpoint
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset endpoint wip
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset endpoint wip
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset mgt endpoint
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * feat(config-api): asset mgt endpoint
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    ---------
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    Former-commit-id: 60310a02c67aafda44cfa7640db78e790808de4c

--- jans-config-api/server/src/main/java/io/jans/configapi/service/auth/ConfigurationService.java
+++ jans-config-api/server/src/main/java/io/jans/configapi/service/auth/ConfigurationService.java
@@ -13,17 +13,24 @@ import io.jans.config.GluuConfiguration;
 import io.jans.configapi.configuration.ConfigurationFactory;
 import io.jans.configapi.model.status.StatsData;
 import io.jans.orm.PersistenceEntryManager;
+import io.jans.service.document.store.conf.DocumentStoreConfiguration;
 import io.jans.util.StringHelper;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import jakarta.ws.rs.WebApplicationException;
+
+import org.slf4j.Logger;
 
 /**
  * @author Yuriy Zabrovarnyy
  */
 @ApplicationScoped
 public class ConfigurationService {
+    
+    @Inject
+    Logger logger;
 
     @Inject
     @Named(ApplicationFactory.PERSISTENCE_ENTRY_MANAGER_NAME)
@@ -60,6 +67,41 @@ public class ConfigurationService {
         }
         return persistenceManager.find(GluuConfiguration.class, configurationDn);
     }
+    
+    public DocumentStoreConfiguration getDocumentStoreConfiguration() {
+        GluuConfiguration gluuConfiguration = this.findGluuConfiguration();
+        logger.info("gluuConfiguration:{}",gluuConfiguration);
+        DocumentStoreConfiguration documentStoreConfiguration = null;
+        if(gluuConfiguration == null) {
+            throw new WebApplicationException("Cannot fetch DocumentStoreConfiguration as GluuConfiguration is null! ");
+        }
+        
+        documentStoreConfiguration = gluuConfiguration.getDocumentStoreConfiguration();
+        logger.info("Fetched documentStoreConfiguration:{}",documentStoreConfiguration);
+        return documentStoreConfiguration;
+    }
+    
+    public DocumentStoreConfiguration updateDocumentStoreConfiguration(DocumentStoreConfiguration documentStoreConfiguration) {
+        logger.info("documentStoreConfiguration:{}",documentStoreConfiguration);
+        if(documentStoreConfiguration == null) {
+            return documentStoreConfiguration;
+        }
+        
+        GluuConfiguration gluuConfiguration = findGluuConfiguration();
+        logger.info("gluuConfiguration:{}",gluuConfiguration);
+        
+        if(gluuConfiguration==null) {
+            throw new WebApplicationException("Cannot update DocumentStoreConfiguration as GluuConfiguration is null! ");
+        }
+        
+        gluuConfiguration.setDocumentStoreConfiguration(documentStoreConfiguration);
+        merge(gluuConfiguration);
+        
+        documentStoreConfiguration = gluuConfiguration.getDocumentStoreConfiguration();
+        logger.info("Updated documentStoreConfiguration:{}",documentStoreConfiguration);
+        return documentStoreConfiguration;
+    }
+    
 
     public String getPersistenceType() {
         return configurationFactory.getBaseConfiguration().getString("persistence.type");
