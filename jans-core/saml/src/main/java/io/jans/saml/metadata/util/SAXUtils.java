756349ad796e72db41dc50146ed36f9463f159ff
Rolain Djeumen <uprightech@gmail.com>
2024-03-25 04:51:35 +0100

    feat(jans-keycloak-integration): sync tr config between config-api and kc #8008 * modifed saxutils to optimize re-use of sax parsing objects
    Signed-off-by: Rolain Djeumen <uprightech@gmail.com>

--- jans-core/saml/src/main/java/io/jans/saml/metadata/util/SAXUtils.java
+++ jans-core/saml/src/main/java/io/jans/saml/metadata/util/SAXUtils.java
@@ -14,19 +14,36 @@ import javax.xml.transform.stream.StreamSource;
 
 import org.xml.sax.SAXException;
 
+import io.jans.saml.metadata.parser.ParserCreateError;
+
 public class SAXUtils  {
 
     private static final Source [] schemasources = new Source [] {
         new StreamSource(SAXUtils.class.getResourceAsStream("/META-INF/saml.schemas/saml-schema-metadata-2.0.xsd"))
     };
 
+    private static SAXParserFactory parserfactory = null;
+
+    private SAXUtils() {
+
+    }
+
+    public static void init() throws ParserConfigurationException, SAXException {
+        if(parserfactory == null) {
+            parserfactory = SAXParserFactory.newInstance();
+            parserfactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", false);
+            parserfactory.setSchema(newSchemaFactory().newSchema(schemasources))    ;
+            parserfactory.setNamespaceAware(true);
+        }
+    }
+
 
     public static final SAXParser createParser() throws ParserConfigurationException, SAXException {
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl",false);
-        factory.setSchema(newSchemaFactory().newSchema(schemasources));
-        factory.setNamespaceAware(true);
-        return factory.newSAXParser();
+        
+        if(parserfactory == null) {
+            throw new ParserCreateError("Please call SAXParser.init() first");
+        }
+        return parserfactory.newSAXParser();
     }
 
     private static final SchemaFactory newSchemaFactory() throws SAXException {
@@ -38,9 +55,5 @@ public class SAXUtils  {
         schemafactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING,true);
         schemafactory.setResourceResolver(new LSResourceResolverImpl());
         return schemafactory;
-    }
-
-    private SAXUtils() {
-
     }
 }
