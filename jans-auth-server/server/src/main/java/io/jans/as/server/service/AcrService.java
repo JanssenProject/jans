bef98e98fa51e5dc6942cc17244504e619e51f94
YuriyZ <yzabrovarniy@gmail.com>
2024-04-16 10:25:48 +0300

    feat(jans-auth-server): added acr mappings support #8216
    https://github.com/JanssenProject/jans/issues/8216
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>

--- jans-auth-server/server/src/main/java/io/jans/as/server/service/AcrService.java
+++ jans-auth-server/server/src/main/java/io/jans/as/server/service/AcrService.java
@@ -5,6 +5,7 @@ import io.jans.as.common.model.session.SessionId;
 import io.jans.as.common.model.session.SessionIdState;
 import io.jans.as.model.authorize.AuthorizeErrorResponseType;
 import io.jans.as.model.common.Prompt;
+import io.jans.as.model.configuration.AppConfiguration;
 import io.jans.as.model.util.Util;
 import io.jans.as.server.authorize.ws.rs.AuthzRequest;
 import io.jans.as.server.model.exception.AcrChangedException;
@@ -17,7 +18,13 @@ import io.jans.model.custom.script.conf.CustomScriptConfiguration;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import org.apache.commons.lang.StringUtils;
+import org.python.google.common.collect.Lists;
 import org.slf4j.Logger;
+
+import java.util.List;
+import java.util.Map;
+
+import static io.jans.as.model.util.StringUtils.implode;
 
 /**
  * @author Yuriy Z
@@ -37,15 +44,72 @@ public class AcrService {
     @Inject
     private ExternalAuthenticationService externalAuthenticationService;
 
+    @Inject
+    private AppConfiguration appConfiguration;
+
+    public static boolean isAgama(String acr) {
+        return StringUtils.isNotBlank(acr) &&  acr.startsWith("agama_");
+    }
+
     public void validateAcrs(AuthzRequest authzRequest, Client client) throws AcrChangedException {
-        if (!client.getAttributes().getAuthorizedAcrValues().isEmpty() &&
-                !client.getAttributes().getAuthorizedAcrValues().containsAll(authzRequest.getAcrValuesList())) {
-            throw authzRequest.getRedirectUriResponse().createWebException(AuthorizeErrorResponseType.INVALID_REQUEST,
-                    "Restricted acr value request, please review the list of authorized acr values for this client");
-        }
+        applyAcrMappings(authzRequest);
+
+        checkClientAuthorizedAcrs(authzRequest, client);
 
         checkAcrScriptIsAvailable(authzRequest);
         checkAcrChanged(authzRequest, identity.getSessionId()); // check after redirect uri is validated
+    }
+
+    public void checkClientAuthorizedAcrs(AuthzRequest authzRequest, Client client) {
+        final List<String> authorizedAcrs = client.getAttributes().getAuthorizedAcrValues();
+        if (authorizedAcrs.isEmpty()) {
+            return;
+        }
+
+        final String mappedAuthorizedAcrs = applyAcrMappings(authorizedAcrs);
+
+        for (String acr : authzRequest.getAcrValuesList()) {
+            // check both actual acr and mapped (we should allow mapped values as well)
+            if (!authorizedAcrs.contains(acr) && !mappedAuthorizedAcrs.contains(acr)) {
+                throw authzRequest.getRedirectUriResponse().createWebException(AuthorizeErrorResponseType.INVALID_REQUEST,
+                        "Restricted acr value request, please review the list of authorized acr values for this client");
+            }
+        }
+    }
+
+    public void applyAcrMappings(AuthzRequest authzRequest) {
+        final List<String> acrValues = authzRequest.getAcrValuesList();
+        final String result = applyAcrMappings(acrValues);
+        authzRequest.setAcrValues(result);
+    }
+
+    public String applyAcrMappings(List<String> acrValues) {
+        final Map<String, String> mappings = appConfiguration.getAcrMappings();
+        if (acrValues == null || acrValues.isEmpty()) {
+            return "";
+        }
+
+        if (mappings == null || mappings.isEmpty()) {
+            return implode(acrValues, " ");
+        }
+
+        boolean updated = false;
+        for (int i = 0; i < acrValues.size(); i++) {
+            final String acr = acrValues.get(i);
+            final String value = mappings.get(acr);
+            if (StringUtils.isNotBlank(value)) {
+                log.debug("Replaced acr {} with {}, defined from acrMapping.", acr, value);
+                acrValues.set(i, value);
+                updated = true;
+            }
+        }
+
+        final String result = implode(acrValues, " ");
+        if (updated) {
+            log.debug("Mapped result: {}", result);
+        }
+
+        return result;
     }
 
     public void checkAcrScriptIsAvailable(AuthzRequest authzRequest) {
@@ -58,13 +122,26 @@ public class AcrService {
             return; // no need for script for built-in "simple_password_auth"
         }
 
-        CustomScriptConfiguration script = externalAuthenticationService.determineCustomScriptConfiguration(AuthenticationScriptUsageType.INTERACTIVE, authzRequest.getAcrValuesList());
+        List<String> acrsToDetermineScript = getAcrsToDetermineScript(authzRequest.getAcrValuesList());
+        CustomScriptConfiguration script = externalAuthenticationService.determineCustomScriptConfiguration(AuthenticationScriptUsageType.INTERACTIVE, acrsToDetermineScript);
         if (script == null) {
             String msg = String.format("Unable to find script for acr: %s. Send error: %s",
-                    acrValues, AuthorizeErrorResponseType.UNMET_AUTHENTICATION_REQUIREMENTS.getParameter());
+                    acrsToDetermineScript, AuthorizeErrorResponseType.UNMET_AUTHENTICATION_REQUIREMENTS.getParameter());
             log.debug(msg);
             throw authzRequest.getRedirectUriResponse().createWebException(AuthorizeErrorResponseType.UNMET_AUTHENTICATION_REQUIREMENTS, msg);
         }
+    }
+
+    public static List<String> getAcrsToDetermineScript(List<String> acrValues) {
+        if (acrValues == null || acrValues.isEmpty()) {
+            return Lists.newArrayList();
+        }
+
+        if (isAgama(acrValues.get(0))) {
+            return Lists.newArrayList("agama");
+        }
+
+        return acrValues;
     }
 
     private void checkAcrChanged(AuthzRequest authzRequest, SessionId sessionUser) throws AcrChangedException {
