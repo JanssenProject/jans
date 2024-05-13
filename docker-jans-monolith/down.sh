6a5e55818df0c51ba8f2ff368b573a85b6fdc5a5
Markus Knecht <markus.knecht@fhnw.ch>
2024-05-13 15:38:09 +0200

    Improved Monolith behavior during restarts (see #8414)  - restart (or stop then start): CMD in Dockerfile will now clean the jetty temp directory to prevent disk size grow  - down then up: added scripts (down.sh and up.sh) that create a post installation image on a down and use it on a later up such that only first up runs the installation script  - clean: added clean.sh script to remove all docker artifacts (allows for a fresh start)  - Guidelines: Updated README.md and Guiding echoes in startjanssenmonolithdemo.sh  - host mounted log files: The log files are now mounted on the host to improve access to them and being capable to delete them easily

---
+++ docker-jans-monolith/down.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "The used db was not specified as argument, will use mysql as default"
+    yaml="jans-mysql-compose.yml"
+else
+	case "$1" in
+		mysql|ldap|postgres)
+			yaml="jans-${1}-compose.yml"
+			;;
+		*)
+			yaml="${1}"
+			;;
+	esac
+fi
+
+# Get the directory of the script
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+DOCKER_PROJECT=$(basename "$SCRIPT_DIR")
+
+if [ -z "$INSTALLED_JANNSEN_NAME" ]; then
+    INSTALLED_JANNSEN_NAME="after-install-jans"
+fi
+
+if [ -z "$JANSSEN_VERSION" ]; then
+    JANSSEN_VERSION="1.1.2_dev"
+fi
+
+if [ -z "$DATABASE_VOLUME_NAME" ]; then
+    DATABASE_VOLUME_NAME="db-data"
+fi
+
+if [ -z "$JANSSEN_SERVICE_NAME" ]; then
+    JANSSEN_SERVICE_NAME="jans"
+fi
+
+cd $SCRIPT_DIR
+
+JANSSEN_IMAGE="${DOCKER_PROJECT}_${INSTALLED_JANNSEN_NAME}:${JANSSEN_VERSION}"
+JANSSEN_CONTAINER="${DOCKER_PROJECT}-${JANSSEN_SERVICE_NAME}-1"
+
+if ! docker image inspect ${JANSSEN_IMAGE} &> /dev/null; then
+	if docker exec "${JANSSEN_CONTAINER}" sh -c '[ -e /janssen/deployed ]'; then
+		echo "installation of janssen was sucessfull - an after install image will be created (this can take a while)"
+		docker stop ${JANSSEN_CONTAINER} &> /dev/null;
+		docker commit ${JANSSEN_CONTAINER} ${JANSSEN_IMAGE}
+	fi
+fi
+docker compose -f ${yaml} down
+docker rm -f ${yaml} down
+
+
+
+
+
+
+
