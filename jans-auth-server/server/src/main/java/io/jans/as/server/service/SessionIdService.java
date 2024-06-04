1d317af40ed0db2bc3c07684d812498f389d2d5c
YuriyZ <yzabrovarniy@gmail.com>
2024-06-04 19:57:36 +0300

    fix(jans-auth-server): do not recognize different agama flows as different acr #8652 (#8654)
    * fix(jans-auth-server): do not recognized different agama flows as different acr #8652
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * fix(jans-auth-server): do not recognize different agama flows as different acr #8652
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    ---------
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    Former-commit-id: b5e73451d3282516c87bdff02acc4b6316f7c7dc

--- jans-auth-server/server/src/main/java/io/jans/as/server/service/SessionIdService.java
+++ jans-auth-server/server/src/main/java/io/jans/as/server/service/SessionIdService.java
@@ -169,6 +169,10 @@ public class SessionIdService {
         return acr;
     }
 
+    public static boolean isAgamaInSessionAndRequest(String sessionAcr, List<String> acrValuesList) {
+        return isAgama(sessionAcr) && !acrValuesList.isEmpty() && isAgama(acrValuesList.iterator().next());
+    }
+
     // #34 - update session attributes with each request
     // 1) redirect_uri change -> update session
     // 2) acr change -> throw acr change exception
@@ -190,8 +194,7 @@ public class SessionIdService {
             }
 
             List<String> acrValuesList = acrValuesList(acrValuesStr);
-            boolean isAgama = isAgama(sessionAcr) && !acrValuesList.isEmpty() && isAgama(acrValuesList.iterator().next());
-            boolean isAcrChanged = !acrValuesList.isEmpty() && !acrValuesList.contains(sessionAcr) && !isAgama;
+            boolean isAcrChanged = !acrValuesList.isEmpty() && !acrValuesList.contains(sessionAcr) && !isAgamaInSessionAndRequest(sessionAcr, acrValuesList);
             if (isAcrChanged) {
                 Map<String, Integer> acrToLevel = externalAuthenticationService.acrToLevelMapping();
                 Integer sessionAcrLevel = Util.asInt(acrToLevel.get(externalAuthenticationService.scriptName(sessionAcr)), -1);
@@ -916,9 +919,16 @@ public class SessionIdService {
 
         HashSet<String> resultAcrs = new HashSet<>();
         for (String acr : acrs) {
-            resultAcrs.add(externalAuthenticationService.scriptName(acr));
+            String acrForScript = isAgama(acr) ? AcrService.AGAMA : acr;
+            final String scriptName = externalAuthenticationService.scriptName(acrForScript);
+            if (StringUtils.isNotBlank(scriptName)) {
+                resultAcrs.add(acr);
+            }
         }
 
+        if (log.isTraceEnabled()) {
+            log.trace("acrValuesList {}", resultAcrs);
+        }
         return new ArrayList<>(resultAcrs);
     }
 
