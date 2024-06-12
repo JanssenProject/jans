453b992b046d4809205b56199989c9379847cf92
Jose Gonzalez <bonustrack310@gmail.com>
2024-06-12 09:50:42 -0500

    fix: reduce Freemarker's "incompatible improvements version" (#8702)
    fix: reduce Freemarker's "incompatible improvements version" #8696
    
    Signed-off-by: jgomer2001 <bonustrack310@gmail.com>

--- agama/transpiler/src/main/java/io/jans/agama/dsl/Transpiler.java
+++ agama/transpiler/src/main/java/io/jans/agama/dsl/Transpiler.java
@@ -79,7 +79,9 @@ public class Transpiler {
             throw new RuntimeException("Unable to read utility script", e);
         }
 
-        FM_CONFIG = new Configuration(Configuration.VERSION_2_3_33);
+        //Using a value higher than 2.3.32 for "incompatible improvements version" gives trouble.
+        //Raising this value requires troubleshooting of this lib (agama-transpiler)
+        FM_CONFIG = new Configuration(Configuration.VERSION_2_3_32);
         FM_CONFIG.setClassLoaderForTemplateLoading(CLS_LOADER, "/");
         FM_CONFIG.setDefaultEncoding(UTF_8.toString());
         FM_CONFIG.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
