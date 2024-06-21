76ee3fb92028748f4a22dd3809aa32f30191ba7e
sokorototo <nyachiengatoto@gmail.com>
2024-06-21 21:10:10 +0300

    chore(jans-lock): cleaned up commit
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>

--- jans-lock/cedarling/src/cedar.rs
+++ jans-lock/cedarling/src/cedar.rs
@@ -4,28 +4,28 @@ use crate::types;
 
 // SAFETY: Webassembly is single-threaded, so this is safe. The statics are also scoped
 pub(crate) fn policies(swap: Option<PolicySet>) -> &'static PolicySet {
-	static mut ENTITIES: Option<PolicySet> = None;
+	static mut POLICIES: Option<PolicySet> = None;
 
 	unsafe {
-		swap.map(|s| ENTITIES = Some(s));
-		ENTITIES.as_ref().unwrap()
+		swap.map(|s| POLICIES = Some(s));
+		POLICIES.as_ref().unwrap()
 	}
 }
 
 pub(crate) fn trusted_issuers(swap: Option<Vec<types::TrustedIssuer>>) -> &'static [types::TrustedIssuer] {
-	static mut ENTITIES: Vec<types::TrustedIssuer> = Vec::new();
+	static mut ISSUERS: Vec<types::TrustedIssuer> = Vec::new();
 
 	unsafe {
-		swap.map(|s| ENTITIES = s);
-		ENTITIES.as_slice()
+		swap.map(|s| ISSUERS = s);
+		ISSUERS.as_slice()
 	}
 }
 
 pub(crate) fn schema(swap: Option<Schema>) -> Option<&'static Schema> {
-	static mut ENTITIES: Option<Schema> = None;
+	static mut SCHEMA: Option<Schema> = None;
 
 	unsafe {
-		swap.map(|s| ENTITIES = Some(s));
-		ENTITIES.as_ref()
+		swap.map(|s| SCHEMA = Some(s));
+		SCHEMA.as_ref()
 	}
 }
