eb6366133dcc60bdd4d6febd1780404faff6e406
Mustafa Baser <mbaser@mail.com>
2024-07-20 23:02:52 +0300

    fix(jans-cli-tui): check response content type in cli mode
    Signed-off-by: Mustafa Baser <mbaser@mail.com>

--- jans-cli-tui/cli_tui/plugins/100_saml/edit_tr_dialog.py
+++ jans-cli-tui/cli_tui/plugins/100_saml/edit_tr_dialog.py
@@ -455,13 +455,15 @@ class EditTRDialog(JansGDialog, DialogUtils):
 
         data = {'trustRelationship': new_data}
         if sp_meta_data_source_type == 'file':
-            if not self.metadata_file_path:
+            if self.new_tr and not self.metadata_file_path:
                 self.app.show_message(_(common_strings.error), _("Please browse a metadata source file"), tobefocused=self.edit_tr_container)
+                self.app.stop_progressing(_("Failed to save Trust Relationship."))
                 return
 
-                self.app.stop_progressing(_("Failed to save Trust Relationship."))
-
-            data['metaDataFile'] = self.metadata_file_path
+            if self.metadata_file_path:
+                data['metaDataFile'] = self.metadata_file_path
+            elif 'spMetaDataFN' in self.data:
+                data['trustRelationship']['spMetaDataFN'] = self.data['spMetaDataFN']
 
 
         async def coroutine():
