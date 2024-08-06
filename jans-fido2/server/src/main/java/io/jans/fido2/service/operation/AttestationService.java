762b34cd2562393416b49e149ada8eb2e3b1b01f
Arnab Dutta <arnab.bdutta@gmail.com>
2024-08-06 10:32:46 +0530

    feat: add support for BS / BE flags in AuthData #8903 (#8968)
    * feat: add support for BS / BE flags in AuthData #8903
    
    Signed-off-by: Arnab Dutta <arnab.bdutta@gmail.com>
    
    * feat: adding comments
    
    Signed-off-by: Arnab Dutta <arnab.bdutta@gmail.com>
    
    * feat: adding comments
    
    Signed-off-by: Arnab Dutta <arnab.bdutta@gmail.com>
    
    ---------
    
    Signed-off-by: Arnab Dutta <arnab.bdutta@gmail.com>
    Co-authored-by: Ackermann Yuriy <1636116+yackermann@users.noreply.github.com>

--- jans-fido2/server/src/main/java/io/jans/fido2/service/operation/AttestationService.java
+++ jans-fido2/server/src/main/java/io/jans/fido2/service/operation/AttestationService.java
@@ -266,6 +266,9 @@ public class AttestationService {
 		registrationData.setType(PublicKeyCredentialType.PUBLIC_KEY.getKeyName());
 		registrationData.setAttestationType(attestationData.getAttestationType());
 
+		registrationData.setBackupEligibilityFlag(attestationData.getBackupEligibilityFlag());
+		registrationData.setBackupStateFlag(attestationData.getBackupStateFlag());
+
         // Support cancel request
         if (cancelRequest) {
         	registrationData.setStatus(Fido2RegistrationStatus.canceled);
