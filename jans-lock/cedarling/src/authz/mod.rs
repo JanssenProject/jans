b09531a891d2346b71cdf291319a6b11c8802e80
Newton Toto <53893043+sokorototo@users.noreply.github.com>
2024-08-13 18:10:31 +0300

    feat(jans-lock): `cedarling` Progress Update (#9045)
    * feat(jans-lock): authz implementation
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Slim down TRUST_STORE interface
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Complete authz implementation
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * chore(jans-lock): code cleanup
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Fixed http methods on Web, fixed remote startup strategy
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Moved to singular entity design
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Create Resource entity directly from JS input
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Enable setting default entities from PolicyStore
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Fixed TRUST_STORE refresh on Node.js
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Parse Schema, PolicySet as base64 strings. Enable PolicyId to be loaded from PolicyStore
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Code cleanup using cargo-clippy
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Parse trustedIssuers as object rather than array
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Some OAuth servers might not support DCR
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Flushed out get_token_entity() methods
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Implemented boolean statement parser
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Initial boolean DSL impl
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Support disabling JWT valdiation
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Removed old, unused environment tests
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Better user ergonimics and error reporting
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Better testing framework
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): ...
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    * feat(jans-lock): Added example Policy Store for remote and local startup strategy
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    
    ---------
    
    Signed-off-by: sokorototo <nyachiengatoto@gmail.com>
    Signed-off-by: Safin Wasi <6601566+SafinWasi@users.noreply.github.com>
    Co-authored-by: Safin Wasi <6601566+SafinWasi@users.noreply.github.com>

--- jans-lock/cedarling/src/authz/mod.rs
+++ jans-lock/cedarling/src/authz/mod.rs
@@ -1,15 +1,58 @@
-use serde_wasm_bindgen::from_value;
+use std::sync::OnceLock;
+
+use serde_wasm_bindgen::{from_value, to_value};
 use wasm_bindgen::prelude::*;
 
-use crate::startup;
+use crate::*;
 
-mod token2entity;
-mod types;
+pub mod statement;
+pub mod token2entity;
+pub mod types;
+
+pub static APPLICATION_NAME: OnceLock<String> = OnceLock::new();
+pub static REQUIRE_AUD_VALIDATION: OnceLock<bool> = OnceLock::new();
+
+pub fn init(config: &startup::types::CedarlingConfig) {
+	APPLICATION_NAME.set(config.application_name.clone()).unwrap_throw();
+	REQUIRE_AUD_VALIDATION.set(config.require_aud_validation).unwrap_throw();
+}
 
 #[wasm_bindgen]
 pub async fn authz(req: JsValue) -> JsValue {
-	let _request = from_value::<types::AuthzInput>(req).unwrap();
-	JsValue::NULL
-}
+	let input = from_value::<types::AuthzInput>(req).unwrap_throw();
 
-pub(crate) fn init(_: &startup::types::CedarlingConfig) {}
+	// generate extra parameters for cedar decision
+	let (uids, mut entities) = token2entity::token2entities(&input);
+
+	// append resource entity
+	let r = serde_json::json!([input.resource]);
+	entities = entities.add_entities_from_json_value(r, startup::SCHEMA.get()).unwrap_throw();
+
+	// prepare request
+	let action = input.action.parse().expect_throw("Unable to parse action");
+	let resource = {
+		#[derive(serde::Deserialize)]
+		struct Extract {
+			#[serde(rename = "type")]
+			pub _type: String,
+			pub id: String,
+		}
+
+		let uid = input.resource.get("uid").cloned().expect_throw("Can't find uid in Resource entity definition");
+		let uid: Extract = serde_json::from_value(uid).expect_throw("Resource uid is not a valid cedar uid");
+		let id = serde_json::json!({ "__entity": { "type": uid._type, "id": uid.id } });
+
+		cedar_policy::EntityUid::from_json(id).unwrap_throw()
+	};
+	let context = cedar_policy::Context::from_json_value(input.context, None).expect_throw("Unable to generate context Object");
+
+	// generate statement
+	let statement = statement::parse(input.statement.as_deref().unwrap_or("User"));
+	let input = (action, resource, context);
+
+	// evaluate query
+	let mut ctx = Default::default();
+	let answer = statement::evaluate(statement, &uids, &entities, &input, &mut ctx);
+
+	to_value(&serde_json::json!({ "decision": answer, "policies": ctx.policies })).unwrap_throw()
+}
