e07c5b1fc101dcb9bd0d29f19b1695d1d14df805
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2024-08-26 12:39:43 +0300

    feat(jans-core): document store manager (#9256)
    * feat(jans-core): add pull mechanism for custom assets pushed to the DB
    
    * feat(jans-core): add pull mechanism for custom assets pushed to the DB
    
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    * feat(jans-core): add pull mechanism for custom assets pushed to the DB
    
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    ---------
    
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    Former-commit-id: bb4dcc297154930aa8c1cae6dbb0a7bf4ab87db8

--- jans-core/document-store/src/main/java/io/jans/service/document/store/provider/WebDavDocumentStoreProvider.java
+++ jans-core/document-store/src/main/java/io/jans/service/document/store/provider/WebDavDocumentStoreProvider.java
@@ -10,22 +10,22 @@ import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.jans.service.document.store.conf.DocumentStoreConfiguration;
+import io.jans.service.document.store.conf.DocumentStoreType;
+import io.jans.service.document.store.conf.WebDavDocumentStoreConfiguration;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
-import io.jans.service.document.store.conf.DocumentStoreConfiguration;
-import io.jans.service.document.store.conf.WebDavDocumentStoreConfiguration;
-import io.jans.service.document.store.conf.DocumentStoreType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * @author Yuriy Movchan on 04/10/2020
  */
 @ApplicationScoped
-public class WebDavDocumentStoreProvider extends DocumentStoreProvider<WebDavDocumentStoreProvider> {
+public class WebDavDocumentStoreProvider extends DocumentStoreProvider<String> {
 
     @Inject
     private Logger log;
@@ -117,6 +117,11 @@ public class WebDavDocumentStoreProvider extends DocumentStoreProvider<WebDavDoc
 	public boolean removeDocument(String path) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public List<String> findDocumentsByModules(List<String> moduleList, String ... attributes) {
+        throw new RuntimeException("Not yet implemented");
 	}
 
 }
