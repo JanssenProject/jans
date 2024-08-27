fc9f04fe3b3d3d168f3090214b22ee80be371d92
Isman Firmansyah <iromli@users.noreply.github.com>
2024-08-28 00:06:35 +0700

    feat(jans-pycloudlib): add support reading configuration from file (#9037)
    * feat(jans-pycloudlib): add support reading configuration from file
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * tests(jans-pycloudlib): adjust testcases
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * feat(jans-pycloudlib): add CLI command to generate configuration spec
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * refactor: simplified configmaps and secrets adapters
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * fix: handle missing params
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * fix: add backward-compat for configuration and dump files
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * test(jans-pycloudlib): fix transform_data testcase
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * refactor: handle mounted password files
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * refactor: handle mounted files in OCI images
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * fix: ensure couchbase password files are exist
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * refactor: generate opendj.pkcs12 on-the-fly
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * fix: resolve required password files
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * fix: pre-populate LDAP bindDN
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * fix: remove duplicated jansAccessTknSigAlg attribute
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * chore: clarify local secrets and configmaps will be excluded if configuration.json is missing
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * refactor: remove unused backward-compat
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    * refactor: bootstrap Vault RoleID and SecretID (if required)
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    
    ---------
    
    Signed-off-by: iromli <isman.firmansyah@gmail.com>
    Signed-off-by: Isman Firmansyah <iromli@users.noreply.github.com>

--- jans-pycloudlib/jans/pycloudlib/secret/__init__.py
+++ jans-pycloudlib/jans/pycloudlib/secret/__init__.py
@@ -3,6 +3,7 @@ from jans.pycloudlib.secret.kubernetes_secret import KubernetesSecret  # noqa: F
 from jans.pycloudlib.secret.vault_secret import VaultSecret  # noqa: F401
 from jans.pycloudlib.secret.google_secret import GoogleSecret  # noqa: F401
 from jans.pycloudlib.secret.aws_secret import AwsSecret  # noqa: F401
+from jans.pycloudlib.secret.file_secret import FileSecret  # noqa: F401
 
 # avoid implicit reexport disabled error
 __all__ = [
@@ -10,4 +11,5 @@ __all__ = [
     "VaultSecret",
     "GoogleSecret",
     "AwsSecret",
+    "FileSecret",
 ]
