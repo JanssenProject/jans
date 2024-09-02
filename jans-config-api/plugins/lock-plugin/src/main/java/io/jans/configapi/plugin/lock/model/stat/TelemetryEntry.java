c5cb4bbdee85e83ea33448ef0010f4376d8db124
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2024-09-02 22:47:55 +0300

    feat(jans-config-api): add missing properties to log/audit/telemetry records (#9334)
    * feat(jans-config-api): update log/telemetry/health entries
    
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    * feat(jans-config-api): update log/telemetry/health entries
    
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>
    
    ---------
    
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>

--- jans-config-api/plugins/lock-plugin/src/main/java/io/jans/configapi/plugin/lock/model/stat/TelemetryEntry.java
+++ jans-config-api/plugins/lock-plugin/src/main/java/io/jans/configapi/plugin/lock/model/stat/TelemetryEntry.java
@@ -1,43 +1,49 @@
 package io.jans.configapi.plugin.lock.model.stat;
 
-import io.jans.orm.annotation.AttributeName;
-import io.jans.orm.annotation.DN;
-import io.jans.orm.annotation.DataEntry;
-import io.jans.orm.annotation.JsonObject;
-import io.jans.orm.annotation.ObjectClass;
-
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.jans.orm.annotation.AttributeName;
+import io.jans.orm.annotation.DataEntry;
+import io.jans.orm.annotation.JsonObject;
+import io.jans.orm.annotation.ObjectClass;
+import io.jans.orm.model.base.BaseEntry;
+
 @DataEntry
 @ObjectClass(value = "jansTelemetryEntry")
-public class TelemetryEntry implements Serializable {
+public class TelemetryEntry extends BaseEntry implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
-    @DN
-    private String dn;
+	private static final long serialVersionUID = 3237727784024903177L;
 
     @JsonProperty("inum")
     @AttributeName(name = "inum", ignoreDuringUpdate = true)
     private String inum;
 
-    @AttributeName(name = "jansLastUpd")
-    private Date lastPolicyLoadTime;
+    @AttributeName(name = "creationDate")
+    private Date creationDate;
 
-    @AttributeName(name = "size")
-    private Integer lastPolicyLoadSize;
+    @AttributeName(name = "eventTime")
+    private Date eventTime;
+
+    @AttributeName(name = "jansService")
+    private String service;
+
+    @AttributeName(name = "jansNodeId")
+    private String nodeId;
 
     @AttributeName(name = "jansStatus")
     private String status;
 
-    @AttributeName(name = "jansCounter")
+    @AttributeName(name = "jansDownloadSize")
+    private Integer lastPolicyLoadSize;
+
+    @AttributeName(name = "jansSuccessLoadCounter")
     private long policySuccessLoadCounter;
 
-    @AttributeName(name = "jansFailCounter")
+    @AttributeName(name = "jansFaiedlLoadCounter")
     private long policyFailedLoadCounter;
 
     @AttributeName(name = "evaluationTimeNs")
@@ -52,114 +58,131 @@ public class TelemetryEntry implements Serializable {
     @AttributeName(name = "requestCounter")
     private long evaluationRequestsCount;
 
-    @AttributeName(name = "policyStats")
     @JsonObject
+    @AttributeName(name = "policyStats")
     private Map<String, String> policyStats;
 
-    public String getDn() {
-        return dn;
-    }
-
-    public void setDn(String dn) {
-        this.dn = dn;
-    }
-
     public String getInum() {
-        return inum;
-    }
+		return inum;
+	}
 
-    public void setInum(String inum) {
-        this.inum = inum;
-    }
+	public void setInum(String inum) {
+		this.inum = inum;
+	}
 
-    public Date getLastPolicyLoadTime() {
-        return lastPolicyLoadTime;
-    }
+	public Date getCreationDate() {
+		return creationDate;
+	}
 
-    public void setLastPolicyLoadTime(Date lastPolicyLoadTime) {
-        this.lastPolicyLoadTime = lastPolicyLoadTime;
-    }
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
 
-    public Integer getLastPolicyLoadSize() {
-        return lastPolicyLoadSize;
-    }
+	public Date getEventTime() {
+		return eventTime;
+	}
 
-    public void setLastPolicyLoadSize(Integer lastPolicyLoadSize) {
-        this.lastPolicyLoadSize = lastPolicyLoadSize;
-    }
+	public void setEventTime(Date eventTime) {
+		this.eventTime = eventTime;
+	}
 
-    public String getStatus() {
-        return status;
-    }
+	public String getService() {
+		return service;
+	}
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+	public void setService(String service) {
+		this.service = service;
+	}
 
-    public long getPolicySuccessLoadCounter() {
-        return policySuccessLoadCounter;
-    }
+	public String getNodeId() {
+		return nodeId;
+	}
 
-    public void setPolicySuccessLoadCounter(long policySuccessLoadCounter) {
-        this.policySuccessLoadCounter = policySuccessLoadCounter;
-    }
+	public void setNodeId(String nodeId) {
+		this.nodeId = nodeId;
+	}
 
-    public long getPolicyFailedLoadCounter() {
-        return policyFailedLoadCounter;
-    }
+	public String getStatus() {
+		return status;
+	}
 
-    public void setPolicyFailedLoadCounter(long policyFailedLoadCounter) {
-        this.policyFailedLoadCounter = policyFailedLoadCounter;
-    }
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
-    public Date getLastPolicyEvaluationTimeNs() {
-        return lastPolicyEvaluationTimeNs;
-    }
+	public Integer getLastPolicyLoadSize() {
+		return lastPolicyLoadSize;
+	}
 
-    public void setLastPolicyEvaluationTimeNs(Date lastPolicyEvaluationTimeNs) {
-        this.lastPolicyEvaluationTimeNs = lastPolicyEvaluationTimeNs;
-    }
+	public void setLastPolicyLoadSize(Integer lastPolicyLoadSize) {
+		this.lastPolicyLoadSize = lastPolicyLoadSize;
+	}
 
-    public Date getAvgPolicyEvaluationTimeNs() {
-        return avgPolicyEvaluationTimeNs;
-    }
+	public long getPolicySuccessLoadCounter() {
+		return policySuccessLoadCounter;
+	}
 
-    public void setAvgPolicyEvaluationTimeNs(Date avgPolicyEvaluationTimeNs) {
-        this.avgPolicyEvaluationTimeNs = avgPolicyEvaluationTimeNs;
-    }
+	public void setPolicySuccessLoadCounter(long policySuccessLoadCounter) {
+		this.policySuccessLoadCounter = policySuccessLoadCounter;
+	}
 
-    public String getMemoryUsage() {
-        return memoryUsage;
-    }
+	public long getPolicyFailedLoadCounter() {
+		return policyFailedLoadCounter;
+	}
 
-    public void setMemoryUsage(String memoryUsage) {
-        this.memoryUsage = memoryUsage;
-    }
+	public void setPolicyFailedLoadCounter(long policyFailedLoadCounter) {
+		this.policyFailedLoadCounter = policyFailedLoadCounter;
+	}
 
-    public long getEvaluationRequestsCount() {
-        return evaluationRequestsCount;
-    }
+	public Date getLastPolicyEvaluationTimeNs() {
+		return lastPolicyEvaluationTimeNs;
+	}
 
-    public void setEvaluationRequestsCount(long evaluationRequestsCount) {
-        this.evaluationRequestsCount = evaluationRequestsCount;
-    }
+	public void setLastPolicyEvaluationTimeNs(Date lastPolicyEvaluationTimeNs) {
+		this.lastPolicyEvaluationTimeNs = lastPolicyEvaluationTimeNs;
+	}
 
-    public Map<String, String> getPolicyStats() {
-        return policyStats;
-    }
+	public Date getAvgPolicyEvaluationTimeNs() {
+		return avgPolicyEvaluationTimeNs;
+	}
 
-    public void setPolicyStats(Map<String, String> policyStats) {
-        this.policyStats = policyStats;
-    }
+	public void setAvgPolicyEvaluationTimeNs(Date avgPolicyEvaluationTimeNs) {
+		this.avgPolicyEvaluationTimeNs = avgPolicyEvaluationTimeNs;
+	}
 
-    @Override
-    public String toString() {
-        return "TelemetryEntry [dn=" + dn + ", inum=" + inum + ", lastPolicyLoadTime=" + lastPolicyLoadTime
-                + ", lastPolicyLoadSize=" + lastPolicyLoadSize + ", status=" + status + ", policySuccessLoadCounter="
-                + policySuccessLoadCounter + ", policyFailedLoadCounter=" + policyFailedLoadCounter
-                + ", lastPolicyEvaluationTimeNs=" + lastPolicyEvaluationTimeNs + ", avgPolicyEvaluationTimeNs="
-                + avgPolicyEvaluationTimeNs + ", memoryUsage=" + memoryUsage + ", evaluationRequestsCount="
-                + evaluationRequestsCount + ", policyStats=" + policyStats + "]";
-    }
+	public String getMemoryUsage() {
+		return memoryUsage;
+	}
+
+	public void setMemoryUsage(String memoryUsage) {
+		this.memoryUsage = memoryUsage;
+	}
+
+	public long getEvaluationRequestsCount() {
+		return evaluationRequestsCount;
+	}
+
+	public void setEvaluationRequestsCount(long evaluationRequestsCount) {
+		this.evaluationRequestsCount = evaluationRequestsCount;
+	}
+
+	public Map<String, String> getPolicyStats() {
+		return policyStats;
+	}
+
+	public void setPolicyStats(Map<String, String> policyStats) {
+		this.policyStats = policyStats;
+	}
+
+	@Override
+	public String toString() {
+		return "TelemetryEntry [inum=" + inum + ", creationDate=" + creationDate + ", eventTime=" + eventTime
+				+ ", service=" + service + ", nodeId=" + nodeId + ", status=" + status + ", lastPolicyLoadSize="
+				+ lastPolicyLoadSize + ", policySuccessLoadCounter=" + policySuccessLoadCounter
+				+ ", policyFailedLoadCounter=" + policyFailedLoadCounter + ", lastPolicyEvaluationTimeNs="
+				+ lastPolicyEvaluationTimeNs + ", avgPolicyEvaluationTimeNs=" + avgPolicyEvaluationTimeNs
+				+ ", memoryUsage=" + memoryUsage + ", evaluationRequestsCount=" + evaluationRequestsCount
+				+ ", policyStats=" + policyStats + ", toString()=" + super.toString() + "]";
+	}
     
 }
