0bd7a821a0cbf17fa09bd01aa4e7dae21a59e149
Oleh Bohzok <olehbozhok@gmail.com>
2024-09-07 02:04:12 +0300

    feat(cedarling):  added load PolicyStore from file and URI
    also updated the python binding.
    
    also renamed the enum
    
    PolicyStoreConfig::LocalJson to JsonRaw
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>

--- jans-lock/cedarling/init_engine/src/policy_store_config.rs
+++ jans-lock/cedarling/init_engine/src/policy_store_config.rs
@@ -1,11 +1,14 @@
+use std::{
+	fs::File,
+	path::{Path, PathBuf},
+};
+
 use crate::policy_store::PolicyStore;
 
-#[derive(serde::Deserialize, serde::Serialize, Debug)]
-#[serde(tag = "strategy")]
-#[serde(rename_all = "kebab-case")]
-#[serde(rename_all_fields = "camelCase")]
+#[derive(Debug)]
 pub enum PolicyStoreConfig {
-	LocalJson(String),
+	JsonRaw(String),
+	File(PathBuf),
 	RemoteURI(String),
 }
 
@@ -13,18 +16,38 @@ pub enum PolicyStoreConfig {
 pub enum GetPolicyError {
 	#[error("could not parse policy from json: {0}")]
 	ParseJson(#[from] serde_json::Error),
+	#[error("could not read policy file: {0}")]
+	ReadFile(#[from] std::io::Error),
+	#[error("could make http request: {0}")]
+	HttpRequest(String),
 }
 
 impl PolicyStoreConfig {
 	pub fn get_policy(self) -> Result<PolicyStore, GetPolicyError> {
 		match self {
-			Self::LocalJson(policy_raw_json) => Self::get_local_policy(policy_raw_json.as_str()),
-			Self::RemoteURI(_uri) => todo!(),
+			Self::JsonRaw(policy_raw_json) => {
+				Self::get_local_policy_raw_json(policy_raw_json.as_str())
+			}
+			Self::File(path) => Self::get_local_policy_file(path.as_path()),
+			Self::RemoteURI(uri) => Self::get_policy_remote(uri.as_str()),
 		}
 	}
 
-	pub fn get_local_policy(policy_raw_json: &str) -> Result<PolicyStore, GetPolicyError> {
+	pub fn get_local_policy_raw_json(policy_raw_json: &str) -> Result<PolicyStore, GetPolicyError> {
 		let policy: PolicyStore = serde_json::from_str(policy_raw_json)?;
+		Ok(policy)
+	}
+
+	pub fn get_local_policy_file(path: &Path) -> Result<PolicyStore, GetPolicyError> {
+		let policy: PolicyStore = serde_json::from_reader(File::open(path)?)?;
+		Ok(policy)
+	}
+
+	pub fn get_policy_remote(uri: &str) -> Result<PolicyStore, GetPolicyError> {
+		let policy: PolicyStore = ehttp::fetch_blocking(&ehttp::Request::get(uri))
+			.map_err(GetPolicyError::HttpRequest)?
+			.json()?;
+
 		Ok(policy)
 	}
 }
