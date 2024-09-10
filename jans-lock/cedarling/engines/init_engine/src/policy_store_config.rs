1b44c0145667f7bc368d45c1dc716828eb0ea31e
Oleh Bohzok <olehbozhok@gmail.com>
2024-09-10 22:01:34 +0300

    test(cedarling): PolicyStoreConfig now includes unit tests
    Tests cover policy parsing from JSON, reading from file and error handling.
    
    Excluded loading policy from URI in unit tests as it's closer to integration testing.
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>

--- jans-lock/cedarling/engines/init_engine/src/policy_store_config.rs
+++ jans-lock/cedarling/engines/init_engine/src/policy_store_config.rs
@@ -51,3 +51,119 @@ impl PolicyStoreConfig {
 		Ok(policy)
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	const POLICY_RAW_JSON: &str = include_str!("../../../demo/policy-store/local.json");
+	const POLICY_RAW_JSON_POISONED: &str =
+		include_str!("../../../demo/policy-store/local_poisoned.json");
+	const POLICY_RAW_JSON_SCHEMA_POISONED: &str =
+		include_str!("../../../demo/policy-store/local_schema_poisoned.json");
+	const POLICY_RAW_JSON_POLICY_POISONED: &str =
+		include_str!("../../../demo/policy-store/local_policy_poisoned.json");
+
+	fn check_policy_store(policy: &PolicyStore) {
+		// policies and schema we don`t check because it is parsed and validated during the parsing json
+		let google = policy
+			.trusted_issuers
+			.get("Google")
+			.expect("Google issuer not found");
+
+		// just check if we read fields correctly
+		assert_eq!(
+			google.openid_configuration_endpoint,
+			"https://accounts.google.com/.well-known/openid-configuration"
+		);
+		assert!(google.access_tokens.trusted);
+		assert!(google.id_tokens.trusted);
+		assert!(google.userinfo_tokens.trusted);
+
+		assert_eq!(
+			google.id_tokens.principal_identifier,
+			Some("email".to_string())
+		);
+		assert_eq!(
+			google.userinfo_tokens.role_mapping,
+			Some("role".to_string())
+		)
+	}
+
+	#[test]
+	fn get_local_policy_raw_json() {
+		let policy = PolicyStoreConfig::get_local_policy_raw_json(POLICY_RAW_JSON).unwrap();
+		check_policy_store(&policy);
+	}
+
+	#[test]
+	fn get_local_policy_raw_json_fail() {
+		let err = PolicyStoreConfig::get_local_policy_raw_json(POLICY_RAW_JSON_POISONED)
+			.expect_err("should be error");
+		assert!(
+			matches!(err, GetPolicyError::ParseJson(_)),
+			"Expected ParseJson error, got: {:?}",
+			err
+		);
+	}
+
+	#[test]
+	fn get_local_policy_raw_json_schema_fail() {
+		let err = PolicyStoreConfig::get_local_policy_raw_json(POLICY_RAW_JSON_SCHEMA_POISONED)
+			.expect_err("should be error");
+		assert!(
+			matches!(err, GetPolicyError::ParseJson(_)),
+			"Expected ParseJson error, got: {:?}",
+			err
+		);
+	}
+
+	#[test]
+	fn get_local_policy_raw_json_policy_fail() {
+		let err = PolicyStoreConfig::get_local_policy_raw_json(POLICY_RAW_JSON_POLICY_POISONED)
+			.expect_err("should be error");
+		assert!(
+			matches!(err, GetPolicyError::ParseJson(_)),
+			"Expected ParseJson error, got: {:?}",
+			err
+		);
+	}
+
+	#[test]
+	fn get_local_policy_file_json() {
+		let policy = PolicyStoreConfig::get_local_policy_file(
+			PathBuf::from("../../demo/policy-store/local.json".to_string()).as_path(),
+		)
+		.unwrap();
+		check_policy_store(&policy);
+	}
+
+	#[test]
+	fn get_local_policy_file_parce_json_fail() {
+		let err = PolicyStoreConfig::get_local_policy_file(
+			PathBuf::from("../../demo/policy-store/local_poisoned.json".to_string()).as_path(),
+		)
+		.expect_err("should be error");
+
+		assert!(
+			matches!(err, GetPolicyError::ParseJson(_)),
+			"Expected ParseJson error, got: {:?}",
+			err
+		);
+	}
+
+	#[test]
+	fn get_local_policy_file_path_fail() {
+		let err = PolicyStoreConfig::get_local_policy_file(
+			PathBuf::from("../../demo/policy-store/not_existent_file_path.json".to_string())
+				.as_path(),
+		)
+		.expect_err("should be error");
+
+		assert!(
+			matches!(err, GetPolicyError::ReadFile(_)),
+			"Expected ReadFile error, got: {:?}",
+			err
+		);
+	}
+}
