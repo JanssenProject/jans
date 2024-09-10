447f60dbd110732c8ca5200c6c25ed9d03368dc9
Oleh <olehbozhok@gmail.com>
2024-09-10 11:04:17 +0300

    feat(jans-lock): implement fetch policy stores (#9423)
    * chore(jans-cedarling): rebase from main after cedarling_mvp branch deletion
    
    The cedarling_mvp branch was merged and subsequently deleted. This commit rebases the current changes from the main branch to keep the branch up to date.
    
    list of rebased commits:
    rust demo code without connect to python
    
    cargo improvements
    
    fix format rule
    
    add to gitignore files that is used in debug process
    
    add parsing roles from token and it mapping
    
    remove unused text in readme
    
    added guide how to build
    
    add python binding
    
     python example hotfix
    
    make Id in python example more illustrative
    
    update to make tokens field jti optional
    
    fix readme file
    
    feat: store the sample policy stores in demo folder #9373
    
    Signed-off-by: Arnab Dutta <arnab.bdutta@gmail.com>
    
    rename role mapper to token mapper
    
    add loading policy store from file or json
    
    show in example that we can use setter
    
    use single quote for action in python example
    
    update python bindings to use object Request
    
    added crate init_engine
    
    refactor authz, move some parts to init_engine
    
    added skip check clippy::enum_variant_names
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore: delete Readme.md
    
    For some reason my git client start but out.
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(chedarling) rename PolicyStoreEntry to PolicyStore
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * feat(cedarling):  added load PolicyStore from file and URI
    
    also updated the python binding.
    
    also renamed the enum
    
    PolicyStoreConfig::LocalJson to JsonRaw
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * docs(cedarling-python): added section Classes and Methods in python library
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * docs(cedarling): added to python binding documentation example how to init PolicyStore using methods:
    - from_raw_json
    - from_filepath
    - from_remote_uri
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * docs(cedarling): added to python binding documentation notice that for windows we no need to install
     maturin[patchelf]
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * docs(cedarling): added example how to extract the "role" claim from the userinfo token
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * docs(cedarling): added note that we should see `cedarling_python` listed among the available packages.
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * doc(cedarling): added note that user on linux maybe need to install build-essential for cedarling_python
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    ---------
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    Co-authored-by: Mohammad Abudayyeh <47318409+moabu@users.noreply.github.com>

--- jans-lock/cedarling/cedarling_python/src/policy_store.rs
+++ jans-lock/cedarling/cedarling_python/src/policy_store.rs
@@ -6,27 +6,46 @@ use serde_pyobject::from_pyobject;
 #[derive(Debug, Clone)]
 #[pyclass]
 pub struct PolicyStore {
-	pub(crate) inner: authz::PolicyStoreEntry,
+	pub(crate) inner: init_engine::policy_store::PolicyStore,
+}
+
+fn get_store_from_config(config: authz::PolicyStoreConfig) -> PyResult<PolicyStore> {
+	let store = config
+		.get_policy()
+		.map_err(|err| PyValueError::new_err(err.to_string()))?;
+	Ok(PolicyStore { inner: store })
+}
+
+fn to_rust_string(py_str: &Bound<'_, PyString>) -> PyResult<String> {
+	py_str
+		.to_str()
+		.map(|s| s.to_owned())
+		.map_err(|_| PyValueError::new_err("Failed to convert Python string to Rust string"))
 }
 
 #[pymethods]
 impl PolicyStore {
 	#[new]
 	fn new(input: Bound<'_, PyAny>) -> PyResult<Self> {
-		let store: authz::PolicyStoreEntry = from_pyobject(input)?;
+		let store: init_engine::policy_store::PolicyStore = from_pyobject(input)?;
 		Ok(PolicyStore { inner: store })
 	}
 
 	#[staticmethod]
 	pub fn from_raw_json(raw_json: &Bound<'_, PyString>) -> PyResult<Self> {
-		let json_str = raw_json
-			.to_str()
-			.map_err(|_| PyValueError::new_err("Failed to convert Python string to Rust string"))?;
+		let rust_str = to_rust_string(raw_json)?;
+		get_store_from_config(authz::PolicyStoreConfig::JsonRaw(rust_str))
+	}
 
-		let store = authz::PolicyStoreConfig::LocalJson(json_str.to_owned())
-			.get_policy()
-			.map_err(|err| PyValueError::new_err(err.to_string()))?;
+	#[staticmethod]
+	pub fn from_filepath(filepath: &Bound<'_, PyString>) -> PyResult<Self> {
+		let rust_str = to_rust_string(filepath)?;
+		get_store_from_config(authz::PolicyStoreConfig::File(rust_str.into()))
+	}
 
-		Ok(PolicyStore { inner: store })
+	#[staticmethod]
+	pub fn from_remote_uri(uri: &Bound<'_, PyString>) -> PyResult<Self> {
+		let rust_str = to_rust_string(uri)?;
+		get_store_from_config(authz::PolicyStoreConfig::RemoteURI(rust_str.into()))
 	}
 }
