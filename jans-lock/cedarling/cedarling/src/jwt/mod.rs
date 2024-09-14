a734098097a078372210d76451ff73378ad805fc
Oleh Bohzok <olehbozhok@gmail.com>
2024-09-14 03:44:38 +0300

    chore(jans-lock): added license notice
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>

--- jans-lock/cedarling/cedarling/src/jwt/mod.rs
+++ jans-lock/cedarling/cedarling/src/jwt/mod.rs
@@ -1,3 +1,9 @@
+/*
+ * This software is available under the Apache-2.0 license. 
+ * See https://www.apache.org/licenses/LICENSE-2.0.txt for full text.
+ *
+ * Copyright (c) 2024, Gluu, Inc. 
+ */
 //! # JWT Engine
 //! Part of Cedarling that main purpose is:
 //! - validate JWT signature
--- jans-lock/cedarling/cedarling/src/lib.rs
+++ jans-lock/cedarling/cedarling/src/lib.rs
@@ -1,3 +1,9 @@
+/*
+ * This software is available under the Apache-2.0 license.
+ * See https://www.apache.org/licenses/LICENSE-2.0.txt for full text.
+ *
+ * Copyright (c) 2024, Gluu, Inc.
+ */
 #![deny(missing_docs)]
 //! # Cedarling
 //! The Cedarling is a performant local authorization service that runs the Rust Cedar Engine.
