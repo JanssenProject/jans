896725de74e4dad23003432a1c1ed01a16232417
Oleh <olehbozhok@gmail.com>
2024-09-16 06:58:19 +0300

    feat(jans-lock): cedarling code refactoring to improve its readability and maintainance (#9493)
    * feat!(jans-lock): remove cedarling code
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-lock): created empty cedarling crate, with default parameters
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-lock): add auth module
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-lock): add init module
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-lock): add jwt module
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * docs(jans-lock): added readme file
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-lock): added license notice
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-lock): add test module
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-lock): add lock module
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-lock): add models module
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-lock): add log module
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    ---------
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    Former-commit-id: 262229d6785d7aa29bbae86c6668b14adc87d797

---
+++ jans-lock/cedarling/cedarling/src/jwt/mod.rs
@@ -0,0 +1,11 @@
+/*
+ * This software is available under the Apache-2.0 license. 
+ * See https://www.apache.org/licenses/LICENSE-2.0.txt for full text.
+ *
+ * Copyright (c) 2024, Gluu, Inc. 
+ */
+//! # JWT Engine
+//! Part of Cedarling that main purpose is:
+//! - validate JWT signature
+//! - validate JWT status
+//! - extract JWT claims
--- jans-lock/cedarling/examples/README.md
+++
@@ -1,6 +0,0 @@
-# Examples
-This folder contains examples of `cedarling` for the following languages:
-- python
-
-## Python Folders
-- cedarling_python_poetry: A sample `poetry` project that uses the python bindings of `cedarling`.
