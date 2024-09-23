e01bc545e5ca2e262dfd799e7f2994552647a51a
Oleh Bohzok <olehbozhok@gmail.com>
2024-09-24 00:31:28 +0300

    docs(jans-cedarling): add docs for PolicyStore
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>

--- jans-cedarling/cedarling/src/models/policy_store.rs
+++ jans-cedarling/cedarling/src/models/policy_store.rs
@@ -2,14 +2,15 @@ use std::collections::HashMap;
 
 use crate::init::cedar_schema::parse_cedar_schema;
 
+/// PolicyStore contains all the data the Cedarling needs to verify JWT tokens and evaluate policies
 #[derive(Debug, serde::Deserialize)]
 pub struct PolicyStore {
     #[serde(deserialize_with = "parse_cedar_schema")]
     pub schema: cedar_policy::Schema,
 }
 
+/// PolicyStoreSet it is a store for `PolicyStore` accessible by key.
 #[derive(Debug, serde::Deserialize)]
-
 pub(crate) struct PolicyStoreSet {
     #[serde(flatten)]
     pub policy_stores: HashMap<String, PolicyStore>,
