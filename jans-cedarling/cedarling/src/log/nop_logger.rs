ed80ee6f18710b9d680e135c17fc8eab28f08008
Oleh <olehbozhok@gmail.com>
2024-09-23 14:43:20 +0300

    feat (cedarling): Log Startup message (#9546)
    chore(jans-cedarling): move `cedarling` from `jans-lock` to `jans-cedarling` top level
    
    Squash all previous changes:
    
    chore(jans-cedarling): refactor, improve organization of config entity imports
    docs(jans-cedarling): add to README how to generate documentation
    chore(jans-cedarling): rename unexported field in the writer
    feat(jans-cedarling): add Authz to the Cedarling instance
    feat(jans-cedarling): add Cedarling instance
    docs(jans-cedarling): fix docstring
    test(jans-cedarling): fix log/test, import LogWriter
    docs(jans-cedarling): added to README information how to run unit tests
    test(jans-cedarling): move unit test for LogStrategy to log/test module
    docs(jans-cedarling): added to README information how to get code-coverage
    test(jans-cedarling): add unit test for LogStrategy
    test(jans-cedarling): add unit test for MemoryLogger
    chore(jans-cedarling): make fields in AuthorizationLogInfo struct public
    test(jans-cedarling): add unit test for StdOutLogger
    docs(jans-cedarling): added README to log module
    chore(jans-cedarling): add note about using uuids in different situation
    docs(jans-cedarling): improved README, added not about `log_init.rs`  file. And information about each log type.
    chore(jans-cedarling): fix clippy warnings about doc identation
    chore(jans-cedarling): fix clippy warnings
    docs(jans-cedarling): improved documentation in the log crate.
    docs(jans-cedarling): add links to the bootstrap properties in the documentation
    docs(jans-cedarling): improved documentation in the log crate. Made it public to see message from autogenerated documentation page
    chore(jans-cedarling): remove unused imports
    chore(jans-cedarling): add macros allow(dead_code) to the Authz struct
    chore(jans-cedarling): refactor make StdOutLogger more simple
    chore(jans-cedarling): refactor to use imported names for shorter match cases
    feat(jans-cedarling): add example of run cedarling Authz and updated Readme
    chore(jans-cedarling): refactor implementation  MemoryLogger method log
    chore(jans-cedarling): rename method get_logs to get_log_ids according to last changes in the `Final Cedarling Log Design`
    chore(jans-cedarling): change name on init Authz
    chore(jans-cedarling): add derive Clone, Copy to the LogType and MemoryLogConfig
    chore(jans-cedarling): added doc message to MemoryLogConfig
    chore(jans-cedarling): add documentation notes for LogStrategy
    chore(jans-cedarling): add copyright note
    chore(jans-cedarling): add simple Authz with initialization logger service
    chore(jans-cedarling): add init_logger function
    chore(jans-cedarling): fix missed documentation
    chore(jans-cedarling): initialize logger using configuration settings
    chore(jans-cedarling): add configs for logger
    chore(jans-cedarling): add LogStrategy to unify multiple implementations under a common API
    chore(jans-cedarling): add MemoryLogger
    chore(jans-cedarling): add MemoryLogger and implement  LogWriter trait
    chore(jans-cedarling): make LogEntry properties public
    chore(jans-cedarling): added to sparkv error text error message
    chore(jans-cedarling): added сopyright note to sparkv
    chore(jans-cedarling): added StdOutLogger
    chore(jans-cedarling): added NopLogger, that do nothing.
    chore(jans-cedarling): added log interfaces
    chore(jans-cedarling): added log LogEntry struct and related to it.
    feat(jans-cedarling): added fork of the SparKV as is
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>

---
+++ jans-cedarling/cedarling/src/log/nop_logger.rs
@@ -0,0 +1,19 @@
+/*
+ * This software is available under the Apache-2.0 license.
+ * See https://www.apache.org/licenses/LICENSE-2.0.txt for full text.
+ *
+ * Copyright (c) 2024, Gluu, Inc.
+ */
+
+use super::interface::LogWriter;
+use crate::models::log_entry::LogEntry;
+
+/// A logger that do nothing.
+pub(crate) struct NopLogger;
+
+// Implementation of LogWriter
+impl LogWriter for NopLogger {
+    fn log(&self, _entry: LogEntry) {
+        // Do nothing
+    }
+}
