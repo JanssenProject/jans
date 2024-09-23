7dac32a58689eb70fea829f80d2847be398fc8df
Oleh Bohzok <olehbozhok@gmail.com>
2024-09-24 00:23:57 +0300

    test(jans-cedarling): add unit tests to check `parse_cedar_schema` (deserialize cedar schema from json)
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>

--- jans-cedarling/cedarling/src/init/cedar_schema.rs
+++ jans-cedarling/cedarling/src/init/cedar_schema.rs
@@ -21,3 +21,54 @@ where
 
     Ok(schema)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::models::policy_store::PolicyStoreSet;
+
+    #[test]
+    fn test_read_ok() {
+        static POLICY_STORE_RAW: &str = include_str!("test_files/policy-store_ok.json");
+
+        let policy_result = serde_json::from_str::<PolicyStoreSet>(POLICY_STORE_RAW);
+        assert!(policy_result.is_ok());
+    }
+
+    #[test]
+    fn test_read_base64_error() {
+        static POLICY_STORE_RAW: &str =
+            include_str!("test_files/policy-store_schema_err_base64.json");
+
+        let policy_result = serde_json::from_str::<PolicyStoreSet>(POLICY_STORE_RAW);
+        assert!(policy_result
+            .unwrap_err()
+            .to_string()
+            .contains(MSG_UNABLE_DECODE_SCHEMA_BASE64));
+    }
+
+    #[test]
+    fn test_read_json_error() {
+        static POLICY_STORE_RAW: &str =
+            include_str!("test_files/policy-store_schema_err_json.json");
+
+        let policy_result = serde_json::from_str::<PolicyStoreSet>(POLICY_STORE_RAW);
+        assert!(policy_result
+            .unwrap_err()
+            .to_string()
+            .contains(MSG_UNABLE_DECODE_SCHEMA_JSON));
+    }
+
+    #[test]
+    fn test_read_cedar_error() {
+        static POLICY_STORE_RAW: &str =
+            include_str!("test_files/policy-store_schema_err_cedar_mistake.json");
+
+        let policy_result = serde_json::from_str::<PolicyStoreSet>(POLICY_STORE_RAW);
+        // in this scenario error message looks like:
+        // `unable to decode cedar policy schema json: failed to resolve type: User_TypeNotExist", line: 35, column: 1`
+        let err_msg = policy_result.unwrap_err().to_string();
+        assert!(err_msg.contains(MSG_UNABLE_DECODE_SCHEMA_JSON));
+        assert!(err_msg.contains("failed to resolve type"));
+    }
+}
