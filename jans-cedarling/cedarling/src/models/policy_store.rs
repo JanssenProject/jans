e7240903388e39f17bbcce8d755c27f3a55dc617
Oleh Bohzok <olehbozhok@gmail.com>
2024-09-24 12:35:25 +0300

    chore(jans-cedarling): move position of PolicyStoreMap to be first
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>

--- jans-cedarling/cedarling/src/models/policy_store.rs
+++ jans-cedarling/cedarling/src/models/policy_store.rs
@@ -9,17 +9,17 @@ use std::collections::HashMap;
 
 use crate::init::cedar_schema::parse_cedar_schema;
 
+/// PolicyStoreMap it is a store for `PolicyStore` accessible by key.
+#[derive(Debug, serde::Deserialize)]
+pub(crate) struct PolicyStoreMap {
+    #[serde(flatten)]
+    pub policy_stores: HashMap<String, PolicyStore>,
+}
+
 /// PolicyStore contains all the data the Cedarling needs to verify JWT tokens and evaluate policies
 #[derive(Debug, serde::Deserialize)]
 pub struct PolicyStore {
     #[serde(deserialize_with = "parse_cedar_schema")]
     #[allow(dead_code)]
     pub schema: cedar_policy::Schema,
-}
-
-/// PolicyStoreMap it is a store for `PolicyStore` accessible by key.
-#[derive(Debug, serde::Deserialize)]
-pub(crate) struct PolicyStoreMap {
-    #[serde(flatten)]
-    pub policy_stores: HashMap<String, PolicyStore>,
 }
