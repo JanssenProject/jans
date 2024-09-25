b32aafaf37eb86ef95511fef4922d85a79f7bc08
Oleh Bohzok <olehbozhok@gmail.com>
2024-09-25 14:38:23 +0300

    test(jans-cedarling): add unit tests to parse_policy_set
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>

--- jans-cedarling/cedarling/src/init/policy.rs
+++ jans-cedarling/cedarling/src/init/policy.rs
@@ -10,9 +10,11 @@ use std::collections::HashMap;
 use base64::prelude::*;
 use cedar_policy::PolicyId;
 
-const MSG_UNABLE_DECODE_POLICY_BASE64: &str = "decode base64 error:";
-const MSG_UNABLE_DECODE_POLICY_STRING: &str = "decode to utf8 string error:";
-const MSG_UNABLE_DECODE_POLICY_HUMAN_REDABLE: &str = "decode from human redable format error:";
+const MSG_UNABLE_DECODE_POLICY_BASE64: &str = "decode base64, error:";
+const MSG_UNABLE_DECODE_POLICY_STRING: &str = "decode to utf8 string, error:";
+const MSG_UNABLE_DECODE_POLICY_HUMAN_REDABLE: &str = "decode from human redable format, error:";
+const MSG_UNABLE_TO_CREATE_POLICY_SET: &str =
+    "could not collect policy store's to policy set, error:";
 
 /// Represents a raw data of the `Policy` in the `PolicyStore`
 /// is private and used only in the [`parse_policy_set`] function
@@ -47,9 +49,7 @@ where
 
     Ok(
         cedar_policy::PolicySet::from_policies(policy_vec.into_iter()).map_err(|err| {
-            serde::de::Error::custom(format!(
-                "could not collect policy store's to policy set, error: {err}"
-            ))
+            serde::de::Error::custom(format!("{MSG_UNABLE_TO_CREATE_POLICY_SET} {err}"))
         })?,
     )
 }
@@ -74,4 +74,55 @@ where
         })?;
 
     Ok(policy)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::models::policy_store::PolicyStoreMap;
+
+    #[test]
+    fn test_ok() {
+        static POLICY_STORE_RAW: &str = include_str!("test_files/policy-store_ok.json");
+
+        let policy_result = serde_json::from_str::<PolicyStoreMap>(POLICY_STORE_RAW);
+        assert!(policy_result.is_ok());
+    }
+
+    #[test]
+    fn test_base64_error() {
+        static POLICY_STORE_RAW: &str =
+            include_str!("test_files/policy-store_policy_err_base64.json");
+
+        let policy_result = serde_json::from_str::<PolicyStoreMap>(POLICY_STORE_RAW);
+        assert!(policy_result
+            .unwrap_err()
+            .to_string()
+            .contains(MSG_UNABLE_DECODE_POLICY_BASE64));
+    }
+
+    #[test]
+    fn test_string_error() {
+        static POLICY_STORE_RAW: &str =
+            include_str!("test_files/policy-store_policy_err_broken_utf8.json");
+
+        let policy_result = serde_json::from_str::<PolicyStoreMap>(POLICY_STORE_RAW);
+        assert!(policy_result
+            .unwrap_err()
+            .to_string()
+            .contains(MSG_UNABLE_DECODE_POLICY_STRING));
+    }
+
+    #[test]
+    fn test_policy_error() {
+        static POLICY_STORE_RAW: &str =
+            include_str!("test_files/policy-store_policy_err_broken_policy.json");
+
+        let policy_result = serde_json::from_str::<PolicyStoreMap>(POLICY_STORE_RAW);
+        // in this scenario error message looks like:
+        // "unable to decode policy with id: 840da5d85403f35ea76519ed1a18a33989f855bf1cf8, error: decode from human redable format error: unexpected token `)`
+        let err_msg = policy_result.unwrap_err().to_string();
+        assert!(err_msg.contains(MSG_UNABLE_DECODE_POLICY_HUMAN_REDABLE));
+        assert!(err_msg.contains("unexpected token `)`"));
+    }
 }
