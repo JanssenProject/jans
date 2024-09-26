713c692ab3e7f6deed81863144aeb7618e376725
Oleh <olehbozhok@gmail.com>
2024-09-26 09:32:24 +0300

    feat(jans-cedarling): Policy Store: Parse Policies - both valid and invalid (#9584)
    * feat(jans-cedarling): added parsing policy to get `cedar_policy::PolicySet`
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * test(jans-cedarling): add unit tests to parse_policy_set
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * docs(jans-cedarling): add doc string to the `load_policy_store_map` function
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): refactor, move errors messages to the enum
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * test(jans-cedarling): add compare full string in `test_read_cedar_error`, so it should be better
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): refactor, rename entites to be more descriptive
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * test(jans-cedarling): add compare full string in `test_policy_error`, so it should be better
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): refactor, rename ParsePolicySetErrMsg to ParsePolicySetMessage to be more descriptive
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): refactor, rename ParceCedarSchemaErrMsg to ParceCedarSchemaSetMessage to be more descriptive
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    ---------
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    Co-authored-by: Mohammad Abudayyeh <47318409+moabu@users.noreply.github.com>
    Former-commit-id: ea17aeed549ecd514e2f6ab473794971682ebbce

--- jans-cedarling/cedarling/src/init/policy_store.rs
+++ jans-cedarling/cedarling/src/init/policy_store.rs
@@ -10,9 +10,9 @@ use crate::models::policy_store_config::{PolicyStoreConfig, PolicyStoreSource};
 
 /// Error cases for loading policy
 #[derive(Debug, thiserror::Error)]
-pub enum ErrorLoadPolicyStore {
+pub enum LoadPolicyStoreError {
     #[error("{0}")]
-    JsonParce(#[from] serde_json::Error),
+    Parse(#[from] serde_json::Error),
     #[error("store policy is empty")]
     PolicyEmpty,
     #[error("the `store_key` is not specified and the count on policies more than 1")]
@@ -21,23 +21,31 @@ pub enum ErrorLoadPolicyStore {
     FindPolicy(String),
 }
 
+/// Load policy store from source
+fn load_policy_store_map(
+    source: PolicyStoreSource,
+) -> Result<PolicyStoreMap, LoadPolicyStoreError> {
+    let policy_store_map: PolicyStoreMap = match source {
+        PolicyStoreSource::Json(json_raw) => serde_json::from_str(json_raw.as_str())?,
+    };
+    Ok(policy_store_map)
+}
+
 /// Load policy store based on config
 //
 // Unit tests will be added when will be implemented other types of sources
 pub(crate) fn load_policy_store(
     config: PolicyStoreConfig,
-) -> Result<PolicyStore, ErrorLoadPolicyStore> {
-    let mut policy_store_map: PolicyStoreMap = match config.source {
-        PolicyStoreSource::Json(json_raw) => serde_json::from_str(json_raw.as_str())?,
-    };
+) -> Result<PolicyStore, LoadPolicyStoreError> {
+    let mut policy_store_map = load_policy_store_map(config.source)?;
 
     let policy: PolicyStore = match (config.store_id, policy_store_map.policy_stores.len()) {
         (Some(store_id), _) => policy_store_map
             .policy_stores
             .remove(store_id.as_str())
-            .ok_or(ErrorLoadPolicyStore::FindPolicy(store_id))?,
+            .ok_or(LoadPolicyStoreError::FindPolicy(store_id))?,
         (None, 0) => {
-            return Err(ErrorLoadPolicyStore::PolicyEmpty);
+            return Err(LoadPolicyStoreError::PolicyEmpty);
         },
         (None, 1) => {
             // getting first element and we know it is save to use unwrap here,
@@ -50,7 +58,7 @@ pub(crate) fn load_policy_store(
                 .unwrap()
         },
         (None, 2..) => {
-            return Err(ErrorLoadPolicyStore::MoreThanOnePolicy);
+            return Err(LoadPolicyStoreError::MoreThanOnePolicy);
         },
     };
 
