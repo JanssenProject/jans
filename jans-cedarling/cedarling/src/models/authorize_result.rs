e3765139ce835fc1084ad55248bf136465a2216d
Oleh <olehbozhok@gmail.com>
2024-10-10 19:02:13 +0300

    feat(jans-cedarling): Parse Cedar Schema for Access Token and Workload entities (#9694)
    * feat(jans-cedarling): added real authorize request and dynamic parse entities from access_token
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    chore(jans-cedarling): prepare to rebase
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    feat(jans-cedarling): added real authorize request and dynamic parse entities from access_token
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): add crate to better unit testing
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * test(jans-cedarling): add test cases on creating entities
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): bugfix, mistype after fix clippy issues
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * feat(jans-cedarling): add log of authorization request
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * docs(jans-cedarling): add documentation how to run the `authorize` example file
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): spell fix
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): remove python binding to `authorize` method. Will be implemented in further PR
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): update metadata for AccessTokenMeta, use `jti` field for entity id
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    ---------
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>

---
+++ jans-cedarling/cedarling/src/models/authorize_result.rs
@@ -0,0 +1,19 @@
+use cedar_policy::Decision;
+
+/// Result of authorization and evaluation cedar policy
+/// based on the [Request](crate::models::request::Request) and policy store
+pub struct AuthorizeResult {
+    /// Result of authorization where principal is `Jans::Workload`
+    pub workload: cedar_policy::Response,
+    // The response for the user entity
+    // next field will be added in further PR
+    // pub person: cedar_policy::Response,
+}
+
+impl AuthorizeResult {
+    /// Returns true if workload is allowed
+    pub fn is_allowed(&self) -> bool {
+        // in future we should also check if the person is allowed
+        self.workload.decision() == Decision::Allow
+    }
+}
