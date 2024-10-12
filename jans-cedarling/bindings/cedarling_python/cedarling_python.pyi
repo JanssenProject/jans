343d8667ee150f88910cf9197b5e3f73afc97965
Oleh Bohzok <olehbozhok@gmail.com>
2024-10-12 19:58:19 +0300

    chore(jans-cedarling): fix mypy errors
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>

--- jans-cedarling/bindings/cedarling_python/cedarling_python.pyi
+++ jans-cedarling/bindings/cedarling_python/cedarling_python.pyi
@@ -1,6 +1,7 @@
-from typing import *
+from typing import Optional, List, final
 
 
+@final
 class MemoryLogConfig:
 
     log_ttl: int
@@ -8,57 +9,64 @@ class MemoryLogConfig:
     def __init__(self, log_ttl: int = 60) -> None: ...
 
 
+@final
 class StdOutLogConfig:
-    def __init__(self) -> None: ...
+    def __init__(self, /, *args, **kwargs) -> None: ...
 
 
+@final
 class DisabledLoggingConfig:
-    def __init__(self) -> None: ...
+    def __init__(self, /, *args, **kwargs) -> None: ...
 
 
-LogConfigs = MemoryLogConfig | StdOutLogConfig | DisabledLoggingConfig
-
-
+@final
 class PolicyStoreSource:
     def __init__(self, json: Optional[str] = None) -> None: ...
 
 
+@final
 class PolicyStoreConfig:
+    source: PolicyStoreSource | None
+    store_id: str | None
+
     def __init__(
         self,
-        source: Optional[PolicyStoreSource] = None,
-        store_id: Optional[str] = None
+        source: PolicyStoreSource | None = None,
+        store_id: str | None = None
     ) -> None: ...
 
 
+@final
 class JwtConfig:
     enabled: bool
-    signature_algorithms: List[str]
+    signature_algorithms: List[str] | None
 
     def __init__(
         self, enabled: bool,
-        signature_algorithms: List[str] = None) -> None: ...
+        signature_algorithms: List[str] | None = None) -> None: ...
 
 
+@final
 class BootstrapConfig:
     # under the hood next fields are optional but
     # we want enforce to provide them
     # and if pass not initialized field to the Cedarling it will raise error
 
-    application_name: str
-    log_config: LogConfigs
-    policy_store_config: PolicyStoreConfig
-    jwt_config: JwtConfig
+    application_name: str | None
+    log_config: MemoryLogConfig | StdOutLogConfig | DisabledLoggingConfig | None
+    policy_store_config: PolicyStoreConfig | None
+    jwt_config: JwtConfig | None
 
     def __init__(
         self,
-        application_name: str = None,
-        log_config: LogConfigs = None,
-        policy_store_config: PolicyStoreConfig = None,
-        jwt_config: JwtConfig = None
+        application_name: str | None = None,
+        log_config: MemoryLogConfig | StdOutLogConfig | DisabledLoggingConfig | None = None,
+        policy_store_config: PolicyStoreConfig | None = None,
+        jwt_config: JwtConfig | None = None
     ) -> None: ...
 
 
+@final
 class Cedarling:
     def __init__(self, config: BootstrapConfig) -> None: ...
 
