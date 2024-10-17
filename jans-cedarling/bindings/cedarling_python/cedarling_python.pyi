12fe2b93b01c0e408071e50700c2a4707e6120fa
Oleh Bohzok <olehbozhok@gmail.com>
2024-10-17 18:09:49 +0300

    feat(jans-cedarling): add stub info about new python types for authorize method
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>

--- jans-cedarling/bindings/cedarling_python/cedarling_python.pyi
+++ jans-cedarling/bindings/cedarling_python/cedarling_python.pyi
@@ -1,4 +1,4 @@
-from typing import Optional, List, final
+from typing import Optional, List, final, Dict, Any
 
 
 @final
@@ -75,3 +75,64 @@ class Cedarling:
     def get_log_by_id(self, id: str) -> Optional[dict]: ...
 
     def get_log_ids(self) -> List[str]: ...
+
+    def authorize(self, request: Request) -> AuthorizeResult: ...
+
+
+@final
+class Request:
+    access_token: str
+    action: str
+    resource: ResourceData
+    context: Dict[str, Any]
+
+    def __init__(self, access_token: str,
+                 action: str,
+                 resource: ResourceData,
+                 context: Dict[str, Any]) -> None: ...
+
+
+@final
+class ResourceData:
+    resource_type: str
+    id: str
+    payload: Dict[str, Any]
+
+    def __init__(self, resource_type: str, id: str, **kwargs) -> None: ...
+
+    @classmethod
+    def from_dict(cls, value: Dict[str, Any]) -> "ResourceData": ...
+
+
+@final
+class AuthorizeResult:
+    def is_allowed(self) -> bool: ...
+
+    def workload(self) -> AuthorizeResultResponse: ...
+
+
+@final
+class AuthorizeResultResponse:
+    decision: Decision
+    diagnostics: Diagnostics
+
+
+@final
+class Decision:
+    value: str
+
+    def __str__(self) -> str: ...
+
+    def __eq__(self, value: object) -> bool: ...
+
+
+@final
+class Diagnostics:
+    reason: set[str]
+    errors: List[PolicyEvaluationError]
+
+
+@final
+class PolicyEvaluationError:
+    id: str
+    error: str
