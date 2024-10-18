8042813a5eab0aa6fb01d75943e3514c357a6bbd
Oleh Bohzok <olehbozhok@gmail.com>
2024-10-18 05:08:39 +0300

    chore(jans-cedarling): add more comments and description what the unit test do
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>

--- jans-cedarling/bindings/cedarling_python/tests/test_authorize.py
+++ jans-cedarling/bindings/cedarling_python/tests/test_authorize.py
@@ -5,16 +5,50 @@ from cedarling_python import ResourceData, Request, authorize_errors
 
 # In python unit tests we not cover all possible scenarios, but most common.
 
-
+# JSON payload of access token
+# {
+#   "sub": "boG8dfc5MKTn37o7gsdCeyqL8LpWQtgoO41m1KZwdq0",
+#   "code": "bf1934f6-3905-420a-8299-6b2e3ffddd6e",
+#   "iss": "https://admin-ui-test.gluu.org",
+#   "token_type": "Bearer",
+#   "client_id": "5b4487c4-8db1-409d-a653-f907b8094039",
+#   "aud": "5b4487c4-8db1-409d-a653-f907b8094039",
+#   "acr": "basic",
+#   "x5t#S256": "",
+#   "scope": [
+#     "openid",
+#     "profile"
+#   ],
+#   "org_id": "some_long_id",
+#   "auth_time": 1724830746,
+#   "exp": 1724945978,
+#   "iat": 1724832259,
+#   "jti": "lxTmCVRFTxOjJgvEEpozMQ",
+#   "name": "Default Admin User",
+#   "status": {
+#     "status_list": {
+#       "idx": 201,
+#       "uri": "https://admin-ui-test.gluu.org/jans-auth/restv1/status_list"
+#     }
+#   }
+# }
 ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJib0c4ZGZjNU1LVG4zN283Z3NkQ2V5cUw4THBXUXRnb080MW0xS1p3ZHEwIiwiY29kZSI6ImJmMTkzNGY2LTM5MDUtNDIwYS04Mjk5LTZiMmUzZmZkZGQ2ZSIsImlzcyI6Imh0dHBzOi8vYWRtaW4tdWktdGVzdC5nbHV1Lm9yZyIsInRva2VuX3R5cGUiOiJCZWFyZXIiLCJjbGllbnRfaWQiOiI1YjQ0ODdjNC04ZGIxLTQwOWQtYTY1My1mOTA3YjgwOTQwMzkiLCJhdWQiOiI1YjQ0ODdjNC04ZGIxLTQwOWQtYTY1My1mOTA3YjgwOTQwMzkiLCJhY3IiOiJiYXNpYyIsIng1dCNTMjU2IjoiIiwic2NvcGUiOlsib3BlbmlkIiwicHJvZmlsZSJdLCJvcmdfaWQiOiJzb21lX2xvbmdfaWQiLCJhdXRoX3RpbWUiOjE3MjQ4MzA3NDYsImV4cCI6MTcyNDk0NTk3OCwiaWF0IjoxNzI0ODMyMjU5LCJqdGkiOiJseFRtQ1ZSRlR4T2pKZ3ZFRXBvek1RIiwibmFtZSI6IkRlZmF1bHQgQWRtaW4gVXNlciIsInN0YXR1cyI6eyJzdGF0dXNfbGlzdCI6eyJpZHgiOjIwMSwidXJpIjoiaHR0cHM6Ly9hZG1pbi11aS10ZXN0LmdsdXUub3JnL2phbnMtYXV0aC9yZXN0djEvc3RhdHVzX2xpc3QifX19._eQT-DsfE_kgdhA0YOyFxxPEMNw44iwoelWa5iU1n9s"
+# currently id token is unused and it just valid JWT file
 ID_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJib0c4ZGZjNU1LVG4zN283Z3NkQ2V5cUw4THBXUXRnb080MW0xS1p3ZHEwIiwiY29kZSI6ImJmMTkzNGY2LTM5MDUtNDIwYS04Mjk5LTZiMmUzZmZkZGQ2ZSIsImlzcyI6Imh0dHBzOi8vYWRtaW4tdWktdGVzdC5nbHV1Lm9yZyIsInRva2VuX3R5cGUiOiJCZWFyZXIiLCJjbGllbnRfaWQiOiI1YjQ0ODdjNC04ZGIxLTQwOWQtYTY1My1mOTA3YjgwOTQwMzkiLCJhdWQiOiI1YjQ0ODdjNC04ZGIxLTQwOWQtYTY1My1mOTA3YjgwOTQwMzkiLCJhY3IiOiJiYXNpYyIsIng1dCNTMjU2IjoiIiwic2NvcGUiOlsib3BlbmlkIiwicHJvZmlsZSJdLCJvcmdfaWQiOiJzb21lX2xvbmdfaWQiLCJhdXRoX3RpbWUiOjE3MjQ4MzA3NDYsImV4cCI6MTcyNDk0NTk3OCwiaWF0IjoxNzI0ODMyMjU5LCJqdGkiOiJseFRtQ1ZSRlR4T2pKZ3ZFRXBvek1RIiwibmFtZSI6IkRlZmF1bHQgQWRtaW4gVXNlciIsInN0YXR1cyI6eyJzdGF0dXNfbGlzdCI6eyJpZHgiOjIwMSwidXJpIjoiaHR0cHM6Ly9hZG1pbi11aS10ZXN0LmdsdXUub3JnL2phbnMtYXV0aC9yZXN0djEvc3RhdHVzX2xpc3QifX19._eQT-DsfE_kgdhA0YOyFxxPEMNw44iwoelWa5iU1n9s"
+
+# string representation of the authorize decision result
 ALLOW_DECISION_STR = "ALLOW"
 DENY_DECISION_STR = "DENY"
 
 
 def test_authorize_ok(sample_bootstrap_config):
+    '''
+    Test create correct cedarling requst where Resource.org_id is same as Workload.org_id.
+    Workload is created from ACCESS_TOKEN
+    '''
     instance = Cedarling(sample_bootstrap_config)
 
+    # Create resouce with type "Jans::Issue" from cedar-policy schema.
     resource = ResourceData.from_dict({
         "type": "Jans::Issue",
         "id": "random_id",
@@ -33,7 +67,9 @@ def test_authorize_ok(sample_bootstrap_config):
     workload_result = authorize_result.workload()
 
     decision = workload_result.decision
+    # check that Decision type converts to the string correctly
     assert str(decision) == ALLOW_DECISION_STR
+    # check that Decision.value is correct
     assert decision.value == ALLOW_DECISION_STR
 
     diagnostics = workload_result.diagnostics
@@ -46,6 +82,10 @@ def test_authorize_ok(sample_bootstrap_config):
 
 # function that we will call in tests where check error handling
 def raise_authorize_error(sample_bootstrap_config):
+    '''
+        The function attempts to make an authorized request but raises an error0 (authorize_errors.ResourceEntityError)
+        because, in the cedar-policy type Jans::Issue, field `org_id` should be a string.
+    '''
     instance = Cedarling(sample_bootstrap_config)
 
     resource = ResourceData.from_dict({
