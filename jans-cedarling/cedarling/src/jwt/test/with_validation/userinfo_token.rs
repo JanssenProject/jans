c1ae43eaf47cf369e50b6a49b80299ea782a60a8
rmarinn <34529290+rmarinn@users.noreply.github.com>
2024-10-31 21:07:16 +0800

    test(jans-cedarling): add unit tests for validating `nbf`
    - add test expecting to error when using access_token before nbf
    - add test expecting to error when using id_token before nbf
    - add test expecting to error when using userinfo_token nbf
    
    Signed-off-by: rmarinn <34529290+rmarinn@users.noreply.github.com>

--- jans-cedarling/cedarling/src/jwt/test/with_validation/userinfo_token.rs
+++ jans-cedarling/cedarling/src/jwt/test/with_validation/userinfo_token.rs
@@ -21,6 +21,7 @@
 //!   - Tests for errors when the `userinfo_token` has a different `iss` with the access_token
 //!   - Tests for errors when the `userinfo_token` has a different `aud` with the access_token
 //!   - Tests for errors when the `userinfo_token` has a different `sub` with the id_token
+//!   - Tests for errors when the `nbf` has not passed yet.
 
 use super::super::*;
 use crate::common::policy_store::TrustedIssuer;
@@ -768,6 +769,123 @@ fn errors_on_invalid_sub() {
             )),
         ),
         "Expected decoding to fail due to `userinfo_token` not having the same `sub` as `access_token`: {:?}",
+        decode_result
+    );
+
+    // assert that there aren't any additional calls to the mock server
+    openid_conf_mock.assert();
+    jwks_uri_mock.assert();
+}
+
+#[test]
+/// Tests that [`JwtService::decode_claims`] returns an error when the `id_token` is used
+/// before the `nbf` timestamp
+fn errors_on_token_used_before_nbf() {
+    // initialize mock server to simulate OpenID configuration and JWKS responses
+    let mut server = mockito::Server::new();
+
+    // generate keys and setup the encoding keys and JWKS (JSON Web Key Set)
+    let (encoding_keys, jwks) = generate_keys();
+
+    // Invalid access_token claims (expired)
+    let access_token_claims = json!({
+        "iss": server.url(),
+        "aud": "some_aud".to_string(),
+        "sub": "some_sub".to_string(),
+        "scopes": "some_scope".to_string(),
+        "iat": Timestamp::now(),
+        "exp": Timestamp::one_hour_after_now(),
+    });
+    // Invalid access_token claims
+    let id_token_claims = json!({
+        "iss": server.url(),
+        "aud": "some_aud".to_string(),
+        "sub": "some_sub".to_string(),
+        "email": "some_email@gmail.com".to_string(),
+        "iat": Timestamp::now(),
+        "exp": Timestamp::one_hour_after_now(),
+    });
+    // Valid userinfo_token claims (nbf has not yet passed)
+    let userinfo_token_claims = json!({
+        "iss": server.url(),
+        "aud": "some_aud".to_string(),
+        "sub": "some_sub".to_string(),
+        "client_id": "some_aud".to_string(),
+        "name": "ferris".to_string(),
+        "email": "ferris@gluu.com".to_string(),
+        "iat": Timestamp::now(),
+        "exp": Timestamp::one_hour_after_now()*2,
+        "nbf": Timestamp::one_hour_after_now(),
+    });
+
+    // generate the signed token strings
+    let access_token = generate_token_using_claims(
+        &access_token_claims,
+        &encoding_keys[0].0,
+        &encoding_keys[0].1,
+    );
+    let id_token =
+        generate_token_using_claims(&id_token_claims, &encoding_keys[1].0, &encoding_keys[1].1);
+    let userinfo_token = generate_token_using_claims(
+        &userinfo_token_claims,
+        &encoding_keys[0].0,
+        &encoding_keys[0].1,
+    );
+
+    // setup mock server responses for OpenID configuration and JWKS URIs
+    let openid_config_response = json!({
+        "issuer": server.url(),
+        "jwks_uri": &format!("{}/jwks", server.url()),
+        "unexpected": 123123, // a random number used to simulate having unexpected fields in the response
+    });
+    let openid_conf_mock = server
+        .mock("GET", "/.well-known/openid-configuration")
+        .with_status(200)
+        .with_header("content-type", "application/json")
+        .with_body(openid_config_response.to_string())
+        .create();
+    let jwks_uri_mock = server
+        .mock("GET", "/jwks")
+        .with_status(200)
+        .with_header("content-type", "application/json")
+        .with_body(jwks)
+        .create();
+
+    // initialize JwtService with validation enabled and ES256 as the supported algorithm
+    let jwt_service = JwtService::new_with_config(crate::jwt::JwtServiceConfig::WithValidation {
+        supported_algs: vec![Algorithm::ES256],
+        trusted_idps: vec![TrustedIssuer {
+            name: "some_idp".to_string(),
+            description: "some_desc".to_string(),
+            openid_configuration_endpoint: format!(
+                "{}/.well-known/openid-configuration",
+                server.url()
+            ),
+            token_metadata: None,
+        }],
+    });
+
+    // key service should fetch the jwks_uri on init
+    openid_conf_mock.assert();
+    // key service should fetch the jwks on init
+    jwks_uri_mock.assert();
+
+    // decode and validate the tokens
+    let decode_result = jwt_service
+        .decode_tokens::<serde_json::Value, serde_json::Value, serde_json::Value>(
+            &access_token,
+            &id_token,
+            &userinfo_token,
+        );
+
+    assert!(
+        matches!(
+            decode_result,
+            Err(jwt::Error::InvalidUserinfoToken(
+                jwt::decoding_strategy::Error::Validation(_)
+            )),
+        ),
+        "Expected decoding to fail due to `userinfo_token` being used before the `nbf` timestamp: {:?}",
         decode_result
     );
 
