fba2a607e65a29ea0a8971e2b4481f8bfee0091a
Oleh Bohzok <olehbozhok@gmail.com>
2024-11-04 18:18:32 +0200

    chore(jans-cedarling): minor changes for better readability
    Extended comment
    and refactored boolean expression
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>

--- jans-cedarling/cedarling/src/authz/authorize_result.rs
+++ jans-cedarling/cedarling/src/authz/authorize_result.rs
@@ -12,9 +12,15 @@ pub struct AuthorizeResult {
 }
 
 impl AuthorizeResult {
-    /// Returns true if request is allowed
-    /// check decision algorithm:
-    /// PRINCIPAL AND (USER OR ROLE)
+    /// Evaluates the authorization result to determine if the request is allowed.  
+    ///  
+    /// This function checks the decision based on the following rule:  
+    /// - The `workload` must allow the request (PRINCIPAL).  
+    /// - Either the `person` or `role` must also allow the request (USER OR ROLE).  
+    ///  
+    /// This approach represents a hierarchical decision-making model, where the  
+    /// `workload` (i.e., primary principal) needs to permit the request and
+    /// additional conditions (either `person` or `role`) must also indicate allowance.
     pub fn is_allowed(&self) -> bool {
         let role_decision = self
             .role
@@ -22,7 +28,10 @@ impl AuthorizeResult {
             .map(|result| result.decision())
             .unwrap_or(Decision::Deny);
 
-        self.workload.decision() == Decision::Allow
-            && (self.person.decision() == Decision::Allow || role_decision == Decision::Allow)
+        let workload_allowed = self.workload.decision() == Decision::Allow;
+        let person_or_role_allowed =
+            self.person.decision() == Decision::Allow || role_decision == Decision::Allow;
+
+        workload_allowed && person_or_role_allowed
     }
 }
