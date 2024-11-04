d9531bcd38b787e0af58bfc14beab87e66c40272
Oleh <olehbozhok@gmail.com>
2024-11-04 23:16:43 +0200

    feat(jans-cedarling): add build `Role` entity (#10016)
    * chore(jans-cedarling): remove  #[allow(unused)] in key_service
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): refactor initialization of KeyService to be more readable
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): add getting trusted issuer when decode JWT tokens
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): remove transaction token
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * test(jans-cedarling): fix test case after deleting  `transaction_token`
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * feat(jans-cedarling): add entity Jans::Role to entity store
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * test(jans-cedarling): fix unit tests and add some refactor, extract function `build_entity_attributes`
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): fix copy-paste error.
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * feat(jans-cedarling): add to authorize check `execute_authorize` with principal `Jans::Role`
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * test(jans-cedarling): fix python unit tests
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * feat(jans-cedarling): add to python bindings `AuthorizeResult` field person and role
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * feat(jans-cedarling): add parsing `Jans::Role` only if field present in JWT token
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): change default search Role to the Userinfo token
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): add #[allow(dead_code)] in test case
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): add parse yaml using config
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * test(jans-cedarling): refactor current unit test `success_test_json` to be more readable
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * test(jans-cedarling): move `success_test_json` to own file
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): add config yaml file for testing `policy-store_ok_2.yaml`
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * test(jans-cedarling): add test case on check authorization request, positive and negative
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): fix python binding result of authorize for role
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * docs(jans-cedarling): add update to documentation related to adding role check on authorization request
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * test(jans-cedarling): fix python tests
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): fix copy-paste comment about YAML usage
    
    also added text
    `Mostly used only for testing purposes.`
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): refactor code to be more readable,
    
     add match statement in function `create_role_entities`
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): remove cloning the entity_uid in function `create_entity`
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): fix the markdown file using linter
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * docs(jans-cedarling): add information about minimum supported `cedar-policy schema`
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): update pyo3 to latest
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    * chore(jans-cedarling): remove outdated comment
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>
    
    ---------
    
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>

--- jans-cedarling/cedarling/src/common/cedar_schema.rs
+++ jans-cedarling/cedarling/src/common/cedar_schema.rs
@@ -14,9 +14,9 @@ pub(crate) mod cedar_json;
 /// content_type is one of cedar or cedar-json#[derive(Debug, Clone, serde::Deserialize)]
 #[derive(Debug, Clone, serde::Deserialize)]
 struct EncodedSchema {
-    pub encoding : super::Encoding,
-    pub content_type : super::ContentType,
-    pub body : String,
+    pub encoding: super::Encoding,
+    pub content_type: super::ContentType,
+    pub body: String,
 }
 
 /// Intermediate struct to handle both kinds of cedar_schema values.
@@ -29,7 +29,7 @@ struct EncodedSchema {
 #[serde(untagged)]
 enum MaybeEncoded {
     Plain(String),
-    Tagged(EncodedSchema)
+    Tagged(EncodedSchema),
 }
 
 /// Box that holds the [`cedar_policy::Schema`] and
@@ -50,31 +50,31 @@ impl PartialEq for CedarSchema {
         let self_principals = self.schema.principals().collect::<HashSet<_>>();
         let other_principals = other.schema.principals().collect::<HashSet<_>>();
         if self_principals != other_principals {
-            return false
+            return false;
         }
 
         let self_resources = self.schema.resources().collect::<HashSet<_>>();
         let other_resources = other.schema.resources().collect::<HashSet<_>>();
         if self_resources != other_resources {
-            return false
+            return false;
         }
 
         let self_action_groups = self.schema.action_groups().collect::<HashSet<_>>();
         let other_action_groups = other.schema.action_groups().collect::<HashSet<_>>();
         if self_action_groups != other_action_groups {
-            return false
+            return false;
         }
 
         let self_entity_types = self.schema.entity_types().collect::<HashSet<_>>();
         let other_entity_types = other.schema.entity_types().collect::<HashSet<_>>();
         if self_entity_types != other_entity_types {
-            return false
+            return false;
         }
 
         let self_actions = self.schema.actions().collect::<HashSet<_>>();
         let other_actions = other.schema.actions().collect::<HashSet<_>>();
         if self_actions != other_actions {
-            return false
+            return false;
         }
 
         // and this only checks the schema anyway
@@ -83,15 +83,15 @@ impl PartialEq for CedarSchema {
 }
 
 impl<'de> serde::Deserialize<'de> for CedarSchema {
-    fn deserialize<D : serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error>
-    {
+    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
         //  Read the next thing as either a String or a Map, using the MaybeEncoded enum to distinguish
-        let encoded_schema = match <MaybeEncoded as serde::Deserialize>::deserialize(deserializer)? {
-            MaybeEncoded::Plain(body) => EncodedSchema{
+        let encoded_schema = match <MaybeEncoded as serde::Deserialize>::deserialize(deserializer)?
+        {
+            MaybeEncoded::Plain(body) => EncodedSchema {
                 // These are the default if the encoding is not specified.
                 encoding: super::Encoding::Base64,
                 content_type: super::ContentType::CedarJson,
-                body
+                body,
             },
             MaybeEncoded::Tagged(encoded_schema) => encoded_schema,
         };
@@ -101,12 +101,20 @@ impl<'de> serde::Deserialize<'de> for CedarSchema {
             super::Encoding::Base64 => {
                 use base64::prelude::*;
                 let buf = BASE64_STANDARD.decode(encoded_schema.body).map_err(|err| {
-                    serde::de::Error::custom(format!("{}: {}", deserialize::ParseCedarSchemaSetMessage::Base64, err))
+                    serde::de::Error::custom(format!(
+                        "{}: {}",
+                        deserialize::ParseCedarSchemaSetMessage::Base64,
+                        err
+                    ))
                 })?;
                 String::from_utf8(buf).map_err(|err| {
-                    serde::de::Error::custom(format!("{}: {}", deserialize::ParseCedarSchemaSetMessage::Utf8, err))
+                    serde::de::Error::custom(format!(
+                        "{}: {}",
+                        deserialize::ParseCedarSchemaSetMessage::Utf8,
+                        err
+                    ))
                 })?
-            }
+            },
         };
 
         // Need both of these because CedarSchema wants both.
@@ -114,42 +122,62 @@ impl<'de> serde::Deserialize<'de> for CedarSchema {
             super::ContentType::Cedar => {
                 // parse cedar policy from the cedar representation
                 // TODO must log warnings or something
-                let (schema_fragment, _warning) = cedar_policy::SchemaFragment::from_cedarschema_str(&decoded_body)
-                    .map_err(|err| {
-                        serde::de::Error::custom(format!("{}: {}", deserialize::ParseCedarSchemaSetMessage::Parse, err))
-                    })?;
+                let (schema_fragment, _warning) =
+                    cedar_policy::SchemaFragment::from_cedarschema_str(&decoded_body).map_err(
+                        |err| {
+                            serde::de::Error::custom(format!(
+                                "{}: {}",
+                                deserialize::ParseCedarSchemaSetMessage::Parse,
+                                err
+                            ))
+                        },
+                    )?;
 
                 // urgh now recreate the json representation
-                let json_string = schema_fragment.to_json_string()
-                    .map_err(|err| {
-                        serde::de::Error::custom(format!("{}: {}", deserialize::ParseCedarSchemaSetMessage::CedarSchemaJsonFormat, err))
-                    })?;
+                let json_string = schema_fragment.to_json_string().map_err(|err| {
+                    serde::de::Error::custom(format!(
+                        "{}: {}",
+                        deserialize::ParseCedarSchemaSetMessage::CedarSchemaJsonFormat,
+                        err
+                    ))
+                })?;
 
                 (schema_fragment, json_string)
-            }
+            },
             super::ContentType::CedarJson => {
                 // parse cedar policy from the json representation
                 let schema_fragment = cedar_policy::SchemaFragment::from_json_str(&decoded_body)
                     .map_err(|err| {
-                        serde::de::Error::custom(format!("{}: {}", deserialize::ParseCedarSchemaSetMessage::CedarSchemaJsonFormat, err))
+                        serde::de::Error::custom(format!(
+                            "{}: {}",
+                            deserialize::ParseCedarSchemaSetMessage::CedarSchemaJsonFormat,
+                            err
+                        ))
                     })?;
                 (schema_fragment, decoded_body)
-            }
+            },
         };
 
         // create the schema
         let fragment_iter = std::iter::once(schema_fragment);
         let schema = cedar_policy::Schema::from_schema_fragments(fragment_iter.into_iter())
             .map_err(|err| {
-                serde::de::Error::custom(format!("{}: {}", deserialize::ParseCedarSchemaSetMessage::Parse, err))
+                serde::de::Error::custom(format!(
+                    "{}: {}",
+                    deserialize::ParseCedarSchemaSetMessage::Parse,
+                    err
+                ))
             })?;
 
-        let json = serde_json::from_str(&json_string)
-            .map_err(|err| {
-                serde::de::Error::custom(format!("{}: {}", deserialize::ParseCedarSchemaSetMessage::CedarSchemaJsonFormat, err ))
-            })?;
+        let json = serde_json::from_str(&json_string).map_err(|err| {
+            serde::de::Error::custom(format!(
+                "{}: {}",
+                deserialize::ParseCedarSchemaSetMessage::CedarSchemaJsonFormat,
+                err
+            ))
+        })?;
 
-        Ok(CedarSchema{schema, json})
+        Ok(CedarSchema { schema, json })
     }
 }
 
@@ -160,7 +188,7 @@ mod deserialize {
         Base64,
         #[error("unable to unmarshal cedar policy schema json to the structure")]
         CedarSchemaJsonFormat,
-        #[error("unable to parse cedar policy schema json")]
+        #[error("unable to parse cedar policy schema")]
         Parse,
         #[error("invalid utf8 detected while decoding cedar policy")]
         Utf8,
@@ -168,6 +196,8 @@ mod deserialize {
 
     #[cfg(test)]
     mod tests {
+        use test_utils::assert_eq;
+
         use crate::common::policy_store::PolicyStore;
 
         use super::*;
@@ -194,17 +224,24 @@ mod deserialize {
 
         #[test]
         fn test_readable_yaml_ok() {
-            static YAML_POLICY_STORE: &str = include_str!("../../../test_files/policy-store_readable.yaml");
+            static YAML_POLICY_STORE: &str =
+                include_str!("../../../test_files/policy-store_readable.yaml");
             let yaml_policy_result = serde_yml::from_str::<PolicyStore>(YAML_POLICY_STORE);
-            assert!(yaml_policy_result.is_ok(), "{:?}", yaml_policy_result.unwrap_err());
+            assert!(
+                yaml_policy_result.is_ok(),
+                "{:?}",
+                yaml_policy_result.unwrap_err()
+            );
         }
 
         #[test]
         fn test_readable_yaml_identical_readable_json() {
-            static YAML_POLICY_STORE: &str = include_str!("../../../test_files/policy-store_readable.yaml");
+            static YAML_POLICY_STORE: &str =
+                include_str!("../../../test_files/policy-store_readable.yaml");
             let yaml_policy_result = serde_yml::from_str::<PolicyStore>(YAML_POLICY_STORE);
 
-            static JSON_POLICY_STORE: &str = include_str!("../../../test_files/policy-store_readable.json");
+            static JSON_POLICY_STORE: &str =
+                include_str!("../../../test_files/policy-store_readable.json");
             let json_policy_result = serde_yml::from_str::<PolicyStore>(JSON_POLICY_STORE);
 
             assert_eq!(yaml_policy_result.unwrap(), json_policy_result.unwrap());
@@ -212,6 +249,7 @@ mod deserialize {
 
         // In fact this fails because of limitations in cedar_policy::Policy::from_json
         // see PolicyContentType
+        #[allow(dead_code)]
         fn test_both_ok() {
             static POLICY_STORE_RAW: &str =
                 include_str!("../../../test_files/policy-store_blobby.json");
@@ -230,7 +268,10 @@ mod deserialize {
             let policy_result = serde_json::from_str::<PolicyStore>(POLICY_STORE_RAW);
             let err = policy_result.unwrap_err();
             let msg = err.to_string();
-            assert!(msg.contains(&ParseCedarSchemaSetMessage::Base64.to_string()), "{err:?}");
+            assert!(
+                msg.contains(&ParseCedarSchemaSetMessage::Base64.to_string()),
+                "{err:?}"
+            );
         }
 
         #[test]
@@ -241,7 +282,10 @@ mod deserialize {
             let policy_result = serde_json::from_str::<PolicyStore>(POLICY_STORE_RAW);
             let err = policy_result.unwrap_err();
             let msg = err.to_string();
-            assert!(msg.contains("unable to unmarshal cedar policy schema json"), "{err:?}");
+            assert!(
+                msg.contains("unable to unmarshal cedar policy schema json"),
+                "{err:?}"
+            );
         }
 
         #[test]
@@ -253,7 +297,7 @@ mod deserialize {
             let err_msg = policy_result.unwrap_err().to_string();
             assert_eq!(
                 err_msg,
-                "unable to parse cedar policy schema json: failed to resolve type: User_TypeNotExist at line 32 column 1"
+                "unable to parse cedar policy schema: failed to resolve type: User_TypeNotExist at line 32 column 1"
             );
         }
     }
