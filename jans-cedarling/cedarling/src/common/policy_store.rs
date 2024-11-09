655191324e694b380e8f257a4ef9c2e36c1d7fdb
rmarinn <34529290+rmarinn@users.noreply.github.com>
2024-11-10 01:20:49 +0800

    feat(jans-cedarling): implement parser for new Claim Mapping field
    - implement a `ClaimMapping` struct for the new `claim_mapping` field in
      the policy store
    - implement deserialize for the `ClaimMapping` struct
    
    Signed-off-by: rmarinn <34529290+rmarinn@users.noreply.github.com>

--- jans-cedarling/cedarling/src/common/policy_store.rs
+++ jans-cedarling/cedarling/src/common/policy_store.rs
@@ -5,6 +5,7 @@
  * Copyright (c) 2024, Gluu, Inc.
  */
 
+mod claim_mapping;
 #[cfg(test)]
 mod test;
 
@@ -22,7 +23,7 @@ pub struct AgamaPolicyStore {
     #[allow(dead_code)]
     pub cedar_version: Version,
 
-    pub policy_stores : HashMap<String,PolicyStore>,
+    pub policy_stores: HashMap<String, PolicyStore>,
 }
 
 /// Represents the store of policies used for JWT validation and policy evaluation in Cedarling.
@@ -58,7 +59,7 @@ pub struct PolicyStore {
     pub trusted_issuers: Option<Vec<TrustedIssuer>>,
 
     #[allow(dead_code)]
-    pub identity_source: Option<HashMap<String,IdentitySource>>,
+    pub identity_source: Option<HashMap<String, IdentitySource>>,
 }
 
 /// Represents a trusted issuer that can provide JWTs.
@@ -281,12 +282,13 @@ where
             // Check for "v" prefix
             let version = version.strip_prefix('v').unwrap_or(&version);
 
-            let version = Version::parse(version)
-                .map_err(|e| serde::de::Error::custom(format!("error parsing cedar version :{}", e)))?;
+            let version = Version::parse(version).map_err(|e| {
+                serde::de::Error::custom(format!("error parsing cedar version :{}", e))
+            })?;
 
             Ok(Some(version))
-        }
-        None => Ok(None)
+        },
+        None => Ok(None),
     }
 }
 
@@ -445,7 +447,7 @@ where
             cedar_policy::Policy::parse(Some(PolicyId::new(id)), decoded_body).map_err(|err| {
                 serde::de::Error::custom(format!("{}: {err}", ParsePolicySetMessage::HumanReadable))
             })?
-        }
+        },
     };
 
     Ok(policy)
---
+++ jans-cedarling/cedarling/src/common/policy_store/claim_mapping.rs
@@ -0,0 +1,183 @@
+use serde::{de, Deserialize};
+use serde_json::Value;
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq)]
+#[allow(dead_code)]
+pub enum ClaimMapping {
+    Regex {
+        r#type: String,
+        regex_expression: String,
+        fields: HashMap<String, RegexField>,
+    },
+    Json {
+        r#type: String,
+    },
+}
+
+impl<'de> Deserialize<'de> for ClaimMapping {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let value = Value::deserialize(deserializer)?;
+
+        let parser = value
+            .get("parser")
+            .and_then(|v| v.as_str())
+            .ok_or_else(|| de::Error::missing_field("parser"))?;
+
+        match parser {
+            "regex" => {
+                let r#type = value
+                    .get("type")
+                    .and_then(|v| v.as_str())
+                    .ok_or(de::Error::missing_field("type"))?
+                    .to_string();
+
+                let regex_expression = value
+                    .get("regex_expression")
+                    .and_then(|v| v.as_str())
+                    .ok_or(de::Error::missing_field("regex_expression"))?
+                    .to_string();
+
+                let mut fields = HashMap::new();
+                if let Some(obj) = value.as_object() {
+                    for (key, val) in obj.iter() {
+                        if key != "parser" && key != "type" && key != "regex_expression" {
+                            let field = serde_json::from_value::<RegexField>(val.clone())
+                                .map_err(de::Error::custom)?;
+                            fields.insert(key.clone(), field);
+                        }
+                    }
+                }
+                Ok(ClaimMapping::Regex {
+                    r#type,
+                    regex_expression,
+                    fields,
+                })
+            },
+            "json" => {
+                let r#type = value
+                    .get("type")
+                    .and_then(|v| v.as_str())
+                    .ok_or_else(|| de::Error::missing_field("type"))?
+                    .to_string();
+
+                Ok(ClaimMapping::Json { r#type })
+            },
+            _ => Err(de::Error::custom("unknown parser type")),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct RegexField {
+    pub attr: String,
+    pub r#type: String,
+}
+
+#[cfg(test)]
+mod test {
+    use super::ClaimMapping;
+    use crate::common::policy_store::claim_mapping::RegexField;
+    use serde_json::json;
+    use std::collections::HashMap;
+    use test_utils::assert_eq;
+
+    #[test]
+    /// Tests if a token entity metadata with a regex parser can be parsed
+    /// from a JSON string
+    fn can_parse_token_entity_metadata_regex() {
+        // Setup expected output
+        let mut expected_fields = HashMap::new();
+        expected_fields.insert(
+            "UID".to_string(),
+            RegexField {
+                attr: "uid".to_string(),
+                r#type: "string".to_string(),
+            },
+        );
+        expected_fields.insert(
+            "DOMAIN".to_string(),
+            RegexField {
+                attr: "domain".to_string(),
+                r#type: "string".to_string(),
+            },
+        );
+        let expected = ClaimMapping::Regex {
+            r#type: "Acme::Email".to_string(),
+            regex_expression: r#"^(?P<UID>[^@]+)@(?P<DOMAIN>.+)$"#.to_string(),
+            fields: expected_fields,
+        };
+
+        // Setup JSON
+        let claim_mapping_json = json!({
+            "parser": "regex",
+            "type": "Acme::Email",
+            "regex_expression": "^(?P<UID>[^@]+)@(?P<DOMAIN>.+)$",
+            "UID": { "attr": "uid", "type": "string" },
+            "DOMAIN": { "attr": "domain", "type": "string" },
+        })
+        .to_string();
+
+        // Parse JSON
+        let parsed = serde_json::from_str::<ClaimMapping>(&claim_mapping_json)
+            .expect("should parse token entity metadata");
+
+        // Assert if the JSON got parsed correctly
+        assert_eq!(
+            parsed, expected,
+            "Expected the Claim Mapping to be parsed correctly: {:?}",
+            parsed
+        );
+    }
+
+    #[test]
+    /// Tests if a token entity metadata with a JSON parser can be parsed
+    /// from a JSON string
+    fn can_parse_token_entity_metadata_json() {
+        // Setup expected output
+        let expected = ClaimMapping::Json {
+            r#type: "Acme::Dolphin".to_string(),
+        };
+
+        // Setup JSON
+        let claim_mapping_json = json!({
+            "parser": "json",
+            "type": "Acme::Dolphin",
+        })
+        .to_string();
+
+        // Parse JSON
+        let parsed = serde_json::from_str::<ClaimMapping>(&claim_mapping_json)
+            .expect("should parse token entity metadata");
+
+        // Assert if the JSON got parsed correctly
+        assert_eq!(
+            parsed, expected,
+            "Expected the Claim Mapping to be parsed correctly: {:?}",
+            parsed
+        );
+    }
+
+    #[test]
+    /// Tests if an error is thrown for an unknown parser type
+    fn errors_on_unkown_parser_type() {
+        // Setup JSON
+        let claim_mapping_json = json!({
+            "parser": "",
+            "type": "Acme::Dolphin",
+        })
+        .to_string();
+
+        // Parse JSON
+        let parsed = serde_json::from_str::<ClaimMapping>(&claim_mapping_json);
+
+        assert!(
+            matches!(parsed, Err(ref e) if e.to_string().contains("unknown parser type")),
+            "Expected an error when encountering an unknown parser: {:?}",
+            parsed
+        );
+    }
+}
