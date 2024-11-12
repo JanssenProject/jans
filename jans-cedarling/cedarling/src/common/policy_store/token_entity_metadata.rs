25f406df86eb1c4ed832656bddfcf4001f1b64ef
rmarinn <34529290+rmarinn@users.noreply.github.com>
2024-11-12 15:11:38 +0800

    test(jans-cedarling): add parser tests for TokenEntityMetadata
    - add test for parsing JSON into TokenEntityMetadata
    - add test for parsing YAML into TokenEntityMetadata
    
    Signed-off-by: rmarinn <34529290+rmarinn@users.noreply.github.com>

--- jans-cedarling/cedarling/src/common/policy_store/token_entity_metadata.rs
+++ jans-cedarling/cedarling/src/common/policy_store/token_entity_metadata.rs
@@ -13,3 +13,67 @@ pub struct TokenEntityMetadata {
     #[serde(deserialize_with = "parse_option_hashmap", default)]
     pub claim_mapping: Option<HashMap<String, ClaimMapping>>,
 }
+
+#[cfg(test)]
+mod test {
+    use super::TokenEntityMetadata;
+    use serde_json::json;
+
+    #[test]
+    fn can_parse_from_json() {
+        let json = json!({});
+        let parsed = serde_json::from_value::<TokenEntityMetadata>(json)
+            .expect("Failed to parse an empty JSON object into TokenEntityMetadata");
+        assert_eq!(
+            parsed,
+            TokenEntityMetadata::default(),
+            "Expected empty JSON to be parsed into default TokenEntityMetadata"
+        );
+
+        let json = json!({
+            "user_id": "sub",
+            "role_mapping": "",
+        });
+        let parsed = serde_json::from_value::<TokenEntityMetadata>(json).expect(
+            "Failed to parse JSON object with user_id and role_mapping into TokenEntityMetadata",
+        );
+        assert_eq!(
+            parsed, 
+            TokenEntityMetadata { 
+                user_id: Some("sub".into()), 
+                role_mapping: None, 
+                claim_mapping: None 
+            }, 
+            "Expected JSON with user_id and empty role_mapping to be parsed into TokenEntityMetadata"
+        );
+    }
+
+    #[test]
+    fn can_parse_from_yaml() {
+        let yaml = "";
+        let parsed = serde_yml::from_str::<TokenEntityMetadata>(yaml)
+            .expect("Failed to parse an empty YAML object into TokenEntityMetadata");
+        assert_eq!(
+            parsed,
+            TokenEntityMetadata::default(),
+            "Expected empty YAML to be parsed into default TokenEntityMetadata"
+        );
+        
+        let yaml = "
+            user_id: 'sub'
+            role_mapping: ''
+        ";
+        let parsed = serde_yml::from_str::<TokenEntityMetadata>(yaml).expect(
+            "Failed to parse YAML object with user_id and role_mapping into TokenEntityMetadata",
+        );
+        assert_eq!(
+            parsed, 
+            TokenEntityMetadata { 
+                user_id: Some("sub".into()), 
+                role_mapping: None, 
+                claim_mapping: None 
+            }, 
+            "Expected YAML with user_id and empty role_mapping to be parsed into TokenEntityMetadata"
+        );
+    }
+}
