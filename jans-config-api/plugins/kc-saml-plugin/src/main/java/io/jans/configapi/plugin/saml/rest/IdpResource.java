e7c519eba252ca2fdf1612424096d4e1fc2fb0bf
pujavs <43700552+pujavs@users.noreply.github.com>
2024-11-15 19:36:28 +0530

    fix(config-api): adding missing scope in spec and udated example of search field (#10156)
    * fix(config-api): missing scope in spec
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    * fix(config-api): missing scope in spec
    
    Signed-off-by: pujavs <pujas.works@gmail.com>
    
    ---------
    
    Signed-off-by: pujavs <pujas.works@gmail.com>

--- jans-config-api/plugins/kc-saml-plugin/src/main/java/io/jans/configapi/plugin/saml/rest/IdpResource.java
+++ jans-config-api/plugins/kc-saml-plugin/src/main/java/io/jans/configapi/plugin/saml/rest/IdpResource.java
@@ -84,7 +84,7 @@ public class IdpResource extends BaseResource {
             @Parameter(description = "The 1-based index of the first query result") @DefaultValue(ApiConstants.DEFAULT_LIST_START_INDEX) @QueryParam(value = ApiConstants.START_INDEX) int startIndex,
             @Parameter(description = "Attribute whose value will be used to order the returned response") @DefaultValue(ApiConstants.INUM) @QueryParam(value = ApiConstants.SORT_BY) String sortBy,
             @Parameter(description = "Order in which the sortBy param is applied. Allowed values are \"ascending\" and \"descending\"") @DefaultValue(ApiConstants.ASCENDING) @QueryParam(value = ApiConstants.SORT_ORDER) String sortOrder,
-            @Parameter(description = "Field and value pair for seraching", examples = @ExampleObject(name = "Field value example", value = "applicationType=web,persistClientAuthorizations=true")) @DefaultValue("") @QueryParam(value = ApiConstants.FIELD_VALUE_PAIR) String fieldValuePair)
+            @Parameter(description = "Field and value pair for seraching", examples = @ExampleObject(name = "Field value example", value = "displayName=saml-idp,realm=jans")) @DefaultValue("") @QueryParam(value = ApiConstants.FIELD_VALUE_PAIR) String fieldValuePair)
              {
         if (log.isDebugEnabled()) {
             log.debug(
