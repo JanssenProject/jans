bbe9432b43f64e95124ac81d6bcf262e2e0f0d15
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2024-11-19 16:15:47 +0300

    fix(jans-auth): add Junti5 user authenticator and custom attributes tests
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>

--- jans-core/service/src/main/java/io/jans/service/UserAuthenticatorService.java
+++ jans-core/service/src/main/java/io/jans/service/UserAuthenticatorService.java
@@ -94,6 +94,9 @@ public class UserAuthenticatorService {
 					break;
 				}
 			}
+			if (userAuthenticatorList.getAuthenticators().size() == 0) {
+				user.setAuthenticator(null);
+			}
 		}
 
 		List<String> externalUidList = new ArrayList<>();
@@ -113,7 +116,11 @@ public class UserAuthenticatorService {
 				}
 			}
 			
-			user.setExternalUid(externalUidList.toArray(EMPTY_STRING_ARRAY));
+			if (externalUidList.size() == 0) {
+				user.setExternalUid(null);
+			} else {
+				user.setExternalUid(externalUidList.toArray(EMPTY_STRING_ARRAY));
+			}
 		}
 	}
 
@@ -125,6 +132,9 @@ public class UserAuthenticatorService {
 				if (type.equals(authenticator.getType())) {
 					it.remove();
 				}
+			}
+			if (userAuthenticatorList.getAuthenticators().size() == 0) {
+				user.setAuthenticator(null);
 			}
 		}
 
@@ -143,7 +153,11 @@ public class UserAuthenticatorService {
 				}
 			}
 			
-			user.setExternalUid(externalUidList.toArray(EMPTY_STRING_ARRAY));
+			if (externalUidList.size() == 0) {
+				user.setExternalUid(null);
+			} else {
+				user.setExternalUid(externalUidList.toArray(EMPTY_STRING_ARRAY));
+			}
 		}
 	}
 
