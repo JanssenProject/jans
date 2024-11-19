b6b89b64c76a5dbcc107b19b2042c807cd46a808
John Anderson <john@gluu.org>
2024-11-18 23:04:55 -0500

    feat(jans-cedarling-ruby): expose core cedarling version
    Signed-off-by: John Anderson <john@gluu.org>

--- jans-cedarling/bindings/cedarling_ruby/ext/cedarling/src/lib.rs
+++ jans-cedarling/bindings/cedarling_ruby/ext/cedarling/src/lib.rs
@@ -7,11 +7,20 @@
 
 extern crate cedarling;
 
+// this env var is set by build.rs
+const CEDARLING_VERSION: &str = env!("CEDARLING_VERSION");
+
+/// Return the current version of cedarling - not the current version of the gem.
+fn version_core() -> Result<String,magnus::Error> {
+    Ok(CEDARLING_VERSION.into())
+}
+
 #[magnus::init]
 fn init(ruby: &magnus::Ruby) -> Result<(), magnus::Error> {
     use magnus::{function, prelude::*};
 
     let module = ruby.define_module("Cedarling")?;
+    module.define_singleton_method("version_core", function!(version_core, 0))?;
 
     Ok(())
 }
--- jans-cedarling/bindings/cedarling_ruby/spec/cedarling_spec.rb
+++ jans-cedarling/bindings/cedarling_ruby/spec/cedarling_spec.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
+require 'json'
 
 RSpec.describe Cedarling do
-  it "has a version number" do
+  it "has a gem version number" do
     expect(Cedarling::VERSION).not_to be nil
+  end
+
+  it "has the core version number" do
+    expect(Cedarling::version_core).not_to be nil
+    expect(Cedarling::version_core).to match(/^\d+.\d+.\d+$/)
+  end
+
+  it "core version number matches actual core version" do
+    # fetch manifest data using cargo
+    json = JSON.parse `cargo metadata --format-version 1 --manifest-path ../../cedarling/Cargo.toml`, symbolize_names: true
+    cedarling_manifest_version = json[:packages].find{ _1[:name] == 'cedarling' }.dig(:version)
+    # compare to version from source
+    expect(cedarling_manifest_version).to eq(Cedarling::version_core)
   end
 end
