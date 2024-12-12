59fd83e1ca97f560f9376ae587bccbee3d904e8a
Oleh Bohzok <olehbozhok@gmail.com>
2024-12-12 19:30:03 +0200

    chore(jans-cedarling): add using serde Deserialize, Serialize for type LogLevel
    Signed-off-by: Oleh Bohzok <olehbozhok@gmail.com>

--- jans-cedarling/cedarling/src/log/log_level.rs
+++ jans-cedarling/cedarling/src/log/log_level.rs
@@ -5,13 +5,24 @@
  * Copyright (c) 2024, Gluu, Inc.
  */
 
-use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
-use std::fmt;
+use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 
 /// Log levels
 /// Fatal level is the highest, trace is lowest
-#[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Copy, Clone, Default)]
+#[derive(
+    Debug,
+    PartialEq,
+    PartialOrd,
+    Eq,
+    Ord,
+    Copy,
+    Clone,
+    Default,
+    derive_more::Display,
+    Deserialize,
+    Serialize,
+)]
 pub enum LogLevel {
     /// Fatal level
     FATAL = 5,
@@ -28,19 +39,6 @@ pub enum LogLevel {
     TRACE = 0,
 }
 
-impl LogLevel {
-    fn as_str(&self) -> &'static str {
-        match self {
-            LogLevel::FATAL => "FATAL",
-            LogLevel::ERROR => "ERROR",
-            LogLevel::WARN => "WARN",
-            LogLevel::INFO => "INFO",
-            LogLevel::DEBUG => "DEBUG",
-            LogLevel::TRACE => "TRACE",
-        }
-    }
-}
-
 impl FromStr for LogLevel {
     type Err = String;
 
@@ -54,51 +52,5 @@ impl FromStr for LogLevel {
             "TRACE" => Ok(LogLevel::TRACE),
             _ => Err(format!("Invalid log level: {}", s)),
         }
-    }
-}
-
-impl Serialize for LogLevel {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        serializer.serialize_str(self.as_str())
-    }
-}
-
-impl<'de> Deserialize<'de> for LogLevel {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        deserializer.deserialize_str(LogLevelVisitor)
-    }
-}
-
-// Custom Visitor for LogLevel deserialization
-//
-// we use custom visitor to avoid error: `invalid type: string "DEBUG", expected a borrowed string`
-// so we implement deserialization for `&str` and for `String`
-struct LogLevelVisitor;
-
-impl de::Visitor<'_> for LogLevelVisitor {
-    type Value = LogLevel;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str("a log level string (FATAL, ERROR, WARN, INFO, DEBUG, or TRACE)")
-    }
-
-    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-    where
-        E: de::Error,
-    {
-        LogLevel::from_str(v).map_err(de::Error::custom)
-    }
-
-    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
-    where
-        E: de::Error,
-    {
-        LogLevel::from_str(&v).map_err(de::Error::custom)
     }
 }
