00af5078a0c3b7355f0f8c0008313a5eecd734e4
moabu <47318409+moabu@users.noreply.github.com>
2024-12-17 11:10:16 +0300

    ci: fix packaging builds

--- automation/packaging/rpm/suse15/checksum.sh
+++ automation/packaging/rpm/suse15/checksum.sh
@@ -5,14 +5,14 @@ REL=$(echo "%VERSION%" | sed "s/^${VERSION}//g" | sed "s/^-//g")
 if [ -z "$REL" ]; then
         RELEASE="suse15"
 else
-        RELEASE="$REL.suse15"
+        RELEASE="$REL-suse15"
 fi
 pushd rpmbuild/RPMS/x86_64
 echo "VERSION: $VERSION"
 echo "RELEASE: $RELEASE"
 if [[ $VERSION == "0.0.0" ]]; then
   echo "Creating checksum file for nightly build"
-  sha256sum jans-"$VERSION"-"$RELEASE".x86_64.rpm > jans-0.0.0-nightly-suse15.x86_64.rpm.sha256sum
+  sha256sum jans-"$VERSION"-"$RELEASE".x86_64.rpm > jans-0.0.0-nightly-"$RELEASE".x86_64.rpm.sha256sum
 else
   echo "Creating checksum file for release build"
   sha256sum jans-"$VERSION"-"$RELEASE".x86_64.rpm > jans-"$VERSION"-"$RELEASE".x86_64.rpm.sha256sum
