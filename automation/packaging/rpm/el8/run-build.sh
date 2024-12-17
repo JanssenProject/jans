a9f1122e39328a7ee0b0f27b6a301d0b28fe77d3
moabu <47318409+moabu@users.noreply.github.com>
2024-12-17 11:10:16 +0300

    ci: fix packaging builds
    Signed-off-by: Yuriy Movchan <Yuriy.Movchan@gmail.com>

--- automation/packaging/rpm/el8/run-build.sh
+++ automation/packaging/rpm/el8/run-build.sh
@@ -7,7 +7,7 @@ sed -i "s/%VER%/$VERSION/g" jans.spec
 if [ -z "$REL" ]; then
         RELEASE="el8"
 else
-        RELEASE="$REL.el8"
+        RELEASE="$REL-el8"
 fi
 sed -i "s/%RELEASE%/$RELEASE/g" jans.spec
 rpmbuild_path="$current_dir/rpmbuild"
