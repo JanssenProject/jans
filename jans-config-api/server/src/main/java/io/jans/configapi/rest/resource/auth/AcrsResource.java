80760061ef811f93d118062f6a70d9eb88b80d2f
pujavs <pujas.works@gmail.com>
2025-01-02 19:01:22 +0530

    fix(config-api): setting agama flow as auth method
    Signed-off-by: pujavs <pujas.works@gmail.com>

--- jans-config-api/server/src/main/java/io/jans/configapi/rest/resource/auth/AcrsResource.java
+++ jans-config-api/server/src/main/java/io/jans/configapi/rest/resource/auth/AcrsResource.java
@@ -31,6 +31,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.Optional;
@@ -203,7 +204,7 @@ public class AcrsResource extends ConfigBaseResource {
             }
             log.info(" agamaAcr:{},", agamaAcr);
 
-            if (keys != null && !keys.isEmpty() && keys.contains(agamaAcr)) {
+            if (!keys.isEmpty() && keys.contains(agamaAcr)) {
                 log.debug(" keys.contains(agamaAcr):{},", keys.contains(agamaAcr));
                 isValid = true;
             }
@@ -214,24 +215,24 @@ public class AcrsResource extends ConfigBaseResource {
 
     private Set<String> getDirectLaunchFlows(List<Deployment> agamaDeploymentList) {
         log.info(" agamaDeploymentList:{}", agamaDeploymentList);
-        Set<String> keys = null;
+        Set<String> keys = new HashSet<>();
         List<String> noDirectLaunchFlows = new ArrayList<>();
         if (agamaDeploymentList == null || agamaDeploymentList.isEmpty()) {
             return keys;
         }
         for (Deployment deployment : agamaDeploymentList) {
-            log.info("Agama deployment:{},", deployment);
+            log.info("Agama deployment:{}, deployment.getId():{},", deployment, deployment.getId());
             if (deployment.getDetails() != null && deployment.getDetails().getFlowsError() != null) {
-                keys = deployment.getDetails().getFlowsError().keySet();
-                log.info(" Agama flow keys:{},", keys);
+                keys.addAll(deployment.getDetails().getFlowsError().keySet());
+                log.info("\n Agama flow keys:{},", keys);
 
-                if (deployment.getDetails().getProjectMetadata() != null) {
+                if (deployment.getDetails().getProjectMetadata() != null && deployment.getDetails().getProjectMetadata().getNoDirectLaunchFlows()!=null) {
                     noDirectLaunchFlows.addAll(deployment.getDetails().getProjectMetadata().getNoDirectLaunchFlows());
                 }
             }
         }
-        log.info("All deployed agama keys:{}, noDirectLaunchFlows:{}", keys, noDirectLaunchFlows);
-        if (keys != null && !keys.isEmpty() && noDirectLaunchFlows != null) {
+        log.info("\n All deployed agama keys:{}, noDirectLaunchFlows:{}", keys, noDirectLaunchFlows);
+        if (!keys.isEmpty() && !noDirectLaunchFlows.isEmpty()) {
             keys.removeAll(noDirectLaunchFlows);
         }
         log.info("Final agama main flow keys:{}", keys);
