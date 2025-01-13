447d8860a0fe371cb90ec8b25d20149fe304252b
John Anderson <john@gluu.org>
2025-01-13 08:27:22 -0500

    chore(jans-cedarling): SparKV add copyright to tests files
    Signed-off-by: John Anderson <john@gluu.org>

--- jans-cedarling/sparkv/src/test_json_value.rs
+++ jans-cedarling/sparkv/src/test_json_value.rs
@@ -1,3 +1,8 @@
+// This software is available under the Apache-2.0 license.
+// See https://www.apache.org/licenses/LICENSE-2.0.txt for full text.
+//
+// Copyright (c) 2024, Gluu, Inc.
+
 use serde_json;
 use crate::Config;
 use crate::SparKV;
