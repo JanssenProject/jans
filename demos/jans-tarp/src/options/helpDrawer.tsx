97ba63c0a9fe2bbd815ea5b3085a1279b53ef782
Arnab Dutta <arnab.bdutta@gmail.com>
2025-01-23 11:27:52 +0530

    fix: adjust help drawer to various screen sizes
    Signed-off-by: Arnab Dutta <arnab.bdutta@gmail.com>

--- demos/jans-tarp/src/options/helpDrawer.tsx
+++ demos/jans-tarp/src/options/helpDrawer.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 
 export default function HelpDrawer({ isOpen, handleDrawer }) {
   const [open, setOpen] = React.useState(isOpen);
-
+  const [width, setWidth] = React.useState((window.innerWidth * 2)/3 )
   React.useEffect(() => {
     handleDrawer(isOpen)
     setOpen(isOpen);
@@ -20,104 +17,81 @@ export default function HelpDrawer({ isOpen, handleDrawer }) {
   };
 
   const DrawerList = (
-    <>
-      <Box sx={{ width: 1126 }} role="presentation">
-        <Card sx={{ maxWidth: 1126 }}>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Step 1
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Click on 'Add Client' button for Dynamic Client Registration on authentication server.
-            </Typography>
-          </CardContent>
-          <CardMedia
-            sx={{ height: 460 }}
-            image={'tarpDocs1.png'}
-          />
-        </Card>
-      </Box>
-      <Box sx={{ width: 1126 }} role="presentation">
-        <Card sx={{ maxWidth: 1126 }}>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Step 2
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              On registration form enter details and register.
-            </Typography>
-          </CardContent>
-          <CardMedia
-            sx={{ height: 617 }}
-            image={'tarpDocs2.png'}
-          />
-        </Card>
-      </Box>
-      <Box sx={{ width: 1126 }} role="presentation">
-        <Card sx={{ maxWidth: 1126 }}>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Step 3 (Optional: For Cedarlig authorization testing)
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Add Cedarling bootstrap configuration on <b>Cedarling</b> tab.
-            </Typography>
-          </CardContent>
-          <CardMedia
-            sx={{ height: 294 }}
-            image={'tarpDocs3.png'}
-          />
-        </Card>
-      </Box>
-      <Box sx={{ width: 1126 }} role="presentation">
-        <Card sx={{ maxWidth: 1126 }}>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Step 4
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Start Authentication code flow.
-            </Typography>
-          </CardContent>
-          <CardMedia
-            sx={{ height: 294 }}
-            image={'tarpDocs4.png'}
-          />
-        </Card>
-      </Box>
-      <Box sx={{ width: 1126 }} role="presentation">
-        <Card sx={{ maxWidth: 1126 }}>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Step 5
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Enter required details before starting Authentication Code flow.
-            </Typography>
-          </CardContent>
-          <CardMedia
-            sx={{ height: 653 }}
-            image={'tarpDocs5.png'}
-          />
-        </Card>
-      </Box>
-      <Box sx={{ width: 739 }} role="presentation">
-        <Card sx={{ maxWidth: 739 }}>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Step 6 (Optional: For Cedarlig authorization testing)
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              After authentication, test Cedarling Authorization decision using following request form.
-            </Typography>
-          </CardContent>
-          <CardMedia
-            sx={{ height: 767 }}
-            image={'tarpDocs6.png'}
-          />
-        </Card>
-      </Box>
-    </>
+
+    <Box sx={{ width: width }} role="presentation">
+      <div style={{ padding: "20px" }}>
+        <Typography gutterBottom variant="h5" component="div">
+          Step 1
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Click on 'Add Client' button for Dynamic Client Registration on authentication server.
+        </Typography>
+      </div>
+      <div className="image-container">
+        <img id="dynamic-image" src={'tarpDocs1.png'} />
+      </div>
+
+      <div style={{ padding: "20px" }}>
+        <Typography gutterBottom variant="h5" component="div">
+          Step 2
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          On registration form enter details and register.
+        </Typography>
+      </div>
+      <div className="image-container">
+        <img id="dynamic-image" src={'tarpDocs2.png'} />
+      </div>
+
+      <div style={{ padding: "20px" }}>
+        <Typography gutterBottom variant="h5" component="div">
+          Step 3 (Optional: For Cedarlig authorization testing)
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Add Cedarling bootstrap configuration on <b>Cedarling</b> tab.
+        </Typography>
+      </div>
+      <div className="image-container">
+        <img id="dynamic-image" src={'tarpDocs3.png'} />
+      </div>
+
+      <div style={{ padding: "20px" }}>
+        <Typography gutterBottom variant="h5" component="div">
+          Step 4
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Start Authentication code flow.
+        </Typography>
+      </div>
+      <div className="image-container">
+        <img id="dynamic-image" src={'tarpDocs4.png'} />
+      </div>
+
+      <div style={{ padding: "20px" }}>
+        <Typography gutterBottom variant="h5" component="div">
+          Step 5
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Enter required details before starting Authentication Code flow.
+        </Typography>
+      </div>
+      <div className="image-container">
+        <img id="dynamic-image" src={'tarpDocs5.png'} />
+      </div>
+
+      <div style={{ padding: "20px" }}>
+        <Typography gutterBottom variant="h5" component="div">
+          Step 6 (Optional: For Cedarlig authorization testing)
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          After authentication, test Cedarling Authorization decision using following request form.
+        </Typography>
+      </div>
+      <div className="image-container">
+        <img id="dynamic-image" src={'tarpDocs6.png'} />
+      </div>
+    </Box>
+
   );
 
   return (
