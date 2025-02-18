0a6d218ac421f54697a58338d6a4239849e7106c
duttarnab <arnab.bdutta@gmail.com>
2025-02-18 14:24:43 +0530

    feat: resolving clippy error about missing  function
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>

--- jans-cedarling/bindings/cedarling_uniffi/src/bin/uniffi-bindgen.rs
+++ jans-cedarling/bindings/cedarling_uniffi/src/bin/uniffi-bindgen.rs
@@ -2,7 +2,7 @@
 // See https://www.apache.org/licenses/LICENSE-2.0.txt for full text.
 //
 // Copyright (c) 2024, Gluu, Inc.
-#![cfg(not(target_arch = "wasm32"))]
+#[cfg(not(target_arch = "wasm32"))]
 
 fn main() {
     uniffi::uniffi_bindgen_main()
