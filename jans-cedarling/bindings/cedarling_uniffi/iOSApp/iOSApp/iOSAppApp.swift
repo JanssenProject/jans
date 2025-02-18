026ed0b765ff2a7960f2f52b7860278c564c92fc
Arnab Dutta <arnab.bdutta@gmail.com>
2025-02-18 21:57:31 +0530

    feat(jans-cedarling): create uniffi binding for cedarling with sample ios app using it (#10816)
    * feat: initial commit for ios binding
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: commiting remaining files
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: pushing uniffi-bindgen.rs file
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: adding Gluu's copyright and software license
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: adding android section in README
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: restoring python binding
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: update README and add fix to prevent WASM test failure
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: correct typo in README
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: resolving review comments
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: resolving review comments
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: adding uniffi binding docs
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: adding uniffi binding docs
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: adding missing files
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: remove unused imports
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: add demo video in docs
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: correct typo
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * fix: export uniffi::uniffi_bindgen_main
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * fix: export uniffi::uniffi_bindgen_main
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * fix: fixing wasm build error
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * fix: fixing wasm build error
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: adding title to logs
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: resolving clippy error about missing  function
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: resolving clippy error about missing  function
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: resolving clippy error about missing  function
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    * feat: resolving clippy error about empty line above main function
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>
    
    ---------
    
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>

---
+++ jans-cedarling/bindings/cedarling_uniffi/iOSApp/iOSApp/iOSAppApp.swift
@@ -0,0 +1,15 @@
+// This software is available under the Apache-2.0 license.
+// See https://www.apache.org/licenses/LICENSE-2.0.txt for full text.
+//
+// Copyright (c) 2024, Gluu, Inc.
+
+import SwiftUI
+
+@main
+struct iOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
