bd82f334df39b337e47e26c1ec503ccf797e81cf
YuriyZ <yzabrovarniy@gmail.com>
2025-03-17 12:32:00 +0200

    feat(jans-auth-server): introduce rate limit support for DCR (#10991)
    * feat(jans-auth-server): draft for rate limit service
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): rate limit - added cached body request
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): rate limit - added main filter
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): added rate_limit feature flag
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): missed enum
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): added rate limit exception with type
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): added bucket4j dependency for easy rate limiting support
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): code improvements for rate limit filter
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): added rate limit filter to web descriptor
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * feat(jans-auth-server): added rate limit feature flag to installation and test data loader
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * test(jans-auth-server): fixed ssa create test
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * test(jans-auth-server): covered rate limit service with test
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * test(jans-auth-server): added new test to testng
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    * doc(jans-auth-server): added rate limit documentation
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    ---------
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    Signed-off-by: Oleh Bozhok <olehbozhok@gmail.com>

---
+++ jans-auth-server/server/src/main/java/io/jans/as/server/rate/RateLimitService.java
@@ -0,0 +1,80 @@
+package io.jans.as.server.rate;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.jans.as.client.RegisterRequest;
+import io.jans.as.model.configuration.AppConfiguration;
+import io.jans.as.server.register.ws.rs.RegisterService;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+
+import java.time.Duration;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author Yuriy Z
+ */
+@ApplicationScoped
+public class RateLimitService {
+
+    private static final int DEFAULT_REQUEST_LIMIT = 10;
+    private static final int DEFAULT_PERIOD_LIMIT = 60;
+
+    private final ConcurrentHashMap<String, Bucket> buckets = new ConcurrentHashMap<>();
+
+    @Inject
+    private Logger log;
+
+    @Inject
+    private AppConfiguration appConfiguration;
+
+    @Inject
+    private RegisterService registerService;
+
+    public void validateRateLimitForRegister(String key) throws RateLimitedException {
+        int requestLimit = getRequestLimit(appConfiguration.getRateLimitRegistrationRequestCount());
+        int periodLimit = getPeriodLimit(appConfiguration.getRateLimitRegistrationPeriodInSeconds());
+
+        Bucket bucket = buckets.computeIfAbsent(key, k -> newBucket(requestLimit, periodLimit));
+        if (!bucket.tryConsume(1)) {
+            String msg = String.format("Rate limited '/register', key %s. Exceeds limit %s requests per %s seconds.", key, requestLimit, periodLimit);
+            log.debug(msg);
+            throw new RateLimitedException(RateLimitType.REGISTRATION, msg);
+        }
+    }
+
+    private int getRequestLimit(Integer requestLimit) {
+        if (requestLimit == null || requestLimit <= 0) {
+            return DEFAULT_REQUEST_LIMIT;
+        }
+        return requestLimit;
+    }
+
+    private int getPeriodLimit(Integer periodInSeconds) {
+        if (periodInSeconds == null || periodInSeconds <= 0) {
+            periodInSeconds = DEFAULT_PERIOD_LIMIT;
+        }
+        return periodInSeconds;
+    }
+
+    private Bucket newBucket(int requestLimit, int periodInSeconds) {
+        return Bucket.builder()
+                .addLimit(Bandwidth.builder()
+                        .capacity(requestLimit)
+                        .refillGreedy(requestLimit, Duration.ofSeconds(periodInSeconds))
+                        .build())
+                .build();
+    }
+
+    public RegisterRequest parseRegisterRequest(String body) {
+        try {
+            final JSONObject requestObject = registerService.parseRequestObjectWithoutValidation(body);
+            return RegisterRequest.fromJson(requestObject);
+        } catch (Exception e) {
+            log.error("Failed to parse registration request.", e);
+            return null;
+        }
+    }
+}
