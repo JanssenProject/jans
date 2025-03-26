aad24a35c86af8b39dd15d8b186553e35fd715d0
Devrim <devrimyatar@gluu.org>
2025-03-26 18:32:46 +0300

    fix(jans-linux-setup): owner of service status script (#11122)
    Signed-off-by: Mustafa Baser <mbaser@mail.com>

--- jans-linux-setup/jans_setup/setup_app/installers/jans.py
+++ jans-linux-setup/jans_setup/setup_app/installers/jans.py
@@ -267,7 +267,7 @@ class JansInstaller(BaseInstaller, SetupUtils):
             self.run([paths.cmd_chmod, '+x', script])
 
         # scripts that can be executed by user jetty
-        jetty_user_scripts = (Config.jansScriptFiles[3],)
+        jetty_user_scripts = (Config.jansScriptFiles[2], Config.jansScriptFiles[3])
         for script in jetty_user_scripts:
             script_fn = os.path.join(Config.jansOptBinFolder, os.path.basename(script))
             self.chown(script_fn, user=Config.root_user, group=Config.jetty_user)
