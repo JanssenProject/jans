e856545e45f4b23d1d0f2c437fe661fb9eeaa1ef
Richard Marin <34529290+rmarinn@users.noreply.github.com>
2025-05-15 17:56:13 +0800

    fix(jans-cedarling): fix logs in jwt module not getting displayed correctly (#11369)
    * fix(jans-cedarling): logs in jwt module not getting displayed correctly
    
    Signed-off-by: rmarinn <34529290+rmarinn@users.noreply.github.com>
    
    * refactor: make JwtLogEntry creation more flexible with the LogLevel
    
    Signed-off-by: rmarinn <34529290+rmarinn@users.noreply.github.com>
    
    * refactor: improve error logging in the JWT module
    
    Signed-off-by: rmarinn <34529290+rmarinn@users.noreply.github.com>
    
    * refactor: improve error logging in the JWT module
    
    ---------
    
    Signed-off-by: rmarinn <34529290+rmarinn@users.noreply.github.com>
    Co-authored-by: 0xtinkle <dbkey.one1@gmail.com>
    Co-authored-by: 0xtinkle <46352676+0xtinkle@users.noreply.github.com>
    Co-authored-by: Mohammad Abudayyeh <47318409+moabu@users.noreply.github.com>

--- jans-cedarling/cedarling/src/jwt/mod.rs
+++ jans-cedarling/cedarling/src/jwt/mod.rs
@@ -21,6 +21,7 @@ mod token;
 mod validator;
 
 use crate::JwtConfig;
+use crate::LogLevel;
 use crate::LogWriter;
 use crate::common::policy_store::TrustedIssuer;
 use crate::log::Logger;
@@ -94,19 +95,42 @@ impl JwtService {
         let key_service: Arc<_> =
             match (&config.jwt_sig_validation, &config.jwks, &trusted_issuers) {
                 // Case: no JWKS provided
-                (true, None, None) => Err(JwtServiceInitError::MissingJwksConfig)?,
+                (true, None, None) => {
+                    let err = JwtServiceInitError::MissingJwksConfig;
+                    logger.log_any(JwtLogEntry::new(format!("{err}"), Some(LogLevel::ERROR)));
+                    return Err(err);
+                },
                 // Case: Trusted issuers provided
-                (true, None, Some(issuers)) => Some(
-                    KeyService::new_from_trusted_issuers(issuers)
+                (true, None, Some(issuers)) => {
+                    let key_service = KeyService::new_from_trusted_issuers(issuers)
                         .await
-                        .map_err(JwtServiceInitError::KeyService)?,
-                ),
+                        .inspect_err(|e| {
+                            logger.log_any(JwtLogEntry::new(
+                                format!("failed to initialize the JWT validation service: {}", e),
+                                Some(LogLevel::ERROR),
+                            ))
+                        })
+                        .map_err(JwtServiceInitError::KeyService)?;
+                    Some(key_service)
+                },
                 // Case: Local JWKS provided
                 (true, Some(jwks), None) => {
-                    Some(KeyService::new_from_str(jwks).map_err(JwtServiceInitError::KeyService)?)
+                    let key_service = KeyService::new_from_str(jwks)
+                        .inspect_err(|e| {
+                            logger.log_any(JwtLogEntry::new(
+                                format!("failed to initialize the JWT validation service: {}", e),
+                                Some(LogLevel::ERROR),
+                            ))
+                        })
+                        .map_err(JwtServiceInitError::KeyService)?;
+                    Some(key_service)
                 },
                 // Case: Both a local JWKS and trusted issuers were provided
-                (true, Some(_), Some(_)) => Err(JwtServiceInitError::ConflictingJwksConfig)?,
+                (true, Some(_), Some(_)) => {
+                    let err = JwtServiceInitError::ConflictingJwksConfig;
+                    logger.log_any(JwtLogEntry::new(format!("{err}"), Some(LogLevel::ERROR)));
+                    return Err(err);
+                },
                 // Case: Signature validation is Off so no key service is needed.
                 _ => None,
             }
@@ -125,11 +149,10 @@ impl JwtService {
                 let origin = iss.oidc_endpoint.origin().ascii_serialization();
                 for (tkn, metadata) in iss.token_metadata.iter() {
                     if !metadata.trusted {
-                        if let Some(logger) = logger.as_ref() {
-                            logger.log_any(JwtLogEntry::system(format!(
-                                "skipping metadata for {tkn} since `trusted == false`"
-                            )));
-                        }
+                        logger.log_any(JwtLogEntry::new(
+                            format!("skipping metadata for {tkn} since `trusted == false`"),
+                            Some(LogLevel::WARN),
+                        ));
                         continue;
                     }
 
@@ -163,13 +186,6 @@ impl JwtService {
         Ok(Self { validators, logger })
     }
 
-    /// Helper for making [`crate::LogType::System`] logs.
-    fn system_log(&self, msg: String) {
-        if let Some(logger) = self.logger.as_ref() {
-            logger.log_any(JwtLogEntry::system(msg));
-        }
-    }
-
     pub async fn validate_tokens<'a>(
         &'a self,
         tokens: &'a HashMap<String, String>,
@@ -197,18 +213,34 @@ impl JwtService {
                 // we just ignore input tokens that are not defined
                 // in the policy store's tokens
                 if let Some(iss) = iss {
-                    self.system_log(format!(
-                        "ignoring {token_name} since it's from an untrusted issuer: '{iss}'"
+                    self.logger.log_any(JwtLogEntry::new(
+                        format!(
+                            "ignoring {token_name} since it's from an untrusted issuer: '{iss}'"
+                        ),
+                        Some(LogLevel::WARN),
                     ));
                 }
                 continue;
             };
 
-            let validated_jwt = validator
-                .process_jwt(jwt)
-                .map_err(|e| JwtProcessingError::InvalidToken(token_name.to_string(), e))?;
+            let validated_jwt = validator.process_jwt(jwt).map_err(|e| {
+                self.logger.log_any(JwtLogEntry::new(
+                    format!("failed to validate token: {e}"),
+                    Some(LogLevel::ERROR),
+                ));
+                JwtProcessingError::InvalidToken(token_name.to_string(), e)
+            })?;
+
             let claims = serde_json::from_value::<TokenClaims>(validated_jwt.claims)
+                .map_err(|err| {
+                    self.logger.log_any(JwtLogEntry::new(
+                        format!("failed to deserialize token claims: {err}"),
+                        Some(LogLevel::ERROR),
+                    ));
+                    err
+                })
                 .map_err(JwtProcessingError::StringDeserialization)?;
+
             validated_tokens.insert(
                 token_name.to_string(),
                 Token::new(token_name, claims, validated_jwt.trusted_iss),
