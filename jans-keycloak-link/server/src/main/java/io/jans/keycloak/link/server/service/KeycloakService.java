62befadc6b395e562f4b461a177780799ac11179
pujavs <pujas.works@gmail.com>
2025-05-29 17:46:14 +0530

    refactor: remove jans kc link and link from code #11446

---
+++ jans-keycloak-link/server/src/main/java/io/jans/keycloak/link/server/service/KeycloakService.java
@@ -0,0 +1,100 @@
+package io.jans.keycloak.link.server.service;
+
+import io.jans.keycloak.link.model.config.LinkConfiguration;
+import io.jans.keycloak.link.model.config.KeycloakConfiguration;
+import io.jans.keycloak.link.service.config.ConfigurationFactory;
+import io.jans.link.util.PropertyUtil;
+import io.jans.service.EncryptionService;
+import io.jans.util.security.StringEncrypter;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.admin.client.resource.RealmsResource;
+import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.slf4j.Logger;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
+public class KeycloakService implements Serializable{
+
+    @Inject
+    private Logger log;
+    @Inject
+    private ConfigurationFactory configurationFactory;
+
+    @Inject
+    private EncryptionService encryptionService;
+
+    boolean isEncoded;
+
+    public Keycloak getKeycloakInstance() {
+        LinkConfiguration linkConfiguration = configurationFactory.getAppConfiguration();
+
+        KeycloakConfiguration keycloakConfiguration = linkConfiguration.getKeycloakConfiguration();
+        String clientSecret = decryptString(keycloakConfiguration.getClientSecret());
+        if(clientSecret == null){
+            log.info("clientSecret is null or not encrypted");
+            clientSecret = keycloakConfiguration.getClientSecret();
+        }
+
+        String password = decryptString(keycloakConfiguration.getPassword());
+        if (password == null){
+            log.info("password is null or not encrypted");
+            password = keycloakConfiguration.getPassword();
+        }
+
+        ClassLoader oldClassLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
+        Keycloak instance = KeycloakBuilder.builder()
+                .serverUrl(keycloakConfiguration.getServerUrl())
+                .realm(keycloakConfiguration.getRealm())
+                .clientId(keycloakConfiguration.getClientId())
+                .clientSecret(clientSecret)
+                .grantType(keycloakConfiguration.getGrantType())
+                .username(keycloakConfiguration.getUsername())
+                .password(password)
+                .build();
+        log.info("getKeycloakInstance() instance::"+instance.getClass());
+
+        return instance;
+    }
+
+    @Produces
+    @ApplicationScoped
+    public List<UserRepresentation> getUserList() {
+        Keycloak keycloak = getKeycloakInstance();
+        List<UserRepresentation> finalUsersResourceList = new ArrayList<>();
+
+        RealmsResource RealmsResource = keycloak.realms();
+        List<RealmRepresentation> RealmResource = RealmsResource.findAll();
+
+        for(RealmRepresentation realmRepresentation : RealmResource){
+            log.info("check " + realmRepresentation.getRealm());
+            String realm = realmRepresentation.getRealm();
+            List<UserRepresentation> usersResourceList = keycloak.realm(realm).users().list();
+            finalUsersResourceList.addAll(usersResourceList);
+        }
+
+        for(UserRepresentation userRepresentation : finalUsersResourceList){
+            log.info("username  " + userRepresentation.getUsername());
+        }
+
+        return finalUsersResourceList;
+    }
+
+    public String decryptString(String value) {
+        try {
+            return encryptionService.decrypt(value);
+        } catch (StringEncrypter.EncryptionException ex) {
+            log.error("Failed to decrypt string: " + value, ex);
+        }
+
+        return null;
+    }
+}
---
+++ jans-link/CHANGELOG.md
@@ -0,0 +1,37 @@
+# Changelog
+
+## 1.0.21 (2023-12-14)
+
+
+### Features
+
+* **jans-keycloak-link:** implement password decryption ([#6335](https://github.com/JanssenProject/jans/issues/6335)) ([b6ec0c1](https://github.com/JanssenProject/jans/commit/b6ec0c1863696c2c07d915860a23100cca4901f5))
+* keycloak refactoring referred to Issue [#5330](https://github.com/JanssenProject/jans/issues/5330) review ([#6157](https://github.com/JanssenProject/jans/issues/6157)) ([7319120](https://github.com/JanssenProject/jans/commit/73191202f2e39bf040749b69a31d01bdfbcec8eb))
+
+
+### Documentation
+
+* **casa:** add sonarcloud properties to enable analysis ([#6190](https://github.com/JanssenProject/jans/issues/6190)) ([0599417](https://github.com/JanssenProject/jans/commit/059941724a7d6b5209253b93e2fdb90e64632523))
+
+## 1.0.19 (2023-10-11)
+
+
+### Features
+
+* BCFIPS support (sub-part 01) ([#5767](https://github.com/JanssenProject/jans/issues/5767)) ([d8cea00](https://github.com/JanssenProject/jans/commit/d8cea008a73ccecb1b734a010b9e9bdd363c8432))
+* **jans-link:** add health check service in jans link [#5608](https://github.com/JanssenProject/jans/issues/5608) ([#5685](https://github.com/JanssenProject/jans/issues/5685)) ([8bf964c](https://github.com/JanssenProject/jans/commit/8bf964c2cba412fd82c8b682047363d6e78e2f1e))
+* **jans-link:** break in modules ([#5547](https://github.com/JanssenProject/jans/issues/5547)) ([043e401](https://github.com/JanssenProject/jans/commit/043e40164fb23604bc12bb30215f96b96126d94a))
+* **jans-link:** change schema name cache refresh to link ([#5484](https://github.com/JanssenProject/jans/issues/5484)) ([412f753](https://github.com/JanssenProject/jans/commit/412f753f1f11337b51ab1e212b9bd013021102be))
+* keycloak refactoring referred to Issue [#5330](https://github.com/JanssenProject/jans/issues/5330) review ([#6157](https://github.com/JanssenProject/jans/issues/6157)) ([7319120](https://github.com/JanssenProject/jans/commit/73191202f2e39bf040749b69a31d01bdfbcec8eb))
+
+
+### Bug Fixes
+
+* **jans-link:** corrected jansTertiaryKeyValue attribute [#5695](https://github.com/JanssenProject/jans/issues/5695) ([#5696](https://github.com/JanssenProject/jans/issues/5696)) ([d78249f](https://github.com/JanssenProject/jans/commit/d78249f30dbcd58d739953f908b8a00f9dbe9f3d))
+* **jans-link:** fix null pointer exception for sourceConfig [#5709](https://github.com/JanssenProject/jans/issues/5709) ([#5710](https://github.com/JanssenProject/jans/issues/5710)) ([082365d](https://github.com/JanssenProject/jans/commit/082365decb63fb637c30fa36a48870f8fa250f31))
+* **jans-link:** polling interval and time difference calculation [#5667](https://github.com/JanssenProject/jans/issues/5667) ([#5668](https://github.com/JanssenProject/jans/issues/5668)) ([1094934](https://github.com/JanssenProject/jans/commit/10949340fad810af94ac54cd95776931e8cf7fca))
+
+
+### Documentation
+
+* **casa:** add sonarcloud properties to enable analysis ([#6190](https://github.com/JanssenProject/jans/issues/6190)) ([0599417](https://github.com/JanssenProject/jans/commit/059941724a7d6b5209253b93e2fdb90e64632523))
