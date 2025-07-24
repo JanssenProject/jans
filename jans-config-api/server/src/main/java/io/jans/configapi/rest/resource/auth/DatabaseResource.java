4cf2e113a9bbf706b55b732a86b5dd3ece413a40
pujavs <pujas.works@gmail.com>
2025-07-24 21:50:05 +0530

    feat(config-api): database endpoint
    Signed-off-by: pujavs <pujas.works@gmail.com>

--- jans-config-api/server/src/main/java/io/jans/configapi/rest/resource/auth/DatabaseResource.java
+++ jans-config-api/server/src/main/java/io/jans/configapi/rest/resource/auth/DatabaseResource.java
@@ -6,22 +6,14 @@
 
 package io.jans.configapi.rest.resource.auth;
 
-import io.jans.config.GluuConfiguration;
-import io.jans.configapi.core.model.ApiError;
+
 import io.jans.configapi.core.rest.ProtectedApi;
 import io.jans.configapi.model.configuration.ApiAppConfiguration;
-
 import io.jans.configapi.service.auth.DatabaseService;
-import io.jans.configapi.service.auth.LdapConfigurationService;
 import io.jans.configapi.util.ApiAccessConstants;
 import io.jans.configapi.util.ApiConstants;
-import io.jans.model.custom.script.model.CustomScript;
-import io.jans.model.ldap.GluuLdapConfiguration;
 import io.jans.orm.model.AttributeType;
-import io.jans.orm.model.PagedResult;
-import io.jans.service.custom.CustomScriptService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -29,18 +21,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.*;
 
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.Optional;
-
-import org.apache.commons.lang3.StringUtils;
-
 import jakarta.inject.Inject;
-import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -55,8 +37,6 @@ import org.slf4j.Logger;
 @Produces(MediaType.APPLICATION_JSON)
 public class DatabaseResource extends ConfigBaseResource {
 
-    private  Map<String, Map<String, AttributeType>> tableColumnsMap;
-
     @Inject
     Logger log;
 
@@ -66,26 +46,21 @@ public class DatabaseResource extends ConfigBaseResource {
     @Inject
     DatabaseService databaseService;
 
-    
-
     @Operation(summary = "Gets schema objects", description = "Gets schema objects.", operationId = "get-schema", tags = {
             "Database" }, security = @SecurityRequirement(name = "oauth2", scopes = {
                     ApiAccessConstants.DATABASE_READ_ACCESS, ApiAccessConstants.DATABASE_WRITE_ACCESS,
                     ApiAccessConstants.SUPER_ADMIN_READ_ACCESS }))
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = AuthenticationMethod.class), examples = @ExampleObject(name = "Response example", value = "example/database/tableInfo.json"))),
+            @ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = Map.class), examples = @ExampleObject(name = "Response example", value = "example/database/tableInfo.json"))),
             @ApiResponse(responseCode = "401", description = "Unauthorized"),
             @ApiResponse(responseCode = "500", description = "InternalServerError") })
     @GET
-    @ProtectedApi(scopes = { ApiAccessConstants.ACRS_READ_ACCESS }, groupScopes = {
-            ApiAccessConstants.ACRS_WRITE_ACCESS }, superScopes = { ApiAccessConstants.SUPER_ADMIN_READ_ACCESS })
+    @ProtectedApi(scopes = { ApiAccessConstants.DATABASE_READ_ACCESS }, groupScopes = {
+            ApiAccessConstants.DATABASE_WRITE_ACCESS }, superScopes = { ApiAccessConstants.SUPER_ADMIN_READ_ACCESS })
     public Response getDefaultAuthenticationMethod() {
         Map<String, Map<String, AttributeType>> tableColumnsMap = databaseService.getTableColumnsMap();
 
-
         return Response.ok(tableColumnsMap).build();
     }
-
-   
 
 }
--- jans-config-api/server/src/main/java/io/jans/configapi/service/auth/DatabaseService.java
+++ jans-config-api/server/src/main/java/io/jans/configapi/service/auth/DatabaseService.java
@@ -6,17 +6,14 @@
 
 package io.jans.configapi.service.auth;
 
-import io.jans.model.SearchRequest;
 import io.jans.orm.PersistenceEntryManager;
 import io.jans.orm.model.AttributeType;
-import io.jans.orm.model.PagedResult;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 import org.slf4j.Logger;
 
 import java.util.*;
-
 
 @ApplicationScoped
 public class DatabaseService {
@@ -29,50 +26,6 @@ public class DatabaseService {
 
     public Map<String, Map<String, AttributeType>> getTableColumnsMap() {
         return persistenceEntryManager.getTableColumnsMap();
-    }
-    
-    private PagedResult<Map<String, Map<String, AttributeType>>> getDatabaseObjectsPagedResult(SearchRequest searchRequest) {
-            logger.info("Database searchRequest:{}", searchRequest);
-
-
-        PagedResult<Map<String, Map<String, AttributeType>>> pagedResult = new PagedResult<>();
-            try {
-
-                Map<String, Map<String, AttributeType>> tableInfo = getTableColumnsMap();
-                logger.info("Database tableInfo:{}", tableInfo);
-                
-                if (tableInfo == null && tableInfo.isEmpty()) {
-                    return pagedResult;
-                }
-                    
-                int startIndex = 0;
-                int limit = searchRequest.getCount();
-                int totalEntries = tableInfo.size();
-                int toIndex = tableInfo.size();
-                
-                if(searchRequest !=null) {
-                    startIndex = searchRequest.getStartIndex();   
-                    toIndex = (startIndex + limit <= totalEntries) ? startIndex + limit
-                            : totalEntries;
-                }
-           
-                logger.info("Final startIndex:{}, limit:{}, toIndex:{}", startIndex, limit, toIndex);
-
-          
-                pagedResult.setStart(startIndex);
-                pagedResult.setEntriesCount(limit);
-                pagedResult.setTotalEntriesCount(totalEntries);
-               // pagedResult.setEntries(tableInfo);
-
-            } catch (IndexOutOfBoundsException ioe) {
-                
-            }
-        
-
-        logger.info("Database logPagedResult:{}", pagedResult);
-
-        return pagedResult;
-    }
-
-       
+    }    
+   
 }
