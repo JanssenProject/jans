8ac68f63cd9db0701e0206a19193017223576388
imran <imranishaq7071@gmail.com>
2025-07-31 18:43:22 +0500

    Revert "feat(jans-fido2): rebasing from main"
    This reverts commit 1d8b2b62633241e9c683ccd1e8e795c3f98f20d3, reversing
    changes made to c15f7f4b2ca0c167abf40528d52f4aa45abf9636.

--- jans-linux-setup/jans_setup/jans_setup.py
+++ jans-linux-setup/jans_setup/jans_setup.py
@@ -410,11 +410,6 @@ def main():
                     jansCliInstaller.start_installation()
                     jansCliInstaller.configure()
 
-            if (Config.installed_instance and jansCliInstaller.install_var in Config.addPostSetupService) or (
-                        not Config.installed_instance and Config.get(jansCliInstaller.install_var)):
-                    jansCliInstaller.start_installation()
-                    jansCliInstaller.configure()
-
             if Config.profile == 'jans':
 
                 if (Config.installed_instance and 'install_fido2' in Config.addPostSetupService) or (
