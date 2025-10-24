ae4bfc6c26ce1126b6d003ececb7b73a6f5f37fc
Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
2025-10-24 17:15:55 +0300

    fix(jans-cedarling): fix clippy warn result_large_err
    for more information go to
    https://rust-lang.github.io/rust-clippy/master/index.html#result_large_err
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>

--- jans-cedarling/cedarling/src/entity_builder/build_expr.rs
+++ jans-cedarling/cedarling/src/entity_builder/build_expr.rs
@@ -39,8 +39,9 @@ impl TknClaimAttrSrc {
 }
 
 impl EntityRefAttrSrc {
-    pub fn build_expr(&self, id: &str) -> Result<RestrictedExpression, BuildExprError> {
-        let uid = EntityUid::from_str(&format!("{}::\"{}\"", &self.0, id))?;
+    pub fn build_expr(&self, id: &str) -> Result<RestrictedExpression, Box<BuildExprError>> {
+        let uid = EntityUid::from_str(&format!("{}::\"{}\"", &self.0, id))
+            .map_err(|e| Box::new(e.into()))?;
         Ok(RestrictedExpression::new_entity_uid(uid))
     }
 }
@@ -162,6 +163,12 @@ impl BuildExprErrorVec {
     }
 }
 
+impl From<Vec<Box<BuildExprError>>> for BuildExprErrorVec {
+    fn from(errs: Vec<Box<BuildExprError>>) -> Self {
+        Self(errs.into_iter().map(|e| *e).collect())
+    }
+}
+
 impl From<Vec<BuildExprError>> for BuildExprErrorVec {
     fn from(errs: Vec<BuildExprError>) -> Self {
         Self(errs)
@@ -193,19 +200,19 @@ impl Display for BuildExprErrorVec {
 
 impl From<BuildExprError> for BuildExprErrorVec {
     fn from(err: BuildExprError) -> Self {
-        Self(Vec::from([err]))
+        Vec::from([err]).into()
     }
 }
 
 impl From<TypeMismatchError> for BuildExprErrorVec {
     fn from(err: TypeMismatchError) -> Self {
-        Self(Vec::from([BuildExprError::TypeMismatch(err)]))
+        Vec::from([BuildExprError::TypeMismatch(err)]).into()
     }
 }
 
 impl From<cedar_policy::ExpressionConstructionError> for BuildExprErrorVec {
     fn from(err: cedar_policy::ExpressionConstructionError) -> Self {
-        Self(Vec::from([BuildExprError::ConstructExpr(err)]))
+        Vec::from([BuildExprError::ConstructExpr(err)]).into()
     }
 }
 
