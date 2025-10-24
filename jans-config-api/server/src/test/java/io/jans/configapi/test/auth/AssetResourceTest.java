66c4c318d41df3156edf350d694e510938332efe
pujavs <pujas.works@gmail.com>
2025-10-24 21:23:15 +0530

    feat(config-api): feat(config-api): feature wise admin scope for endpoints#11633
    Signed-off-by: pujavs <pujas.works@gmail.com>

---
+++ jans-config-api/server/src/test/java/io/jans/configapi/test/auth/AssetResourceTest.java
@@ -0,0 +1,63 @@
+/*
+ * Janssen Project software is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2020, Janssen Project
+ */
+
+package io.jans.configapi.test.auth;
+
+import static org.testng.Assert.assertEquals;
+
+import io.jans.configapi.ConfigServerBaseTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import jakarta.ws.rs.HttpMethod;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.Invocation.Builder;
+
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
+public class AssetResourceTest extends ConfigServerBaseTest {
+	
+	private static final  INVALID_SCOPE = "https://jans.io/oauth/config/attributes.readonly";
+	private static final  SUPER_ADMIN_SCOPE = "https://jans.io/oauth/config/asset.admin";
+
+    @Parameters({ "test.issuer", "assetUrl" })
+    @Test
+    public void getAssetsUsingInvalidToken(final String issuer, final String assetUrl) {
+        log.info("getAssetsUsingInvalidToken() - accessToken:{}, issuer:{}, assetUrl:{}", accessToken, issuer,
+                assetUrl);
+		String invalidToken = this.getAccessTokenForGivenScope(INVALID_SCOPE);
+        log.info("getAssetsUsingInvalidToken() - invalidToken:{}, issuer:{}, assetUrl:{}", invalidToken, issuer, assetUrl);
+        Builder request = getResteasyService().getClientBuilder(issuer + assetUrl);
+        request.header(AUTHORIZATION, AUTHORIZATION_TYPE + " " + invalidToken);
+        request.header(CONTENT_TYPE, MediaType.APPLICATION_JSON);
+        Response response = request.get();
+        log.info("Response for getAssetsUsingInvalidToken() -  response:{}, response.getStatus():{}", response,
+                response.getStatus());
+        assertEquals(response.getStatus(), Status.UNAUTHORIZED.getStatusCode());
+
+    }
+	
+	@Parameters({ "test.issuer", "assetUrl" })
+    @Test
+    public void getAssets(final String issuer, final String assetUrl) {
+        log.info("getAssets() - accessToken:{}, issuer:{}, assetUrl:{}", accessToken, issuer,
+                assetUrl);
+		String adminToken = this.getAccessTokenForGivenScope(SUPER_ADMIN_SCOPE);
+        log.info("getAssets() - adminToken:{}, issuer:{}, assetUrl:{}", adminToken, issuer, assetUrl);
+        Builder request = getResteasyService().getClientBuilder(issuer + assetUrl);
+        request.header(AUTHORIZATION, AUTHORIZATION_TYPE + " " + adminToken);
+        request.header(CONTENT_TYPE, MediaType.APPLICATION_JSON);
+        Response response = request.get();
+        log.info("Response for getAssets() -  response:{}, response.getStatus():{}", response,
+                response.getStatus());
+        assertEquals(response.getStatus(), Status.OK.getStatusCode());
+
+    }
+
+
+}
---
+++ jans-config-api/server/src/test/java/io/jans/configapi/test/auth/StatResourceTest.java
@@ -0,0 +1,65 @@
+/*
+ * Janssen Project software is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2020, Janssen Project
+ */
+
+package io.jans.configapi.test.auth;
+
+import static org.testng.Assert.assertEquals;
+
+import io.jans.configapi.ConfigServerBaseTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import jakarta.ws.rs.HttpMethod;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.Invocation.Builder;
+
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
+public class StatResourceTest extends ConfigServerBaseTest {
+	
+	private static final  INVALID_SCOPE = "https://jans.io/oauth/config/attributes.readonly";
+	private static final  SUPER_ADMIN_SCOPE = "https://jans.io/oauth/config/stats.admin";
+	private static final  STAT_READONLY_ADMIN_SCOPE = "https://jans.io/oauth/config/stats.readonly";
+    private static final  STAT_SCOPE = "jans_stat";
+
+    @Parameters({ "test.issuer", "statUrl" })
+    @Test
+    public void getStatsInvalidToken(final String issuer, final String statUrl) {
+        log.info("getStatsInvalidToken() - accessToken:{}, issuer:{}, statUrl:{}", accessToken, issuer,
+                statUrl);
+		String invalidToken = this.getAccessTokenForGivenScope(INVALID_SCOPE);
+        log.info("getStatsInvalidToken() - invalidToken:{}, issuer:{}, statUrl:{}", invalidToken, issuer, statUrl);
+        Builder request = getResteasyService().getClientBuilder(issuer + statUrl);
+        request.header(AUTHORIZATION, AUTHORIZATION_TYPE + " " + invalidToken);
+        request.header(CONTENT_TYPE, MediaType.APPLICATION_JSON);
+        Response response = request.get();
+        log.info("Response for getStatsInvalidToken() - response:{}, response.getStatus():{}", response,
+                response.getStatus());
+        assertEquals(response.getStatus(), Status.UNAUTHORIZED.getStatusCode());
+
+    }
+	
+	@Parameters({ "test.issuer", "statUrl" })
+    @Test
+    public void getStats(final String issuer, final String statUrl) {
+        log.info("getAssets() - accessToken:{}, issuer:{}, statUrl:{}", accessToken, issuer,
+                statUrl);
+		String adminToken = this.getAccessTokenForGivenScope(SUPER_ADMIN_SCOPE);
+        log.info("getAssets() - adminToken:{}, issuer:{}, statUrl:{}", adminToken, issuer, statUrl);
+        Builder request = getResteasyService().getClientBuilder(issuer + statUrl);
+        request.header(AUTHORIZATION, AUTHORIZATION_TYPE + " " + adminToken);
+        request.header(CONTENT_TYPE, MediaType.APPLICATION_JSON);
+        Response response = request.get();
+        log.info("Response for getAssets() -  response:{}, response.getStatus():{}", response,
+                response.getStatus());
+        assertEquals(response.getStatus(), Status.OK.getStatusCode());
+
+    }
+
+
+}
