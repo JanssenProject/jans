d194763df5ee6bb967afd59362759d6e4ef72efe
yuriyz <yzabrovarniy@gmail.com>
2025-11-06 08:53:39 +0100

    feat(jans-auth-server): added methods to tx_token script for jwt payload modification and response modification #8376

--- jans-auth-server/server/src/main/java/io/jans/as/server/service/external/ExternalTxTokenService.java
+++ jans-auth-server/server/src/main/java/io/jans/as/server/service/external/ExternalTxTokenService.java
@@ -1,6 +1,7 @@
 package io.jans.as.server.service.external;
 
 import com.google.common.collect.Lists;
+import io.jans.as.model.token.JsonWebResponse;
 import io.jans.as.server.model.common.ExecutionContext;
 import io.jans.as.server.service.external.context.ExternalScriptContext;
 import io.jans.model.custom.script.CustomScriptType;
@@ -10,6 +11,7 @@ import io.jans.service.custom.script.ExternalScriptService;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.WebApplicationException;
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -18,6 +20,82 @@ public class ExternalTxTokenService extends ExternalScriptService {
 
     public ExternalTxTokenService() {
         super(CustomScriptType.TX_TOKEN);
+    }
+
+    public boolean modifyTokenPayload(CustomScriptConfiguration script, JsonWebResponse jsonWebResponse, ExternalScriptContext context) {
+        try {
+            log.trace("Executing 'modifyTokenPayload' method, script name: {}, jsonWebResponse: {}, context: {}", script.getName(), jsonWebResponse, context);
+            context.getExecutionContext().setScript(script);
+
+            TxTokenType scriptType = (TxTokenType) script.getExternalType();
+            final boolean result = scriptType.modifyTokenPayload(jsonWebResponse, context);
+            log.trace("Finished 'modifyTokenPayload' method, script name: {}, jsonWebResponse: {}, context: {}, result: {}", script.getName(), jsonWebResponse, context, result);
+
+            context.throwWebApplicationExceptionIfSet();
+            return result;
+        } catch (WebApplicationException e) {
+            throw e;
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex);
+            saveScriptError(script.getCustomScript(), ex);
+        }
+
+        return false;
+    }
+
+    public boolean modifyTokenPayload(JsonWebResponse jsonWebResponse, ExternalScriptContext context) {
+        List<CustomScriptConfiguration> scripts = getScripts(context.getExecutionContext());
+        if (scripts.isEmpty()) {
+            return false;
+        }
+        log.trace("Executing {} 'modifyTokenPayload' scripts.", scripts.size());
+
+        for (CustomScriptConfiguration script : scripts) {
+            if (!modifyTokenPayload(script, jsonWebResponse, context)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean modifyResponse(CustomScriptConfiguration script, JSONObject response, ExternalScriptContext context) {
+        try {
+            log.trace("Executing 'modifyResponse' method, script name: {}, response: {}, context: {}", script.getName(), response, context);
+            context.getExecutionContext().setScript(script);
+
+            TxTokenType scriptType = (TxTokenType) script.getExternalType();
+            final boolean result = scriptType.modifyResponse(response, context);
+            log.trace("Finished 'modifyResponse' method, script name: {}, response: {}, context: {}, result: {}", script.getName(), response, context, result);
+
+            context.throwWebApplicationExceptionIfSet();
+            return result;
+        } catch (WebApplicationException e) {
+            throw e;
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex);
+            saveScriptError(script.getCustomScript(), ex);
+        }
+
+        return false;
+    }
+
+    public boolean modifyResponse(JSONObject response, ExternalScriptContext context) {
+        List<CustomScriptConfiguration> scripts = getScripts(context.getExecutionContext());
+        if (scripts.isEmpty()) {
+            log.trace("No TxTokenType scripts found.");
+            return false;
+        }
+
+        log.trace("Executing {} 'modifyResponse' scripts.", scripts.size());
+
+        for (CustomScriptConfiguration script : scripts) {
+            if (!modifyResponse(script, response, context)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public int getTxTokenLifetimeInSeconds(CustomScriptConfiguration script, ExternalScriptContext context) {
