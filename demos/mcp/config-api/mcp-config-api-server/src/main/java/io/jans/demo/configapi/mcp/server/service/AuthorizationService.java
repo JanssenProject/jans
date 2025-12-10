65e8710165b0b51b9eb547b48462130f7b4a4503
ossdhaval <343411+ossdhaval@users.noreply.github.com>
2025-12-10 14:56:55 +0530

    fix:add authz scaffolding
    Signed-off-by: ossdhaval <343411+ossdhaval@users.noreply.github.com>

--- demos/mcp/config-api/mcp-config-api-server/src/main/java/io/jans/demo/configapi/mcp/server/service/AuthorizationService.java
+++ demos/mcp/config-api/mcp-config-api-server/src/main/java/io/jans/demo/configapi/mcp/server/service/AuthorizationService.java
@@ -1,8 +1,10 @@
 package io.jans.demo.configapi.mcp.server.service;
 
-import io.jans.demo.configapi.mcp.server.model.Permission;
-import io.jans.demo.configapi.mcp.server.model.Role;
-import io.jans.demo.configapi.mcp.server.model.User;
+import io.jans.cedarling.binding.wrapper.CedarlingAdapter;
+import uniffi.cedarling_uniffi.*;
+import io.jans.cedarling.binding.wrapper.CedarlingAdapter;
+
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,8 +18,31 @@ import java.util.concurrent.ConcurrentHashMap;
 public class AuthorizationService {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthorizationService.class);
+    private static CedarlingAdapter cedarlingAdapter;
 
     public static AuthorizationService initAuthorizationService() {
+
+        String bootstrapJsonStr = """
+                    {
+                    "CEDARLING_APPLICATION_NAME":   "MyApp",
+                    "CEDARLING_POLICY_STORE_ID":    "your-policy-store-id",
+                    "CEDARLING_USER_AUTHZ":         "enabled",
+                    "CEDARLING_WORKLOAD_AUTHZ":     "enabled",
+                    "CEDARLING_LOG_LEVEL":          "INFO",
+                    "CEDARLING_LOG_TYPE":           "std_out",
+                    "CEDARLING_POLICY_STORE_LOCAL_FN": "/path/to/policy-store.json"
+                }
+                """;
+
+        try {
+            cedarlingAdapter = new CedarlingAdapter();
+            cedarlingAdapter.loadFromJson(bootstrapJsonStr);
+        } catch (CedarlingException e) {
+            System.out.println("Unable to initialize Cedarling" + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Unable to initialize Cedarling" + e.getMessage());
+        }
+
         AuthorizationService authorizationService = new AuthorizationService();
         return authorizationService;
     }
@@ -25,10 +50,26 @@ public class AuthorizationService {
     /**
      * Check if current user has a specific permission
      */
-    public boolean checkAuthorization() {
+    public boolean checkAuthorization(Map<String, String> tokens, String action, String resource, String context)
+            throws UnauthorizedException {
 
-        return Boolean.TRUE;
-
+        // Perform authorization
+        try {
+            AuthorizeResult result = cedarlingAdapter.authorize(tokens, action, new JSONObject(resource),
+                    new JSONObject(context));
+            if (result.getDecision()) {
+                return Boolean.TRUE;
+            } else {
+                return Boolean.FALSE;
+            }
+        } catch (AuthorizeException e) {
+            logger.error("Unable to authorize user", e);
+            // TODO: don't swallow these exceptions
+        } catch (EntityException e) {
+            logger.error("Unable to authorize user", e);
+            // TODO: don't swallow these exceptions
+        }
+        return Boolean.FALSE;
     }
 
     /**
