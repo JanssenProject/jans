a19d82dd3766b734f23a8dc08a9a02f626c8013a
YuriyZ <yzabrovarniy@gmail.com>
2025-12-17 14:06:57 +0200

    fix(jans-auth-server): put in access_token and id_token jwt only explicitly requested and allowed claims (#12848)
    * fix(jans-auth-server): put in access_token and id_token jwt only explicitly requested and allowed claims
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    Signed-off-by: yuriyz <yzabrovarniy@gmail.com>
    
    * Improvements
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    
    Signed-off-by: yuriyz <yzabrovarniy@gmail.com>
    
    ---------
    
    Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    Signed-off-by: yuriyz <yzabrovarniy@gmail.com>

--- jans-auth-server/model/src/main/java/io/jans/as/model/configuration/AppConfiguration.java
+++ jans-auth-server/model/src/main/java/io/jans/as/model/configuration/AppConfiguration.java
@@ -100,6 +100,9 @@ public class AppConfiguration implements Configuration {
     @DocProperty(description = "URL for Pushed Authorisation Request (PAR) Endpoint")
     private String parEndpoint;
 
+    @DocProperty(description = "Boolean value to indicate whether to include requested claims in id_token (specified by 'claims' parameter at Authorization Endpoint). Default value is false to put minimize claims in token (for security).")
+    private Boolean includeRequestedClaimsInIdToken = false;
+
     @DocProperty(description = "Boolean value to indicate whether to allow client assertion 'aud' without strict server issuer match. Default value is false which means that server requires strict match.", defaultValue = "false")
     private Boolean allowClientAssertionAudWithoutStrictIssuerMatch = false;
 
@@ -2054,6 +2057,15 @@ public class AppConfiguration implements Configuration {
 
     public void setParEndpoint(String parEndpoint) {
         this.parEndpoint = parEndpoint;
+    }
+
+    public Boolean getIncludeRequestedClaimsInIdToken() {
+        if (includeRequestedClaimsInIdToken == null) includeRequestedClaimsInIdToken = false;
+        return includeRequestedClaimsInIdToken;
+    }
+
+    public void setIncludeRequestedClaimsInIdToken(Boolean includeRequestedClaimsInIdToken) {
+        this.includeRequestedClaimsInIdToken = includeRequestedClaimsInIdToken;
     }
 
     public Boolean getAllowClientAssertionAudWithoutStrictIssuerMatch() {
