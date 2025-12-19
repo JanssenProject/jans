053abaf62379d0981674000473b47388afc261d8
Yuriy <Yuriy.Movchan@gmail.com>
2025-12-19 22:35:22 +0300

    feat(core): update package name
    Signed-off-by: Yuriy <Yuriy.Movchan@gmail.com>

--- jans-core/service/src/main/java/io/jans/service/logger/LoggerService.java
+++ jans-core/service/src/main/java/io/jans/service/logger/LoggerService.java
@@ -319,7 +319,7 @@ public abstract class LoggerService {
 		int count = 0;
 		for (org.apache.logging.log4j.core.Logger logger : ctx.getLoggers()) {
 		    String loggerName = logger.getName();
-		    if (loggerName.startsWith("org.gluu")) {
+		    if (loggerName.startsWith("io.jans")) {
 		        if (logger.getLevel() != newLevel) {
 		            count++;
 		            logger.setLevel(newLevel);
