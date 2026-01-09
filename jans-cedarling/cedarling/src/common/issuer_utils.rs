3e61fb139ae94aef168b6bdc17a176d24f3b8f31
Oleh <6554798+olehbozhok@users.noreply.github.com>
2026-01-09 17:09:09 +0200

    fix(jans-cedarling): Fix cedarling to handle iss in different cases (#12982)
    fix(jans-cedarling): Fix cedarling to handle domains in different cases (upper and lower)
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>

--- jans-cedarling/cedarling/src/common/issuer_utils.rs
+++ jans-cedarling/cedarling/src/common/issuer_utils.rs
@@ -7,15 +7,27 @@
 /// This handles cases where IDPs like Auth0 return issuers with trailing slashes
 /// but the policy store configuration might not have them
 pub(crate) fn normalize_issuer(issuer: &str) -> String {
-    issuer.trim_end_matches('/').to_string()
+    // Parse the issuer as a URL be consistent after parsing trusted issuers URL by `url` crate.
+    if let Ok(url) = url::Url::parse(issuer) {
+        url.to_string().trim_end_matches('/').to_string()
+    } else {
+        issuer.trim_end_matches('/').to_string()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test_utils::assert_eq;
 
     #[test]
     fn test_normalize_issuer() {
+        // Test that uppercase letters are lowered
+        assert_eq!(
+            normalize_issuer("https://DEV-1e737fn2gji0j3fe.us.auth0.com/"),
+            "https://dev-1e737fn2gji0j3fe.us.auth0.com"
+        );
+
         // Test that trailing slashes are removed
         assert_eq!(
             normalize_issuer("https://dev-1e737fn2gji0j3fe.us.auth0.com/"),
