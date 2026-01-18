b0fd0e7e27fe38053c31fbd106a32155b5e55028
duttarnab <arnab.bdutta@gmail.com>
2026-01-18 17:39:41 +0530

    fix(jans-config-api): address code-rabbit comments
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>

--- jans-config-api/plugins/admin-ui-plugin/src/main/java/io/jans/ca/plugin/adminui/service/auth/OAuth2Service.java
+++ jans-config-api/plugins/admin-ui-plugin/src/main/java/io/jans/ca/plugin/adminui/service/auth/OAuth2Service.java
@@ -17,6 +17,7 @@ import io.jans.ca.plugin.adminui.utils.ErrorResponse;
 import io.jans.configapi.core.model.adminui.AUIConfiguration;
 import io.jans.configapi.core.model.adminui.AdminUISession;
 import io.jans.orm.PersistenceEntryManager;
+import io.jans.orm.exception.EntryPersistenceException;
 import io.jans.orm.search.filter.Filter;
 import io.jans.service.EncryptionService;
 import jakarta.inject.Inject;
@@ -213,6 +214,9 @@ public class OAuth2Service extends BaseService {
         try {
             adminUISession = entryManager
                     .find(AdminUISession.class, getDnForSession(sessionId));
+        } catch (EntryPersistenceException e) {
+            //do not throw error is the record is not present in database
+            return adminUISession;
         } catch (Exception ex) {
             log.error(SID_GET_MSG + "{}", sessionId, ex);
             throw new ApplicationException(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), SID_GET_MSG + sessionId);
