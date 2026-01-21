8b9d3550a017a7b1ee688facf348cc6b72980a30
duttarnab <arnab.bdutta@gmail.com>
2026-01-21 12:16:43 +0530

    fix: move AdminUICookieFilter to Admin UI plugin
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>

--- jans-config-api/plugins/admin-ui-plugin/src/main/java/io/jans/ca/plugin/adminui/service/adminui/AdminUISessionService.java
+++ jans-config-api/plugins/admin-ui-plugin/src/main/java/io/jans/ca/plugin/adminui/service/adminui/AdminUISessionService.java
@@ -1,4 +1,4 @@
-package io.jans.configapi.service.auth;
+package io.jans.ca.plugin.adminui.service.adminui;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
