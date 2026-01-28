2435a0532ac779dee0a044275280c67c19672c9b
yuriyz <yzabrovarniy@gmail.com>
2026-01-28 20:25:16 +0200

    AuthZEN upgrade: made Context free form and added verbose tests for Context and AccessEvaluationRequest Signed-off-by: YuriyZ <yzabrovarniy@gmail.com>
    Signed-off-by: yuriyz <yzabrovarniy@gmail.com>

--- jans-core/model/src/main/java/io/jans/model/authzen/Context.java
+++ jans-core/model/src/main/java/io/jans/model/authzen/Context.java
@@ -1,46 +1,60 @@
 package io.jans.model.authzen;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonNode;
 
 import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
+ * AuthZEN Context - completely unstructured key-value map.
+ * Per AuthZEN spec, context is environmental data with no fixed schema.
+ *
  * @author Yuriy Z
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Context implements Serializable {
 
-    @JsonProperty("properties")
-    private JsonNode properties;
+    private final Map<String, Object> attributes = new LinkedHashMap<>();
 
-    @JsonProperty("time")
-    private String time;
-
-    public JsonNode getProperties() {
-        return properties;
+    public Context() {
     }
 
-    public Context setProperties(JsonNode properties) {
-        this.properties = properties;
+    public Context(Map<String, Object> attributes) {
+        if (attributes != null) {
+            this.attributes.putAll(attributes);
+        }
+    }
+
+    @JsonAnySetter
+    public Context put(String key, Object value) {
+        attributes.put(key, value);
         return this;
     }
 
-    public String getTime() {
-        return time;
+    @JsonAnyGetter
+    public Map<String, Object> getAttributes() {
+        return attributes;
     }
 
-    public Context setTime(String time) {
-        this.time = time;
-        return this;
+    public Object get(String key) {
+        return attributes.get(key);
+    }
+
+    public boolean containsKey(String key) {
+        return attributes.containsKey(key);
+    }
+
+    public boolean isEmpty() {
+        return attributes.isEmpty();
     }
 
     @Override
     public String toString() {
         return "Context{" +
-                "properties=" + properties +
-                ", time=" + time +
+                "attributes=" + attributes +
                 '}';
     }
 }
