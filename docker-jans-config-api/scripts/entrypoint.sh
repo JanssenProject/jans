9b1751f5ffd85331c53cd4b2ddf8576a42fe59d5
iromli <isman.firmansyah@gmail.com>
2026-02-04 20:29:19 +0700

    refactor: allow custom java.security file
    Signed-off-by: iromli <isman.firmansyah@gmail.com>

--- docker-jans-config-api/scripts/entrypoint.sh
+++ docker-jans-config-api/scripts/entrypoint.sh
@@ -39,8 +39,7 @@ get_java_options() {
     # if admin-ui plugin is enabled, override java.security via Java option
     sec_prop_option=""
     if [ -f /opt/jans/jetty/jans-config-api/custom/libs/admin-ui-plugin.jar ]; then
-        cp /app/templates/jans-config-api/java.security /opt/jans/jetty/jans-config-api/etc/security/java.security
-        sec_prop_option="-Djava.security.properties=etc/security/java.security"
+        sec_prop_option="-Djava.security.properties=etc/jetty/security/java.security"
     fi
 
     if [ -n "${CN_CONFIG_API_JAVA_OPTIONS}" ]; then
