85608774a61a0691747adeadf56783ca8f2fafb7
Oleh <6554798+olehbozhok@users.noreply.github.com>
2026-02-24 11:09:54 +0200

    fix(jans-cedarling): Fix deprecated method usage (#13286)
    * chore(jans-cedarling): fix WASM usage `unchecked_from_js`
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    * feat(jans-cedarling): update `rand` and `uuid7` module
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    * chore(jans-cedarling):  remove `lazy_static`
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    * feat(jans-cedarling): update `jsonwebtoken`
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    * feat(jans-cedarling): update `jsonwebkey`
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    * feat(jans-cedarling): update `reqwest`
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    * feat(jans-cedarling): update `typed-builder ` and `derive_more`
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    * feat(jans-cedarling): update `typed-builder `, `derive_more` and `strum`
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    * feat(jans-cedarling): update `datalogic-rs`
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    * feat(jans-cedarling): update `zip`
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    * feat(jans-cedarling): update `criterion` and `pprof`
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    * feat(jans-cedarling): update testing crates
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    * chore(jans-cedarling): fix clippy issues
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    * chore(jans-cedarling):  remove clippy rule that refer to the unreachable function
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    * chore(jans-cedarling): prettify toml file
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    * chore(jans-cedarling): fix masking uuid4 according to RFC 4122
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Oleh Bozhok <6554798+olehbozhok@users.noreply.github.com>

--- jans-cedarling/cedarling/src/tests/json_logic.rs
+++ jans-cedarling/cedarling/src/tests/json_logic.rs
@@ -11,10 +11,10 @@ use std::str::FromStr;
 use std::sync::LazyLock;
 
 use crate::common::json_rules::ApplyRuleError;
+use crate::log::gen_uuid4;
 use cedar_policy::{Decision, EntityUid, Response};
 use serde_json::json;
 use test_utils::assert_eq;
-use uuid7::uuid4;
 
 use crate::{AuthorizeResult, JsonRule};
 
@@ -78,7 +78,7 @@ fn get_result(
         workload_response,
         person_response,
         // just randomly generated UUID
-        uuid4(),
+        gen_uuid4(),
     )
 }
 
@@ -349,12 +349,20 @@ fn test_using_eq_operator() {
     }))
     .unwrap();
 
-    let _ = get_result(None, Some(true), &rule)
-        .expect_err("should throw an error when workload is None because throw Nan");
+    let result =
+        get_result(None, Some(true), &rule).expect("should not throw error with missing variable");
+    assert_eq!(
+        result.decision, true,
+        "Decision should be ALLOW because user is ALLOW"
+    );
 
-    // we should get error in this case, but looks like it is not throwing error because we use OR operator and first condition is true
-    let _ = get_result(Some(true), None, &rule)
-        .expect("we don't expect error because we use OR operator and first condition is true");
+    // when workload is ALLOW and user missing, workload clause true, OR true
+    let result =
+        get_result(Some(true), None, &rule).expect("should not throw error with missing variable");
+    assert_eq!(
+        result.decision, true,
+        "Decision should be ALLOW because workload is ALLOW"
+    );
 }
 
 /// Test with only workload principal.
@@ -424,8 +432,12 @@ fn test_where_compare_op_eq_with_bool() {
     }))
     .unwrap();
 
-    let _ = get_result(Some(true), Some(true), &rule)
-        .expect_err("should fail when comparing using `==` operator with bool");
+    let result = get_result(Some(true), Some(true), &rule)
+        .expect("should not fail when comparing using `==` operator with bool");
+    assert_eq!(
+        result.decision, false,
+        "Decision should be DENY because string 'ALLOW' does not equal true"
+    );
 }
 
 #[test]
@@ -456,9 +468,14 @@ fn test_with_multiple_principals() {
         ),
     ]);
 
-    let result =
-        AuthorizeResult::new_for_many_principals(&rule, principal_responses, None, None, uuid4())
-            .expect("Shouldn't fail to create an AuthorizeResult with multiple principals.");
+    let result = AuthorizeResult::new_for_many_principals(
+        &rule,
+        principal_responses,
+        None,
+        None,
+        gen_uuid4(),
+    )
+    .expect("Shouldn't fail to create an AuthorizeResult with multiple principals.");
 
     assert_eq!(result.decision, true, "Decision should be ALLOW");
 
