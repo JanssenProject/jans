b89d37887a9e5e16a335666832271c52230e0f6d
iromli <isman.firmansyah@gmail.com>
2026-03-05 01:40:52 +0700

    refactor: implement timeout and remove f-string style for logger
    Signed-off-by: iromli <isman.firmansyah@gmail.com>

--- docker-jans-all-in-one/app/jans_aio/monitor.py
+++ docker-jans-all-in-one/app/jans_aio/monitor.py
@@ -18,28 +18,28 @@ def watch_jans_auth():
     try:
         host = os.environ.get("CN_AUTH_JETTY_HOST", "127.0.0.1")  # nosec: B104
         port = os.environ.get("CN_AUTH_JETTY_PORT", "8081")
-        req = requests.get(f"http://{host}:{port}/sys/health-check")
+        req = requests.get(f"http://{host}:{port}/sys/health-check", timeout=15)
 
         if req.ok:
-            logger.info(f"Got response from jans-auth: status={req.status_code}, body={req.json()}")
+            logger.info("Got response from jans-auth: status=%s, body=%s", req.status_code, req.json())
         else:
-            logger.warning(f"Got unexpected response from jans-auth: status={req.status_code}, body={req.text}")
+            logger.warning("Got unexpected response from jans-auth: status=%s, body=%s", req.status_code, req.text)
             logger.info("Checking network")
             out, err, retcode = exec_cmd("netstat -peanut")
 
             if retcode != 0:
                 err = err or out
-                logger.warning(f"Unable to run netstat command; reason={err.decode()}")
+                logger.warning("Unable to run netstat command; reason=%s", err.decode())
             else:
-                logger.info(f"Got network status: {out.decode()}")
-    except requests.exceptions.ConnectionError as exc:
-        logger.warning(f"Got unexpected response from jans-auth; reason={exc}")
+                logger.info("Got network status: %s", out.decode())
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout) as exc:
+        logger.warning("Got unexpected response from jans-auth; reason=%s", exc)
 
 
 def main():
     while True:
         watch_jans_auth()
-        time.sleep(30)
+        time.sleep(60)
 
 
 if __name__ == "__main__":
