7b340b356bd9231d3cb6848b790e41e6156db5a5
SafinWasi <6601566+SafinWasi@users.noreply.github.com>
2026-03-04 09:59:50 +0600

    chore: use json config file
    Signed-off-by: SafinWasi <6601566+SafinWasi@users.noreply.github.com>

--- jans-cedarling/cedarling_opa/plugins/cedarling_opa/plugin.go
+++ jans-cedarling/cedarling_opa/plugins/cedarling_opa/plugin.go
@@ -4,36 +4,21 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"sync"
-
 	"github.com/JanssenProject/jans/jans-cedarling/bindings/cedarling_go"
 	"github.com/open-policy-agent/opa/v1/plugins"
 	"github.com/open-policy-agent/opa/v1/util"
+	"sync"
 )
 
 // #cgo LDFLAGS: -L. -lcedarling_go
 import "C"
-
-var bootstrapConfig string = `
-{
-        "CEDARLING_APPLICATION_NAME": "TestApp",
-        "CEDARLING_USER_AUTHZ": "enabled",
-        "CEDARLING_WORKLOAD_AUTHZ": "enabled",
-        "CEDARLING_JWT_SIG_VALIDATION": "disabled",
-        "CEDARLING_JWT_STATUS_VALIDATION": "disabled",
-        "CEDARLING_ID_TOKEN_TRUST_MODE": "never",
-        "CEDARLING_LOG_TYPE": "std_out",
-        "CEDARLING_LOG_TTL": 60,
-        "CEDARLING_LOG_LEVEL": "DEBUG",
-        "CEDARLING_JWT_SIGNATURE_ALGORITHMS_SUPPORTED": ["HS256"]
-}
-`
 
 const PluginName = "cedarling_opa"
 
 type Config struct {
 	BootstrapConfig map[string]any `json:"bootstrap_config"`
 	Stderr          bool           `json:"stderr"` // false => stdout, true => stderr
+	PolicyStore     map[string]any `json:"policy_store"`
 }
 
 type CedarPlugin struct {
@@ -46,12 +31,14 @@ type CedarPlugin struct {
 func (p *CedarPlugin) Start(ctx context.Context) error {
 	p.mtx.Lock()
 	defer p.mtx.Unlock()
-	policyStoreJsonPath := "../test_files/policy-store_ok.yaml"
+	//policyStoreJsonPath := "../test_files/policy-store_ok.yaml"
 	var config map[string]any = p.config.BootstrapConfig
-	config["CEDARLING_POLICY_STORE_LOCAL_FN"] = policyStoreJsonPath
-	if err := json.Unmarshal([]byte(bootstrapConfig), &config); err != nil {
+	var policy_store map[string]any = p.config.PolicyStore
+	policy_store_bytes, err := json.Marshal(policy_store)
+	if err != nil {
 		return err
 	}
+	config["CEDARLING_POLICY_STORE_LOCAL"] = string(policy_store_bytes)
 	fmt.Println("Initializing cedarling")
 	instance, err := cedarling_go.NewCedarling(config)
 	if err != nil {
