578937e30f0c68fe6fa325fbca39d94afd78e618
duttarnab <arnab.bdutta@gmail.com>
2026-03-27 18:40:14 +0530

    feat: address code-rabbit comment
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>

--- jans-config-api/plugins/admin-ui-plugin/src/main/java/io/jans/ca/plugin/adminui/model/adminui/AdminUIPolicyStore.java
+++ jans-config-api/plugins/admin-ui-plugin/src/main/java/io/jans/ca/plugin/adminui/model/adminui/AdminUIPolicyStore.java
@@ -7,10 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.core.MediaType;
 import org.jboss.resteasy.annotations.providers.multipart.PartType;
-
 import java.io.InputStream;
-import java.io.Serial;
-import java.io.Serializable;
 
 public class AdminUIPolicyStore {
 
