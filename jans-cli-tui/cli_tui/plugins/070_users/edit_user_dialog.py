44bb2c6f5801c95d643159326e9198a3d3f78831
Mustafa Baser <mbaser@mail.com>
2026-04-09 13:16:17 +0300

    fix(jans-cli-tui): user claim country code should be two characters
    Signed-off-by: Mustafa Baser <mbaser@mail.com>

--- jans-cli-tui/cli_tui/plugins/070_users/edit_user_dialog.py
+++ jans-cli-tui/cli_tui/plugins/070_users/edit_user_dialog.py
@@ -14,7 +14,7 @@ from prompt_toolkit.widgets import Button, Label, CheckboxList, Dialog, TextArea
 from prompt_toolkit.eventloop import get_event_loop
 
 from utils.multi_lang import _
-from utils.static import DialogResult
+from utils.static import DialogResult, common_strings
 from utils.utils import DialogUtils, common_data, check_email
 from wui_components.jans_dialog_with_nav import JansDialogWithNav
 from wui_components.jans_cli_dialog import JansGDialog
@@ -336,6 +336,16 @@ class EditUserDialog(JansGDialog, DialogUtils):
             if ca['name'] == 'memberOf':
                 user_info['customAttributes'].append(ca)
                 break
+
+        for ca in user_info.get('customAttributes', []):
+            if ca['name'] == 'c':
+                for cval in ca['values']:
+                    if len(cval) != 2:
+                        common_data.app.show_message(
+                                _(common_strings.oops),
+                                _("Country should be two letters code ")
+                            )
+                        return
 
         async def coroutine():
             operation_id = 'put-user' if self.data.get('baseDn') else 'post-user'
