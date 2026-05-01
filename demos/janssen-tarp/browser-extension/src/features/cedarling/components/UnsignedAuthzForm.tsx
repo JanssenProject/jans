b28a218e277801b5c174201e965851e57556469f
duttarnab <arnab.bdutta@gmail.com>
2026-05-01 15:40:59 +0530

    feat: address code rabbit comments
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>

--- demos/janssen-tarp/browser-extension/src/features/cedarling/components/UnsignedAuthzForm.tsx
+++ demos/janssen-tarp/browser-extension/src/features/cedarling/components/UnsignedAuthzForm.tsx
@@ -37,7 +37,7 @@ export default function UnsignedAuthzForm({ data }: UnsignedAuthzFormProps) {
         });
       }
     });
-  }, [data]);
+  }, []);
 
   const triggerCedarlingAuthzRequest = async () => {
     setAuthzResult('');
