97ea482ecaa289b7fba28b16e45f5a254f31106c
Mustafa Baser <mbaser@mail.com>
2026-07-25 14:55:12 +0300

    fix(jans-linux-setup): coderabbitai suggestions
    Signed-off-by: Mustafa Baser <mbaser@mail.com>

--- jans-linux-setup/jans_setup/setup_app/utils/base.py
+++ jans-linux-setup/jans_setup/setup_app/utils/base.py
@@ -382,15 +382,15 @@ def download(url, dst, verbose=False, headers=None):
     opener.addheaders = headers
     urllib.request.install_opener(opener)
 
-    mylog(f"Downloading {url} to {dst}")
+    dst_tmp_fn = dst + '~' + os.urandom(8).hex()
+    mylog(f"Downloading {url} to {dst_tmp_fn}")
     download_tries = 1
     download_ok = False
+
     while download_tries < 4:
         try:
-            dst_tmp_fn = dst + '~' + os.urandom(4).hex()
             urllib.request.urlretrieve(url, dst_tmp_fn)
-            shutil.move(dst_tmp_fn, dst)
-            mylog("Download size: {} bytes".format(os.path.getsize(dst)))
+            mylog(f"Download size: {os.path.getsize(dst_tmp_fn)} bytes")
             time.sleep(0.1)
             download_ok = True
         except (HTTPError, URLError, OSError):
@@ -400,10 +400,14 @@ def download(url, dst, verbose=False, headers=None):
             if download_tries >=3:
                 time.sleep(retry_sec)
         except Exception as e:
-            mylog("Can't contuinue {e}")
+            mylog(f"Can't contuinue {e}")
             sys.exit(2)
         else:
             break
+
+    if download_ok and os.path.exists(dst_tmp_fn):
+        mylog(f"Moving file {dst_tmp_fn} to {dst}")
+        shutil.move(dst_tmp_fn, dst)
 
     if not download_ok:
         env_var = re.sub(r'[^_a-zA-Z0-9]', '_', fn)
