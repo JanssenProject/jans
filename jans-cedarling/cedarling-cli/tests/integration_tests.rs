d15fcc8e22e17888644c2768c3aca42a1a6ae094
haileyesus2433 <haileyesusbe@gmail.com>
2026-07-31 13:10:21 -0400

    feat(jans-cedarling): add integration tests for CLI validation command
    - Added tests for `validate` command success, failure, and missing
      source.
    - Updated policy stores to include existence checks for optional
      attributes.
    - Adjusted test expectations to reflect accurate validation results.
    
    Signed-off-by: haileyesus2433 <haileyesusbe@gmail.com>

--- jans-cedarling/cedarling-cli/tests/integration_tests.rs
+++ jans-cedarling/cedarling-cli/tests/integration_tests.rs
@@ -29,3 +29,42 @@ fn test_integration_fail() {
         .code(1)
         .stdout(predicates::str::contains("results: 0 passed, 1 failed"));
 }
+
+#[test]
+fn test_validate_pass() {
+    let mut cmd = Command::cargo_bin("cedarling_cli").unwrap();
+    cmd.env("CEDARLING_JWT_SIG_VALIDATION", "disabled")
+        .arg("--policy-store")
+        .arg("tests/test_store.yaml")
+        .arg("validate");
+
+    cmd.assert()
+        .success()
+        .stdout(predicates::str::contains("schema   ................ OK"));
+}
+
+#[test]
+fn test_validate_fail() {
+    let mut cmd = Command::cargo_bin("cedarling_cli").unwrap();
+    cmd.env("CEDARLING_JWT_SIG_VALIDATION", "disabled")
+        .arg("--policy-store")
+        .arg("tests/test_store_schema_error.yaml")
+        .arg("validate");
+
+    cmd.assert()
+        .failure()
+        .code(1)
+        .stdout(predicates::str::contains("schema   ................ FAIL"));
+}
+
+#[test]
+fn test_validate_missing_source() {
+    let mut cmd = Command::cargo_bin("cedarling_cli").unwrap();
+    cmd.env("CEDARLING_JWT_SIG_VALIDATION", "disabled")
+        .arg("validate");
+
+    cmd.assert()
+        .failure()
+        .code(2)
+        .stderr(predicates::str::contains("failed to resolve bootstrap config from env/file"));
+}
