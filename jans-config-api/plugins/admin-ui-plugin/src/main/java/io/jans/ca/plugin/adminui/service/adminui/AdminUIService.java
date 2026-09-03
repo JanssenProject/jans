18a519205f527387d79bfc0496b5a917db09abb7
duttarnab <arnab.bdutta@gmail.com>
2026-09-03 14:39:24 +0530

    fix: errror due to removal on unused policy-store config from Admin UI plugin
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>

--- jans-config-api/plugins/admin-ui-plugin/src/main/java/io/jans/ca/plugin/adminui/service/adminui/AdminUIService.java
+++ jans-config-api/plugins/admin-ui-plugin/src/main/java/io/jans/ca/plugin/adminui/service/adminui/AdminUIService.java
@@ -102,14 +102,7 @@ public class AdminUIService {
                 adminConf.getMainSettings().getUiConfig().setCedarlingLogType(appConfigResponse.getCedarlingLogType().getValue());
                 auiConfigurationService.getAUIConfiguration().setCedarlingLogType(appConfigResponse.getCedarlingLogType());
             }
-            if (!Strings.isNullOrEmpty(appConfigResponse.getAuiPolicyStoreUrl())) {
-                adminConf.getMainSettings().getUiConfig().setAuiPolicyStoreUrl(appConfigResponse.getAuiPolicyStoreUrl());
-                auiConfigurationService.getAUIConfiguration().setAuiCedarlingPolicyStoreUrl(appConfigResponse.getAuiPolicyStoreUrl());
-            }
-            if (!Strings.isNullOrEmpty(appConfigResponse.getAuiDefaultPolicyStorePath())) {
-                adminConf.getMainSettings().getUiConfig().setAuiDefaultPolicyStorePath(appConfigResponse.getAuiDefaultPolicyStorePath());
-                auiConfigurationService.getAUIConfiguration().setAuiCedarlingDefaultPolicyStorePath(appConfigResponse.getAuiDefaultPolicyStorePath());
-            }
+
             entryManager.merge(adminConf);
             return getAdminUIEditableConfiguration();
         } catch (Exception e) {
