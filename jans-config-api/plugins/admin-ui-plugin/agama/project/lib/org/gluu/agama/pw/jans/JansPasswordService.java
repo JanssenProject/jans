ce4b8f2948e06a761ca607f58dc1e331957d2e34
duttarnab <arnab.bdutta@gmail.com>
2026-09-04 16:39:12 +0530

    fix: address code-rabbit comments
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>

--- jans-config-api/plugins/admin-ui-plugin/agama/project/lib/org/gluu/agama/pw/jans/JansPasswordService.java
+++ jans-config-api/plugins/admin-ui-plugin/agama/project/lib/org/gluu/agama/pw/jans/JansPasswordService.java
@@ -66,8 +66,16 @@ public class JansPasswordService extends PasswordService {
         if (invalidLoginCount != null) {
             currentFailCount = Integer.parseInt(invalidLoginCount) + 1;
         }
-        String currentStatus = currentUser.getStatus() != null ? currentUser.getStatus().getValue() : "";
+        String currentStatus = currentUser.getStatus() != null ? currentUser.getStatus().getValue() : null;
         logger.info("Current user status is: {}", currentStatus);
+        if (currentStatus == null) {
+            logger.info("User status is not set. Marking the user as inactive.");
+            String object_to_store = "{'locked': 'true'}";
+            currentUser.setStatus(GluuStatus.INACTIVE); // this line of code required to save user's status
+            setCustomAttribute(currentUser, JANS_STATUS, INACTIVE);
+            cacheService.put(DEFAULT_LOCK_EXP_TIME, CACHE_PREFIX + username, object_to_store);
+            return "User status is not set. Marking the user as inactive."
+        }
         if (currentFailCount < DEFAULT_MAX_LOGIN_ATTEMPT) {
             int remainingCount = DEFAULT_MAX_LOGIN_ATTEMPT - currentFailCount;
             logger.info("Remaining login count: {} for user {}", remainingCount, username);
