2d9024f81f62e1d32834818fe25dd256c3d6c7ab
duttarnab <arnab.bdutta@gmail.com>
2026-09-04 14:01:41 +0530

    fix: address code-rabbit comments
    Signed-off-by: duttarnab <arnab.bdutta@gmail.com>

--- jans-config-api/plugins/admin-ui-plugin/agama/project/lib/org/gluu/agama/pw/jans/JansPasswordService.java
+++ jans-config-api/plugins/admin-ui-plugin/agama/project/lib/org/gluu/agama/pw/jans/JansPasswordService.java
@@ -65,7 +65,7 @@ public class JansPasswordService extends PasswordService {
         if (invalidLoginCount != null) {
             currentFailCount = Integer.parseInt(invalidLoginCount) + 1;
         }
-        String currentStatus = currentUser.getStatus();
+        String currentStatus = currentUser.getStatus() != null ? currentUser.getStatus().getValue() : "";
         logger.info("Current user status is: {}", currentStatus);
         if (currentFailCount < DEFAULT_MAX_LOGIN_ATTEMPT) {
             int remainingCount = DEFAULT_MAX_LOGIN_ATTEMPT - currentFailCount;
