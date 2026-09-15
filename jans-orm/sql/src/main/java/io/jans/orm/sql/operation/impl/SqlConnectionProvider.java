07ca8d26432be90c9ed5b22636a078c4d95b0a57
Yuriy <Yuriy.Movchan@gmail.com>
2026-09-15 13:57:04 +0000

    feat(orm): use sslRootKeystoreUrl
    Signed-off-by: Yuriy <Yuriy.Movchan@gmail.com>

--- jans-orm/sql/src/main/java/io/jans/orm/sql/operation/impl/SqlConnectionProvider.java
+++ jans-orm/sql/src/main/java/io/jans/orm/sql/operation/impl/SqlConnectionProvider.java
@@ -161,7 +161,7 @@ public class SqlConnectionProvider {
 		resolveTrustStorePropertyPath("sslfactoryarg");
 
 		// MySQL
-		resolveTrustStorePropertyPath("trustCertificateKeyStoreUrl");
+		resolveTrustStorePropertyPath("sslRootKeystoreUrl");
 
 		if (props.containsKey("db.disable.time-zone")) {
 			disableTimeZone = StringHelper.toBoolean(props.getProperty("db.disable.time-zone"), false);
